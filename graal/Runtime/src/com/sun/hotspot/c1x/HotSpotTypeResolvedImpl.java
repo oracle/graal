08b823e50275384370aa931c74ccbfcc730ca4ae
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2011-04-22 21:52:33 +0200

    Small adjustment to assertion and constants size calculation.

--- graal/Runtime/src/com/sun/hotspot/c1x/HotSpotTypeResolvedImpl.java
+++ graal/Runtime/src/com/sun/hotspot/c1x/HotSpotTypeResolvedImpl.java
@@ -209,7 +209,6 @@ public final class HotSpotTypeResolvedImpl extends HotSpotType implements HotSpo
             result = new HotSpotField(compiler, this, name, type, offset, flags);
             fieldCache.put(id, result);
         } else {
-            assert result.type().equals(type);
             assert result.name().equals(name);
             assert result.accessFlags() == flags;
         }
