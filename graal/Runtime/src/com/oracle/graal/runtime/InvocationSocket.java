632d4777a5d7951c8b317d5b34ddd4193391986d
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2011-04-26 21:48:09 +0200

    Copyright rebranding. Update checkstyle file.

--- graal/Runtime/src/com/oracle/graal/runtime/InvocationSocket.java
+++ graal/Runtime/src/com/oracle/graal/runtime/InvocationSocket.java
@@ -1,22 +1,24 @@
 /*
- * Copyright (c) 2010 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * Sun Microsystems, Inc. has intellectual property rights relating to technology embodied in the product
- * that is described in this document. In particular, and without limitation, these intellectual property
- * rights may include one or more of the U.S. patents listed at http://www.sun.com/patents and one or
- * more additional patents or pending patent applications in the U.S. and in other countries.
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
  *
- * U.S. Government Rights - Commercial software. Government users are subject to the Sun
- * Microsystems, Inc. standard license agreement and applicable provisions of the FAR and its
- * supplements.
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * Use is subject to license terms. Sun, Sun Microsystems, the Sun logo, Java and Solaris are trademarks or
- * registered trademarks of Sun Microsystems, Inc. in the U.S. and other countries. All SPARC trademarks
- * are used under license and are trademarks or registered trademarks of SPARC International, Inc. in the
- * U.S. and other countries.
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * UNIX is a registered trademark in the U.S. and other countries, exclusively licensed through X/Open
- * Company, Ltd.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package com.oracle.graal.runtime;
 
@@ -188,7 +190,7 @@ public class InvocationSocket {
             Object in;
             try {
                 in = input.readObject();
-            } catch(EOFException e) {
+            } catch (EOFException e) {
                 if (eofExpected) {
                     return null;
                 }
@@ -254,7 +256,7 @@ public class InvocationSocket {
                 } finally {
                     if (DEBUG) {
                         if (result instanceof Result) {
-                            Logger.endScope(" = " + ((Result)result).result);
+                            Logger.endScope(" = " + ((Result) result).result);
                         } else {
                             Logger.endScope(" = " + result);
                         }
