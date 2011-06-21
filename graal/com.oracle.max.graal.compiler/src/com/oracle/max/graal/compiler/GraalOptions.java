066db7acdf32002bf2e946c097bb32041ea5be62
Lukas Stadler <lukas.stadler@jku.at>
2011-06-21 12:15:38 +0200

    disable UseExceptionProbability by default (not stable yet)

--- graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
+++ graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
@@ -113,7 +113,7 @@ public final class GraalOptions {
     public static boolean GenLIR                             = true;
     public static boolean GenCode                            = true;
     public static boolean UseBranchPrediction                = true;
-    public static boolean UseExceptionProbability            = true;
+    public static boolean UseExceptionProbability            = ____;
     public static int     MatureInvocationCount              = 100;
 
     public static boolean UseConstDirectCall                 = ____;
