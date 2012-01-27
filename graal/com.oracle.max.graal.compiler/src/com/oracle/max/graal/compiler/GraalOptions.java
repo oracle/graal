49eb641ffb1f09a3229ad215bd724cbcdb09822c
Lukas Stadler <lukas.stadler@jku.at>
2012-01-27 17:28:24 +0100

    enable UseExceptionProbability by default and fix the InliningUtil to correctly handle FrameState.AFTER_EXCEPTION_BCI for Invokes without an exception edge

--- graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
+++ graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
@@ -129,7 +129,7 @@ public final class GraalOptions {
     public static boolean GenLIR                             = true;
     public static boolean GenCode                            = true;
     public static boolean UseBranchPrediction                = true;
-    public static boolean UseExceptionProbability            = ____;
+    public static boolean UseExceptionProbability            = true;
     public static boolean AllowExplicitExceptionChecks       = true;
     public static boolean OmitHotExceptionStacktrace         = ____;
     public static int     MatureInvocationCount              = 100;
