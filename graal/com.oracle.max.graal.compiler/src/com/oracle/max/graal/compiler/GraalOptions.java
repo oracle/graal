f1d035907bf361c0f099334532067a69d4cc819a
Christian Haeubl <christian.haeubl@oracle.com>
2012-02-27 19:16:28 -0800

    prefer inlining within loops

--- graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
+++ graal/com.oracle.max.graal.compiler/src/com/oracle/max/graal/compiler/GraalOptions.java
@@ -75,7 +75,7 @@ public final class GraalOptions {
 
     // absolute probability analysis
     public static boolean ProbabilityAnalysis                = true;
-    public static int     LoopFrequencyPropagationPolicy     = -3;
+    public static int     LoopFrequencyPropagationPolicy     = -1;
 
     // profiling information
     public static int     MatureExecutionsBranch             = 50;
