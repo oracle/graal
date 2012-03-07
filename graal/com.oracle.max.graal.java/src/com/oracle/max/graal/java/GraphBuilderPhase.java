4f15c86f4802865be68c9a35d60b1b802b774c4e
Christian Haeubl <christian.haeubl@oracle.com>
2012-03-07 10:03:52 -0800

    fix after merge

--- graal/com.oracle.max.graal.java/src/com/oracle/max/graal/java/GraphBuilderPhase.java
+++ graal/com.oracle.max.graal.java/src/com/oracle/max/graal/java/GraphBuilderPhase.java
@@ -557,7 +557,7 @@ public final class GraphBuilderPhase extends Phase {
         if (probability < 0) {
             assert probability == -1 : "invalid probability";
             Debug.log("missing probability in %s at bci %d", method, bci());
-            takenProbability = 0.5;
+            probability = 0.5;
         }
 
         CompareNode condition = currentGraph.unique(new CompareNode(x, cond, y));
