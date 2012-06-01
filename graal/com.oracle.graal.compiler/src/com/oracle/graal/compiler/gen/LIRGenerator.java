5ad308d7a77228465c0f9d0a274d7b7d1ff84b96
Doug Simon <doug.simon@oracle.com>
2012-06-01 11:03:50 +0200

    allow a framestate to flow though a framestate-less loop

--- graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/gen/LIRGenerator.java
+++ graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/gen/LIRGenerator.java
@@ -352,9 +352,20 @@ public abstract class LIRGenerator extends LIRGeneratorTool {
             for (Block pred : block.getPredecessors()) {
                 if (fs == null) {
                     fs = blockLastState.get(pred);
-                } else if (fs != blockLastState.get(pred)) {
-                    fs = null;
-                    break;
+                } else {
+                    if (blockLastState.get(pred) == null) {
+                        // Only a back edge can have a null state for its enclosing block.
+                        assert pred.getEndNode() instanceof LoopEndNode;
+
+                        if (block.getBeginNode().stateAfter() == null) {
+                            // We'll assert later that the begin and end of a framestate-less loop
+                            // share the frame state that flowed into the loop
+                            blockLastState.put(pred, fs);
+                        }
+                    } else if (fs != blockLastState.get(pred)) {
+                        fs = null;
+                        break;
+                    }
                 }
             }
             if (GraalOptions.TraceLIRGeneratorLevel >= 2) {
@@ -432,6 +443,10 @@ public abstract class LIRGenerator extends LIRGeneratorTool {
         if (GraalOptions.TraceLIRGeneratorLevel >= 1) {
             TTY.println("END Generating LIR for block B" + block.getId());
         }
+
+        // Check that the begin and end of a framestate-less loop
+        // share the frame state that flowed into the loop
+        assert blockLastState.get(block) == null || blockLastState.get(block) == lastState;
 
         blockLocks.put(currentBlock, curLocks);
         blockLastState.put(block, lastState);
