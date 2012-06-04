178f4ca6b4dcb2bc8fd01e21e93a894594b22c14
Gilles Duboscq <duboscq@ssw.jku.at>
2012-06-04 16:01:40 +0200

    Made nodeClass final, remove unused import introduced in a merge

--- graal/com.oracle.graal.graph/src/com/oracle/graal/graph/Node.java
+++ graal/com.oracle.graal.graph/src/com/oracle/graal/graph/Node.java
@@ -124,7 +124,7 @@ public abstract class Node implements Cloneable, Formattable {
     private NodeUsagesList usages;
     private Node predecessor;
     private int modCount;
-    private NodeClass nodeClass;
+    private final NodeClass nodeClass;
 
     public Node() {
         this.graph = null;
