cad4c17d795e03610a0dcdb426a0ba0f274027bc
Doug Simon <doug.simon@oracle.com>
2012-06-12 23:29:10 +0200

    removed unused @NodePhase

--- graal/com.oracle.graal.graph/src/com/oracle/graal/graph/Node.java
+++ graal/com.oracle.graal.graph/src/com/oracle/graal/graph/Node.java
@@ -90,12 +90,6 @@ public abstract class Node implements Cloneable, Formattable {
         Class value() default NodeIntrinsic.class;
     }
 
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target(ElementType.METHOD)
-    public static @interface NodePhase {
-        Class value() default NodePhase.class;
-    }
-
     public interface ValueNumberable {}
 
     public interface IterableNodeType {}
