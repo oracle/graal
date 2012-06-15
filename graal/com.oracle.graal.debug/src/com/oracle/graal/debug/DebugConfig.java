1baaefb1283751ee068cf03c1c4075c100b4b1f0
Doug Simon <doug.simon@oracle.com>
2012-06-15 14:42:11 +0200

    added support for extending the filtering context of a DebugConfig

--- graal/com.oracle.graal.debug/src/com/oracle/graal/debug/DebugConfig.java
+++ graal/com.oracle.graal.debug/src/com/oracle/graal/debug/DebugConfig.java
@@ -28,19 +28,37 @@ import java.util.*;
 
 public interface DebugConfig {
     /**
+     * Determines if logging is enabled in the {@linkplain Debug#currentScope() current debug scope}.
+     *
      * @see Debug#log(String, Object...)
      */
     boolean isLogEnabled();
 
     /**
+     * Determines if metering is enabled in the {@linkplain Debug#currentScope() current debug scope}.
+     *
      * @see Debug#metric(String)
      */
     boolean isMeterEnabled();
 
     /**
+     * Determines if dumping is enabled in the {@linkplain Debug#currentScope() current debug scope}.
+     *
      * @see Debug#dump(Object, String, Object...)
      */
     boolean isDumpEnabled();
+
+    /**
+     * Adds an object the context used by this configuration to do filtering.
+     */
+    void addToContext(Object o);
+
+    /**
+     * Removes an object the context used by this configuration to do filtering.
+     *
+     * This should only removes extra context added by {@link #addToContext(Object)}.
+     */
+    void removeFromContext(Object o);
 
     /**
      * @see Debug#timer(String)
