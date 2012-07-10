c377d65507650eb6591411d1c2880d305c5ea149
Lukas Stadler <lukas.stadler@jku.at>
2012-07-10 10:49:31 +0200

    less aggressive tail duplication

--- graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/GraalOptions.java
+++ graal/com.oracle.graal.compiler/src/com/oracle/graal/compiler/GraalOptions.java
@@ -73,8 +73,8 @@ public final class GraalOptions {
     public static int     ForcedInlineEscapeWeight           = 10;
     public static boolean PrintEscapeAnalysis                = ____;
 
-    public static double TailDuplicationProbability          = 0.2;
-    public static int    TailDuplicationTrivialSize          = 6;
+    public static double TailDuplicationProbability          = 0.5;
+    public static int    TailDuplicationTrivialSize          = 1;
 
     // absolute probability analysis
     public static boolean ProbabilityAnalysis                = true;
