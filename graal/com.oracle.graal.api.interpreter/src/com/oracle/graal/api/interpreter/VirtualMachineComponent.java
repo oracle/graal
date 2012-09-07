4418ddcc60c86ad41c31745ffc5e5554a8305b10
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2012-09-07 11:10:48 +0200

    Move VirtualMachineComponent to interpreter project.

--- graal/com.oracle.graal.api.interpreter/src/com/oracle/graal/api/interpreter/VirtualMachineComponent.java
+++ graal/com.oracle.graal.api.interpreter/src/com/oracle/graal/api/interpreter/VirtualMachineComponent.java
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.graal.api.meta;
+package com.oracle.graal.api.interpreter;
 
 
 public interface VirtualMachineComponent {
