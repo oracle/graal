4bb3f9663abd5a49330b2063a02966b48dc0f561
Doug Simon <doug.simon@oracle.com>
2012-09-20 17:09:53 +0200

    Added usage to javadoc for BreakpointNode

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/BreakpointNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/BreakpointNode.java
@@ -32,6 +32,17 @@ import com.oracle.graal.nodes.type.*;
  * interest in the Java ABI specified parameter locations corresponding to the
  * kinds of the values. That is, the arguments are set up as if the breakpoint instruction
  * was a call to a compiled Java method.
+ * <p>
+ * A breakpoint is usually place by defining a node intrinsic method as follows:
+ * <pre>
+ *     {@literal @}NodeIntrinsic(BreakpointNode.class)
+ *     static void breakpoint(Object object, Word mark, Word value) {
+ *          throw new GraalInternalError("");
+ *     }
+ * </pre>
+ *
+ * Note that the signature is arbitrary. It's sole purpose is to capture values you
+ * may want to inspect in the native debugger when the breakpoint is hit.
  */
 public final class BreakpointNode extends FixedWithNextNode implements LIRLowerable {
 
