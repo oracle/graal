0a410577f1a6fba4489a7500b30c31cba15cdd33
Lukas Stadler <lukas.stadler@jku.at>
2012-11-05 12:58:32 +0100

    PEA bugfix: look up scalar aliases when initializing virtual object entries

--- graal/com.oracle.graal.virtual/src/com/oracle/graal/virtual/phases/ea/PartialEscapeClosure.java
+++ graal/com.oracle.graal.virtual/src/com/oracle/graal/virtual/phases/ea/PartialEscapeClosure.java
@@ -93,7 +93,11 @@ class PartialEscapeClosure extends BlockIteratorClosure<BlockState> {
                 } else {
                     effects.addFloatingNode(virtualObject);
                 }
-                state.addObject(virtualObject, new ObjectState(virtualObject, op.fieldState(), 0));
+                ValueNode[] fieldState = op.fieldState();
+                for (int i = 0; i < fieldState.length; i++) {
+                    fieldState[i] = state.getScalarAlias(fieldState[i]);
+                }
+                state.addObject(virtualObject, new ObjectState(virtualObject, fieldState, 0));
                 state.addAndMarkAlias(virtualObject, (ValueNode) node, usages);
                 effects.deleteFixedNode((FixedWithNextNode) node);
                 virtualIds++;
