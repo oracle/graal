02fbf8a1c94507b613c913a8d872a6eee809d969
Lukas Stadler <lukas.stadler@jku.at>
2012-11-09 16:11:12 +0100

    fix for bug in GraphBuilderPhase (which caused a NPE in insertLoopProxies)

--- graal/com.oracle.graal.java/src/com/oracle/graal/java/GraphBuilderPhase.java
+++ graal/com.oracle.graal.java/src/com/oracle/graal/java/GraphBuilderPhase.java
@@ -173,11 +173,16 @@ public final class GraphBuilderPhase extends Phase {
 
         // finish the start block
         ((StateSplit) lastInstr).setStateAfter(frameState.create(0));
+
+        currentBlock = blockMap.startBlock;
+        blockMap.startBlock.entryState = frameState;
         if (blockMap.startBlock.isLoopHeader) {
+            // TODO(lstadler,gduboscq) createTarget might not be safe at this position, since it expects currentBlock,
+            // etc. to be set up correctly. A better solution to this problem of start blocks that are loop headers
+            // would be to create a dummy block in BciBlockMapping.
             appendGoto(createTarget(blockMap.startBlock, frameState));
         } else {
             blockMap.startBlock.firstInstruction = lastInstr;
-            blockMap.startBlock.entryState = frameState;
         }
 
         for (Block block : blockMap.blocks) {
