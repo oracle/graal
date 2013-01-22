8628b57274ab0691c130547b3a51e6430f5bbecd
Doug Simon <doug.simon@oracle.com>
2013-01-22 20:44:15 +0100

    disabled auto-formatting of manually formatted code

--- graal/com.oracle.truffle.codegen.processor/src/com/oracle/truffle/codegen/processor/node/NodeData.java
+++ graal/com.oracle.truffle.codegen.processor/src/com/oracle/truffle/codegen/processor/node/NodeData.java
@@ -283,6 +283,7 @@ public class NodeData extends Template {
         return specializations;
     }
 
+    // @formatter:off
     public String dump() {
         StringBuilder b = new StringBuilder();
         b.append(String.format("[name = %s\n" +
@@ -300,6 +301,7 @@ public class NodeData extends Template {
                         ));
         return b.toString();
     }
+    // @formatter:on
 
     private static String dumpList(Object[] array) {
         if (array == null) {
