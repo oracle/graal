33bf8cb187ad14d2fe9ee94cc66e4f74d2b07f77
Lukas Stadler <lukas.stadler@jku.at>
2013-02-01 17:34:01 +0100

    fix probabilities for explicit NPE and AIOOBE in GraphBuilderPhase

--- graal/com.oracle.graal.java/src/com/oracle/graal/java/GraphBuilderPhase.java
+++ graal/com.oracle.graal.java/src/com/oracle/graal/java/GraphBuilderPhase.java
@@ -829,7 +829,7 @@ public final class GraphBuilderPhase extends Phase {
         }
         BlockPlaceholderNode trueSucc = currentGraph.add(new BlockPlaceholderNode());
         BlockPlaceholderNode falseSucc = currentGraph.add(new BlockPlaceholderNode());
-        IfNode ifNode = currentGraph.add(new IfNode(currentGraph.unique(new IsNullNode(receiver)), trueSucc, falseSucc, 0.5));
+        IfNode ifNode = currentGraph.add(new IfNode(currentGraph.unique(new IsNullNode(receiver)), trueSucc, falseSucc, 0.1));
 
         append(ifNode);
         lastInstr = falseSucc;
@@ -855,7 +855,7 @@ public final class GraphBuilderPhase extends Phase {
     private void emitBoundsCheck(ValueNode index, ValueNode length) {
         BlockPlaceholderNode trueSucc = currentGraph.add(new BlockPlaceholderNode());
         BlockPlaceholderNode falseSucc = currentGraph.add(new BlockPlaceholderNode());
-        IfNode ifNode = currentGraph.add(new IfNode(currentGraph.unique(new IntegerBelowThanNode(index, length)), trueSucc, falseSucc, 0.5));
+        IfNode ifNode = currentGraph.add(new IfNode(currentGraph.unique(new IntegerBelowThanNode(index, length)), trueSucc, falseSucc, 0.9));
 
         append(ifNode);
         lastInstr = trueSucc;
