c3755d08ae5dc6f070ed7e1ba86a8ac51f329490
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2013-03-18 00:33:31 +0100

    Make r10 allocatable.

--- graal/com.oracle.graal.hotspot.amd64/src/com/oracle/graal/hotspot/amd64/AMD64HotSpotRegisterConfig.java
+++ graal/com.oracle.graal.hotspot.amd64/src/com/oracle/graal/hotspot/amd64/AMD64HotSpotRegisterConfig.java
@@ -76,7 +76,7 @@ public class AMD64HotSpotRegisterConfig implements RegisterConfig {
 
     private static Register[] initAllocatable() {
         Register[] allocatable = {
-                        rax, rbx, rcx, rdx, /*rsp,*/ rbp, rsi, rdi, r8, r9, /* r10, */r11, r12, r13, r14, /*r15, */
+                        rax, rbx, rcx, rdx, /*rsp,*/ rbp, rsi, rdi, r8, r9,  r10, r11, r12, r13, r14, /*r15, */
                         xmm0, xmm1, xmm2,  xmm3,  xmm4,  xmm5,  xmm6,  xmm7,
                         xmm8, xmm9, xmm10, xmm11, xmm12, xmm13, xmm14, xmm15
                     };
