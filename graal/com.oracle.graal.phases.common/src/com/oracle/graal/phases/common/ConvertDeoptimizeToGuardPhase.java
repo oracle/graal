4ea56a537be8d903cbb0ca0a3fd74096ecf59130
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2013-03-19 18:05:35 +0100

    Killing cfg path to deoptimization.

--- graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/ConvertDeoptimizeToGuardPhase.java
+++ graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/ConvertDeoptimizeToGuardPhase.java
@@ -28,6 +28,7 @@ import com.oracle.graal.debug.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.nodes.*;
 import com.oracle.graal.nodes.java.*;
+import com.oracle.graal.nodes.util.*;
 import com.oracle.graal.phases.*;
 
 public class ConvertDeoptimizeToGuardPhase extends Phase {
@@ -75,27 +76,36 @@ public class ConvertDeoptimizeToGuardPhase extends Phase {
                     visitDeoptBegin(findBeginNode(deopt), deopt, graph);
                 }
             }
+            return;
         } else if (deoptBegin.predecessor() instanceof IfNode) {
             IfNode ifNode = (IfNode) deoptBegin.predecessor();
             BeginNode otherBegin = ifNode.trueSuccessor();
             LogicNode conditionNode = ifNode.condition();
-            if (conditionNode instanceof InstanceOfNode || conditionNode instanceof InstanceOfDynamicNode) {
+            if (!(conditionNode instanceof InstanceOfNode) && !(conditionNode instanceof InstanceOfDynamicNode)) {
                 // TODO The lowering currently does not support a FixedGuard as the usage of an
                 // InstanceOfNode. Relax this restriction.
+                FixedGuardNode guard = graph.add(new FixedGuardNode(conditionNode, deopt.reason(), deopt.action(), deoptBegin == ifNode.trueSuccessor()));
+                FixedWithNextNode pred = (FixedWithNextNode) ifNode.predecessor();
+                if (deoptBegin == ifNode.trueSuccessor()) {
+                    graph.removeSplitPropagate(ifNode, ifNode.falseSuccessor());
+                } else {
+                    graph.removeSplitPropagate(ifNode, ifNode.trueSuccessor());
+                }
+                Debug.log("Converting %s on %-5s branch of %s to guard for remaining branch %s.", deopt, deoptBegin == ifNode.trueSuccessor() ? "true" : "false", ifNode, otherBegin);
+                FixedNode next = pred.next();
+                pred.setNext(guard);
+                guard.setNext(next);
                 return;
             }
-            FixedGuardNode guard = graph.add(new FixedGuardNode(conditionNode, deopt.reason(), deopt.action(), deoptBegin == ifNode.trueSuccessor()));
-            FixedWithNextNode pred = (FixedWithNextNode) ifNode.predecessor();
-            if (deoptBegin == ifNode.trueSuccessor()) {
-                graph.removeSplitPropagate(ifNode, ifNode.falseSuccessor());
-            } else {
-                graph.removeSplitPropagate(ifNode, ifNode.trueSuccessor());
-            }
-            Debug.log("Converting %s on %-5s branch of %s to guard for remaining branch %s.", deopt, deoptBegin == ifNode.trueSuccessor() ? "true" : "false", ifNode, otherBegin);
-            FixedNode next = pred.next();
-            pred.setNext(guard);
-            guard.setNext(next);
-            Debug.dump(graph, "After introducing fixed guard %s", guard);
+        }
+
+        // We could not convert the control split - at least cut off control flow after the split.
+        FixedNode next = deoptBegin.next();
+        if (next != deopt) {
+            FixedWithNextNode pred = (FixedWithNextNode) deopt.predecessor();
+            pred.setNext(null);
+            deoptBegin.setNext(deopt);
+            GraphUtil.killCFG(next);
         }
     }
 }
