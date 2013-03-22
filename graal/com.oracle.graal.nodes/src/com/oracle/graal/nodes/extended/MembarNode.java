d9441313eaad8b0640a819098305116dce995671
Doug Simon <doug.simon@oracle.com>
2013-03-22 19:14:50 +0100

    fixed warnings

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/MembarNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/MembarNode.java
@@ -22,14 +22,7 @@
  */
 package com.oracle.graal.nodes.extended;
 
-import static com.oracle.graal.graph.UnsafeAccess.*;
-
-import java.lang.reflect.*;
-
-import sun.misc.*;
-
 import com.oracle.graal.api.code.*;
-import com.oracle.graal.graph.*;
 import com.oracle.graal.nodes.*;
 import com.oracle.graal.nodes.spi.*;
 import com.oracle.graal.nodes.type.*;
