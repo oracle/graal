23c27a88e6305464356a6fa8fe6936eba6a23f3b
Lukas Stadler <lukas.stadler@jku.at>
2013-04-12 10:50:28 +0200

    make GraalOptions.BenchmarkDynamicCounters configurable

--- graal/com.oracle.graal.phases/src/com/oracle/graal/phases/GraalOptions.java
+++ graal/com.oracle.graal.phases/src/com/oracle/graal/phases/GraalOptions.java
@@ -128,7 +128,7 @@ public final class GraalOptions {
     public static String  MethodFilter                       = null;
     public static boolean DumpOnError                        = ____;
     public static boolean GenericDynamicCounters             = ____;
-    public static boolean BenchmarkDynamicCounters           = ____;
+    public static String  BenchmarkDynamicCounters           = null;
 
     // Ideal graph visualizer output settings
     public static boolean PrintBinaryGraphs                  = true;
