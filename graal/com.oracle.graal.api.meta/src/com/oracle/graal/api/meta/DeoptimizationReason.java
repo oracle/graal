020ab39f88b5add767fd4947998659a6454b2131
Lukas Stadler <lukas.stadler@jku.at>
2013-04-22 15:38:01 +0200

    remove useless /@formatter:off

--- graal/com.oracle.graal.api.meta/src/com/oracle/graal/api/meta/DeoptimizationReason.java
+++ graal/com.oracle.graal.api.meta/src/com/oracle/graal/api/meta/DeoptimizationReason.java
@@ -25,7 +25,6 @@ package com.oracle.graal.api.meta;
 /**
  * Enumeration of reasons for why a deoptimization is happening.
  */
-// @formatter:off
 public enum DeoptimizationReason {
     None,
     NullCheckException,
