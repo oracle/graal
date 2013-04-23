85b99753ae446d27363be415d4af83cc536c01ce
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2013-04-23 20:16:34 +0200

    Move ExactMath from truffle.api.intrinsics to truffle.api package.

--- graal/com.oracle.truffle.api/src/com/oracle/truffle/api/ExactMath.java
+++ graal/com.oracle.truffle.api/src/com/oracle/truffle/api/ExactMath.java
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.truffle.api.intrinsics;
+package com.oracle.truffle.api;
 
 /**
  * This class contains methods that will be part of java.lang.Math starting with JDK 8. Until JDK 8
