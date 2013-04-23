d5209ba70adb0e17e2fbdcf12e64b0bba87dce66
Roland Schatz <roland.schatz@oracle.com>
2013-04-23 10:29:54 +0200

    Rename generateLea to generateAddress.

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/AddLocationNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/AddLocationNode.java
@@ -86,20 +86,20 @@ public final class AddLocationNode extends LocationNode implements Canonicalizab
     }
 
     @Override
-    public Value generateLea(LIRGeneratorTool gen, Value base) {
-        Value xAddr = getX().generateLea(gen, base);
-        return getY().generateLea(gen, xAddr);
+    public Value generateAddress(LIRGeneratorTool gen, Value base) {
+        Value xAddr = getX().generateAddress(gen, base);
+        return getY().generateAddress(gen, xAddr);
     }
 
     @Override
     public Value generateLoad(LIRGeneratorTool gen, Value base, DeoptimizingNode deopting) {
-        Value xAddr = getX().generateLea(gen, base);
+        Value xAddr = getX().generateAddress(gen, base);
         return getY().generateLoad(gen, xAddr, deopting);
     }
 
     @Override
     public void generateStore(LIRGeneratorTool gen, Value base, Value value, DeoptimizingNode deopting) {
-        Value xAddr = getX().generateLea(gen, base);
+        Value xAddr = getX().generateAddress(gen, base);
         getY().generateStore(gen, xAddr, value, deopting);
     }
 
