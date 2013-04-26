ea9715a9dfb5aad606746115d29ed379427592d5
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2013-04-26 14:15:17 +0200

    Perform deopt-to-guard conversion at snippet installation time.

--- graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/ReplacementsImpl.java
+++ graal/com.oracle.graal.replacements/src/com/oracle/graal/replacements/ReplacementsImpl.java
@@ -282,6 +282,7 @@ public class ReplacementsImpl implements Replacements {
             if (!SnippetTemplate.hasConstantParameter(method)) {
                 NodeIntrinsificationVerificationPhase.verify(graph);
             }
+            new ConvertDeoptimizeToGuardPhase().apply(graph);
 
             if (original == null) {
                 new SnippetFrameStateCleanupPhase().apply(graph);
