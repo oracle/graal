6f797744b13955fc532ce40100f9b204e90f77a7
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2013-04-28 07:50:32 +0200

    Disable removeIntermediateMaterialization.

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/IfNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/IfNode.java
@@ -162,10 +162,6 @@ public final class IfNode extends ControlSplitNode implements Simplifiable, LIRL
             if (removeOrMaterializeIf(tool)) {
                 return;
             }
-
-            if (removeIntermediateMaterialization(tool)) {
-                return;
-            }
         }
 
         if (falseSuccessor().usages().isEmpty() && (!(falseSuccessor() instanceof LoopExitNode)) && falseSuccessor().next() instanceof IfNode) {
@@ -401,7 +397,7 @@ public final class IfNode extends ControlSplitNode implements Simplifiable, LIRL
      * 
      * @return true if a transformation was made, false otherwise
      */
-    private boolean removeIntermediateMaterialization(SimplifierTool tool) {
+    public boolean removeIntermediateMaterialization(SimplifierTool tool) {
         if (!(condition() instanceof CompareNode)) {
             return false;
         }
