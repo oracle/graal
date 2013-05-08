53f8267ba161bc5c3c4534c16d6905930091cca2
Christian Wimmer <christian.wimmer@oracle.com>
2013-05-08 13:07:41 -0700

    Method can be static

--- graal/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/AbstractTest.java
+++ graal/com.oracle.truffle.sl.test/src/com/oracle/truffle/sl/test/AbstractTest.java
@@ -49,7 +49,7 @@ public class AbstractTest {
         return result.toString();
     }
 
-    protected void executeSL(String[] input, String[] expectedOutput, boolean useConsole) {
+    protected static void executeSL(String[] input, String[] expectedOutput, boolean useConsole) {
         InputStream in = new ByteArrayInputStream(concat(input).getBytes());
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
