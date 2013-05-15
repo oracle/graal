f2991533c43eab608e24d715cc35e267d57baf6b
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2013-05-14 22:10:13 -0700

    Remove unused read nodes.

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/ReadNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/ReadNode.java
@@ -23,6 +23,7 @@
 package com.oracle.graal.nodes.extended;
 
 import com.oracle.graal.api.meta.*;
+import com.oracle.graal.debug.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.nodes.*;
 import com.oracle.graal.nodes.extended.LocationNode.LocationIdentity;
@@ -72,6 +73,10 @@ public final class ReadNode extends FloatableAccessNode implements Node.Iterable
 
     public static ValueNode canonicalizeRead(ValueNode read, LocationNode location, ValueNode object, CanonicalizerTool tool) {
         MetaAccessProvider runtime = tool.runtime();
+        if (read.usages().count() == 0) {
+            // Read without usages can be savely removed.
+            return null;
+        }
         if (runtime != null && object != null && object.isConstant()) {
             if (location.getLocationIdentity() == LocationNode.FINAL_LOCATION && location instanceof ConstantLocationNode) {
                 long displacement = ((ConstantLocationNode) location).getDisplacement();
