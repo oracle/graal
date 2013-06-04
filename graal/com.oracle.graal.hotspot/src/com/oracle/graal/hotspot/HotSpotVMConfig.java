4c22dde05f8fbce29301d48d8e216bf22064e876
Christos Kotselidis <christos.kotselidis@oracle.com>
2013-06-04 13:54:44 +0200

    Delegate compressed oop arguments from HotSpot to Graal

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotVMConfig.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotVMConfig.java
@@ -50,6 +50,13 @@ public final class HotSpotVMConfig extends CompilerObject {
     public boolean useAESIntrinsics;
     public boolean useG1GC;
 
+    // Compressed Oops related values.
+    public boolean useCompressedOops;
+    public boolean useCompressedKlassPointers;
+    public long narrowOopBase;
+    public int narrowOopShift;
+    public int logMinObjAlignment;
+
     // CPU capabilities
     public int useSSE;
     public int useAVX;
