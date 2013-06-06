af59df2ec9d0e7dd26ce4b313a8f3aa9b79c1cde
Doug Simon <doug.simon@oracle.com>
2013-06-06 10:10:46 +0200

    re-enabled initialization of debug environment on main thread

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/bridge/VMToCompilerImpl.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/bridge/VMToCompilerImpl.java
@@ -52,6 +52,7 @@ import com.oracle.graal.options.*;
 import com.oracle.graal.phases.*;
 import com.oracle.graal.phases.PhasePlan.PhasePosition;
 import com.oracle.graal.phases.common.*;
+import com.oracle.graal.printer.*;
 import com.oracle.graal.replacements.*;
 
 /**
@@ -177,6 +178,7 @@ public class VMToCompilerImpl implements VMToCompiler {
 
         if (DebugEnabled.getValue()) {
             Debug.enable();
+            DebugEnvironment.initialize(log);
         }
 
         // Install intrinsics.
