c925a67a298804bf34a26d43d5f143c56982b83a
Christian Haeubl <haeubl@ssw.jku.at>
2013-06-14 09:28:07 +0200

    Fixed probability computation for invokes with an exception edge.

--- graal/com.oracle.graal.phases/src/com/oracle/graal/phases/graph/ComputeProbabilityClosure.java
+++ graal/com.oracle.graal.phases/src/com/oracle/graal/phases/graph/ComputeProbabilityClosure.java
@@ -45,7 +45,6 @@ import com.oracle.graal.nodes.util.*;
  * <li>{@link PropagateLoopFrequency} propagates the loop frequencies and multiplies each
  * {@link FixedNode}'s probability with its loop frequency.</li>
  * </ol>
- * TODO: add exception probability information to Invokes
  */
 public class ComputeProbabilityClosure {
 
@@ -291,19 +290,11 @@ public class ComputeProbabilityClosure {
         public void afterSplit(AbstractBeginNode node) {
             assert node.predecessor() != null;
             Node pred = node.predecessor();
-            if (pred instanceof Invoke) {
-                Invoke x = (Invoke) pred;
-                if (x.next() != node) {
-                    probability = 0;
-                }
-            } else {
-                assert pred instanceof ControlSplitNode;
-                ControlSplitNode x = (ControlSplitNode) pred;
-                double nodeProbability = x.probability(node);
-                assert nodeProbability >= 0.0 : "Node " + x + " provided negative probability for begin " + node + ": " + nodeProbability;
-                probability *= nodeProbability;
-                assert probability >= 0.0;
-            }
+            ControlSplitNode x = (ControlSplitNode) pred;
+            double nodeProbability = x.probability(node);
+            assert nodeProbability >= 0.0 : "Node " + x + " provided negative probability for begin " + node + ": " + nodeProbability;
+            probability *= nodeProbability;
+            assert probability >= 0.0;
         }
     }
 
