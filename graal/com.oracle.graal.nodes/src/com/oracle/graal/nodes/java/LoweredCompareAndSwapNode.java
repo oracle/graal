c68286ef3b5632d076b0e646d3f3d1c73d16b37b
Christos Kotselidis <christos.kotselidis@oracle.com>
2013-07-22 22:03:27 +0200

    Add LoweredCompareAndSwapNode

---
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/java/LoweredCompareAndSwapNode.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.nodes.java;
+
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.graph.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.extended.*;
+import com.oracle.graal.nodes.spi.*;
+import com.oracle.graal.nodes.type.*;
+
+/**
+ * Represents the lowered version of an atomic compare-and-swap operation{@code CompareAndSwapNode}.
+ */
+public class LoweredCompareAndSwapNode extends AccessNode implements StateSplit, LIRLowerable, MemoryCheckpoint.Single, Node.IterableNodeType {
+
+    @Input private ValueNode object;
+    @Input private ValueNode expectedValue;
+    @Input private ValueNode newValue;
+    @Input private LocationNode location;
+    @Input(notDataflow = true) private FrameState stateAfter;
+
+    public FrameState stateAfter() {
+        return stateAfter;
+    }
+
+    public void setStateAfter(FrameState x) {
+        assert x == null || x.isAlive() : "frame state must be in a graph";
+        updateUsages(stateAfter, x);
+        stateAfter = x;
+    }
+
+    public boolean hasSideEffect() {
+        return true;
+    }
+
+    public ValueNode getObject() {
+        return object;
+    }
+
+    public ValueNode getExpectedValue() {
+        return expectedValue;
+    }
+
+    public ValueNode getNewValue() {
+        return newValue;
+    }
+
+    public LocationNode getLocation() {
+        return location;
+    }
+
+    public LoweredCompareAndSwapNode(ValueNode object, LocationNode location, ValueNode expectedValue, ValueNode newValue, BarrierType barrierType, boolean compressible) {
+        super(object, location, StampFactory.forKind(Kind.Boolean.getStackKind()), barrierType, compressible);
+        assert expectedValue.kind() == newValue.kind();
+        this.object = object;
+        this.expectedValue = expectedValue;
+        this.newValue = newValue;
+        this.location = location;
+    }
+
+    @Override
+    public LocationIdentity getLocationIdentity() {
+        return location.getLocationIdentity();
+    }
+
+    @Override
+    public void generate(LIRGeneratorTool gen) {
+        gen.visitCompareAndSwap(this, getLocation().generateAddress(gen, gen.operand(getObject())));
+    }
+}
