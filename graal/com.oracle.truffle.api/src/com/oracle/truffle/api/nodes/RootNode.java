4276d13f981c3afe3d509ea40bf69591bd554764
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2013-09-05 15:03:36 -0700

    Truffle:  add alternate Node/RootNode constructors with SourceSection argument.

--- graal/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/RootNode.java
+++ graal/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/RootNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,6 +33,13 @@ import com.oracle.truffle.api.frame.*;
  * {@link TruffleRuntime#createCallTarget(RootNode, FrameDescriptor)}.
  */
 public abstract class RootNode extends Node {
+
+    protected RootNode() {
+    }
+
+    protected RootNode(SourceSection sourceSection) {
+        super(sourceSection);
+    }
 
     /**
      * Executes this function using the specified frame and returns the result value.
