09255643ea674bd609c35a25911d785252517885
Michael Van De Vanter <michael.van.de.vanter@oracle.com>
2013-10-06 19:54:56 -0700

    SL: update node constructors to use the Node(SourceSection) constructor

--- graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/SLNode.java
+++ graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/SLNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,6 +27,11 @@ import com.oracle.truffle.api.nodes.*;
 
 @TypeSystemReference(SLTypes.class)
 public class SLNode extends Node {
+
+    public SLNode() {
+        // No source attribution
+        super(null);
+    }
 
     @Override
     public String toString() {
