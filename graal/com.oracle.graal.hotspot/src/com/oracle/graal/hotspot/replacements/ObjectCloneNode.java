409504d1d0caa2f05aaa74f04ffe74ce032f8d35
Christian Wirth <christian.wirth@oracle.com>
2013-11-29 12:19:50 +0100

    fix problem detected by -G:-PartialEscapeAnalysis

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/replacements/ObjectCloneNode.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/replacements/ObjectCloneNode.java
@@ -80,7 +80,7 @@ public class ObjectCloneNode extends MacroNode implements VirtualizableAllocatio
                 type = getConcreteType(getObject().stamp(), tool.assumptions(), tool.getMetaAccess());
                 if (type != null) {
                     StructuredGraph newGraph = new StructuredGraph();
-                    LocalNode local = newGraph.add(new LocalNode(0, getObject().stamp()));
+                    LocalNode local = newGraph.unique(new LocalNode(0, getObject().stamp()));
                     NewInstanceNode newInstance = newGraph.add(new NewInstanceNode(type, true));
                     newGraph.addAfterFixed(newGraph.start(), newInstance);
                     ReturnNode returnNode = newGraph.add(new ReturnNode(newInstance));
