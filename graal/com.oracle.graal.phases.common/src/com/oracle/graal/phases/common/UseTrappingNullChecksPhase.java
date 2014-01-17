b6d067f032a40bd30718e8a79bef39a2a7601141
Gilles Duboscq <duboscq@ssw.jku.at>
2014-01-17 14:01:02 +0100

    UseTrappingNullChecksPhase: do not use trapping null check for a reason which is not null check or has an attached speculation

--- graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/UseTrappingNullChecksPhase.java
+++ graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/UseTrappingNullChecksPhase.java
@@ -22,6 +22,7 @@
  */
 package com.oracle.graal.phases.common;
 
+import com.oracle.graal.api.meta.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.nodes.*;
 import com.oracle.graal.nodes.StructuredGraph.GuardsStage;
@@ -46,6 +47,12 @@ public class UseTrappingNullChecksPhase extends BasePhase<LowTierContext> {
     }
 
     private static void tryUseTrappingNullCheck(DeoptimizeNode deopt) {
+        if (deopt.reason() != DeoptimizationReason.NullCheckException) {
+            return;
+        }
+        if (deopt.getSpeculation() != null && deopt.getSpeculation() != Constant.NULL_OBJECT) {
+            return;
+        }
         Node predecessor = deopt.predecessor();
         Node branch = null;
         while (predecessor instanceof AbstractBeginNode) {
