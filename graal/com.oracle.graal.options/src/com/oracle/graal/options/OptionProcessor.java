055dbc51c70ff65872840d4dadfc56061f8e708b
Doug Simon <doug.simon@oracle.com>
2014-03-08 10:05:57 +0100

    fixed incorrect test for null

--- graal/com.oracle.graal.options/src/com/oracle/graal/options/OptionProcessor.java
+++ graal/com.oracle.graal.options/src/com/oracle/graal/options/OptionProcessor.java
@@ -264,7 +264,7 @@ public class OptionProcessor extends AbstractProcessor {
 
     private static Element topDeclaringType(Element element) {
         Element enclosing = element.getEnclosingElement();
-        if (element == null || enclosing.getKind() == ElementKind.PACKAGE) {
+        if (enclosing == null || enclosing.getKind() == ElementKind.PACKAGE) {
             assert element.getKind() == ElementKind.CLASS || element.getKind() == ElementKind.INTERFACE;
             return element;
         }
