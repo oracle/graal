9ac273b97cd9230722bdffdc17a0fb753fe5eb91
Roland Schatz <roland.schatz@oracle.com>
2014-04-07 11:46:20 +0200

    Relax assertions in CompilationResultBuilder.

--- graal/com.oracle.graal.lir/src/com/oracle/graal/lir/asm/CompilationResultBuilder.java
+++ graal/com.oracle.graal.lir/src/com/oracle/graal/lir/asm/CompilationResultBuilder.java
@@ -265,22 +265,22 @@ public class CompilationResultBuilder {
     }
 
     public AbstractAddress asByteAddr(Value value) {
-        assert value.getKind() == Kind.Byte || value.getKind() == Kind.Boolean;
+        assert value.getKind().getByteCount() >= Kind.Byte.getByteCount();
         return asAddress(value);
     }
 
     public AbstractAddress asShortAddr(Value value) {
-        assert value.getKind() == Kind.Short || value.getKind() == Kind.Char;
+        assert value.getKind().getByteCount() >= Kind.Short.getByteCount();
         return asAddress(value);
     }
 
     public AbstractAddress asIntAddr(Value value) {
-        assert value.getKind() == Kind.Int;
+        assert value.getKind().getByteCount() >= Kind.Int.getByteCount();
         return asAddress(value);
     }
 
     public AbstractAddress asLongAddr(Value value) {
-        assert value.getKind() == Kind.Long;
+        assert value.getKind().getByteCount() >= Kind.Long.getByteCount();
         return asAddress(value);
     }
 
@@ -290,12 +290,12 @@ public class CompilationResultBuilder {
     }
 
     public AbstractAddress asFloatAddr(Value value) {
-        assert value.getKind() == Kind.Float;
+        assert value.getKind().getByteCount() >= Kind.Float.getByteCount();
         return asAddress(value);
     }
 
     public AbstractAddress asDoubleAddr(Value value) {
-        assert value.getKind() == Kind.Double;
+        assert value.getKind().getByteCount() >= Kind.Double.getByteCount();
         return asAddress(value);
     }
 
