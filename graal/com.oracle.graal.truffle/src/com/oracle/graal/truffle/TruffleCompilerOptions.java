ee7cab8046b200eba1d1a260f2d5fc93d68b598d
Doug Simon <doug.simon@oracle.com>
2014-04-15 16:16:24 +0200

    made TraceTruffleCompilation option false by default

--- graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/TruffleCompilerOptions.java
+++ graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/TruffleCompilerOptions.java
@@ -80,7 +80,7 @@ public class TruffleCompilerOptions {
 
     // tracing
     @Option(help = "")
-    public static final OptionValue<Boolean> TraceTruffleCompilation = new OptionValue<>(true);
+    public static final OptionValue<Boolean> TraceTruffleCompilation = new OptionValue<>(false);
     @Option(help = "")
     public static final OptionValue<Boolean> TraceTruffleCompilationDetails = new OptionValue<>(false);
     @Option(help = "")
