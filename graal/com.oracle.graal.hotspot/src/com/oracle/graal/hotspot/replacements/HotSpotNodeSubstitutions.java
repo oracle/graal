fe99e7e1e4094685c13fc002370e1e0911e763fd
Doug Simon <doug.simon@oracle.com>
2014-04-16 11:16:00 +0200

    clarified comment

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/replacements/HotSpotNodeSubstitutions.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/replacements/HotSpotNodeSubstitutions.java
@@ -39,8 +39,8 @@ public class HotSpotNodeSubstitutions {
     @MethodSubstitution(isStatic = false)
     public static NodeClass getNodeClass(final Node node) {
         // HotSpot creates the NodeClass for each Node subclass while initializing it
-        // so we are guaranteed to read a non-null value here. The fact that NodeClass
-        // is final will automatically make the stamp of the PiNode exact.
+        // so we are guaranteed to read a non-null value here. As long as NodeClass
+        // is final, the stamp of the PiNode below will automatically be exact.
         Word klass = loadHub(node);
         return piCastNonNull(klass.readObject(Word.signed(klassNodeClassOffset()), KLASS_NODE_CLASS), NodeClass.class);
     }
