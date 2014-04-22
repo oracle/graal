89c870f0d0fa83b4ec7c983587e5fef8fa9655f9
Roland Schatz <roland.schatz@oracle.com>
2014-04-22 16:21:37 +0200

    Override asNode() in FloatingNode and FixedWithNextNode.

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/FloatingNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/FloatingNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,5 +30,10 @@ public abstract class FloatingNode extends ValueNode implements Node.ValueNumber
 
     public FloatingNode(Stamp stamp) {
         super(stamp);
+    }
+
+    @Override
+    public FloatingNode asNode() {
+        return this;
     }
 }
