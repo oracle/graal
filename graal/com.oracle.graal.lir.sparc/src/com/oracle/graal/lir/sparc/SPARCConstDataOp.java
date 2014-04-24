427408efa7276607b3e9c5887013b831856f2f1d
Stefan Anzinger <stefan.anzinger@gmail.com>
2014-04-24 14:09:40 +0200

    Make proper code for c const strings required for fixup in hotspot

--- graal/com.oracle.graal.lir.sparc/src/com/oracle/graal/lir/sparc/SPARCConstDataOp.java
+++ graal/com.oracle.graal.lir.sparc/src/com/oracle/graal/lir/sparc/SPARCConstDataOp.java
@@ -2,12 +2,17 @@ package com.oracle.graal.lir.sparc;
 
 import static com.oracle.graal.lir.LIRInstruction.OperandFlag.*;
 
+import com.oracle.graal.api.code.*;
 import com.oracle.graal.api.code.CompilationResult.RawData;
 import com.oracle.graal.api.meta.*;
 import com.oracle.graal.asm.sparc.*;
+import com.oracle.graal.lir.*;
 import com.oracle.graal.lir.asm.*;
 import com.oracle.graal.lir.sparc.SPARCMove.LoadAddressOp;
+import com.oracle.graal.sparc.*;
+import com.sun.javafx.binding.SelectBinding.*;
 
+@Opcode("CONST_DATA")
 public class SPARCConstDataOp extends SPARCLIRInstruction {
 
     @Def({REG}) private AllocatableValue dst;
@@ -20,8 +25,10 @@ public class SPARCConstDataOp extends SPARCLIRInstruction {
 
     @Override
     public void emitCode(CompilationResultBuilder crb, SPARCMacroAssembler masm) {
-        RawData data = new RawData(val, 16);
-        new LoadAddressOp(dst, (SPARCAddress) crb.recordDataReferenceInCode(data)).emitCode(crb);
+        Register dstReg = ValueUtil.asLongReg(dst);
+        long addr = 0; // Will be fixed up by the loader
+        new SPARCAssembler.Sethi((int) addr, dstReg).emit(masm);
+        new SPARCAssembler.Add(SPARC.g0, (int) addr, dstReg).emit(masm);
     }
 
 }
