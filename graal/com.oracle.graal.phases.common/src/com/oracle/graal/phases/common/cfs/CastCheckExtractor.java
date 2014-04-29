841a4e5e889945302d13a5a34251badcc29c9871
Miguel Garcia <miguel.m.garcia@oracle.com>
2014-04-29 14:47:57 +0200

    [flow-sensitive] better encapsulation, CastCheckExtractor

--- graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/cfs/CastCheckExtractor.java
+++ graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/cfs/CastCheckExtractor.java
@@ -42,7 +42,7 @@ class CastCheckExtractor {
         this.subject = subject;
     }
 
-    static CastCheckExtractor extractCastCheckInfo(LogicNode x, LogicNode y) {
+    private static CastCheckExtractor extractCastCheckInfo(LogicNode x, LogicNode y) {
         if (x instanceof IsNullNode) {
             IsNullNode isNull = (IsNullNode) x;
             ValueNode subject = isNull.object();
@@ -77,7 +77,7 @@ class CastCheckExtractor {
     /**
      * Porcelain method.
      */
-    public static boolean isInstanceOfCheckOn(LogicNode cond, ValueNode subject) {
+    static boolean isInstanceOfCheckOn(LogicNode cond, ValueNode subject) {
         if (!(cond instanceof InstanceOfNode)) {
             return false;
         }
