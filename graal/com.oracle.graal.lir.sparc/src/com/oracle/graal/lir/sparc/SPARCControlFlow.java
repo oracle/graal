925e113d6bcc69a3e67fed579120287cbe2ddbfb
Stefan Anzinger <stefan.anzinger@gmail.com>
2014-06-05 16:28:13 +0200

    [SPARC] Fixing BC_new, BC_fcmpxx

--- graal/com.oracle.graal.lir.sparc/src/com/oracle/graal/lir/sparc/SPARCControlFlow.java
+++ graal/com.oracle.graal.lir.sparc/src/com/oracle/graal/lir/sparc/SPARCControlFlow.java
@@ -348,6 +348,7 @@ public class SPARCControlFlow {
             case Object:
                 new Movcc(cond, CC.Xcc, asRegister(other), asRegister(result)).emit(masm);
                 break;
+            case Float:
             case Double:
                 FCond fc = null;
                 switch (cond) {
