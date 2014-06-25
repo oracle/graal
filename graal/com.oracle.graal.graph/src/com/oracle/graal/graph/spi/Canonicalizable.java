7c3f16e543de84af49a6c3356b2afcd1a8d6dda6
Lukas Stadler <lukas.stadler@oracle.com>
2014-06-25 10:58:42 +0200

    new Unary and Binary canonicalizable interfaces

--- graal/com.oracle.graal.graph/src/com/oracle/graal/graph/spi/Canonicalizable.java
+++ graal/com.oracle.graal.graph/src/com/oracle/graal/graph/spi/Canonicalizable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,4 +27,18 @@ import com.oracle.graal.graph.*;
 public interface Canonicalizable {
 
     Node canonical(CanonicalizerTool tool);
+
+    public interface Unary<T extends Node> {
+        T canonical(CanonicalizerTool tool, T forValue);
+
+        T getValue();
+    }
+
+    public interface Binary<T extends Node> {
+        T canonical(CanonicalizerTool tool, T forX, T forY);
+
+        T getX();
+
+        T getY();
+    }
 }
