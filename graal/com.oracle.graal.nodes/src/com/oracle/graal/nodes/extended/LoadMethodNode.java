1723ea264e6f152c7ebe9e077c35c80e4d37280a
Lukas Stadler <lukas.stadler@oracle.com>
2014-06-26 17:29:37 +0200

    fix for LoadMethodNode

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/LoadMethodNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/extended/LoadMethodNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -70,7 +70,7 @@ public final class LoadMethodNode extends FixedWithNextNode implements Lowerable
                 ResolvedJavaMethod resolvedMethod = type.findUniqueConcreteMethod(method);
                 if (resolvedMethod != null && !type.isInterface() && method.getDeclaringClass().isAssignableFrom(type)) {
                     tool.assumptions().recordConcreteMethod(method, type, resolvedMethod);
-                    return ConstantNode.forConstant(resolvedMethod.getEncoding(), tool.getMetaAccess(), graph());
+                    return ConstantNode.forConstant(resolvedMethod.getEncoding(), tool.getMetaAccess());
                 }
             }
         }
@@ -96,9 +96,9 @@ public final class LoadMethodNode extends FixedWithNextNode implements Lowerable
              * This really represent a misuse of LoadMethod since we're loading from a class which
              * isn't known to implement the original method but for now at least fold it away.
              */
-            return ConstantNode.forConstant(Constant.NULL_OBJECT, null, graph());
+            return ConstantNode.forConstant(Constant.NULL_OBJECT, null);
         } else {
-            return ConstantNode.forConstant(newMethod.getEncoding(), tool.getMetaAccess(), graph());
+            return ConstantNode.forConstant(newMethod.getEncoding(), tool.getMetaAccess());
         }
     }
 
