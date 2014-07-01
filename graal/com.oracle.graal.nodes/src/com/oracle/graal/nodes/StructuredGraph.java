7d363cdd0ceec5235eaa35832c444ac17180b5de
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2014-07-01 21:44:57 +0200

    Add missing import.

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/StructuredGraph.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/StructuredGraph.java
@@ -26,6 +26,7 @@ import java.util.*;
 import java.util.concurrent.atomic.*;
 
 import com.oracle.graal.api.meta.*;
+import com.oracle.graal.compiler.common.type.*;
 import com.oracle.graal.graph.*;
 import com.oracle.graal.graph.spi.*;
 import com.oracle.graal.nodes.calc.*;
