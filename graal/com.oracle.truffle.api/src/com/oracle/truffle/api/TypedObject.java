ddb3c71067efd5e476cdc59fea0eca69653cac1f
Christian Humer <christian.humer@gmail.com>
2014-07-04 21:29:40 +0200

    Fixed headers.

--- graal/com.oracle.truffle.api/src/com/oracle/truffle/api/TypedObject.java
+++ graal/com.oracle.truffle.api/src/com/oracle/truffle/api/TypedObject.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
