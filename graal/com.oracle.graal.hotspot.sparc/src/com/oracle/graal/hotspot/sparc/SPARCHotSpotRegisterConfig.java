8ed52202a9d78ba78bc71e0b1a1ba73f1a4eef26
Stefan Anzinger <stefan.anzinger@gmail.com>
2014-07-21 10:45:43 -0700

    [SPARC] Put extended arguments (index > 6) on the right place in stack

--- graal/com.oracle.graal.hotspot.sparc/src/com/oracle/graal/hotspot/sparc/SPARCHotSpotRegisterConfig.java
+++ graal/com.oracle.graal.hotspot.sparc/src/com/oracle/graal/hotspot/sparc/SPARCHotSpotRegisterConfig.java
@@ -95,6 +95,11 @@ public class SPARCHotSpotRegisterConfig implements RegisterConfig {
 
     private final CalleeSaveLayout csl;
 
+    /**
+     * Offset where the extended arguments resides in the stack.
+     */
+    private final int extendedArgumentOffset = 16 * 8;
+
     private static Register findRegister(String name, Register[] all) {
         for (Register reg : all) {
             if (reg.name.equals(name)) {
@@ -195,7 +200,7 @@ public class SPARCHotSpotRegisterConfig implements RegisterConfig {
 
         int currentGeneral = 0;
         int currentFloating = 0;
-        int currentStackOffset = 0;
+        int currentStackOffset = extendedArgumentOffset;
 
         for (int i = 0; i < parameterTypes.length; i++) {
             final Kind kind = parameterTypes[i].getKind();
