32857bb677683caef79bd4fa75b3340d3d34cd94
Doug Simon <doug.simon@oracle.com>
2014-08-13 21:28:23 +0200

    fixed header

--- graal/com.oracle.graal.graph/src/com/oracle/graal/graph/GeneratedNode.java
+++ graal/com.oracle.graal.graph/src/com/oracle/graal/graph/GeneratedNode.java
@@ -1,12 +1,10 @@
 /*
- * Copyright (c) 2012, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
