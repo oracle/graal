d16d21ec92543e6f7e0743c50fedfd96e26b6665
Christian Humer <christian.humer@gmail.com>
2014-08-18 19:02:18 +0200

    Truffle: fixed header dates.

--- graal/com.oracle.truffle.api/src/com/oracle/truffle/api/utilities/BinaryConditionProfile.java
+++ graal/com.oracle.truffle.api/src/com/oracle/truffle/api/utilities/BinaryConditionProfile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -35,7 +35,7 @@ import com.oracle.truffle.api.CompilerDirectives.*;
  *
  * <pre>
  * private final ConditionProfile zero = new BooleanConditionProfile();
- * 
+ *
  * int value = ...;
  * if (zero.profile(value == 0)) {
  *   return 0;
--- graal/com.oracle.truffle.api/src/com/oracle/truffle/api/utilities/ConditionProfile.java
+++ graal/com.oracle.truffle.api/src/com/oracle/truffle/api/utilities/ConditionProfile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,7 +32,7 @@ package com.oracle.truffle.api.utilities;
  *
  * <pre>
  * private final ConditionProfile zero = ...;
- *
+ * 
  * int value = ...;
  * if (zero.profile(value == 0)) {
  *   return 0;
