f7d6b703c95e9ffe08a5b3bf33ef483c447221ff
Christian Humer <christian.humer@gmail.com>
2014-08-23 19:31:33 +0200

    SL: better toString for SL nodes.

--- graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/SLStatementNode.java
+++ graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/SLStatementNode.java
@@ -52,6 +52,39 @@ public abstract class SLStatementNode extends Node implements Instrumentable {
     }
 
     @Override
+    public String toString() {
+        return formatSourceSection(this);
+    }
+
+    /**
+     * Formats a source section of a node in human readable form. If no source section could be
+     * found it looks up the parent hierarchy until it finds a source section. Nodes where this was
+     * required append a <code>'~'</code> at the end.
+     *
+     * @param node the node to format.
+     * @return a formatted source section string
+     */
+    private static String formatSourceSection(Node node) {
+        if (node == null) {
+            return "<unknown>";
+        }
+        SourceSection section = node.getSourceSection();
+        boolean estimated = false;
+        if (section == null) {
+            section = node.getEncapsulatingSourceSection();
+            estimated = true;
+        }
+
+        if (section == null || section.getSource() == null) {
+            return "<unknown source>";
+        } else {
+            String sourceName = new File(section.getSource().getName()).getName();
+            int startLine = section.getStartLine();
+            return String.format("%s:%d%s", sourceName, startLine, estimated ? "~" : "");
+        }
+    }
+
+    @Override
     public Probe probe(ExecutionContext context) {
         Node parent = getParent();
 
