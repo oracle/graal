233304c7ceb9f709a7ea61a6836d45e228e71e06
Stefan Anzinger <stefan.anzinger@gmail.com>
2014-08-25 10:30:03 -0700

    [SPARC] Fix incorrect implementation of IBSR

--- graal/com.oracle.graal.lir.sparc/src/com/oracle/graal/lir/sparc/SPARCBitManipulationOp.java
+++ graal/com.oracle.graal.lir.sparc/src/com/oracle/graal/lir/sparc/SPARCBitManipulationOp.java
@@ -99,7 +99,7 @@ public class SPARCBitManipulationOp extends SPARCLIRInstruction {
                     assert !tmp.equals(dst);
                     new Srl(src, 1, tmp).emit(masm);
                     new Srl(src, 0, dst).emit(masm);
-                    new Or(src, tmp, dst).emit(masm);
+                    new Or(dst, tmp, dst).emit(masm);
                     new Srl(dst, 2, tmp).emit(masm);
                     new Or(dst, tmp, dst).emit(masm);
                     new Srl(dst, 4, tmp).emit(masm);
