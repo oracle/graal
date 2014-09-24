9660484cb23ce152e0305ffd3a2d04229f9d4a21
Josef Eisl <josef.eisl@jku.at>
2014-09-24 11:34:40 +0200

    Handle deleted instructions in CFGPrinter.

--- graal/com.oracle.graal.printer/src/com/oracle/graal/printer/CFGPrinter.java
+++ graal/com.oracle.graal.printer/src/com/oracle/graal/printer/CFGPrinter.java
@@ -450,30 +450,35 @@ class CFGPrinter extends CompilationPrinter {
 
         for (int i = 0; i < lirInstructions.size(); i++) {
             LIRInstruction inst = lirInstructions.get(i);
-            out.printf("nr %4d ", inst.id()).print(COLUMN_END);
+            if (inst == null) {
+                out.print("nr   -1 ").print(COLUMN_END).print(" instruction ").print("<deleted>").print(COLUMN_END);
+                out.println(COLUMN_END);
+            } else {
+                out.printf("nr %4d ", inst.id()).print(COLUMN_END);
 
-            final StringBuilder stateString = new StringBuilder();
-            inst.forEachState(new StateProcedure() {
+                final StringBuilder stateString = new StringBuilder();
+                inst.forEachState(new StateProcedure() {
 
-                @Override
-                protected void doState(LIRFrameState state) {
-                    if (state.hasDebugInfo()) {
-                        DebugInfo di = state.debugInfo();
-                        stateString.append(debugInfoToString(di.getBytecodePosition(), di.getReferenceMap(), di.getCalleeSaveInfo(), target.arch));
-                    } else {
-                        stateString.append(debugInfoToString(state.topFrame, null, null, target.arch));
+                    @Override
+                    protected void doState(LIRFrameState state) {
+                        if (state.hasDebugInfo()) {
+                            DebugInfo di = state.debugInfo();
+                            stateString.append(debugInfoToString(di.getBytecodePosition(), di.getReferenceMap(), di.getCalleeSaveInfo(), target.arch));
+                        } else {
+                            stateString.append(debugInfoToString(state.topFrame, null, null, target.arch));
+                        }
                     }
+                });
+                if (stateString.length() > 0) {
+                    int level = out.indentationLevel();
+                    out.adjustIndentation(-level);
+                    out.print(" st ").print(HOVER_START).print("st").print(HOVER_SEP).print(stateString.toString()).print(HOVER_END).print(COLUMN_END);
+                    out.adjustIndentation(level);
                 }
-            });
-            if (stateString.length() > 0) {
-                int level = out.indentationLevel();
-                out.adjustIndentation(-level);
-                out.print(" st ").print(HOVER_START).print("st").print(HOVER_SEP).print(stateString.toString()).print(HOVER_END).print(COLUMN_END);
-                out.adjustIndentation(level);
-            }
 
-            out.print(" instruction ").print(inst.toString()).print(COLUMN_END);
-            out.println(COLUMN_END);
+                out.print(" instruction ").print(inst.toString()).print(COLUMN_END);
+                out.println(COLUMN_END);
+            }
         }
         end("IR");
     }
