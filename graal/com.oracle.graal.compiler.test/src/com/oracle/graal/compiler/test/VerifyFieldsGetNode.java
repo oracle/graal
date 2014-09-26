9ebd9d67c9970743af3e9c877ea6ca24f6c741a3
Doug Simon <doug.simon@oracle.com>
2014-09-26 14:53:35 +0200

    check that all callers of Fields.getObject(Object, int, Class) pass a class literal for the last parameter

---
+++ graal/com.oracle.graal.compiler.test/src/com/oracle/graal/compiler/test/VerifyFieldsGetNode.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.compiler.test;
+
+import com.oracle.graal.api.meta.*;
+import com.oracle.graal.compiler.common.*;
+import com.oracle.graal.nodes.*;
+import com.oracle.graal.nodes.java.*;
+import com.oracle.graal.phases.*;
+import com.oracle.graal.phases.tiers.*;
+
+/**
+ * Verifies that the third parameter of a call to {@link Fields#getObject(Object, int, Class)} is a
+ * class literal. This is required so that the DeferredPiNode in the substitution of this method is
+ * resolved to a constant after the (substitute) method is inlined.
+ */
+@SuppressWarnings("javadoc")
+public class VerifyFieldsGetNode extends VerifyPhase<PhaseContext> {
+
+    @Override
+    protected boolean verify(StructuredGraph graph, PhaseContext context) {
+        ResolvedJavaMethod getObjectMethod;
+        try {
+            getObjectMethod = context.getMetaAccess().lookupJavaMethod(Fields.class.getDeclaredMethod("getObject", Object.class, int.class, Class.class));
+        } catch (Exception e) {
+            throw new VerificationError(e.toString());
+        }
+
+        for (MethodCallTargetNode t : graph.getNodes(MethodCallTargetNode.class)) {
+            ResolvedJavaMethod callee = t.targetMethod();
+            if (callee.equals(getObjectMethod)) {
+                ValueNode arg3 = t.arguments().get(3);
+                if (!arg3.isConstant()) {
+                    StackTraceElement e = graph.method().asStackTraceElement(t.invoke().bci());
+                    throw new VerificationError(String.format("%s: parameter 2 of call to %s must be a class literal, not %s", e, callee.format("%H.%n(%p)"), arg3));
+                }
+            }
+        }
+        return true;
+    }
+}
