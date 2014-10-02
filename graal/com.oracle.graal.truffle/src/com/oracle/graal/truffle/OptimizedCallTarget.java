0d206f1f56489d3ef95ce08e000bca064d446017
Christian Humer <christian.humer@gmail.com>
2014-10-02 13:13:00 +0200

    Truffle: fixed passed wrong arguments to the frame in callBoundary.

--- graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedCallTarget.java
+++ graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedCallTarget.java
@@ -226,7 +226,7 @@ public class OptimizedCallTarget extends InstalledCode implements RootCallTarget
                 args1 = castArguments(args1);
             }
         }
-        VirtualFrame frame = createFrame(getRootNode().getFrameDescriptor(), args);
+        VirtualFrame frame = createFrame(getRootNode().getFrameDescriptor(), args1);
         Object result = callProxy(frame);
 
         // Profile call return type
