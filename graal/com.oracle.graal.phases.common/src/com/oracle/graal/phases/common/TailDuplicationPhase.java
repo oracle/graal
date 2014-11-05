0255059b0f7b35c5eb5025a77c9b8736095d9b1f
Josef Eisl <josef.eisl@jku.at>
2014-11-05 11:48:13 +0100

    TailDuplicationPhase.DummyAnchorNode: add allowedUsageTypes.

--- graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/TailDuplicationPhase.java
+++ graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/TailDuplicationPhase.java
@@ -60,7 +60,7 @@ public class TailDuplicationPhase extends BasePhase<PhaseContext> {
 
     private final CanonicalizerPhase canonicalizer;
 
-    @NodeInfo
+    @NodeInfo(allowedUsageTypes = {InputType.Guard, InputType.Anchor})
     static class DummyAnchorNode extends FixedWithNextNode implements GuardingNode, AnchoringNode {
         public static DummyAnchorNode create() {
             return new DummyAnchorNode();
