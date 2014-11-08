e7458a7eb520b66df27a4b0fa37f862e6793a4ef
Chris Seaton <chris.seaton@oracle.com>
2014-11-08 22:22:00 +0000

    Truffle: simpler, more consistent toString for DirectCallNode subclasses.

--- graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedDirectCallNode.java
+++ graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedDirectCallNode.java
@@ -173,8 +173,4 @@ public final class OptimizedDirectCallNode extends DirectCallNode implements Mat
         return true;
     }
 
-    @Override
-    public String toString() {
-        return String.format("OptimizedDirectCallNode(target=%s, parent=%s)", getCurrentCallTarget().toString(), getParent());
-    }
 }
