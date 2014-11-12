a8157560129f9e6c33202634a8aceed75922c878
Gilles Duboscq <duboscq@ssw.jku.at>
2014-11-12 13:48:47 +0100

    Add some javadoc to MergeableState

--- graal/com.oracle.graal.phases/src/com/oracle/graal/phases/graph/MergeableState.java
+++ graal/com.oracle.graal.phases/src/com/oracle/graal/phases/graph/MergeableState.java
@@ -31,11 +31,18 @@ public abstract class MergeableState<T> {
     @Override
     public abstract T clone();
 
+    /**
+     * This method is called on merge on the state of the first branch. The {@code withStates} list
+     * contains the states of the of the other branches in the order of the merge's end nodes.
+     *
+     * @param merge the merge node
+     * @param withStates the state at the the merge's end node except the first one.
+     */
     public abstract boolean merge(MergeNode merge, List<T> withStates);
 
     /**
      * This method is called before a loop is entered (before the {@link LoopBeginNode} is visited).
-     * 
+     *
      * @param loopBegin the begin node of the loop
      */
     public void loopBegin(LoopBeginNode loopBegin) {
@@ -44,7 +51,7 @@ public abstract class MergeableState<T> {
 
     /**
      * This method is called after all {@link LoopEndNode}s belonging to a loop have been visited.
-     * 
+     *
      * @param loopBegin the begin node of the loop
      * @param loopEndStates the states at the loop ends, sorted according to
      *            {@link LoopBeginNode#orderedLoopEnds()}
@@ -55,7 +62,7 @@ public abstract class MergeableState<T> {
 
     /**
      * This method is called before the successors of a {@link ControlSplitNode} are visited.
-     * 
+     *
      * @param node the successor of the control split that is about to be visited
      */
     public void afterSplit(BeginNode node) {
