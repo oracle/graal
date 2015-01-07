85c01e0569c0bef4e740f4f8c79e776ee22e8aa9
Christian Humer <christian.humer@gmail.com>
2015-01-07 18:01:24 +0100

    Truffle-DSL: fix implicit cast optimization bug.

--- graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/generator/NodeGenFactory.java
+++ graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/generator/NodeGenFactory.java
@@ -1726,7 +1726,7 @@ public class NodeGenFactory {
         final Set<ExecutableTypeData> executableTypes = findSpecializedExecutableTypes(execution, target.getType());
         if (executableTypes.isEmpty()) {
             throw new AssertionError(); // cannot execute child
-        } else if (executableTypes.size() == 1) {
+        } else if (executableTypes.size() == 1 && !target.getType().hasImplicitSourceTypes()) {
             ExecutableTypeData executableType = executableTypes.iterator().next();
             if (target.getType().isGeneric() && executableType.getEvaluatedCount() == 0) {
                 return createPolymorphicExecuteChild(execution, target, currentValues, shared);
