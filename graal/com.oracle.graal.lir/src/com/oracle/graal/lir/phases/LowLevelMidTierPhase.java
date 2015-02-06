1918efc2ed4ef7d0bcd4e993ff2347c75e8c8746
Josef Eisl <josef.eisl@jku.at>
2015-02-06 20:00:32 +0100

    Add LowLevelMidTierPhase.

---
+++ graal/com.oracle.graal.lir/src/com/oracle/graal/lir/phases/LowLevelMidTierPhase.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.graal.lir.phases;
+
+import java.util.*;
+
+import com.oracle.graal.api.code.*;
+import com.oracle.graal.compiler.common.cfg.*;
+import com.oracle.graal.lir.gen.*;
+
+public abstract class LowLevelMidTierPhase<B extends AbstractBlock<B>> extends LowLevelPhase<LowLevelMidTierPhase.Context<B>> {
+
+    public static final class Context<B extends AbstractBlock<B>> {
+        final List<B> codeEmittingOrder;
+        final List<B> linearScanOrder;
+
+        public Context(List<B> codeEmittingOrder, List<B> linearScanOrder) {
+            this.codeEmittingOrder = codeEmittingOrder;
+            this.linearScanOrder = linearScanOrder;
+        }
+    }
+
+    @Override
+    protected void run(TargetDescription target, LIRGenerationResult lirGenRes, Context<B> context) {
+        run(target, lirGenRes, context.codeEmittingOrder, context.linearScanOrder);
+    }
+
+    protected abstract void run(TargetDescription target, LIRGenerationResult lirGenRes, List<B> codeEmittingOrder, List<B> linearScanOrder);
+
+}
