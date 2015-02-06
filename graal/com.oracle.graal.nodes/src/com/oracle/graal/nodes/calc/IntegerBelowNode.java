82e6b29ad6dabfb6291c90bcf0993d5cc47cfb9d
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2015-02-06 05:22:59 +0100

    Fix method IntegerBelowNode#findSynonym.

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/IntegerBelowNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/IntegerBelowNode.java
@@ -62,6 +62,10 @@ public class IntegerBelowNode extends CompareNode {
         if (synonym != null) {
             return synonym;
         }
+        if (forX.isConstant() && forX.asJavaConstant().asLong() == 0) {
+            // 0 |<| y is the same as 0 != y
+            return new LogicNegationNode(CompareNode.createCompareNode(Condition.EQ, forX, forY));
+        }
         return this;
     }
 
@@ -78,10 +82,6 @@ public class IntegerBelowNode extends CompareNode {
                     return LogicConstantNode.contradiction();
                 }
             }
-        }
-        if (forX.isConstant() && forX.asJavaConstant().asLong() == 0) {
-            // 0 |<| y is the same as 0 != y
-            return new LogicNegationNode(CompareNode.createCompareNode(Condition.EQ, forX, forY));
         }
         return null;
     }
