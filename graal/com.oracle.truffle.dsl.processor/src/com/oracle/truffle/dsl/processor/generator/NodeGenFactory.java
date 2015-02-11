c2e114fb307dee8f3039dc2b97249de953d094f9
Christian Humer <christian.humer@gmail.com>
2015-02-11 18:10:02 +0100

    Truffle-DSL: fixed warning

--- graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/generator/NodeGenFactory.java
+++ graal/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/generator/NodeGenFactory.java
@@ -2125,8 +2125,7 @@ public class NodeGenFactory {
         return new CodeTree[]{expressionBuilder.build(), assertionBuilder.build()};
     }
 
-    private Map<Variable, CodeTree> castBoundTypes(Map<Variable, LocalVariable> bindings) {
-
+    private static Map<Variable, CodeTree> castBoundTypes(Map<Variable, LocalVariable> bindings) {
         Map<Variable, CodeTree> resolvedBindings = new HashMap<>();
         for (Variable variable : bindings.keySet()) {
             LocalVariable localVariable = bindings.get(variable);
