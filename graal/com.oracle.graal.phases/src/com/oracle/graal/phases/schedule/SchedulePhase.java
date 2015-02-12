d984f01fd7bcbd0ebb329cd35c83129c2a2e231f
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2015-02-12 20:38:02 +0100

    Do not schedule virtual object nodes.

--- graal/com.oracle.graal.phases/src/com/oracle/graal/phases/schedule/SchedulePhase.java
+++ graal/com.oracle.graal.phases/src/com/oracle/graal/phases/schedule/SchedulePhase.java
@@ -449,6 +449,9 @@ public final class SchedulePhase extends Phase {
         if (cfg.getNodeToBlock().containsKey(node)) {
             return;
         }
+        if (node instanceof VirtualObjectNode) {
+            return;
+        }
         // PhiNodes, ProxyNodes and FixedNodes should already have been placed in blocks by
         // ControlFlowGraph.identifyBlocks
         if (node instanceof PhiNode || node instanceof ProxyNode || node instanceof FixedNode) {
