0931f5d835879231fd8c4dc63c9537f45f3918a1
Tom Rodriguez <tom.rodriguez@oracle.com>
2015-02-16 15:39:01 -0800

    Temporarily add inversion count to LoopBegin

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/LoopBeginNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/LoopBeginNode.java
@@ -43,6 +43,8 @@ public final class LoopBeginNode extends AbstractMergeNode implements IterableNo
     protected double loopFrequency;
     protected int nextEndIndex;
     protected int unswitches;
+    protected int inversionCount;
+
     @OptionalInput(InputType.Guard) GuardingNode overflowGuard;
 
     public LoopBeginNode() {
@@ -187,6 +189,14 @@ public final class LoopBeginNode extends AbstractMergeNode implements IterableNo
 
     public void incUnswitches() {
         unswitches++;
+    }
+
+    public int getInversionCount() {
+        return inversionCount;
+    }
+
+    public void setInversionCount(int count) {
+        inversionCount = count;
     }
 
     @Override
