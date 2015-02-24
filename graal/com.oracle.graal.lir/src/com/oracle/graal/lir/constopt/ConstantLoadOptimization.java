3a7d05914bb64b2f4e43839123c2fd3c49d9d80b
Josef Eisl <josef.eisl@jku.at>
2015-02-24 19:45:16 +0100

    constopt.DefUseTree: remove ValuePosition.

--- graal/com.oracle.graal.lir/src/com/oracle/graal/lir/constopt/ConstantLoadOptimization.java
+++ graal/com.oracle.graal.lir/src/com/oracle/graal/lir/constopt/ConstantLoadOptimization.java
@@ -195,14 +195,13 @@ public final class ConstantLoadOptimization extends PreAllocationOptimizationPha
                     }
                 };
 
-                ValuePositionProcedure useProcedure = (instruction, position) -> {
-                    Value value = position.get(instruction);
+                InstructionValueConsumer useConsumer = (instruction, value, mode, flags) -> {
                     if (isVariable(value)) {
                         Variable var = (Variable) value;
                         if (!phiConstants.get(var.index)) {
                             DefUseTree tree = map.get(var);
                             if (tree != null) {
-                                tree.addUsage(block, instruction, position);
+                                tree.addUsage(block, instruction, value);
                                 Debug.log("usage of %s : %s", var, instruction);
                             }
                         }
@@ -214,8 +213,8 @@ public final class ConstantLoadOptimization extends PreAllocationOptimizationPha
                     // set instruction id to the index in the lir instruction list
                     inst.setId(opId++);
                     inst.visitEachOutput(loadConsumer);
-                    inst.forEachInputPos(useProcedure);
-                    inst.forEachAlivePos(useProcedure);
+                    inst.forEachInput(useConsumer);
+                    inst.forEachAlive(useConsumer);
 
                 }
             }
