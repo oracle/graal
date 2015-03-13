d23f7d0b1d14b2c195b2078744d6153e6eb429f4
Doug Simon <doug.simon@oracle.com>
2015-03-13 23:46:12 +0100

    disabled crypto intrinsics until partial intrinsification (i.e., self recursive intrinsics) bug is fixed

--- graal/com.oracle.graal.hotspot.test/src/com/oracle/graal/hotspot/test/HotSpotCryptoSubstitutionTest.java
+++ graal/com.oracle.graal.hotspot.test/src/com/oracle/graal/hotspot/test/HotSpotCryptoSubstitutionTest.java
@@ -22,6 +22,8 @@
  */
 package com.oracle.graal.hotspot.test;
 
+import static com.oracle.graal.nodes.spi.Replacements.*;
+
 import java.io.*;
 import java.lang.reflect.*;
 import java.security.*;
@@ -58,6 +60,8 @@ public class HotSpotCryptoSubstitutionTest extends HotSpotGraalCompilerTest {
 
     @Test
     public void testEncryptSubstitution() throws Exception {
+        Assume.assumeTrue(SELF_RECURSIVE_INTRINSICS_ENABLED);
+
         byte[] seed = {0x4, 0x7, 0x1, 0x1};
         SecureRandom random = new SecureRandom(seed);
         KeyGenerator aesKeyGen = KeyGenerator.getInstance("AES");
