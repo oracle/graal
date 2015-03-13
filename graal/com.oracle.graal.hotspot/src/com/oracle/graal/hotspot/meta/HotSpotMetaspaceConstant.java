00888d9bf2d7a1d4818319d16d29de740a5c6fe5
Roland Schatz <roland.schatz@oracle.com>
2015-03-13 15:20:16 +0100

    Move isCompressed method to HotSpotConstant interface.

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotMetaspaceConstant.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotMetaspaceConstant.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,8 +26,6 @@ import com.oracle.graal.api.meta.*;
 import com.oracle.graal.hotspot.HotSpotVMConfig.CompressEncoding;
 
 public interface HotSpotMetaspaceConstant extends HotSpotConstant, VMConstant {
-
-    boolean isCompressed();
 
     Constant compress(CompressEncoding encoding);
 
--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotObjectConstant.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotObjectConstant.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,8 +37,6 @@ public interface HotSpotObjectConstant extends JavaConstant, HotSpotConstant, VM
     JavaConstant compress();
 
     JavaConstant uncompress();
-
-    boolean isCompressed();
 
     /**
      * Gets the resolved Java type of the object represented by this constant.
