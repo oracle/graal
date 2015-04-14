19662c01cb258f9d9faafc4349b20357aa8f4dd9
Christian Humer <christian.humer@gmail.com>
2015-04-14 15:16:14 +0200

    SL: use DSL for property writes.

--- graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/access/SLWritePropertyNode.java
+++ graal/com.oracle.truffle.sl/src/com/oracle/truffle/sl/nodes/access/SLWritePropertyNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -54,7 +54,7 @@ public final class SLWritePropertyNode extends SLExpressionNode {
         this.receiverNode = receiverNode;
         this.propertyName = propertyName;
         this.valueNode = valueNode;
-        this.cacheNode = SLWritePropertyCacheNode.create(propertyName);
+        this.cacheNode = SLWritePropertyCacheNodeGen.create(propertyName);
     }
 
     @Override
