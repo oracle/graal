f10db1f0af37a56447cf8ad3972e5ea8c68d61a6
Doug Simon <doug.simon@oracle.com>
2015-04-21 20:23:59 +0200

    fixed race in HotSpotSnippetReflectionProvider.getInjectedNodeIntrinsicParameter()

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotSnippetReflectionProvider.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/meta/HotSpotSnippetReflectionProvider.java
@@ -78,7 +78,9 @@ public class HotSpotSnippetReflectionProvider implements SnippetReflectionProvid
     private ResolvedJavaType configType;
 
     public Object getInjectedNodeIntrinsicParameter(ResolvedJavaType type) {
-        if (wordTypesType == null) {
+        // Need to test all fields since there no guarantee under the JMM
+        // about the order in which these fields are written.
+        if (configType == null || wordTypesType == null || configType == null) {
             MetaAccessProvider metaAccess = runtime.getHostProviders().getMetaAccess();
             wordTypesType = metaAccess.lookupJavaType(runtime.getHostProviders().getWordTypes().getClass());
             runtimeType = metaAccess.lookupJavaType(runtime.getClass());
