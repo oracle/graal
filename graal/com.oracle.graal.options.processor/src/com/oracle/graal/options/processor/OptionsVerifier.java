547469fa0b3f96d83d61a7c023bbb5d4cb6f1892
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2015-05-28 16:54:14 +0200

    remove hotspot.sourcegen

--- graal/com.oracle.graal.options.processor/src/com/oracle/graal/options/processor/OptionsVerifier.java
+++ graal/com.oracle.graal.options.processor/src/com/oracle/graal/options/processor/OptionsVerifier.java
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.graal.hotspot.sourcegen;
+package com.oracle.graal.options.processor;
 
 import static java.lang.String.*;
 
