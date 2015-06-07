911eff15e709d45ad9cb28e23014703db7b73e7d
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2015-06-07 16:59:03 +0200

    Fix the anchoring node in the lowering phase to not skip over loop exits.

--- graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/LoweringPhase.java
+++ graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/LoweringPhase.java
@@ -284,7 +284,13 @@ public class LoweringPhase extends BasePhase<PhaseContext> {
 
             @Override
             public Frame<?> enterAlwaysReached(Block b) {
-                return new ProcessFrame(b, activeGuards, anchor, this);
+                AnchoringNode newAnchor = anchor;
+                if (parent != null && b.getLoop() != parent.block.getLoop() && !b.isLoopHeader()) {
+                    // We are exiting a loop => cannot reuse the anchor without inserting loop
+                    // proxies.
+                    newAnchor = b.getBeginNode();
+                }
+                return new ProcessFrame(b, activeGuards, newAnchor, this);
             }
 
             @Override
@@ -401,7 +407,7 @@ public class LoweringPhase extends BasePhase<PhaseContext> {
      *     if (alwaysReachedBlock != null &amp;&amp; alwaysReachedBlock.getDominator() == block) {
      *         processBlock(alwaysReachedBlock);
      *     }
-     * 
+     *
      *     // Now go for the other dominators.
      *     for (Block dominated : block.getDominated()) {
      *         if (dominated != alwaysReachedBlock) {
