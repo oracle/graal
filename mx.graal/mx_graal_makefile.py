4fd39f26582622c2a4cf418d12b3f5294181a58f
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2015-07-15 18:23:34 +0200

    mx makefile: Remove dead code

--- mx.graal/mx_graal_makefile.py
+++ mx.graal/mx_graal_makefile.py
@@ -55,8 +55,7 @@ def build_makefile(args):
     if opts.selectedDists == None or len(opts.selectedDists) == 0:
         opts.selectedDists = [d.name for d in mx_graal._jdkDeployedDists if d.partOfHotSpot]
     mf = Makefile()
-    commandline = " ".join(["mx.sh", "makefile"] + args)
-    if do_build_makefile(mf, opts.selectedDists, commandline):
+    if do_build_makefile(mf, opts.selectedDists):
         contents = mf.generate()
         if opts.output == None:
             print contents
@@ -153,7 +152,7 @@ def make_dist_rule(dist, mf):
 
 
 
-def do_build_makefile(mf, selectedDists, commandline):
+def do_build_makefile(mf, selectedDists):
     java = mx.java()
     bootClassPath = java.bootclasspath()
     bootClassPath = bootClassPath.replace(os.path.realpath(java.jdk), "$(ABS_BOOTDIR)")
