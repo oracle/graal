d5d5344070062325a80648a4407aa290b0f269c0
Josef Eisl <josef.eisl@jku.at>
2015-08-13 14:08:34 +0200

    TraceRA: share spilling information between traces.

--- graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceLinearScanLifetimeAnalysisPhase.java
+++ graal/com.oracle.graal.lir/src/com/oracle/graal/lir/alloc/trace/TraceLinearScanLifetimeAnalysisPhase.java
@@ -148,6 +148,13 @@ public class TraceLinearScanLifetimeAnalysisPhase extends LinearScanLifetimeAnal
                                 Interval from = allocator.getOrCreateInterval((AllocatableValue) fromValue);
                                 Interval to = allocator.getOrCreateInterval((AllocatableValue) toValue);
                                 setHint(label, to, from);
+                            } else if (TraceRAshareSpillInformation.getValue() && TraceUtil.isShadowedRegisterValue(fromValue)) {
+                                ShadowedRegisterValue shadowedRegisterValue = TraceUtil.asShadowedRegisterValue(fromValue);
+                                Interval from = allocator.getOrCreateInterval(shadowedRegisterValue.getRegister());
+                                Interval to = allocator.getOrCreateInterval((AllocatableValue) toValue);
+                                setHint(label, to, from);
+                                to.setSpillSlot(shadowedRegisterValue.getStackSlot());
+                                to.setSpillState(SpillState.StartInMemory);
                             }
                         }
                     }
