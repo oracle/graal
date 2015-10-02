7bd4297b80018d129ccf1e25bb0c80708b22b814
Tom Rodriguez <tom.rodriguez@oracle.com>
2015-10-02 10:15:51 -0700

    Update jvmci import

--- mx.graal/suite.py
+++ mx.graal/suite.py
@@ -6,7 +6,7 @@ suite = {
     "suites": [
             {
                "name" : "jvmci",
-               "version" : "c33f0cb02b34763c55dafa5589dca932e91d3b71",
+               "version" : "8ed4037e828628c8a957ed1ec9cdfdd694c64d91",
                "urls" : [
                     {"url" : "http://lafo.ssw.uni-linz.ac.at/hg/graal-jvmci-8", "kind" : "hg"},
                     {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
