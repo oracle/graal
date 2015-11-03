6c70aa8d04d2531268c5ff9d2a8d9e424e713958
Tom Rodriguez <tom.rodriguez@oracle.com>
2015-11-03 12:11:47 -0800

    Don't discard type information in getSucceedingStampForValue

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/IsNullNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/calc/IsNullNode.java
@@ -103,7 +103,7 @@ public final class IsNullNode extends UnaryOpLogicNode implements LIRLowerable, 
 
     @Override
     public Stamp getSucceedingStampForValue(boolean negated) {
-        return negated ? StampFactory.objectNonNull() : StampFactory.alwaysNull();
+        return negated ? getValue().stamp().join(StampFactory.objectNonNull()) : StampFactory.alwaysNull();
     }
 
     @Override
