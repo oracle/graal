35bfad81e15f42d41aae64919f0d710ab15dc1a2
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2015-11-21 18:58:39 +0100

    Remove value proxy phase should also remove guard proxies.

--- graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/RemoveValueProxyPhase.java
+++ graal/com.oracle.graal.phases.common/src/com/oracle/graal/phases/common/RemoveValueProxyPhase.java
@@ -22,6 +22,7 @@
  */
 package com.oracle.graal.phases.common;
 
+import com.oracle.graal.nodes.EntryProxyNode;
 import com.oracle.graal.nodes.FrameState;
 import com.oracle.graal.nodes.LoopExitNode;
 import com.oracle.graal.nodes.ProxyNode;
@@ -35,7 +36,7 @@ public class RemoveValueProxyPhase extends Phase {
     @Override
     protected void run(StructuredGraph graph) {
         for (ProxyNode vpn : graph.getNodes(ProxyNode.TYPE)) {
-            if (vpn instanceof ValueProxyNode) {
+            if (!(vpn instanceof EntryProxyNode)) {
                 graph.replaceFloating(vpn, vpn.value());
             }
         }
