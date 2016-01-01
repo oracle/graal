20eb03b03afbcf93b83d3741678dd91350a0492d
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2016-01-01 15:37:32 +0100

    Remove FilteredNodeIterable#distinct.

--- graal/com.oracle.graal.graph/src/com/oracle/graal/graph/iterators/FilteredNodeIterable.java
+++ graal/com.oracle.graal.graph/src/com/oracle/graal/graph/iterators/FilteredNodeIterable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -38,13 +38,6 @@ public class FilteredNodeIterable<T extends Node> implements NodeIterable<T> {
     public FilteredNodeIterable<T> and(NodePredicate nodePredicate) {
         this.predicate = this.predicate.and(nodePredicate);
         return this;
-    }
-
-    @Override
-    public DistinctFilteredNodeIterable<T> distinct() {
-        DistinctFilteredNodeIterable<T> distinct = new DistinctFilteredNodeIterable<>(nodeIterable);
-        distinct.predicate = predicate;
-        return distinct;
     }
 
     @Override
