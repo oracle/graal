d3e900294751ad910a1a77bc6338b886c8e121d4
Josef Eisl <josef.eisl@jku.at>
2016-01-19 10:24:05 +0100

    TraceRA: do not use streams in TraceBuilder asserts.

--- graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/alloc/TraceBuilder.java
+++ graal/com.oracle.graal.compiler.common/src/com/oracle/graal/compiler/common/alloc/TraceBuilder.java
@@ -150,7 +150,7 @@ public final class TraceBuilder<T extends AbstractBlockBase<T>> {
     }
 
     private boolean verify(ArrayList<List<T>> traces) {
-        assert traces.stream().flatMap(l -> l.stream()).distinct().count() == blocked.length : "Not all blocks assigned to traces!";
+        assert verifyAllBlocksScheduled(traces, blocked.length) : "Not all blocks assigned to traces!";
         for (List<T> trace : traces) {
             T last = null;
             for (T current : trace) {
@@ -159,6 +159,16 @@ public final class TraceBuilder<T extends AbstractBlockBase<T>> {
             }
         }
         return true;
+    }
+
+    private static <T extends AbstractBlockBase<T>> boolean verifyAllBlocksScheduled(ArrayList<List<T>> traces, int expectedLength) {
+        BitSet handled = new BitSet(expectedLength);
+        for (List<T> trace : traces) {
+            for (T block : trace) {
+                handled.set(block.getId());
+            }
+        }
+        return handled.cardinality() == expectedLength;
     }
 
     protected ArrayList<List<T>> buildTraces(T startBlock) {
@@ -181,7 +191,7 @@ public final class TraceBuilder<T extends AbstractBlockBase<T>> {
      */
     @SuppressWarnings("try")
     private List<T> startTrace(T block, int traceNumber) {
-        assert block.getPredecessors().stream().allMatch(this::processed) : "Predecessor unscheduled: " + block.getPredecessors().stream().filter(this::processed).findFirst().get();
+        assert checkPredecessorsProcessed(block);
         ArrayList<T> trace = new ArrayList<>();
         int blockNumber = 0;
         try (Indent i = Debug.logAndIndent("StartTrace: " + block)) {
@@ -195,6 +205,18 @@ public final class TraceBuilder<T extends AbstractBlockBase<T>> {
             }
         }
         return trace;
+    }
+
+    private boolean checkPredecessorsProcessed(T block) {
+        List<T> predecessors = block.getPredecessors();
+        for (T pred : predecessors) {
+            if (!processed(pred)) {
+                assert false : "Predecessor unscheduled: " + pred;
+                return false;
+            }
+
+        }
+        return true;
     }
 
     /**
