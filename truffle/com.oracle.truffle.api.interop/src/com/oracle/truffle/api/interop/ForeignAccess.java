06086c6ae39bfaf95bfe093cebc1287bd5a3a13b
Matthias Grimmer <grimmer@ssw.jku.at>
2016-01-26 12:16:38 +0100

    Fix checkstyle warnings and eclipseformat

--- truffle/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/ForeignAccess.java
+++ truffle/com.oracle.truffle.api.interop/src/com/oracle/truffle/api/interop/ForeignAccess.java
@@ -171,8 +171,8 @@ public final class ForeignAccess {
      *             property for the given <code>identifier</code>
      * @throws UnsupportedTypeException if <code>value</code> has an unsupported type
      */
-    public static Object sendWrite(Node writeNode, VirtualFrame frame, TruffleObject receiver, Object identifier, Object value)
-                    throws UnknownIdentifierException, UnsupportedTypeException, UnsupportedMessageException {
+    public static Object sendWrite(Node writeNode, VirtualFrame frame, TruffleObject receiver, Object identifier, Object value) throws UnknownIdentifierException, UnsupportedTypeException,
+                    UnsupportedMessageException {
         ForeignObjectAccessHeadNode fn = (ForeignObjectAccessHeadNode) writeNode;
         try {
             return fn.executeForeignImpl(frame, receiver, new Object[]{identifier, value});
@@ -279,8 +279,8 @@ public final class ForeignAccess {
      * @throws UnsupportedMessageException if the <code>receiver</code> does not support the
      *             {@link Message#createNode() message represented} by <code>invokeNode</code>
      */
-    public static Object sendInvoke(Node invokeNode, VirtualFrame frame, TruffleObject receiver, String identifier, Object[] arguments)
-                    throws UnsupportedTypeException, ArityException, UnknownIdentifierException, UnsupportedMessageException {
+    public static Object sendInvoke(Node invokeNode, VirtualFrame frame, TruffleObject receiver, String identifier, Object[] arguments) throws UnsupportedTypeException, ArityException,
+                    UnknownIdentifierException, UnsupportedMessageException {
         ForeignObjectAccessHeadNode fn = (ForeignObjectAccessHeadNode) invokeNode;
         try {
             Object[] args = new Object[arguments.length + 1];
