6cf7217a915623d9ec57adb8703272bc0a36d023
Benoit Daloze <eregontp@gmail.com>
2016-02-02 18:53:09 +0100

    Choose a standard name for bad frame arguments

--- truffle/com.oracle.truffle.api.dsl.test/src/com/oracle/truffle/api/dsl/test/interop/Execute2.java
+++ truffle/com.oracle.truffle.api.dsl.test/src/com/oracle/truffle/api/dsl/test/interop/Execute2.java
@@ -29,7 +29,7 @@ import com.oracle.truffle.api.interop.AcceptMessage;
 public final class Execute2 extends BaseExecute2 {
     @SuppressWarnings({"static-method", "unused"})
     @ExpectError({"The first argument must be a com.oracle.truffle.api.frame.VirtualFrame- but is java.lang.String"})
-    public Object access(String frame, ValidTruffleObject object, Object[] args) {
+    public Object access(String string, ValidTruffleObject object, Object[] args) {
         return true;
     }
 }
