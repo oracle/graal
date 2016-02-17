3013c75ead8a61dace61c25054781f6fda338df3
Christian Humer <christian.humer@oracle.com>
2016-02-17 15:29:07 +0100

    Fix bug in error reporting which generated code even if the parsed node was invalid.

--- truffle/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/model/MessageContainer.java
+++ truffle/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/model/MessageContainer.java
@@ -221,7 +221,7 @@ public abstract class MessageContainer implements Iterable<MessageContainer> {
         }
         for (MessageContainer sink : findChildContainers()) {
             if (visitedSinks.contains(sink)) {
-                return false;
+                continue;
             }
 
             visitedSinks.add(sink);
