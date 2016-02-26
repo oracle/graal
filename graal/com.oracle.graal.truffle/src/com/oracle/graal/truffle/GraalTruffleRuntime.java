6d1d86fc4d91571f35458b98c0c34683d71d948f
Christian Humer <christian.humer@oracle.com>
2016-02-26 15:35:56 +0100

    Fix checkstyle

--- graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/GraalTruffleRuntime.java
+++ graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/GraalTruffleRuntime.java
@@ -257,14 +257,14 @@ public abstract class GraalTruffleRuntime implements TruffleRuntime {
         private boolean first = true;
         private int skipFrames;
 
-        public FrameVisitor(FrameInstanceVisitor<T> visitor, CallMethods methods, int skip) {
+        FrameVisitor(FrameInstanceVisitor<T> visitor, CallMethods methods, int skip) {
             this.visitor = visitor;
             this.callTargetMethod = methods.callTargetMethod;
             this.callNodeMethod = methods.callNodeMethod;
             this.skipFrames = skip;
         }
 
-        public final T visitFrame(InspectedFrame frame) {
+        public T visitFrame(InspectedFrame frame) {
             if (nextAvailable) {
                 T result = onNext(frame);
                 if (result != null) {
@@ -326,9 +326,7 @@ public abstract class GraalTruffleRuntime implements TruffleRuntime {
 
     @Override
     public FrameInstance getCallerFrame() {
-        return iterateImpl(frame -> {
-            return frame;
-        }, 1);
+        return iterateImpl(frame -> frame, 1);
     }
 
     @TruffleBoundary
