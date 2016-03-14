a66b0350b5486fbb301b6b017df9018216b4f265
Cosmin Basca <cosmin.basca@gmail.com>
2016-03-14 19:03:08 +0100

    package-info.java is in. Fixed project name typo (example -> examples). Specified missing generic types.

--- truffle/com.oracle.truffle.examples/src/com/oracle/truffle/examples/simplegraphanalytics/GraphSampleApp.java
+++ truffle/com.oracle.truffle.examples/src/com/oracle/truffle/examples/simplegraphanalytics/GraphSampleApp.java
@@ -1,14 +1,55 @@
 /*
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
  * Example graph analytics query program using Truffle for speeding up queries.
  */
-package com.oracle.truffle.example.simplegraphanalitycs;
+package com.oracle.truffle.examples.simplegraphanalytics;
 
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.RootCallTarget;
 import java.util.function.Supplier;
 
 public class GraphSampleApp {
-    
+
     public static final int MEASUREMENT_COUNT = 100;
     public static final int QUERIES_PER_MEASUREMENT = 100;
 
@@ -18,7 +59,7 @@ public class GraphSampleApp {
         Query q2 = new Query(graph, 2);
         run(q1, q2);
     }
-    
+
     public static void run(Query q1, Query q2) {
         RootCallTarget truffleQ1 = TruffleExt.createCallTarget(q1);
         RootCallTarget truffleQ2 = TruffleExt.createCallTarget(q2);
@@ -64,7 +105,7 @@ public class GraphSampleApp {
      * The query sums up the value of all edge endpoint values where the sum of source and destination node value is divisible
      * by {@link #divisor}.
      */
-    public static class Query implements Supplier {
+    public static class Query implements Supplier<Integer>{
         
         private final Graph graph;
         private final int divisor;
@@ -82,7 +123,7 @@ public class GraphSampleApp {
          * @return the value of the query
          */
         @Override
-        public Object get() {
+        public Integer get() {
             int result = 0;
             for (int i = 0; i < graph.nodeValues.length; ++i) {
                 result += divisibleEdgeSumAll(i, divisor);
