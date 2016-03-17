74a8f75cd322d749b7fc7a4a5b1fa1f1f52b5a6f
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-03-17 09:54:11 +0100

    Initialize the oneVM assumption lazily to prevent query into Truffle.getRuntime() sooner than it is initialized

--- truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
+++ truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
@@ -307,7 +307,7 @@ public abstract class Accessor {
     }
 
     private static Reference<Object> previousVM = new WeakReference<>(null);
-    private static Assumption oneVM = Truffle.getRuntime().createAssumption();
+    private static Assumption oneVM;
 
     @TruffleBoundary
     protected Closeable executionStart(Object vm, @SuppressWarnings("unused") int currentDepth, boolean debugger, Source s) {
@@ -317,9 +317,10 @@ public abstract class Accessor {
         final Closeable debugClose = DEBUG == null ? null : DEBUG.executionStart(vm, prev == null ? 0 : -1, debugger, s);
         if (!(vm == previousVM.get())) {
             previousVM = new WeakReference<>(vm);
-            oneVM.invalidate();
-            oneVM = Truffle.getRuntime().createAssumption();
-
+            if (oneVM != null) {
+                oneVM.invalidate();
+            }
+            oneVMAssumption();
         }
         CURRENT_VM.set(vm);
         class ContextCloseable implements Closeable {
@@ -339,7 +340,10 @@ public abstract class Accessor {
         SPI.dispatchEvent(vm, event);
     }
 
-    static Assumption oneVMAssumption() {
+    synchronized static Assumption oneVMAssumption() {
+        if (oneVM == null || oneVM.isValid()) {
+            oneVM = Truffle.getRuntime().createAssumption();
+        }
         return oneVM;
     }
 
