bcc38a72966c61564fa2b0e52196f93ff7a48d09
David Leopoldseder <david.leopoldseder@jku.at>
2016-03-17 09:18:30 +0100

    verify debug usage: remove eager toString call in bailout case

--- graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/substitutions/TruffleGraphBuilderPlugins.java
+++ graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/substitutions/TruffleGraphBuilderPlugins.java
@@ -318,7 +318,7 @@ public class TruffleGraphBuilderPlugins {
                         sb.append(")");
                     }
                     Debug.dump(value.graph(), "Graph before bailout at node %s", sb);
-                    throw b.bailout("Partial evaluation did not reduce value to a constant, is a regular compiler node: " + sb.toString());
+                    throw b.bailout("Partial evaluation did not reduce value to a constant, is a regular compiler node: " + sb);
                 }
             }
         });
