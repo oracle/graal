5bfc24ea9ee8016f0a68abf46e5230a09da2212d
Chris Seaton <chris.seaton@oracle.com>
2016-03-27 18:22:36 +0100

    Add a su-gfortran command

--- mx.sulong/mx_sulong.py
+++ mx.sulong/mx_sulong.py
@@ -169,6 +169,11 @@ def dragonEgg(args=None):
     executeCommand = [getGCC(), "-fplugin=" + _dragonEggPath, '-fplugin-arg-dragonegg-emit-ir']
     return mx.run(executeCommand + args)
 
+def dragonEggGFortran(args=None):
+    """executes GCC Fortran with dragonegg"""
+    executeCommand = [getGFortran(), "-fplugin=" + _dragonEggPath, '-fplugin-arg-dragonegg-emit-ir']
+    return mx.run(executeCommand + args)
+
 def dragonEggGPP(args=None):
     """executes G++ with dragonegg"""
     executeCommand = [getGPP(), "-fplugin=" + _dragonEggPath, '-fplugin-arg-dragonegg-emit-ir']
@@ -200,6 +205,15 @@ def getDefaultGCC():
         return 'gcc46'
     return None
 
+def getDefaultGFortran():
+    # Ubuntu
+    if which('gfortran-4.6') is not None:
+        return 'gfortran-4.6'
+    # Mac
+    if which('gfortran46') is not None:
+        return 'gfortran46'
+    return None
+
 def getDefaultGPP():
     # Ubuntu
     if which('g++-4.6') is not None:
@@ -226,6 +240,17 @@ def getGCC():
         return getDefaultGCC()
     else:
         mx.abort('Could not find a compatible GCC version to execute Dragonegg! Please install gcc-4.6 or another compatible version and specify it in the env file')
+
+def getGFortran():
+    """tries to locate a gfortran version suitable to execute Dragonegg"""
+    specifiedGFortran = getCommand('SULONG_GFORTRAN')
+    if specifiedGFortran is not None:
+        return specifiedGFortran
+    if getDefaultGFortran() is not None:
+        return getDefaultGFortran()
+    else:
+        mx.abort('Could not find a compatible GFortran version to execute Dragonegg! Please install gfortran-4.6 or another compatible version and specify it in the env file')
+
 
 def getGPP():
     """tries to locate a g++ version suitable to execute Dragonegg"""
@@ -613,6 +638,7 @@ mx.update_commands(_suite, {
     'su-clang++' : [compileWithClangPP, ''],
     'su-opt' : [opt, ''],
     'su-gcc' : [dragonEgg, ''],
+    'su-gfortran' : [dragonEggGFortran, ''],
     'su-g++' : [dragonEggGPP, ''],
     'su-travis1' : [travis1, ''],
     'su-travis2' : [travis2, '']
