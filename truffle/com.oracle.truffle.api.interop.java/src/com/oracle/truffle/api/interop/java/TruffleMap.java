10764114a091aeaafed35dbb8efccec846a03a98
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-03-29 14:47:08 +0200

    Implement entrySet <-> Message.PROPERTIES mapping

--- truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/TruffleMap.java
+++ truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/TruffleMap.java
@@ -28,6 +28,8 @@ import com.oracle.truffle.api.interop.InteropException;
 import com.oracle.truffle.api.interop.Message;
 import com.oracle.truffle.api.interop.TruffleObject;
 import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -48,7 +50,21 @@ final class TruffleMap<K, V> extends AbstractMap<K, V> {
 
     @Override
     public Set<Entry<K, V>> entrySet() {
-        throw new UnsupportedOperationException();
+        try {
+            Object props = ToJavaNode.message(Message.PROPERTIES, obj);
+            if (Boolean.TRUE.equals(ToJavaNode.message(Message.HAS_SIZE, props))) {
+                Number size = (Number)ToJavaNode.message(Message.GET_SIZE, props);
+                LinkedHashSet<Entry<K,V>> entries = new LinkedHashSet<>();
+                for (int i = 0; i < size.intValue(); i++) {
+                    Object key = ToJavaNode.message(Message.READ, props, i);
+                    entries.add(new TruffleEntry(keyType.cast(key)));
+                }
+                return entries;
+            }
+            return Collections.emptySet();
+        } catch (InteropException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 
     @Override
@@ -69,6 +85,39 @@ final class TruffleMap<K, V> extends AbstractMap<K, V> {
             return valueType.cast(ToJavaNode.message(Message.WRITE, obj, key, value));
         } catch (InteropException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    private final class TruffleEntry implements Entry<K, V> {
+        private final K key;
+
+        public TruffleEntry(K key) {
+            this.key = key;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            try {
+                return valueType.cast(ToJavaNode.message(Message.READ, obj, key));
+            } catch (InteropException ex) {
+                throw new IllegalStateException(ex);
+            }
+        }
+
+        @Override
+        public V setValue(V value) {
+            try {
+                V prev = getValue();
+                ToJavaNode.message(Message.WRITE, obj, key, value);
+                return prev;
+            } catch (InteropException ex) {
+                throw new IllegalStateException(ex);
+            }
         }
     }
 }
