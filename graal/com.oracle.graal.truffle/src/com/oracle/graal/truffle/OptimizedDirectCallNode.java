0cc708c22aed5ae4c997ea77e03ce2c97d1d6835
Benoit Daloze <eregontp@gmail.com>
2016-03-30 17:32:59 +0200

    Ensure OptimizedDirectCallNode is only created with "source" (not split) call targets.

--- graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedDirectCallNode.java
+++ graal/com.oracle.graal.truffle/src/com/oracle/graal/truffle/OptimizedDirectCallNode.java
@@ -48,6 +48,7 @@ public final class OptimizedDirectCallNode extends DirectCallNode implements Mat
 
     public OptimizedDirectCallNode(GraalTruffleRuntime runtime, OptimizedCallTarget target) {
         super(target);
+        assert target.getSourceCallTarget() == null;
         this.runtime = runtime;
         this.splittingStrategy = new DefaultTruffleSplittingStrategy(this);
     }
