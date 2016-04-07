b7eb0b6139be9b389950a94dd6c1f79c4c5d3774
Manuel Rigger <manuel.rigger@jku.at>
2016-04-07 13:40:45 +0200

    Remove obsolete createPhiNode method in the facade adapter

--- projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/NodeFactoryFacadeAdapter.java
+++ projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/NodeFactoryFacadeAdapter.java
@@ -282,11 +282,6 @@ public abstract class NodeFactoryFacadeAdapter implements NodeFactoryFacade {
     }
 
     @Override
-    public LLVMNode createPhiNode() {
-        return null;
-    }
-
-    @Override
     public LLVMNode createBasicBlockNode(LLVMNode[] statementNodes, LLVMNode terminatorNode) {
         return null;
     }
