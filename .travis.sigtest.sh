f91c754ade0e67b399c5fb8bad3a09839ca7496e
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-04-11 10:23:21 +0200

    If the local, relative mx can't be found, use the one from global PATH

--- .travis.sigtest.sh
+++ .travis.sigtest.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 wget http://hudson.apidesign.org/job/sigtest-truffle/lastSuccessfulBuild/artifact/truffle-signature.tgz
 tar fxvz truffle-signature.tgz
-mx/mx build
-mx/mx sigtest --check all || echo There are changes!
+MX=mx
+if [ -e mx/mx ]; then
+  MX=mx/mx
+fi
+$MX build
+$MX sigtest --check all || echo There are changes!
