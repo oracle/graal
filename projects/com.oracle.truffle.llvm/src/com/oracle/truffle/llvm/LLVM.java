4d92ce2444c721110ebbb52bc5e2710b4875d617
Manuel Rigger <manuel.rigger@jku.at>
2016-04-14 14:36:06 +0200

    Fix wrong formatting

--- projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/LLVM.java
+++ projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/LLVM.java
@@ -97,7 +97,7 @@ public class LLVM {
                     try {
                         library.readContents(dependentLibrary -> {
                             throw new UnsupportedOperationException();
-                        } , source -> {
+                        }, source -> {
                             ParserResult parserResult;
                             try {
                                 parserResult = parseString(source.getCode(), context);
