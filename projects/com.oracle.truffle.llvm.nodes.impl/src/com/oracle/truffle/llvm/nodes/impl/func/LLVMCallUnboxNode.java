26e03c9946a840f282d44d79347daec8423a0584
Matthias Grimmer <grimmer@ssw.jku.at>
2016-04-20 14:06:41 +0200

    Add TruffleObject specialization to LLVMAddressCallUnboxNode

--- projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/func/LLVMCallUnboxNode.java
+++ projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/func/LLVMCallUnboxNode.java
@@ -32,6 +32,7 @@ package com.oracle.truffle.llvm.nodes.impl.func;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.llvm.nodes.base.LLVMExpressionNode;
 import com.oracle.truffle.llvm.nodes.base.LLVMNode;
 import com.oracle.truffle.llvm.nodes.impl.base.LLVMAddressNode;
@@ -166,6 +167,11 @@ public abstract class LLVMCallUnboxNode {
 
     @NodeChild(type = LLVMExpressionNode.class, value = "functionCallNode")
     public abstract static class LLVMAddressCallUnboxNode extends LLVMAddressNode {
+
+        @Specialization
+        public TruffleObject executePointee(TruffleObject value) {
+            return value;
+        }
 
         @Specialization
         public LLVMAddress executePointee(Object value) {
