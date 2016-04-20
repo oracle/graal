dbd2ffc09e4efbccf431abe14feda294642e8f9d
David Gnedt <david.gnedt@jku.at>
2016-04-20 10:28:54 +0200

    Introduce flags for test cases
    Flags can be set in test case specification files.
    
    Implemented flags:
     UNDEFINED_RETURN_CODE

--- projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/TestCaseFiles.java
+++ projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/TestCaseFiles.java
@@ -30,6 +30,8 @@
 package com.oracle.truffle.llvm.test;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.Set;
 
 import com.oracle.truffle.llvm.tools.util.PathUtil;
 
@@ -38,20 +40,22 @@ public final class TestCaseFiles {
     private final File originalFile;
     private final File bitCodeFile;
     private final File expectedResult;
+    private final Set<TestCaseFlag> flags;
 
-    private TestCaseFiles(File bitCodeFile) {
-        this(bitCodeFile, bitCodeFile, bitCodeFile);
+    private TestCaseFiles(File bitCodeFile, Set<TestCaseFlag> flags) {
+        this(bitCodeFile, bitCodeFile, bitCodeFile, flags);
     }
 
-    private TestCaseFiles(File originalFile, File byteCodeFile) {
-        this(originalFile, byteCodeFile, null);
+    private TestCaseFiles(File originalFile, File byteCodeFile, Set<TestCaseFlag> flags) {
+        this(originalFile, byteCodeFile, null, flags);
     }
 
-    private TestCaseFiles(File originalFile, File bitCodeFile, File expectedResult) {
+    private TestCaseFiles(File originalFile, File bitCodeFile, File expectedResult, Set<TestCaseFlag> flags) {
         checkBitCodeFile(bitCodeFile);
         this.originalFile = originalFile;
         this.bitCodeFile = bitCodeFile;
         this.expectedResult = expectedResult;
+        this.flags = flags;
     }
 
     public File getOriginalFile() {
@@ -66,20 +70,28 @@ public final class TestCaseFiles {
         return expectedResult;
     }
 
-    public static TestCaseFiles createFromCompiledFile(File originalFile, File bitCodeFile, File expectedResult) {
-        return new TestCaseFiles(originalFile, bitCodeFile, expectedResult);
+    public boolean hasFlag(TestCaseFlag flag) {
+        return flags.contains(flag);
     }
 
-    public static TestCaseFiles createFromCompiledFile(File originalFile, File bitCodeFile) {
-        return new TestCaseFiles(originalFile, bitCodeFile);
+    public Set<TestCaseFlag> getFlags() {
+        return Collections.unmodifiableSet(flags);
     }
 
-    public static TestCaseFiles createFromBitCodeFile(File bitCodeFile) {
-        return new TestCaseFiles(bitCodeFile);
+    public static TestCaseFiles createFromCompiledFile(File originalFile, File bitCodeFile, File expectedResult, Set<TestCaseFlag> flags) {
+        return new TestCaseFiles(originalFile, bitCodeFile, expectedResult, flags);
     }
 
-    public static TestCaseFiles createFromBitCodeFile(File bitCodeFile, File expectedResult) {
-        return new TestCaseFiles(bitCodeFile, bitCodeFile, expectedResult);
+    public static TestCaseFiles createFromCompiledFile(File originalFile, File bitCodeFile, Set<TestCaseFlag> flags) {
+        return new TestCaseFiles(originalFile, bitCodeFile, flags);
+    }
+
+    public static TestCaseFiles createFromBitCodeFile(File bitCodeFile, Set<TestCaseFlag> flags) {
+        return new TestCaseFiles(bitCodeFile, flags);
+    }
+
+    public static TestCaseFiles createFromBitCodeFile(File bitCodeFile, File expectedResult, Set<TestCaseFlag> flags) {
+        return new TestCaseFiles(bitCodeFile, bitCodeFile, expectedResult, flags);
     }
 
     private static void checkBitCodeFile(File bitCodeFile) {
@@ -108,7 +120,7 @@ public final class TestCaseFiles {
 
     @Override
     public String toString() {
-        return "original file: " + originalFile + " bitcode file: " + bitCodeFile + " expected result: " + expectedResult;
+        return "original file: " + originalFile + " bitcode file: " + bitCodeFile + " expected result: " + expectedResult + " flags: " + flags;
     }
 
 }
