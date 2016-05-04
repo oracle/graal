eb554c4c509d2d1f6bf0b192fd458af052162efe
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-05-04 14:23:28 +0200

    Be ready for default methods - if there is more methods in the interface, ignore those that aren't abstract

--- truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/JavaInterop.java
+++ truffle/com.oracle.truffle.api.interop.java/src/com/oracle/truffle/api/interop/java/JavaInterop.java
@@ -35,6 +35,7 @@ import com.oracle.truffle.api.interop.Message;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.RootNode;
+import java.lang.reflect.Modifier;
 
 /**
  * Helper methods to simplify access to objects of {@link TruffleLanguage Truffle languages} from
@@ -225,11 +226,32 @@ public final class JavaInterop {
      * @since 0.9
      */
     public static <T> TruffleObject asTruffleFunction(Class<T> functionalType, T implementation) {
-        final Method[] arr = functionalType.getDeclaredMethods();
-        if (!functionalType.isInterface() || arr.length != 1) {
+        final Method method = functionalInterfaceMethod(functionalType);
+        if (method == null) {
             throw new IllegalArgumentException();
         }
-        return new JavaFunctionObject(arr[0], implementation);
+        return new JavaFunctionObject(method, implementation);
+    }
+
+    private static <T> Method functionalInterfaceMethod(Class<T> functionalType) {
+        if (!functionalType.isInterface()) {
+            return null;
+        }
+        final Method[] arr = functionalType.getMethods();
+        if (arr.length == 1) {
+            return arr[0];
+        }
+        Method found = null;
+        for (Method m : arr) {
+            if ((m.getModifiers() & Modifier.ABSTRACT) == 0) {
+                continue;
+            }
+            if (found != null) {
+                throw new IllegalArgumentException();
+            }
+            found = m;
+        }
+        return found;
     }
 
     private static class TemporaryConvertRoot extends RootNode {
