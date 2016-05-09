404b5d9b0bf25f1eae21eb4e6e8b22600e5d1df5
Doug Simon <doug.simon@oracle.com>
2016-05-09 17:07:02 +0200

    updated jvmci import

--- mx.graal-core/suite.py
+++ mx.graal-core/suite.py
@@ -8,7 +8,7 @@ suite = {
         "name" : "jvmci",
         "jdkProvidedSince" : "9",
         "optional" : "true",
-        "version" : "3f70efb8f473c860f9908f5cd06719d7948ac60d",
+        "version" : "cd8fd4cced6ca96311c9e223f8da4f383ddbeaf9",
         "urls" : [
           {"url" : "http://lafo.ssw.uni-linz.ac.at/hg/graal-jvmci-8", "kind" : "hg"},
           {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
