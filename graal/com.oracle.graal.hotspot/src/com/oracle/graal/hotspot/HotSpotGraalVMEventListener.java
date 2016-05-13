0f8a5e3073ee95b3dac3e40e2f9b940d6b6d1dd9
Tom Rodriguez <tom.rodriguez@oracle.com>
2016-05-12 21:22:56 -0700

    Remove assert that InstalledCode and CompilationResult must be paired in dump

--- graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotGraalVMEventListener.java
+++ graal/com.oracle.graal.hotspot/src/com/oracle/graal/hotspot/HotSpotGraalVMEventListener.java
@@ -24,6 +24,7 @@ package com.oracle.graal.hotspot;
 
 import java.util.ArrayList;
 
+import com.oracle.graal.code.CompilationResult;
 import com.oracle.graal.debug.Debug;
 import com.oracle.graal.serviceprovider.ServiceProvider;
 
@@ -51,6 +52,8 @@ public class HotSpotGraalVMEventListener extends HotSpotVMEventListener {
     @Override
     public void notifyInstall(HotSpotCodeCacheProvider codeCache, InstalledCode installedCode, CompiledCode compiledCode) {
         if (Debug.isDumpEnabled(Debug.BASIC_LOG_LEVEL)) {
+            CompilationResult compResult = Debug.contextLookup(CompilationResult.class);
+            assert compResult != null : "can't dump installed code properly without CompilationResult";
             Debug.dump(Debug.BASIC_LOG_LEVEL, installedCode, "After code installation");
         }
         if (Debug.isLogEnabled()) {
