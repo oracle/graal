b5fa409df9b0eaac8040e43c0017bf933742f4cc
Manuel Rigger <manuel.rigger@jku.at>
2016-05-16 11:32:11 +0200

    Add test cases for packed structs

---
+++ projects/com.oracle.truffle.llvm.test/tests/c/struct/packed/packed2.1.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#pragma pack(1)
+
+struct test {
+  char a;
+  int b;
+  int c;
+};
+
+int main() {
+  struct test t;
+  t.a = 3;
+  t.b = 5;
+  t.c = 0;
+  long val = *((long *)&t.a);
+  if (t.a != 3 || t.b != 5 || t.c != 0) {
+    abort();
+  }
+  if (val != 1283L) {
+    abort();
+  }
+  return 0;
+}
