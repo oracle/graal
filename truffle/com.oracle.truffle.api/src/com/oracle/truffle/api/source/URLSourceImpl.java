9285f96025af434b2cfcb76960f47bcfdea502c9
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-05-19 09:53:39 +0200

    Catching all exception that may originate from Paths.get

--- truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/URLSourceImpl.java
+++ truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/source/URLSourceImpl.java
@@ -32,6 +32,7 @@ import java.lang.ref.WeakReference;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -104,7 +105,7 @@ final class URLSourceImpl extends Content {
             if (firstGuess != null) {
                 return firstGuess;
             }
-        } catch (URISyntaxException ex) {
+        } catch (URISyntaxException | IllegalArgumentException | FileSystemNotFoundException ex) {
             // swallow and go on
         }
         return url.openConnection().getContentType();
