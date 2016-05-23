266a4b771292fd4b6085310cf2ca7d4638ebad1b
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-05-23 12:53:30 +0200

    When running individual unit tests, the debugger subsystem may not be present on classpath

--- truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
+++ truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/Accessor.java
@@ -202,7 +202,11 @@ public abstract class Accessor {
         try {
             Class.forName("com.oracle.truffle.api.debug.Debugger", true, Accessor.class.getClassLoader());
         } catch (ClassNotFoundException ex) {
-            throw new IllegalStateException(ex);
+            boolean assertOn = false;
+            assert assertOn = true;
+            if (!assertOn) {
+                throw new IllegalStateException(ex);
+            }
         }
     }
 
