4d93805f80a5bbd0cfdbe646435db45a182e851a
PrinzKatharina <kaykay.sunny@gmx.at>
2016-06-01 14:42:16 +0200

    Fix inline assembly format

--- projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly003.c
+++ projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly003.c
@@ -1,8 +1,12 @@
+#include <stdlib.h>
+
 int main() {
-	int arg1 = 0;
-	int not = 0;
-	__asm__ ( "notl %%eax;"
-        : "=a" (not)
-        : "a" (arg1) );
-	return not;
+  int arg1 = 0;
+  int not = 0;
+  __asm__("notl %%eax;" : "=a"(not) : "a"(arg1));
+
+  if (not != -1) {
+    abort();
+  }
+  return 0;
 }
