03a29d60a2466adb948c0383ab7c524d739412ee
Manuel Rigger <manuel.rigger@jku.at>
2016-06-08 09:29:34 +0200

    Add a LLVMFloatVectorToI64Node cast node
    This change is in preparation for complex numbers.

--- projects/com.oracle.truffle.llvm.parser.factories/src/com/oracle/truffle/llvm/parser/factories/LLVMCastsFactory.java
+++ projects/com.oracle.truffle.llvm.parser.factories/src/com/oracle/truffle/llvm/parser/factories/LLVMCastsFactory.java
@@ -42,6 +42,7 @@ import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI32Node;
 import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI64Node;
 import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMI8Node;
 import com.oracle.truffle.llvm.nodes.impl.base.integers.LLVMIVarBitNode;
+import com.oracle.truffle.llvm.nodes.impl.base.vector.LLVMFloatVectorNode;
 import com.oracle.truffle.llvm.nodes.impl.base.vector.LLVMI32VectorNode;
 import com.oracle.truffle.llvm.nodes.impl.base.vector.LLVMI8VectorNode;
 import com.oracle.truffle.llvm.nodes.impl.cast.LLVMTo80BitFloatingNodeFactory.LLVMDoubleToLLVM80BitFloatNodeGen;
@@ -110,6 +111,7 @@ import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMAddressT
 import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMDoubleToI64BitCastNodeGen;
 import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMDoubleToI64NodeGen;
 import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMFloatToI64NodeGen;
+import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMFloatVectorToI64NodeGen;
 import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMFunctionToI64NodeGen;
 import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMI16ToI64NodeGen;
 import com.oracle.truffle.llvm.nodes.impl.cast.LLVMToI64NodeFactory.LLVMI16ToI64ZeroExtNodeGen;
@@ -214,8 +216,19 @@ public final class LLVMCastsFactory {
                 return factory.castFromPointer((LLVMAddressNode) fromNode);
             case FUNCTION_ADDRESS:
                 return factory.castFromFunctionPointer((LLVMFunctionNode) fromNode);
+            case FLOAT_VECTOR:
+                return factory.castFromFloatVector((LLVMFloatVectorNode) fromNode);
             default:
                 throw new AssertionError(fromType);
+        }
+    }
+
+    private LLVMExpressionNode castFromFloatVector(LLVMFloatVectorNode fromNode) {
+        switch (targetType) {
+            case I64:
+                return LLVMFloatVectorToI64NodeGen.create(fromNode);
+            default:
+                throw new AssertionError(targetType + " " + conv);
         }
     }
 
