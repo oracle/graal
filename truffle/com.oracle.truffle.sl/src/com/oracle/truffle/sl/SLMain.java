d1579af3465c9eebed1c30c6e59db5fee555ca54
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2016-06-08 19:24:06 +0200

    Using non-deprecated Source builder methods in SL

--- truffle/com.oracle.truffle.sl/src/com/oracle/truffle/sl/SLMain.java
+++ truffle/com.oracle.truffle.sl/src/com/oracle/truffle/sl/SLMain.java
@@ -237,7 +237,7 @@ public final class SLMain {
         if (ex.getNode() != null && ex.getNode().getSourceSection() != null) {
             SourceSection ss = ex.getNode().getSourceSection();
             if (ss != null && ss.getSource() != null) {
-                result.append(" at ").append(ss.getSource().getShortName()).append(" line ").append(ss.getStartLine()).append(" col ").append(ss.getStartColumn());
+                result.append(" at ").append(ss.getSource().getName()).append(" line ").append(ss.getStartLine()).append(" col ").append(ss.getStartColumn());
             }
         }
         result.append(": operation");
