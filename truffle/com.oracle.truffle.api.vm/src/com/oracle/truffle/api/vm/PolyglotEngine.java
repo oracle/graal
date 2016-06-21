cd2cb9b787372875e18e3cf7379aace2494d1132
Christian Humer <christian.humer@oracle.com>
2016-06-21 22:20:56 +0200

    Fix enabling and disabling the same instrument in parallel must not be possible. Introduce a lock for that.

--- truffle/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotEngine.java
+++ truffle/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotEngine.java
@@ -949,7 +949,7 @@ public class PolyglotEngine {
     public final class Instrument {
 
         private final InstrumentCache info;
-
+        private final Object instrumentLock = new Object();
         private volatile boolean enabled;
 
         Instrument(InstrumentCache cache) {
@@ -1016,35 +1016,37 @@ public class PolyglotEngine {
             if (disposed) {
                 throw new IllegalStateException("Engine has already been disposed");
             }
-            if (this.enabled != enabled) {
-                if (executor == null) {
-                    setEnabledImpl(enabled, true);
-                } else {
-                    ComputeInExecutor<Void> compute = new ComputeInExecutor<Void>(executor) {
-                        @Override
-                        protected Void compute() throws IOException {
-                            setEnabledImpl(enabled, true);
-                            return null;
-                        }
 
-                    };
-                    try {
-                        compute.perform();
-                    } catch (IOException ex) {
-                        throw new IllegalStateException(ex);
+            if (this.enabled != enabled) {
+                synchronized (instrumentLock) {
+                    if (this.enabled != enabled) {
+                        if (executor == null) {
+                            setEnabledImpl(enabled, true);
+                        } else {
+                            ComputeInExecutor<Void> compute = new ComputeInExecutor<Void>(executor) {
+                                @Override
+                                protected Void compute() throws IOException {
+                                    setEnabledImpl(enabled, true);
+                                    return null;
+                                }
+                            };
+                            try {
+                                compute.perform();
+                            } catch (IOException ex) {
+                                throw new IllegalStateException(ex);
+                            }
+                        }
+                        this.enabled = enabled;
                     }
                 }
             }
         }
 
         void setEnabledImpl(final boolean enabled, boolean cleanup) {
-            if (this.enabled != enabled) { // check again for thread safety
-                if (enabled) {
-                    Access.INSTRUMENT.addInstrument(instrumentationHandler, this, getCache().getInstrumentationClass());
-                } else {
-                    Access.INSTRUMENT.disposeInstrument(instrumentationHandler, this, cleanup);
-                }
-                this.enabled = enabled;
+            if (enabled) {
+                Access.INSTRUMENT.addInstrument(instrumentationHandler, this, getCache().getInstrumentationClass());
+            } else {
+                Access.INSTRUMENT.disposeInstrument(instrumentationHandler, this, cleanup);
             }
         }
 
