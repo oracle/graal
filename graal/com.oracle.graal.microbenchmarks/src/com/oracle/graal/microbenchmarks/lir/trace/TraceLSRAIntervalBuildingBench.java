138341d68093e703b395e7b0d9265ccbc75dca42
Josef Eisl <josef.eisl@jku.at>
2016-06-28 13:30:57 +0200

    TraceRA: fix TraceLSRAIntervalBuildingBench

--- graal/com.oracle.graal.microbenchmarks/src/com/oracle/graal/microbenchmarks/lir/trace/TraceLSRAIntervalBuildingBench.java
+++ graal/com.oracle.graal.microbenchmarks/src/com/oracle/graal/microbenchmarks/lir/trace/TraceLSRAIntervalBuildingBench.java
@@ -67,7 +67,7 @@ public class TraceLSRAIntervalBuildingBench extends GraalBenchmark {
             TraceBuilderResult<B> resultTraces = getTraces(context);
 
             for (Trace<B> trace : resultTraces.getTraces()) {
-                intervalData = new IntervalData(target, lirGenRes, registerAllocationConfig, trace);
+                intervalData = new IntervalData(target, lirGenRes, registerAllocationConfig);
                 Analyser a = new TraceLinearScanLifetimeAnalysisPhase.Analyser(intervalData, resultTraces, trace.getBlocks(), lirGenRes.getLIR(), true, spillMoveFactory,
                                 registerAllocationConfig.getRegisterConfig().getCallerSaveRegisters());
                 a.analyze();
