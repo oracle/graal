3babb3786e7ad794d60854d4ce146ec5c0bcb9af
David Leopoldseder <david.d.leopoldseder@oracle.com>
2016-07-11 11:49:39 +0200

    node costs: document _UNSET, _UNKNOWN functionality

--- graal/com.oracle.graal.nodeinfo/src/com/oracle/graal/nodeinfo/NodeCycles.java
+++ graal/com.oracle.graal.nodeinfo/src/com/oracle/graal/nodeinfo/NodeCycles.java
@@ -22,10 +22,29 @@
  */
 package com.oracle.graal.nodeinfo;
 
+/**
+ * Constants representing an estimation of the number of CPU cycles needed to execute a certain
+ * compiler node.
+ */
 public enum NodeCycles {
 
+    /**
+     * The default value of the {@link NodeInfo#cycles()} property.
+     * <p>
+     * For further information about the use of {@code CYCLES_UNSET} see {@link NodeInfo#cycles()}.
+     */
     CYCLES_UNSET(0),
+    /**
+     * Nodes for which, due to arbitrary reason, no estimation can be made either (1) statically
+     * without inspecting the properties of a node or (2) at all (like e.g. for an invocation).
+     * <p>
+     * Nodes annotated with {@code CYCLES_UNKNOWN} should specify the
+     * {@link NodeInfo#cyclesRationale()} property to clarify why an estimation cannot be done.
+     */
     CYCLES_UNKNOWN(0),
+    /**
+     * Nodes that do not consume any CPU time during the "execution", e.g. Constants.
+     */
     CYCLES_0(0),
     CYCLES_1(1),
     CYCLES_2(2),
