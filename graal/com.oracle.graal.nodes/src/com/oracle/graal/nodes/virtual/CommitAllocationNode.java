d8a45a8dde0814d024281d8a99cacf2846eb1d74
Juan Fumero <juan.fumero@ed.ac.uk>
2016-07-14 07:25:34 +0100

    Javadoc for CommitAllocationNode#getVirtualObjectsArrays imrpoved

--- graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/virtual/CommitAllocationNode.java
+++ graal/com.oracle.graal.nodes/src/com/oracle/graal/nodes/virtual/CommitAllocationNode.java
@@ -220,9 +220,8 @@ public final class CommitAllocationNode extends FixedWithNextNode implements Vir
     /**
      * For all the virtual object nodes that depends on commit allocation, this method maps the node
      * with its values. For example, a commit allocation could depend on a {@link VirtualArrayNode}
-     * with many {@link ValueNode}. The hash table will contain the corresponding
-     * {@link VirtualArrayNode} as a key with the array of {@link ValueNode}. This array can be used
-     * to inspect those {@link ValueNode}.
+     * with many {@link ValueNode}s. The map will contain the corresponding {@link VirtualArrayNode}
+     * as a key with the array of {@link ValueNode}s.
      */
     public HashMap<VirtualObjectNode, Object[]> getVirtualObjectsArrays() {
         HashMap<VirtualObjectNode, Object[]> arrayValues = new HashMap<>();
