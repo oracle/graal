5089a017651e26771827f0e8a02388a6fde6d7bd
Christian Humer <christian.humer@oracle.com>
2016-07-18 14:51:12 +0200

    Make @NodeInfo inherited by subclassees.

--- truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/NodeInfo.java
+++ truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/NodeInfo.java
@@ -25,17 +25,19 @@
 package com.oracle.truffle.api.nodes;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Annotation for providing additional information on nodes.
- * 
+ *
  * @since 0.8 or earlier
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface NodeInfo {
 
     /**
