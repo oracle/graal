ad4ebbcb92e1e12a92cc7fd89bc9003911583ef4
Daniel Pekarek <pekarekdaniel@gmail.com>
2016-07-20 11:18:27 +0000

    Change invalid code test case to use invalid IR

--- projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/interop/LLVMTckTest.java
+++ projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/interop/LLVMTckTest.java
@@ -119,9 +119,14 @@ public class LLVMTckTest extends TruffleTCK {
     protected String invalidCode() {
         // @formatter:off
         return
+                        "define i32 @main() nounwind uwtable readnone {\n" +
+                        "  ret j32 0\n" +
+                        "}";
+        /* alternatively, this should also work and will need its own test case:
+        return
             "f unction main() {\n" +
             "  retu rn 42;\n" +
-            "}\n";
+            "}\n";*/
         // @formatter:on
     }
 
@@ -133,14 +138,17 @@ public class LLVMTckTest extends TruffleTCK {
     @Override
     protected String multiplyCode(String firstName, String secondName) {
         // @formatter:off
-        /*
-        return
-            "function multiply(" + firstName + ", " + secondName + ") {\n" +
-            "  return " + firstName + " * " + secondName + ";\n" +
-            "}\n";
+        return "; ModuleID = 'multiply.c'\n" +
+                        "target datalayout = \"e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128-n8:16:32:64-S128\"\n" +
+                        "target triple = \"x86_64-unknown-linux-gnu\"\n\n" +
+                        "define i32 @main(i32 %" + firstName + ", i32 %" + secondName + ") nounwind uwtable readnone {\n" +
+                        "  %1 = mul nsw i32 %" + secondName + ", %" + firstName + "\n" +
+                        "  ret i32 %1\n" +
+                        //"}\n" +
+                        //"define i32 @main() nounwind uwtable readnone {\n" +
+                        //"  ret i32 21\n" +
+                        "}";
         // @formatter:on
-         */
-        return null;
     }
 
     @Override
@@ -183,7 +191,7 @@ public class LLVMTckTest extends TruffleTCK {
         return "complexSumReal";
     }
 
-    // Disabled tests
+    // Disabled failing tests: no structs, no function passing and other unimplemented functionality
     @Override
     @Ignore
     @Test
@@ -261,6 +269,7 @@ public class LLVMTckTest extends TruffleTCK {
         super.testAddComplexNumbersWithMethod();
     }
 
+    // ... and some other strange behavior
     @Override
     @Ignore
     @Test
@@ -380,13 +389,7 @@ public class LLVMTckTest extends TruffleTCK {
         super.testPrimitiveReturnTypeDouble();
     }
 
-    @Override
-    @Ignore
-    @Test
-    public void testInvalidTestMethod() throws Exception {
-        super.testInvalidTestMethod();
-    }
-
+    // this is not yet implemented
     @Override
     @Ignore
     @Test
