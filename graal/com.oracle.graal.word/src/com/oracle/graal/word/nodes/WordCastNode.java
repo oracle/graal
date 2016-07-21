2b5038d750cc0ebd4ab2c14fb5c858d26e49a359
David Leopoldseder <david.d.leopoldseder@oracle.com>
2016-07-21 17:32:15 +0200

    node costs: fix word cast cycles to 1

--- graal/com.oracle.graal.word/src/com/oracle/graal/word/nodes/WordCastNode.java
+++ graal/com.oracle.graal.word/src/com/oracle/graal/word/nodes/WordCastNode.java
@@ -22,7 +22,7 @@
  */
 package com.oracle.graal.word.nodes;
 
-import static com.oracle.graal.nodeinfo.NodeCycles.CYCLES_2;
+import static com.oracle.graal.nodeinfo.NodeCycles.CYCLES_1;
 import static com.oracle.graal.nodeinfo.NodeSize.SIZE_1;
 
 import com.oracle.graal.compiler.common.LIRKind;
@@ -54,7 +54,7 @@ import jdk.vm.ci.meta.ValueKind;
  * operations. It has an impact on the pointer maps for the GC, so it must not be scheduled or
  * optimized away.
  */
-@NodeInfo(cycles = CYCLES_2, size = SIZE_1)
+@NodeInfo(cycles = CYCLES_1, size = SIZE_1)
 public final class WordCastNode extends FixedWithNextNode implements LIRLowerable, Canonicalizable {
 
     public static final NodeClass<WordCastNode> TYPE = NodeClass.create(WordCastNode.class);
