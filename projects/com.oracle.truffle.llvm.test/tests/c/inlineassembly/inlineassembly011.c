29c0cb760e45d0d0eee3dd503f106937c7427080
PrinzKatharina <kaykay.sunny@gmx.at>
2016-08-02 15:30:23 +0200

    Fix format asm tests

--- projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly011.c
+++ projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly011.c
@@ -3,6 +3,9 @@ int main() {
   int arg2 = 20;
   int arg3 = 21;
   int calc = 0;
-  __asm__ ( "addl %%ebx, %%eax;" "subl %%ecx, %%eax;" : "=a"(calc) : "a"(arg1), "b"(arg2), "c"(arg3));
+  __asm__("addl %%ebx, %%eax;"
+          "subl %%ecx, %%eax;"
+          : "=a"(calc)
+          : "a"(arg1), "b"(arg2), "c"(arg3));
   return calc;
 }
--- projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly018.c
+++ projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly018.c
@@ -2,6 +2,10 @@ int main() {
   int arg1 = 0;
   int arg2 = 0;
   int calc = 0;
-  __asm__ ( "movl $2, %%eax;" "movl $10, %%ebx;" "addl %%eax, %%ebx;" : "=a"(calc) : "a"(arg1), "b"(arg2));
+  __asm__("movl $2, %%eax;"
+          "movl $10, %%ebx;"
+          "addl %%eax, %%ebx;"
+          : "=a"(calc)
+          : "a"(arg1), "b"(arg2));
   return calc;
 }
