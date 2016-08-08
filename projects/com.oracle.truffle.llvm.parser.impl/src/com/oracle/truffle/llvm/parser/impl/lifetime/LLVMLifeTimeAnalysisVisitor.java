16c93518736ab4a01dd0431e6ab9c3c62fb57ea4
Manuel Rigger <manuel.rigger@jku.at>
2016-08-08 10:35:04 +0200

    Use an interface for the lifetime analysis result

--- projects/com.oracle.truffle.llvm.parser.impl/src/com/oracle/truffle/llvm/parser/impl/lifetime/LLVMLifeTimeAnalysisVisitor.java
+++ projects/com.oracle.truffle.llvm.parser.impl/src/com/oracle/truffle/llvm/parser/impl/lifetime/LLVMLifeTimeAnalysisVisitor.java
@@ -27,7 +27,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.oracle.truffle.llvm.parser.impl;
+package com.oracle.truffle.llvm.parser.impl.lifetime;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -59,7 +59,9 @@ import com.intel.llvm.ireditor.lLVM_IR.impl.Instruction_brImpl;
 import com.intel.llvm.ireditor.lLVM_IR.impl.LocalValueRefImpl;
 import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.FrameSlot;
+import com.oracle.truffle.llvm.parser.impl.LLVMPhiVisitor;
 import com.oracle.truffle.llvm.parser.impl.LLVMPhiVisitor.Phi;
+import com.oracle.truffle.llvm.parser.impl.LLVMReadVisitor;
 import com.oracle.truffle.llvm.runtime.options.LLVMBaseOptionFacade;
 
 /**
@@ -82,17 +84,19 @@ public final class LLVMLifeTimeAnalysisVisitor {
         phiRefs = LLVMPhiVisitor.visit(function);
     }
 
-    public static class LifeTimeAnalysisResult {
+    static class LifeTimeAnalysisResultImpl implements LLVMLifeTimeAnalysisResult {
 
-        public LifeTimeAnalysisResult(Map<BasicBlock, FrameSlot[]> beginDead, Map<BasicBlock, FrameSlot[]> endDead) {
+        public LifeTimeAnalysisResultImpl(Map<BasicBlock, FrameSlot[]> beginDead, Map<BasicBlock, FrameSlot[]> endDead) {
             this.beginDead = beginDead;
             this.endDead = endDead;
         }
 
+        @Override
         public Map<BasicBlock, FrameSlot[]> getBeginDead() {
             return beginDead;
         }
 
+        @Override
         public Map<BasicBlock, FrameSlot[]> getEndDead() {
             return endDead;
         }
@@ -102,8 +106,8 @@ public final class LLVMLifeTimeAnalysisVisitor {
 
     }
 
-    public static LifeTimeAnalysisResult visit(FunctionDef function, FrameDescriptor frameDescriptor) {
-        LifeTimeAnalysisResult mapping = new LLVMLifeTimeAnalysisVisitor(function, frameDescriptor).visit();
+    public static LLVMLifeTimeAnalysisResult visit(FunctionDef function, FrameDescriptor frameDescriptor) {
+        LifeTimeAnalysisResultImpl mapping = new LLVMLifeTimeAnalysisVisitor(function, frameDescriptor).visit();
         if (LLVMBaseOptionFacade.printLifeTimeAnalysis()) {
             printAnalysisResults(function, mapping.getEndDead());
         }
@@ -161,7 +165,7 @@ public final class LLVMLifeTimeAnalysisVisitor {
 
     }
 
-    private LifeTimeAnalysisResult visit() {
+    private LifeTimeAnalysisResultImpl visit() {
         initializeInstructionReads();
         initializeInstructionInOuts();
         initializeVariableDefinitions();
@@ -173,7 +177,7 @@ public final class LLVMLifeTimeAnalysisVisitor {
             Set<FrameSlot> bbBegin = bbBeginKills.get(block);
             beginKills.put(block, bbBegin.toArray(new FrameSlot[bbBegin.size()]));
         }
-        return new LifeTimeAnalysisResult(beginKills, endKills);
+        return new LifeTimeAnalysisResultImpl(beginKills, endKills);
     }
 
     private Map<Instruction, List<FrameSlot>> instructionReads = new HashMap<>();
