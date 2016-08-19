4c0e0868410ea7672b84f13cd8833b6ce08a45cd
Andreas Woess <andreas.woess@oracle.com>
2016-08-19 14:26:31 +0200

    Fix illegal package access in DOTestAsserts

--- truffle/com.oracle.truffle.object.basic.test/src/com/oracle/truffle/object/basic/test/DOTestAsserts.java
+++ truffle/com.oracle.truffle.object.basic.test/src/com/oracle/truffle/object/basic/test/DOTestAsserts.java
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.truffle.object.basic;
+package com.oracle.truffle.object.basic.test;
 
 import org.junit.Assert;
 
