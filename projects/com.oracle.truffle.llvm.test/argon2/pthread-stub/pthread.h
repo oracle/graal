6f888e23c0ef4c9db4293e423f104cd38a560d63
David Gnedt <david.gnedt@jku.at>
2016-08-23 10:52:15 +0200

    Fix Argon2 pthread stub for OSX

--- projects/com.oracle.truffle.llvm.test/argon2/pthread-stub/pthread.h
+++ projects/com.oracle.truffle.llvm.test/argon2/pthread-stub/pthread.h
@@ -1,8 +1,12 @@
 #ifndef _BITS_PTHREADTYPES_H
 #define _BITS_PTHREADTYPES_H
 
+#ifndef _PTHREAD_T
 typedef unsigned long pthread_t;
+#endif
+#ifndef _PTHREAD_ATTR_T
 typedef void pthread_attr_t;
+#endif
 
 #endif
 
