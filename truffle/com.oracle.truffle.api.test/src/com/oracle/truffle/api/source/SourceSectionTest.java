46369b794feed3b6a09aa2d499b30aa026e048a9
Christian Humer <christian.humer@oracle.com>
2016-09-06 19:03:33 +0200

    Fix import.

--- truffle/com.oracle.truffle.api.test/src/com/oracle/truffle/api/source/SourceSectionTest.java
+++ truffle/com.oracle.truffle.api.test/src/com/oracle/truffle/api/source/SourceSectionTest.java
@@ -27,7 +27,6 @@ import java.io.FileWriter;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
