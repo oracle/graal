5fdd03115152de7ed66a3d788981dedbfb53d02e
Jacob Kreindl <jacob.kreindl@gmail.com>
2016-09-15 11:03:08 +0200

    Support Void Truffle Intrinsics

--- projects/com.oracle.truffle.llvm.parser.bc.impl/src/com/oracle/truffle/llvm/parser/bc/impl/LLVMBitcodeInstructionVisitor.java
+++ projects/com.oracle.truffle.llvm.parser.bc.impl/src/com/oracle/truffle/llvm/parser/bc/impl/LLVMBitcodeInstructionVisitor.java
@@ -633,22 +633,25 @@ public final class LLVMBitcodeInstructionVisitor implements InstructionVisitor {
 
     @Override
     public void visit(VoidCallInstruction call) {
-        Symbol target = call.getCallTarget();
+        final Symbol target = call.getCallTarget();
+        final int argumentCount = call.getArgumentCount();
+        final LLVMExpressionNode[] args = new LLVMExpressionNode[argumentCount + 1];
 
-        int argumentCount = call.getArgumentCount();
-
-        LLVMExpressionNode[] args = new LLVMExpressionNode[argumentCount + 1];
-
-        args[0] = LLVMFrameReadWriteFactory.createFrameRead(LLVMBaseType.ADDRESS, method.getStackSlot());
-
+        int argIndex = 0;
+        args[argIndex++] = LLVMFrameReadWriteFactory.createFrameRead(LLVMBaseType.ADDRESS, method.getStackSlot());
         for (int i = 0; i < argumentCount; i++) {
-            args[i + 1] = symbols.resolve(call.getArgument(i));
+            args[argIndex++] = symbols.resolve(call.getArgument(i));
         }
 
-        LLVMNode node;
-
+        final LLVMNode node;
         if (target instanceof FunctionDeclaration && (((ValueSymbol) target).getName()).startsWith("@llvm.")) {
             node = LLVMIntrinsicFactory.create(((ValueSymbol) target).getName(), args, call.getCallType().getArgumentTypes().length, method.getStackSlot(), method.getOptimizationConfiguration());
+
+        } else if (target instanceof FunctionDeclaration && (((ValueSymbol) target).getName()).startsWith("@truffle_")) {
+            node = LLVMTruffleIntrinsicFactory.create(((ValueSymbol) target).getName(), args);
+            if (node == null) {
+                throw new UnsupportedOperationException("Unknown Truffle Intrinsic: " + ((ValueSymbol) target).getName());
+            }
         } else {
             LLVMFunctionNode function = (LLVMFunctionNode) symbols.resolve(target);
             node = LLVMFunctionFactory.createFunctionCall(function, args, typeHelper.getLLVMBaseType(call.getType()));
