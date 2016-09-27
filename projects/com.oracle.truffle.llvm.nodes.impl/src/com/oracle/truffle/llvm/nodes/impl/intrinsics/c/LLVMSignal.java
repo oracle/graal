8f8047f9e202887841268ff0c735abae3ce50fad
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-09-27 13:00:30 +0200

    Add some TruffleBoundary to be sure

--- projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/intrinsics/c/LLVMSignal.java
+++ projects/com.oracle.truffle.llvm.nodes.impl/src/com/oracle/truffle/llvm/nodes/impl/intrinsics/c/LLVMSignal.java
@@ -132,6 +132,7 @@ public abstract class LLVMSignal extends LLVMFunctionNode {
         Lock lock = new ReentrantLock();
         private AtomicBoolean isRunning = new AtomicBoolean(true);
 
+        @TruffleBoundary
         private LLVMSignalHandler(Signal signal, LLVMFunctionDescriptor function) throws IllegalArgumentException {
             this.signal = signal;
             this.function = function;
@@ -164,6 +165,7 @@ public abstract class LLVMSignal extends LLVMFunctionNode {
         }
 
         @Override
+        @TruffleBoundary
         protected void finalize() throws Throwable {
             super.finalize();
             stop();
