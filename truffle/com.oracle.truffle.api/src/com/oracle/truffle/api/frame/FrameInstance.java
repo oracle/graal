cade4e9fcca5eff108e7e605a4925851d437f5cb
Christian Humer <christian.humer@oracle.com>
2016-11-07 20:49:23 +0100

    Fix eclipseformat.

--- truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameInstance.java
+++ truffle/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameInstance.java
@@ -43,8 +43,7 @@ public interface FrameInstance {
          * @since 0.8 or earlier
          * @deprecated without replacement. This mode always returns <code>null</code>.
          **/
-        @Deprecated
-        NONE,
+        @Deprecated NONE,
 
         /**
          * This mode allows to read the frame and provides read only access to its local variables.
