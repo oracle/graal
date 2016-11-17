8eafea8dbdda054514f1579a95f6fbb21708665f
Manuel Rigger <manuel.rigger@jku.at>
2016-11-17 17:21:57 +0100

    Implement toString in LLVMType

--- projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LLVMType.java
+++ projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LLVMType.java
@@ -53,4 +53,13 @@ public class LLVMType {
     public LLVMType getPointee() {
         return pointee;
     }
+
+    @Override
+    public String toString() {
+        if (isPointer()) {
+            return String.format("&%s", pointee);
+        } else {
+            return type.toString();
+        }
+    }
 }
