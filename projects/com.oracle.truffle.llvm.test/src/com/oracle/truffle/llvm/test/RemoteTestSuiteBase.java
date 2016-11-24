dca10bcb59dc0224da5c1a35b1ce22833d4e077a
Matthias Grimmer <grimmer@ssw.jku.at>
2016-11-24 10:29:20 +0100

    Add missing line break at the end of the RemoteTestRunner stdout

--- projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/RemoteTestSuiteBase.java
+++ projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/RemoteTestSuiteBase.java
@@ -180,6 +180,7 @@ public class RemoteTestSuiteBase extends TestSuiteBase {
             List<String> launchRemote = launchRemote(tuple);
             int sulongRetValue = parseAndRemoveReturnValue(launchRemote);
             String sulongLines = launchRemote.stream().collect(Collectors.joining("\n"));
+            sulongLines = sulongLines.length() == 0 ? sulongLines : sulongLines + "\n";
             ProcessResult processResult = TestHelper.executeLLVMBinary(tuple.getBitCodeFile());
             String expectedLines = processResult.getStdOutput();
             int expectedReturnValue = processResult.getReturnValue();
