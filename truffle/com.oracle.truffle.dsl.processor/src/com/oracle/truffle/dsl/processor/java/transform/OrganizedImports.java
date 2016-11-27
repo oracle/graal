17c1f2f45338d94d1e3fc1c98f3f7b0fe0373e54
Christian Humer <christian.humer@oracle.com>
2016-11-27 19:09:33 +0100

    Fix ambiguous class simple names in generated code.

--- truffle/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/java/transform/OrganizedImports.java
+++ truffle/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/java/transform/OrganizedImports.java
@@ -133,7 +133,7 @@ public final class OrganizedImports {
             String qualifiedName = ElementUtils.getEnclosedQualifiedName(type);
 
             if (!qualifiedName.equals(qualifiedImport)) {
-                name = qualifiedName;
+                name = ElementUtils.getQualifiedName(type);
             }
         }
 
