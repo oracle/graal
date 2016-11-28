8a2894ff83e5092a26e8ec5fbc9ed16f710c5e87
Chris Seaton <chris.seaton@oracle.com>
2016-11-28 15:45:33 +0000

    Version of memcpy that works with managed objects

---
+++ mx.sulong/libs/memcpy.c
@@ -0,0 +1,6 @@
+void *truffle_managed_memcpy(void *destination, const void *source, size_t count) {
+  for (size_t n = 0; n < count; n++) {
+    ((char *)destination)[n] = ((char *)source)[n];
+  }
+  return destination;
+}
