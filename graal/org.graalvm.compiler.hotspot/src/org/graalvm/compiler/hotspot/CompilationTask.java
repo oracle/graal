dd11ad4cc1f1de2a5bb9abae86fc3aff9dfac7aa
Doug Simon <doug.simon@oracle.com>
2016-12-14 14:53:23 +0100

    disable Inline and InlineDuringParsing if they have default values and -XX:-Inline is specified

--- graal/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/CompilationTask.java
+++ graal/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/CompilationTask.java
@@ -32,7 +32,9 @@ import static org.graalvm.compiler.core.GraalCompilerOptions.PrintStackTraceOnEx
 import static org.graalvm.compiler.core.phases.HighTier.Options.Inline;
 import static org.graalvm.compiler.java.BytecodeParserOptions.InlineDuringParsing;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.graalvm.compiler.code.CompilationResult;
 import org.graalvm.compiler.debug.Debug;
@@ -45,6 +47,7 @@ import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.debug.Management;
 import org.graalvm.compiler.debug.TTY;
 import org.graalvm.compiler.debug.TimeSource;
+import org.graalvm.compiler.options.OptionKey;
 import org.graalvm.compiler.options.OptionValues;
 
 import jdk.vm.ci.code.BailoutException;
@@ -116,11 +119,20 @@ public class CompilationTask {
          */
         HotSpotGraalRuntimeProvider graalRuntime = compiler.getGraalRuntime();
         GraalHotSpotVMConfig config = graalRuntime.getVMConfig();
-        if (Inline.getValue(options) && !config.inline && !Inline.hasBeenSet(options)) {
-            this.options = new OptionValues(options, Inline, false, InlineDuringParsing, false);
-        } else {
-            this.options = options;
+        OptionValues newOptions = options;
+        if (!config.inline) {
+            Map<OptionKey<?>, Object> m = new HashMap<>();
+            if (Inline.getValue(options) && !Inline.hasBeenSet(options)) {
+                m.put(Inline, false);
+            }
+            if (InlineDuringParsing.getValue(options) && !InlineDuringParsing.hasBeenSet(options)) {
+                m.put(InlineDuringParsing, false);
+            }
+            if (!m.isEmpty()) {
+                newOptions = new OptionValues(options, m);
+            }
         }
+        this.options = newOptions;
     }
 
     public HotSpotResolvedJavaMethod getMethod() {
