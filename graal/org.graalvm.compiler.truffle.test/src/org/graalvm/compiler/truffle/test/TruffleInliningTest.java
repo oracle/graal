0fdb0ff41c202c8a6934f1cf656174db79e88c62
Boris Spasojevic <boris.spasojevic@oracle.com>
2017-02-03 15:02:09 +0100

    Cut dependency to GraalTest.

--- graal/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/TruffleInliningTest.java
+++ graal/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/TruffleInliningTest.java
@@ -22,8 +22,6 @@
  */
 package org.graalvm.compiler.truffle.test;
 
-import static org.graalvm.compiler.test.GraalTest.assertTrue;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,6 +37,7 @@ import org.graalvm.compiler.truffle.TruffleCompilerOptions;
 import org.graalvm.compiler.truffle.TruffleInlining;
 import org.graalvm.compiler.truffle.TruffleInliningDecision;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
@@ -216,11 +215,11 @@ public class TruffleInliningTest {
     protected TruffleInliningTestScenarioBuilder builder = new TruffleInliningTestScenarioBuilder();
 
     void assertInlined(TruffleInlining decisions, String name) {
-        assertTrue(countInlines(decisions, name) > 0, name + " was not inlined!");
+        Assert.assertTrue(name + " was not inlined!", countInlines(decisions, name) > 0);
     }
 
     void assertNotInlined(TruffleInlining decisions, String name) {
-        assertTrue(countInlines(decisions, name) == 0, name + " was inlined!");
+        Assert.assertTrue(name + " was inlined!", countInlines(decisions, name) == 0);
     }
 
     void traverseDecisions(List<TruffleInliningDecision> decisions, Consumer<TruffleInliningDecision> f) {
