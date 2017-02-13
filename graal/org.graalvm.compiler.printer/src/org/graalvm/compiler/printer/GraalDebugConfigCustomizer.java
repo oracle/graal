487759f848cb25889eb1482d4d3bd969390c0bb7
Doug Simon <doug.simon@oracle.com>
2017-02-13 20:58:18 +0100

    automatically fall back to dumping graphs to files when IGV is unavailable/unreachable

--- graal/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/GraalDebugConfigCustomizer.java
+++ graal/org.graalvm.compiler.printer/src/org/graalvm/compiler/printer/GraalDebugConfigCustomizer.java
@@ -127,7 +127,12 @@ public class GraalDebugConfigCustomizer implements DebugConfigCustomizer {
              */
             return null;
         } catch (IOException e) {
-            throw new IOException(String.format("Could not connect to the IGV on %s:%d", host, port), e);
+            if (!Options.PrintIdealGraphFile.hasBeenSet(options)) {
+                TTY.println(String.format("Could not connect to the IGV on %s:%d - falling back to file dumping...", host, port));
+                return createFilePrinter(options);
+            } else {
+                throw new IOException(String.format("Could not connect to the IGV on %s:%d", host, port), e);
+            }
         }
     }
 
