7fcb7ab158dc6132e339877f72b5264d331b39aa
Doug Simon <doug.simon@oracle.com>
2017-02-20 17:06:23 +0100

    removed CloneNodesWithUnsafe option

--- graal/org.graalvm.compiler.graph/src/org/graalvm/compiler/graph/Graph.java
+++ graal/org.graalvm.compiler.graph/src/org/graalvm/compiler/graph/Graph.java
@@ -55,8 +55,6 @@ public class Graph {
         public static final OptionKey<Boolean> VerifyGraalGraphEdges = new OptionKey<>(false);
         @Option(help = "Graal graph compression is performed when percent of live nodes falls below this value", type = OptionType.Debug)//
         public static final OptionKey<Integer> GraphCompressionThreshold = new OptionKey<>(70);
-        @Option(help = "Use Unsafe to clone graph nodes thus avoiding copying fields that will be re-initialized anyway", type = OptionType.Debug)//
-        public static final OptionKey<Boolean> CloneNodesWithUnsafe = new OptionKey<>(true);
     }
 
     public final String name;
