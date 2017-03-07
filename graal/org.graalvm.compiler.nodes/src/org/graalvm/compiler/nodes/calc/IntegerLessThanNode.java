e196bac444f1e95c018d0909af12d9a049b4be24
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2017-03-07 07:57:42 -0800

    Canonicalize `a + MIN < b + MIN` to `a |<| b`

--- graal/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/IntegerLessThanNode.java
+++ graal/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/calc/IntegerLessThanNode.java
@@ -33,12 +33,14 @@ import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.graph.NodeClass;
 import org.graalvm.compiler.graph.spi.CanonicalizerTool;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
+import org.graalvm.compiler.nodes.ConstantNode;
 import org.graalvm.compiler.nodes.LogicNode;
 import org.graalvm.compiler.nodes.ValueNode;
 
 import jdk.vm.ci.code.CodeUtil;
 import jdk.vm.ci.meta.Constant;
 import jdk.vm.ci.meta.ConstantReflectionProvider;
+import jdk.vm.ci.meta.JavaConstant;
 import jdk.vm.ci.meta.JavaKind;
 import jdk.vm.ci.meta.PrimitiveConstant;
 
@@ -109,6 +111,53 @@ public final class IntegerLessThanNode extends IntegerLowerThanNode {
 
             if (!subtractMayUnderflow(xStamp.lowerBound(), yStamp.upperBound(), minValue) && !subtractMayOverflow(xStamp.upperBound(), yStamp.lowerBound(), maxValue)) {
                 return new IntegerLessThanNode(sub.getX(), sub.getY());
+            }
+        }
+
+        int bits = ((IntegerStamp) getX().stamp()).getBits();
+        assert ((IntegerStamp) getY().stamp()).getBits() == bits;
+        long min = OP.minValue(bits);
+        long xResidue = 0;
+        ValueNode left = null;
+        JavaConstant leftCst = null;
+        if (forX instanceof AddNode) {
+            AddNode xAdd = (AddNode) forX;
+            if (xAdd.getY().isJavaConstant()) {
+                long xCst = xAdd.getY().asJavaConstant().asLong();
+                xResidue = xCst - min;
+                left = xAdd.getX();
+            }
+        } else if (forX.isJavaConstant()) {
+            leftCst = forX.asJavaConstant();
+        }
+        if (left != null || leftCst != null) {
+            long yResidue = 0;
+            ValueNode right = null;
+            JavaConstant rightCst = null;
+            if (forY instanceof AddNode) {
+                AddNode yAdd = (AddNode) forY;
+                if (yAdd.getY().isJavaConstant()) {
+                    long yCst = yAdd.getY().asJavaConstant().asLong();
+                    yResidue = yCst - min;
+                    right = yAdd.getX();
+                }
+            } else if (forY.isJavaConstant()) {
+                rightCst = forY.asJavaConstant();
+            }
+            if (right != null || rightCst != null) {
+                if ((xResidue == 0 && left != null) || (yResidue == 0 && right != null)) {
+                    if (left == null) {
+                        left = ConstantNode.forIntegerBits(bits, leftCst.asLong() - min);
+                    } else if (xResidue != 0) {
+                        left = AddNode.create(left, ConstantNode.forIntegerBits(bits, xResidue));
+                    }
+                    if (right == null) {
+                        right = ConstantNode.forIntegerBits(bits, rightCst.asLong() - min);
+                    } else if (yResidue != 0) {
+                        right = AddNode.create(right, ConstantNode.forIntegerBits(bits, yResidue));
+                    }
+                    return new IntegerBelowNode(left, right);
+                }
             }
         }
         return this;
