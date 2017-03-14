92abdc9d0a82db0b2679d92ac834e8cd70b8d9ea
Doug Simon <doug.simon@oracle.com>
2017-03-15 00:20:06 +0100

    allow option descriptor existence check to be overridden

--- graal/org.graalvm.compiler.options/src/org/graalvm/compiler/options/OptionKey.java
+++ graal/org.graalvm.compiler.options/src/org/graalvm/compiler/options/OptionKey.java
@@ -55,7 +55,10 @@ public class OptionKey<T> {
         return descriptor;
     }
 
-    private boolean checkDescriptorExists() {
+    /**
+     * Checks that a descriptor exists for this key after triggering loading of descriptors.
+     */
+    protected boolean checkDescriptorExists() {
         OptionKey.Lazy.init();
         if (descriptor == null) {
             Formatter buf = new Formatter();
@@ -64,7 +67,7 @@ public class OptionKey<T> {
                             "org.graalvm.compiler.options.processor.OptionProcessor annotation processor.", Option.class.getName());
             StackTraceElement[] stackTrace = new Exception().getStackTrace();
             if (stackTrace.length > 2 &&
-                            stackTrace[1].getClassName().equals(getClass().getName()) &&
+                            stackTrace[1].getClassName().equals(OptionKey.class.getName()) &&
                             stackTrace[1].getMethodName().equals("getValue")) {
                 String caller = stackTrace[2].getClassName();
                 buf.format(" In suite.py, add GRAAL_OPTIONS_PROCESSOR to the \"annotationProcessors\" attribute of the project " +
