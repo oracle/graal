5dfcdf0ae1902818cb681da9f2b81b5fedb5e50f
Thomas Würthinger <thomas.wuerthinger@oracle.com>
2017-03-17 16:48:32 +0100

    Add additional canonicalizations to the multiplication node for the case where one operand is constant.

--- graal/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/ConditionalEliminationMulTest.java
+++ graal/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/ConditionalEliminationMulTest.java
@@ -22,13 +22,12 @@
  */
 package org.graalvm.compiler.core.test;
 
-import org.junit.Test;
-
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.StructuredGraph.AllowAssumptions;
 import org.graalvm.compiler.phases.common.CanonicalizerPhase;
 import org.graalvm.compiler.phases.common.DominatorConditionalEliminationPhase;
 import org.graalvm.compiler.phases.tiers.HighTierContext;
+import org.junit.Test;
 
 public class ConditionalEliminationMulTest extends GraalCompilerTest {
 
@@ -76,11 +75,9 @@ public class ConditionalEliminationMulTest extends GraalCompilerTest {
     private StructuredGraph prepareGraph(String snippet) {
         StructuredGraph graph = parseEager(snippet, AllowAssumptions.NO);
         HighTierContext context = getDefaultHighTierContext();
-        CanonicalizerPhase c = new CanonicalizerPhase();
-        c.apply(graph, context);
         DominatorConditionalEliminationPhase.create(false).apply(graph, context);
+        CanonicalizerPhase c = new CanonicalizerPhase();
         c.apply(graph, context);
         return graph;
     }
-
 }
