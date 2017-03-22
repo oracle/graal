1e41064941ae883f9d287f64862481fe3c436302
Tom Rodriguez <tom.rodriguez@oracle.com>
2017-03-22 10:11:48 -0700

    Fix bug with virtualization of arraycopy for primitive types

--- graal/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/replacements/arraycopy/ArrayCopyNode.java
+++ graal/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/replacements/arraycopy/ArrayCopyNode.java
@@ -22,8 +22,6 @@
  */
 package org.graalvm.compiler.hotspot.replacements.arraycopy;
 
-import jdk.vm.ci.meta.JavaKind;
-
 import static org.graalvm.compiler.core.common.LocationIdentity.any;
 
 import org.graalvm.compiler.core.common.LocationIdentity;
@@ -33,11 +31,12 @@ import org.graalvm.compiler.nodes.NamedLocationIdentity;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.spi.Lowerable;
 import org.graalvm.compiler.nodes.spi.LoweringTool;
-import org.graalvm.compiler.nodes.spi.Virtualizable;
 import org.graalvm.compiler.replacements.nodes.BasicArrayCopyNode;
 
+import jdk.vm.ci.meta.JavaKind;
+
 @NodeInfo
-public final class ArrayCopyNode extends BasicArrayCopyNode implements Virtualizable, Lowerable {
+public final class ArrayCopyNode extends BasicArrayCopyNode implements Lowerable {
 
     public static final NodeClass<ArrayCopyNode> TYPE = NodeClass.create(ArrayCopyNode.class);
 
