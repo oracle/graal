536ae387ce9092f0b9be85c7ea2a874c3f49c27b
Tom Rodriguez <tom.rodriguez@oracle.com>
2017-04-01 20:41:07 -0700

    InputFilter during CE should have an early bailout

--- graal/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/DominatorConditionalEliminationPhase.java
+++ graal/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/DominatorConditionalEliminationPhase.java
@@ -908,6 +908,10 @@ public class DominatorConditionalEliminationPhase extends BasePhase<PhaseContext
 
         @Override
         public Node apply(Node node, Node curNode) {
+            if (!ok) {
+                // Abort the recursion
+                return curNode;
+            }
             if (!(curNode instanceof ValueNode)) {
                 ok = false;
                 return curNode;
