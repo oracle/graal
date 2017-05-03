d0d2222aaf394601dfead54e7d66402a5bfffcd0
Christian Haeubl <christian.haeubl@oracle.com>
2017-05-03 17:32:44 +0200

    Make sure that the memset loops can be unrolled if the size is a constant value.

--- projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/intrinsics/llvm/LLVMMemSet.java
+++ projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/intrinsics/llvm/LLVMMemSet.java
@@ -87,19 +87,20 @@ public abstract class LLVMMemSet extends LLVMBuiltin {
         if (inJava) {
             if (length <= MAX_JAVA_LEN) {
                 long current = address.getVal();
-                long end = current + length;
-                if (end - current >= 8) {
+                long i64ValuesToWrite = length >> 3;
+                if (i64ValuesToWrite > 0) {
                     long v16 = ((long) value) << 8 | ((long) value & 0xFF);
                     long v32 = v16 << 16 | v16;
                     long v64 = v32 << 32 | v32;
 
-                    do {
+                    for (int i = 0; i < i64ValuesToWrite; i++) {
                         LLVMMemory.putI64(LLVMAddress.fromLong(current), v64);
                         current += 8;
-                    } while (end - current >= 8);
+                    }
                 }
 
-                while (current < end) {
+                long i8ValuesToWrite = length & 0x07;
+                for (int i = 0; i < i8ValuesToWrite; i++) {
                     LLVMMemory.putI8(LLVMAddress.fromLong(current), value);
                     current++;
                 }
