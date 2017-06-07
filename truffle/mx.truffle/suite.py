ca06f5cb03627b3602125b55db9b256aa4f5439f
Roland Schatz <roland.schatz@oracle.com>
2017-06-07 12:11:02 +0200

    Allow license 'BSD-new' in lafo-snapshots repository.

--- truffle/mx.truffle/suite.py
+++ truffle/mx.truffle/suite.py
@@ -17,7 +17,7 @@ suite = {
   "repositories" : {
     "lafo-snapshots" : {
       "url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots",
-      "licenses" : ["GPLv2-CPE", "UPL"]
+      "licenses" : ["GPLv2-CPE", "UPL", "BSD-new"]
     },
   },
   "defaultLicense" : "GPLv2-CPE",
