3d8e8cda8f509e260662e3df92a14d26d3a54ac8
Christian Humer <christian.humer@oracle.com>
2017-07-06 15:05:32 +0200

    Update language javadoc.

--- sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/Language.java
+++ sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/Language.java
@@ -28,6 +28,15 @@ import org.graalvm.options.OptionDescriptor;
 import org.graalvm.options.OptionDescriptors;
 import org.graalvm.polyglot.impl.AbstractPolyglotImpl.AbstractLanguageImpl;
 
+/**
+ * A handle for a Truffle language installed in a {@link Engine engine}. The handle provides access
+ * to the language's metadata, including the language's {@linkplain #getId() id},
+ * {@linkplain #getName() name} and {@linkplain #getVersion() version}
+ *
+ * @see Engine#getLanguage(String) To return a single language
+ * @see Engine#getLanguages() To return all installed languages
+ * @since 1.0
+ */
 public final class Language {
 
     final AbstractLanguageImpl impl;
@@ -37,9 +46,10 @@ public final class Language {
     }
 
     /**
-     * Returns the primary identification string of this language. The language id is used as the
+     * Gets the primary identification string of this language. The language id is used as the
      * primary way of identifying languages in the polyglot API.
      *
+     * @returns a language id string
      * @since 1.0
      */
     public String getId() {
@@ -47,8 +57,9 @@ public final class Language {
     }
 
     /**
-     * Returns a human readable name of the language.
+     * Gets a human readable name of the language.
      *
+     * @returns this language name
      * @since 1.0
      */
     public String getName() {
@@ -65,7 +76,7 @@ public final class Language {
     }
 
     /**
-     * Returns version information of the language in an arbitrary language specific format.
+     * Gets the version information of the language in an arbitrary language specific format.
      *
      * @since 1.0
      */
@@ -75,7 +86,7 @@ public final class Language {
 
     /**
      * Returns <code>true</code> if a the language is suitable for interactive evaluation of
-     * {@link Source sources}. {@link #interactive() Interactive} languages should be displayed in
+     * {@link Source sources}. {@link #setInteractive() Interactive} languages should be displayed in
      * interactive environments and presented to the user.
      *
      * @since 1.0
@@ -128,10 +139,10 @@ public final class Language {
     }
 
     /**
-     * Returns the set of options provided by this language. The option descriptor
-     * {@link OptionDescriptor#getName() name} can be used as option when building a new
-     * {@link Engine.Builder#setOption(String, String) engine} or a new
-     * {@link Context.Builder#setOption(String, String) context}.
+     * Returns the set of options provided by this language. Option values for languages can either
+     * be provided wile building an {@link Engine.Builder#setOption(String, String) engine} or a
+     * {@link Context.Builder#setOption(String, String) context}. The option descriptor
+     * {@link OptionDescriptor#getName() name} must be used as option name.
      *
      * @since 1.0
      */
