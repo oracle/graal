79c6d9bb50c42fd44eba9e619295b4a1bd6882df
Josef Eisl <josef.eisl@jku.at>
2017-07-07 17:13:23 +0200

    [fix] TraceRA[LSRA]: do not allocate registers for unused intervals

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/alloc/trace/lsra/TraceInterval.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/alloc/trace/lsra/TraceInterval.java
@@ -365,10 +365,6 @@ final class TraceInterval extends IntervalHint {
         return intTo;
     }
 
-    int numUsePositions() {
-        return numUsePos();
-    }
-
     public void setLocationHint(IntervalHint interval) {
         locationHint = interval;
     }
@@ -450,6 +446,10 @@ final class TraceInterval extends IntervalHint {
     public boolean inMemoryAt(int opId) {
         SpillState spillSt = spillState();
         return spillSt == SpillState.StartInMemory || (spillSt == SpillState.SpillStore && opId > spillDefinitionPos() && !canMaterialize());
+    }
+
+    public boolean preSpilledAllocated() {
+        return spillState() == SpillState.StartInMemory && numUsePos() == 0 && !hasHint();
     }
 
     // test intersection
