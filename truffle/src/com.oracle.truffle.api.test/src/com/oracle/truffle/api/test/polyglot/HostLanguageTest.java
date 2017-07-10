d22c1c34c7517868110d6ae1fb5edb1fc2de9646
Christian Humer <christian.humer@oracle.com>
2017-07-10 22:19:18 +0200

    Fix HostLauncherTest.

--- truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/HostLanguageTest.java
+++ truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/polyglot/HostLanguageTest.java
@@ -39,7 +39,7 @@ public class HostLanguageTest {
 
     @Test
     public void testHostLanguage() {
-        Context context = Context.newBuilder("java").setOption("java.AllowClassLoading", "true").build();
+        Context context = Context.newBuilder("java").option("java.AllowClassLoading", "true").build();
         Language language = context.getEngine().getLanguage("java");
 
         assertTrue(language.isHost());
