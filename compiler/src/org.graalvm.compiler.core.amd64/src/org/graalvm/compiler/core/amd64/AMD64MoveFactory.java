3cdf145367b39a0233b3695541687e3676c2ccf0
Tom Rodriguez <tom.rodriguez@oracle.com>
2017-07-12 11:08:53 -0700

    Object constants must be materialized in registers

--- compiler/src/org.graalvm.compiler.core.amd64/src/org/graalvm/compiler/core/amd64/AMD64MoveFactory.java
+++ compiler/src/org.graalvm.compiler.core.amd64/src/org/graalvm/compiler/core/amd64/AMD64MoveFactory.java
@@ -28,6 +28,7 @@ import static org.graalvm.compiler.lir.LIRValueUtil.isConstantValue;
 import static org.graalvm.compiler.lir.LIRValueUtil.isStackSlotValue;
 import static jdk.vm.ci.code.ValueUtil.isRegister;
 
+import jdk.vm.ci.meta.JavaKind;
 import org.graalvm.compiler.core.common.NumUtil;
 import org.graalvm.compiler.core.common.type.DataPointerConstant;
 import org.graalvm.compiler.debug.GraalError;
@@ -72,7 +73,13 @@ public abstract class AMD64MoveFactory extends AMD64MoveFactoryBase {
 
     @Override
     public boolean allowConstantToStackMove(Constant constant) {
-        return !(constant instanceof DataPointerConstant);
+        if (constant instanceof DataPointerConstant) {
+            return false;
+        }
+        if (constant instanceof JavaConstant && ((JavaConstant) constant).getJavaKind().getStackKind() == JavaKind.Object && !constant.isDefaultForKind()) {
+            return false;
+        }
+        return true;
     }
 
     @Override
