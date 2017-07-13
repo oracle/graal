f2386e8270a90e19850be3b9a80ff6acdb642d1b
Christian Humer <christian.humer@oracle.com>
2017-07-13 14:30:23 +0200

    Revert rename of Context#importSymbol/exportSymbol.

--- truffle/src/com.oracle.truffle.tck/src/com/oracle/truffle/tck/TruffleTestInvoker.java
+++ truffle/src/com.oracle.truffle.tck/src/com/oracle/truffle/tck/TruffleTestInvoker.java
@@ -115,7 +115,7 @@ final class TruffleTestInvoker<T extends CallTarget> extends TVMCI.TestAccessor<
             Context prevContext = rule.context;
             try (Context context = Context.create()) {
                 rule.context = context;
-                context.put("currentTestStatement", this);
+                context.exportSymbol("currentTestStatement", this);
                 context.eval("truffletestinvoker", "");
                 if (throwable != null) {
                     throw throwable;
