9c8af79a71ed0f177fd110fdf0e136386df94051
Codrut Stancu <codrut.stancu@oracle.com>
2017-08-31 17:51:00 -0700

    Add source position for unresolved.

--- compiler/src/org.graalvm.compiler.java/src/org/graalvm/compiler/java/BytecodeParser.java
+++ compiler/src/org.graalvm.compiler.java/src/org/graalvm/compiler/java/BytecodeParser.java
@@ -2547,9 +2547,7 @@ public class BytecodeParser implements GraphBuilderContext {
     private FixedNode createTarget(double probability, BciBlock block, FrameStateBuilder stateAfter) {
         assert probability >= 0 && probability <= 1.01 : probability;
         if (isNeverExecutedCode(probability)) {
-            DeoptimizeNode deopt = graph.add(new DeoptimizeNode(InvalidateReprofile, UnreachedCode));
-            deopt.updateNodeSourcePosition(() -> createBytecodePosition());
-            return deopt;
+            return graph.add(new DeoptimizeNode(InvalidateReprofile, UnreachedCode));
         } else {
             assert block != null;
             return createTarget(block, stateAfter);
@@ -2765,8 +2763,7 @@ public class BytecodeParser implements GraphBuilderContext {
                     if (skippedType.isAssignableFrom(checkedCatchType.getType())) {
                         BciBlock nextBlock = block.getSuccessorCount() == 1 ? blockMap.getUnwindBlock() : block.getSuccessor(1);
                         ValueNode exception = frameState.stack[0];
-                        DeoptimizeNode trueSuccessor = graph.add(new DeoptimizeNode(InvalidateReprofile, UnreachedCode));
-                        trueSuccessor.updateNodeSourcePosition(() -> createBytecodePosition());
+                        FixedNode trueSuccessor = graph.add(new DeoptimizeNode(InvalidateReprofile, UnreachedCode));
                         FixedNode nextDispatch = createTarget(nextBlock, frameState);
                         append(new IfNode(graph.addOrUniqueWithInputs(createInstanceOf(checkedCatchType, exception)), trueSuccessor, nextDispatch, 0));
                         return;
