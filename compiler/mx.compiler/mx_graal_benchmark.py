53e17cd92ca0dd40da0529e0052176040fe62011
Doug Simon <doug.simon@oracle.com>
2017-09-07 15:27:15 +0200

    remove useless check for batik benchmark

--- compiler/mx.compiler/mx_graal_benchmark.py
+++ compiler/mx.compiler/mx_graal_benchmark.py
@@ -910,7 +910,7 @@ class SpecJvm2008BenchmarkSuite(mx_benchmark.JavaBenchmarkSuite):
     def createCommandLineArgs(self, benchmarks, bmSuiteArgs):
         if benchmarks is None:
             # No benchmark specified in the command line, so run everything.
-            benchmarks = [b for b in self.benchmarkList(bmSuiteArgs) if b != 'batik']
+            benchmarks = [b for b in self.benchmarkList(bmSuiteArgs)]
 
         vmArgs = self.vmArgs(bmSuiteArgs)
         runArgs = self.runArgs(bmSuiteArgs)
