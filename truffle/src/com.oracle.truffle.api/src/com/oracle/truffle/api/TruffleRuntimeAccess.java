bcaa2c1ce1c7c5c3c9be91d5fe914f27e9bc1395
Paul Wögerer <paul.woegerer@oracle.com>
2017-09-20 17:18:45 +0200

    Add missing @since tag

--- truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/TruffleRuntimeAccess.java
+++ truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/TruffleRuntimeAccess.java
@@ -40,6 +40,8 @@ public interface TruffleRuntimeAccess {
 
     /**
      * Allows disambiguation if more than one {@link TruffleRuntime} implementation is available.
+     *
+     * @since 0.28
      */
     int getPriority();
 }
