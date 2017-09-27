e0aad69a8776710dfc9961f98ffc93663c3bf72a
Boris Spasojevic <boris.spasojevic@oracle.com>
2017-09-27 15:06:55 +0200

    Added tests for CPUTracer.

---
+++ truffle/src/com.oracle.truffle.tools.profiler.test/src/com/oracle/truffle/tools/profiler/test/CPUTracerTest.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.tools.profiler.test;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.oracle.truffle.api.source.Source;
+import com.oracle.truffle.api.vm.PolyglotRuntime;
+import com.oracle.truffle.tools.profiler.CPUTracer;
+
+public class CPUTracerTest extends AbstractProfilerTest {
+
+    private CPUTracer tracer;
+
+    @Before
+    public void setupTracer() {
+        for (PolyglotRuntime.Instrument instrument : engine.getRuntime().getInstruments().values()) {
+            tracer = instrument.lookup(CPUTracer.class);
+            if (tracer != null) {
+                break;
+            }
+        }
+        Assert.assertNotNull(tracer);
+    }
+
+    @Test
+    public void testCollecting() {
+
+        Assert.assertFalse(tracer.isCollecting());
+
+        tracer.setCollecting(true);
+
+        Assert.assertEquals(0, tracer.getCounters().size());
+        Assert.assertTrue(tracer.isCollecting());
+
+        execute(defaultSource);
+
+        Assert.assertNotEquals(0, tracer.getCounters().size());
+        Assert.assertTrue(tracer.isCollecting());
+
+        tracer.setCollecting(false);
+
+        Assert.assertFalse(tracer.isCollecting());
+
+        tracer.clearData();
+
+        Assert.assertEquals(0, tracer.getCounters().size());
+    }
+
+    @Test
+    public void testCorrectRootCount() {
+        final Map<String, Long> expectedCountMap = new HashMap<>();
+        expectedCountMap.put("baz", 1L);
+        expectedCountMap.put("bar", 11L);
+        expectedCountMap.put("foo", 110L);
+        expectedCountMap.put("", 1L);
+
+        synchronized (tracer) {
+            tracer.setFilter(NO_INTERNAL_ROOT_TAG_FILTER);
+        }
+        executeAndCheckRootNameCounters(defaultSource, expectedCountMap);
+    }
+
+    @Test
+    public void testCorrectCallCount() {
+        final Map<String, Long> expectedCountMap = new HashMap<>();
+        expectedCountMap.put("baz", 10L);
+        expectedCountMap.put("bar", 110L);
+        expectedCountMap.put("", 1L);
+        // every call in the root is its own source section so this is just a placeholder
+        expectedCountMap.put(":", 1L);
+
+        synchronized (tracer) {
+            tracer.setFilter(NO_INTERNAL_CALL_TAG_FILTER);
+        }
+
+        executeAndCheckRootNameCounters(defaultSource, expectedCountMap);
+    }
+
+    @Test
+    public void testCorrectStatementCount() {
+        final Map<String, Long> expectedCountMap = new HashMap<>();
+
+        Map<String, Long> foo = new HashMap<>();
+        foo.put("foo", 110L);
+        expectedCountMap.put("baz", 1L);
+        expectedCountMap.put("bar", 11L);
+        expectedCountMap.put("foo", 110L);
+
+        synchronized (tracer) {
+            tracer.setFilter(NO_INTERNAL_STATEMENT_TAG_FILTER);
+        }
+        executeAndCheckStatementCounters(defaultSource, expectedCountMap);
+    }
+
+    @Test
+    public void testCorrectRootCountRecursive() {
+        final Map<String, Long> expectedCountMap = new HashMap<>();
+        expectedCountMap.put("foo", 110L);
+        expectedCountMap.put("bar", 1L);
+        expectedCountMap.put("", 1L);
+
+        synchronized (tracer) {
+            tracer.setFilter(NO_INTERNAL_ROOT_TAG_FILTER);
+        }
+        executeAndCheckRootNameCounters(defaultRecursiveSource, expectedCountMap);
+    }
+
+    @Test
+    public void testCorrectCallCountRecursive() {
+        final Map<String, Long> expectedCountMap = new HashMap<>();
+        expectedCountMap.put("foo", 110L);
+        expectedCountMap.put("bar", 10L);
+        expectedCountMap.put("", 1L);
+
+        synchronized (tracer) {
+            tracer.setFilter(NO_INTERNAL_CALL_TAG_FILTER);
+        }
+        executeAndCheckRootNameCounters(defaultRecursiveSource, expectedCountMap);
+    }
+
+    @Test
+    public void testCorrectStatementCountRecursive() {
+        final Map<String, Long> expectedCountMap = new HashMap<>();
+
+        expectedCountMap.put("foo", 110L);
+        expectedCountMap.put("bar", 1L);
+
+        synchronized (tracer) {
+            tracer.setFilter(NO_INTERNAL_STATEMENT_TAG_FILTER);
+        }
+        executeAndCheckStatementCounters(defaultRecursiveSource, expectedCountMap);
+    }
+
+    // Works only assuming unique root names in counters
+    // This is, for example, not true for statement tracing
+    private void executeAndCheckRootNameCounters(Source recursiveSource, Map<String, Long> expectedCountMap) {
+        final int longExecutionCount = 1000;
+
+        tracer.setCollecting(true);
+        execute(recursiveSource);
+        Collection<CPUTracer.Counter> counters = tracer.getCounters();
+        Assert.assertEquals("Total number of counters does not match after one elxecution", expectedCountMap.size(), counters.size());
+
+        for (CPUTracer.Counter counter : counters) {
+            final long expectedCount = expectedCountMap.get(counter.getRootName());
+            final long count = counter.getCount();
+            Assert.assertEquals(counter.getRootName() + " count not correct", expectedCount, count);
+        }
+
+        for (int i = 1; i < longExecutionCount; i++) {
+            execute(recursiveSource);
+        }
+
+        counters = tracer.getCounters();
+        Assert.assertEquals("Total number of counters does not match after one execution", expectedCountMap.size(), counters.size());
+
+        for (CPUTracer.Counter counter : counters) {
+            final long expectedCount = longExecutionCount * expectedCountMap.get(counter.getRootName());
+            final long count = counter.getCount();
+            Assert.assertEquals(counter.getRootName() + " count not correct", expectedCount, count);
+        }
+    }
+
+    private void executeAndCheckStatementCounters(Source source, Map<String, Long> expectedCountMap) {
+        tracer.setCollecting(true);
+        execute(source);
+        Collection<CPUTracer.Counter> counters = tracer.getCounters();
+
+        Assert.assertEquals("Total number of counters does not match", expectedCountMap.size(), counters.size());
+
+        for (CPUTracer.Counter counter : counters) {
+            Long expected = expectedCountMap.get(counter.getRootName());
+            Assert.assertEquals(expected.longValue(), counter.getCount());
+        }
+
+    }
+}
