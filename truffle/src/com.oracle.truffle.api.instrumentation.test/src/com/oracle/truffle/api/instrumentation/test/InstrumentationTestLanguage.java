c9ab279db3b377039f7af32fda52640ce31308c1
Boris Spasojevic <boris.spasojevic@oracle.com>
2017-09-27 19:06:10 +0200

    Removing unnecessary modifier.

--- truffle/src/com.oracle.truffle.api.instrumentation.test/src/com/oracle/truffle/api/instrumentation/test/InstrumentationTestLanguage.java
+++ truffle/src/com.oracle.truffle.api.instrumentation.test/src/com/oracle/truffle/api/instrumentation/test/InstrumentationTestLanguage.java
@@ -603,7 +603,7 @@ public class InstrumentationTestLanguage extends TruffleLanguage<Context>
 
     private static class AllocationNode extends InstrumentedNode {
 
-        public AllocationNode(BaseNode[] children) {
+        AllocationNode(BaseNode[] children) {
             super(children);
         }
 
@@ -620,7 +620,7 @@ public class InstrumentationTestLanguage extends TruffleLanguage<Context>
         public static int state = 0;
         private int wasteCount = 1000;
 
-        public WasteTime(BaseNode[] children) {
+        WasteTime(BaseNode[] children) {
             super(children);
         }
 
