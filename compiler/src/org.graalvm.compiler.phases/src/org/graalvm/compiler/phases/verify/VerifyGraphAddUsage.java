1217a0c7cb75b63412aa35bc0fc04a0c92b9b41c
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2017-09-28 21:27:22 +0200

    Fix usages of addOrUnique where argument is not explicitly created.

--- compiler/src/org.graalvm.compiler.phases/src/org/graalvm/compiler/phases/verify/VerifyGraphAddUsage.java
+++ compiler/src/org.graalvm.compiler.phases/src/org/graalvm/compiler/phases/verify/VerifyGraphAddUsage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,66 +22,121 @@
  */
 package org.graalvm.compiler.phases.verify;
 
-import org.graalvm.compiler.core.common.PermanentBailoutException;
-import org.graalvm.compiler.core.common.RetryableBailoutException;
-import org.graalvm.compiler.debug.GraalError;
-import org.graalvm.compiler.nodes.StructuredGraph;
-import org.graalvm.compiler.nodes.java.MethodCallTargetNode;
-import org.graalvm.compiler.phases.VerifyPhase;
-import org.graalvm.compiler.phases.tiers.PhaseContext;
-
-import jdk.vm.ci.code.BailoutException;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
 import jdk.vm.ci.meta.ResolvedJavaType;
+import org.graalvm.compiler.debug.GraalError;
+import org.graalvm.compiler.graph.Graph;
+import org.graalvm.compiler.graph.Node;
+import org.graalvm.compiler.nodes.ConstantNode;
+import org.graalvm.compiler.nodes.Invoke;
+import org.graalvm.compiler.nodes.ParameterNode;
+import org.graalvm.compiler.nodes.PiNode;
+import org.graalvm.compiler.nodes.StructuredGraph;
+import org.graalvm.compiler.nodes.ValueNode;
+import org.graalvm.compiler.nodes.ValuePhiNode;
+import org.graalvm.compiler.nodes.ValueProxyNode;
+import org.graalvm.compiler.nodes.java.MethodCallTargetNode;
+import org.graalvm.compiler.nodes.java.NewInstanceNode;
+import org.graalvm.compiler.nodes.spi.LoweringProvider;
+import org.graalvm.compiler.phases.VerifyPhase;
+import org.graalvm.compiler.phases.tiers.PhaseContext;
+import org.graalvm.util.EconomicSet;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class VerifyGraphAddUsage extends VerifyPhase<PhaseContext> {
-
-    private static final String[] AllowedPackagePrefixes;
-
-    private static String getPackageName(Class<?> c) {
-        String classNameWithPackage = c.getName();
-        String simpleName = c.getSimpleName();
-        return classNameWithPackage.substring(0, classNameWithPackage.length() - simpleName.length() - 1);
-    }
+    private static final Method ADD_OR_UNIQUE;
+    private static final Method CONSTRUCTOR_NEW_INSTANCE;
+    private static final EconomicSet<Class<?>> ALLOWED_CLASSES = EconomicSet.create();
 
     static {
         try {
-            AllowedPackagePrefixes = new String[]{getPackageName(PermanentBailoutException.class), "jdk.vm.ci"};
-        } catch (Throwable t) {
-            throw new GraalError(t);
+            ADD_OR_UNIQUE = Graph.class.getDeclaredMethod("addOrUnique", Node.class);
+            CONSTRUCTOR_NEW_INSTANCE = Constructor.class.getDeclaredMethod("newInstance", Object[].class);
+        } catch (NoSuchMethodException e) {
+            throw new GraalError(e);
         }
+
+        ALLOWED_CLASSES.add(Graph.class);
+        ALLOWED_CLASSES.add(LoweringProvider.class);
     }
 
-    private static boolean matchesPrefix(String packageName) {
-        for (String allowedPackagePrefix : AllowedPackagePrefixes) {
-            if (packageName.startsWith(allowedPackagePrefix)) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    private static final String[] AllowedPackagePrefixes;
+
+//    private static String getPackageName(Class<?> c) {
+//        String classNameWithPackage = c.getName();
+//        String simpleName = c.getSimpleName();
+//        return classNameWithPackage.substring(0, classNameWithPackage.length() - simpleName.length() - 1);
+//    }
+//
+//    static {
+//        try {
+//            AllowedPackagePrefixes = new String[]{getPackageName(PermanentBailoutException.class), "jdk.vm.ci"};
+//        } catch (Throwable t) {
+//            throw new GraalError(t);
+//        }
+//    }
+
+//    private static boolean matchesPrefix(String packageName) {
+//        for (String allowedPackagePrefix : AllowedPackagePrefixes) {
+//            if (packageName.startsWith(allowedPackagePrefix)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
     @Override
     protected boolean verify(StructuredGraph graph, PhaseContext context) {
-        final ResolvedJavaType bailoutType = context.getMetaAccess().lookupJavaType(BailoutException.class);
-        ResolvedJavaMethod caller = graph.method();
-        String holderQualified = caller.format("%H");
-        String holderUnqualified = caller.format("%h");
-        String packageName = holderQualified.substring(0, holderQualified.length() - holderUnqualified.length() - 1);
-        if (!matchesPrefix(packageName)) {
+        boolean allowed = false;
+        for (Class<?> cls : ALLOWED_CLASSES) {
+            ResolvedJavaType declaringClass = graph.method().getDeclaringClass();
+            if (context.getMetaAccess().lookupJavaType(cls).isAssignableFrom(declaringClass)) {
+                allowed = true;
+            }
+        }
+        if (!allowed) {
+            ResolvedJavaMethod addOrUniqueMethod = context.getMetaAccess().lookupJavaMethod(ADD_OR_UNIQUE);
             for (MethodCallTargetNode t : graph.getNodes(MethodCallTargetNode.TYPE)) {
                 ResolvedJavaMethod callee = t.targetMethod();
-                if (callee.getDeclaringClass().equals(bailoutType)) {
-                    // we only allow the getter
-                    if (!callee.getName().equals("isPermanent")) {
-                        throw new VerificationError("Call to %s at callsite %s is prohibited. Consider using %s for permanent bailouts or %s for retryables.", callee.format("%H.%n(%p)"),
-                                        caller.format("%H.%n(%p)"), PermanentBailoutException.class.getName(),
-                                        RetryableBailoutException.class.getName());
-                    }
+                if (callee.equals(addOrUniqueMethod)) {
+                    ValueNode nodeArgument = t.arguments().get(1);
+                    EconomicSet<Node> seen = EconomicSet.create();
+                    checkNonFactory(graph, seen, context, nodeArgument);
                 }
             }
         }
+
         return true;
+    }
+
+    private void checkNonFactory(StructuredGraph graph, EconomicSet<Node> seen, PhaseContext context, ValueNode node) {
+        if (seen.contains(node)) {
+            return;
+        }
+        seen.add(node);
+
+        // Check where the value came from recursively, or if it is allowed.
+        if (node instanceof ValuePhiNode) {
+            for (ValueNode input : ((ValuePhiNode) node).values()) {
+                checkNonFactory(graph, seen, context, input);
+            }
+        } else if (node instanceof PiNode) {
+            checkNonFactory(graph, seen, context, ((PiNode) node).object());
+        } else if (node instanceof ParameterNode) {
+            return;
+        } else if (node instanceof ConstantNode) {
+            return;
+        } else if (node instanceof ValueProxyNode) {
+            checkNonFactory(graph, seen, context, ((ValueProxyNode) node).value());
+        } else if (node instanceof Invoke && ((Invoke) node).callTarget().targetMethod().equals(context.getMetaAccess().lookupJavaMethod(CONSTRUCTOR_NEW_INSTANCE))) {
+            return;
+        } else if (!(node instanceof NewInstanceNode)) {
+            // In all other cases, the argument must be a new instance.
+            throw new VerificationError("Must add node '%s' with inputs in method '%s' of class '%s'.",
+                            node, graph.method().getName(), graph.method().getDeclaringClass().getName());
+        }
     }
 
 }
