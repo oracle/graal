7b387d10869f3aa652835a3d8d16038e0188d222
Matthias Grimmer <matthias.grimmer@oracle.com>
2017-10-18 16:53:44 +0200

    Ensure that Sulong build and tests are quiet and do write to stderr nor stdout.

--- tests/com.oracle.truffle.llvm.tests.interop/interop/interop077.c
+++ tests/com.oracle.truffle.llvm.tests.interop/interop/interop077.c
@@ -3,7 +3,8 @@
 
 int main() {
     const char* cstr = truffle_string_to_cstr(truffle_execute(truffle_import("getstring"), 1));
-    int l = fprintf(stderr, "%s", cstr);
+    char buffer[100];
+    int l = snprintf(buffer, 100, "%s", cstr);
     truffle_free_cstr(cstr);
     return l;
 }
