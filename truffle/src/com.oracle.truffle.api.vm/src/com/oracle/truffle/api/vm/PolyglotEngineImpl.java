f33a8055a65bb15d3c13f2acf56a2aad76d5bd31
Christian Humer <christian.humer@oracle.com>
2017-10-20 14:04:19 +0200

    Fix bug in langauge ordering.

--- truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotEngineImpl.java
+++ truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotEngineImpl.java
@@ -379,9 +379,7 @@ class PolyglotEngineImpl extends org.graalvm.polyglot.impl.AbstractPolyglotImpl.
 
     private void visitLanguage(Map<String, RuntimeException> initErrors, Map<String, LanguageCache> cachedLanguages, LinkedHashSet<LanguageCache> serializedLanguages,
                     LanguageCache language) {
-        if (!language.getDependentLanguages().isEmpty()) {
-            visitLanguageImpl(new HashSet<>(), initErrors, cachedLanguages, serializedLanguages, language);
-        }
+        visitLanguageImpl(new HashSet<>(), initErrors, cachedLanguages, serializedLanguages, language);
     }
 
     private void visitLanguageImpl(Set<String> visitedIds, Map<String, RuntimeException> initErrors, Map<String, LanguageCache> cachedLanguages, LinkedHashSet<LanguageCache> serializedLanguages,
