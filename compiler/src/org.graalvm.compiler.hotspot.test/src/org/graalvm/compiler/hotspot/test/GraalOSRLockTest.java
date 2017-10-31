8b83703584a39e2cc5a40bdbef4de10534d7282d
David Leopoldseder <david.leopoldseder@jku.at>
2017-10-31 13:17:37 +0100

    [fix] GraalOSRLockTest: Move ManagementFactory availability check to beforeclass.

--- compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/GraalOSRLockTest.java
+++ compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/GraalOSRLockTest.java
@@ -56,16 +56,13 @@ public class GraalOSRLockTest extends GraalOSRTestBase {
     private static boolean TestInSeparateThread = false;
     private static final String COMPILE_ONLY_FLAG = "-Xcomp";
 
-    public GraalOSRLockTest() {
+    @BeforeClass
+    public static void checkVMArguments() {
         try {
             Class.forName("java.lang.management.ManagementFactory");
         } catch (ClassNotFoundException ex) {
             Assume.assumeNoException("cannot check for monitors without java.management JDK9 module", ex);
         }
-    }
-
-    @BeforeClass
-    public static void checkVMArguments() {
         /*
          * Note: The -Xcomp execution mode of the VM will stop most of the OSR test cases from
          * working as every method is compiled at level3 (followed by level4 on the second
