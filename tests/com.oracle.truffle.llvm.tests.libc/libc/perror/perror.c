352b357f2535165ef2795b222d5dff6688d29897
Roland Schatz <roland.schatz@oracle.com>
2017-11-06 13:39:47 +0100

    Remove internal stderr redirection from unit test.

--- tests/com.oracle.truffle.llvm.tests.libc/libc/perror/perror.c
+++ tests/com.oracle.truffle.llvm.tests.libc/libc/perror/perror.c
@@ -2,19 +2,10 @@
 #include <errno.h>
 
 int main() {
-  char name[200];
-  FILE *file = fopen(tmpnam(name), "w");
-  fclose(file);
-  freopen(name, "w", stderr);
+  char buffer[100];
   for (int i = 0; i < 10; i++) {
+    snprintf(buffer, 100, "errno %d", i);
     errno = i;
-    perror("hello world!");
+    perror(buffer);
   }
-  fclose(stderr);
-  file = fopen(name, "r");
-  char buf[500];
-  while (fgets(buf, 500, file) != NULL) {
-    puts(buf);
-  }
-  fclose(file);
 }
