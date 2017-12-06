5a05b32865a30c0cf5ca0367c0faf58b6922b209
Tim Felgentreff <tim.felgentreff@oracle.com>
2017-12-06 10:42:25 +0100

    remove redundant public modifier

--- truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/RootNodeTest.java
+++ truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/RootNodeTest.java
@@ -155,7 +155,7 @@ public class RootNodeTest {
     class TestException extends RuntimeException implements TruffleException {
         MaterializedFrame frame;
 
-        public TestException(VirtualFrame frame) {
+        TestException(VirtualFrame frame) {
             this.frame = frame.materialize();
         }
 
