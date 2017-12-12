pekd/sulong

Add partial support for function vectors

projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/store/LLVMStoreVectorNode.java
@@ -41,6 +41,7 @@ import com.oracle.truffle.llvm.runtime.types.Type;
 import com.oracle.truffle.llvm.runtime.vector.LLVMAddressVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMDoubleVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMFloatVector;
+import com.oracle.truffle.llvm.runtime.vector.LLVMFunctionVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI16Vector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI1Vector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI32Vector;
@@ -145,6 +146,18 @@ public abstract class LLVMStoreVectorNode extends LLVMStoreNode {
 
     @Specialization
     protected Object writeVector(LLVMGlobalVariable address, LLVMAddressVector value, @Cached("createGlobalAccess()") LLVMGlobalVariableAccess globalAccess) {
+        LLVMMemory.putVector(globalAccess.getNativeLocation(address), value);
+        return null;
+    }
+
+    @Specialization
+    protected Object writeVector(LLVMAddress address, LLVMFunctionVector value) {
+        LLVMMemory.putVector(address, value);
+        return null;
+    }
+
+    @Specialization
+    protected Object writeVector(LLVMGlobalVariable address, LLVMFunctionVector value, @Cached("createGlobalAccess()") LLVMGlobalVariableAccess globalAccess) {
         LLVMMemory.putVector(globalAccess.getNativeLocation(address), value);
         return null;
     }

oracle/graal

projects/com.oracle.truffle.llvm.nodes/src/com/oracle/truffle/llvm/nodes/memory/store/LLVMStoreVectorNode.java
@@ -41,6 +41,7 @@ import com.oracle.truffle.llvm.runtime.types.Type;
 import com.oracle.truffle.llvm.runtime.vector.LLVMAddressVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMDoubleVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMFloatVector;
+import com.oracle.truffle.llvm.runtime.vector.LLVMFunctionVector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI16Vector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI1Vector;
 import com.oracle.truffle.llvm.runtime.vector.LLVMI32Vector;
@@ -167,6 +168,20 @@ public abstract class LLVMStoreVectorNode extends LLVMStoreNode {
 
     @Specialization
     protected Object writeVector(VirtualFrame frame, LLVMGlobal address, LLVMAddressVector value,
+                    @Cached("createToNativeWithTarget()") LLVMToNativeNode globalAccess,
+                    @Cached("getLLVMMemory()") LLVMMemory memory) {
+        memory.putVector(globalAccess.executeWithTarget(frame, address), value);
+        return null;
+    }
+
+    @Specialization
+    protected Object writeVector(LLVMAddress address, LLVMFunctionVector value, @Cached("getLLVMMemory()") LLVMMemory memory) {
+        memory.putVector(address, value);
+        return null;
+    }
+
+    @Specialization
+    protected Object writeVector(VirtualFrame frame, LLVMGlobal address, LLVMFunctionVector value,
                     @Cached("createToNativeWithTarget()") LLVMToNativeNode globalAccess,
                     @Cached("getLLVMMemory()") LLVMMemory memory) {
         memory.putVector(globalAccess.executeWithTarget(frame, address), value);