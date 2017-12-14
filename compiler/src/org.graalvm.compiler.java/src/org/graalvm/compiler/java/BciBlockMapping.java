3b79dfd160af4ce7c3bd52fbbf494c4b67f02e7b
Yudi Zheng <yudi.zheng@oracle.com>
2017-12-14 12:16:59 +0100

    Style fix.

--- compiler/src/org.graalvm.compiler.java/src/org/graalvm/compiler/java/BciBlockMapping.java
+++ compiler/src/org.graalvm.compiler.java/src/org/graalvm/compiler/java/BciBlockMapping.java
@@ -415,9 +415,6 @@ public final class BciBlockMapping {
     }
 
     public static class ExceptionDispatchBlock extends BciBlock {
-
-        private EconomicMap<ExceptionHandler, ExceptionDispatchBlock> exceptionDispatch = EconomicMap.create(Equivalence.DEFAULT);
-
         public ExceptionHandler handler;
         public int deoptBci;
     }
