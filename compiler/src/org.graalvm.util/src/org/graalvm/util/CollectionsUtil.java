75faf90e7b80d450e72c80836a2f85566f67092f
Yudi Zheng <yudi.zheng@oracle.com>
2018-01-05 11:26:10 +0100

    Style fix.

--- compiler/src/org.graalvm.util/src/org/graalvm/util/CollectionsUtil.java
+++ compiler/src/org.graalvm.util/src/org/graalvm/util/CollectionsUtil.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
