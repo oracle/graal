89234452400b7c833782ad3a6dd4346bd9bee0dc
Aleksandar Pejović <aleksandar.pejovic@oracle.com>
2018-01-22 12:49:08 +0100

    Merge Unwind nodes in ReflectiveWriteMethod

--- substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/ReflectionSubstitutionType.java
+++ substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/ReflectionSubstitutionType.java
@@ -443,6 +443,8 @@ public final class ReflectionSubstitutionType extends CustomSubstitutionType<Cus
                 throwIllegalArgumentException(graphKit, "cannot write field of type " + targetField.getJavaKind() + " with Field." + method.getName());
             }
 
+            graphKit.mergeUnwinds();
+
             assert graphKit.getGraph().verify();
             return graphKit.getGraph();
         }
