64fb438268059d568dc6cc807e3f532aef28e28d
Doug Simon <doug.simon@oracle.com>
2018-02-22 17:42:02 +0100

    allow jdk9 or later to not be monotonic

--- compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/lang/UnaryMath.java
+++ compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/lang/UnaryMath.java
@@ -35,6 +35,20 @@ public abstract class UnaryMath extends JTTTest {
      * Tests a unary {@link Math} method on a wide range of values.
      */
     void testManyValues(OptionValues options, ResolvedJavaMethod method) throws AssertionError {
+        if (!Java8OrEarlier) {
+            /*
+             * GR-8276: Allow for variance on JVMCI > 8 until a JVMCI version that includes
+             * https://github.com/graalvm/graal-jvmci-8/commit/
+             * c86fb66f86b8d52a08dd2495d34879d3730f9987 or Graal has stubs that a monotonic with
+             * other HotSpot implementations of these Math routines.
+             */
+            ulpDelta = 2D;
+        } else {
+            /*
+             * Forces the assertion message shows the ulps by which a computed result is wrong.
+             */
+            ulpDelta = 0D;
+        }
         Object receiver = null;
         long testIteration = 0;
         for (long l = Long.MIN_VALUE;; l += STEP) {
