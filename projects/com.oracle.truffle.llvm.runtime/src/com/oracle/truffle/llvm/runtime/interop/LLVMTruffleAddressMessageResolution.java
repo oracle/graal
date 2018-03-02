6d6e1b02255629882868fa7536461dc184815e89
Roland Schatz <roland.schatz@oracle.com>
2018-03-02 16:17:50 +0100

    Rename LLVMAddressMessageResolution to LLVMTruffleAddressMessageResolution.

--- projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/LLVMTruffleAddressMessageResolution.java
+++ projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/LLVMTruffleAddressMessageResolution.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates.
+ * Copyright (c) 2016, 2018, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -42,7 +42,7 @@ import com.oracle.truffle.llvm.runtime.interop.LLVMAddressMessageResolutionNodeF
 import com.oracle.truffle.llvm.runtime.interop.LLVMAddressMessageResolutionNodeFactory.LLVMAddressWriteMessageResolutionNodeGen;
 
 @MessageResolution(receiverType = LLVMTruffleAddress.class)
-public class LLVMAddressMessageResolution {
+public class LLVMTruffleAddressMessageResolution {
 
     @Resolve(message = "HAS_SIZE")
     public abstract static class ForeignHasSize extends Node {
