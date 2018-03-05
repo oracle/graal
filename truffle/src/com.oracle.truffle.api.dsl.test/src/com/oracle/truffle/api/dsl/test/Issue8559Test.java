d9fd4aa29da3c0750ac14ed67c9e20f8d9efdae1
Christian Humer <christian.humer@oracle.com>
2018-03-05 19:46:40 +0100

    Fix style error.

--- truffle/src/com.oracle.truffle.api.dsl.test/src/com/oracle/truffle/api/dsl/test/Issue8559Test.java
+++ truffle/src/com.oracle.truffle.api.dsl.test/src/com/oracle/truffle/api/dsl/test/Issue8559Test.java
@@ -54,7 +54,7 @@ public class Issue8559Test {
         }
     }
 
-    public static abstract class BaseNode extends Node {
+    public abstract static class BaseNode extends Node {
 
         public abstract Object execute(VirtualFrame frame);
 
