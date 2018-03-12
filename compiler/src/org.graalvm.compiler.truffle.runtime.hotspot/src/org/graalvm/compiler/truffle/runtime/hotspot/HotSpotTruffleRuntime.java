b6f41d4e0a64d9b3cdd8d937b7f703c4b81941d8
Christian Humer <christian.humer@oracle.com>
2018-03-12 14:59:40 +0100

    Fix formatting.

--- compiler/src/org.graalvm.compiler.truffle.runtime.hotspot/src/org/graalvm/compiler/truffle/runtime/hotspot/HotSpotTruffleRuntime.java
+++ compiler/src/org.graalvm.compiler.truffle.runtime.hotspot/src/org/graalvm/compiler/truffle/runtime/hotspot/HotSpotTruffleRuntime.java
@@ -189,8 +189,8 @@ public final class HotSpotTruffleRuntime extends GraalTruffleRuntime implements 
     }
 
     /**
-     * Prevents C1 or C2 from inlining a call to a method annotated by {@link TruffleCallBoundary} so
-     * that we never miss the chance to switch from the Truffle interpreter to compiled code.
+     * Prevents C1 or C2 from inlining a call to a method annotated by {@link TruffleCallBoundary}
+     * so that we never miss the chance to switch from the Truffle interpreter to compiled code.
      *
      * @see HotSpotTruffleCompiler#installTruffleCallBoundaryMethods()
      */
