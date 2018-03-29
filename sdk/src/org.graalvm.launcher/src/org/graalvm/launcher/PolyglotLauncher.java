523f3b32e9576370b679250891ca3f3bb152fb1b
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2018-03-29 15:09:20 +0200

    Avoid using a shared engine in PolyglotLauncher
    (cherry picked from commit 0eb1ca1341e9bbf81c6af1c233b02320b04d2213)

--- sdk/src/org.graalvm.launcher/src/org/graalvm/launcher/PolyglotLauncher.java
+++ sdk/src/org.graalvm.launcher/src/org/graalvm/launcher/PolyglotLauncher.java
@@ -168,15 +168,14 @@ public final class PolyglotLauncher extends Launcher {
         if (runPolyglotAction()) {
             return;
         }
-        Engine engine = Engine.create();
-        Context.Builder contextBuilder = Context.newBuilder().engine(engine).options(options).in(System.in).out(System.out).err(System.err);
+        Context.Builder contextBuilder = Context.newBuilder().options(options).in(System.in).out(System.out).err(System.err);
         if (!isAOT()) {
             contextBuilder.allowHostAccess(true);
         }
         contextBuilder.allowCreateThread(true);
 
         if (version) {
-            printVersion(engine);
+            printVersion(Engine.newBuilder().options(options).build());
             throw exit();
         }
 
