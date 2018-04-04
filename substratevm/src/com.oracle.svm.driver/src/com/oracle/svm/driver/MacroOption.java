2e6646d67e799e61c1c384cfcec618a7e72d70d7
Paul Wögerer <paul.woegerer@oracle.com>
2018-04-04 10:17:06 +0200

    Make MacroOptionKind naming lowercase

--- substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/MacroOption.java
+++ substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/MacroOption.java
@@ -58,6 +58,20 @@ final class MacroOption {
             }
             throw new InvalidMacroException("No MacroOptionKind for subDir: " + subdir);
         }
+
+        static MacroOptionKind fromString(String kindName) {
+            for (MacroOptionKind kind : MacroOptionKind.values()) {
+                if (kind.toString().equals(kindName)) {
+                    return kind;
+                }
+            }
+            throw new InvalidMacroException("No MacroOptionKind for kindName: " + kindName);
+        }
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
     }
 
     Path getImageJarsDirectory() {
@@ -79,7 +93,7 @@ final class MacroOption {
         if (commandLineStyle) {
             sb.append(macroOptionPrefix);
         }
-        sb.append(kind.name()).append(":").append(getOptionName());
+        sb.append(kind.toString()).append(":").append(getOptionName());
         return sb.toString();
     }
 
@@ -282,7 +296,7 @@ final class MacroOption {
             if (!optionsToShow.isEmpty()) {
                 StringBuilder sb = new StringBuilder().append("Available ");
                 if (forKind != null) {
-                    sb.append(forKind.name()).append(' ');
+                    sb.append(forKind.toString()).append(' ');
                 } else {
                     sb.append("macro-");
                 }
@@ -314,7 +328,7 @@ final class MacroOption {
 
             MacroOptionKind kindPart;
             try {
-                kindPart = MacroOptionKind.valueOf(specParts[0]);
+                kindPart = MacroOptionKind.fromString(specParts[0]);
             } catch (Exception e) {
                 if (context == null) {
                     return false;
