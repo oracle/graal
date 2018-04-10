2cfc1cf576cba66dde089cb118f73e03db24d234
Tomas Zezula <tomas.zezula@oracle.com>
2018-04-10 22:57:59 +0200

    [GR-9258] Fixed NullPointerException.

--- tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/instrument/InspectorInstrument.java
+++ tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/instrument/InspectorInstrument.java
@@ -45,6 +45,8 @@ import com.oracle.truffle.api.nodes.LanguageInfo;
 
 import com.oracle.truffle.tools.chromeinspector.TruffleExecutionContext;
 import com.oracle.truffle.tools.chromeinspector.server.WebSocketServer;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Chrome inspector as an instrument.
@@ -195,8 +197,8 @@ public final class InspectorInstrument extends TruffleInstrument {
             info.println("    " + address);
             info.flush();
             if (debugBreak || waitAttached) {
-                final EventBinding<?>[] execEnter = new EventBinding<?>[1];
-                execEnter[0] = env.getInstrumenter().attachContextsListener(new ContextsListener() {
+                final AtomicReference<EventBinding<?>> execEnter = new AtomicReference<>();
+                execEnter.set(env.getInstrumenter().attachContextsListener(new ContextsListener() {
                     @Override
                     public void onContextCreated(TruffleContext context) {
                     }
@@ -212,7 +214,10 @@ public final class InspectorInstrument extends TruffleInstrument {
                             executionContext.waitForRunPermission();
                         } catch (InterruptedException ex) {
                         }
-                        execEnter[0].dispose();
+                        final EventBinding<?> binding = execEnter.getAndSet(null);
+                        if (binding != null) {
+                            binding.dispose();
+                        }
                     }
 
                     @Override
@@ -226,7 +231,10 @@ public final class InspectorInstrument extends TruffleInstrument {
                     @Override
                     public void onContextClosed(TruffleContext context) {
                     }
-                }, true);
+                }, true));
+                if (disposeBinding.get()) {
+                    execEnter.get().dispose();
+                }
             }
             return address;
         }
