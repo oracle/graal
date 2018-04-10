07b25607c018764abd6c8481afdb5debacc97e72
Martin Entlicher <martin.entlicher@oracle.com>
2018-04-10 23:12:18 +0200

    Corrected disposeBinding variable.
    (cherry picked from commit c2b837099c11bbf611b79a97575e9bb8506f8b4f)

--- tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/instrument/InspectorInstrument.java
+++ tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/instrument/InspectorInstrument.java
@@ -198,6 +198,7 @@ public final class InspectorInstrument extends TruffleInstrument {
             info.flush();
             if (debugBreak || waitAttached) {
                 final AtomicReference<EventBinding<?>> execEnter = new AtomicReference<>();
+                final AtomicBoolean disposeBinding = new AtomicBoolean(false);
                 execEnter.set(env.getInstrumenter().attachContextsListener(new ContextsListener() {
                     @Override
                     public void onContextCreated(TruffleContext context) {
@@ -217,6 +218,8 @@ public final class InspectorInstrument extends TruffleInstrument {
                         final EventBinding<?> binding = execEnter.getAndSet(null);
                         if (binding != null) {
                             binding.dispose();
+                        } else {
+                            disposeBinding.set(true);
                         }
                     }
 
