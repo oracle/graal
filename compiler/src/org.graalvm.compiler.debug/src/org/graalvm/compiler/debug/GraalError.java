2862038f0911702e3e261a964936c7605de269c9
Andrew Haley <aph@redhat.com>
2018-04-13 15:29:38 +0100

    Remove accidental edit

--- compiler/src/org.graalvm.compiler.debug/src/org/graalvm/compiler/debug/GraalError.java
+++ compiler/src/org.graalvm.compiler.debug/src/org/graalvm/compiler/debug/GraalError.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2011, 2014, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2018, Red Hat Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -148,7 +147,8 @@ public class GraalError extends Error {
     /**
      * This constructor creates a {@link GraalError} with a message assembled via
      * {@link String#format(String, Object...)}. It always uses the ENGLISH locale in order to
-internal is     *
+     * always generate the same output.
+     *
      * @param msg the message that will be associated with the error, in String.format syntax
      * @param args parameters to String.format - parameters that implement {@link Iterable} will be
      *            expanded into a [x, x, ...] representation.
