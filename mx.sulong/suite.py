3b99e94a515f5155a6f2875ae746afd84cd255a8
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2018-04-16 11:51:41 +0200

    Update graal import.

--- mx.sulong/suite.py
+++ mx.sulong/suite.py
@@ -8,7 +8,7 @@ suite = {
       {
         "name" : "truffle",
         "subdir" : True,
-        "version" : "1920ecf956d16175b95c0e301d290bc0a8e2c995",
+        "version" : "5a45980a4cb8b32a3d964a99d6e356717fcdb678",
         "urls" : [
           {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
           {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
