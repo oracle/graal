22610d017e52dee50d9f6fdb7b1b12572c7abfd1
Florian Angerer <florian.angerer@oracle.com>
2018-04-16 11:09:11 +0200

    Revert "Fix interop test."
    This reverts commit d9eb13b646fabad4b3b071fa0f74e2a0a52948f7.

--- projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/interop/LLVMInteropTest.java
+++ projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/interop/LLVMInteropTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2018, Oracle and/or its affiliates.
+ * Copyright (c) 2016, 2017, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -1047,7 +1047,7 @@ public class LLVMInteropTest {
         @Resolve(message = "READ")
         abstract static class ReadNode extends Node {
             int access(ForeignObject object, Object key) {
-                Assert.assertEquals("foo", key);
+                Assert.assertEquals(0, key);
                 return object.foo;
             }
         }
