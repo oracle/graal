21d8d6fbc296578b2cc0e6e2e33080656deb797b
Christian Humer <christian.humer@oracle.com>
2018-05-03 16:45:05 +0200

    Use Interpreted instead of Default name.

--- truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/DefaultTruffleRuntime.java
+++ truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/impl/DefaultTruffleRuntime.java
@@ -82,7 +82,7 @@ public final class DefaultTruffleRuntime implements TruffleRuntime {
 
     @Override
     public String getName() {
-        return "Default";
+        return "Interpreted";
     }
 
     @SuppressWarnings("deprecation")
