acc793870dc05c8822cc2e324591af35206f10d0
Paul Wögerer <paul.woegerer@oracle.com>
2018-05-07 09:27:09 +0200

    EXECUTABLE and default switch case (for documentation purpose)

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeBootImageViaCC.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeBootImageViaCC.java
@@ -38,6 +38,7 @@ import com.oracle.svm.core.LinkerInvocation;
 import com.oracle.svm.core.OS;
 import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.util.UserError;
+import com.oracle.svm.core.util.VMError;
 import com.oracle.svm.hosted.FeatureImpl.BeforeImageWriteAccessImpl;
 import com.oracle.svm.hosted.NativeImageOptions;
 import com.oracle.svm.hosted.c.NativeLibraries;
@@ -68,12 +69,16 @@ public abstract class NativeBootImageViaCC extends NativeBootImage {
         @Override
         protected void setOutputKind(List<String> cmd) {
             switch (kind) {
+                case EXECUTABLE:
+                    break;
                 case STATIC_EXECUTABLE:
                     cmd.add("-static");
                     break;
                 case SHARED_LIBRARY:
                     cmd.add("-shared");
                     break;
+                default:
+                    VMError.shouldNotReachHere();
             }
         }
 
