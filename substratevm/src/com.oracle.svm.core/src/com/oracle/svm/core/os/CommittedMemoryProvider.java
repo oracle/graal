58fc84161aa95f3e897ed9128750b80a1b8317fd
Peter Hofer <peter.hofer@oracle.com>
2018-05-17 17:22:46 +0200

    Use constant 1 for CommittedMemoryProvider.UNALIGNED

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/os/CommittedMemoryProvider.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/os/CommittedMemoryProvider.java
@@ -42,7 +42,7 @@ public interface CommittedMemoryProvider {
      * Value for alignment parameters that indicates that no specific alignment is required (other
      * than the {@linkplain #getGranularity() granularity} usually).
      */
-    UnsignedWord UNALIGNED = WordFactory.zero();
+    UnsignedWord UNALIGNED = WordFactory.unsigned(1);
 
     @Fold
     static CommittedMemoryProvider get() {
