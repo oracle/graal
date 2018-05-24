fd0591a65f19ef4d00e233454e8f0999d99f7080
Christian Humer <christian.humer@oracle.com>
2018-05-24 16:29:08 +0200

    Fix source caching. Merge arguments key with source key.

--- truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotSourceCache.java
+++ truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/PolyglotSourceCache.java
@@ -49,18 +49,10 @@ final class PolyglotSourceCache {
         CallTarget target;
         if (source.isCached()) {
             Object sourceId = VMAccessor.SOURCE.getSourceIdentifier(source);
-            if (argumentNames != null && argumentNames.length > 0) {
-                sourceId = new ArgumentSourceId(sourceId, argumentNames);
-            }
-
-            target = sourceCache.get(sourceId);
+            WeakSourceKey ref = new WeakSourceKey(sourceId, source, argumentNames, deadSources);
+            target = sourceCache.get(ref);
             if (target == null) {
-                /*
-                 * We use a weak reference as key to get notified when sources are collected.
-                 */
-                SourceWeakReference ref = new SourceWeakReference(sourceId, source, deadSources);
-                Source weakSource = VMAccessor.SOURCE.copySource(source);
-                target = parseImpl(context, argumentNames, weakSource);
+                target = parseImpl(context, argumentNames, VMAccessor.SOURCE.copySource(source));
                 CallTarget prev = sourceCache.putIfAbsent(ref, target);
                 if (prev != null) {
                     /*
@@ -84,67 +76,41 @@ final class PolyglotSourceCache {
     }
 
     private void cleanupStaleEntries() {
-        SourceWeakReference sourceRef = null;
-        while ((sourceRef = (SourceWeakReference) deadSources.poll()) != null) {
+        WeakSourceKey sourceRef = null;
+        while ((sourceRef = (WeakSourceKey) deadSources.poll()) != null) {
             sourceCache.remove(sourceRef);
         }
     }
 
-    private static final class SourceWeakReference extends WeakReference<Source> {
+    private static final class WeakSourceKey extends WeakReference<Source> {
 
         final Object key;
-
-        SourceWeakReference(Object key, Source value, ReferenceQueue<? super Source> q) {
-            super(value, q);
-            this.key = key;
-        }
-
-        @Override
-        public int hashCode() {
-            return key.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            Object other;
-            if (!(obj instanceof SourceWeakReference)) {
-                other = obj;
-            } else {
-                other = ((SourceWeakReference) obj).key;
-            }
-            return key.equals(other);
-        }
-    }
-
-    private static class ArgumentSourceId {
-
-        private final Object sourceId;
         private final String[] arguments;
 
-        ArgumentSourceId(Object sourceId, String[] arguments) {
-            this.sourceId = sourceId;
-            this.arguments = arguments;
+        WeakSourceKey(Object key, Source value, String[] arguments, ReferenceQueue<? super Source> q) {
+            super(value, q);
+            this.key = key;
+            this.arguments = arguments != null && arguments.length == 0 ? null : arguments;
         }
 
         @Override
         public int hashCode() {
             final int prime = 31;
             int result = 1;
-            result = prime * result + ((sourceId == null) ? 0 : sourceId.hashCode());
+            result = prime * result + key.hashCode();
             result = prime * result + Arrays.hashCode(arguments);
             return result;
         }
 
         @Override
         public boolean equals(Object obj) {
-            if (!(obj instanceof ArgumentSourceId)) {
+            if (obj instanceof WeakSourceKey) {
+                WeakSourceKey other = (WeakSourceKey) obj;
+                return key.equals(other.key) && Arrays.equals(arguments, other.arguments);
+            } else {
                 return false;
             }
-            ArgumentSourceId other = (ArgumentSourceId) obj;
-            return sourceId.equals(other.sourceId) &&
-                            Arrays.equals(arguments, other.arguments);
         }
-
     }
 
 }
