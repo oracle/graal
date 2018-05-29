bade450af5e9e08935a5b6df7ba69a4dd9ed6026
Codrut Stancu <codrut.stancu@oracle.com>
2018-05-29 14:52:28 -0700

    Set a default invocation handler for the proxy instance.

--- substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/AccessorComputer.java
+++ substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/AccessorComputer.java
@@ -24,16 +24,44 @@ package com.oracle.svm.reflect.hosted;
 
 // Checkstyle: allow reflection
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Member;
+import java.lang.reflect.Proxy;
+
+import org.graalvm.nativeimage.ImageSingletons;
+
 import com.oracle.svm.core.UnsafeAccess;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.util.VMError;
-import java.lang.reflect.Member;
 
 import jdk.vm.ci.meta.MetaAccessProvider;
 import jdk.vm.ci.meta.ResolvedJavaField;
-import org.graalvm.nativeimage.ImageSingletons;
 
 public final class AccessorComputer implements RecomputeFieldValue.CustomFieldValueComputer {
+
+    private static final Field hField;
+    private static final InvocationHandler invocationHandler;
+
+    static {
+        try {
+            hField = Proxy.class.getDeclaredField("h");
+            hField.setAccessible(true);
+        } catch (NoSuchFieldException ex) {
+            throw VMError.shouldNotReachHere(ex);
+        }
+        invocationHandler = (proxy, method, args) -> {
+            if (method.getName().equals("toString")) {
+                return proxy.getClass().getName() + "@" + Integer.toHexString(System.identityHashCode(proxy));
+            }
+
+            if (method.getName().equals("hashCode") || method.getName().equals("equals") || method.getName().equals("invoke")) {
+                throw VMError.shouldNotReachHere("You should not call " + method + " on an instance of " + proxy.getClass() + " during image building.");
+            }
+
+            throw VMError.shouldNotReachHere("Unknown method " + method + " called on an instance of " + proxy.getClass() + ".");
+        };
+    }
 
     @Override
     public Object compute(MetaAccessProvider metaAccess, ResolvedJavaField original, ResolvedJavaField annotated, Object receiver) {
@@ -45,8 +73,19 @@ public final class AccessorComputer implements RecomputeFieldValue.CustomFieldVa
             throw VMError.shouldNotReachHere();
         }
         try {
-            return UnsafeAccess.UNSAFE.allocateInstance(proxyClass);
-        } catch (InstantiationException ex) {
+            Proxy proxyInstance = (Proxy) UnsafeAccess.UNSAFE.allocateInstance(proxyClass);
+            /*
+             * Set a default invocation handler for the proxy instance. In the generated image the
+             * "h" field is deleted anyway (see Target_java_lang_reflect_Proxy) and the methods
+             * (invoke, toString, hashCode, equals) that would use the handler are substituted (see
+             * ReflectionSubstitutionType). However the handler is needed when the proxy instance is
+             * used during image building, e.g., toString() is called on it for error reporting (see
+             * NativeImageHeap.addObjectToBootImageHeap()).
+             */
+            hField.set(proxyInstance, invocationHandler);
+            return proxyInstance;
+
+        } catch (InstantiationException | IllegalAccessException ex) {
             throw VMError.shouldNotReachHere(ex);
         }
     }
