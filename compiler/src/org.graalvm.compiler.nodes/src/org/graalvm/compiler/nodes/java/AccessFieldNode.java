336b86b06d3059974affe1f2268ffb7656b79ad9
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-06-07 19:24:27 +0200

    Update comment on volatileAccess.

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/AccessFieldNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/AccessFieldNode.java
@@ -50,6 +50,7 @@ public abstract class AccessFieldNode extends FixedWithNextNode implements Lower
     @OptionalInput ValueNode object;
 
     protected final ResolvedJavaField field;
+    protected final boolean volatileAccess;
 
     public ValueNode object() {
         return object;
@@ -60,11 +61,25 @@ public abstract class AccessFieldNode extends FixedWithNextNode implements Lower
      *
      * @param object the instruction producing the receiver object
      * @param field the compiler interface representation of the field
+     * @param volatileAccess specifies if the field access is volatile or not,
+     *                       this is independent from and overrides the field
+     *                       volatile attribute.
      */
-    public AccessFieldNode(NodeClass<? extends AccessFieldNode> c, Stamp stamp, ValueNode object, ResolvedJavaField field) {
+    public AccessFieldNode(NodeClass<? extends AccessFieldNode> c, Stamp stamp, ValueNode object, ResolvedJavaField field, boolean volatileAccess) {
         super(c, stamp);
         this.object = object;
         this.field = field;
+        this.volatileAccess = volatileAccess;
+    }
+
+    /**
+     * Constructs a new access field object.
+     *
+     * @param object the instruction producing the receiver object
+     * @param field the compiler interface representation of the field
+     */
+    public AccessFieldNode(NodeClass<? extends AccessFieldNode> c, Stamp stamp, ValueNode object, ResolvedJavaField field) {
+        this(c, stamp, object, field, field.isVolatile());
     }
 
     /**
@@ -86,12 +101,13 @@ public abstract class AccessFieldNode extends FixedWithNextNode implements Lower
     }
 
     /**
-     * Checks whether this field is declared volatile.
+     * Checks whether this access has volatile semantics.
      *
-     * @return {@code true} if the field is resolved and declared volatile
+     * It is possible to access volatile fields, using non-volatile semantics
+     * using VarHandles. In general the access should
      */
     public boolean isVolatile() {
-        return field.isVolatile();
+        return volatileAccess;
     }
 
     @Override
@@ -116,7 +132,7 @@ public abstract class AccessFieldNode extends FixedWithNextNode implements Lower
 
     @Override
     public NodeSize estimatedNodeSize() {
-        if (field.isVolatile()) {
+        if (isVolatile()) {
             return SIZE_2;
         }
         return super.estimatedNodeSize();
