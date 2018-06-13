eb8dbb538218a4f89e9d8e9194e5833eea0e00b7
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-06-13 17:22:21 +0200

    Remove ResolvedJavaMethod import hack. See GR-9819.

--- compiler/src/org.graalvm.compiler.replacements.jdk9.test/src/org/graalvm/compiler/replacements/jdk9/UnsafeReplacementsTest.java
+++ compiler/src/org.graalvm.compiler.replacements.jdk9.test/src/org/graalvm/compiler/replacements/jdk9/UnsafeReplacementsTest.java
@@ -27,7 +27,6 @@ package org.graalvm.compiler.replacements.jdk9;
 import jdk.vm.ci.aarch64.AArch64;
 import jdk.vm.ci.amd64.AMD64;
 import jdk.vm.ci.code.TargetDescription;
-import jdk.vm.ci.meta.ResolvedJavaMethod;
 import org.graalvm.compiler.api.test.Graal;
 import org.graalvm.compiler.core.phases.HighTier;
 import org.graalvm.compiler.options.OptionValues;
@@ -40,9 +39,6 @@ import java.lang.reflect.Field;
 
 @AddExports("java.base/jdk.internal.misc")
 public class UnsafeReplacementsTest extends MethodSubstitutionTest {
-
-    // See GR-9819.
-    @SuppressWarnings("unused") ResolvedJavaMethod method = null;
 
     static class Container {
         public volatile boolean booleanField;
