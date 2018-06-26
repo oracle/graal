a63cadd07d060f6e28b2e2ea75c2607fbdb5b34d
Roland Schatz <roland.schatz@oracle.com>
2018-06-26 15:18:46 +0200

    Fix typo.

--- projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LazyToTruffleConverterImpl.java
+++ projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/LazyToTruffleConverterImpl.java
@@ -96,10 +96,10 @@ public class LazyToTruffleConverterImpl implements LazyToTruffleConverter {
 
         // setup the uniquesRegion
         UniquesRegion uniquesRegion = new UniquesRegion();
-        GetStackSpaceFactory getStackSpaceFactoy = GetStackSpaceFactory.createGetStackSpaceFactory(uniquesRegion);
+        GetStackSpaceFactory getStackSpaceFactory = GetStackSpaceFactory.createGetStackSpaceFactory(uniquesRegion);
 
         LLVMLivenessAnalysisResult liveness = LLVMLivenessAnalysis.computeLiveness(frame, runtime.getContext(), phis, method);
-        LLVMSymbolReadResolver symbols = new LLVMSymbolReadResolver(runtime, frame, getStackSpaceFactoy);
+        LLVMSymbolReadResolver symbols = new LLVMSymbolReadResolver(runtime, frame, getStackSpaceFactory);
         List<FrameSlot> notNullable = new ArrayList<>();
 
         LLVMRuntimeDebugInformation dbgInfoHandler = new LLVMRuntimeDebugInformation(frame, runtime.getNodeFactory(), runtime.getContext(), notNullable, symbols);
