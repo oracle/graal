baac2dae50769cf6a39cd0e4a49d03da835c62cf
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-07-02 11:19:43 +0200

    Tests for reconstructing the shadow stack.

--- tools/src/com.oracle.truffle.tools.profiler.test/src/com/oracle/truffle/tools/profiler/test/CPUSamplerTest.java
+++ tools/src/com.oracle.truffle.tools.profiler.test/src/com/oracle/truffle/tools/profiler/test/CPUSamplerTest.java
@@ -24,21 +24,35 @@
  */
 package com.oracle.truffle.tools.profiler.test;
 
-import java.util.Collection;
-import java.util.Iterator;
-
+import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.instrumentation.GenerateWrapper;
+import com.oracle.truffle.api.instrumentation.InstrumentableNode;
+import com.oracle.truffle.api.instrumentation.ProbeNode;
+import com.oracle.truffle.api.instrumentation.ProvidedTags;
+import com.oracle.truffle.api.instrumentation.StandardTags;
+import com.oracle.truffle.api.instrumentation.Tag;
+import com.oracle.truffle.api.nodes.DirectCallNode;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RootNode;
+import com.oracle.truffle.api.source.SourceSection;
+import com.oracle.truffle.tools.profiler.CPUSampler;
+import com.oracle.truffle.tools.profiler.ProfilerNode;
 import org.graalvm.polyglot.Source;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import com.oracle.truffle.tools.profiler.CPUSampler;
-import com.oracle.truffle.tools.profiler.ProfilerNode;
+import java.util.Collection;
+import java.util.Iterator;
 
 public class CPUSamplerTest extends AbstractProfilerTest {
 
-    private CPUSampler sampler;
+    private static CPUSampler sampler;
 
     final int executionCount = 10;
 
@@ -189,5 +203,291 @@ public class CPUSamplerTest extends AbstractProfilerTest {
 
     private static void checkTimeline(CPUSampler.Payload payload) {
         Assert.assertEquals("Timeline length and self hit count to not match!", payload.getSelfHitCount(), payload.getSelfHitTimes().size());
+    }
+
+    @TruffleLanguage.Registration(id = RecreateShadowStackTestLanguage.ID, mimeType = RecreateShadowStackTestLanguage.MIME_TYPE, name = "RecreateShadowStackTestLanguage", version = "0.1")
+    @ProvidedTags({StandardTags.StatementTag.class, StandardTags.RootTag.class})
+    public static class RecreateShadowStackTestLanguage extends TruffleLanguage<Integer> {
+
+        public static final String ID = "RecreateShadowStackTestLanguage";
+        public static final String MIME_TYPE = "RecreateShadowStackTestLanguageMime";
+
+        @Override
+        protected Integer createContext(Env env) {
+            return 0;
+        }
+
+        @Override
+        protected boolean isObjectOfLanguage(Object object) {
+            return false;
+        }
+
+        @Override
+        protected boolean isThreadAccessAllowed(Thread thread, boolean singleThreaded) {
+            return true;
+        }
+
+        @Override
+        protected CallTarget parse(ParsingRequest request) throws Exception {
+            com.oracle.truffle.api.source.Source source = request.getSource();
+            SourceSection statement = null;
+            SourceSection root = null;
+            Statement startSamplerChild = null;
+
+            // Case when we want to test statements and roots
+            if (source.getCharacters().toString().equals("Statement Root")) {
+                statement = source.createSection(0, 9);
+                root = source.createSection(10, 4);
+                startSamplerChild = new Statement(statement, new SleepNode());
+            }
+
+            // Case when we want to test roots only
+            if (source.getCharacters().toString().equals("Root Root")) {
+                statement = source.createUnavailableSection();
+                root = source.createSection(0, 4);
+                RootCallTarget sleepTarget = Truffle.getRuntime().createCallTarget(new SRootNode(this, new Root(root, new SleepNode())));
+                startSamplerChild = new Statement(statement, new CallNode(Truffle.getRuntime().createDirectCallNode(sleepTarget)));
+            }
+
+            RootCallTarget innerTarget = Truffle.getRuntime().createCallTarget(
+                            new SRootNode(this, new Root(root,
+                                            new Wrapper(new Statement(statement, new Wrapper(new Statement(statement, new Wrapper(new StartSamplerNode(sampler, startSamplerChild)))))))));
+            DirectCallNode directCallNode = Truffle.getRuntime().createDirectCallNode(innerTarget);
+            return Truffle.getRuntime().createCallTarget(
+                            new SRootNode(this, new Root(root, new Wrapper(new Statement(statement, new Wrapper(new Statement(statement, new Wrapper(new CallNode(directCallNode)))))))));
+
+        }
+
+        abstract static class SamplerTestNode extends Node {
+            public abstract Object execute(VirtualFrame frame);
+        }
+
+        static class SleepNode extends SamplerTestNode {
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    Assert.fail("Thread interrupted");
+                }
+                return 5;
+            }
+        }
+
+        static class Wrapper extends SamplerTestNode {
+            @Child SamplerTestNode node;
+
+            Wrapper(SamplerTestNode node) {
+                this.node = node;
+                adoptChildren();
+            }
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                return node.execute(frame);
+            }
+        }
+
+        @GenerateWrapper
+        static class Statement extends SamplerTestNode implements InstrumentableNode {
+
+            @Child SamplerTestNode node;
+            SourceSection sourceSection;
+
+            Statement(Statement other) {
+                node = other.node;
+                sourceSection = other.sourceSection;
+                adoptChildren();
+            }
+
+            Statement(SourceSection sourceSection, SamplerTestNode node) {
+                this.sourceSection = sourceSection;
+                this.node = node;
+            }
+
+            @Override
+            public SourceSection getSourceSection() {
+                return sourceSection;
+            }
+
+            @Override
+            public boolean isInstrumentable() {
+                return true;
+            }
+
+            @Override
+            public WrapperNode createWrapper(ProbeNode probe) {
+                return new StatementWrapper(this, this, probe);
+            }
+
+            @Override
+            public boolean hasTag(Class<? extends Tag> tag) {
+                return tag == StandardTags.StatementTag.class;
+            }
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                return node.execute(frame);
+            }
+        }
+
+        @GenerateWrapper
+        static class Root extends SamplerTestNode implements InstrumentableNode {
+
+            @Child SamplerTestNode node;
+            SourceSection sourceSection;
+
+            Root(Root other) {
+                node = other.node;
+                sourceSection = other.sourceSection;
+                adoptChildren();
+            }
+
+            @Override
+            public SourceSection getSourceSection() {
+                return sourceSection;
+            }
+
+            Root(SourceSection sourceSection, SamplerTestNode node) {
+                this.sourceSection = sourceSection;
+                this.node = node;
+            }
+
+            @Override
+            public boolean isInstrumentable() {
+                return true;
+            }
+
+            @Override
+            public WrapperNode createWrapper(ProbeNode probe) {
+                return new RootWrapper(this, this, probe);
+            }
+
+            @Override
+            public boolean hasTag(Class<? extends Tag> tag) {
+                return tag == StandardTags.RootTag.class;
+            }
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                return node.execute(frame);
+            }
+        }
+
+        static class SRootNode extends RootNode {
+
+            private final RecreateShadowStackTestLanguage language;
+            @Child SamplerTestNode child;
+
+            SRootNode(RecreateShadowStackTestLanguage language, SamplerTestNode child) {
+                super(language);
+                this.language = language;
+                this.child = child;
+                adoptChildren();
+            }
+
+            protected SRootNode(SRootNode other) {
+                super(other.language);
+                language = other.language;
+                child = other.child;
+                adoptChildren();
+            }
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                return child.execute(frame);
+            }
+        }
+
+        static class CallNode extends SamplerTestNode {
+            @Child DirectCallNode callNode;
+
+            CallNode(DirectCallNode callNode) {
+                this.callNode = callNode;
+                adoptChildren();
+            }
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                return callNode.call(new Object[]{});
+            }
+        }
+
+        static class StartSamplerNode extends SamplerTestNode {
+            CPUSampler sampler;
+            @Child SamplerTestNode child;
+
+            StartSamplerNode(CPUSampler sampler, SamplerTestNode child) {
+                this.sampler = sampler;
+                this.child = child;
+                adoptChildren();
+            }
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                Assert.assertTrue("Found roots before enabling sampler", sampler.getRootNodes().isEmpty());
+                sampler.setCollecting(true);
+                return child.execute(frame);
+            }
+        }
+    }
+
+    @Test
+    public void testCorrectInitShadowStackStatements() {
+        sampler.setMode(CPUSampler.Mode.STATEMENTS);
+        sampler.setFilter(NO_INTERNAL_STATEMENT_TAG_FILTER);
+        Source test = Source.newBuilder(RecreateShadowStackTestLanguage.ID, "Statement Root", "test").buildLiteral();
+        context.eval(test);
+        Collection<ProfilerNode<CPUSampler.Payload>> rootNodes = sampler.getRootNodes();
+
+        ProfilerNode<CPUSampler.Payload> current = rootNodes.iterator().next();
+        Assert.assertEquals("Stack not correct", "Root", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Statement", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Statement", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Root", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Statement", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Statement", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Statement", current.getSourceSection().getCharacters().toString());
+        Assert.assertFalse("Stack too deep", current.getChildren().iterator().hasNext());
+    }
+
+    @Test
+    public void testCorrectInitShadowStackRoots() {
+        sampler.setMode(CPUSampler.Mode.ROOTS);
+        sampler.setFilter(NO_INTERNAL_ROOT_TAG_FILTER);
+        Source test = Source.newBuilder(RecreateShadowStackTestLanguage.ID, "Root Root", "test").buildLiteral();
+        context.eval(test);
+        Collection<ProfilerNode<CPUSampler.Payload>> rootNodes = sampler.getRootNodes();
+
+        ProfilerNode<CPUSampler.Payload> current = rootNodes.iterator().next();
+        Assert.assertEquals("Stack not correct", "Root", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Root", current.getSourceSection().getCharacters().toString());
+        Assert.assertTrue("Stack not deep enough", current.getChildren().iterator().hasNext());
+
+        current = current.getChildren().iterator().next();
+        Assert.assertEquals("Stack not correct", "Root", current.getSourceSection().getCharacters().toString());
+        Assert.assertFalse("Stack too deep", current.getChildren().iterator().hasNext());
     }
 }
