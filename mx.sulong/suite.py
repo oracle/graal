6d0f8114490ebc4e35013abf1b241991dc1947d5
Roland Schatz <roland.schatz@oracle.com>
2018-07-12 16:13:19 +0200

    Update truffle import.

--- mx.sulong/suite.py
+++ mx.sulong/suite.py
@@ -8,7 +8,7 @@ suite = {
       {
         "name" : "truffle",
         "subdir" : True,
-        "version" : "a2267424f072edbef5916cd519a08a0514658bdd",
+        "version" : "dc37c9a244fc97a92b7b37f716e484cb4b7ee5ed",
         "urls" : [
           {"url" : "https://github.com/graalvm/graal", "kind" : "git"},
           {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
