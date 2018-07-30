37fe78365731fb8d9ea700fa8d1412e2e77ea122
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2018-07-30 19:57:47 +0200

    [GR-11047] Temporary fix for the root dir of the standalone distributions.

--- vm/mx.vm/mx_vm.py
+++ vm/mx.vm/mx_vm.py
@@ -1181,11 +1181,13 @@ class GraalVmStandaloneComponent(mx.LayoutTARDistribution):  # pylint: disable=t
         if _get_svm_support().is_supported() and _get_launcher_configs(installable.main_component):
             other_comp_names += [c.short_name for c in mx_sdk.graalvm_components() if c.dir_name == 'svm']
 
-        main_comp_name = installable.main_component.suite.name
-        version = _suite.release_version()
-
-        name = '_'.join([main_comp_name, 'standalone'] + other_comp_names).upper().replace('-', '_')
-        base_dir = './{comp_name}-{version}-{os}-{arch}/'.format(comp_name=main_comp_name, version=version, os=get_graalvm_os(), arch=mx.get_arch()).lower().replace('_', '-')
+        name = '_'.join([installable.main_component.dir_name, 'standalone'] + other_comp_names).upper().replace('-', '_')
+        base_dir = './{comp_name}-{version}-{os}-{arch}/'.format(
+                comp_name=installable.main_component.suite.name,
+                version=_suite.release_version(),
+                os=get_graalvm_os(),
+                arch=mx.get_arch()
+            ).lower().replace('_', '-')
         layout = {}
 
         def is_jar_distribution(val):
