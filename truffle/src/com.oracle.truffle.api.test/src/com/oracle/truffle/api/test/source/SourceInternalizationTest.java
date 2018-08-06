89417a878346271895761d4f222aba08b39514a9
Christian Humer <christian.humer@oracle.com>
2018-08-06 19:09:27 +0200

    Tune source internalization and polyglot caching test speed. Disable tests entirely on fastdebug configurations.

--- truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/source/SourceInternalizationTest.java
+++ truffle/src/com.oracle.truffle.api.test/src/com/oracle/truffle/api/test/source/SourceInternalizationTest.java
@@ -48,6 +48,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 
 import com.oracle.truffle.api.source.Source;
@@ -173,6 +174,8 @@ public class SourceInternalizationTest {
 
     @Test
     public void testSourceInterning() {
+        Assume.assumeFalse("This test is too slow in fastdebug.", System.getProperty("java.vm.version").contains("fastdebug"));
+
         byte[] bytes = new byte[16 * 1024 * 1024];
         byte byteValue = (byte) 'a';
         Arrays.fill(bytes, byteValue);
@@ -180,7 +183,7 @@ public class SourceInternalizationTest {
 
         ReferenceQueue<Object> queue = new ReferenceQueue<>();
         List<WeakReference<Object>> sources = new ArrayList<>();
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 5; i++) {
             sources.add(new WeakReference<>(createTestSource(testString, i), queue));
             System.gc();
         }
