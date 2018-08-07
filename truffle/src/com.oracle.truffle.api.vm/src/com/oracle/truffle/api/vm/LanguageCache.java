9c2518d93088e56321bdc713fbcda259f067374f
Boris Spasojevic <boris.spasojevic@oracle.com>
2018-08-07 15:35:52 +0200

    Continue with language home as null.

--- truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/LanguageCache.java
+++ truffle/src/com.oracle.truffle.api.vm/src/com/oracle/truffle/api/vm/LanguageCache.java
@@ -218,14 +218,13 @@ final class LanguageCache implements Comparable<LanguageCache> {
                      * call `URI.toASCIIString()` all reserved and non-ASCII characters are
                      * percent-quoted.
                      */
-                    Path path;
                     try {
+                        Path path;
                         path = Paths.get(((JarURLConnection) connection).getJarFileURL().toURI());
+                        languageHome = path.getParent().toString();
                     } catch (URISyntaxException e) {
-                        throw new RuntimeException(e);
+                        assert false : "Could not resolve path.";
                     }
-
-                    languageHome = path.getParent().toString();
                 }
                 list.add(new LanguageCache(id, prefix, p, loader, languageHome));
             }
