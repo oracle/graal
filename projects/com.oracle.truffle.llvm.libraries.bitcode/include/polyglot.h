53fb6c4a4d9b3fdfc79181a0f559e3ed6957061e
Roland Schatz <roland.schatz@oracle.com>
2018-08-13 17:54:51 +0200

    Fix type in polyglot.h documentation.

--- projects/com.oracle.truffle.llvm.libraries.bitcode/include/polyglot.h
+++ projects/com.oracle.truffle.llvm.libraries.bitcode/include/polyglot.h
@@ -676,37 +676,37 @@ __POLYGLOT_DECLARE_GENERIC_ARRAY(double, double)
 /**
  * Get a polyglot typeid for the primitive bool type.
  */
-static void *polyglot_boolean_typeid();
+static polyglot_typeid polyglot_boolean_typeid();
 
 /**
  * Get a polyglot typeid for the primitive int8_t type.
  */
-static void *polyglot_int8_typeid();
+static polyglot_typeid polyglot_int8_typeid();
 
 /**
  * Get a polyglot typeid for the primitive int16_t type.
  */
-static void *polyglot_int16_typeid();
+static polyglot_typeid polyglot_int16_typeid();
 
 /**
  * Get a polyglot typeid for the primitive int32_t type.
  */
-static void *polyglot_int32_typeid();
+static polyglot_typeid polyglot_int32_typeid();
 
 /**
  * Get a polyglot typeid for the primitive int64_t type.
  */
-static void *polyglot_int64_typeid();
+static polyglot_typeid polyglot_int64_typeid();
 
 /**
  * Get a polyglot typeid for the primitive float type.
  */
-static void *polyglot_float_typeid();
+static polyglot_typeid polyglot_float_typeid();
 
 /**
  * Get a polyglot typeid for the primitive double type.
  */
-static void *polyglot_double_typeid();
+static polyglot_typeid polyglot_double_typeid();
 
 /**
  * Converts a polyglot value to an integer array.
