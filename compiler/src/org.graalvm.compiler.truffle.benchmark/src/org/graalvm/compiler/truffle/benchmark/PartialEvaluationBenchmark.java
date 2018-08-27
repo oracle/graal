065e443a9cfcf14db71ce1d00e6152e5b738ab55
Florian Latifi <florian.latifi@jku.at>
2018-08-27 16:07:43 +0200

    Create objects for partial evaluation within benchmark method
    This commit moves the creation of objects needed for partial evaluation
    into the benchmark methods in order to mimmic the behavior of the
    Truffle compiler.

--- compiler/src/org.graalvm.compiler.truffle.benchmark/src/org/graalvm/compiler/truffle/benchmark/PartialEvaluationBenchmark.java
+++ compiler/src/org.graalvm.compiler.truffle.benchmark/src/org/graalvm/compiler/truffle/benchmark/PartialEvaluationBenchmark.java
@@ -22,10 +22,9 @@
  */
 package org.graalvm.compiler.truffle.benchmark;
 
-import java.util.Arrays;
-
 import org.graalvm.compiler.core.common.CompilationIdentifier;
 import org.graalvm.compiler.debug.DebugContext;
+import org.graalvm.compiler.debug.DebugHandlersFactory;
 import org.graalvm.compiler.nodes.Cancellable;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.StructuredGraph.AllowAssumptions;
@@ -34,15 +33,16 @@ import org.graalvm.compiler.phases.OptimisticOptimizations;
 import org.graalvm.compiler.phases.PhaseSuite;
 import org.graalvm.compiler.phases.tiers.HighTierContext;
 import org.graalvm.compiler.phases.tiers.PhaseContext;
+import org.graalvm.compiler.phases.util.Providers;
 import org.graalvm.compiler.truffle.common.TruffleCompilerOptions;
 import org.graalvm.compiler.truffle.common.TruffleCompilerRuntime;
+import org.graalvm.compiler.truffle.common.TruffleDebugJavaMethod;
 import org.graalvm.compiler.truffle.compiler.PartialEvaluator;
 import org.graalvm.compiler.truffle.compiler.TruffleCompilerImpl;
 import org.graalvm.compiler.truffle.runtime.CancellableCompileTask;
 import org.graalvm.compiler.truffle.runtime.DefaultInliningPolicy;
 import org.graalvm.compiler.truffle.runtime.OptimizedCallTarget;
 import org.graalvm.compiler.truffle.runtime.TruffleInlining;
-import org.graalvm.compiler.truffle.runtime.TruffleTreeDebugHandlersFactory;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
@@ -50,7 +50,6 @@ import org.openjdk.jmh.annotations.State;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.nodes.RootNode;
 
-import jdk.vm.ci.meta.ResolvedJavaMethod;
 import jdk.vm.ci.meta.SpeculationLog;
 
 @State(Scope.Benchmark)
@@ -58,39 +57,24 @@ public abstract class PartialEvaluationBenchmark {
 
     private static final int AST_WARMUPS = 5;
 
-    private final TruffleCompilerImpl compiler = (TruffleCompilerImpl) TruffleCompilerRuntime.getRuntime().getTruffleCompiler();
-    private final PartialEvaluator partialEvaluator = compiler.getPartialEvaluator();
+    private final TruffleCompilerImpl compiler;
+    private final PartialEvaluator partialEvaluator;
 
     private final OptimizedCallTarget callTarget;
     private final Object[] callArguments;
     private final OptionValues optionValues;
     private final DebugContext debugContext;
-    private final TruffleInlining truffleInlining;
-    private final CompilationIdentifier identifier;
-    private final SpeculationLog speculationLog;
-    private final Cancellable cancellable;
-
-    private final String name;
-    private final ResolvedJavaMethod method;
-    private final PhaseContext phaseContext;
-    private final HighTierContext tierContext;
 
     public PartialEvaluationBenchmark() {
+        compiler = (TruffleCompilerImpl) TruffleCompilerRuntime.getRuntime().getTruffleCompiler();
+        partialEvaluator = compiler.getPartialEvaluator();
+
         callTarget = (OptimizedCallTarget) Truffle.getRuntime().createCallTarget(rootNode());
         callArguments = callArguments();
         optionValues = TruffleCompilerOptions.getOptions();
-        debugContext = DebugContext.create(optionValues, Arrays.asList(new TruffleTreeDebugHandlersFactory()));
-        truffleInlining = new TruffleInlining(callTarget, new DefaultInliningPolicy());
-        identifier = compiler.getCompilationIdentifier(callTarget);
-        speculationLog = callTarget.getSpeculationLog();
-        cancellable = new CancellableCompileTask();
+        debugContext = DebugContext.create(optionValues, DebugHandlersFactory.LOADER);
 
-        name = callTarget.getName();
-        method = compiler.getPartialEvaluator().rootForCallTarget(callTarget);
-        phaseContext = new PhaseContext(compiler.getPartialEvaluator().getProviders());
-        tierContext = new HighTierContext(compiler.getPartialEvaluator().getProviders(), new PhaseSuite<HighTierContext>(), OptimisticOptimizations.NONE);
-
-        // run call target so that all classes are loaded and initialized
+        // run call target so that all classes are loaded and AST is specialized
         for (int i = 0; i < AST_WARMUPS; i++) {
             callTarget.call(callArguments);
         }
@@ -108,26 +92,43 @@ public abstract class PartialEvaluationBenchmark {
     }
 
     @Benchmark
+    @SuppressWarnings("try")
     public StructuredGraph createGraph() {
-        // @formatter:off
-        return partialEvaluator.createGraph(
-            debugContext, callTarget, truffleInlining,
-            AllowAssumptions.YES, identifier, speculationLog, cancellable);
-        // @formatter:on
+        try (DebugContext.Scope s = debugContext.scope("TruffleCompilation", new TruffleDebugJavaMethod(callTarget))) {
+            TruffleInlining truffleInlining = new TruffleInlining(callTarget, new DefaultInliningPolicy());
+            CompilationIdentifier identifier = compiler.getCompilationIdentifier(callTarget);
+            SpeculationLog speculationLog = callTarget.getSpeculationLog();
+            Cancellable cancellable = new CancellableCompileTask();
+
+            return partialEvaluator.createGraph(
+                            debugContext, callTarget, truffleInlining,
+                            AllowAssumptions.YES, identifier, speculationLog, cancellable);
+        } catch (Throwable e) {
+            throw debugContext.handle(e);
+        }
     }
 
     @Benchmark
     public void fastPartialEvaluate() {
+        TruffleInlining truffleInlining = new TruffleInlining(callTarget, new DefaultInliningPolicy());
+        CompilationIdentifier identifier = compiler.getCompilationIdentifier(callTarget);
+        SpeculationLog speculationLog = callTarget.getSpeculationLog();
+        Cancellable cancellable = new CancellableCompileTask();
+        Providers providers = partialEvaluator.getProviders();
+        PhaseContext phaseContext = new PhaseContext(providers);
+        HighTierContext tierContext = new HighTierContext(providers, new PhaseSuite<HighTierContext>(), OptimisticOptimizations.NONE);
+
         // @formatter:off
         StructuredGraph structuredGraph =
             new StructuredGraph.Builder(optionValues, debugContext, AllowAssumptions.YES)
-                               .name(name)
-                               .method(method)
+                               .name(callTarget.getName())
+                               .method(partialEvaluator.rootForCallTarget(callTarget))
                                .speculationLog(speculationLog)
                                .compilationId(identifier)
                                .cancellable(cancellable)
                                .build();
         // @formatter:on
+
         partialEvaluator.fastPartialEvaluation(callTarget, truffleInlining, structuredGraph, phaseContext, tierContext);
     }
 
