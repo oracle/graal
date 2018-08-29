55449831f568af53338ca6ff271768a4f84098ad
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2018-08-29 11:52:10 +0200

    Update VisualVM import.
    (cherry picked from commit f61f3e9768e63b31efc9b6903df8f464028411fd)

--- tools/mx.tools/suite.py
+++ tools/mx.tools/suite.py
@@ -115,21 +115,21 @@ suite = {
           "sha1" : "8819cea8bfe22c9c63f55465e296b3855ea41786",
         },
         "VISUALVM_COMMON" : {
-            "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/visualvm-597.tar.gz"],
-            "sha1" : "b2144b2db9514868fb198d74f5bb8f81016fdf42",
+            "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/visualvm-601.tar.gz"],
+            "sha1" : "daa78f01bf682b5dda767ecdb52cee505855c63a",
         },
         "VISUALVM_PLATFORM_SPECIFIC" : {
             "os_arch" : {
                 "linux" : {
                     "amd64" : {
-                        "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/visualvm-597-linux-amd64.tar.gz"],
-                        "sha1" : "a9de5838e8652bb07deab1945311bc5ffaf02822",
+                        "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/visualvm-601-linux-amd64.tar.gz"],
+                        "sha1" : "c57e671b1db86c01e6cd4838736facacd97c0ede",
                     }
                 },
                 "darwin" : {
                     "amd64" : {
-                        "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/visualvm-597-macosx-x86_64.tar.gz"],
-                        "sha1" : "12690cfad6e1b042db17a15e1b926e43da82ed18",
+                        "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/visualvm-601-macosx-x86_64.tar.gz"],
+                        "sha1" : "b9374e74ce0cd649980fed79c5112b10d9301c1b",
                     }
                 },
             }
