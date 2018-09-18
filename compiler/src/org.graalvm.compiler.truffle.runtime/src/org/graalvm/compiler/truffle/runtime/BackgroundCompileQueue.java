00a98162070ee5295c534b98f0e4049f9b516ac4
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2018-09-18 21:10:25 +0200

    Tweak compilation priority policy.

--- compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/BackgroundCompileQueue.java
+++ compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/BackgroundCompileQueue.java
@@ -25,35 +25,45 @@
 package org.graalvm.compiler.truffle.runtime;
 
 import org.graalvm.compiler.core.CompilerThreadFactory;
+import org.graalvm.compiler.debug.TTY;
 import org.graalvm.compiler.nodes.Cancellable;
 import org.graalvm.compiler.options.OptionValues;
 import org.graalvm.compiler.truffle.common.TruffleCompilerOptions;
 
 import java.lang.ref.WeakReference;
+import java.util.Comparator;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.RunnableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.graalvm.compiler.truffle.common.TruffleCompilerOptions.TruffleCompilerThreads;
+import static org.graalvm.compiler.truffle.common.TruffleCompilerOptions.TruffleLowGradeCompilation;
 import static org.graalvm.compiler.truffle.common.TruffleCompilerOptions.getOptions;
 import static org.graalvm.compiler.truffle.common.TruffleCompilerOptions.overrideOptions;
 
 public class BackgroundCompileQueue {
+    private final AtomicLong idCounter;
     private final ExecutorService compilationExecutorService;
 
-    public static class Request implements Runnable {
+    public class Request implements Runnable, Comparable<Request> {
+        private final long id;
         private final GraalTruffleRuntime runtime;
         private final OptionValues optionOverrides;
         private final WeakReference<OptimizedCallTarget> weakCallTarget;
         private final Cancellable cancellable;
+        private final boolean isLowGrade;
 
         public Request(GraalTruffleRuntime runtime, OptionValues optionOverrides, OptimizedCallTarget callTarget, Cancellable cancellable) {
+            this.id = idCounter.getAndIncrement();
             this.runtime = runtime;
             this.optionOverrides = optionOverrides;
             this.weakCallTarget = new WeakReference<>(callTarget);
             this.cancellable = cancellable;
+            this.isLowGrade = optionOverrides != null && TruffleLowGradeCompilation.getValue(optionOverrides);
         }
 
         @SuppressWarnings("try")
@@ -62,12 +72,11 @@ public class BackgroundCompileQueue {
             OptimizedCallTarget callTarget = weakCallTarget.get();
             try (TruffleCompilerOptions.TruffleOptionsOverrideScope scope = optionOverrides != null ? overrideOptions(optionOverrides.getMap()) : null) {
                 // if (!TruffleLowGradeCompilation.getValue(getOptions())) {
-                //     TTY.println("submitted htc: " + optimizedCallTarget + ", " + callTarget);
+                //     TTY.println("running htc: " + callTarget);
+                // } else {
+                //     TTY.println("running ltc: " + callTarget);
                 // }
                 if (callTarget != null) {
-                    // if (!TruffleLowGradeCompilation.getValue(getOptions())) {
-                    //     TTY.println("ehm.. if: " + optimizedCallTarget + ", " + callTarget + ", " + optionOverrides);
-                    // }
                     OptionValues options = getOptions();
                     runtime.doCompile(options, callTarget, cancellable);
                 }
@@ -77,11 +86,48 @@ public class BackgroundCompileQueue {
                 }
             }
         }
+
+        @Override
+        public int compareTo(Request that) {
+            if (this.isLowGrade != that.isLowGrade) {
+                return this.isLowGrade ? -1 : 1;
+            }
+            return (int) (this.id - that.id);
+        }
+
+        @Override
+        public String toString() {
+            return "Request(lo: " + isLowGrade + ", id: " + id + ", " + weakCallTarget + ")";
+        }
+    }
+
+    public class RequestFutureTask<V> extends FutureTask<V> implements Comparable<Runnable> {
+        private final Request request;
+
+        public RequestFutureTask(Runnable runnable, V result) {
+            super(runnable, result);
+            this.request = (Request) runnable;
+        }
+
+        @Override
+        public int compareTo(Runnable that) {
+            if (that instanceof RequestFutureTask) {
+                return this.request.compareTo(((RequestFutureTask<?>) that).request);
+            } else {
+                return this.request.compareTo((Request) that);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Future(" + request + ")";
+        }
     }
 
     public BackgroundCompileQueue() {
-        CompilerThreadFactory factory = new CompilerThreadFactory("TruffleCompilerThread");
+        this.idCounter = new AtomicLong();
 
+        CompilerThreadFactory factory = new CompilerThreadFactory("TruffleCompilerThread");
         int selectedProcessors = TruffleCompilerOptions.getValue(TruffleCompilerThreads);
         if (selectedProcessors == 0) {
             // No manual selection made, check how many processors are available.
@@ -91,11 +137,16 @@ public class BackgroundCompileQueue {
             }
         }
         selectedProcessors = Math.max(1, selectedProcessors);
-        compilationExecutorService = Executors.newFixedThreadPool(selectedProcessors, factory);
+        this.compilationExecutorService = new ThreadPoolExecutor(selectedProcessors, selectedProcessors, 0, TimeUnit.MILLISECONDS,
+                        new PriorityBlockingQueue<>(), factory) {
+            @Override
+            protected <T> RunnableFuture<T> newTaskFor(Runnable runnable, T value) {
+                return new RequestFutureTask<>(runnable, value);
+            }
+        };
     }
 
     public CancellableCompileTask submitCompilationRequest(GraalTruffleRuntime runtime, OptimizedCallTarget optimizedCallTarget) {
-        // TODO
         final OptionValues optionOverrides = TruffleCompilerOptions.getCurrentOptionOverrides();
         CancellableCompileTask cancellable = new CancellableCompileTask();
         // try (TruffleOptionsOverrideScope scope = optionOverrides != null ? overrideOptions(optionOverrides.getMap()) : null) {
@@ -103,14 +154,14 @@ public class BackgroundCompileQueue {
         //         TTY.println("submitting htc...: " + optimizedCallTarget);
         //     }
         // }
-        cancellable.setFuture(compilationExecutorService.submit(new Request(runtime, optionOverrides, optimizedCallTarget, cancellable)));
-        // task and future must never diverge from each other
+        Request request = new Request(runtime, optionOverrides, optimizedCallTarget, cancellable);
+        cancellable.setFuture(compilationExecutorService.submit(request));
+        // Task and future must never diverge from each other.
         assert cancellable.future != null;
         return cancellable;
     }
 
     public int getQueueSize() {
-        // TODO
         if (compilationExecutorService instanceof ThreadPoolExecutor) {
             return ((ThreadPoolExecutor) compilationExecutorService).getQueue().size();
         } else {
@@ -119,7 +170,6 @@ public class BackgroundCompileQueue {
     }
 
     public void shutdownAndAwaitTermination(long timeout) {
-        // TODO
         compilationExecutorService.shutdownNow();
         try {
             compilationExecutorService.awaitTermination(timeout, TimeUnit.MILLISECONDS);
--- compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCompilationProfile.java
+++ compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/OptimizedCompilationProfile.java
@@ -39,6 +39,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import org.graalvm.compiler.debug.TTY;
 import org.graalvm.compiler.truffle.common.TruffleCompilerOptions;
 import org.graalvm.options.OptionValues;
 
@@ -302,7 +303,6 @@ public class OptimizedCompilationProfile {
         ensureProfiling(1, replaceBackoff);
     }
 
-    private Random random = new Random();
     @CompilerDirectives.TruffleBoundary
     final boolean lowGradeCall(OptimizedCallTarget callTarget) {
         int callCount = ++lowGradeCallCount;
