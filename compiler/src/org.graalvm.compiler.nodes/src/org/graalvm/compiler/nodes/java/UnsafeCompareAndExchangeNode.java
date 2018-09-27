a14855752b89a1bcce32c146c9d5b678a4705e3a
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2018-09-27 11:57:54 +0200

    Make Unsafe.compareAndExchange virtualizable

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/UnsafeCompareAndExchangeNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/UnsafeCompareAndExchangeNode.java
@@ -28,12 +28,15 @@ import jdk.vm.ci.meta.JavaKind;
 import org.graalvm.compiler.core.common.type.Stamp;
 import org.graalvm.compiler.graph.NodeClass;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
+import org.graalvm.compiler.nodes.LogicNode;
 import org.graalvm.compiler.nodes.NodeView;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.memory.AbstractMemoryCheckpoint;
 import org.graalvm.compiler.nodes.memory.MemoryCheckpoint;
 import org.graalvm.compiler.nodes.spi.Lowerable;
 import org.graalvm.compiler.nodes.spi.LoweringTool;
+import org.graalvm.compiler.nodes.spi.Virtualizable;
+import org.graalvm.compiler.nodes.spi.VirtualizerTool;
 import org.graalvm.word.LocationIdentity;
 
 import static org.graalvm.compiler.nodeinfo.InputType.Memory;
@@ -45,26 +48,13 @@ import static org.graalvm.compiler.nodeinfo.NodeSize.SIZE_8;
  * Represents an atomic compare-and-swap operation. The result is the current value of the memory
  * location that was compared.
  */
-@NodeInfo(allowedUsageTypes = {Value, Memory}, cycles = CYCLES_8, size = SIZE_8)
-public final class UnsafeCompareAndExchangeNode extends AbstractMemoryCheckpoint implements Lowerable, MemoryCheckpoint.Single {
+@NodeInfo
+public final class UnsafeCompareAndExchangeNode extends AbstractUnsafeCompareAndSwapNode {
 
     public static final NodeClass<UnsafeCompareAndExchangeNode> TYPE = NodeClass.create(UnsafeCompareAndExchangeNode.class);
-    @Input ValueNode object;
-    @Input ValueNode offset;
-    @Input ValueNode expected;
-    @Input ValueNode newValue;
-
-    private final JavaKind valueKind;
-    private final LocationIdentity locationIdentity;
 
     public UnsafeCompareAndExchangeNode(ValueNode object, ValueNode offset, ValueNode expected, ValueNode newValue, JavaKind valueKind, LocationIdentity locationIdentity) {
-        super(TYPE, meetInputs(expected.stamp(NodeView.DEFAULT), newValue.stamp(NodeView.DEFAULT)));
-        this.object = object;
-        this.offset = offset;
-        this.expected = expected;
-        this.newValue = newValue;
-        this.valueKind = valueKind;
-        this.locationIdentity = locationIdentity;
+        super(TYPE, meetInputs(expected.stamp(NodeView.DEFAULT), newValue.stamp(NodeView.DEFAULT)), object, offset, expected, newValue, valueKind, locationIdentity);
     }
 
     private static Stamp meetInputs(Stamp expected, Stamp newValue) {
@@ -72,33 +62,8 @@ public final class UnsafeCompareAndExchangeNode extends AbstractMemoryCheckpoint
         return expected.unrestricted().meet(newValue.unrestricted());
     }
 
-    public ValueNode object() {
-        return object;
-    }
-
-    public ValueNode offset() {
-        return offset;
-    }
-
-    public ValueNode expected() {
-        return expected;
-    }
-
-    public ValueNode newValue() {
-        return newValue;
-    }
-
-    public JavaKind getValueKind() {
-        return valueKind;
-    }
-
     @Override
-    public LocationIdentity getLocationIdentity() {
-        return locationIdentity;
-    }
-
-    @Override
-    public void lower(LoweringTool tool) {
-        tool.getLowerer().lower(this, tool);
+    protected void finishVirtualize(VirtualizerTool tool, LogicNode equalsNode, ValueNode fieldValue) {
+        tool.replaceWith(fieldValue);
     }
 }
--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/UnsafeCompareAndSwapNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/java/UnsafeCompareAndSwapNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,162 +24,38 @@
  */
 package org.graalvm.compiler.nodes.java;
 
-import static org.graalvm.compiler.core.common.calc.CanonicalCondition.EQ;
-import static org.graalvm.compiler.debug.DebugContext.DETAILED_LEVEL;
-import static org.graalvm.compiler.nodeinfo.InputType.Memory;
-import static org.graalvm.compiler.nodeinfo.InputType.Value;
-import static org.graalvm.compiler.nodeinfo.NodeCycles.CYCLES_8;
-import static org.graalvm.compiler.nodeinfo.NodeSize.SIZE_8;
-
 import org.graalvm.compiler.core.common.type.StampFactory;
 import org.graalvm.compiler.graph.NodeClass;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
 import org.graalvm.compiler.nodes.ConstantNode;
-import org.graalvm.compiler.nodes.LogicConstantNode;
 import org.graalvm.compiler.nodes.LogicNode;
 import org.graalvm.compiler.nodes.NodeView;
 import org.graalvm.compiler.nodes.ValueNode;
-import org.graalvm.compiler.nodes.calc.CompareNode;
 import org.graalvm.compiler.nodes.calc.ConditionalNode;
-import org.graalvm.compiler.nodes.calc.ObjectEqualsNode;
-import org.graalvm.compiler.nodes.memory.AbstractMemoryCheckpoint;
-import org.graalvm.compiler.nodes.memory.MemoryCheckpoint;
-import org.graalvm.compiler.nodes.spi.Lowerable;
-import org.graalvm.compiler.nodes.spi.LoweringTool;
-import org.graalvm.compiler.nodes.spi.Virtualizable;
 import org.graalvm.compiler.nodes.spi.VirtualizerTool;
-import org.graalvm.compiler.nodes.virtual.VirtualArrayNode;
-import org.graalvm.compiler.nodes.virtual.VirtualInstanceNode;
-import org.graalvm.compiler.nodes.virtual.VirtualObjectNode;
 import org.graalvm.word.LocationIdentity;
 
 import jdk.vm.ci.meta.JavaKind;
-import jdk.vm.ci.meta.ResolvedJavaField;
 
 /**
  * Represents an atomic compare-and-swap operation. The result is a boolean that contains whether
  * the value matched the expected value.
  */
-@NodeInfo(allowedUsageTypes = {Value, Memory}, cycles = CYCLES_8, size = SIZE_8)
-public final class UnsafeCompareAndSwapNode extends AbstractMemoryCheckpoint implements Lowerable, MemoryCheckpoint.Single, Virtualizable {
+@NodeInfo
+public final class UnsafeCompareAndSwapNode extends AbstractUnsafeCompareAndSwapNode {
 
     public static final NodeClass<UnsafeCompareAndSwapNode> TYPE = NodeClass.create(UnsafeCompareAndSwapNode.class);
-    @Input ValueNode object;
-    @Input ValueNode offset;
-    @Input ValueNode expected;
-    @Input ValueNode newValue;
-
-    private final JavaKind valueKind;
-    private final LocationIdentity locationIdentity;
 
     public UnsafeCompareAndSwapNode(ValueNode object, ValueNode offset, ValueNode expected, ValueNode newValue, JavaKind valueKind, LocationIdentity locationIdentity) {
-        super(TYPE, StampFactory.forKind(JavaKind.Boolean.getStackKind()));
+        super(TYPE, StampFactory.forKind(JavaKind.Boolean.getStackKind()), object, offset, expected, newValue, valueKind, locationIdentity);
         assert expected.stamp(NodeView.DEFAULT).isCompatible(newValue.stamp(NodeView.DEFAULT));
-        this.object = object;
-        this.offset = offset;
-        this.expected = expected;
-        this.newValue = newValue;
-        this.valueKind = valueKind;
-        this.locationIdentity = locationIdentity;
     }
 
-    public ValueNode object() {
-        return object;
-    }
-
-    public ValueNode offset() {
-        return offset;
-    }
-
-    public ValueNode expected() {
-        return expected;
-    }
-
-    public ValueNode newValue() {
-        return newValue;
-    }
-
-    public JavaKind getValueKind() {
-        return valueKind;
-    }
-
-    @Override
-    public LocationIdentity getLocationIdentity() {
-        return locationIdentity;
-    }
-
-    @Override
-    public void lower(LoweringTool tool) {
-        tool.getLowerer().lower(this, tool);
-    }
-
-    @Override
-    public void virtualize(VirtualizerTool tool) {
-        ValueNode offsetAlias = tool.getAlias(offset);
-        if (!offsetAlias.isJavaConstant()) {
-            return;
+    protected void finishVirtualize(VirtualizerTool tool, LogicNode equalsNode, ValueNode fieldValue) {
+        ValueNode result = ConditionalNode.create(equalsNode, ConstantNode.forBoolean(true, graph()), ConstantNode.forBoolean(false, graph()), NodeView.DEFAULT);
+        if (!result.isAlive()) {
+            tool.addNode(result);
         }
-        long constantOffset = offsetAlias.asJavaConstant().asLong();
-        ValueNode objectAlias = tool.getAlias(object);
-        int index;
-        if (objectAlias instanceof VirtualInstanceNode) {
-            VirtualInstanceNode obj = (VirtualInstanceNode) objectAlias;
-
-            ResolvedJavaField field = obj.type().findInstanceFieldWithOffset(constantOffset, expected.getStackKind());
-            if (field == null) {
-                tool.getDebug().log(DETAILED_LEVEL, "%s.virtualize() -> Unknown field", this);
-                return;
-            }
-            index = obj.fieldIndex(field);
-        } else if (objectAlias instanceof VirtualArrayNode) {
-            VirtualArrayNode array = (VirtualArrayNode) objectAlias;
-            index = array.entryIndexForOffset(tool.getMetaAccess(), constantOffset, valueKind);
-        } else {
-            return;
-        }
-        if (index < 0) {
-            tool.getDebug().log(DETAILED_LEVEL, "%s.virtualize() -> Unknown index", this);
-            return;
-        }
-        VirtualObjectNode obj = (VirtualObjectNode) objectAlias;
-        ValueNode currentValue = tool.getEntry(obj, index);
-        ValueNode expectedAlias = tool.getAlias(this.expected);
-
-        LogicNode equalsNode = null;
-        if (valueKind.isObject()) {
-            equalsNode = ObjectEqualsNode.virtualizeComparison(expectedAlias, currentValue, graph(), tool);
-        }
-        if (equalsNode == null && !(expectedAlias instanceof VirtualObjectNode) && !(currentValue instanceof VirtualObjectNode)) {
-            equalsNode = CompareNode.createCompareNode(EQ, expectedAlias, currentValue, tool.getConstantReflection(), NodeView.DEFAULT);
-        }
-        if (equalsNode == null) {
-            tool.getDebug().log(DETAILED_LEVEL, "%s.virtualize() -> Expected and/or current values are virtual and the comparison can not be folded", this);
-            return;
-        }
-
-        ValueNode newValueAlias = tool.getAlias(this.newValue);
-        ValueNode fieldValue;
-        if (equalsNode instanceof LogicConstantNode) {
-            fieldValue = ((LogicConstantNode) equalsNode).getValue() ? newValue : currentValue;
-        } else {
-            if (newValueAlias instanceof VirtualObjectNode) {
-                tool.getDebug().log(DETAILED_LEVEL, "%s.virtualize() -> Unknown outcome and new value is virtual", this);
-                return;
-            }
-            assert !(currentValue instanceof VirtualObjectNode);
-            fieldValue = ConditionalNode.create(equalsNode, newValueAlias, currentValue, NodeView.DEFAULT);
-        }
-        if (!tool.setVirtualEntry(obj, index, fieldValue, valueKind, constantOffset)) {
-            tool.getDebug().log(DETAILED_LEVEL, "%s.virtualize() -> Could not set virtual entry", this);
-            return;
-        }
-        tool.getDebug().log(DETAILED_LEVEL, "%s.virtualize() -> Success: virtualizing", this);
-        ValueNode result = ConditionalNode.create(equalsNode, ConstantNode.forBoolean(true), ConstantNode.forBoolean(false), NodeView.DEFAULT);
-        tool.addNode(equalsNode);
-        if (!fieldValue.isAlive()) {
-            tool.addNode(fieldValue);
-        }
-        tool.addNode(result);
         tool.replaceWith(result);
     }
 }
