bb1395bf73bd1b0746340d42e48aaf65676298a5
Daniel Stolpe <daniel.stolpe@gmx.de>
2018-10-18 18:02:26 +0200

    fix Launcher qualified name

--- mx.truffle-lsp/mx_truffle_lsp.py
+++ mx.truffle-lsp/mx_truffle_lsp.py
@@ -6,7 +6,7 @@ def lsp(args):
     print(dists)
     vm_args, server_args = mx.extract_VM_args(args, useDoubleDash=True, defaultAllVMArgs=False)
     vm_args += mx.get_runtime_jvm_args(dists)
-    vm_args.append("de.hpi.swa.trufflelsp.launcher.TruffleLSPLauncher")
+    vm_args.append("de.hpi.swa.trufflelsp.launcher.GraalLanguageServerLauncher")
     return mx.run_java(vm_args + server_args)
 
 
