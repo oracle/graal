8e3c53a1a28fa043ed20081def1d3857a620b315
Paul Wögerer <paul.woegerer@oracle.com>
2018-10-23 13:41:24 +0200

    Use HostedOptionKey<String[]> for IncludeResources option

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/ResourcesFeature.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/ResourcesFeature.java
@@ -25,12 +25,6 @@
 
 package com.oracle.svm.core.jdk;
 
-import org.graalvm.compiler.options.Option;
-
-import com.oracle.svm.core.annotate.AutomaticFeature;
-import com.oracle.svm.core.option.HostedOptionKey;
-import com.oracle.svm.core.util.UserError;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -45,49 +39,55 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.regex.Pattern;
 
+import org.graalvm.compiler.options.Option;
 import org.graalvm.compiler.options.OptionType;
 import org.graalvm.nativeimage.Feature;
+
+import com.oracle.svm.core.annotate.AutomaticFeature;
+import com.oracle.svm.core.option.HostedOptionKey;
+import com.oracle.svm.core.util.UserError;
 
 @AutomaticFeature
 public final class ResourcesFeature implements Feature {
 
     public static class Options {
         @Option(help = "Regexp to match names of resources to be included in the image.", type = OptionType.User)//
-        public static final HostedOptionKey<String> IncludeResources = new HostedOptionKey<>("");
+        public static final HostedOptionKey<String[]> IncludeResources = new HostedOptionKey<>(new String[0]);
     }
 
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
-        String regExp = Options.IncludeResources.getValue();
-        if (regExp.length() == 0) {
-            return;
-        }
+        for (String regExp : Options.IncludeResources.getValue()) {
+            if (regExp.length() == 0) {
+                return;
+            }
 
-        Pattern pattern = Pattern.compile(regExp);
+            Pattern pattern = Pattern.compile(regExp);
 
-        final Set<File> todo = new HashSet<>();
-        // Checkstyle: stop
-        final ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-        if (contextClassLoader instanceof URLClassLoader) {
-            for (URL url : ((URLClassLoader) contextClassLoader).getURLs()) {
-                try {
-                    final File file = new File(url.toURI());
-                    todo.add(file);
-                } catch (URISyntaxException | IllegalArgumentException e) {
-                    throw UserError.abort("Unable to handle imagecp element '" + url.toExternalForm() + "'. Make sure that all imagecp entries are either directories or valid jar files.");
+            final Set<File> todo = new HashSet<>();
+            // Checkstyle: stop
+            final ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+            if (contextClassLoader instanceof URLClassLoader) {
+                for (URL url : ((URLClassLoader) contextClassLoader).getURLs()) {
+                    try {
+                        final File file = new File(url.toURI());
+                        todo.add(file);
+                    } catch (URISyntaxException | IllegalArgumentException e) {
+                        throw UserError.abort("Unable to handle imagecp element '" + url.toExternalForm() + "'. Make sure that all imagecp entries are either directories or valid jar files.");
+                    }
                 }
             }
-        }
-        // Checkstyle: resume
-        for (File element : todo) {
-            try {
-                if (element.isDirectory()) {
-                    scanDirectory(element, "", pattern);
-                } else {
-                    scanJar(element, pattern);
+            // Checkstyle: resume
+            for (File element : todo) {
+                try {
+                    if (element.isDirectory()) {
+                        scanDirectory(element, "", pattern);
+                    } else {
+                        scanJar(element, pattern);
+                    }
+                } catch (IOException ex) {
+                    throw UserError.abort("Unable to handle classpath element '" + element + "'. Make sure that all classpath entries are either directories or valid jar files.");
                 }
-            } catch (IOException ex) {
-                throw UserError.abort("Unable to handle classpath element '" + element + "'. Make sure that all classpath entries are either directories or valid jar files.");
             }
         }
     }
