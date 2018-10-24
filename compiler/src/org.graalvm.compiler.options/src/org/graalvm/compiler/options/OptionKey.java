bfc191aef5332c9c2b512c0913f7ddcfbc525b77
Paul Wögerer <paul.woegerer@oracle.com>
2018-10-24 17:46:21 +0200

    More elaborate JavaDoc for OptionKey.getDelimiterRegex

--- compiler/src/org.graalvm.compiler.options/src/org/graalvm/compiler/options/OptionKey.java
+++ compiler/src/org.graalvm.compiler.options/src/org/graalvm/compiler/options/OptionKey.java
@@ -180,7 +180,10 @@ public class OptionKey<T> {
     }
 
     /**
-     * Allows customization of Options where the value describes multiple values.
+     * If the OptionKey value type is an array-type this method allows to modify the interpretation
+     * of a single-value. The returned value is used to split up the string that is passed as option
+     * argument and the individual split results are added as array members. If null is returned
+     * multiple elements for the value array can only be specified by subsequent use of the option.
      *
      * @return provides the delimiting regular expression
      */
