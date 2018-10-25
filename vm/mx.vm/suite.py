994998f2e4c8d37349d313d9f85c65f7d22cf142
Jirka Marsik <jiri.marsik@oracle.com>
2018-10-25 14:48:44 +0200

    Bump up graal-js and graal-nodejs imports in vm

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -19,7 +19,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0c778c17db32eff91b631c9a08f254556a86a952",
+                "version": "e03edeb4898836b20148f074c9143087c25cd431",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -29,7 +29,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0c778c17db32eff91b631c9a08f254556a86a952",
+                "version": "e03edeb4898836b20148f074c9143087c25cd431",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
