d22155540eac57c6c5bcf9d1b53b9c90033dfdf2
Daniel Stolpe <daniel.stolpe@gmx.de>
2018-10-26 14:46:23 +0200

    fix mx lsp command to skip test dependencies

--- mx.truffle-lsp/mx_truffle_lsp.py
+++ mx.truffle-lsp/mx_truffle_lsp.py
@@ -3,6 +3,7 @@ import mx, mx_gate, mx_subst, os
 
 def lsp(args):
     dists = [d for s in mx._suites.values() for d in s.dists if isinstance(d, mx.ClasspathDependency)]
+    dists = [d for d in dists if not d.remoteName().endswith("TEST")]
     print(dists)
     vm_args, server_args = mx.extract_VM_args(args, useDoubleDash=True, defaultAllVMArgs=False)
     vm_args += mx.get_runtime_jvm_args(dists)
