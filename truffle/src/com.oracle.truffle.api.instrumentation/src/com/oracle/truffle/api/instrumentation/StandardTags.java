e9b6faeeb47f4974f0938435eb3ac6781fa186e2
Daniel Stolpe <daniel.stolpe@gmx.de>
2018-10-29 15:47:40 +0100

    remove DeclarationTag.Kind enum

--- truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/StandardTags.java
+++ truffle/src/com.oracle.truffle.api.instrumentation/src/com/oracle/truffle/api/instrumentation/StandardTags.java
@@ -218,52 +218,6 @@ public final class StandardTags {
         public static final String KIND = "kind";
         public static final String CONTAINER = "container";
 
-        public enum Kind {
-            File(1),
-            Module(2),
-            Namespace(3),
-            Package(4),
-            Class(5),
-            Method(6),
-            Property(7),
-            Field(8),
-            Constructor(9),
-            Enum(10),
-            Interface(11),
-            Function(12),
-            Variable(13),
-            Constant(14),
-            String(15),
-            Number(16),
-            Boolean(17),
-            Array(18),
-            Object(19),
-            Key(20),
-            Null(21),
-            EnumMember(22),
-            Struct(23),
-            Event(24),
-            Operator(25),
-            TypeParameter(26);
-
-            public final int value;
-
-            Kind(int value) {
-                this.value = value;
-            }
-
-            public final int getValue() {
-                return value;
-            }
-
-            public static Kind forValue(int value) {
-                Kind[] allValues = Kind.values();
-                if (value < 1 || value > allValues.length)
-                    throw new IllegalArgumentException("Illegal enum value: " + value);
-                return allValues[value - 1];
-            }
-        }
-
         private DeclarationTag() {
             /* No instances */
         }
