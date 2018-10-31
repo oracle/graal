faeeffa3428f8ec52152f72d2dd3dd30f1305463
Doug Simon <doug.simon@oracle.com>
2018-10-31 22:23:51 +0100

    enforce default HotSpotNmethods to have a null name

--- substratevm/src/com.oracle.svm.core.graal/src/com/oracle/svm/core/graal/code/amd64/SubstrateAMD64Backend.java
+++ substratevm/src/com.oracle.svm.core.graal/src/com/oracle/svm/core/graal/code/amd64/SubstrateAMD64Backend.java
@@ -842,7 +842,11 @@ public class SubstrateAMD64Backend extends Backend {
     }
 
     @Override
-    public CompiledCode createCompiledCode(ResolvedJavaMethod method, CompilationRequest compilationRequest, CompilationResult compilationResult, OptionValues options) {
+    public CompiledCode createCompiledCode(ResolvedJavaMethod method,
+                    CompilationRequest compilationRequest,
+                    CompilationResult compilationResult,
+                    boolean isDefault,
+                    OptionValues options) {
         return new SubstrateCompiledCode(compilationResult);
     }
 
