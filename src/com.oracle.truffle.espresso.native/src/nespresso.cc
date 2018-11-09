c4c958f3e9e67b6cd5b1f7ee1169cb6372e29b42
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-11-09 14:25:24 +0100

    Implement JNI native methods.
    NewObject (in Java) and its native varargs variants
    NewDirectByteBuffer
    NewString

--- src/com.oracle.truffle.espresso.native/src/nespresso.cc
+++ src/com.oracle.truffle.espresso.native/src/nespresso.cc
@@ -2,262 +2,241 @@
 #include <ffi.h>
 #include <trufflenfi.h>
 #include <cstdlib>
+#include <cstring>
 
 #define EXPAND(...) __VA_ARGS__
 
 #define JNI_FUNCTION_LIST(V) \
-  V(GetVersion) \
-  V(GetObjectClass) \
-  V(GetMethodID) \
-  V(GetStaticMethodID)
-/*
-// V(GetVersion) \
-// V(DefineClass) \
-// V(FindClass) \
-// V(FromReflectedMethod) \
-// V(FromReflectedField) \
-// V(ToReflectedMethod) \
-// V(GetSuperclass) \
-// V(IsAssignableFrom) \
-// V(ToReflectedField) \
-// V(Throw) \
-// V(ThrowNew) \
-// V(ExceptionOccurred) \
-// V(ExceptionDescribe) \
-// V(ExceptionClear) \
-// V(FatalError) \
-// V(PushLocalFrame) \
-// V(PopLocalFrame) \
-// V(NewGlobalRef) \
-// V(DeleteGlobalRef) \
-// V(DeleteLocalRef) \
-// V(IsSameObject) \
-// V(NewLocalRef) \
-// V(EnsureLocalCapacity) \
-// V(AllocObject) \
-// V(NewObject) \
-// V(NewObjectV) \
-// V(NewObjectA) \
-// V(GetObjectClass) \
-// V(IsInstanceOf) \
-// V(GetMethodID) \
-// \
-// V(CallObjectMethod) \
-// V(CallObjectMethodV) \
-// V(CallObjectMethodA) \
-// V(CallBooleanMethod) \
-// V(CallBooleanMethodV) \
-// V(CallBooleanMethodA) \
-// V(CallByteMethod) \
-// V(CallByteMethodV) \
-// V(CallByteMethodA) \
-// V(CallCharMethod) \
-// V(CallCharMethodV) \
-// V(CallCharMethodA) \
-// V(CallShortMethod) \
-// V(CallShortMethodV) \
-// V(CallShortMethodA) \
-// V(CallIntMethod) \
-// V(CallIntMethodV) \
-// V(CallIntMethodA) \
-// V(CallLongMethod) \
-// V(CallLongMethodV) \
-// V(CallLongMethodA) \
-// V(CallFloatMethod) \
-// V(CallFloatMethodV) \
-// V(CallFloatMethodA) \
-// V(CallDoubleMethod) \
-// V(CallDoubleMethodV) \
-// V(CallDoubleMethodA) \
-// V(CallVoidMethod) \
-// V(CallVoidMethodV) \
-// V(CallVoidMethodA) \
-// \
-// V(CallNonvirtualObjectMethod) \
-// V(CallNonvirtualObjectMethodV) \
-// V(CallNonvirtualObjectMethodA) \
-// V(CallNonvirtualBooleanMethod) \
-// V(CallNonvirtualBooleanMethodV) \
-// V(CallNonvirtualBooleanMethodA) \
-// V(CallNonvirtualByteMethod) \
-// V(CallNonvirtualByteMethodV) \
-// V(CallNonvirtualByteMethodA) \
-// V(CallNonvirtualCharMethod) \
-// V(CallNonvirtualCharMethodV) \
-// V(CallNonvirtualCharMethodA) \
-// V(CallNonvirtualShortMethod) \
-// V(CallNonvirtualShortMethodV) \
-// V(CallNonvirtualShortMethodA) \
-// V(CallNonvirtualIntMethod) \
-// V(CallNonvirtualIntMethodV) \
-// V(CallNonvirtualIntMethodA) \
-// V(CallNonvirtualLongMethod) \
-// V(CallNonvirtualLongMethodV) \
-// V(CallNonvirtualLongMethodA) \
-// V(CallNonvirtualFloatMethod) \
-// V(CallNonvirtualFloatMethodV) \
-// V(CallNonvirtualFloatMethodA) \
-// V(CallNonvirtualDoubleMethod) \
-// V(CallNonvirtualDoubleMethodV) \
-// V(CallNonvirtualDoubleMethodA) \
-// V(CallNonvirtualVoidMethod) \
-// V(CallNonvirtualVoidMethodV) \
-// V(CallNonvirtualVoidMethodA) \
-// \
-// V(GetFieldID) \
-// \
-// V(GetObjectField) \
-// V(GetBooleanField) \
-// V(GetByteField) \
-// V(GetCharField) \
-// V(GetShortField) \
-// V(GetIntField) \
-// V(GetLongField) \
-// V(GetFloatField) \
-// V(GetDoubleField) \
-// \
-// V(SetObjectField) \
-// V(SetBooleanField) \
-// V(SetByteField) \
-// V(SetCharField) \
-// V(SetShortField) \
-// V(SetIntField) \
-// V(SetLongField) \
-// V(SetFloatField) \
-// V(SetDoubleField) \
-// \
-// V(GetStaticMethodID) \
-// \
-// V(CallStaticObjectMethod) \
-// V(CallStaticObjectMethodV) \
-// V(CallStaticObjectMethodA) \
-// V(CallStaticBooleanMethod) \
-// V(CallStaticBooleanMethodV) \
-// V(CallStaticBooleanMethodA) \
-// V(CallStaticByteMethod) \
-// V(CallStaticByteMethodV) \
-// V(CallStaticByteMethodA) \
-// V(CallStaticCharMethod) \
-// V(CallStaticCharMethodV) \
-// V(CallStaticCharMethodA) \
-// V(CallStaticShortMethod) \
-// V(CallStaticShortMethodV) \
-// V(CallStaticShortMethodA) \
-// V(CallStaticIntMethod) \
-// V(CallStaticIntMethodV) \
-// V(CallStaticIntMethodA) \
-// V(CallStaticLongMethod) \
-// V(CallStaticLongMethodV) \
-// V(CallStaticLongMethodA) \
-// V(CallStaticFloatMethod) \
-// V(CallStaticFloatMethodV) \
-// V(CallStaticFloatMethodA) \
-// V(CallStaticDoubleMethod) \
-// V(CallStaticDoubleMethodV) \
-// V(CallStaticDoubleMethodA) \
-// V(CallStaticVoidMethod) \
-// V(CallStaticVoidMethodV) \
-// V(CallStaticVoidMethodA) \
-// \
-// V(GetStaticFieldID) \
-// \
-// V(GetStaticObjectField) \
-// V(GetStaticBooleanField) \
-// V(GetStaticByteField) \
-// V(GetStaticCharField) \
-// V(GetStaticShortField) \
-// V(GetStaticIntField) \
-// V(GetStaticLongField) \
-// V(GetStaticFloatField) \
-// V(GetStaticDoubleField) \
-// \
-// V(SetStaticObjectField) \
-// V(SetStaticBooleanField) \
-// V(SetStaticByteField) \
-// V(SetStaticCharField) \
-// V(SetStaticShortField) \
-// V(SetStaticIntField) \
-// V(SetStaticLongField) \
-// V(SetStaticFloatField) \
-// V(SetStaticDoubleField) \
-// \
-// V(NewString) \
-// V(GetStringLength) \
-// V(GetStringChars) \
-// V(ReleaseStringChars) \
-// V(NewStringUTF) \
-// V(GetStringUTFLength) \
-// V(GetStringUTFChars) \
-// V(ReleaseStringUTFChars) \
-// V(GetArrayLength) \
-// V(NewObjectArray) \
-// V(GetObjectArrayElement) \
-// V(SetObjectArrayElement) \
-// \
-// V(NewBooleanArray) \
-// V(NewByteArray) \
-// V(NewCharArray) \
-// V(NewShortArray) \
-// V(NewIntArray) \
-// V(NewLongArray) \
-// V(NewFloatArray) \
-// V(NewDoubleArray) \
-// \
-// V(GetBooleanArrayElements) \
-// V(GetByteArrayElements) \
-// V(GetCharArrayElements) \
-// V(GetShortArrayElements) \
-// V(GetIntArrayElements) \
-// V(GetLongArrayElements) \
-// V(GetFloatArrayElements) \
-// V(GetDoubleArrayElements) \
-// \
-// V(ReleaseBooleanArrayElements) \
-// V(ReleaseByteArrayElements) \
-// V(ReleaseCharArrayElements) \
-// V(ReleaseShortArrayElements) \
-// V(ReleaseIntArrayElements) \
-// V(ReleaseLongArrayElements) \
-// V(ReleaseFloatArrayElements) \
-// V(ReleaseDoubleArrayElements) \
-// \
-// V(GetBooleanArrayRegion) \
-// V(GetByteArrayRegion) \
-// V(GetCharArrayRegion) \
-// V(GetShortArrayRegion) \
-// V(GetIntArrayRegion) \
-// V(GetLongArrayRegion) \
-// V(GetFloatArrayRegion) \
-// V(GetDoubleArrayRegion) \
-// \
-// V(SetBooleanArrayRegion) \
-// V(SetByteArrayRegion) \
-// V(SetCharArrayRegion) \
-// V(SetShortArrayRegion) \
-// V(SetIntArrayRegion) \
-// V(SetLongArrayRegion) \
-// V(SetFloatArrayRegion) \
-// V(SetDoubleArrayRegion) \
-// \
-// V(RegisterNatives) \
-// V(UnregisterNatives) \
-// V(MonitorEnter) \
-// V(MonitorExit) \
-// V(GetJavaVM) \
-// V(GetStringRegion) \
-// V(GetStringUTFRegion) \
-// V(GetPrimitiveArrayCritical) \
-// V(ReleasePrimitiveArrayCritical) \
-// V(GetStringCritical) \
-// V(ReleaseStringCritical) \
-// V(NewWeakGlobalRef) \
-// V(DeleteWeakGlobalRef) \
-// V(ExceptionCheck) \
-// V(NewDirectByteBuffer) \
-// V(GetDirectBufferAddress) \
-// V(GetDirectBufferCapacity) \
-// V(GetObjectRefType)
-*/
+V(GetVersion) \
+V(DefineClass) \
+V(FindClass) \
+V(FromReflectedMethod) \
+V(FromReflectedField) \
+V(ToReflectedMethod) \
+V(GetSuperclass) \
+V(IsAssignableFrom) \
+V(ToReflectedField) \
+V(Throw) \
+V(ThrowNew) \
+V(ExceptionOccurred) \
+V(ExceptionDescribe) \
+V(ExceptionClear) \
+V(FatalError) \
+V(PushLocalFrame) \
+V(PopLocalFrame) \
+V(NewGlobalRef) \
+V(DeleteGlobalRef) \
+V(DeleteLocalRef) \
+V(IsSameObject) \
+V(NewLocalRef) \
+V(EnsureLocalCapacity) \
+V(AllocObject) \
+V(NewObject) \
+V(NewObjectV) \
+V(NewObjectA) \
+V(GetObjectClass) \
+V(IsInstanceOf) \
+V(GetMethodID) \
+V(CallObjectMethod) \
+V(CallObjectMethodV) \
+V(CallObjectMethodA) \
+V(CallBooleanMethod) \
+V(CallBooleanMethodV) \
+V(CallBooleanMethodA) \
+V(CallByteMethod) \
+V(CallByteMethodV) \
+V(CallByteMethodA) \
+V(CallCharMethod) \
+V(CallCharMethodV) \
+V(CallCharMethodA) \
+V(CallShortMethod) \
+V(CallShortMethodV) \
+V(CallShortMethodA) \
+V(CallIntMethod) \
+V(CallIntMethodV) \
+V(CallIntMethodA) \
+V(CallLongMethod) \
+V(CallLongMethodV) \
+V(CallLongMethodA) \
+V(CallFloatMethod) \
+V(CallFloatMethodV) \
+V(CallFloatMethodA) \
+V(CallDoubleMethod) \
+V(CallDoubleMethodV) \
+V(CallDoubleMethodA) \
+V(CallVoidMethod) \
+V(CallVoidMethodV) \
+V(CallVoidMethodA) \
+V(CallNonvirtualObjectMethod) \
+V(CallNonvirtualObjectMethodV) \
+V(CallNonvirtualObjectMethodA) \
+V(CallNonvirtualBooleanMethod) \
+V(CallNonvirtualBooleanMethodV) \
+V(CallNonvirtualBooleanMethodA) \
+V(CallNonvirtualByteMethod) \
+V(CallNonvirtualByteMethodV) \
+V(CallNonvirtualByteMethodA) \
+V(CallNonvirtualCharMethod) \
+V(CallNonvirtualCharMethodV) \
+V(CallNonvirtualCharMethodA) \
+V(CallNonvirtualShortMethod) \
+V(CallNonvirtualShortMethodV) \
+V(CallNonvirtualShortMethodA) \
+V(CallNonvirtualIntMethod) \
+V(CallNonvirtualIntMethodV) \
+V(CallNonvirtualIntMethodA) \
+V(CallNonvirtualLongMethod) \
+V(CallNonvirtualLongMethodV) \
+V(CallNonvirtualLongMethodA) \
+V(CallNonvirtualFloatMethod) \
+V(CallNonvirtualFloatMethodV) \
+V(CallNonvirtualFloatMethodA) \
+V(CallNonvirtualDoubleMethod) \
+V(CallNonvirtualDoubleMethodV) \
+V(CallNonvirtualDoubleMethodA) \
+V(CallNonvirtualVoidMethod) \
+V(CallNonvirtualVoidMethodV) \
+V(CallNonvirtualVoidMethodA) \
+V(GetFieldID) \
+V(GetObjectField) \
+V(GetBooleanField) \
+V(GetByteField) \
+V(GetCharField) \
+V(GetShortField) \
+V(GetIntField) \
+V(GetLongField) \
+V(GetFloatField) \
+V(GetDoubleField) \
+V(SetObjectField) \
+V(SetBooleanField) \
+V(SetByteField) \
+V(SetCharField) \
+V(SetShortField) \
+V(SetIntField) \
+V(SetLongField) \
+V(SetFloatField) \
+V(SetDoubleField) \
+V(GetStaticMethodID) \
+V(CallStaticObjectMethod) \
+V(CallStaticObjectMethodV) \
+V(CallStaticObjectMethodA) \
+V(CallStaticBooleanMethod) \
+V(CallStaticBooleanMethodV) \
+V(CallStaticBooleanMethodA) \
+V(CallStaticByteMethod) \
+V(CallStaticByteMethodV) \
+V(CallStaticByteMethodA) \
+V(CallStaticCharMethod) \
+V(CallStaticCharMethodV) \
+V(CallStaticCharMethodA) \
+V(CallStaticShortMethod) \
+V(CallStaticShortMethodV) \
+V(CallStaticShortMethodA) \
+V(CallStaticIntMethod) \
+V(CallStaticIntMethodV) \
+V(CallStaticIntMethodA) \
+V(CallStaticLongMethod) \
+V(CallStaticLongMethodV) \
+V(CallStaticLongMethodA) \
+V(CallStaticFloatMethod) \
+V(CallStaticFloatMethodV) \
+V(CallStaticFloatMethodA) \
+V(CallStaticDoubleMethod) \
+V(CallStaticDoubleMethodV) \
+V(CallStaticDoubleMethodA) \
+V(CallStaticVoidMethod) \
+V(CallStaticVoidMethodV) \
+V(CallStaticVoidMethodA) \
+V(GetStaticFieldID) \
+V(GetStaticObjectField) \
+V(GetStaticBooleanField) \
+V(GetStaticByteField) \
+V(GetStaticCharField) \
+V(GetStaticShortField) \
+V(GetStaticIntField) \
+V(GetStaticLongField) \
+V(GetStaticFloatField) \
+V(GetStaticDoubleField) \
+V(SetStaticObjectField) \
+V(SetStaticBooleanField) \
+V(SetStaticByteField) \
+V(SetStaticCharField) \
+V(SetStaticShortField) \
+V(SetStaticIntField) \
+V(SetStaticLongField) \
+V(SetStaticFloatField) \
+V(SetStaticDoubleField) \
+V(NewString) \
+V(GetStringLength) \
+V(GetStringChars) \
+V(ReleaseStringChars) \
+V(NewStringUTF) \
+V(GetStringUTFLength) \
+V(GetStringUTFChars) \
+V(ReleaseStringUTFChars) \
+V(GetArrayLength) \
+V(NewObjectArray) \
+V(GetObjectArrayElement) \
+V(SetObjectArrayElement) \
+V(NewBooleanArray) \
+V(NewByteArray) \
+V(NewCharArray) \
+V(NewShortArray) \
+V(NewIntArray) \
+V(NewLongArray) \
+V(NewFloatArray) \
+V(NewDoubleArray) \
+V(GetBooleanArrayElements) \
+V(GetByteArrayElements) \
+V(GetCharArrayElements) \
+V(GetShortArrayElements) \
+V(GetIntArrayElements) \
+V(GetLongArrayElements) \
+V(GetFloatArrayElements) \
+V(GetDoubleArrayElements) \
+V(ReleaseBooleanArrayElements) \
+V(ReleaseByteArrayElements) \
+V(ReleaseCharArrayElements) \
+V(ReleaseShortArrayElements) \
+V(ReleaseIntArrayElements) \
+V(ReleaseLongArrayElements) \
+V(ReleaseFloatArrayElements) \
+V(ReleaseDoubleArrayElements) \
+V(GetBooleanArrayRegion) \
+V(GetByteArrayRegion) \
+V(GetCharArrayRegion) \
+V(GetShortArrayRegion) \
+V(GetIntArrayRegion) \
+V(GetLongArrayRegion) \
+V(GetFloatArrayRegion) \
+V(GetDoubleArrayRegion) \
+V(SetBooleanArrayRegion) \
+V(SetByteArrayRegion) \
+V(SetCharArrayRegion) \
+V(SetShortArrayRegion) \
+V(SetIntArrayRegion) \
+V(SetLongArrayRegion) \
+V(SetFloatArrayRegion) \
+V(SetDoubleArrayRegion) \
+V(RegisterNatives) \
+V(UnregisterNatives) \
+V(MonitorEnter) \
+V(MonitorExit) \
+V(GetJavaVM) \
+V(GetStringRegion) \
+V(GetStringUTFRegion) \
+V(GetPrimitiveArrayCritical) \
+V(ReleasePrimitiveArrayCritical) \
+V(GetStringCritical) \
+V(ReleaseStringCritical) \
+V(NewWeakGlobalRef) \
+V(DeleteWeakGlobalRef) \
+V(ExceptionCheck) \
+V(NewDirectByteBuffer) \
+V(GetDirectBufferAddress) \
+V(GetDirectBufferCapacity) \
+V(GetObjectRefType)
+
 
 class VarArgs {
 public:
@@ -360,7 +339,9 @@ public:
   V(jlong, Long)       \
   V(void, Void)
 
+
 struct NespressoEnv {
+  // TODO(peterssen): Add C++ env-less methods.
   #define CALL_METHOD(returnType, Type) \
     returnType (*Call##Type##Method)(JNIEnv *env, jobject obj, jmethodID methodID, jlong varargs); \
     returnType (*CallStatic##Type##Method)(JNIEnv *env, jobject clazz, jmethodID methodID, jlong varargs); \
@@ -368,6 +349,10 @@ struct NespressoEnv {
 
   TYPE_LIST2(CALL_METHOD)
   #undef CALL_METHOD
+
+  // NewObject varargs
+  jobject (*NewObject)(JNIEnv *env, jclass clazz, jmethodID methodID, jlong varargs);
+
 };
 
 #define CALL_METHOD_BRIDGE(returnType, Type) \
@@ -426,17 +411,53 @@ TYPE_LIST2(CALL_NON_VIRTUAL_METHOD_BRIDGE)
 #define VARARGS_METHOD_LIST(V) \
   EXPAND(TYPE_LIST2(V MAKE_METHOD)) \
   EXPAND(TYPE_LIST2(V MAKE_STATIC_METHOD)) \
-  EXPAND(TYPE_LIST2(V MAKE_NON_VIRTUAL_METHOD))
+  EXPAND(TYPE_LIST2(V MAKE_NON_VIRTUAL_METHOD)) \
+  V(NewObject)
 
-#define BRIDGE_METHOD_LIST(V) \
+
+jstring NewString(JNIEnv *env, const jchar *unicode, jsize len) {  
+  jclass java_lang_String = env->FindClass("java.lang.String");
+  jmethodID constructor = env->GetMethodID(java_lang_String, "<init>", "([CZ)V");
+  jcharArray chars = env->NewCharArray(len);
+  env->SetCharArrayRegion(chars, 0, len, unicode);
+  return (jstring) env->NewObject(java_lang_String, constructor, chars, (jboolean) JNI_FALSE);  
+}
+
+// Spawn a "guest" direct byte buffer.
+jobject NewDirectByteBuffer(JNIEnv* env, void* address, jlong capacity) {
+  jclass java_nio_DirectByteBuffer = env->FindClass("java.nio.DirectByteBuffer");
+  // TODO(peterssen): Cache class and constructor.
+  jmethodID constructor = env->GetMethodID(java_nio_DirectByteBuffer, "<init>", "(JI)V");
+  // TODO(peterssen): Check narrowing conversion.
+  return env->NewObject(java_nio_DirectByteBuffer, constructor, (jlong) address, (jint) capacity);
+}  
+
+#define BRIDGE_METHOD_LIST(V) \ 
   EXPAND(TYPE_LIST2(V MAKE_METHOD_A)) \
   EXPAND(TYPE_LIST2(V MAKE_METHOD_V)) \
   EXPAND(TYPE_LIST2(V MAKE_STATIC_METHOD_A)) \
   EXPAND(TYPE_LIST2(V MAKE_STATIC_METHOD_V)) \
   EXPAND(TYPE_LIST2(V MAKE_NON_VIRTUAL_METHOD_A)) \
-  EXPAND(TYPE_LIST2(V MAKE_NON_VIRTUAL_METHOD_V))
+  EXPAND(TYPE_LIST2(V MAKE_NON_VIRTUAL_METHOD_V)) \
+  V(NewObjectA) \
+  V(NewObjectV) \
+  V(NewString) \
+  V(NewDirectByteBuffer)
+
 
 extern "C" {
+
+jobject NewObjectV(JNIEnv *env, jclass clazz, jmethodID methodID, va_list args) { 
+  VarArgsVaList varargs(args);
+  NespressoEnv *nespresso_env = (NespressoEnv*) env->functions->reserved0;
+  return nespresso_env->NewObject(env, clazz, methodID, (jlong) &varargs);
+}
+
+jobject NewObjectA(JNIEnv *env, jclass clazz, jmethodID methodID, const jvalue *args) {
+  VarArgsJValues varargs(args);
+  NespressoEnv *nespresso_env = (NespressoEnv*) env->functions->reserved0;
+  return nespresso_env->NewObject(env, clazz, methodID, (jlong) &varargs);
+}
 
 JNIEnv* initializeNativeContext(TruffleEnv* truffle_env, void* (*fetch_by_name)(const char *)) {
   JNIEnv* env = new JNIEnv();
@@ -447,6 +468,7 @@ JNIEnv* initializeNativeContext(TruffleEnv* truffle_env, void* (*fetch_by_name)(
   // Fetch Java ... varargs methods.
   #define INIT_VARARGS_METHOD__(name) \
     nespresso_env->name = truffle_env->dupClosureRef((typeof(nespresso_env->name)) fetch_by_name(#name));
+
     VARARGS_METHOD_LIST(INIT_VARARGS_METHOD__)
   #undef INIT_VARARGS_METHOD__
 
@@ -456,11 +478,11 @@ JNIEnv* initializeNativeContext(TruffleEnv* truffle_env, void* (*fetch_by_name)(
   #undef INIT__
 
   // Put ... bridges in the env.
-  #define INIT_BRIDGE_METHOD__(name) \
+  #define INIT_NATIVE_METHOD__(name) \
     functions->name = &name;
 
-    BRIDGE_METHOD_LIST(INIT_BRIDGE_METHOD__)
-  #undef INIT_BRIDGE_METHOD__
+    BRIDGE_METHOD_LIST(INIT_NATIVE_METHOD__)
+  #undef INIT_NATIVE_METHOD__
 
   env->functions = functions;
 
