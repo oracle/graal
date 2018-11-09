279da22b3aee361de843180232aecc01102a91ef
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-11-09 15:09:11 +0100

    Remove obsolete substitutions in j.l.Runtime.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/intrinsics/Target_java_lang_Runtime.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/intrinsics/Target_java_lang_Runtime.java
@@ -34,11 +34,6 @@ import com.oracle.truffle.espresso.runtime.StaticObjectArray;
 
 @EspressoIntrinsics
 public class Target_java_lang_Runtime {
-    @Intrinsic(hasReceiver = true)
-    public static int availableProcessors(Object self) {
-        return Runtime.getRuntime().availableProcessors();
-    }
-
     // TODO(peterssen): This a hack to be able to spawn processes without going down to UNIXProcess.
     @Intrinsic(hasReceiver = true)
     public static @Type(Process.class) Object exec(StaticObject self, @Type(String[].class) StaticObject cmdarray) {
@@ -46,22 +41,12 @@ public class Target_java_lang_Runtime {
         Object[] wrapped = ((StaticObjectArray) cmdarray).getWrapped();
         String[] hostArgs = new String[wrapped.length];
         Arrays.setAll(hostArgs, i -> meta.toHost(wrapped[i]));
-
+        
         try {
             Runtime.getRuntime().exec(hostArgs);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return StaticObject.NULL;
-    }
-
-    @Intrinsic(hasReceiver = true)
-    public static long totalMemory(Object self) {
-        return Runtime.getRuntime().totalMemory();
-    }
-
-    @Intrinsic(hasReceiver = true)
-    public static void gc(Object self) {
-        Runtime.getRuntime().gc();
     }
 }
