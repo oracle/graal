1e0a0607ff4102e135a149e9c02032dfd3da7b41
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-11-09 16:33:36 +0100

    Add license headers.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Utf8.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Utf8.java
@@ -1,9 +1,33 @@
+/*
+ * Copyright (c) 2018, 2018, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package com.oracle.truffle.espresso.jni;
 
-import java.io.OutputStream;
-
 public class Utf8 {
-    private Utf8() { /* no instances */ }
+
+    private Utf8() {
+        /* no instances */
+    }
+
     public static byte[] asUTF(String str) {
         int strlen = str.length();
         int utflen = 0;
@@ -23,25 +47,26 @@ public class Utf8 {
 
         byte[] bytearr = new byte[utflen];
 
-        int i=0;
-        for (i=0; i<strlen; i++) {
+        int i = 0;
+        for (i = 0; i < strlen; i++) {
             c = str.charAt(i);
-            if (!((c >= 0x0001) && (c <= 0x007F))) break;
+            if (!((c >= 0x0001) && (c <= 0x007F)))
+                break;
             bytearr[count++] = (byte) c;
         }
 
-        for (;i < strlen; i++){
+        for (; i < strlen; i++) {
             c = str.charAt(i);
             if ((c >= 0x0001) && (c <= 0x007F)) {
                 bytearr[count++] = (byte) c;
 
             } else if (c > 0x07FF) {
                 bytearr[count++] = (byte) (0xE0 | ((c >> 12) & 0x0F));
-                bytearr[count++] = (byte) (0x80 | ((c >>  6) & 0x3F));
-                bytearr[count++] = (byte) (0x80 | ((c >>  0) & 0x3F));
+                bytearr[count++] = (byte) (0x80 | ((c >> 6) & 0x3F));
+                bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
             } else {
-                bytearr[count++] = (byte) (0xC0 | ((c >>  6) & 0x1F));
-                bytearr[count++] = (byte) (0x80 | ((c >>  0) & 0x3F));
+                bytearr[count++] = (byte) (0xC0 | ((c >> 6) & 0x1F));
+                bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
             }
         }
 
