09e900ad02037b51f91f6333fc79aceb393d6e61
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-11-15 17:21:02 +0100

    Cleanup UTF-8 conversions.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Utf8.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Utf8.java
@@ -47,49 +47,8 @@ public class Utf8 {
         return utflen;
     }
 
-    public static byte[] asUTF(String str) {
-        int strlen = str.length();
-        int utflen = 0;
-        int c, count = 0;
-
-        /* use charAt instead of copying String to char array */
-        for (int i = 0; i < strlen; i++) {
-            c = str.charAt(i);
-            if ((c >= 0x0001) && (c <= 0x007F)) {
-                utflen++;
-            } else if (c > 0x07FF) {
-                utflen += 3;
-            } else {
-                utflen += 2;
-            }
-        }
-
-        byte[] bytearr = new byte[utflen];
-
-        int i = 0;
-        for (i = 0; i < strlen; i++) {
-            c = str.charAt(i);
-            if (!((c >= 0x0001) && (c <= 0x007F)))
-                break;
-            bytearr[count++] = (byte) c;
-        }
-
-        for (; i < strlen; i++) {
-            c = str.charAt(i);
-            if ((c >= 0x0001) && (c <= 0x007F)) {
-                bytearr[count++] = (byte) c;
-
-            } else if (c > 0x07FF) {
-                bytearr[count++] = (byte) (0xE0 | ((c >> 12) & 0x0F));
-                bytearr[count++] = (byte) (0x80 | ((c >> 6) & 0x3F));
-                bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
-            } else {
-                bytearr[count++] = (byte) (0xC0 | ((c >> 6) & 0x1F));
-                bytearr[count++] = (byte) (0x80 | ((c >> 0) & 0x3F));
-            }
-        }
-
-        return bytearr;
+    public static byte[] asUTF(String str, boolean append0) {
+        return asUTF(str, 0, str.length(), append0);
     }
 
     public static byte[] asUTF(String str, int start, int len, boolean append0) {
