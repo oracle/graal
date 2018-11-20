153078fbe708ed12408fb80c43eff18eacb7ef64
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-11-20 21:47:09 +0100

    Remove obsolete substitutions.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/intrinsics/Target_java_lang_reflect_Field.java
+++
@@ -1,9 +0,0 @@
-package com.oracle.truffle.espresso.intrinsics;
-
-@EspressoIntrinsics
-public class Target_java_lang_reflect_Field {
-    @Intrinsic(hasReceiver = true)
-    public static byte[] getTypeAnnotationBytes0(Object self) {
-        return new byte[0];
-    }
-}
