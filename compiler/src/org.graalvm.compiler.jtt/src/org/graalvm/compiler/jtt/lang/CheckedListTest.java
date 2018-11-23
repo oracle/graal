cb910021a20f0cd8580cc03260a9c0bd2ae4ee5e
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2018-11-23 22:56:35 +0100

    Fix warnings in the test.

--- compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/lang/CheckedListTest.java
+++ compiler/src/org.graalvm.compiler.jtt/src/org/graalvm/compiler/jtt/lang/CheckedListTest.java
@@ -42,6 +42,7 @@ public final class CheckedListTest extends JTTTest {
         runTest("testCast");
     }
 
+    @SuppressWarnings({"unchecked", "rawtypes", "serial"})
     public void testCast() {
         final AtomicBoolean addAllWasCalled = new AtomicBoolean();
         ArrayList orig = new ArrayList() {
@@ -69,9 +70,9 @@ public final class CheckedListTest extends JTTTest {
     }
 
     public void testCopyOf() {
-        Object[] mixed = new Object[] {"a", "b", 18};
+        Object[] mixed = new Object[]{"a", "b", 18};
         try {
-            Arrays.copyOf(mixed, 4, new String[0].getClass());
+            Arrays.copyOf(mixed, 4, String[].class);
         } catch (ArrayStoreException e) {
             return;
         }
@@ -84,7 +85,7 @@ public final class CheckedListTest extends JTTTest {
     }
 
     public void testArraycopy() {
-        Object[] mixed = new Object[] {"a", "b", 18};
+        Object[] mixed = new Object[]{"a", "b", 18};
         try {
             String[] strings = new String[4];
             System.arraycopy(mixed, 0, strings, 0, 3);
