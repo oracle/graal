48f492797b2da67e88698c31d2490bcb0a9e3a38
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2018-11-27 14:03:15 +0100

    Fix comment.

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/extended/HubGetClassNodeInterface.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/extended/HubGetClassNodeInterface.java
@@ -28,8 +28,8 @@ import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.ValueNodeInterface;
 
 /**
- * In some systems, the hub and the class are different objects,
- * and this is a base interface for operations that converts a hub to the class.
+ * In some systems, the hub and the class are different objects, and this is a base interface for
+ * operations that converts a hub to the class.
  */
 public interface HubGetClassNodeInterface extends ValueNodeInterface {
     ValueNode getHub();
