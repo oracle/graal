a575a2d8cfac1553306a792990470b89a2f81d20
Paul Wögerer <paul.woegerer@oracle.com>
2018-12-05 14:28:31 +0100

    More Java 11 fixes for native-image building

--- substratevm/src/com.oracle.svm.core.jdk9/src/com/oracle/svm/core/jdk/JDK9OrLaterSecuritySubstitutions.java
+++ substratevm/src/com.oracle.svm.core.jdk9/src/com/oracle/svm/core/jdk/JDK9OrLaterSecuritySubstitutions.java
@@ -22,33 +22,32 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+package com.oracle.svm.core.jdk;
 
-package com.oracle.svm.reflect.target;
-
-// Checkstyle: allow reflection
+import java.security.Policy;
+import java.security.Provider;
 
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.util.VMError;
 
-@TargetClass(value = sun.misc.Unsafe.class)
-@SuppressWarnings({"static-method"})
-public final class Target_sun_misc_Unsafe {
-
+@TargetClass(className = "sun.security.jca.ProviderConfig", innerClass = "ProviderLoader", onlyWith = JDK9OrLater.class)
+@SuppressWarnings({"static-method", "unused"})
+final class Target_sun_security_jca_ProviderConfig_ProviderLoader {
     @Substitute
-    public long objectFieldOffset(Target_java_lang_reflect_Field field) {
-
-        int offset = field.root == null ? field.offset : field.root.offset;
-
-        if (offset > 0) {
-            return offset;
-        }
-
-        throw VMError.unsupportedFeature("The offset of " + field + " is accessed without the field being first registered as unsafe accessed. " +
-                        "Please register the field as unsafe accessed. You can do so by using a custom Feature. " +
-                        "First, create a class that implements the org.graalvm.nativeimage.Feature interface. " +
-                        "Then, implement the method beforeAnalysis(org.graalvm.nativeimage.BeforeAnalysisAccess config). " +
-                        "Next, use the config object to register the field for unsafe access by calling config.registerAsUnsafeAccessed(java.lang.reflect.Field) method. " +
-                        "Finally, specify the custom feature to the native image building tool using the -H:Features=MyCustomFeature option.");
+    private Provider legacyLoad(String classname) {
+        throw VMError.unsupportedFeature("JDK9OrLater: sun.security.jca.ProviderConfig.legacyLoad(String classname)");
     }
+}
+
+@TargetClass(value = java.security.Policy.class, onlyWith = JDK9OrLater.class)
+@SuppressWarnings({"static-method", "unused"})
+final class Target_java_security_Policy {
+    @Substitute
+    static Policy getPolicyNoCheck() {
+        throw VMError.unsupportedFeature("JDK9OrLater: java.security.Policy.getPolicyNoCheck()");
+    }
+}
+
+public final class JDK9OrLaterSecuritySubstitutions {
 }
