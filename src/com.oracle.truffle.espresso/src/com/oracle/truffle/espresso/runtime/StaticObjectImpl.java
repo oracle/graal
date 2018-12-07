31a7db1835cc00df8e92fd9cc484554c68cd907d
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-12-07 17:15:04 +0100

    Remove static(ness) field from (every) StaticObject.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObjectImpl.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObjectImpl.java
@@ -38,26 +38,24 @@ import com.oracle.truffle.espresso.meta.MetaUtil;
 
 public class StaticObjectImpl implements StaticObject {
     private Map<String, Object> hiddenFields;
-    private final boolean isStatic;
     private final Klass klass;
 
     private final Object[] fields;
 
-    public boolean isStatic() {
-        return isStatic;
-    }
-
-    public StaticObjectImpl(Klass klass, Map<String, Object> hiddenFields, Object[] fields, boolean isStatic) {
+    public StaticObjectImpl(Klass klass, Map<String, Object> hiddenFields, Object[] fields) {
         this.klass = klass;
         this.hiddenFields = hiddenFields;
-        this.isStatic = isStatic;
         this.fields = fields;
+    }
+
+    public boolean isStatic() {
+        return this == klass.getStatics();
     }
 
     // Shallow copy.
     public StaticObject copy() {
         HashMap<String, Object> hiddenFieldsCopy = hiddenFields != null ? new HashMap<>(hiddenFields) : null;
-        return new StaticObjectImpl(getKlass(), hiddenFieldsCopy, fields.clone(), isStatic);
+        return new StaticObjectImpl(getKlass(), hiddenFieldsCopy, fields.clone());
     }
 
     public StaticObjectImpl(ObjectKlass klass) {
@@ -68,7 +66,6 @@ public class StaticObjectImpl implements StaticObject {
         assert !isStatic || klass.isInitialized();
         this.klass = klass;
         this.hiddenFields = null;
-        this.isStatic = isStatic;
         this.fields = isStatic ? new Object[klass.getStaticFieldSlots()] : new Object[klass.getInstanceFieldSlots()];
         FieldInfo[] allFields = isStatic ? klass.getStaticFields() : klass.getInstanceFields(true);
         for (FieldInfo fi : allFields) {
