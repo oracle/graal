88916073f221c6bf88084404af86b8fb9bc9a0fa
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2018-12-10 19:51:56 +0100

    Add mx espresso-native command.

--- mx.espresso/mx_espresso.py
+++ mx.espresso/mx_espresso.py
@@ -21,12 +21,13 @@
 # questions.
 #
 
+import os
+from argparse import ArgumentParser
+
 import mx
 import mx_sdk
-
 from mx_gate import Task, add_gate_runner
 from mx_unittest import unittest
-from argparse import ArgumentParser
 
 _suite = mx.suite('espresso')
 
@@ -34,15 +35,52 @@ _suite = mx.suite('espresso')
 class EspressoDefaultTags:
     default = 'default'
     all = 'all'
+    native = 'native'
 
 
-def _run_espresso(args, verbose=False):
+def _run_espresso(args):
     vm_args, args = mx.extract_VM_args(args, useDoubleDash=True, defaultAllVMArgs=False)
 
     mx.run_java(vm_args
                 + mx.get_runtime_jvm_args(['ESPRESSO', 'ESPRESSO_LAUNCHER'], jdk=mx.get_jdk())
-                + ["com.oracle.truffle.espresso.launcher.EspressoLauncher"]
+                + [mx.distribution('ESPRESSO_LAUNCHER').mainClass]
                 + args)
+
+
+def _run_espresso_native(graalvm_home, args, distributions=None):
+    espresso_native = os.path.join(graalvm_home, 'bin', 'espresso')
+    if not os.path.exists(espresso_native):
+        mx.abort(
+            'Cannot find Espresso native image: "{}". Did you forget to run "mx --env espresso.env build"?'.format(
+                espresso_native))
+
+    vm_args, args = mx.extract_VM_args(args, useDoubleDash=False, defaultAllVMArgs=False, allowClasspath=True)
+
+    native_cp = mx.classpath(distributions or [])
+    user_cp_index, user_cp_value = mx.find_classpath_arg(vm_args)
+
+    if user_cp_value:
+        native_cp = os.pathsep.join([native_cp, user_cp_value]) if native_cp else user_cp_value
+        vm_args[user_cp_index - 1:user_cp_index + 1] = []
+
+    return mx.run([espresso_native] +
+                  vm_args +
+                  mx.get_runtime_jvm_args(distributions or [], jdk=mx.get_jdk(), cp_suffix=native_cp) +
+                  args
+                  )
+
+
+def _run_espresso_playground(args, verbose=False):
+    vm_args, args = mx.extract_VM_args(args, useDoubleDash=False, defaultAllVMArgs=False)
+    parser = ArgumentParser(prog='mx espresso-playground')
+    parser.add_argument('main_class', action='store', help='Unqualified class name to run.')
+    parser.add_argument('main_class_args', nargs='*')
+    args = parser.parse_args(args)
+
+    return _run_espresso(vm_args +
+                         ['-cp', mx.classpath("ESPRESSO_PLAYGROUND"),
+                          'com.oracle.truffle.espresso.playground.' + args.main_class] + args.main_class_args,
+                         verbose)
 
 
 def _espresso_gate_runner(args, tasks):
@@ -50,11 +88,14 @@ def _espresso_gate_runner(args, tasks):
         if t:
             unittest(['--enable-timing', '--very-verbose', 'com.oracle.truffle.espresso.test'])
 
+    with Task('HelloWorld', tasks, tags=[EspressoDefaultTags.native]) as t:
+        if t:
+            _run_espresso_native(['--distribution', 'ESPRESSO_PLAYGROUND', 'com.oracle.truffle.espresso.playground.HelloWorld'])
+
 
 # REGISTER MX GATE RUNNER
 #########################
 add_gate_runner(_suite, _espresso_gate_runner)
-
 
 mx_sdk.register_graalvm_component(mx_sdk.GraalVmLanguage(
     suite=_suite,
@@ -78,19 +119,9 @@ mx_sdk.register_graalvm_component(mx_sdk.GraalVmLanguage(
     ],
 ))
 
-
-def _run_espresso_playground(args, verbose=False):
-    vm_args, args = mx.extract_VM_args(args, useDoubleDash=False, defaultAllVMArgs=False)
-    parser = ArgumentParser(prog='mx espresso-playground')
-    parser.add_argument('main_class', action='store', help='Unqualified class name to run.')
-    parser.add_argument('main_class_args', nargs='*')
-    args = parser.parse_args(args)
-    return _run_espresso(vm_args + ['-cp', mx.distribution("espresso:ESPRESSO_PLAYGROUND").path,
-                          'com.oracle.truffle.espresso.playground.' + args.main_class] + args.main_class_args, verbose)
-
-
 # register new commands which can be used from the commandline with mx
 mx.update_commands(_suite, {
     'espresso': [_run_espresso, ''],
     'espresso-playground': [_run_espresso_playground, ''],
+    'espresso-native': [_run_espresso_native, ''],
 })
