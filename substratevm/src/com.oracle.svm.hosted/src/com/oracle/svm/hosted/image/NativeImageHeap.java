616115ef2bd70eef99af085b53f440b3695193fc
Peter Hofer <peter.hofer@oracle.com>
2018-12-28 12:37:09 +0100

    Better error on failure due to non-immutable object with relocations.

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageHeap.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageHeap.java
@@ -453,7 +453,7 @@ public final class NativeImageHeap {
             throw shouldNotReachHere();
         }
 
-        final HeapPartition partition = choosePartition(!written || immutable, references, relocatable);
+        final HeapPartition partition = choosePartition(object, !written || immutable, references, relocatable);
         info.assignToHeapPartition(partition, layout);
     }
 
@@ -481,7 +481,7 @@ public final class NativeImageHeap {
         return info;
     }
 
-    private HeapPartition choosePartition(boolean immutable, boolean references, boolean relocatable) {
+    private HeapPartition choosePartition(Object object, boolean immutable, boolean references, boolean relocatable) {
         if (SubstrateOptions.UseOnlyWritableBootImageHeap.getValue()) {
             assert !spawnIsolates();
             // Emergency use only! Alarms will sound!
@@ -494,7 +494,9 @@ public final class NativeImageHeap {
             }
             return references ? readOnlyReference : readOnlyPrimitive;
         } else {
-            VMError.guarantee(!relocatable, "Objects with relocatable pointers must be immutable");
+            if (relocatable) {
+                VMError.shouldNotReachHere("Object with relocatable pointers must be immutable: " + object);
+            }
             return references ? writableReference : writablePrimitive;
         }
     }
