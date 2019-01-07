7c4cf29e4c0c24b0ed29d5c1e8e8d12db515b495
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2019-01-07 18:50:28 +0100

    Fix dev cycle.

--- truffle/mx.truffle/suite.py
+++ truffle/mx.truffle/suite.py
@@ -41,7 +41,7 @@
 suite = {
   "mxversion" : "5.189.0",
   "name" : "truffle",
-  "version": "1.0.0-rc10",
+  "version" : "1.0.0-rc11",
   "release" : False,
   "groupId" : "org.graalvm.truffle",
   "sourceinprojectwhitelist" : [],
