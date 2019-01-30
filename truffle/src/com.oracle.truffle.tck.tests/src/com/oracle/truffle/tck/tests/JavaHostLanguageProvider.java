fc170c5bd9e1a8e2aeda290131e0ffdfdd5ff58b
Tomas Zezula <tomas.zezula@oracle.com>
2019-01-30 13:54:36 +0100

    [GR-13515] Added a custom Java object into JavaHostLanguageProvider.

--- truffle/src/com.oracle.truffle.tck.tests/src/com/oracle/truffle/tck/tests/JavaHostLanguageProvider.java
+++ truffle/src/com.oracle.truffle.tck.tests/src/com/oracle/truffle/tck/tests/JavaHostLanguageProvider.java
@@ -132,6 +132,10 @@ public final class JavaHostLanguageProvider implements LanguageProvider {
                         export(context, new ValueSupplier<>(new Object())),
                         TypeDescriptor.intersection(TypeDescriptor.HOST_OBJECT, TypeDescriptor.OBJECT)).build());
         result.add(Snippet.newBuilder(
+                        "POJO",
+                        export(context, new ValueSupplier<>(new Complex(1, 2))),
+                        TypeDescriptor.intersection(TypeDescriptor.HOST_OBJECT, TypeDescriptor.OBJECT)).build());
+        result.add(Snippet.newBuilder(
                         "java.util.List<Integer>",
                         export(context, new ValueSupplier<>(new ArrayList<>(Arrays.asList(1, 2)))),
                         TypeDescriptor.intersection(TypeDescriptor.HOST_OBJECT, TypeDescriptor.OBJECT, TypeDescriptor.array(TypeDescriptor.NUMBER))).build());
@@ -359,6 +363,20 @@ public final class JavaHostLanguageProvider implements LanguageProvider {
             } else {
                 return EMPTY;
             }
+        }
+    }
+
+    public static final class Complex {
+        public double real;
+        public double imag;
+
+        Complex(double real, double imag) {
+            this.real = real;
+            this.imag = imag;
+        }
+
+        public double abs() {
+            return Math.sqrt(real * real + imag * imag);
         }
     }
 }
