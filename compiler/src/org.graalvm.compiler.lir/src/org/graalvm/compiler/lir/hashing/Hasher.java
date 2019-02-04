75b4b534fb14a6635ab513071ba584441b7e1a60
Tom Rodriguez <tom.rodriguez@oracle.com>
2019-02-04 11:43:38 -0800

    Copyright updates plus minor edit

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/hashing/Hasher.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/hashing/Hasher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -94,10 +94,9 @@ public final class Hasher {
         Set<Integer> seen = new HashSet<>(keys.length);
         for (JavaConstant key : keys) {
             int hash = function.apply(key.asLong(), min) & (cardinality - 1);
-            if (seen.contains(hash)) {
+            if (!seen.add(hash)) {
                 return false;
             }
-            seen.add(hash);
         }
         return true;
     }
