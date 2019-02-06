acf073ee29df3a85e458da5f774ff82a83d71bf5
Martin Entlicher <martin.entlicher@oracle.com>
2019-02-06 19:23:44 +0100

    [GR-13781] Remove the deprecated Remote option.

--- tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/instrument/InspectorInstrument.java
+++ tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/instrument/InspectorInstrument.java
@@ -175,9 +175,6 @@ public final class InspectorInstrument extends TruffleInstrument {
     @com.oracle.truffle.api.Option(help = "Path to the chrome inspect. (default: randomly generated)", category = OptionCategory.EXPERT) //
     static final OptionKey<String> Path = new OptionKey<>("");
 
-    @com.oracle.truffle.api.Option(help = "Don't use loopback address. (default:false)", category = OptionCategory.EXPERT, deprecated = true) //
-    static final OptionKey<Boolean> Remote = new OptionKey<>(false);
-
     @com.oracle.truffle.api.Option(help = "Inspect internal sources. (default:false)", category = OptionCategory.DEBUG) //
     static final OptionKey<Boolean> Internal = new OptionKey<>(false);
 
@@ -209,11 +206,11 @@ public final class InspectorInstrument extends TruffleInstrument {
             HostAndPort hostAndPort = options.get(Inspect);
             connectionWatcher = new ConnectionWatcher();
             try {
-                InetSocketAddress socketAddress = hostAndPort.createSocket(options.get(Remote));
+                InetSocketAddress socketAddress = hostAndPort.createSocket();
                 server = new Server(env, "Main Context", socketAddress, options.get(Attach), options.get(Suspend), options.get(WaitAttached), options.get(HideErrors), options.get(Internal),
                                 options.get(Initialization), options.get(Path), options.get(Secure), new KeyStoreOptions(options), options.get(SourcePath), connectionWatcher);
             } catch (IOException e) {
-                throw new InspectorIOException(hostAndPort.getHostPort(options.get(Remote)), e);
+                throw new InspectorIOException(hostAndPort.getHostPort(), e);
             }
         }
 
@@ -234,12 +231,12 @@ public final class InspectorInstrument extends TruffleInstrument {
                 connectionWatcher = new ConnectionWatcher();
                 hostAndPort = new HostAndPort(host, port);
                 try {
-                    InetSocketAddress socketAddress = hostAndPort.createSocket(options.get(Remote));
+                    InetSocketAddress socketAddress = hostAndPort.createSocket();
                     server = new Server(env, "Main Context", socketAddress, false, false, wait, options.get(HideErrors), options.get(Internal),
                                     options.get(Initialization), null, options.get(Secure), new KeyStoreOptions(options), options.get(SourcePath), connectionWatcher);
                 } catch (IOException e) {
                     PrintWriter info = new PrintWriter(env.err());
-                    info.println(new InspectorIOException(hostAndPort.getHostPort(false), e).getLocalizedMessage());
+                    info.println(new InspectorIOException(hostAndPort.getHostPort(), e).getLocalizedMessage());
                     info.flush();
                 }
                 return server != null ? server.getConnection() : null;
@@ -350,13 +347,11 @@ public final class InspectorInstrument extends TruffleInstrument {
             }
         }
 
-        String getHostPort(boolean remote) {
+        String getHostPort() {
             String hostName = host;
             if (hostName == null || hostName.isEmpty()) {
                 if (inetAddress != null) {
                     hostName = inetAddress.toString();
-                } else if (remote) {
-                    hostName = "localhost";
                 } else {
                     hostName = InetAddress.getLoopbackAddress().toString();
                 }
@@ -364,14 +359,10 @@ public final class InspectorInstrument extends TruffleInstrument {
             return hostName + ":" + port;
         }
 
-        InetSocketAddress createSocket(boolean remote) throws UnknownHostException {
+        InetSocketAddress createSocket() {
             InetAddress ia;
             if (inetAddress == null) {
-                if (remote) {
-                    ia = InetAddress.getLocalHost();
-                } else {
-                    ia = InetAddress.getLoopbackAddress();
-                }
+                ia = InetAddress.getLoopbackAddress();
             } else {
                 ia = inetAddress;
             }
