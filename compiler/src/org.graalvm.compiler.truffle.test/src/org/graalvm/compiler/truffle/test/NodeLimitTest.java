1675e0e45f99778ef913e5edc545c49ed1c06146
Boris Spasojevic <boris.spasojevic@oracle.com>
2019-02-07 09:24:09 +0100

    Test improvements.

--- compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/NodeLimitTest.java
+++ compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/NodeLimitTest.java
@@ -29,6 +29,7 @@ import org.graalvm.compiler.core.common.GraalBailoutException;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.truffle.compiler.TruffleCompilerOptions;
 import org.graalvm.compiler.truffle.runtime.OptimizedCallTarget;
+import org.graalvm.compiler.truffle.runtime.OptimizedDirectCallNode;
 import org.graalvm.compiler.truffle.runtime.SharedTruffleRuntimeOptions;
 import org.graalvm.compiler.truffle.runtime.TruffleRuntimeOptions;
 import org.junit.Assert;
@@ -45,14 +46,30 @@ import com.oracle.truffle.api.nodes.RootNode;
 
 public class NodeLimitTest extends PartialEvaluationTest {
 
-    private TruffleRuntime runtime;
-
-    // Used for filler code
-    @SuppressWarnings("unused") private static int global;
-
     public NodeLimitTest() {
-        this.runtime = Truffle.getRuntime();
+        runtime = Truffle.getRuntime();
     }
+
+    @BeforeClass
+    public static void before() {
+        // Cannot run with compile immediately because overriding TrufflePENodeLimit does not work.
+        Assume.assumeFalse(TruffleRuntimeOptions.getValue(SharedTruffleRuntimeOptions.TruffleCompileImmediately));
+    }
+
+    @Test
+    public void oneRootNodeTest() {
+        fullTest(createRootNodeFillerOnly(), createRootNodeFillerAndTest());
+    }
+
+    @Test
+    public void testWithTruffleInlining() {
+        fullTest(createRootNodeWithCall(createRootNodeFillerOnly()), createRootNodeWithCall(createRootNodeFillerAndTest()));
+    }
+
+    private static TruffleRuntime runtime;
+
+    // Used as a black hole for filler code
+    @SuppressWarnings("unused") private static int global;
 
     private static RootNode createRootNodeFillerOnly() {
         return new RootNode(null) {
@@ -94,38 +111,56 @@ public class NodeLimitTest extends PartialEvaluationTest {
         };
     }
 
-    @Test
-    public void testWithBudget() {
+    private void fullTest(RootNode rootNodeFillerOnly, RootNode rootNodeFillerAndTest) {
+        expectNeverPartOfCompilation(rootNodeFillerOnly, rootNodeFillerAndTest);
+        expectAllOK(rootNodeFillerOnly, rootNodeFillerAndTest);
+    }
+
+    private void expectNeverPartOfCompilation(RootNode fillerOnly, RootNode fillerAndTest) {
         try {
-            peRootNodeWithFillerAndTest(getBaselineGraphNodeCount());
+            peRootNodeWithFillerAndTest(getBaselineGraphNodeCount(fillerOnly), fillerAndTest);
             throw new AssertionError("Expected to throw but did not.");
         } catch (GraalBailoutException e) {
             Assert.assertEquals(e.getMessage(), "CompilerAsserts.neverPartOfCompilation()");
         }
     }
 
-    @Test
-    public void runTestNoBudget() {
-        peRootNodeWithFillerAndTest(getBaselineGraphNodeCount() - 10);
+    private void expectAllOK(RootNode fillerOnly, RootNode fillerAndTest) {
+        peRootNodeWithFillerAndTest(getBaselineGraphNodeCount(fillerOnly) - 10, fillerAndTest);
     }
 
-    private int getBaselineGraphNodeCount() {
-        final OptimizedCallTarget baselineGraphTarget = (OptimizedCallTarget) runtime.createCallTarget(createRootNodeFillerOnly());
+    private int getBaselineGraphNodeCount(RootNode rootNode) {
+        final OptimizedCallTarget baselineGraphTarget = (OptimizedCallTarget) runtime.createCallTarget(rootNode);
         final StructuredGraph baselineGraph = partialEval(baselineGraphTarget, new Object[]{}, StructuredGraph.AllowAssumptions.YES, CompilationIdentifier.INVALID_COMPILATION_ID);
         return baselineGraph.getNodeCount();
     }
 
     @SuppressWarnings("try")
-    private void peRootNodeWithFillerAndTest(int nodeLimit) {
+    private void peRootNodeWithFillerAndTest(int nodeLimit, RootNode rootNode) {
         try (TruffleCompilerOptions.TruffleOptionsOverrideScope scope = TruffleCompilerOptions.overrideOptions(TruffleCompilerOptions.TruffleMaximumGraalNodeCount, nodeLimit)) {
-            RootCallTarget target = runtime.createCallTarget(createRootNodeFillerAndTest());
+            RootCallTarget target = runtime.createCallTarget(rootNode);
             partialEval((OptimizedCallTarget) target, new Object[]{}, StructuredGraph.AllowAssumptions.YES, CompilationIdentifier.INVALID_COMPILATION_ID);
         }
     }
 
-    @BeforeClass
-    public static void before() {
-        // Cannot run with compile immediately because overriding TrufflePENodeLimit does not work.
-        Assume.assumeFalse(TruffleRuntimeOptions.getValue(SharedTruffleRuntimeOptions.TruffleCompileImmediately));
+    private static RootNode createRootNodeWithCall(final RootNode rootNode) {
+        return new RootNode(null) {
+
+            @Child OptimizedDirectCallNode call = (OptimizedDirectCallNode) runtime.createDirectCallNode(runtime.createCallTarget(rootNode));
+
+            @Override
+            public Object execute(VirtualFrame frame) {
+                foo();
+                call.call(new Object[0]);
+                return null;
+            }
+
+            private void foo() {
+                for (int i = 0; i < 1000; i++) {
+                    global += i;
+                }
+
+            }
+        };
     }
 }
