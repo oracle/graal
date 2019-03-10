1603ddf538414f082fe4e7aea22b654cd7b92b64
Christian Humer <christian.humer@oracle.com>
2019-03-10 21:40:00 +0100

    Add javadoc to @GenerateUncached.

--- truffle/src/com.oracle.truffle.api.dsl/src/com/oracle/truffle/api/dsl/GenerateUncached.java
+++ truffle/src/com.oracle.truffle.api.dsl/src/com/oracle/truffle/api/dsl/GenerateUncached.java
@@ -41,8 +41,65 @@
 package com.oracle.truffle.api.dsl;
 
 /**
- * Generates a uncached version of a Node.
+ * Generates an uncached version of a node with specializations. Uncached versions of nodes don't
+ * specialize and don't use any profiling state. This allows to store them statically and to use
+ * them whenever no specialization/profiling is desired. The uncached version of the node is
+ * accessible using a static method called <code>getUncached()</code> on the generated node.
+ * <p>
+ * The generated code for the uncached version is based on the specialization closure. The
+ * specialization closure only includes specializations that were are not replaced by others. This,
+ * for example, automatically excludes inline caches from the closure. Uses of the {@link Cached}
+ * annotation will automatically use {@linkplain Cached#uncached() getUncached} instead of a cached
+ * version to initialize the cache.
+ * <p>
+ * A node subclass must full-fill the following requirements in order to be uncachable:
+ * <ul>
+ * <li>At least one specialization and one execute method must be specified.
+ * <li>The node has no instance fields.
+ * <li>All {@link Cached} parameters provide valid {@linkplain Cached#uncached() uncached}
+ * initializers.
+ * <li>All specializations of the closure must not use {@linkplain Specialization#rewriteOn()
+ * rewriteOn} attribute.
+ * <li>All guards/cache/limit expressions must not bind the node receiver.
+ * <li>The node must not declare any {@link NodeChild} or {@link NodeField} annotations.
+ * </ul>
+ * If any of these requirements are violated then an error will be shown.
+ * <p>
+ * <b>Example:</b>
  *
+ * <pre>
+ * &#64;GenerateUncached
+ * abstract static class UncachableNode extends Node {
+ *
+ *     abstract Object execute(Object arg);
+ *
+ *     &#64;Specialization(guards = "v == cachedV")
+ *     static Object doCached(int v, &#64;Cached("v") int cachedV) {
+ *         // do cached
+ *     }
+ *
+ *     &#64;Specialization(replaces = "doCached")
+ *     static String doGeneric(int v) {
+ *         // do uncached
+ *     }
+ *
+ * }
+ * </pre>
+ *
+ * This node produces the following uncached version of the execute method:
+ *
+ * <pre>
+ * &#64;Override
+ * Object execute(Object arg0Value) {
+ *     if (arg0Value instanceof Integer) {
+ *         int arg0Value_ = (int) arg0Value;
+ *         return UncachableNode.doGeneric(arg0Value_);
+ *     }
+ *     throw new UnsupportedSpecializationException(this, new Node[]{null}, arg0Value);
+ * }
+ * </pre>
+ *
+ * @see Cached
  * @since 1.0
  */
 public @interface GenerateUncached {
