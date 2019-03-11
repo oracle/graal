b7077c325d1711d9b6e25ea1a3cd8a812b90944b
Peter Hofer <peter.hofer@oracle.com>
2019-03-11 17:26:23 +0100

    Disallow substituting a non-final field with a final field.

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/substitute/AnnotationSubstitutionProcessor.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/substitute/AnnotationSubstitutionProcessor.java
@@ -517,6 +517,7 @@ public class AnnotationSubstitutionProcessor extends SubstitutionProcessor {
             ResolvedJavaField alias = fieldValueRecomputation(annotatedClass, field, field, f);
             if (!alias.equals(field)) {
                 ResolvedJavaField originalField = findOriginalField(f, originalClass, true);
+                guarantee(originalField == null || !(alias.isFinal() && !originalField.isFinal()), "a non-final field cannot be redeclared as final through substitution: %s", field);
                 register(fieldSubstitutions, field, originalField, alias);
             } else {
                 handleAnnotatedFieldInSubstitutionClass(f, originalClass);
@@ -728,7 +729,7 @@ public class AnnotationSubstitutionProcessor extends SubstitutionProcessor {
         RecomputeFieldValue.Kind kind = RecomputeFieldValue.Kind.None;
         Class<?> targetClass = originalClass;
         String targetName = "";
-        boolean isFinal = annotated.isFinal();
+        boolean isFinal = original.isFinal() && annotated.isFinal();
 
         if (recomputeAnnotation != null) {
             kind = recomputeAnnotation.kind();
