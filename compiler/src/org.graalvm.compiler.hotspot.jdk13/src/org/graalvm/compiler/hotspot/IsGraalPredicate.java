c097f3746b8dbc4590df8edf5258b1ffc49deb38
Tom Rodriguez <tom.rodriguez@oracle.com>
2019-03-11 23:50:14 -0700

    Refactor IsGraalPredicate

--- compiler/src/org.graalvm.compiler.hotspot.jdk13/src/org/graalvm/compiler/hotspot/IsGraalPredicate.java
+++ compiler/src/org.graalvm.compiler.hotspot.jdk13/src/org/graalvm/compiler/hotspot/IsGraalPredicate.java
@@ -24,16 +24,48 @@
  */
 package org.graalvm.compiler.hotspot;
 
-import org.graalvm.compiler.debug.GraalError;
+import jdk.vm.ci.hotspot.HotSpotJVMCICompilerFactory;
+import jdk.vm.ci.hotspot.HotSpotJVMCIRuntime;
 
 /**
  * Determines if a given class is a JVMCI or Graal class for the purpose of
  * {@link HotSpotGraalCompilerFactory.Options#CompileGraalWithC1Only}.
  */
 class IsGraalPredicate extends IsGraalPredicateBase {
+    /**
+     * Module containing {@link HotSpotJVMCICompilerFactory}.
+     */
+    private final Module jvmciModule;
+
+    /**
+     * Module containing {@link HotSpotGraalCompilerFactory}.
+     */
+    private final Module graalModule;
+
+    /**
+     * Module containing the {@linkplain CompilerConfigurationFactory#selectFactory selected}
+     * configuration.
+     */
+    private Module compilerConfigurationModule;
+
+    IsGraalPredicate() {
+        jvmciModule = HotSpotJVMCICompilerFactory.class.getModule();
+        graalModule = HotSpotGraalCompilerFactory.class.getModule();
+    }
+
+    @Override
+    void onCompilerConfigurationFactorySelection(HotSpotJVMCIRuntime runtime, CompilerConfigurationFactory factory) {
+        compilerConfigurationModule = factory.getClass().getModule();
+    }
 
     @Override
     boolean apply(Class<?> declaringClass) {
-        throw GraalError.shouldNotReachHere("must have versioned implementation");
+        Module module = declaringClass.getModule();
+        return jvmciModule == module || graalModule == module || compilerConfigurationModule == module;
+    }
+
+    @Override
+    HotSpotJVMCICompilerFactory.CompilationLevelAdjustment getCompilationLevelAdjustment() {
+        return HotSpotJVMCICompilerFactory.CompilationLevelAdjustment.ByHolder;
     }
 }
