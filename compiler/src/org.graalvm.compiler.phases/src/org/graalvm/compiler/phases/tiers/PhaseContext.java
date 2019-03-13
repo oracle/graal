7497a45c99ada2c5ec449e197d2fe8f1911f5593
Tom Rodriguez <tom.rodriguez@oracle.com>
2019-03-13 12:31:54 -0700

    PhaseContext should wrap a CoreProvider

--- compiler/src/org.graalvm.compiler.phases/src/org/graalvm/compiler/phases/tiers/PhaseContext.java
+++ compiler/src/org.graalvm.compiler.phases/src/org/graalvm/compiler/phases/tiers/PhaseContext.java
@@ -24,12 +24,56 @@
  */
 package org.graalvm.compiler.phases.tiers;
 
+import org.graalvm.compiler.core.common.spi.ConstantFieldProvider;
+import org.graalvm.compiler.core.common.spi.ForeignCallsProvider;
 import org.graalvm.compiler.nodes.spi.CoreProviders;
-import org.graalvm.compiler.nodes.spi.CoreProvidersImpl;
+import org.graalvm.compiler.nodes.spi.LoweringProvider;
+import org.graalvm.compiler.nodes.spi.Replacements;
+import org.graalvm.compiler.nodes.spi.StampProvider;
 
-public class PhaseContext extends CoreProvidersImpl {
+import jdk.vm.ci.meta.ConstantReflectionProvider;
+import jdk.vm.ci.meta.MetaAccessProvider;
+
+public class PhaseContext implements CoreProviders {
+
+    private final CoreProviders providers;
 
     public PhaseContext(CoreProviders providers) {
-        super(providers);
+        this.providers = providers;
+    }
+
+    @Override
+    public MetaAccessProvider getMetaAccess() {
+        return providers.getMetaAccess();
+    }
+
+    @Override
+    public ConstantReflectionProvider getConstantReflection() {
+        return providers.getConstantReflection();
+    }
+
+    @Override
+    public ConstantFieldProvider getConstantFieldProvider() {
+        return providers.getConstantFieldProvider();
+    }
+
+    @Override
+    public LoweringProvider getLowerer() {
+        return providers.getLowerer();
+    }
+
+    @Override
+    public Replacements getReplacements() {
+        return providers.getReplacements();
+    }
+
+    @Override
+    public StampProvider getStampProvider() {
+        return providers.getStampProvider();
+    }
+
+    @Override
+    public ForeignCallsProvider getForeignCalls() {
+        return providers.getForeignCalls();
     }
 }
