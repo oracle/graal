a92e2d0c8a061708ae62fb6b1afa0204b340d46a
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2019-03-15 11:46:32 +0100

    Renaming to HostAccessPolicy also in Graal compiler suite

--- compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/ValueToHostClassCastExceptionDeoptTest.java
+++ compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/ValueToHostClassCastExceptionDeoptTest.java
@@ -38,7 +38,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.oracle.truffle.api.Truffle;
-import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.HostAccessPolicy;
 
 public class ValueToHostClassCastExceptionDeoptTest {
     private static TruffleRuntimeOptionsOverrideScope backgroundCompilationScope;
@@ -80,7 +80,7 @@ public class ValueToHostClassCastExceptionDeoptTest {
         final GraalTruffleRuntime runtime = (GraalTruffleRuntime) Truffle.getRuntime();
         final CompilationCountingListener listener = new CompilationCountingListener();
         runtime.addListener(listener);
-        final Value toString = Context.newBuilder().allowHostAccess(HostAccess.PUBLIC).build().asValue(String.class).getMember("toString");
+        final Value toString = Context.newBuilder().allowHostAccess(HostAccessPolicy.ALL).build().asValue(String.class).getMember("toString");
         for (int i = 0; i < 10; i++) {
             try {
                 toString.asNativePointer();
