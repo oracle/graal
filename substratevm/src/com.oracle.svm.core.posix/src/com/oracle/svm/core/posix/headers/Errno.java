799605e8bdda4cbdfa70dd75c8c87b3525543093
Josef Eisl <josef.eisl@oracle.com>
2019-03-15 12:14:38 +0100

    svm: user ErrnoDirectives for Errno

--- substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/headers/Errno.java
+++ substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/headers/Errno.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,7 @@
  */
 package com.oracle.svm.core.posix.headers;
 
+import com.oracle.svm.core.ErrnoDirectives;
 import org.graalvm.nativeimage.Platform;
 import org.graalvm.nativeimage.Platforms;
 import org.graalvm.nativeimage.c.CContext;
@@ -39,7 +40,7 @@ import com.oracle.svm.core.annotate.Uninterruptible;
 /**
  * Definitions manually translated from the C header file sys/errno.h.
  */
-@CContext(PosixDirectives.class)
+@CContext(ErrnoDirectives.class)
 public class Errno {
 
     // /** Operation failed. */
