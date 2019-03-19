76b58ac3b7e1517f480339423dcc3565f249534e
Josef Eisl <josef.eisl@oracle.com>
2019-03-19 11:20:53 +0100

    svm/winfi: ignore ErrnoNFITest only if on Windows and in AOT mode

--- truffle/src/com.oracle.truffle.nfi.test/src/com/oracle/truffle/nfi/test/ErrnoNFITest.java
+++ truffle/src/com.oracle.truffle.nfi.test/src/com/oracle/truffle/nfi/test/ErrnoNFITest.java
@@ -42,6 +42,7 @@ package com.oracle.truffle.nfi.test;
 
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.TruffleOptions;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.ForeignAccess;
 import com.oracle.truffle.api.interop.InteropException;
@@ -64,7 +65,7 @@ public class ErrnoNFITest extends NFITest {
 
     @Before
     public void checkOS() {
-        Assume.assumeFalse("Testcase not supported on Windows (GR-14522)", IS_WINDOWS);
+        Assume.assumeFalse("Testcase not supported on Windows on SVM (GR-14522)", IS_WINDOWS && TruffleOptions.AOT);
     }
 
     @TruffleBoundary
