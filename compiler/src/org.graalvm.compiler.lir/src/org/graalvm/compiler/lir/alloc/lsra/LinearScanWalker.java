91d925214a643385ad9d0903fdde282fddbde1e3
Doug Simon <doug.simon@oracle.com>
2019-03-25 21:33:04 +0100

    suppress Fortify false positive

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/alloc/lsra/LinearScanWalker.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/alloc/lsra/LinearScanWalker.java
@@ -878,7 +878,9 @@ class LinearScanWalker extends IntervalWalker {
                 break;
             }
 
-            // TODO: `reg` can be null
+            // Fortify: Suppress Null Dereference false positive
+            assert reg != null;
+
             boolean needSplit = blockPos[reg.number] <= intervalTo;
 
             int splitPos = blockPos[reg.number];
