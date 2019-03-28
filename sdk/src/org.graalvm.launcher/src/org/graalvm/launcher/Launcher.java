8ac62a3cbbf33d7c51cc60353fb0bba482c4f28b
Benoit Daloze <benoit.daloze@oracle.com>
2019-03-28 17:16:19 +0100

    Print --help:vm together with the rest of the help
    * Which makes --help:vm work with the Bash launchers too.

--- sdk/src/org.graalvm.launcher/src/org/graalvm/launcher/Launcher.java
+++ sdk/src/org.graalvm.launcher/src/org/graalvm/launcher/Launcher.java
@@ -116,6 +116,7 @@ public abstract class Launcher {
     private boolean helpExpert;
     private boolean helpTools;
     private boolean helpLanguages;
+    private boolean helpVM;
     private boolean seenPolyglot;
     private boolean experimentalOptions = false;
     private Path logFile;
@@ -498,6 +499,7 @@ public abstract class Launcher {
         return home;
     }
 
+    // Run by the final process, after the potential execve()
     final boolean runPolyglotAction() {
         OptionCategory helpCategory = helpInternal ? OptionCategory.INTERNAL : (helpExpert ? OptionCategory.EXPERT : OptionCategory.USER);
 
@@ -511,7 +513,7 @@ public abstract class Launcher {
             case None:
                 break;
         }
-        boolean printDefaultHelp = help || ((helpExpert || helpInternal) && !helpTools && !helpLanguages);
+        boolean printDefaultHelp = help || ((helpExpert || helpInternal) && !helpTools && !helpLanguages && !helpVM);
         if (printDefaultHelp) {
             final VMType defaultVMType = BASH_LAUNCHER ? VMType.JVM : this.getDefaultVMType();
 
@@ -563,7 +565,15 @@ public abstract class Launcher {
             printInstrumentOptions(getTempEngine(), helpCategory);
         }
 
-        if (printDefaultHelp || helpLanguages || helpTools) {
+        if (helpVM) {
+            if (nativeAccess == null) {
+                printJvmHelp();
+            } else {
+                nativeAccess.printNativeHelp();
+            }
+        }
+
+        if (printDefaultHelp || helpLanguages || helpTools || helpVM) {
             System.out.println("\nSee http://www.graalvm.org for more information.");
             return true;
         }
@@ -650,6 +660,9 @@ public abstract class Launcher {
                 return true;
             case "--help:languages":
                 helpLanguages = true;
+                return true;
+            case "--help:vm":
+                helpVM = true;
                 return true;
             case "--version:graalvm":
                 versionAction = VersionAction.PrintAndExit;
@@ -782,6 +795,7 @@ public abstract class Launcher {
         options.add("--help:tools");
         options.add("--help:expert");
         options.add("--help:internal");
+        options.add("--help:vm");
         options.add("--version:graalvm");
         options.add("--show-version:graalvm");
         addOptions(engine.getOptions(), options);
@@ -1061,6 +1075,8 @@ public abstract class Launcher {
     private static final String CLASSPATH = System.getProperty("org.graalvm.launcher.classpath");
 
     class Native {
+        // execve() to JVM/polyglot from native if needed.
+        // Only parses --jvm/--native to find the VMType and --vm.* to pass/set the VM options.
         void maybeExec(List<String> args, boolean isPolyglot, Map<String, String> polyglotOptions, VMType defaultVmType) {
             assert isAOT();
             VMType vmType = null;
@@ -1099,17 +1115,15 @@ public abstract class Launcher {
                             throw abort("'--jvm.*' options are deprecated and only supported when this launcher is part of a GraalVM.");
                         }
                     }
+                    vmType = VMType.JVM;
                     if (arg.equals("--jvm.help")) {
                         if (defaultVmType == VMType.JVM) {
                             warn("'--jvm.help' is deprecated, use '--help:vm' instead.");
                         } else {
                             warn("'--jvm.help' is deprecated, use '--jvm --help:vm' instead.");
                         }
-                        printJvmHelp();
-                        throw exit();
-                    }
-                    vmType = VMType.JVM;
-                    if (arg.startsWith("--jvm.")) {
+                        remainingArgs.add("--help:vm");
+                    } else if (arg.startsWith("--jvm.")) {
                         if (!jvmDotWarned) {
                             warn("'--jvm.*' options are deprecated, use '--vm.*' instead.");
                             jvmDotWarned = true;
@@ -1141,10 +1155,8 @@ public abstract class Launcher {
                         } else {
                             warn("'--native.help' is deprecated, use '--native --help:vm' instead.");
                         }
-                        printNativeHelp();
-                        throw exit();
-                    }
-                    if (arg.startsWith("--native.")) {
+                        remainingArgs.add("--help:vm");
+                    } else if (arg.startsWith("--native.")) {
                         if (!jvmDotWarned) {
                             warn("'--native.*' options are deprecated, use '--vm.*' instead.");
                             jvmDotWarned = true;
@@ -1155,7 +1167,7 @@ public abstract class Launcher {
                 } else if (arg.startsWith("--vm.") && arg.length() > "--vm.".length()) {
                     if (arg.equals("--vm.help")) {
                         warn("'--vm.help' is deprecated, use '--help:vm' instead.");
-                        printVmHelp(defaultVmType, vmType);
+                        remainingArgs.add("--help:vm");
                     }
                     String vmArg = arg.substring("--vm.".length());
                     if (vmArg.equals("classpath")) {
@@ -1174,9 +1186,6 @@ public abstract class Launcher {
                     iterator.remove();
                 } else if (arg.equals("--polyglot")) {
                     polyglot = true;
-                } else if (arg.equals("--help:vm")) {
-                    printVmHelp(defaultVmType, vmType);
-                    return;
                 } else {
                     remainingArgs.add(arg);
                 }
@@ -1213,17 +1222,6 @@ public abstract class Launcher {
                 }
                 execNativePolyglot(remainingArgs, polyglotOptions);
             }
-        }
-
-        private void printVmHelp(VMType defaultVmType, VMType vmType) {
-            VMType helpType = vmType != null ? vmType : defaultVmType;
-            if (helpType == VMType.JVM) {
-                printJvmHelp();
-            } else {
-                assert helpType == VMType.Native;
-                printNativeHelp();
-            }
-            throw exit();
         }
 
         private WeakReference<OptionDescriptors> compilerOptionDescriptors;
