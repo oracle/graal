3119f752838caa291861786c961026d8e1538545
Paul Wögerer <paul.woegerer@oracle.com>
2019-03-29 22:05:25 +0100

    Workaround spotbugs false positive

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/FallbackExecutor.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/FallbackExecutor.java
@@ -37,6 +37,7 @@ import org.graalvm.compiler.options.Option;
 import org.graalvm.nativeimage.ProcessProperties;
 
 import com.oracle.svm.core.option.HostedOptionKey;
+import com.oracle.svm.core.util.VMError;
 
 /**
  * This class is used to generate fallback images in case we are unable to build standalone images.
@@ -70,7 +71,11 @@ public class FallbackExecutor {
                 command.add(p);
             }
         }
-        String pathPrefix = Paths.get(ProcessProperties.getExecutableName()).getParent().toAbsolutePath().normalize().toString();
+        Path fallbackImageDir = Paths.get(ProcessProperties.getExecutableName()).getParent();
+        if (fallbackImageDir == null) {
+            VMError.shouldNotReachHere();
+        }
+        String pathPrefix = fallbackImageDir.toAbsolutePath().normalize().toString();
         String relativeClasspath = Options.FallbackExecutorClasspath.getValue();
         String[] split = SubstrateUtil.split(relativeClasspath, File.pathSeparator);
         for (int i = 0; i < split.length; i++) {
