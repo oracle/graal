5aaca88b5406ddc55479d3ef7913f0940ca72317
thomas.garcia <thomas.garcia@oracle.com>
2019-04-04 16:43:53 +0200

    Some more testing for access checks. Implemented reflective interface method calls.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_sun_reflect_NativeMethodAccessorImpl.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_sun_reflect_NativeMethodAccessorImpl.java
@@ -280,7 +280,16 @@ public final class Target_sun_reflect_NativeMethodAccessorImpl {
                     // resolve interface call
                     // Match resolution errors with those thrown due to reflection inlining
                     // Linktime resolution & IllegalAccessCheck already done by Class.getMethod()
-                    throw EspressoError.unimplemented("reflective interface calls");
+                    method = reflectedMethod;
+                    method = targetKlass.itableLookup(method.getDeclaringKlass(), method.getITableIndex());
+                    if (method != null) {
+                        // Check for abstract methods as well
+                        if (!method.hasCode()) {
+                            // new default: 65315
+                            throw meta.throwExWithCause(meta.InvocationTargetException, Meta.initEx(meta.AbstractMethodError));
+                        }
+                    }
+                    // throw EspressoError.unimplemented("reflective interface calls");
                     // This is what it should look like for interfaces.
                     // try {
                     // method = resolveInterfaceCall(klass, reflectedMethod, targetKlass, receiver);
@@ -295,7 +304,6 @@ public final class Target_sun_reflect_NativeMethodAccessorImpl {
                     method = reflectedMethod;
                     // VTable is live, use it
                     method = targetKlass.vtableLookup(method.getVTableIndex());
-                    // No vtable in Espresso, just lookup.
                     if (method != null) {
                         // Check for abstract methods as well
                         if (method.isAbstract()) {
