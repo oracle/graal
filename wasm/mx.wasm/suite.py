8f8bbe7e56d7f6d7f38a25cfa41529243d74e18a
Ergys Dona <ergys.dona@oracle.com>
2019-04-25 18:16:04 +0200

    wasm: add truffle import to mx suite

--- wasm/mx.wasm/suite.py
+++ wasm/mx.wasm/suite.py
@@ -2,4 +2,16 @@ suite = {
   "mxversion" : "5.210.2",
   "name" : "wasm",
   "versionConflictResolution" : "latest",
+
+  "imports" : {
+    "suites" : [
+      {
+        "name" : "truffle",
+        "subdir" : True,
+        "urls": [
+          {"url" : "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind" : "binary"},
+        ],
+      },
+    ],
+  },
 }
