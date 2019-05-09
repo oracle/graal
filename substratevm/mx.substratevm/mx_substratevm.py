e7e6633d99224d3a1669d771ef83c6345b125237
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2019-05-09 03:40:15 +0200

    Rename Native Image license file.

--- substratevm/mx.substratevm/mx_substratevm.py
+++ substratevm/mx.substratevm/mx_substratevm.py
@@ -1025,7 +1025,7 @@ mx_sdk.register_graalvm_component(mx_sdk.GraalVmJreComponent(
     short_name='ni',
     dir_name='svm',
     installable_id='native-image',
-    license_files=['LICENSE_NATIVEIMAGE'],
+    license_files=['LICENSE_NATIVEIMAGE.txt'],
     third_party_license_files=[],
     support_distributions=['substratevm:NATIVE_IMAGE_GRAALVM_SUPPORT'],
     launcher_configs=[
