abd33f28f4302a64571e26c1e53090662c6c86a1
Ergys Dona <ergys.dona@oracle.com>
2019-05-12 18:10:54 +0200

    tests: add wat2wasm tool

---
+++ wasm/src/com.oracle.truffle.wasm.test/src/com/oracle/truffle/wasm/test/WasmTestToolkit.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.oracle.truffle.wasm.test;
+
+import org.junit.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+public class WasmTestToolkit {
+
+    public static byte[] compileWat(String program) throws IOException, InterruptedException {
+        Assert.assertNotNull(
+                String.format("The %s variable must be set in order to be able to compile .wat to .wasm", WasmTestOptions.WAT2WASM_ENV_VAR),
+                System.getenv(WasmTestOptions.WAT2WASM_ENV_VAR));
+        // the wat2wasm tool operates on files
+        // create two temporary files for the text and the binary, write the given program to the first one
+        File watFile = File.createTempFile("wasm-text", ".wat");
+        File wasmFile = File.createTempFile("wasm-bin", ".wasm");
+        Files.write(watFile.toPath(), program.getBytes(StandardCharsets.UTF_8), StandardOpenOption.WRITE);
+        // execute the wat2wasm tool and wait for it to finish execution
+        Runtime runtime = Runtime.getRuntime();
+        Process process = runtime.exec(new String[] {
+                System.getenv(WasmTestOptions.WAT2WASM_ENV_VAR),
+                watFile.getPath(),
+                "-o",
+                wasmFile.getPath(),
+        });
+        process.waitFor();
+        // read the resulting binary, delete the temporary files and return
+        byte[] binary = Files.readAllBytes(wasmFile.toPath());
+        Assert.assertTrue(watFile.delete());
+        Assert.assertTrue(wasmFile.delete());
+        return binary;
+    }
+
+}
