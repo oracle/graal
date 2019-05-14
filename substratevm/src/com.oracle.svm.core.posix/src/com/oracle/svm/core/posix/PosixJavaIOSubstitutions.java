cccdb5baa93cdc668ea4e26ab3d8d91961b05acb
Johan Vos <johan.vos@gluonhq.com>
2019-05-14 20:17:56 +0200

    only apply registrations when Java Version >=11

--- substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/PosixJavaIOSubstitutions.java
+++ substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/PosixJavaIOSubstitutions.java
@@ -178,7 +178,7 @@ class PosixJavaIOSubstituteFeature implements Feature {
             JNIRuntimeAccess.register(java.io.IOException.class);
             JNIRuntimeAccess.register(java.io.IOException.class.getDeclaredConstructor(String.class));
             JNIRuntimeAccess.register(access.findClassByName("java.io.UnixFileSystem"));
-            if (!JavaVersionUtil.Java8OrEarlier) {
+            if (JavaVersionUtil.JAVA_SPECIFICATION_VERSION >= 11) {
                 JNIRuntimeAccess.register(java.util.zip.Inflater.class.getDeclaredField("inputConsumed"));
                 JNIRuntimeAccess.register(java.util.zip.Inflater.class.getDeclaredField("outputConsumed"));
             }
