92db8a379a4fffbfcb3010726aa3d2f0034b57a3
Johan Vos <johan.vos@gluonhq.com>
2019-05-14 15:51:51 +0200

    add inputConsumed and outputConsumed field for JNI access. This makes Inflater work with the JNI platform

--- substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/PosixJavaIOSubstitutions.java
+++ substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/PosixJavaIOSubstitutions.java
@@ -178,6 +178,8 @@ class PosixJavaIOSubstituteFeature implements Feature {
             JNIRuntimeAccess.register(java.io.IOException.class);
             JNIRuntimeAccess.register(java.io.IOException.class.getDeclaredConstructor(String.class));
             JNIRuntimeAccess.register(access.findClassByName("java.io.UnixFileSystem"));
+            JNIRuntimeAccess.register(java.util.zip.Inflater.class.getDeclaredField("inputConsumed"));
+            JNIRuntimeAccess.register(java.util.zip.Inflater.class.getDeclaredField("outputConsumed"));
         } catch (NoSuchFieldException | NoSuchMethodException e) {
             VMError.shouldNotReachHere("PosixJavaIOSubstitutionFeature: Error registering class or method: ", e);
         }
