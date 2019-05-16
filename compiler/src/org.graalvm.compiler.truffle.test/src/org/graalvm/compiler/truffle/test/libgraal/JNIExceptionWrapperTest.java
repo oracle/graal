18e8c777399daff141ea83b451e219f9d551efbf
Tomas Zezula <tomas.zezula@oracle.com>
2019-05-16 10:46:56 +0200

    [GR-15891] Fixed JNIExceptionWrapperTest on Windows.

--- compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/libgraal/JNIExceptionWrapperTest.java
+++ compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/libgraal/JNIExceptionWrapperTest.java
@@ -28,14 +28,20 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import com.oracle.truffle.api.nodes.RootNode;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
+import org.graalvm.compiler.core.CompilationWrapper;
+import org.graalvm.compiler.truffle.common.CompilableTruffleAST;
+import org.graalvm.compiler.truffle.common.TruffleCompilation;
+import org.graalvm.compiler.truffle.common.TruffleCompiler;
 import org.graalvm.compiler.truffle.common.TruffleCompilerListener;
+import org.graalvm.compiler.truffle.common.TruffleDebugContext;
+import org.graalvm.compiler.truffle.common.TruffleInliningPlan;
 import org.graalvm.compiler.truffle.compiler.TruffleCompilerImpl;
 import org.graalvm.compiler.truffle.runtime.GraalTruffleRuntime;
-import org.graalvm.compiler.truffle.runtime.GraalTruffleRuntimeListener;
 import org.graalvm.compiler.truffle.runtime.OptimizedCallTarget;
 import org.graalvm.compiler.truffle.runtime.SharedTruffleRuntimeOptions;
-import org.graalvm.compiler.truffle.runtime.TruffleInlining;
 import org.graalvm.compiler.truffle.runtime.TruffleRuntimeOptions;
 import org.junit.Test;
 
@@ -45,47 +51,62 @@ public class JNIExceptionWrapperTest {
     @SuppressWarnings({"unused", "try"})
     public void testMergedStackTrace() throws Exception {
         try (TruffleRuntimeOptions.TruffleRuntimeOptionsOverrideScope runtimeScope = TruffleRuntimeOptions.overrideOptions(
-                        SharedTruffleRuntimeOptions.TruffleBackgroundCompilation, false,
-                        SharedTruffleRuntimeOptions.TruffleCompileImmediately, true,
                         SharedTruffleRuntimeOptions.TruffleCompilationExceptionsAreThrown, true)) {
             GraalTruffleRuntime runtime = GraalTruffleRuntime.getRuntime();
             OptimizedCallTarget compilable = (OptimizedCallTarget) runtime.createCallTarget(RootNode.createConstantNode(42));
-            TestListener listener = new TestListener();
-            runtime.addListener(listener);
-            try {
-                compilable.compile(false);
+            TruffleCompiler compiler = runtime.getTruffleCompiler();
+            try (TruffleCompilation compilation = compiler.openCompilation(compilable)) {
+                Map<String, Object> options = new HashMap<>();
+                options.put("CompilationFailureAction", CompilationWrapper.ExceptionAction.Silent);
+                options.put("TruffleCompilationExceptionsAreFatal", false);
+                try (TruffleDebugContext debug = compiler.openDebugContext(options, compilation)) {
+                    compilable.getCompilationProfile();
+                    TruffleInliningPlan inliningPlan = runtime.createInliningPlan(compilable, null);
+                    TestListener listener = new TestListener();
+                    compiler.doCompile(debug, compilation, options, inliningPlan, null, listener);
+                }
             } catch (Throwable t) {
                 String message = t.getMessage();
-                int runtimeIndex = findFrame(message, GraalTruffleRuntime.class.getName(), "doCompile");
+                int runtimeIndex = findFrame(message, JNIExceptionWrapperTest.class, "testMergedStackTrace");
                 assertNotEquals(message, -1, runtimeIndex);
-                int listenerIndex = findFrame(message, TestListener.class.getName(), "onCompilationTruffleTierFinished");
+                int listenerIndex = findFrame(message, TestListener.class, "onTruffleTierFinished");
                 assertNotEquals(message, -1, listenerIndex);
-                int compilerIndex = findFrame(message, TruffleCompilerImpl.class.getName(), "compileAST");
+                int compilerIndex = findFrame(message, TruffleCompilerImpl.class, "compileAST");
                 assertNotEquals(message, -1, compilerIndex);
                 assertTrue(listenerIndex < compilerIndex);
                 assertTrue(compilerIndex < runtimeIndex);
-            } finally {
-                runtime.removeListener(listener);
             }
         }
     }
 
-    private static int findFrame(String message, String className, String methodName) {
+    private static int findFrame(String message, Class<?> clazz, String methodName) {
         String[] lines = message.split("\n");
-        Pattern pattern = Pattern.compile("\\s*at\\s+(.*/)?" + Pattern.quote(className + '.' + methodName), 0);
+        Pattern pattern = Pattern.compile("at\\s+(.*/)?" + Pattern.quote(clazz.getName() + '.' + methodName));
         for (int i = 0; i < lines.length; i++) {
-            if (pattern.matcher(lines[i]).find()) {
+            if (pattern.matcher(lines[i].trim()).find()) {
                 return i;
             }
         }
         return -1;
     }
 
-    private static final class TestListener implements GraalTruffleRuntimeListener {
+    private static final class TestListener implements TruffleCompilerListener {
 
         @Override
-        public void onCompilationTruffleTierFinished(OptimizedCallTarget target, TruffleInlining inliningDecision, TruffleCompilerListener.GraphInfo graph) {
+        public void onTruffleTierFinished(CompilableTruffleAST compilable, TruffleInliningPlan inliningPlan, GraphInfo graph) {
             throw new RuntimeException("Expected exception");
+        }
+
+        @Override
+        public void onGraalTierFinished(CompilableTruffleAST compilable, GraphInfo graph) {
+        }
+
+        @Override
+        public void onSuccess(CompilableTruffleAST compilable, TruffleInliningPlan inliningPlan, GraphInfo graphInfo, CompilationResultInfo compilationResultInfo) {
+        }
+
+        @Override
+        public void onFailure(CompilableTruffleAST compilable, String reason, boolean bailout, boolean permanentBailout) {
         }
     }
 }
