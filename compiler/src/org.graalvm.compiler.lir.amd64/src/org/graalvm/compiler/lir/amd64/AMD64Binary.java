2cfe1751028b84f1b15c501e7015af186e5875ea
Niklas Vangerow <nvangerow@twitter.com>
2019-05-31 15:50:44 -0700

    change annotations for correct register allocation

--- compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64Binary.java
+++ compiler/src/org.graalvm.compiler.lir.amd64/src/org/graalvm/compiler/lir/amd64/AMD64Binary.java
@@ -149,8 +149,8 @@ public class AMD64Binary {
         public static final LIRInstructionClass<VectorAddOp> TYPE = LIRInstructionClass.create(VectorAddOp.class);
 
         @Def({REG}) protected AllocatableValue result;
-        @Use({REG}) protected AllocatableValue x;
-        @Use({REG}) protected AllocatableValue y;
+        @Alive({REG}) protected AllocatableValue x;
+        @Alive({REG}) protected AllocatableValue y;
 
         public VectorAddOp(AllocatableValue result, AllocatableValue x, AllocatableValue y) {
             super(TYPE);
