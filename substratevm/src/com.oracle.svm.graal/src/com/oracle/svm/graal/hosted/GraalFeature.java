66bed12312835828cb51122970ee7cb63e91e1b9
Yudi Zheng <yudi.zheng@oracle.com>
2019-06-05 15:44:17 +0200

    Fix compilation error.

--- substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/hosted/GraalFeature.java
+++ substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/hosted/GraalFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -258,8 +258,8 @@ public final class GraalFeature implements Feature {
         }
 
         @Override
-        protected boolean tryInvocationPlugin(InvokeKind invokeKind, ValueNode[] args, ResolvedJavaMethod targetMethod, JavaKind resultType, JavaType returnType) {
-            boolean result = super.tryInvocationPlugin(invokeKind, args, targetMethod, resultType, returnType);
+        protected boolean tryInvocationPlugin(InvokeKind invokeKind, ValueNode[] args, ResolvedJavaMethod targetMethod, JavaKind resultType) {
+            boolean result = super.tryInvocationPlugin(invokeKind, args, targetMethod, resultType);
             if (result) {
                 CompilationInfoSupport.singleton().registerAsDeoptInlininingExclude(targetMethod);
             }
