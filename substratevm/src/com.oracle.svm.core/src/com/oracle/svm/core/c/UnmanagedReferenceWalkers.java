3bd25de583245fdeca1bb1f360f8ef46aeef0ab4
Peter Hofer <peter.hofer@oracle.com>
2019-06-21 16:59:49 +1000

    Use a hash table for UnmanagedReferenceWalkers.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/c/UnmanagedReferenceWalkers.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/c/UnmanagedReferenceWalkers.java
@@ -40,10 +40,13 @@ import com.oracle.svm.core.c.function.JavaMethodLiteral;
 import com.oracle.svm.core.heap.Heap;
 import com.oracle.svm.core.heap.ObjectReferenceVisitor;
 import com.oracle.svm.core.heap.ObjectReferenceWalker;
+import com.oracle.svm.core.util.VMError;
 
 /**
  * Supports function pointers as {@linkplain ObjectReferenceWalker object reference walkers} for the
  * heap, allowing garbage collection to visit and adjust object references in unmanaged memory.
+ * <p>
+ * Implementation: linear probing hash table adapted from OpenJDK {@link java.util.IdentityHashMap}.
  */
 public class UnmanagedReferenceWalkers extends ObjectReferenceWalker {
     @Fold
@@ -71,15 +74,15 @@ public class UnmanagedReferenceWalkers extends ObjectReferenceWalker {
      * Registers the specified walker function that is later invoked with the provided tag word as
      * its argument. This tag word is typically the address of a data structure in unmanaged memory.
      */
-    public void register(ObjectReferenceWalkerFunction walker, ComparableWord tag) {
+    public boolean register(ObjectReferenceWalkerFunction walker, ComparableWord tag) {
         assert !Heap.getHeap().isAllocationDisallowed();
-        assert walker.isNonNull();
+        assert walker.notEqual(none());
         lock.lock();
         try {
             if (array.isNull()) { // lazily and only when actually used
                 Heap.getHeap().getGC().registerObjectReferenceWalker(this);
             }
-            register0(walker, tag);
+            return register0(walker, tag);
         } finally {
             lock.unlock();
         }
@@ -90,7 +93,7 @@ public class UnmanagedReferenceWalkers extends ObjectReferenceWalker {
      */
     public boolean unregister(ObjectReferenceWalkerFunction walker, ComparableWord tag) {
         assert !Heap.getHeap().isAllocationDisallowed();
-        assert walker.isNonNull();
+        assert walker.notEqual(none());
         lock.lock();
         try {
             return unregister0(walker, tag);
@@ -100,53 +103,138 @@ public class UnmanagedReferenceWalkers extends ObjectReferenceWalker {
     }
 
     @Uninterruptible(reason = "Manipulate walkers list atomically with regard to GC.")
-    private void register0(ObjectReferenceWalkerFunction function, ComparableWord tag) {
+    private boolean register0(ObjectReferenceWalkerFunction function, ComparableWord tag) {
         if (array.isNull()) {
-            int capacity = 10;
-            array = NonmovableArrays.createWordArray(scale(capacity));
-        } else {
-            if (scale(count) == NonmovableArrays.lengthOf(array)) {
-                NonmovableArray<ComparableWord> oldArray = array;
-                array = NonmovableArrays.createWordArray(scale(count << 1));
-                NonmovableArrays.arraycopy(oldArray, 0, array, 0, scale(count));
-                NonmovableArrays.releaseUnmanagedArray(oldArray);
+            array = NonmovableArrays.createWordArray(64);
+        }
+        int index;
+        boolean resized;
+        do {
+            int length = NonmovableArrays.lengthOf(array);
+            index = hashIndex(function, tag, length);
+            while (NonmovableArrays.getWord(array, index).notEqual(none())) {
+                assert !(NonmovableArrays.getWord(array, index).equal(function) &&
+                                NonmovableArrays.getWord(array, index + 1).equal(tag)) : "Duplicate walkers not allowed";
+                index = nextIndex(index, length);
+            }
+            resized = false;
+            int newCount = count + 1;
+            if (newCount + (newCount << 1) > length) { // enforce load factor
+                resized = resize(length);
+            }
+        } while (resized);
+        NonmovableArrays.setWord(array, index, function);
+        NonmovableArrays.setWord(array, index + 1, tag);
+        count++;
+        return true;
+    }
+
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    private boolean resize(int capacity) {
+        final int maxCapacity = 1 << 29;
+        int oldLength = NonmovableArrays.lengthOf(array);
+        if (oldLength == 2 * maxCapacity) {
+            VMError.guarantee(count <= maxCapacity - 1, "Maximum capacity exhausted");
+            return false;
+        }
+        int newLength = 2 * capacity;
+        if (oldLength >= newLength) {
+            return false;
+        }
+        NonmovableArray<ComparableWord> oldArray = array;
+        array = NonmovableArrays.createWordArray(newLength);
+        for (int i = 0; i < oldLength; i += 2) {
+            ComparableWord function = NonmovableArrays.getWord(oldArray, i);
+            if (function.notEqual(none())) {
+                NonmovableArrays.setWord(oldArray, i, none());
+                ComparableWord tag = NonmovableArrays.getWord(oldArray, i + 1);
+                int u = hashIndex(function, tag, newLength);
+                while (NonmovableArrays.getWord(array, u).notEqual(none())) {
+                    u = nextIndex(u, newLength);
+                }
+                NonmovableArrays.setWord(array, u, function);
+                NonmovableArrays.setWord(array, u + 1, tag);
             }
         }
-        NonmovableArrays.setWord(array, scale(count), function);
-        NonmovableArrays.setWord(array, scale(count) + 1, tag);
-        count++;
+        NonmovableArrays.releaseUnmanagedArray(oldArray);
+        return true;
     }
 
     @Uninterruptible(reason = "Manipulate walkers list atomically with regard to GC.")
     private boolean unregister0(ObjectReferenceWalkerFunction function, ComparableWord tag) {
         if (array.isNonNull()) {
-            for (int i = 0; i < count; i++) {
-                ObjectReferenceWalkerFunction fn = (ObjectReferenceWalkerFunction) NonmovableArrays.getWord(array, scale(i));
-                if (fn.equal(function) && tag.equal(NonmovableArrays.getWord(array, 1 + scale(i)))) {
-                    NonmovableArrays.arraycopy(array, scale(i + 1), array, scale(i), scale(count - 1 - i));
+            int length = NonmovableArrays.lengthOf(array);
+            int index = hashIndex(function, tag, length);
+            ComparableWord entryFunction = NonmovableArrays.getWord(array, index);
+            while (entryFunction.notEqual(none())) {
+                ComparableWord entryTag = NonmovableArrays.getWord(array, index + 1);
+                if (entryFunction.equal(function) && entryTag.equal(tag)) {
+                    NonmovableArrays.setWord(array, index, none());
+                    NonmovableArrays.setWord(array, index + 1, none());
                     count--;
+                    rehashAfterUnregisterAt(index);
                     return true;
                 }
+                index = nextIndex(index, length);
+                entryFunction = NonmovableArrays.getWord(array, index);
             }
         }
         return false;
     }
 
-    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
-    private static int scale(int i) {
-        return i << 1;
+    /** Rehashes possibly-colliding entries after deletion to preserve collision properties. */
+    @Uninterruptible(reason = "Called from uninterruptible code.")
+    private void rehashAfterUnregisterAt(int index) { // from IdentityHashMap: Knuth 6.4 Algorithm R
+        int length = NonmovableArrays.lengthOf(array);
+        int d = index;
+        int i = nextIndex(d, length);
+        ComparableWord function = NonmovableArrays.getWord(array, i);
+        while (function.notEqual(none())) {
+            ComparableWord tag = NonmovableArrays.getWord(array, i + 1);
+            int r = hashIndex(function, tag, length);
+            if ((i < r && (r <= d || d <= i)) || (r <= d && d <= i)) {
+                NonmovableArrays.setWord(array, d, function);
+                NonmovableArrays.setWord(array, d + 1, tag);
+                NonmovableArrays.setWord(array, i, none());
+                NonmovableArrays.setWord(array, i + 1, none());
+                d = i;
+            }
+            i = nextIndex(i, length);
+            function = NonmovableArrays.getWord(array, i);
+        }
     }
 
     @Override
     public boolean walk(ObjectReferenceVisitor visitor) {
         if (array.isNonNull()) {
-            for (int i = 0; i < count; i++) {
-                ObjectReferenceWalkerFunction fun = (ObjectReferenceWalkerFunction) NonmovableArrays.getWord(array, scale(i));
-                ComparableWord tag = NonmovableArrays.getWord(array, scale(i) + 1);
-                fun.invoke(tag, visitor);
+            int length = NonmovableArrays.lengthOf(array);
+            for (int i = 0; i < length; i += 2) {
+                ObjectReferenceWalkerFunction fun = (ObjectReferenceWalkerFunction) NonmovableArrays.getWord(array, i);
+                if (fun.notEqual(none())) {
+                    ComparableWord tag = NonmovableArrays.getWord(array, i + 1);
+                    fun.invoke(tag, visitor);
+                }
             }
         }
         return true;
+    }
+
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    private static ComparableWord none() {
+        return WordFactory.zero();
+    }
+
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    private static int hashIndex(ComparableWord function, ComparableWord tag, int length) {
+        int h = (int) (function.rawValue() >> 32) * 31 + (int) function.rawValue();
+        h = (h * 31 + (int) (tag.rawValue() >>> 32)) * 31 + (int) tag.rawValue();
+        // Multiply by -127, and left-shift to use least bit as part of hash
+        return ((h << 1) - (h << 8)) & (length - 1);
+    }
+
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    private static int nextIndex(int index, int length) {
+        return (index + 2 < length) ? (index + 2) : 0;
     }
 
     @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
