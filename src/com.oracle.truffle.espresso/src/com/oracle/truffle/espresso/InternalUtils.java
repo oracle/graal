6583c5ba5a3843820169c9c71ef3db6956ecdd3a
thomas.garcia <thomas.garcia@oracle.com>
2019-06-25 17:19:53 +0200

    some more representation utils

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/InternalUtils.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/InternalUtils.java
@@ -1,13 +1,42 @@
 package com.oracle.truffle.espresso;
 
+import com.oracle.truffle.espresso.runtime.StaticObject;
+
+/**
+ * Utility class to spy Espresso's underlying representation from the guest. Mostly used to validate
+ * Espresso's design choices from a guest point of view.
+ * 
+ * @see StaticObject
+ */
 public class InternalUtils {
     public static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
 
+    /**
+     * @param obj the object you want to see the underlying representation of primitive fields.
+     * @return a copy of how primitive fields are represented in memory.
+     */
     public static byte[] getUnderlyingFieldArray(@SuppressWarnings("unused") Object obj) {
         return EMPTY_BYTE_ARRAY;
     }
 
+    /**
+     * Returns a String representing all (even private ones) known fields in obj, except for hidden
+     * ones.
+     * 
+     * @param obj the object you want to represent.
+     * @return The representation of the obj.
+     */
     public static String toVerboseString(@SuppressWarnings("unused") Object obj) {
         return obj.toString();
+    }
+
+    /**
+     * Returns the total number of bytes an instance of clazz takes up in memory.
+     * 
+     * @param clazz the class you want to know the memory consumption of
+     * @return the total number of bytes used by an instance of clazz.
+     */
+    public static int bytesUsed(Class<?> clazz) {
+        return 0;
     }
 }
--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_com_oracle_truffle_espresso_InternalUtils.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_com_oracle_truffle_espresso_InternalUtils.java
@@ -1,10 +1,11 @@
 package com.oracle.truffle.espresso.substitutions;
 
+import com.oracle.truffle.espresso.impl.Klass;
+import com.oracle.truffle.espresso.impl.ObjectKlass;
+import com.oracle.truffle.espresso.meta.JavaKind;
 import com.oracle.truffle.espresso.runtime.StaticObject;
+import sun.misc.Unsafe;
 
-/**
- * Utility class to spy Espresso's underlying representation from the guest.
- */
 @EspressoSubstitutions
 public class Target_com_oracle_truffle_espresso_InternalUtils {
 
@@ -16,5 +17,33 @@ public class Target_com_oracle_truffle_espresso_InternalUtils {
     @Substitution
     public static @Host(String.class) StaticObject toVerboseString(@Host(Object.class) StaticObject self) {
         return self.getKlass().getMeta().toGuestString(self.toVerboseString());
+    }
+
+    @Substitution
+    public static int bytesUsed(@Host(Class.class) StaticObject clazz) {
+        Klass k = clazz.getMirrorKlass();
+        int total = 0;
+        if (k.isArray()) {
+            // ArrayKlass reference
+            total += JavaKind.Int.getByteCount();
+            // null reference for primitive field array
+            total += JavaKind.Int.getByteCount();
+            // Header of the Object field array + storing its reference
+            total += Unsafe.ARRAY_OBJECT_BASE_OFFSET + JavaKind.Int.getByteCount();
+            return total;
+        } else {
+            ObjectKlass klass = (ObjectKlass) k;
+            // Bytes used by the primitive fields
+            total += klass.getPrimitiveFieldTotalByteCount();
+            // Bytes used by the Object fields
+            total += klass.getObjectFieldsCount() * JavaKind.Int.getByteCount();
+            // Header of the primitive field array + storing its reference
+            total += Unsafe.ARRAY_BYTE_BASE_OFFSET + JavaKind.Int.getByteCount();
+            // Header of the Object field array + storing its reference
+            total += Unsafe.ARRAY_OBJECT_BASE_OFFSET + JavaKind.Int.getByteCount();
+            // Reference to the Klass object.
+            total += JavaKind.Int.getByteCount();
+            return total;
+        }
     }
 }
