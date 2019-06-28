b52f9b838c316a16a2bb4db267875f91d0e251e3
Josef Haider <josef.haider@oracle.com>
2019-06-28 14:34:04 +0200

    TRegex: use singleton Tokens in RegexLexer

--- regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/Token.java
+++ regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/Token.java
@@ -33,8 +33,73 @@ import com.oracle.truffle.regex.tregex.util.json.JsonObject;
 
 public class Token implements JsonConvertible {
 
-    public static Token create(Kind kind) {
-        return new Token(kind);
+    public enum Kind {
+        caret,
+        dollar,
+        wordBoundary,
+        nonWordBoundary,
+        backReference,
+        quantifier,
+        alternation,
+        captureGroupBegin,
+        nonCaptureGroupBegin,
+        lookAheadAssertionBegin,
+        lookBehindAssertionBegin,
+        groupEnd,
+        charClass
+    }
+
+    private static final Token CARET = new Token(Kind.caret);
+    private static final Token DOLLAR = new Token(Kind.dollar);
+    private static final Token WORD_BOUNDARY = new Token(Kind.wordBoundary);
+    private static final Token NON_WORD_BOUNDARY = new Token(Kind.nonWordBoundary);
+    private static final Token ALTERNATION = new Token(Kind.alternation);
+    private static final Token CAPTURE_GROUP_BEGIN = new Token(Kind.captureGroupBegin);
+    private static final Token NON_CAPTURE_GROUP_BEGIN = new Token(Kind.nonCaptureGroupBegin);
+    private static final Token LOOK_AHEAD_ASSERTION_BEGIN = new LookAheadAssertionBegin(false);
+    private static final Token NEGATIVE_LOOK_AHEAD_ASSERTION_BEGIN = new LookAheadAssertionBegin(true);
+    private static final Token LOOK_BEHIND_ASSERTION_BEGIN = new LookBehindAssertionBegin(false);
+    private static final Token NEGATIVE_LOOK_BEHIND_ASSERTION_BEGIN = new LookBehindAssertionBegin(true);
+    private static final Token GROUP_END = new Token(Kind.groupEnd);
+
+    public static Token createCaret() {
+        return CARET;
+    }
+
+    public static Token createDollar() {
+        return DOLLAR;
+    }
+
+    public static Token createWordBoundary() {
+        return WORD_BOUNDARY;
+    }
+
+    public static Token createNonWordBoundary() {
+        return NON_WORD_BOUNDARY;
+    }
+
+    public static Token createAlternation() {
+        return ALTERNATION;
+    }
+
+    public static Token createCaptureGroupBegin() {
+        return CAPTURE_GROUP_BEGIN;
+    }
+
+    public static Token createNonCaptureGroupBegin() {
+        return NON_CAPTURE_GROUP_BEGIN;
+    }
+
+    public static Token createLookAheadAssertionBegin() {
+        return LOOK_AHEAD_ASSERTION_BEGIN;
+    }
+
+    public static Token createLookBehindAssertionBegin() {
+        return LOOK_BEHIND_ASSERTION_BEGIN;
+    }
+
+    public static Token createGroupEnd() {
+        return GROUP_END;
     }
 
     public static Token createBackReference(int groupNr) {
@@ -50,27 +115,11 @@ public class Token implements JsonConvertible {
     }
 
     public static Token createLookAheadAssertionBegin(boolean negated) {
-        return new LookAheadAssertionBegin(negated);
+        return negated ? NEGATIVE_LOOK_AHEAD_ASSERTION_BEGIN : LOOK_AHEAD_ASSERTION_BEGIN;
     }
 
     public static Token createLookBehindAssertionBegin(boolean negated) {
-        return new LookBehindAssertionBegin(negated);
-    }
-
-    public enum Kind {
-        caret,
-        dollar,
-        wordBoundary,
-        nonWordBoundary,
-        backReference,
-        quantifier,
-        alternation,
-        captureGroupBegin,
-        nonCaptureGroupBegin,
-        lookAheadAssertionBegin,
-        lookBehindAssertionBegin,
-        groupEnd,
-        charClass
+        return negated ? NEGATIVE_LOOK_BEHIND_ASSERTION_BEGIN : LOOK_BEHIND_ASSERTION_BEGIN;
     }
 
     public final Kind kind;
