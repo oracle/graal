b325880c7ebf3c1994693c0d4fd0ed50d67a1a6a
Andreas Woess <andreas.woess@oracle.com>
2019-07-04 17:08:25 +0200

    Remove ICU4J substitutions and register resources instead.

--- substratevm/src/com.oracle.svm.thirdparty/src/com/oracle/svm/thirdparty/ICU4JFeature.java
+++ substratevm/src/com.oracle.svm.thirdparty/src/com/oracle/svm/thirdparty/ICU4JFeature.java
@@ -24,27 +24,16 @@
  */
 package com.oracle.svm.thirdparty;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.function.BooleanSupplier;
 
-import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.ImageSingletons;
-import org.graalvm.nativeimage.hosted.RuntimeReflection;
+import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
+import org.graalvm.nativeimage.hosted.RuntimeReflection;
 
-import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.AutomaticFeature;
-import com.oracle.svm.core.annotate.InjectAccessors;
-import com.oracle.svm.core.annotate.NeverInline;
-import com.oracle.svm.core.annotate.RecomputeFieldValue;
-import com.oracle.svm.core.annotate.RecomputeFieldValue.Kind;
-import com.oracle.svm.core.annotate.Substitute;
-import com.oracle.svm.core.annotate.TargetClass;
+import com.oracle.svm.core.configure.ResourcesRegistry;
 import com.oracle.svm.core.util.VMError;
-
-import com.oracle.svm.hosted.FeatureImpl.FeatureAccessImpl;
 
 /**
  * ICU4JFeature enables ICU4J library ({@link "http://site.icu-project.org/"} to be used in SVM.
@@ -76,6 +65,9 @@ public final class ICU4JFeature implements Feature {
         registerShimClass(access, "com.ibm.icu.text.NumberFormatServiceShim");
         registerShimClass(access, "com.ibm.icu.text.CollatorServiceShim");
         registerShimClass(access, "com.ibm.icu.text.BreakIteratorFactory");
+
+        ResourcesRegistry resourcesRegistry = ImageSingletons.lookup(ResourcesRegistry.class);
+        resourcesRegistry.addResources("com/ibm/icu/impl/data/icudt.*");
     }
 
     private static void registerShimClass(BeforeAnalysisAccess access, String shimClassName) {
@@ -89,143 +81,8 @@ public final class ICU4JFeature implements Feature {
 
     @Override
     public void duringSetup(DuringSetupAccess access) {
-
         // we should fail the native-image build, if any ICU4J class instance
         // made it to the build-time generated heap
-        RuntimeClassInitialization.initializeAtRunTime(getIcu4jClasses(access));
-
-        // ClassLoaderHelper is a utility class used in @TargetClass annotated substitutions bellow
-        RuntimeClassInitialization.initializeAtBuildTime(ClassLoaderHelper.class);
-        RuntimeClassInitialization.initializeAtBuildTime(ClassLoaderHelper.DummyClassLoader.class);
+        RuntimeClassInitialization.initializeAtRunTime("com.ibm.icu");
     }
-
-    private static Class<?>[] getIcu4jClasses(FeatureAccess access) {
-        List<Class<?>> allClasses = ((FeatureAccessImpl) access).findSubclasses(Object.class);
-        return allClasses.stream().filter(clazz -> clazz.getName().startsWith("com.ibm.icu")).toArray(Class<?>[]::new);
-    }
-}
-
-// Checkstyle: stop
-final class ClassLoaderHelper {
-
-    static final class DummyClassLoader extends ClassLoader {
-        DummyClassLoader(ClassLoader parent) {
-            super(parent);
-        }
-    }
-
-    /** Dummy ClassLoader used only for resource loading. */
-    static final ClassLoader DUMMY_LOADER = new DummyClassLoader(null);
-}
-// CheckStyle: resume
-
-@TargetClass(className = "com.ibm.icu.impl.ClassLoaderUtil", onlyWith = ICU4JFeature.IsEnabled.class)
-final class Target_com_ibm_icu_impl_ClassLoaderUtil {
-    @Substitute
-    // Checkstyle: stop
-    public static ClassLoader getClassLoader() {
-        return ClassLoaderHelper.DUMMY_LOADER;
-    }
-    // Checkstyle: resume
-}
-
-@TargetClass(className = "com.ibm.icu.impl.ICUBinary", onlyWith = ICU4JFeature.IsEnabled.class)
-final class Target_com_ibm_icu_impl_ICUBinary {
-
-    @Alias
-    static native void addDataFilesFromPath(String dataPath, List<?> files);
-
-    @Alias @InjectAccessors(IcuDataFilesAccessors.class) static List<?> icuDataFiles;
-
-    static final class IcuDataFilesAccessors {
-
-        private static final String ICU4J_DATA_PATH_SYS_PROP = "com.ibm.icu.impl.ICUBinary.dataPath";
-        private static final String ICU4J_DATA_PATH_ENV_VAR = "ICU4J_DATA_PATH";
-
-        private static final String NO_DATA_PATH_ERR_MSG = "No ICU4J data path was set or found. This will likely end up with a MissingResourceException. " +
-                        "To take advantage of the ICU4J library, you should either set system property, " +
-                        ICU4J_DATA_PATH_SYS_PROP +
-                        ", or set environment variable, " +
-                        ICU4J_DATA_PATH_ENV_VAR +
-                        ", to contain path to your ICU4J icudt directory";
-
-        // once fully populated, the data file list will be kept here
-        private static volatile List<?> instance = null;
-        // helper collection to which the list will be populated
-        private static List<?> populatingDataFiles = null;
-
-        private static final Object lock = new Object();
-
-        @NeverInline("So the lock does not get eliminated.")
-        static List<?> get() {
-
-            if (instance == null) {
-                // Checkstyle: allow synchronization
-                synchronized (lock) {
-                    if (instance == null) {
-                        if (populatingDataFiles == null) {
-                            // the very first call, from "outside"
-                            populatingDataFiles = new ArrayList<>();
-                        } else {
-                            // second, re-entrant, call from the same thread
-                            // comes from the addDataFilesFromPath method invoked bellow
-                            return populatingDataFiles;
-                        }
-
-                        String dataPath = System.getProperty(ICU4J_DATA_PATH_SYS_PROP);
-                        if (dataPath == null || dataPath.isEmpty()) {
-                            dataPath = System.getenv(ICU4J_DATA_PATH_ENV_VAR);
-                        }
-                        if (dataPath != null && !dataPath.isEmpty()) {
-                            addDataFilesFromPath(dataPath, populatingDataFiles);
-                        } else {
-                            System.err.println(NO_DATA_PATH_ERR_MSG);
-                        }
-                        instance = populatingDataFiles;
-                    }
-                }
-                // Checkstyle: disallow synchronization
-            }
-            return instance;
-        }
-
-        /*
-         * Any attempt to write to the list should be handled as no-op. The list of ICU4J resource
-         * bundle files should be strictly defined by one of the above mentioned system property,
-         * ICU4J_DATA_PATH_SYS_PROP, or environment variable, ICU4J_DATA_PATH_ENV_VAR.
-         */
-        static void set(@SuppressWarnings("unused") List<?> bummer) {
-        }
-    }
-}
-
-@TargetClass(className = "com.ibm.icu.impl.ICUResourceBundle", onlyWith = ICU4JFeature.IsEnabled.class)
-final class Target_com_ibm_icu_impl_ICUResourceBundle {
-    @Alias @RecomputeFieldValue(kind = Kind.FromAlias, isFinal = true)
-    // Checkstyle: stop
-    private static ClassLoader ICU_DATA_CLASS_LOADER = ClassLoaderHelper.DUMMY_LOADER;
-    // Checkstyle: resume
-
-    @SuppressWarnings("unused")
-    @Substitute
-    // Checkstyle: stop
-    private static void addBundleBaseNamesFromClassLoader(final String bn, final ClassLoader root, final Set<String> names) {
-    }
-    // Checkstyle: resume
-}
-
-@TargetClass(className = "com.ibm.icu.impl.ICUResourceBundle$WholeBundle", onlyWith = ICU4JFeature.IsEnabled.class)
-final class Target_com_ibm_icu_impl_ICUResourceBundle_WholeBundle {
-    @Alias @RecomputeFieldValue(kind = Kind.Reset)
-    // Checkstyle: stop
-    ClassLoader loader;
-    // Checkstyle: resume
-}
-
-@TargetClass(className = "com.ibm.icu.impl.ICUResourceBundle$AvailEntry", onlyWith = ICU4JFeature.IsEnabled.class)
-final class Target_com_ibm_icu_impl_ICUResourceBundle_AvailEntry {
-    @Alias @RecomputeFieldValue(kind = Kind.Reset)
-    // Checkstyle: stop
-    ClassLoader loader;
-    // Checkstyle: resume
 }
