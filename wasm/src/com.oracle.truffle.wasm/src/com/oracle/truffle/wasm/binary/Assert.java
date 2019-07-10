5ffa8f5032e26012042c1fb1b52513c9dc4ee0d5
Ergys Dona <ergys.dona@oracle.com>
2019-07-10 18:33:28 +0200

    Add long version of Assert.assertLess and Assert.assertLarger.

--- wasm/src/com.oracle.truffle.wasm/src/com/oracle/truffle/wasm/binary/Assert.java
+++ wasm/src/com.oracle.truffle.wasm/src/com/oracle/truffle/wasm/binary/Assert.java
@@ -64,7 +64,19 @@ public class Assert {
         }
     }
 
+    public static void assertLarger(long n1, long n2, String message) throws BinaryReaderException {
+        if (n1 <= n2) {
+            fail(format("%s: should be larger: %d <= %d", message, n1, n2));
+        }
+    }
+
     public static void assertLess(int n1, int n2, String message) throws BinaryReaderException {
+        if (n1 > n2) {
+            fail(format("%s: should be less: %d > %d", message, n1, n2));
+        }
+    }
+
+    public static void assertLess(long n1, long n2, String message) throws BinaryReaderException {
         if (n1 > n2) {
             fail(format("%s: should be less: %d > %d", message, n1, n2));
         }
