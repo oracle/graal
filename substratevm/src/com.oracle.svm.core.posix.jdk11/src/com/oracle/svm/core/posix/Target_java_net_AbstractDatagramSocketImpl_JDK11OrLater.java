fc6a3947c2a8a5761c574352e1333a166e15cf17
David M. Lloyd <david.lloyd@redhat.com>
2019-07-16 09:14:53 -0500

    Implement AbstractPlainDatagramSocketImpl.isReusePortAvailable0()
    Fixes #1502

--- substratevm/src/com.oracle.svm.core.posix.jdk11/src/com/oracle/svm/core/posix/Target_java_net_AbstractDatagramSocketImpl_JDK11OrLater.java
+++ substratevm/src/com.oracle.svm.core.posix.jdk11/src/com/oracle/svm/core/posix/Target_java_net_AbstractDatagramSocketImpl_JDK11OrLater.java
@@ -30,18 +30,18 @@ import com.oracle.svm.core.jdk.JDK11OrLater;
 import org.graalvm.nativeimage.Platform;
 import org.graalvm.nativeimage.Platforms;
 
-@TargetClass(className = "java.net.AbstractPlainSocketImpl", onlyWith = JDK11OrLater.class)
+@TargetClass(className = "java.net.AbstractPlainDatagramSocketImpl", onlyWith = JDK11OrLater.class)
 @Platforms({Platform.LINUX.class, Platform.DARWIN.class})
-final class Target_java_net_AbstractPlainSocketImpl_JDK11OrLater {
+final class Target_java_net_AbstractDatagramSocketImpl_JDK11OrLater {
     // This class must not hide the "normal" Posix variant, and should be additive
 
     /* Do not re-format commented-out code: @formatter:off */
     // ported from: ./src/java.base/unix/native/libnet/SocketImpl.c
-    //    32  JNIEXPORT jboolean JNICALL
-    //    33  Java_java_net_AbstractPlainSocketImpl_isReusePortAvailable0(JNIEnv* env, jclass c1)
+    //    38  JNIEXPORT jboolean JNICALL
+    //    39  Java_java_net_AbstractPlainDatagramSocketImpl_isReusePortAvailable0(JNIEnv* env, jclass c1)
     @Substitute
     static boolean isReusePortAvailable0() {
-        //    35      return (reuseport_available()) ? JNI_TRUE : JNI_FALSE;
+        //    41      return (reuseport_available()) ? JNI_TRUE : JNI_FALSE;
         return JavaNetNetUtil.reuseport_available();
     }
     /* Do not re-format commented-out code: @formatter:on */
