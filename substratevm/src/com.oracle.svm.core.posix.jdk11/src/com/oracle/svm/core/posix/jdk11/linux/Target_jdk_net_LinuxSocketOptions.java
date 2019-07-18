cea178ffe9b508fc2578af2bcd1e89485d5fc6b1
Bob McWhirter <bmcwhirt@redhat.com>
2019-07-18 11:41:31 -0400

    Copyright addition.

--- substratevm/src/com.oracle.svm.core.posix.jdk11/src/com/oracle/svm/core/posix/jdk11/linux/Target_jdk_net_LinuxSocketOptions.java
+++ substratevm/src/com.oracle.svm.core.posix.jdk11/src/com/oracle/svm/core/posix/jdk11/linux/Target_jdk_net_LinuxSocketOptions.java
@@ -1,4 +1,8 @@
-/* This code is free software; you can redistribute it and/or modify it
+/*
+ * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.  Oracle designates this
  * particular file as subject to the "Classpath" exception as provided
