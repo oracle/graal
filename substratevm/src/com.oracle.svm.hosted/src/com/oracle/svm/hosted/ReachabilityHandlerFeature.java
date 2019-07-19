3b5cf821465efe5cc291a8ed8991d622ee2fcbed
Paul Wögerer <paul.woegerer@oracle.com>
2019-07-19 13:55:23 +0200

    Allow register*ReachabilityHandler from within ReachabilityHandlers

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/ReachabilityHandlerFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/ReachabilityHandlerFeature.java
@@ -26,9 +26,10 @@ package com.oracle.svm.hosted;
 
 import java.lang.reflect.Executable;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
@@ -101,23 +102,31 @@ public class ReachabilityHandlerFeature implements Feature {
     @Override
     public void duringAnalysis(DuringAnalysisAccess a) {
         DuringAnalysisAccessImpl access = (DuringAnalysisAccessImpl) a;
-
-        Iterator<Map.Entry<Object, Set<Object>>> iter = activeHandlers.entrySet().iterator();
-        while (iter.hasNext()) {
-            Map.Entry<Object, Set<Object>> entry = iter.next();
-            Object callback = entry.getKey();
-            Set<Object> triggers = entry.getValue();
-            if (callback instanceof Consumer) {
-                if (isTriggered(access, triggers)) {
-                    triggeredHandlers.put(callback, null);
-                    iter.remove();
-                    toExactCallback(callback).accept(access);
+        HashSet<Object> handledCallbacks = new HashSet<>();
+        HashSet<Object> callbacks = new HashSet<>(activeHandlers.keySet());
+        do {
+            List<Object> completedCallbacks = new ArrayList<>();
+            for (Object callback : callbacks) {
+                Set<Object> triggers = activeHandlers.get(callback);
+                if (callback instanceof Consumer) {
+                    if (isTriggered(access, triggers)) {
+                        triggeredHandlers.put(callback, null);
+                        toExactCallback(callback).accept(access);
+                        completedCallbacks.add(callback);
+                    }
+                } else {
+                    VMError.guarantee(callback instanceof BiConsumer);
+                    processReachable(access, callback, triggers);
                 }
-            } else {
-                VMError.guarantee(callback instanceof BiConsumer);
-                processReachable(access, callback, triggers);
+                handledCallbacks.add(callback);
             }
-        }
+            for (Object completed : completedCallbacks) {
+                activeHandlers.remove(completed);
+                handledCallbacks.remove(completed);
+            }
+            callbacks = new HashSet<>(activeHandlers.keySet());
+            callbacks.removeAll(handledCallbacks);
+        } while (!callbacks.isEmpty());
     }
 
     private static boolean isTriggered(DuringAnalysisAccessImpl access, Set<Object> triggers) {
