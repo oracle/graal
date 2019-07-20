e583dee56e108cd0caff848b335ff7be589ce275
Christoph Pichler <pichristoph@hotmail.com>
2019-07-20 15:37:54 +0200

    Fixed test path for debugexpr tests

--- sulong/projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/debug/LLVMDebugExprParserTest.java
+++ sulong/projects/com.oracle.truffle.llvm.test/src/com/oracle/truffle/llvm/test/debug/LLVMDebugExprParserTest.java
@@ -38,7 +38,7 @@ import com.oracle.truffle.tck.DebuggerTester;
 public final class LLVMDebugExprParserTest {
 
     private static final Path BC_DIR_PATH = Paths.get(TestOptions.TEST_SUITE_PATH, "debugexpr");
-    private static final Path SRC_DIR_PATH = Paths.get(TestOptions.PROJECT_ROOT, "..", "tests", "com.oracle.truffle.llvm.tests.debugexpr", "debug");
+    private static final Path SRC_DIR_PATH = Paths.get(TestOptions.PROJECT_ROOT, "..", "tests", "com.oracle.truffle.llvm.tests.debugexpr", "debugexpr");
     private static final Path TRACE_DIR_PATH = Paths.get(TestOptions.PROJECT_ROOT, "..", "tests", "com.oracle.truffle.llvm.tests.debugexpr", "testExpr");
 
     private static final String OPTION_ENABLE_LVI = "llvm.enableLVI";
