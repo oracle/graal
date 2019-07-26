e5d35524669dc1308be8b4d014ff94b0bb294ea2
Peter Quiring <pquiring@gmail.com>
2019-07-26 11:39:10 -0400

    Support latest Windows SDK
    This one line change "should" allow substrateVM to build on latest Windows SDK.
    The legacy_stdio_definitions.lib includes old names for stdio names.
    See bug#948 for more info.

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeBootImageViaCC.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeBootImageViaCC.java
@@ -221,6 +221,7 @@ public abstract class NativeBootImageViaCC extends NativeBootImage {
             cmd.add("ws2_32.lib");
             cmd.add("secur32.lib");
             cmd.add("iphlpapi.lib");
+            cmd.add("legacy_stdio_definitions.lib");
 
             return cmd;
         }
