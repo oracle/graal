8886256eca9041c0fd5a0096d4cebc0186579db5
Codrut Stancu <codrut.stancu@oracle.com>
2019-08-01 15:28:11 -0700

    Restructure package.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/DeferredCommonPool.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/DeferredCommonPool.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.svm.core.concurrent;
+package com.oracle.svm.core.jdk;
 
 import java.util.Collection;
 import java.util.List;
