98d4befaf94c447a46fd1ef13abe53a51e80e854
Benoit Daloze <benoit.daloze@oracle.com>
2019-08-02 14:10:18 +0200

    Check if svm is present for SvmSupport.is_supported
    * It's equivalent, since if 'svm' is present in stage1, 'ni' is added in
      stage1 in registered_graalvm_components().

--- vm/mx.vm/mx_vm.py
+++ vm/mx.vm/mx_vm.py
@@ -835,7 +835,7 @@ def remove_lib_prefix_suffix(libname, require_suffix_prefix=True):
 
 class SvmSupport(object):
     def __init__(self):
-        self._svm_supported = has_component('ni', stage1=True)
+        self._svm_supported = has_component('svm', stage1=True)
         self._debug_supported = self._svm_supported and has_component('svmee', stage1=True)
         self._pgo_supported = self._svm_supported and has_component('svmee', stage1=True)
 
