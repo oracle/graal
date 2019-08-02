bac4816f1d7dd7404ffa21b2b463874b8f117707
Ergys Dona <ergys.dona@oracle.com>
2019-08-02 15:36:40 +0200

    Add documentation for LoopNode#executeLoopWithStatus and LoopNode#executeLoop.

--- truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/LoopNode.java
+++ truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/nodes/LoopNode.java
@@ -139,11 +139,19 @@ public abstract class LoopNode extends Node {
      *
      * @param frame the current execution frame or null if the repeating node does not require a
      *            frame
+     * @return the loop exit status - this is useful for languages that need to return some information
+     *            when exiting out of a loop (e.g. WebAssembly).
      * @since 0.8 or earlier
      */
     public abstract int executeLoopWithStatus(VirtualFrame frame);
 
-    // TODO: Document this and the *WithStatus variant.
+    /**
+     * Same as {@link #executeLoopWithStatus(VirtualFrame)}, but ignores the loop exit status.
+     * Provides backwards compatibility with language implementations that already depend on this method.
+     *
+     * @param frame the current execution frame or null if the repeating node does not require a
+     *            frame
+     */
     public void executeLoop(VirtualFrame frame) {
         executeLoopWithStatus(frame);
     }
