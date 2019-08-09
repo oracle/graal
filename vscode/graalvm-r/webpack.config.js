3dfae0d670e85c55c705caf633f613b3053c35b8
Dusan Balek <dusan.balek@oracle.com>
2019-08-09 15:33:03 +0200

    [GR-15166] GraalVM Visual Studio Code extensions added.

---
+++ vscode/graalvm-r/webpack.config.js
@@ -0,0 +1,35 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+    target: 'node',
+    entry: './src/extension.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'extension.js',
+        libraryTarget: "commonjs2",
+        devtoolModuleFilenameTemplate: "../[resource-path]",
+    },
+    devtool: 'source-map',
+    externals: {
+        vscode: "commonjs vscode"
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'ts-loader',
+            }]
+        }]
+    },
+}
+
+module.exports = config;
---
+++ vscode/graalvm-ruby/src/extension.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+ */
+
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+const INSTALL_GRAALVM_RUBY_COMPONENT: string = 'Install GraalVM Ruby Component';
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+		if (e.affectsConfiguration('graalvm.home')) {
+			config();
+		}
+	}));
+	config();
+}
+
+export function deactivate() {}
+
+function config() {
+	const graalVMHome = vscode.workspace.getConfiguration('graalvm').get('home') as string;
+	if (graalVMHome) {
+		const executable: string = graalVMHome + '/bin/ruby';
+		if (!fs.existsSync(executable)) {
+			vscode.window.showInformationMessage('Ruby component is not installed in your GraalVM.', INSTALL_GRAALVM_RUBY_COMPONENT).then(value => {
+				switch (value) {
+					case INSTALL_GRAALVM_RUBY_COMPONENT:
+						vscode.commands.executeCommand('extension.graalvm.installGraalVMComponent', 'ruby');
+						const watcher:fs.FSWatcher = fs.watch(graalVMHome + '/bin', () => {
+							setConfig('interpreter.commandPath', executable);
+							watcher.close();
+						});
+						break;
+				}
+			});
+		} else {
+			setConfig('interpreter.commandPath', executable);
+		}
+	}
+}
+
+function setConfig(section: string, path:string) {
+	const config = vscode.workspace.getConfiguration('ruby');
+	const term = config.inspect(section);
+	if (term) {
+		config.update(section, path, true);
+	}
+}
