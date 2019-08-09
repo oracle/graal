5a9e2a4da185fe396cd464479b65b1fbfdbd2e0f
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2019-08-09 19:28:26 +0200

    Fix host type hints in JVM_GetMethodTypeAnnotations.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/vm/VM.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/vm/VM.java
@@ -1037,7 +1037,7 @@ public final class VM extends NativeEnv implements ContextAccess {
 
     @VmImpl
     @JniImpl
-    public @Host(byte[].class) StaticObject JVM_GetMethodTypeAnnotations(@Host(java.lang.reflect.Method.class) StaticObject guestReflectionMethod) {
+    public @Host(byte[].class) StaticObject JVM_GetMethodTypeAnnotations(@Host(java.lang.reflect.Executable.class) StaticObject guestReflectionMethod) {
         // guestReflectionMethod can be either a Method or a Constructor.
         if (InterpreterToVM.instanceOf(guestReflectionMethod, getMeta().Method)) {
             StaticObject methodRoot = getGuestReflectiveMethodRoot(guestReflectionMethod);
