e0358c1dcd848ec1fb9e01da84457380ca34ed98
thomas.garcia <thomas.garcia@oracle.com>
2019-08-12 19:09:40 +0200

    Move around the timings of link/load/verification/preparation. Catches ThreadDeath in guest. Checks that final methods are ot overriden.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/VirtualTable.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/VirtualTable.java
@@ -71,6 +71,9 @@ public class VirtualTable {
         if (!overrides.isEmpty()) {
             int count = 1;
             for (Method override : overrides) {
+                if (override.isFinalFlagSet()) {
+                    throw new VerifyError("Overriding final method: " + override);
+                }
                 int pos = override.getVTableIndex();
                 if (count > 1) {
                     toSet = new Method(m);
