bd55d5a60c8e8526885956ecfec356680a6d62d0
Josef Eisl <josef.eisl@oracle.com>
2019-08-23 10:07:45 +0200

    sulong: allow building the common tests with the bundled llvm version

--- sulong/mx.sulong/suite.py
+++ sulong/mx.sulong/suite.py
@@ -618,6 +618,11 @@ suite = {
       "variants" : ["O0"],
       "buildEnv" : {
         "SUITE_CPPFLAGS" : "-I<path:SULONG_LEGACY>/include",
+        "CLANG" : "<path:SULONG_LLVM_ORG>/bin/clang",
+        "CLANGXX" : "<path:SULONG_LLVM_ORG>/bin/clang++",
+        "OPT" : "<path:SULONG_LLVM_ORG>/bin/opt",
+        "LLVM_AS" : "<path:SULONG_LLVM_ORG>/bin/llvm-as",
+        "LLVM_LINK" : "<path:SULONG_LLVM_ORG>/bin/llvm-link",
       },
       "dependencies" : [
         "SULONG_TEST_SUITES",
@@ -632,6 +637,11 @@ suite = {
       "buildRef" : False,
       "buildEnv" : {
         "SUITE_CPPFLAGS" : "-I<path:SULONG_LEGACY>/include -lm",
+        "CLANG" : "<path:SULONG_LLVM_ORG>/bin/clang",
+        "CLANGXX" : "<path:SULONG_LLVM_ORG>/bin/clang++",
+        "OPT" : "<path:SULONG_LLVM_ORG>/bin/opt",
+        "LLVM_AS" : "<path:SULONG_LLVM_ORG>/bin/llvm-as",
+        "LLVM_LINK" : "<path:SULONG_LLVM_ORG>/bin/llvm-link",
       },
       "dependencies" : [
         "SULONG_TEST",
@@ -813,6 +823,11 @@ suite = {
       "buildEnv" : {
         "SUITE_CPPFLAGS" : "-I<path:SULONG_LEGACY>/include",
         "SUITE_LDFLAGS" : "-lm -lgmp",
+        "CLANG" : "<path:SULONG_LLVM_ORG>/bin/clang",
+        "CLANGXX" : "<path:SULONG_LLVM_ORG>/bin/clang++",
+        "OPT" : "<path:SULONG_LLVM_ORG>/bin/opt",
+        "LLVM_AS" : "<path:SULONG_LLVM_ORG>/bin/llvm-as",
+        "LLVM_LINK" : "<path:SULONG_LLVM_ORG>/bin/llvm-link",
       },
       "dependencies" : [
         "SULONG_TEST",
