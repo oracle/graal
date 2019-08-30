0af067bf6755cf90e460a669084aa5eb2c96474a
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2019-08-30 12:21:28 +0200

    Remove forces assumption that the operand stack is empty on every back edge. The correct invariant is: The operand stack offset should be the same (and constant) as the target BCI for every back-edge.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/BytecodeNode.java
@@ -524,8 +524,6 @@ public final class BytecodeNode extends EspressoBaseNode implements CustomNodeCo
 
     // region Local accessors
 
-    @CompilationFinal private boolean zeroStackBackEdges = false;
-
     @Override
     @SuppressWarnings("unchecked")
     @ExplodeLoop(kind = ExplodeLoop.LoopExplosionKind.MERGE_EXPLODE)
@@ -1111,14 +1109,6 @@ public final class BytecodeNode extends EspressoBaseNode implements CustomNodeCo
         if (targetBCI < curBCI) {
             if (CompilerDirectives.inInterpreter()) {
                 LoopNode.reportLoopCount(this, 1);
-            }
-            if (!zeroStackBackEdges) {
-                if (newTop != 0) {
-                    CompilerDirectives.transferToInterpreterAndInvalidate();
-                    zeroStackBackEdges = true;
-                } else {
-                    return 0;
-                }
             }
         }
         return newTop;
