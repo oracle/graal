d04ceccdf1ed81db12da27c04faebb41f33a69b1
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2019-09-12 18:18:24 +0300

    Add copyright header to `rebuild-images.cmd`.
    (cherry picked from commit 88e93e82fa126e92f719475cc6c4aa78441b3071)

--- substratevm/mx.substratevm/rebuild-images.cmd
+++ substratevm/mx.substratevm/rebuild-images.cmd
@@ -1,5 +1,28 @@
 @echo off
 
+rem Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
+rem DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+rem
+rem This code is free software; you can redistribute it and/or modify it
+rem under the terms of the GNU General Public License version 2 only, as
+rem published by the Free Software Foundation.  Oracle designates this
+rem particular file as subject to the "Classpath" exception as provided
+rem by Oracle in the LICENSE file that accompanied this code.
+rem
+rem This code is distributed in the hope that it will be useful, but WITHOUT
+rem ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+rem FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+rem version 2 for more details (a copy is included in the LICENSE file that
+rem accompanied this code).
+rem
+rem You should have received a copy of the GNU General Public License version
+rem 2 along with this work; if not, write to the Free Software Foundation,
+rem Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+rem
+rem Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+rem or visit www.oracle.com if you need additional information or have any
+rem questions.
+
 setlocal enabledelayedexpansion
 
 echo %* | findstr = >nul && (
