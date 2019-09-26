2ca83f009b0d5b89e659ae2a4c5c56150b3c9a1c
Jacob Kreindl <jacob.kreindl@jku.at>
2019-09-26 16:06:15 +0200

    Avoid PEing System.exit()

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/multithreading/LLVMPThreadStart.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/multithreading/LLVMPThreadStart.java
@@ -29,6 +29,7 @@
  */
 package com.oracle.truffle.llvm.runtime.nodes.intrinsics.multithreading;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.FrameSlot;
@@ -82,6 +83,7 @@ public final class LLVMPThreadStart {
                 // exception
 
             } catch (LLVMExitException e) {
+                CompilerDirectives.transferToInterpreter();
                 System.exit(e.getExitStatus());
 
             } finally {
