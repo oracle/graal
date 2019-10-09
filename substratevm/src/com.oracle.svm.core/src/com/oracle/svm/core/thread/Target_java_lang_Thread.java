0e74fd31758ed737d8a58597db12a290c6151a65
Paul Wögerer <paul.woegerer@oracle.com>
2019-10-09 16:40:15 +0200

    Style fix

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/Target_java_lang_Thread.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/thread/Target_java_lang_Thread.java
@@ -292,16 +292,19 @@ final class Target_java_lang_Thread {
     }
 
     @Substitute
+    @SuppressWarnings({"static-method"})
     private void stop0(Object o) {
         VMError.unimplemented();
     }
 
     @Substitute
+    @SuppressWarnings({"static-method"})
     private void suspend0() {
         VMError.unimplemented();
     }
 
     @Substitute
+    @SuppressWarnings({"static-method"})
     private void resume0() {
         VMError.unimplemented();
     }
