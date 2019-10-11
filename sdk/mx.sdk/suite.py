d4265685529156f0c5d3faa5409083fda9e5b806
Christian Humer <christian.humer@oracle.com>
2019-10-11 15:30:38 +0200

    Add version utility that allows to create, validate and compare GraalVM versions.

--- sdk/mx.sdk/suite.py
+++ sdk/mx.sdk/suite.py
@@ -196,6 +196,17 @@ suite = {
       "javaCompliance" : "8+",
       "workingSets" : "API,SDK",
     },
+    "org.graalvm.home.test" : {
+      "subDir" : "src",
+      "sourceDirs" : ["src"],
+      "dependencies" : [
+        "mx:JUNIT",
+        "org.graalvm.home",
+      ],
+      "checkstyle" : "org.graalvm.word",
+      "javaCompliance" : "8+",
+      "workingSets" : "API,SDK",
+    },
   },
   "licenses" : {
     "UPL" : {
@@ -255,6 +266,7 @@ suite = {
         "org.graalvm.collections.test",
         "org.graalvm.nativeimage.test",
         "org.graalvm.launcher.test",
+        "org.graalvm.home.test",
       ],
       "distDependencies" : [
         "GRAAL_SDK",
