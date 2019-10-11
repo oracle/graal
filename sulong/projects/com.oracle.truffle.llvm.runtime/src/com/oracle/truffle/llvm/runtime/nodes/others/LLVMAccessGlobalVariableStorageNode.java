3bc2a90aaf43d79f52c5dbf1112918b3a9b9ec40
Paley Li <paley.li@oracle.com>
2019-10-11 11:48:01 +0200

    rebase issues

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/others/LLVMAccessGlobalVariableStorageNode.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/others/LLVMAccessGlobalVariableStorageNode.java
@@ -61,6 +61,7 @@ public abstract class LLVMAccessGlobalVariableStorageNode extends LLVMExpression
     }
 
     @Specialization
+<<<<<<< HEAD
     Object doAccess(
                     @CachedContext(LLVMLanguage.class) LLVMContext context,
                     @Cached ReadDynamicObjectHelper helper) {
@@ -136,10 +137,9 @@ public abstract class LLVMAccessGlobalVariableStorageNode extends LLVMExpression
         protected Object readIndirect(DynamicObject dynamicObject, LLVMGlobal descriptor) {
             return dynamicObject.get(descriptor);
         }
+=======
+    public Object accessGlobal() {
+        return context.getGlobalStorage(descriptor);
+>>>>>>> LLVMAccessGlobalVariableStorageNode is a now an abstract class and now takes a context to access the global storage.
     }
-
-    /*@Specialization
-    public Object accessGlobal(@CachedContext(LLVMLanguage.class) LLVMContext ctx) {
-        return ctx.getGlobal(descriptor);
-    }*/
 }
