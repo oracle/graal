f228be504863e03b71ac79d2759d6ab173888140
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2019-10-21 16:21:34 +0200

    Allow registered listeners to be disabled

--- tools/src/com.oracle.truffle.tools.agentscript.test/src/com/oracle/truffle/tools/agentscript/test/AgentScriptAPI.java
+++ tools/src/com.oracle.truffle.tools.agentscript.test/src/com/oracle/truffle/tools/agentscript/test/AgentScriptAPI.java
@@ -108,5 +108,14 @@ public interface AgentScriptAPI {
      * @param handler no args function to be notified when execution ends
      */
     void on(String event, OnCloseHandler handler);
+
+    /** Unregisters a handler.
+     * @param event the event type to unregister from
+     * @param handler the instances of handler registered by one of the {@code on}
+     *  methods
+     */
+    void off(String event, OnSourceLoadedHandler handler);
+    void off(String event, OnEventHandler handler);
+    void off(String event, OnCloseHandler handler);
 }
 // END: AgentScriptAPI
