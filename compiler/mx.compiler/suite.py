883251b8a21da9cb79ad4d61c6f71bb91da2463c
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2019-10-21 09:31:18 +0200

    Update version for next dev cycle

--- compiler/mx.compiler/suite.py
+++ compiler/mx.compiler/suite.py
@@ -4,7 +4,7 @@ suite = {
   "sourceinprojectwhitelist" : [],
 
   "groupId" : "org.graalvm.compiler",
-  "version" : "19.3.0",
+  "version" : "20.0.0",
   "release" : False,
   "url" : "http://www.graalvm.org/",
   "developer" : {
