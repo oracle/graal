034fcb9925bbb3c0373ac9f461afd5d861d82d07
Martin Entlicher <martin.entlicher@oracle.com>
2019-10-22 17:50:33 +0200

    [GR-19086] Interpret a request for object members completion from VS Code.

--- tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/InspectorRuntime.java
+++ tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/InspectorRuntime.java
@@ -476,7 +476,7 @@ public final class InspectorRuntime extends RuntimeDomain {
                         public Void executeCommand() throws CommandProcessException {
                             JSONObject result;
                             if (functionNoWS.startsWith(FUNCTION_COMPLETION)) {
-                                result = createCodecompletion(value, scope, generatePreview);
+                                result = createCodecompletion(value, scope, generatePreview, context, true);
                             } else if (functionNoWS.equals(FUNCTION_SET_PROPERTY)) {
                                 // Set of an array element, or object property
                                 if (arguments == null || arguments.length() < 2) {
@@ -733,7 +733,7 @@ public final class InspectorRuntime extends RuntimeDomain {
         }
     }
 
-    private JSONObject createCodecompletion(DebugValue value, DebugScope scope, boolean generatePreview) {
+    static JSONObject createCodecompletion(DebugValue value, DebugScope scope, boolean generatePreview, InspectorExecutionContext context, boolean resultItems) {
         JSONObject result = new JSONObject();
         Iterable<DebugValue> properties = null;
         try {
@@ -743,7 +743,7 @@ public final class InspectorRuntime extends RuntimeDomain {
                 properties = scope.getDeclaredValues();
             }
         } catch (DebugException ex) {
-            fillExceptionDetails(result, ex, generatePreview);
+            fillExceptionDetails(result, ex, context, generatePreview);
             if (ex.isInternalError()) {
                 PrintWriter err = context.getErr();
                 if (err != null) {
@@ -753,15 +753,19 @@ public final class InspectorRuntime extends RuntimeDomain {
             }
         }
         JSONArray valueArray = new JSONArray();
-        JSONObject itemsObj = new JSONObject();
         JSONArray items = new JSONArray();
         if (properties != null) {
             for (DebugValue property : properties) {
                 items.put(property.getName());
             }
         }
-        itemsObj.put("items", items);
-        valueArray.put(itemsObj);
+        if (resultItems) {
+            JSONObject itemsObj = new JSONObject();
+            itemsObj.put("items", items);
+            valueArray.put(itemsObj);
+        } else {
+            valueArray.put(items);
+        }
         result.put("type", "object");
         result.put("value", valueArray);
         return result;
