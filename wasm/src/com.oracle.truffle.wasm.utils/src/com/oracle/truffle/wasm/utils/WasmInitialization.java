382782ddd164bf4d05841e3adc79c4138e84f5c6
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2019-10-24 02:11:57 +0200

    Fix initialization after the test suite refactoring.

--- wasm/src/com.oracle.truffle.wasm.utils/src/com/oracle/truffle/wasm/utils/WasmInitialization.java
+++ wasm/src/com.oracle.truffle.wasm.utils/src/com/oracle/truffle/wasm/utils/WasmInitialization.java
@@ -93,7 +93,9 @@ public final class WasmInitialization implements Consumer<WasmContext>, TruffleO
                 final long address = getValue(addressGlobal);
                 final String valueGlobal = entry.getValue();
                 final long value = getValue(valueGlobal);
-                memory.writeArrayElement(address, value);
+                // The memory array writes are indexed with 64-bit words.
+                // Therefore, we need to divide the byte-based address index with 8.
+                memory.writeArrayElement(address / 8, value);
             }
         } catch (UnknownIdentifierException | UnsupportedMessageException | InvalidArrayIndexException e) {
             throw new RuntimeException(e);
