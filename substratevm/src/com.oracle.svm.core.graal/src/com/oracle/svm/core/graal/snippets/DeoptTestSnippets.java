e810c3c0e467ac2bcde6ca3db84995019a8c44df
Christian Haeubl <christian.haeubl@oracle.com>
2019-10-24 17:35:02 +0200

    Fix transient issue in deoptimization test case.

--- substratevm/src/com.oracle.svm.core.graal/src/com/oracle/svm/core/graal/snippets/DeoptTestSnippets.java
+++ substratevm/src/com.oracle.svm.core.graal/src/com/oracle/svm/core/graal/snippets/DeoptTestSnippets.java
@@ -40,7 +40,6 @@ import org.graalvm.compiler.replacements.SnippetTemplate.Arguments;
 import org.graalvm.compiler.replacements.SnippetTemplate.SnippetInfo;
 import org.graalvm.compiler.replacements.Snippets;
 
-import com.oracle.svm.core.deopt.DeoptTester;
 import com.oracle.svm.core.graal.nodes.DeoptTestNode;
 
 public final class DeoptTestSnippets extends SubstrateTemplates implements Snippets {
