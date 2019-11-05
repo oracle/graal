e7f079cd270d89a4aa371a2f1ab4fb95c26e89e4
Tomas Zezula <tomas.zezula@oracle.com>
2019-11-05 11:10:57 +0100

    [GR-19378] FileSystemNotFoundException when init LanguageCache in Springboot.

--- truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/LanguageCache.java
+++ truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/LanguageCache.java
@@ -47,6 +47,7 @@ import java.net.JarURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.CodeSource;
@@ -516,7 +517,7 @@ final class LanguageCache implements Comparable<LanguageCache> {
             return resolvedId;
         }
 
-        static String getLanguageHomeFromURLConnection(URLConnection connection) {
+        static String getLanguageHomeFromURLConnection(String languageId, URLConnection connection) {
             if (connection instanceof JarURLConnection) {
                 /*
                  * The previous implementation used a `URL.getPath()`, but OS Windows is offended by
@@ -536,12 +537,14 @@ final class LanguageCache implements Comparable<LanguageCache> {
                  * `URI.toASCIIString()` all reserved and non-ASCII characters are percent-quoted.
                  */
                 try {
-                    Path path;
-                    path = Paths.get(((JarURLConnection) connection).getJarFileURL().toURI());
-                    Path parent = path.getParent();
-                    return parent != null ? parent.toString() : null;
-                } catch (URISyntaxException e) {
-                    assert false : "Could not resolve path.";
+                    URL url = ((JarURLConnection) connection).getJarFileURL();
+                    if ("file".equals(url.getProtocol())) {
+                        Path path = Paths.get(url.toURI());
+                        Path parent = path.getParent();
+                        return parent != null ? parent.toString() : null;
+                    }
+                } catch (URISyntaxException | FileSystemNotFoundException | IllegalArgumentException | SecurityException e) {
+                    assert false : "Cannot locate " + languageId + " language home due to " + e.getMessage();
                 }
             }
             return null;
@@ -598,7 +601,7 @@ final class LanguageCache implements Comparable<LanguageCache> {
                 }
                 String languageHome = System.getProperty(id + ".home");
                 if (languageHome == null) {
-                    languageHome = getLanguageHomeFromURLConnection(connection);
+                    languageHome = getLanguageHomeFromURLConnection(id, connection);
                 }
                 String className = info.getProperty(prefix + "className");
                 String implementationName = info.getProperty(prefix + "implementationName");
@@ -818,7 +821,7 @@ final class LanguageCache implements Comparable<LanguageCache> {
                         URL url = provider.getClass().getClassLoader().getResource(className.replace('.', '/') + ".class");
                         if (url != null) {
                             try {
-                                languageHome = getLanguageHomeFromURLConnection(url.openConnection());
+                                languageHome = getLanguageHomeFromURLConnection(id, url.openConnection());
                             } catch (IOException ioe) {
                             }
                         }
