1167a3d5c4242d21c7bd56ceb9e75844e57b4f55
Fred Morcos <fred.morcos@oracle.com>
2019-11-14 16:14:21 +0100

    Sulong: There is no need for the doSignal handler to be an Object
    It is anyways turned into an LLVMPointer later.

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/c/LLVMSignal.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/c/LLVMSignal.java
@@ -64,7 +64,7 @@ import sun.misc.SignalHandler;
 public abstract class LLVMSignal extends LLVMExpressionNode {
 
     @Specialization
-    protected LLVMPointer doSignal(int signal, Object handler,
+    protected LLVMPointer doSignal(int signal, LLVMPointer handler,
                     @CachedContext(LLVMLanguage.class) LLVMContext context,
                     @Cached("createToNativeWithTarget()") LLVMToNativeNode toNative) {
         return setSignalHandler(context, signal, toNative.executeWithTarget(handler));
