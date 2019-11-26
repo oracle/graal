7fd473bd212325937076597ca0b529405aaa354b
Codrut Stancu <codrut.stancu@oracle.com>
2019-11-26 13:29:39 -0800

    Update reflection fallback mechanism to static fields.

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/substitute/AnnotationSubstitutionProcessor.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/substitute/AnnotationSubstitutionProcessor.java
@@ -657,10 +657,17 @@ public class AnnotationSubstitutionProcessor extends SubstitutionProcessor {
 
         } catch (NoSuchFieldException ex) {
             /*
-             * Some fields are hidden from reflection. Try to find the field via the
-             * ResolvedJavaType.
+             * Some fields are hidden from reflection. The set of hidden fields is computed via
+             * {sun.reflect,jdk.internal.reflect}.Reflection.fieldFilterMap. Try to find the field
+             * via the ResolvedJavaType.
              */
-            for (ResolvedJavaField f : metaAccess.lookupJavaType(originalClass).getInstanceFields(true)) {
+            ResolvedJavaField[] fields;
+            if (Modifier.isStatic(annotatedField.getModifiers())) {
+                fields = metaAccess.lookupJavaType(originalClass).getStaticFields();
+            } else {
+                fields = metaAccess.lookupJavaType(originalClass).getInstanceFields(true);
+            }
+            for (ResolvedJavaField f : fields) {
                 if (f.getName().equals(originalName)) {
                     return f;
                 }
