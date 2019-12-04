4d3eb9ebe6592d2d4e7574cf01c0dbf4aa6110c4
Allan Gregersen <allan.gregersen@oracle.com>
2019-12-04 10:57:11 +0100

    fix lock

--- src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/EventFilters.java
+++ src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/EventFilters.java
@@ -22,6 +22,7 @@
  */
 package com.oracle.truffle.espresso.jdwp.impl;
 
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -44,21 +45,21 @@ public final class EventFilters {
     }
 
     public RequestFilter getRequestFilter(int requestId) {
+        Lock readLock = lock.readLock();
         try {
-            lock.readLock().lock();
+            readLock.lock();
             // likely the filters are required from last inserted
             for (int i = requestFilters.length - 1; i > -1; i--) {
                 RequestFilter filter = requestFilters[i];
                 if (filter != null) {
                     if (filter.getRequestId() == requestId) {
-                        lock.readLock().unlock();
                         return filter;
                     }
                 }
             }
+            return null;
         } finally {
-            lock.readLock().unlock();
+            readLock.unlock();
         }
-        return null;
     }
 }
