3532b9ace1cdca71b358fa6d2a9eefd0f5f39bf5
Allan Gregersen <allan.gregersen@oracle.com>
2019-12-05 09:55:34 +0100

    update copyright to 2019 in all new files and remove classpath exception from some headers

--- src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/DebuggerCommand.java
+++ src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/DebuggerCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,10 +36,19 @@ final class DebuggerCommand {
     private final RequestFilter requestFilter;
     private SourceLocation location;
     private BreakpointInfo breakpointInfo;
+    private boolean submitted;
 
     DebuggerCommand(Kind kind, RequestFilter filter) {
         this.kind = kind;
         this.requestFilter = filter;
+    }
+
+    public boolean isSubmitted() {
+        return submitted;
+    }
+
+    public void markSubmitted() {
+        submitted = true;
     }
 
     void setSourceLocation(SourceLocation location) {
--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/bytecode/MapperBCI.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/bytecode/MapperBCI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,7 +20,6 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-
 package com.oracle.truffle.espresso.bytecode;
 
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
