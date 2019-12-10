8176c8b43e83e3e44d625c936b095e3a24178267
Allan Gregersen <allan.gregersen@oracle.com>
2019-12-10 16:09:43 +0100

    add links in javadoc to alternative methods

--- truffle/src/com.oracle.truffle.tck/src/com/oracle/truffle/tck/DebuggerTester.java
+++ truffle/src/com.oracle.truffle.tck/src/com/oracle/truffle/tck/DebuggerTester.java
@@ -236,8 +236,10 @@ public final class DebuggerTester implements AutoCloseable {
     /**
      * Starts a new {@link Context#eval(Source) evaluation} on the background thread. Only one
      * evaluation can be active at a time. Please ensure that {@link #expectDone()} completed
-     * successfully before starting a new evaluation. Throws an {@link IllegalStateException} if
-     * another evaluation is still executing or the tester is already closed.
+     * successfully before starting a new evaluation. When no source is available please refer
+     * to {@link DebuggerTester#startExecute(Function)}.
+     * Throws an {@link IllegalStateException} if another evaluation is still executing or the
+     * tester is already closed.
      *
      * @since 0.27
      */
@@ -252,8 +254,10 @@ public final class DebuggerTester implements AutoCloseable {
     /**
      * Starts a new script evaluation on the background thread. Only one
      * evaluation can be active at a time. Please ensure that {@link #expectDone()} completed
-     * successfully before starting a new evaluation. Throws an {@link IllegalStateException} if
-     * another evaluation is still executing or the tester is already closed.
+     * successfully before starting a new evaluation. If a Source is available please refer to
+     * {@link DebuggerTester#startEval(Source)}.
+     * Throws an {@link IllegalStateException} if another evaluation is still executing or the
+     * tester is already closed.
      *
      * @since 20.0
      */
