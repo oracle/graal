4300f4b624b264b8c63d145ea06f49a18779145f
Yudi Zheng <yudi.zheng@oracle.com>
2019-12-10 14:30:08 +0100

    Add extra tests.

--- compiler/src/org.graalvm.compiler.hotspot.amd64.test/src/org/graalvm/compiler/hotspot/amd64/test/NumberOfTrailingZeroings003.java
+++ compiler/src/org.graalvm.compiler.hotspot.amd64.test/src/org/graalvm/compiler/hotspot/amd64/test/NumberOfTrailingZeroings003.java
@@ -29,13 +29,48 @@ import org.junit.Test;
 
 public class NumberOfTrailingZeroings003 extends GraalCompilerTest {
 
-    public static boolean numberOfTrailingZeros003() {
-        return Integer.numberOfTrailingZeros(0) != 32;
+    public static boolean numberOfTrailingZeros0() {
+        return Integer.numberOfTrailingZeros(0) == 32;
     }
 
     @Test
-    public void testNumberOfTrailingZeros003() {
-        test("numberOfTrailingZeros003");
+    public void testNumberOfTrailingZeros0() {
+        test("numberOfTrailingZeros0");
     }
 
+    public static boolean numberOfTrailingZeros1() {
+        return Integer.numberOfTrailingZeros(1) == 0;
+    }
+
+    @Test
+    public void testNumberOfTrailingZeros1() {
+        test("numberOfTrailingZeros1");
+    }
+
+    public static boolean numberOfTrailingZerosM1() {
+        return Integer.numberOfTrailingZeros(-1) == 0;
+    }
+
+    @Test
+    public void testNumberOfTrailingZerosM1() {
+        test("numberOfTrailingZerosM1");
+    }
+
+    public static boolean numberOfTrailingZerosMin() {
+        return Integer.numberOfTrailingZeros(Integer.MIN_VALUE) == 31;
+    }
+
+    @Test
+    public void testNumberOfTrailingZerosMin() {
+        test("numberOfTrailingZerosMin");
+    }
+
+    public static boolean numberOfTrailingZerosMax() {
+        return Integer.numberOfTrailingZeros(Integer.MAX_VALUE) == 0;
+    }
+
+    @Test
+    public void testNumberOfTrailingZerosMax() {
+        test("numberOfTrailingZerosMax");
+    }
 }
