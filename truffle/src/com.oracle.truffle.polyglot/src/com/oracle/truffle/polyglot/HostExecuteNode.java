f67bfa793c64fc3135be2ff8971769b1b64e78ef
Andreas Woess <andreas.woess@oracle.com>
2019-12-12 18:59:10 +0100

    Fix guest to host call not being inlined.

--- truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/HostExecuteNode.java
+++ truffle/src/com.oracle.truffle.polyglot/src/com/oracle/truffle/polyglot/HostExecuteNode.java
@@ -40,8 +40,6 @@
  */
 package com.oracle.truffle.polyglot;
 
-import static com.oracle.truffle.polyglot.GuestToHostRootNode.createGuestToHost;
-
 import java.lang.reflect.Array;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Type;
@@ -56,7 +54,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.StringJoiner;
 
-import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
@@ -764,24 +761,9 @@ abstract class HostExecuteNode extends Node {
         return arguments;
     }
 
-    private static final CallTarget INVOKE = createGuestToHost(new GuestToHostRootNode(HostObject.class, "doInvoke") {
-        @Override
-        protected Object executeImpl(Object obj, Object[] callArguments) {
-            SingleMethod method = (SingleMethod) callArguments[ARGUMENT_OFFSET];
-            Object[] arguments = (Object[]) callArguments[ARGUMENT_OFFSET + 1];
-            Object ret;
-            try {
-                ret = method.invoke(obj, arguments);
-            } catch (Throwable e) {
-                throw HostInteropReflect.rethrow(e);
-            }
-            return ret;
-        }
-    });
-
     private static Object doInvoke(SingleMethod method, Object obj, Object[] arguments, PolyglotLanguageContext languageContext, ToGuestValueNode toGuest) {
         assert arguments.length == method.getParameterCount();
-        Object ret = GuestToHostRootNode.guestToHostCall(toGuest, INVOKE, languageContext, obj, method, arguments);
+        Object ret = method.invokeGuestToHost(obj, arguments, languageContext, toGuest);
         return toGuest.execute(languageContext, ret);
     }
 
