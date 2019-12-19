6de8efacc92d9206cae840d6e220918764ffa6bf
Allan Gregersen <allan.gregersen@oracle.com>
2019-12-19 14:34:29 +0100

    return empty String as generic field signature when there is no generics in type

--- src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/JDWP.java
+++ src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/JDWP.java
@@ -629,8 +629,15 @@ final class JDWP {
                 for (FieldRef field : declaredFields) {
                     reply.writeLong(context.getIds().getIdAsLong(field));
                     reply.writeString(field.getNameAsString());
-                    reply.writeString(field.getTypeAsString());
-                    reply.writeString(field.getGenericSignatureAsString());
+                    String signature = field.getTypeAsString();
+                    reply.writeString(signature);
+                    String genericSignature = field.getGenericSignatureAsString();
+                    // only return a generic signature if the type has generics
+                    if (genericSignature.equals(signature)) {
+                        reply.writeString("");
+                    } else {
+                        reply.writeString(field.getGenericSignatureAsString());
+                    }
                     int modBits = checkSyntheticFlag(field.getModifiers());
                     reply.writeInt(modBits);
                 }
