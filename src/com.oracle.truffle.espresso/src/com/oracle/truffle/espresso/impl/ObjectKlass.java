9f40d7821eda7d5235e328f97769fea9cc950e01
Allan Gregersen <allan.gregersen@oracle.com>
2020-01-03 13:41:51 +0100

    formatting pass

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
@@ -116,7 +116,6 @@ public final class ObjectKlass extends Klass {
 
     @CompilationFinal private int computedModifiers = -1;
 
-
     public static final int LOADED = 0;
     public static final int LINKED = 1;
     public static final int PREPARED = 2;
