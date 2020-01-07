57bbc0dd9c5a8b92dc703d8de121aadbfcd6b977
Josef Eisl <josef.eisl@oracle.com>
2020-01-07 15:49:35 +0100

    sulong: rethrow the UnsatisfiedLinkError in NFIContextExtension#loadLibrary [GR-20440]

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/NFIContextExtension.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/NFIContextExtension.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019, Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -221,7 +221,7 @@ public final class NFIContextExtension implements ContextExtension {
             if (optional) {
                 return null;
             } else {
-                throw new IllegalArgumentException(loadExpression, ex);
+                throw ex;
             }
         }
     }
