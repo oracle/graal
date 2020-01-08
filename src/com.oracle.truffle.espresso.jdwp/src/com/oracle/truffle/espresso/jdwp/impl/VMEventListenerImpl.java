869fb73b66edae327c976cadc537c4544e592cc2
Allan Gregersen <allan.gregersen@oracle.com>
2020-01-08 11:14:03 +0100

    add logging for debugger stepping events

--- src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/VMEventListenerImpl.java
+++ src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/VMEventListenerImpl.java
@@ -535,6 +535,8 @@ public final class VMEventListenerImpl implements VMEventListener {
         stream.writeLong(currentFrame.getClassId());
         stream.writeLong(currentFrame.getMethodId());
         stream.writeLong(currentFrame.getCodeIndex());
+        JDWPLogger.log("Sending step completed event", JDWPLogger.LogLevel.STEPPING);
+
         if (holdEvents) {
             heldEvents.add(stream);
         } else {
