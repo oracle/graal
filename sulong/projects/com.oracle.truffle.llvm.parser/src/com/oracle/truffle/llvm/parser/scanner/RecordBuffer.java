6a377b3e4f21a1571797a54ed14d1bb364495769
Josef Eisl <josef.eisl@oracle.com>
2020-01-09 19:14:05 +0100

    sulong: make parser throw exception if we truncate values
    (cherry picked from commit b212fc8865921c6d31586c3cf7e74a27452943cd)

--- sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/scanner/RecordBuffer.java
+++ sulong/projects/com.oracle.truffle.llvm.parser/src/com/oracle/truffle/llvm/parser/scanner/RecordBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019, Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates.
  *
  * All rights reserved.
  *
@@ -32,6 +32,7 @@ package com.oracle.truffle.llvm.parser.scanner;
 import java.util.Arrays;
 
 import com.oracle.truffle.llvm.runtime.except.LLVMParserException;
+import com.oracle.truffle.llvm.runtime.types.Type;
 
 public final class RecordBuffer {
 
@@ -103,7 +104,15 @@ public final class RecordBuffer {
     }
 
     public int readInt() {
-        return (int) read();
+        long read = read();
+        return toUnsignedIntExact(read);
+    }
+
+    private int toUnsignedIntExact(long read) {
+        if (Type.fitsIntoUnsignedInt(read)) {
+            return Type.toUnsignedInt(read);
+        }
+        throw new ArithmeticException("unsigned integer overflow");
     }
 
     public boolean readBoolean() {
