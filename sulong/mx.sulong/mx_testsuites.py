e528d6c8a0a7b89f671c92890434f6368c4a4a6d
Josef Eisl <josef.eisl@oracle.com>
2020-01-14 14:43:53 +0100

    sulong: add OS env variable to all SulongTestSuite projects

--- sulong/mx.sulong/mx_testsuites.py
+++ sulong/mx.sulong/mx_testsuites.py
@@ -175,6 +175,8 @@ class SulongTestSuite(SulongTestSuiteBase):  # pylint: disable=too-many-ancestor
         env['LLVM_LINK'] = mx_sulong.findBundledLLVMProgram('llvm-link')
         env['LLVM_OBJCOPY'] = mx_sulong.findBundledLLVMProgram('llvm-objcopy')
         env['GRAALVM_LLVM_HOME'] = mx_subst.path_substitutions.substitute("<path:SULONG_HOME>")
+        if 'OS' not in env:
+            env['OS'] = mx_subst.path_substitutions.substitute("<os>")
         if SulongTestSuite.haveDragonegg():
             env['DRAGONEGG'] = mx_sulong.dragonEggPath()
             env['DRAGONEGG_GCC'] = mx_sulong.getGCC()
