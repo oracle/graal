39b133607b79ddcf0f0b1e24681a4b327431251f
Christian Wirth <christian.wirth@oracle.com>
2020-01-22 21:40:11 +0000

    [GR-16556] Backport: Remove old Nashorn compat mode.
    PullRequest: js/1308

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "17525a2a0803d61846330c0f669e8758e8a2769d",
+                "version": "2170fb3f5a575c8d39900fde3d6d5382dbea7479",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "17525a2a0803d61846330c0f669e8758e8a2769d",
+                "version": "2170fb3f5a575c8d39900fde3d6d5382dbea7479",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
