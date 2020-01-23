2c4499a3c39ea5cb514a156d8228f3ce69af8dc6
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-01-23 16:51:29 +0100

    Remove redundant volatile read.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObject.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/StaticObject.java
@@ -109,7 +109,7 @@ public final class StaticObject implements TruffleObject {
             throw EspressoError.shouldNotReachHere("StaticObject.NULL.getLock()");
         }
         EspressoLock l = lock;
-        if (lock == null) {
+        if (l == null) {
             synchronized (this) {
                 l = lock;
                 if (l == null) {
