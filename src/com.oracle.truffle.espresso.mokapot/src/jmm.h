936b27a9e2e65d105ccb353536c5029e56d33a0b
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-01-30 16:21:23 +0100

    Update license headers + fix Windows line ends.

--- src/com.oracle.truffle.espresso.mokapot/src/jmm.h
+++ src/com.oracle.truffle.espresso.mokapot/src/jmm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,7 +20,6 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-
 #ifndef _JAVA_JMM_H_
 #define _JAVA_JMM_H_
 
