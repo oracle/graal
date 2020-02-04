642d8349410f623c43e4fed50e4215552dba1647
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2020-02-04 05:15:55 +0100

    Backporting GR-21029 and GR-20444 into release 20.0

--- tools/src/com.oracle.truffle.tools.agentscript/src/com/oracle/truffle/tools/agentscript/impl/AgentObject.java
+++ tools/src/com.oracle.truffle.tools.agentscript/src/com/oracle/truffle/tools/agentscript/impl/AgentObject.java
@@ -140,7 +140,7 @@ final class AgentObject implements TruffleObject {
         Instrumenter instrumenter = obj.env.getInstrumenter();
         switch (member) {
             case "on": {
-                AgentType type = AgentType.find((String) args[0]);
+                AgentType type = AgentType.find(convertToString(interop, args[0]));
                 switch (type) {
                     case SOURCE: {
                         SourceFilter filter = SourceFilter.newBuilder().sourceIs(obj.excludeSources).includeInternal(false).build();
@@ -183,7 +183,7 @@ final class AgentObject implements TruffleObject {
             }
             case "off": {
                 CompilerDirectives.transferToInterpreter();
-                AgentType type = AgentType.find((String) args[0]);
+                AgentType type = AgentType.find(convertToString(interop, args[0]));
                 obj.removeHandle(type, args[1]);
                 break;
             }
@@ -191,6 +191,11 @@ final class AgentObject implements TruffleObject {
                 throw UnknownIdentifierException.create(member);
         }
         return obj;
+    }
+
+    @CompilerDirectives.TruffleBoundary
+    private static String convertToString(InteropLibrary interop, Object obj) throws UnsupportedMessageException {
+        return interop.asString(obj);
     }
 
     private static SourceSectionFilter createFilter(AgentObject obj, Object[] args) throws IllegalArgumentException, UnsupportedMessageException {
@@ -227,12 +232,26 @@ final class AgentObject implements TruffleObject {
                         break;
                     case "rootNameFilter":
                         try {
-                            Object rootNameFilter = iop.readMember(config, "rootNameFilter");
-                            if (rootNameFilter != null && !iop.isNull(rootNameFilter)) {
-                                if (!iop.isExecutable(rootNameFilter)) {
+                            Object fn = iop.readMember(config, "rootNameFilter");
+                            if (fn != null && !iop.isNull(fn)) {
+                                if (!iop.isExecutable(fn)) {
                                     throw new IllegalArgumentException("rootNameFilter has to be a function!");
                                 }
-                                builder.rootNameIs(new RootNameFilter(rootNameFilter));
+                                builder.rootNameIs(new RootNameFilter(fn));
+                            }
+                        } catch (UnknownIdentifierException ex) {
+                            // OK
+                        }
+                        break;
+                    case "sourceFilter":
+                        try {
+                            Object fn = iop.readMember(config, "sourceFilter");
+                            if (fn != null && !iop.isNull(fn)) {
+                                if (!iop.isExecutable(fn)) {
+                                    throw new IllegalArgumentException("sourceFilter has to be a function!");
+                                }
+                                SourceFilter filter = SourceFilter.newBuilder().sourceIs(new AgentSourceFilter(fn)).build();
+                                builder.sourceFilter(filter);
                             }
                         } catch (UnknownIdentifierException ex) {
                             // OK
