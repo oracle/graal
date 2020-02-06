455bffc10819df766c269e23002cbf544cedf2f0
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2020-02-06 20:57:43 +0100

    [GR-17763] Add Graal.nodejs to the GraalVM images for Windows.

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "37ba1f2947f233e98c66fd14b1624e43076ae57c",
+                "version": "090649a348a1666fd8705ee2cdbf8c5a3744b891",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "37ba1f2947f233e98c66fd14b1624e43076ae57c",
+                "version": "090649a348a1666fd8705ee2cdbf8c5a3744b891",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
