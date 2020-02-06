c2cb259d4d469bbf1d6fc06cc397d9fb64fa03d4
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-02-06 15:23:03 +0100

    Remove host class to guest class conversions.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/meta/Meta.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/meta/Meta.java
@@ -723,11 +723,6 @@ public final class Meta implements ContextAccess {
 
     // Checkstyle: resume field name check
 
-    private static boolean isKnownClass(java.lang.Class<?> clazz) {
-        // Cheap check: (host) known classes are loaded by the BCL.
-        return clazz.getClassLoader() == null;
-    }
-
     public static StaticObject box(Meta meta, Object arg) {
         if (arg instanceof Boolean) {
             return meta.boxBoolean((boolean) arg);
@@ -818,19 +813,11 @@ public final class Meta implements ContextAccess {
 
     // endregion Guest exception handling (throw)
 
-    @TruffleBoundary
-    public ObjectKlass knownKlass(Symbol<Type> type) {
+    private ObjectKlass knownKlass(Symbol<Type> type) {
+        CompilerAsserts.neverPartOfCompilation();
         assert !Types.isArray(type);
         assert !Types.isPrimitive(type);
         return (ObjectKlass) getRegistries().loadKlassWithBootClassLoader(type);
-    }
-
-    @TruffleBoundary
-    ObjectKlass knownKlass(java.lang.Class<?> hostClass) {
-        assert isKnownClass(hostClass);
-        assert !hostClass.isPrimitive();
-        // Resolve non-primitive classes using BCL.
-        return knownKlass(getTypes().fromClass(hostClass));
     }
 
     /**
