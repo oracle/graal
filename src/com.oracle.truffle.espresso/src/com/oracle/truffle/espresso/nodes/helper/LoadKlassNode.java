d6366f55de28eaf4a534a7e94292e8c0bde583ba
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-02-10 19:34:42 +0100

    Add full cycle unittest.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/helper/LoadKlassNode.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/helper/LoadKlassNode.java
@@ -1,18 +1,17 @@
 package com.oracle.truffle.espresso.nodes.helper;
 
-import com.oracle.truffle.api.CompilerDirectives;
+import java.util.function.IntFunction;
+
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.espresso.EspressoLanguage;
 import com.oracle.truffle.espresso.descriptors.Symbol.Name;
 import com.oracle.truffle.espresso.descriptors.Symbol.Signature;
 import com.oracle.truffle.espresso.meta.Meta;
 import com.oracle.truffle.espresso.runtime.EspressoContext;
-import com.oracle.truffle.espresso.runtime.EspressoException;
 import com.oracle.truffle.espresso.runtime.StaticObject;
-
-import java.util.function.IntFunction;
 
 public final class LoadKlassNode extends RootNode {
 
@@ -25,24 +24,21 @@ public final class LoadKlassNode extends RootNode {
 
     @Override
     public Object execute(VirtualFrame frame) {
+        assert frame.getArguments().length == 0;
+        // TODO(peterssen): Class name is converted to host, then reserialized to guest.
+        String className = null;
         try {
-            assert frame.getArguments().length == 0;
-            // TODO(peterssen): Class name is converted to host, then reserialized to guest.
-            String className = InteropLibrary.getFactory().getUncached().asString(targetClassName);
-            EspressoContext context = EspressoLanguage.getCurrentContext();
-            Meta meta = context.getMeta();
-            StaticObject appClassLoader = (StaticObject) meta.java_lang_ClassLoader_getSystemClassLoader.invokeDirect(null);
-            StaticObject guestClass = (StaticObject) meta.java_lang_Class.lookupDeclaredMethod(Name.forName, Signature.Class_String_boolean_ClassLoader).invokeDirect(null,
-                            meta.toGuestString(className), false, appClassLoader);
-            return guestClass.getMirrorKlass();
-        } catch (EspressoException e) {
-            StaticObject guestException = e.getExceptionObject();
-            guestException.getKlass().lookupMethod(Name.printStackTrace, Signature._void).invokeDirect(guestException);
-            return StaticObject.NULL;
-        } catch (Throwable e) {
-            CompilerDirectives.transferToInterpreter();
-            return StaticObject.NULL;
+            className = InteropLibrary.getFactory().getUncached().asString(targetClassName);
+        } catch (UnsupportedMessageException e) {
+            throw new RuntimeException(e);
         }
+        EspressoContext context = EspressoLanguage.getCurrentContext();
+        Meta meta = context.getMeta();
+        StaticObject appClassLoader = (StaticObject) meta.java_lang_ClassLoader_getSystemClassLoader.invokeDirect(null);
+        StaticObject guestClass = (StaticObject) meta.java_lang_Class.lookupDeclaredMethod(Name.forName, Signature.Class_String_boolean_ClassLoader).invokeDirect(null,
+                        meta.toGuestString(className), false, appClassLoader);
+        return guestClass.getMirrorKlass();
+
     }
 
     private static StaticObject toGuestArguments(EspressoContext context, String... args) {
