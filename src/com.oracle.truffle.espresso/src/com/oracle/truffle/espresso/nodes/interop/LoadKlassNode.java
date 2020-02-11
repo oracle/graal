967b23b300341cf019e0b67b0d42b0cec9edf23e
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-02-11 18:42:54 +0100

    Cleanup LoadKlassNode + add boundaries around exceptions.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/interop/LoadKlassNode.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/interop/LoadKlassNode.java
@@ -1,17 +1,23 @@
 package com.oracle.truffle.espresso.nodes.interop;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.nodes.RootNode;
 import com.oracle.truffle.espresso.EspressoLanguage;
-import com.oracle.truffle.espresso.descriptors.Symbol.Name;
-import com.oracle.truffle.espresso.descriptors.Symbol.Signature;
 import com.oracle.truffle.espresso.meta.Meta;
 import com.oracle.truffle.espresso.runtime.EspressoContext;
+import com.oracle.truffle.espresso.runtime.EspressoException;
 import com.oracle.truffle.espresso.runtime.StaticObject;
+import com.oracle.truffle.espresso.vm.InterpreterToVM;
 
+/**
+ *
+ */
 public final class LoadKlassNode extends RootNode {
+
+    private final InteropLibrary uncached = InteropLibrary.getFactory().getUncached();
 
     private final String targetClassName;
 
@@ -23,19 +29,25 @@ public final class LoadKlassNode extends RootNode {
     @Override
     public Object execute(VirtualFrame frame) {
         assert frame.getArguments().length == 0;
-        // TODO(peterssen): Class name is converted to host, then reserialized to guest.
         String className = null;
         try {
-            className = InteropLibrary.getFactory().getUncached().asString(targetClassName);
+            className = uncached.asString(targetClassName);
         } catch (UnsupportedMessageException e) {
-            throw new RuntimeException(e);
+            CompilerDirectives.transferToInterpreter();
+            throw new IllegalArgumentException(e);
         }
         EspressoContext context = EspressoLanguage.getCurrentContext();
         Meta meta = context.getMeta();
         StaticObject appClassLoader = (StaticObject) meta.java_lang_ClassLoader_getSystemClassLoader.invokeDirect(null);
-        StaticObject guestClass = (StaticObject) meta.java_lang_Class.lookupDeclaredMethod(Name.forName, Signature.Class_String_boolean_ClassLoader).invokeDirect(null,
-                        meta.toGuestString(className), false, appClassLoader);
 
-        return guestClass.getMirrorKlass();
+        try {
+            StaticObject guestClass = (StaticObject) meta.java_lang_Class_forName_String_boolean_ClassLoader.invokeDirect(null, meta.toGuestString(className), false, appClassLoader);
+            return guestClass.getMirrorKlass();
+        } catch (EspressoException e) {
+            if (InterpreterToVM.instanceOf(e.getExceptionObject(), meta.java_lang_ClassNotFoundException)) {
+                return StaticObject.NULL;
+            }
+            throw e;
+        }
     }
 }
