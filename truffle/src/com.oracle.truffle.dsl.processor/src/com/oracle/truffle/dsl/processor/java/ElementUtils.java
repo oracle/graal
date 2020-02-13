1fc18890f8d49f7081fe8e68e084428a0a8d52b1
Tomas Zezula <tomas.zezula@oracle.com>
2020-02-13 15:01:13 +0100

    [GR-20942] Truffle DSL processor fails with ClassCastException in 19.3.0.
    (cherry picked from commit 810029a91d9c9a02e30e01a83501eb87c3710796)

--- truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/java/ElementUtils.java
+++ truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/java/ElementUtils.java
@@ -472,11 +472,11 @@ public class ElementUtils {
             return false;
         }
 
-        if (from instanceof ArrayType && to instanceof ArrayType) {
+        if (from.getKind() == TypeKind.ARRAY && to.getKind() == TypeKind.ARRAY) {
             return isAssignable(((ArrayType) from).getComponentType(), ((ArrayType) to).getComponentType());
         }
 
-        if (from instanceof ArrayType || to instanceof ArrayType) {
+        if (from.getKind() == TypeKind.ARRAY || to.getKind() == TypeKind.ARRAY) {
             return false;
         }
 
@@ -815,7 +815,7 @@ public class ElementUtils {
     }
 
     public static boolean isDeprecated(TypeMirror baseType) {
-        if (baseType instanceof DeclaredType) {
+        if (baseType != null && baseType.getKind() == TypeKind.DECLARED) {
             return isDeprecated((TypeElement) ((DeclaredType) baseType).asElement());
         }
         return false;
