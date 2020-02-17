47cc4d2129e405f26dd660a37bc148a1674b20ee
Fred Morcos <fred.morcos@oracle.com>
2020-02-17 19:36:13 +0100

    Sulong: Make interop pointer constructor public

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropType.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropType.java
@@ -152,7 +152,7 @@ public abstract class LLVMInteropType implements TruffleObject {
             return new Value(kind, null, size);
         }
 
-        static Value pointer(Structured baseType, long size) {
+        public static Value pointer(Structured baseType, long size) {
             return new Value(ValueKind.POINTER, baseType, size);
         }
 
