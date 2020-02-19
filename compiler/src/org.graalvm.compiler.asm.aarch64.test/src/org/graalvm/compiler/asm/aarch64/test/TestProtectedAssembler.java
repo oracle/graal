5b1d4e28647734eca456226d5ed2c0fc4db8df68
Xiaohong Gong <xiaohong.gong@arm.com>
2020-02-19 10:25:12 +0000

    AArch64: Add match rules for bitfield move.
    This patch adds the following match rules to generate bitfield
    move instruction on AArch64:
    * (RightShift (LeftShift value a) b)         -> SBFX/SBFIZ
    * (UnsignedRightShift (LeftShift value a) b) -> UBFX/UBFIZ
    * (LeftShift (SignExtend value) a)           -> SBFIZ
    Eg:
      lsl    w0, w1, #8
      asr    w0, w0, #15
    is optimized to:
      sbfx   w0, w1, #7, #17
    
    It also adds the rules to integrate the ZeroExtend with unsigned
    bitfield move operation.
    Eg:
      ubfiz  w0, w1, #5, #12
      and    x0, x0, #0xffffffff
    is optimized to:
      ubfiz  x0, x1, #5, #12
    
    Change-Id: I2b635d4895db0d5f4c30630176f336e9a226ccaf

--- compiler/src/org.graalvm.compiler.asm.aarch64.test/src/org/graalvm/compiler/asm/aarch64/test/TestProtectedAssembler.java
+++ compiler/src/org.graalvm.compiler.asm.aarch64.test/src/org/graalvm/compiler/asm/aarch64/test/TestProtectedAssembler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -218,7 +218,7 @@ class TestProtectedAssembler extends AArch64Assembler {
     }
 
     @Override
-    protected void sbfm(int size, Register dst, Register src, int r, int s) {
+    public void sbfm(int size, Register dst, Register src, int r, int s) {
         super.sbfm(size, dst, src, r, s);
     }
 
