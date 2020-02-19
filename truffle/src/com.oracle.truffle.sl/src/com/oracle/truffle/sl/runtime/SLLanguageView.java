bee6e13fa71924414f83f8fdda0bc33a06951e13
Christian Humer <christian.humer@oracle.com>
2020-02-19 13:32:47 +0100

    Add documentation to SLLanguageView.

--- truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/runtime/SLLanguageView.java
+++ truffle/src/com.oracle.truffle.sl/src/com/oracle/truffle/sl/runtime/SLLanguageView.java
@@ -49,12 +49,20 @@ import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
-import com.oracle.truffle.api.library.ReflectionLibrary;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.sl.SLLanguage;
 
+/**
+ * Language views are needed in order to allow tools to have a consistent perspective on primitive
+ * or foreign values from the perspective of this language. The interop interpretation for primitive
+ * values like Integer or String is not language specific by default. Therefore this language view
+ * calls routines to print such values the SimpleLanguage way. It is important to note that language
+ * views are not passed as normal values through the interpreter execution. It is designed only as a
+ * temporary helper for tools.
+ * <p>
+ * There is more information in {@link TruffleLanguage#getLanguageView(Object, Object)}
+ */
 @ExportLibrary(value = InteropLibrary.class, delegateTo = "delegate")
-@ExportLibrary(value = ReflectionLibrary.class, delegateTo = "delegate")
 @SuppressWarnings("static-method")
 public final class SLLanguageView implements TruffleObject {
 
@@ -69,6 +77,10 @@ public final class SLLanguageView implements TruffleObject {
         return true;
     }
 
+    /*
+     * Language views must always associate with the language they were created for. This allows
+     * tooling to take a primitive or foreign value and create a value of simple language of it.
+     */
     @ExportMessage
     Class<? extends TruffleLanguage<?>> getLanguage() {
         return SLLanguage.class;
@@ -77,6 +89,16 @@ public final class SLLanguageView implements TruffleObject {
     @ExportMessage
     @ExplodeLoop
     boolean hasMetaObject(@CachedLibrary("this.delegate") InteropLibrary interop) {
+        /*
+         * We use the isInstance method to find out whether one of the builtin simple language types
+         * apply. If yes, then we can provide a meta object in getMetaObject. The interop contract
+         * requires to be precise.
+         *
+         * Since language views are only created for primitive values and values of other languages,
+         * values from simple language itself directly implement has/getMetaObject. For example
+         * SLFunction is already associated with the SLLanguage and therefore the language view will
+         * not be used.
+         */
         for (SLType type : SLType.PRECEDENCE) {
             if (type.isInstance(delegate, interop)) {
                 return true;
@@ -88,6 +110,9 @@ public final class SLLanguageView implements TruffleObject {
     @ExportMessage
     @ExplodeLoop
     Object getMetaObject(@CachedLibrary("this.delegate") InteropLibrary interop) throws UnsupportedMessageException {
+        /*
+         * We do the same as in hasMetaObject but actually return the type this time.
+         */
         for (SLType type : SLType.PRECEDENCE) {
             if (type.isInstance(delegate, interop)) {
                 return type;
@@ -102,6 +127,10 @@ public final class SLLanguageView implements TruffleObject {
         for (SLType type : SLType.PRECEDENCE) {
             if (type.isInstance(this.delegate, interop)) {
                 try {
+                    /*
+                     * The type is a partial evaluation constant here as we use @ExplodeLoop. So
+                     * this if-else cascade should fold after partial evaluation.
+                     */
                     if (type == SLType.NUMBER) {
                         return longToString(interop.asLong(delegate));
                     } else if (type == SLType.BOOLEAN) {
@@ -121,20 +150,24 @@ public final class SLLanguageView implements TruffleObject {
         return "Unsupported";
     }
 
+    /*
+     * Long.toString is not safe for partial evaluation and therefore needs to be called behind a
+     * boundary.
+     */
     @TruffleBoundary
     private static String longToString(long l) {
         return Long.toString(l);
     }
 
     public static Object create(Object value) {
-        assert otherLanguageOrPrimitive(value);
+        assert isPrimitiveOrFromOtherLanguage(value);
         return new SLLanguageView(value);
     }
 
     /*
      * Language views are intended to be used only for primitives and other language values.
      */
-    private static boolean otherLanguageOrPrimitive(Object value) {
+    private static boolean isPrimitiveOrFromOtherLanguage(Object value) {
         InteropLibrary interop = InteropLibrary.getFactory().getUncached(value);
         try {
             return !interop.hasLanguage(value) || interop.getLanguage(value) != SLLanguage.class;
