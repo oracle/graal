5d680dc954dc2533471ff842fc8d138ae7cb175a
Matthieu Bovel <matthieu.bovel@oracle.com>
2020-02-21 14:35:28 +0100

    Fix if parentheses

--- wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/WasmSuiteBase.java
+++ wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/WasmSuiteBase.java
@@ -304,8 +304,9 @@ public abstract class WasmSuiteBase extends WasmTestBase {
     }
 
     private static void validateThrown(WasmCaseData data, WasmCaseData.ErrorType phase, PolyglotException e) throws PolyglotException {
-        if (data.expectedErrorMessage() == null)
+        if (data.expectedErrorMessage() == null) {
             throw e;
+        }
         Assert.assertEquals("Unexpected error message.", data.expectedErrorMessage(), e.getMessage());
         Assert.assertEquals("Unexpected error phase (should not have been thrown during the running phase).", data.expectedErrorTime(), phase);
     }
