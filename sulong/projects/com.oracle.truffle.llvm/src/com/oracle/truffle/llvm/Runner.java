668352ad32726534c655417545055c3fa730e8ad
Josef Eisl <josef.eisl@oracle.com>
2020-02-25 16:54:43 +0100

    sulong: BinaryParserResult should have a Source

--- sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
+++ sulong/projects/com.oracle.truffle.llvm/src/com/oracle/truffle/llvm/Runner.java
@@ -852,8 +852,9 @@ final class Runner {
         return parse(source, lib, source.getBytes(), parseContext);
     }
 
-    private LLVMParserResult parseBinary(BinaryParserResult binaryParserResult, Source source, ExternalLibrary library) {
+    private LLVMParserResult parseBinary(BinaryParserResult binaryParserResult, ExternalLibrary library) {
         ModelModule module = new ModelModule();
+        Source source = binaryParserResult.getSource();
         LLVMScanner.parseBitcode(binaryParserResult.getBitcode(), module, source, context);
         TargetDataLayout layout = module.getTargetDataLayout();
         DataLayout targetDataLayout = new DataLayout(layout.getDataLayout());
@@ -883,7 +884,7 @@ final class Runner {
                     }
                 }
             }
-            LLVMParserResult parserResult = parseBinary(binaryParserResult, source, library);
+            LLVMParserResult parserResult = parseBinary(binaryParserResult, library);
             parserResult.setDependencies(dependencies);
             parseContext.parserResultsAdd(parserResult);
             return parserResult;
