6c660e8e943c02a190c3e82fe273546ea03d874f
Matthieu Bovel <matthieu.bovel@oracle.com>
2020-02-25 14:51:33 +0100

    Add test for invalid table index in element segments

--- wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/BinaryParser.java
+++ wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/BinaryParser.java
@@ -957,8 +957,12 @@ public class BinaryParser extends BinaryStreamParser {
         int numElements = readVectorLength();
         for (int elemSegmentId = 0; elemSegmentId != numElements; ++elemSegmentId) {
             int tableIndex = readUnsignedInt32();
-            // At the moment, WebAssembly only supports one table instance, thus the only valid
+            // At the moment, WebAssembly (1.0, MVP) only supports one table instance, thus the only
+            // valid
             // table index is 0.
+            // Support for different table indices and "segment flags" might be added in the future
+            // (see
+            // https://github.com/WebAssembly/bulk-memory-operations/blob/master/proposals/bulk-memory-operations/Overview.md#element-segments).
             Assert.assertIntEqual(tableIndex, 0, "Invalid table index");
 
             // Table offset expression must be a constant expression with result type i32.
