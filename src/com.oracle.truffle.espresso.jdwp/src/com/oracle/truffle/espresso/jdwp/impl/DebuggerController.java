9bf00fec83daaa590fe86a85a33d7d0f197881c4
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-02-27 17:17:14 +0100

    Update to the latest getRawFrame API changes.

--- src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/DebuggerController.java
+++ src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/DebuggerController.java
@@ -946,7 +946,7 @@ public final class DebuggerController implements ContextsListener {
                     continue;
                 }
 
-                Frame rawFrame = frame.getRawFrame(context.getLanguageClass());
+                Frame rawFrame = frame.getRawFrame(context.getLanguageClass(), FrameInstance.FrameAccess.READ_ONLY);
                 MethodRef method = getContext().getMethodFromRootNode(root);
                 KlassRef klass = method.getDeclaringKlass();
 
