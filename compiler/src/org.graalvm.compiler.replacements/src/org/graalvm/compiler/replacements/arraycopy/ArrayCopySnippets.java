64909d234b83bf17e3c5585044381f9c652b009f
Gergö Barany <gergo.barany@oracle.com>
2020-03-09 17:35:51 +0100

    Remove global expandArraycopyLoop parameter

--- compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/arraycopy/ArrayCopySnippets.java
+++ compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/arraycopy/ArrayCopySnippets.java
@@ -375,7 +375,6 @@ public abstract class ArrayCopySnippets implements Snippets {
 
         private ResolvedJavaMethod originalArraycopy;
         private final Counters counters;
-        private boolean expandArraycopyLoop;
 
         public Templates(ArrayCopySnippets receiver, OptionValues options, Iterable<DebugHandlersFactory> factories, Factory factory, Providers providers,
                         SnippetReflectionProvider snippetReflection, TargetDescription target) {
@@ -398,7 +397,7 @@ public abstract class ArrayCopySnippets implements Snippets {
         }
 
         public void lower(ArrayCopyNode arraycopy, LoweringTool tool) {
-            lower(arraycopy, this.expandArraycopyLoop, tool);
+            lower(arraycopy, false, tool);
         }
 
         public void lower(ArrayCopyNode arraycopy, boolean mayExpandThisArraycopy, LoweringTool tool) {
@@ -460,7 +459,7 @@ public abstract class ArrayCopySnippets implements Snippets {
                 }
             }
 
-            if (this.expandArraycopyLoop && mayExpandThisArraycopy && snippetInfo == arraycopyExactStubCallSnippet) {
+            if (mayExpandThisArraycopy && snippetInfo == arraycopyExactStubCallSnippet) {
                 snippetInfo = arraycopyExactSnippet;
             }
 
@@ -498,9 +497,13 @@ public abstract class ArrayCopySnippets implements Snippets {
         }
 
         public void lower(ArrayCopyWithDelayedLoweringNode arraycopy, LoweringTool tool) {
+            lower(arraycopy, false, tool);
+        }
+
+        public void lower(ArrayCopyWithDelayedLoweringNode arraycopy, boolean mayExpandArraycopyLoops, LoweringTool tool) {
             StructuredGraph graph = arraycopy.graph();
 
-            if (arraycopy.getSnippet() == exactArraycopyWithSlowPathWork && this.expandArraycopyLoop) {
+            if (arraycopy.getSnippet() == exactArraycopyWithSlowPathWork && mayExpandArraycopyLoops) {
                 if (!graph.getGuardsStage().areDeoptsFixed()) {
                     // Don't lower until floating guards are fixed.
                     return;
@@ -600,10 +603,6 @@ public abstract class ArrayCopySnippets implements Snippets {
                 }
             }
             return originalArraycopy;
-        }
-
-        public void setExpandArraycopyLoop(boolean b) {
-            this.expandArraycopyLoop = b;
         }
     }
 }
