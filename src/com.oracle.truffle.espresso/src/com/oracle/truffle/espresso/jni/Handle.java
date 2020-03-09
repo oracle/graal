35e6452f49fc21a328ab05b825b8e3350eb81198
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-03-09 11:21:47 +0100

    Add documentation for @Pointer and @Handle annotations.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Handle.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Handle.java
@@ -1,17 +1,39 @@
 package com.oracle.truffle.espresso.jni;
 
+import com.oracle.truffle.espresso.impl.Field;
+import com.oracle.truffle.espresso.impl.Method;
+import com.oracle.truffle.espresso.runtime.StaticObject;
+
 import static java.lang.annotation.ElementType.TYPE_USE;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marker for parameters and return types to hint the primitive is a handle.
+ *
+ * <h4>Usage:</h4>
+ * <p>
+ * JNI handle:
+ *
+ * <pre>
+ * public &#64;Handle(StaticObject.class) long NewGlobalRef(&#64;Handle(StaticObject.class) long handle) {
+ * </pre>
+ *
+ * Field handle:
+ *
+ * <pre>
+ * public boolean GetBooleanField(StaticObject object, &#64;Handle(Field.class) long fieldId)
+ * </pre>
+ * </p>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value = {TYPE_USE})
 public @interface Handle {
     /**
-     * Class of the expected handle.
+     * Class of the object referenced by the handle. Expected types are {@link Field},
+     * {@link Method} and {@link StaticObject}.
      */
     Class<?> value() default Handle.class;
 }
-
--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Pointer.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/jni/Pointer.java
@@ -5,6 +5,29 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marker for parameters and return types to hint the interop object is a native pointer.
+ *
+ * On the Java/Espresso side all pointers are represented as interop {@code TruffleObject}
+ * instances. This annotation serves to generate proper signatures to communicate with native code.
+ * It can also be used as a hint for fields and local variables; but no checks are performed and no
+ * code is derived for those.
+ *
+ * <h4>Usage:</h4>
+ * <p>
+ * Receives a methodID and a pointer:
+ *
+ * <pre>
+ * public void CallVoidMethodVarargs(&#64;Host(Object.class) StaticObject receiver, &#64;Handle(Method.class) long methodId, &#64;Pointer TruffleObject varargsPtr)
+ * </pre>
+ *
+ * Returning a pointer:
+ *
+ * <pre>
+ * public &#64;Pointer TruffleObject GetDoubleArrayElements(&#64;Host(double[].class) StaticObject array, &#64;Pointer TruffleObject isCopyPtr)
+ * </pre>
+ * </p>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE_USE)
 public @interface Pointer {
