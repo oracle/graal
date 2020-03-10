105dbf977865147909b66b5c11e86ba346208a63
Andrii Rodionov <andrii.rodionov@oracle.com>
2020-03-10 13:57:22 +0200

    [GR-21605] Fix getAllStackTraces potential deadlock

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaUtilSubstitutions.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaUtilSubstitutions.java
@@ -31,12 +31,8 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentNavigableMap;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import com.oracle.svm.core.annotate.Alias;
-import com.oracle.svm.core.annotate.InjectAccessors;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.RecomputeFieldValue.Kind;
 import com.oracle.svm.core.annotate.Substitute;
@@ -227,76 +223,6 @@ final class Target_java_util_concurrent_ConcurrentSkipListMap_SubMap {
 
 @TargetClass(value = java.util.concurrent.ConcurrentSkipListMap.class, innerClass = "Values")
 final class Target_java_util_concurrent_ConcurrentSkipListMap_Values {
-}
-
-@TargetClass(java.util.SplittableRandom.class)
-final class Target_java_util_SplittableRandom {
-
-    @Alias @InjectAccessors(SplittableRandomAccessors.class)//
-    private static AtomicLong defaultGen;
-
-    @Alias
-    static native long mix64(long z);
-}
-
-class SplittableRandomAccessors {
-
-    /*
-     * We run this code deliberately during image generation, so that the SecureRandom code is only
-     * reachable and included in the image when requested by the application.
-     */
-    private static final boolean SECURE_SEED = java.security.AccessController.doPrivileged(
-                    new java.security.PrivilegedAction<Boolean>() {
-                        @Override
-                        public Boolean run() {
-                            return Boolean.getBoolean("java.util.secureRandomSeed");
-                        }
-                    });
-
-    private static volatile AtomicLong defaultGen;
-
-    private static final Lock lock = new ReentrantLock();
-
-    /** The get-accessor for SplittableRandom.defaultGen. */
-    static AtomicLong getDefaultGen() {
-        AtomicLong result = defaultGen;
-        if (result == null) {
-            result = initialize();
-        }
-        return result;
-    }
-
-    private static AtomicLong initialize() {
-        lock.lock();
-        try {
-
-            AtomicLong result = defaultGen;
-            if (result != null) {
-                return result;
-            }
-
-            /*
-             * The code below to compute the seed is taken from the original
-             * SplittableRandom.initialSeed() implementation.
-             */
-            long seed;
-            if (SECURE_SEED) {
-                byte[] seedBytes = java.security.SecureRandom.getSeed(8);
-                seed = seedBytes[0] & 0xffL;
-                for (int i = 1; i < 8; ++i) {
-                    seed = (seed << 8) | (seedBytes[i] & 0xffL);
-                }
-            } else {
-                seed = Target_java_util_SplittableRandom.mix64(System.currentTimeMillis()) ^ Target_java_util_SplittableRandom.mix64(System.nanoTime());
-            }
-
-            result = new AtomicLong(seed);
-            defaultGen = result;
-            return result;
-        } finally {
-            lock.unlock();
-        }
-    }
 }
 
 @TargetClass(java.util.Currency.class)
---
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/SplittableRandomAccessors.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2020, 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.oracle.svm.core.jdk;
+
+import com.oracle.svm.core.annotate.Alias;
+import com.oracle.svm.core.annotate.InjectAccessors;
+import com.oracle.svm.core.annotate.TargetClass;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@TargetClass(java.util.SplittableRandom.class)
+final class Target_java_util_SplittableRandom {
+
+    @Alias @InjectAccessors(SplittableRandomAccessors.class)//
+    private static AtomicLong defaultGen;
+
+    @Alias
+    static native long mix64(long z);
+}
+
+public class SplittableRandomAccessors {
+
+    /*
+     * We run this code deliberately during image generation, so that the SecureRandom code is only
+     * reachable and included in the image when requested by the application.
+     */
+    private static final boolean SECURE_SEED = java.security.AccessController.doPrivileged(
+                    new java.security.PrivilegedAction<Boolean>() {
+                        @Override
+                        public Boolean run() {
+                            return Boolean.getBoolean("java.util.secureRandomSeed");
+                        }
+                    });
+
+    private static volatile AtomicLong defaultGen;
+
+    /** The get-accessor for SplittableRandom.defaultGen. */
+    public static AtomicLong getDefaultGen() {
+        AtomicLong result = defaultGen;
+        if (result == null) {
+            result = initialize();
+        }
+        return result;
+    }
+
+    // Checkstyle: allow synchronization
+    private static synchronized AtomicLong initialize() {
+        AtomicLong result = defaultGen;
+        if (result != null) {
+            return result;
+        }
+
+        /*
+         * The code below to compute the seed is taken from the original
+         * SplittableRandom.initialSeed() implementation.
+         */
+        long seed;
+        if (SECURE_SEED) {
+            byte[] seedBytes = java.security.SecureRandom.getSeed(8);
+            seed = seedBytes[0] & 0xffL;
+            for (int i = 1; i < 8; ++i) {
+                seed = (seed << 8) | (seedBytes[i] & 0xffL);
+            }
+        } else {
+            seed = Target_java_util_SplittableRandom.mix64(System.currentTimeMillis()) ^ Target_java_util_SplittableRandom.mix64(System.nanoTime());
+        }
+
+        result = new AtomicLong(seed);
+        defaultGen = result;
+        return result;
+    }
+    // Checkstyle: disallow synchronization
+}
