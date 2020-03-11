25458499330121dfb104055d958f619858caf478
Tom Rodriguez <tom.rodriguez@oracle.com>
2020-03-10 18:05:06 -0700

    Fix formatting of JavaDoc
    (cherry picked from commit 66bb00c176b71ae2eeb569821e49589321540f1b)

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/CallTargetNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/CallTargetNode.java
@@ -81,36 +81,41 @@ public abstract class CallTargetNode extends ValueNode implements LIRLowerable {
     protected ResolvedJavaMethod targetMethod;
 
     /**
-     *  Receiver type referenced at the interface call site.
+     * Receiver type referenced at the interface call site.
      *
-     *  We need to distinguish the declaring type from the type referenced at the call site. We must use
-     *  the referenced type as lower type bound when doing CHA since interface calls must throw exception
-     *  if the receiver type is not a subtype of the reference type.
+     * We need to distinguish the declaring type from the type referenced at the call site. We must
+     * use the referenced type as lower type bound when doing CHA since interface calls must throw
+     * exception if the receiver type is not a subtype of the reference type.
      *
-     *  Example:
-     *  <pre>
-     *      interface I1 {
-     *          void foo();
-     *      }
-     *      interface I2 extends I1 { }
+     * Example:
      *
-     *      void bar(I2 o) {
-     *          o.foo();
-     *      }
-     *  </pre>
+     * <pre>
+     * interface I1 {
+     *     void foo();
+     * }
      *
-     *  Here at the call site the declaring type for {@code foo()} is {@code I1}, while the referenced type
-     *  is {@code I2}. Only receivers of type {@code T} that is {@code T <: I2} should be allowed at the call site.
-     *  If they are not - an exception should be thrown.
+     * interface I2 extends I1 {
+     * }
      *
-     *  Since the interface types are not verified, another way to think about this call site is to rewrite it
-     *  as follows:
-     *  <pre>
-     *      void bar(Object o) {
-     *          ((I2) o).foo();
-     *      }
-     *  </pre>
-     *  So, in case the receiver is not a subtype of {@code I2} an exception is thrown.
+     * void bar(I2 o) {
+     *     o.foo();
+     * }
+     * </pre>
+     *
+     * Here at the call site the declaring type for {@code foo()} is {@code I1}, while the
+     * referenced type is {@code I2}. Only receivers of type {@code T} that is {@code T <: I2}
+     * should be allowed at the call site. If they are not - an exception should be thrown.
+     *
+     * Since the interface types are not verified, another way to think about this call site is to
+     * rewrite it as follows:
+     *
+     * <pre>
+     * void bar(Object o) {
+     *     ((I2) o).foo();
+     * }
+     * </pre>
+     *
+     * So, in case the receiver is not a subtype of {@code I2} an exception is thrown.
      */
     protected ResolvedJavaType referencedType;
 
