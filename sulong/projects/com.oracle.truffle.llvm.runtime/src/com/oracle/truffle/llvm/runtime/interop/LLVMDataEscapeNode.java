9d9a7d527b6e88826cc05fb909f8a5b0d2dc00e3
Benoit Daloze <benoit.daloze@oracle.com>
2020-03-11 17:33:21 +0100

    Don't convert foreign objects eagerly to native for LLVMI64DataEscapeNode

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/LLVMDataEscapeNode.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/LLVMDataEscapeNode.java
@@ -204,10 +204,15 @@ public abstract class LLVMDataEscapeNode extends LLVMNode {
             return Double.doubleToRawLongBits(escapingValue);
         }
 
-        @Specialization(limit = "3", replaces = "escapingPrimitive")
-        static long escapingPointer(Object escapingValue, @SuppressWarnings("unused") LLVMInteropType.Structured type,
-                        @CachedLibrary("escapingValue") LLVMNativeLibrary library) {
-            return library.toNativePointer(escapingValue).asNative();
+        @Specialization
+        static long escapingNativePointer(LLVMNativePointer escapingValue, @SuppressWarnings("unused") LLVMInteropType.Structured type) {
+            return escapingValue.asNative();
+        }
+
+        @Specialization
+        static Object escapingPointer(LLVMManagedPointer escapingValue, LLVMInteropType.Structured type,
+                        @Cached LLVMPointerDataEscapeNode pointerDataEscapeNode) {
+            return pointerDataEscapeNode.executeWithType(escapingValue, type);
         }
     }
 
