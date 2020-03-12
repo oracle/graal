45dfe72939b16ec27ebbccb4fd70da25bb77ec30
Boris Spasojevic <boris.spasojevic@oracle.com>
2020-03-12 20:51:10 +0100

    Move Shebang to beginning of scripts in external repos.

--- truffle/external_repos/simplelanguage/native/make_native.sh
+++ truffle/external_repos/simplelanguage/native/make_native.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
--- truffle/external_repos/simpletool/runJsWithCoverage.sh
+++ truffle/external_repos/simpletool/runJsWithCoverage.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 #
 # Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
