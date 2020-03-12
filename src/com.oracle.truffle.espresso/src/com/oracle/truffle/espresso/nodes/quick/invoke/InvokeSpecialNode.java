a9ea9276647801d45d597651a6d90a7cf1af392f
Allan Gregersen <allan.gregersen@oracle.com>
2020-03-12 11:29:32 +0100

    insert transfer to interpreter on direct call node updates

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/quick/invoke/InvokeSpecialNode.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/quick/invoke/InvokeSpecialNode.java
@@ -22,6 +22,7 @@
  */
 package com.oracle.truffle.espresso.nodes.quick.invoke;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.DirectCallNode;
@@ -46,6 +47,7 @@ public final class InvokeSpecialNode extends QuickNode {
         BytecodeNode root = getBytecodesNode();
         if (!method.getAssumption().isValid()) {
             // update to the latest method version and grab a new direct call target
+            CompilerDirectives.transferToInterpreterAndInvalidate();
             method = method.getMethod().getMethodVersion();
             directCallNode = DirectCallNode.create(method.getCallTarget());
             adoptChildren();
