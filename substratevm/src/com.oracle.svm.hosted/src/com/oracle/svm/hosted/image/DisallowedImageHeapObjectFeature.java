3bdb4e359d99f9f73d197c8f4b625d1c45418484
Christian Wimmer <christian.wimmer@oracle.com>
2020-03-16 16:46:57 -0700

    Ensure user directories do not leak into image heap

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/DisallowedImageHeapObjectFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/DisallowedImageHeapObjectFeature.java
@@ -24,9 +24,18 @@
  */
 package com.oracle.svm.hosted.image;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.graalvm.nativeimage.hosted.Feature;
 
 import com.oracle.graal.pointsto.constraints.UnsupportedFeatureException;
+import com.oracle.svm.core.SubstrateOptions;
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import com.oracle.svm.core.image.DisallowedImageHeapObjects;
 import com.oracle.svm.core.option.SubstrateOptionsParser;
@@ -43,15 +52,71 @@ public class DisallowedImageHeapObjectFeature implements Feature {
 
     private ClassInitializationSupport classInitialization;
 
+    private String[] disallowedSubstrings;
+    private Map<byte[], Charset> disallowedByteSubstrings;
+
     @Override
     public void duringSetup(DuringSetupAccess access) {
         classInitialization = ((FeatureImpl.DuringSetupAccessImpl) access).getHostVM().getClassInitializationSupport();
         access.registerObjectReplacer(this::replacer);
+
+        if (SubstrateOptions.DetectUserDirectoriesInImageHeap.getValue()) {
+            /*
+             * We do not check for the temp directory name and the user name because they have a too
+             * high chance of being short or generic terms that appear in valid strings.
+             */
+            disallowedSubstrings = new String[]{
+                            System.getProperty("user.home"),
+                            System.getProperty("user.dir"),
+                            System.getProperty("java.home")};
+
+            /* We cannot check all byte[] encodings of strings, but we want to check common ones. */
+            Set<Charset> encodings = new HashSet<>(Arrays.asList(
+                            StandardCharsets.UTF_8,
+                            StandardCharsets.UTF_16,
+                            Charset.forName(System.getProperty("sun.jnu.encoding"))));
+
+            disallowedByteSubstrings = new IdentityHashMap<>();
+            for (int i = 0; i < disallowedSubstrings.length; i++) {
+                String s = disallowedSubstrings[i];
+                for (Charset encoding : encodings) {
+                    disallowedByteSubstrings.put(s.getBytes(encoding), encoding);
+                }
+            }
+        }
     }
 
     private Object replacer(Object original) {
         if (original instanceof Thread && original instanceof ImageGeneratorThreadMarker) {
             return ((ImageGeneratorThreadMarker) original).asTerminated();
+        }
+
+        if (original instanceof String && disallowedSubstrings != null) {
+            String string = (String) original;
+            for (String disallowedSubstring : disallowedSubstrings) {
+                if (string.contains(disallowedSubstring)) {
+                    throw new UnsupportedFeatureException("Detected a string in the image heap that contains a user directory. " +
+                                    "This means that file system information from the native image build is persisted and available at image run time, which is most likely an error." +
+                                    System.lineSeparator() + "String that is problematic: " + string + System.lineSeparator() +
+                                    "Disallowed substring with user directory: " + disallowedSubstring + System.lineSeparator() +
+                                    "This check can be disabled using the option " + SubstrateOptionsParser.commandArgument(SubstrateOptions.DetectUserDirectoriesInImageHeap, "-"));
+                }
+            }
+        }
+
+        if (original instanceof byte[] && disallowedByteSubstrings != null) {
+            byte[] bytes = (byte[]) original;
+            for (Map.Entry<byte[], Charset> entry : disallowedByteSubstrings.entrySet()) {
+                byte[] disallowedSubstring = entry.getKey();
+                if (search(bytes, disallowedSubstring)) {
+                    Charset charset = entry.getValue();
+                    throw new UnsupportedFeatureException("Detected a byte[] in the image heap that contains a user directory. " +
+                                    "This means that file system information from the native image build is persisted and available at image run time, which is most likely an error." +
+                                    System.lineSeparator() + "byte[] that is problematic: " + new String(bytes, charset) + System.lineSeparator() +
+                                    "Disallowed substring with user directory: " + new String(disallowedSubstring, charset) + System.lineSeparator() +
+                                    "This check can be disabled using the option " + SubstrateOptionsParser.commandArgument(SubstrateOptions.DetectUserDirectoriesInImageHeap, "-"));
+                }
+            }
         }
 
         DisallowedImageHeapObjects.check(original, this::error);
@@ -64,5 +129,24 @@ public class DisallowedImageHeapObjectFeature implements Feature {
                         "You can request class initialization at image run time by using the option " +
                         SubstrateOptionsParser.commandArgument(ClassInitializationFeature.Options.ClassInitialization, "<class-name>", "initialize-at-run-time") + ". " +
                         "Or you can write your own initialization methods and call them explicitly from your main entry point.");
+    }
+
+    private static boolean search(byte[] haystack, byte[] needle) {
+        byte first = needle[0];
+        for (int start = 0; start < haystack.length - needle.length; start++) {
+            if (haystack[start] == first) {
+                boolean same = true;
+                for (int i = 1; i < needle.length; i++) {
+                    if (haystack[start + i] != needle[i]) {
+                        same = false;
+                        break;
+                    }
+                }
+                if (same) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
