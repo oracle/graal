9cde9e188b774ab4b65b5708b70daf1cdd37eefe
Vít Šesták <vit.sestak@oracle.com>
2020-03-17 13:25:59 +0100

    Fixed a typo
    (cherry picked from commit 08e99843cb04a4408bf2f045b51e942c327fad1b)

--- tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/server/WSInterceptorServer.java
+++ tools/src/com.oracle.truffle.tools.chromeinspector/src/com/oracle/truffle/tools/chromeinspector/server/WSInterceptorServer.java
@@ -70,10 +70,10 @@ public final class WSInterceptorServer implements InspectorWSConnection, Message
     }
 
     @Override
-    public void close(Token tokenToCLose) throws IOException {
+    public void close(Token tokenToClose) throws IOException {
         iss.setMessageListener(null);
         if (inspectEndpoint != null) {
-            if (tokenToCLose.equals(this.token)) {
+            if (tokenToClose.equals(this.token)) {
                 inspectEndpoint.sendClose();
             }
         }
