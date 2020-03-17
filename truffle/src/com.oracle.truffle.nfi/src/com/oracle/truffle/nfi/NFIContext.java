c2a99bcc99b5f1ad75b25a9c435133926c15f107
Roland Schatz <roland.schatz@oracle.com>
2020-03-17 16:08:40 +0100

    Use language initialization API in Truffle NFI.

--- truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/NFIContext.java
+++ truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/NFIContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -97,21 +97,7 @@ final class NFIContext {
                 NFIBackendFactory backendFactory = env.lookup(language, NFIBackendFactory.class);
                 if (backendFactory != null && backendFactory.getBackendId().equals(id)) {
                     // force initialization of the backend language
-                    Source source = Source.newBuilder(language.getId(), "", "").internal(true).build();
-                    try {
-                        env.parseInternal(source);
-                    } catch (Exception ex) {
-                        boolean rethrow = true;
-                        if (ex instanceof TruffleException) {
-                            TruffleException te = (TruffleException) ex;
-                            if (te.isIncompleteSource() || te.isSyntaxError()) {
-                                rethrow = false;
-                            }
-                        }
-                        if (rethrow) {
-                            throw ex;
-                        }
-                    }
+                    env.initializeLanguage(language);
 
                     NFIBackend backend = backendFactory.createBackend(TOOLS);
                     backendCache.put(backendFactory.getBackendId(), backend);
