52104a3b8e1afbe88f96c0e3f3631eb55f5fee59
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2020-03-18 17:33:46 +0100

    Update js imports.

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "c330ffa95b683f00ea702c92d096be2a199fd63b",
+                "version": "3debd6ee2b9755e82cb067e3df9eacfbf30a0789",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "c330ffa95b683f00ea702c92d096be2a199fd63b",
+                "version": "3debd6ee2b9755e82cb067e3df9eacfbf30a0789",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
