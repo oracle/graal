02b4331ca394a7f6cbaa2adb8d77e81fc6ed132c
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-03-19 18:10:56 +0100

    Avoid lock on StaticObject instance.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ClassRegistries.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ClassRegistries.java
@@ -62,15 +62,20 @@ public final class ClassRegistries {
         // Double-checked locking to attach class registry to guest instance.
         ClassRegistry classRegistry = (ClassRegistry) classLoader.getHiddenFieldVolatile(context.getMeta().HIDDEN_CLASS_LOADER_REGISTRY);
         if (classRegistry == null) {
-            synchronized (classLoader) {
+            // Synchronizing on the classLoader instance would be the natural choice here, but:
+            // On SubstrateVM, synchronizing on a StaticObject instance will add an extra slot/field
+            // to all StaticObject instances. Locking on the weak set instead (maybe) spares one
+            // slot/field in every single guest object.
+            // Setting the class registry happens only once, for such rare operations, no contention
+            // is expected.
+            synchronized (weakClassLoaderSet) {
                 classRegistry = (ClassRegistry) classLoader.getHiddenFieldVolatile(context.getMeta().HIDDEN_CLASS_LOADER_REGISTRY);
                 if (classRegistry == null) {
                     classRegistry = new GuestClassRegistry(context, classLoader);
                     classLoader.setHiddenFieldVolatile(context.getMeta().HIDDEN_CLASS_LOADER_REGISTRY, classRegistry);
                     // Register the class loader in the weak set.
-                    synchronized (weakClassLoaderSet) {
-                        weakClassLoaderSet.add(classLoader);
-                    }
+                    assert Thread.holdsLock(weakClassLoaderSet);
+                    weakClassLoaderSet.add(classLoader);
                 }
             }
         }
