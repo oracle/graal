c58d9581651af295f1222e26e788e5afcb352c85
Andrew Dinn <adinn@redhat.com>
2020-03-20 10:10:28 +0000

    test use of regex to allow red Hat copyrights

--- substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/ObjectFile.java
+++ substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/ObjectFile.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2020, Red Hat Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
