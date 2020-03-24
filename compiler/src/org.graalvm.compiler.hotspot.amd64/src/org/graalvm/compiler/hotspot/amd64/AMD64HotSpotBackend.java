8b176291c26bf93ab23b364ce081c42cdbf3b204
Tom Rodriguez <tom.rodriguez@oracle.com>
2020-03-24 11:36:36 -0700

    record FRAME_COMPLETE on other backends

--- compiler/src/org.graalvm.compiler.hotspot.amd64/src/org/graalvm/compiler/hotspot/amd64/AMD64HotSpotBackend.java
+++ compiler/src/org.graalvm.compiler.hotspot.amd64/src/org/graalvm/compiler/hotspot/amd64/AMD64HotSpotBackend.java
@@ -170,6 +170,9 @@ public class AMD64HotSpotBackend extends HotSpotHostBackend implements LIRGenera
                 } else {
                     asm.decrementq(rsp, frameSize);
                 }
+                if (config.MARKID_FRAME_COMPLETE != -1) {
+                    crb.recordMark(config.MARKID_FRAME_COMPLETE);
+                }
                 if (ZapStackOnMethodEntry.getValue(crb.getOptions())) {
                     final int intSize = 4;
                     for (int i = 0; i < frameSize / intSize; ++i) {
@@ -177,9 +180,6 @@ public class AMD64HotSpotBackend extends HotSpotHostBackend implements LIRGenera
                     }
                 }
                 assert frameMap.getRegisterConfig().getCalleeSaveRegisters() == null;
-            }
-            if (config.MARKID_FRAME_COMPLETE != -1) {
-                crb.recordMark(config.MARKID_FRAME_COMPLETE);
             }
         }
 
