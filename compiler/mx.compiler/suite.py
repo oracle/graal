fef6a458e552007f816d1d7a8466433436172e4a
Tom Rodriguez <tom.rodriguez@oracle.com>
2020-04-01 09:36:26 -0700

    Restore logic for being careful about field layout in unsafe PEA tests

--- compiler/mx.compiler/suite.py
+++ compiler/mx.compiler/suite.py
@@ -600,23 +600,6 @@ suite = {
       "workingSets" : "Graal,HotSpot",
     },
 
-    "org.graalvm.compiler.hotspot.jdk14" : {
-      "subDir" : "src",
-      "sourceDirs" : ["src"],
-      "dependencies": ["org.graalvm.compiler.hotspot"],
-      "requiresConcealed" : {
-        "jdk.internal.vm.ci" : [
-          "jdk.vm.ci.hotspot",
-          "jdk.vm.ci.meta"
-        ],
-      },
-      "overlayTarget" : "org.graalvm.compiler.hotspot",
-      "multiReleaseJarVersion" : "14",
-      "checkstyle" : "org.graalvm.compiler.graph",
-      "javaCompliance" : "14+",
-      "workingSets" : "Graal,HotSpot",
-    },
-
     "org.graalvm.compiler.hotspot.management" : {
       "subDir" : "src",
       "sourceDirs" : ["src"],
