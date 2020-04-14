b97e3d8156bd40972c8cc5dd82566b9c2264f1bc
Christian Wirth <christian.wirth@oracle.com>
2020-04-14 10:32:12 +0000

    [GR-22168] Increase test coverage.
    PullRequest: js/1452

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "cd72398a6957865eca003d212ce1bdc3d3774906",
+                "version": "77e978faa9c3885d4df54cb8da438cf9929585da",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "cd72398a6957865eca003d212ce1bdc3d3774906",
+                "version": "77e978faa9c3885d4df54cb8da438cf9929585da",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
