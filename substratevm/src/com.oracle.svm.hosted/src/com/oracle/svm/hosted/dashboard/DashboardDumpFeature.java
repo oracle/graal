fea6d4fa9684dd5b01181774c7c2177d59a23318
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2020-05-13 11:58:21 +0200

    Optimize dashboard dumping.

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/dashboard/DashboardDumpFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/dashboard/DashboardDumpFeature.java
@@ -25,17 +25,125 @@
 package com.oracle.svm.hosted.dashboard;
 
 import com.oracle.graal.pointsto.reports.ReportUtils;
-import com.oracle.shadowed.com.google.gson.Gson;
-import com.oracle.shadowed.com.google.gson.GsonBuilder;
-import com.oracle.shadowed.com.google.gson.JsonObject;
+import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.nativeimage.hosted.Feature;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @AutomaticFeature
 public class DashboardDumpFeature implements Feature {
+    static class Dict {
+        final LinkedHashMap<String, Object> sections;
+
+        Dict() {
+            this.sections = new LinkedHashMap<>();
+        }
+
+        public void insert(String key, Object value) {
+            sections.put(key, value);
+        }
+
+        public boolean hasKey(String key) {
+            return sections.containsKey(key);
+        }
+
+        public Object get(String key) {
+            return sections.get(key);
+        }
+
+        public Integer getInt(String key) {
+            return (Integer) get(key);
+        }
+
+        public Number getNumber(String key) {
+            return (Number) get(key);
+        }
+
+        @SuppressWarnings("unchecked")
+        public ArrayList<Object> getList(String key) {
+            return (ArrayList<Object>) get(key);
+        }
+
+        public String getString(String key) {
+            return (String) get(key);
+        }
+
+        public Dict getDict(String key) {
+            return (Dict) get(key);
+        }
+
+        public void dump(PrintWriter writer) {
+            writer.println();
+            writer.println("{");
+            int index = 0;
+            for (Map.Entry<String, Object> entry : sections.entrySet()) {
+                writer.print("\"");
+                writer.print(entry.getKey());
+                writer.print("\": ");
+                final Object value = entry.getValue();
+                dumpValue(writer, value);
+                index++;
+                if (index < sections.size()) {
+                    writer.print(",");
+                }
+                writer.println();
+            }
+            writer.println("}");
+            writer.println();
+        }
+
+        public static String escape(String input) {
+            String escaped = input;
+            escaped = escaped.replace("\\", "\\\\");
+            escaped = escaped.replace("\"", "\\\"");
+            escaped = escaped.replace("\b", "\\b");
+            escaped = escaped.replace("\f", "\\f");
+            escaped = escaped.replace("\n", "\\n");
+            escaped = escaped.replace("\r", "\\r");
+            escaped = escaped.replace("\t", "\\t");
+            escaped = escaped.replace("/", "\\/");
+            return escaped;
+        }
+
+        @SuppressWarnings("unchecked")
+        private void dumpValue(PrintWriter writer, Object value) {
+            if (value instanceof Number) {
+                writer.print(value);
+            } else if (value instanceof String) {
+                writer.print("\"");
+                writer.print(escape((String) value));
+                writer.print("\"");
+            } else if (value instanceof ArrayList) {
+                dumpList(writer, (ArrayList<Object>) value);
+            } else if (value instanceof Dict) {
+                ((Dict) value).dump(writer);
+            } else if (value == null) {
+                writer.print("null");
+            } else {
+                throw GraalError.shouldNotReachHere("Unknown value: " + value + ", type: " + value.getClass());
+            }
+        }
+
+        private void dumpList(PrintWriter writer, ArrayList<Object> list) {
+            writer.print("[");
+            int index = 0;
+            for (Object value : list) {
+                dumpValue(writer, value);
+                index++;
+                if (index < list.size()) {
+                    writer.print(", ");
+                }
+            }
+            writer.print("]");
+        }
+    }
+
     private static boolean isHeapBreakdownDumped() {
         return DashboardOptions.DashboardAll.getValue() || DashboardOptions.DashboardHeap.getValue();
     }
@@ -48,15 +156,17 @@ public class DashboardDumpFeature implements Feature {
         return DashboardOptions.DashboardAll.getValue() || DashboardOptions.DashboardCode.getValue();
     }
 
-    private static void dumpToFile(String dumpContent, String dumpPath) {
+    private static void dumpToFile(Dict dumpContent, String dumpPath) {
         final File file = new File(dumpPath).getAbsoluteFile();
-        ReportUtils.report("Dashboard dump output", file.toPath(), writer -> writer.write(dumpContent));
+        ReportUtils.report("Dashboard dump output", file.toPath(), writer -> {
+            dumpContent.dump(writer);
+        });
     }
 
-    private final JsonObject dumpRoot;
+    private final Dict dumpRoot;
 
     public DashboardDumpFeature() {
-        dumpRoot = new JsonObject();
+        dumpRoot = new Dict();
     }
 
     @Override
@@ -68,8 +178,8 @@ public class DashboardDumpFeature implements Feature {
     public void onAnalysisExit(OnAnalysisExitAccess access) {
         if (isPointsToDumped()) {
             final PointsToDumper pointsToDumper = new PointsToDumper();
-            final JsonObject pointsTo = pointsToDumper.dump(access);
-            dumpRoot.add("points-to", pointsTo);
+            final Dict pointsTo = pointsToDumper.dump(access);
+            dumpRoot.insert("points-to", pointsTo);
         }
     }
 
@@ -77,8 +187,8 @@ public class DashboardDumpFeature implements Feature {
     public void afterCompilation(AfterCompilationAccess access) {
         if (isCodeBreakdownDumped()) {
             final CodeBreakdownDumper codeBreakdownDumper = new CodeBreakdownDumper();
-            final JsonObject breakdown = codeBreakdownDumper.dump(access);
-            dumpRoot.add("code-breakdown", breakdown);
+            final Dict breakdown = codeBreakdownDumper.dump(access);
+            dumpRoot.insert("code-breakdown", breakdown);
         }
     }
 
@@ -86,15 +196,13 @@ public class DashboardDumpFeature implements Feature {
     public void afterHeapLayout(AfterHeapLayoutAccess access) {
         if (isHeapBreakdownDumped()) {
             final HeapBreakdownDumper heapBreakdownDumper = new HeapBreakdownDumper();
-            final JsonObject breakdown = heapBreakdownDumper.dump(access);
-            dumpRoot.add("heap-breakdown", breakdown);
+            final Dict breakdown = heapBreakdownDumper.dump(access);
+            dumpRoot.insert("heap-breakdown", breakdown);
         }
     }
 
     @Override
     public void cleanup() {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        final String dump = gson.toJson(dumpRoot);
-        dumpToFile(dump, DashboardOptions.DashboardDump.getValue());
+        dumpToFile(dumpRoot, DashboardOptions.DashboardDump.getValue());
     }
 }
