9f9a435cf6ace5612ac134cf90dae571d9901889
Paley Li <paley.li@oracle.com>
2020-05-13 16:33:16 +0200

    Sulong: minor, rename getLanguageContextExtensions

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/LLVMContext.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/LLVMContext.java
@@ -282,7 +282,7 @@ public final class LLVMContext {
         }
 
         this.threadingStack = new LLVMThreadingStack(Thread.currentThread(), parseStackSize(env.getOptions().get(SulongEngineOption.STACK_SIZE)));
-        for (ContextExtension ext : getLanguageContextExtension()) {
+        for (ContextExtension ext : getContextExtensions()) {
             ext.initialize();
         }
         String languageHome = language.getLLVMLanguageHome();
@@ -297,7 +297,7 @@ public final class LLVMContext {
         loader.loadDefaults(this, internalLibraryPath);
     }
 
-    private List<ContextExtension> getLanguageContextExtension() {
+    private List<ContextExtension> getContextExtensions() {
         verifyContextExtensionsInitialized();
         return contextExtensions;
     }
