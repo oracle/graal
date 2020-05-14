b2fa0333b44f1c3ed447f6e8deb15deeb7d95b31
Josef Eisl <josef.eisl@oracle.com>
2020-05-14 10:08:18 +0200

    sdk: update to LLVM 10

--- sulong/projects/com.oracle.truffle.llvm.toolchain.config/src/com/oracle/truffle/llvm/toolchain/config/LLVMConfig.java
+++ sulong/projects/com.oracle.truffle.llvm.toolchain.config/src/com/oracle/truffle/llvm/toolchain/config/LLVMConfig.java
@@ -39,9 +39,9 @@ public abstract class LLVMConfig {
     private LLVMConfig() {}
 
     /** LLVM version string. */
-    public static final String VERSION = "9.0.0";
+    public static final String VERSION = "10.0.0";
     /** Major version of the LLVM API. */
-    public static final int VERSION_MAJOR = 9;
+    public static final int VERSION_MAJOR = 10;
     /** Minor version of the LLVM API. */
     public static final int VERSION_MINOR = 0;
     /** Patch version of the LLVM API. */
