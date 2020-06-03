ebb0931a863d476206ec8bfa4a049a758ee32f24
Xiaohong Gong <xiaohong.gong@arm.com>
2020-06-03 07:42:37 +0000

    AArch64: Add the full barrier implementation support for CAS.
    Graal always uses "LDAXR+STLXR" for Unsafe atomic operations. However
    this is not sufficient on AArch64 for the VM internal atomics that need
    the full barrier semantics (See [1]).
    
    This patch adds an alternative implementation for "CAS", which has the
    full barrier semantics. The final "CAS" pattern is decided by an flag
    "useBarriers". Since we prefer to use the optimized "LDAXR+STLXR" pattern
    , the flag is false by default. It can be set to true by checking the
    memory "LocationIdentity" for the VM internal "CAS" operation if it needs
    the full barrier semantic.
    
    [1] https://patchwork.kernel.org/patch/3575821/

--- compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64LIRGenerator.java
+++ compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64LIRGenerator.java
@@ -152,21 +152,24 @@ public abstract class AArch64LIRGenerator extends LIRGenerator {
     }
 
     @Override
-    public Variable emitLogicCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue, Value trueValue, Value falseValue) {
-        Variable prevValue = newVariable(expectedValue.getValueKind());
-        Variable scratch = newVariable(LIRKind.value(AArch64Kind.DWORD));
-        append(new CompareAndSwapOp(prevValue, loadReg(expectedValue), loadReg(newValue), asAllocatable(address), scratch));
+    public Variable emitLogicCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue, Value trueValue, Value falseValue, boolean useBarriers) {
+        emitCompareAndSwap(address, expectedValue, newValue, useBarriers);
         assert trueValue.getValueKind().equals(falseValue.getValueKind());
+        assert isIntConstant(trueValue, 1) && isIntConstant(falseValue, 0);
         Variable result = newVariable(trueValue.getValueKind());
-        append(new CondMoveOp(result, ConditionFlag.EQ, asAllocatable(trueValue), asAllocatable(falseValue)));
+        append(new CondSetOp(result, ConditionFlag.EQ));
         return result;
     }
 
     @Override
-    public Variable emitValueCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue) {
-        Variable result = newVariable(newValue.getValueKind());
-        Variable scratch = newVariable(LIRKind.value(AArch64Kind.WORD));
-        append(new CompareAndSwapOp(result, loadReg(expectedValue), loadReg(newValue), asAllocatable(address), scratch));
+    public Variable emitValueCompareAndSwap(LIRKind accessKind, Value address, Value expectedValue, Value newValue, boolean useBarriers) {
+        return emitCompareAndSwap(address, expectedValue, newValue, useBarriers);
+    }
+
+    private Variable emitCompareAndSwap(Value address, Value expectedValue, Value newValue, boolean useBarriers) {
+        Variable result = newVariable(expectedValue.getValueKind());
+        Variable scratch = newVariable(LIRKind.value(AArch64Kind.DWORD));
+        append(new CompareAndSwapOp(result, loadReg(expectedValue), loadReg(newValue), asAllocatable(address), scratch, useBarriers));
         return result;
     }
 
