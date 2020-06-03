f4da2be2ad5af8e09562319e36bf494d7104c140
Vladimir Kozlov <vladimir.kozlov@oracle.com>
2020-06-03 01:58:48 +0000

    Update scripts for Metropolis project

--- compiler/mx.compiler/mx_renamegraalpackages.py
+++ compiler/mx.compiler/mx_renamegraalpackages.py
@@ -43,12 +43,23 @@ def renamegraalpackages(args):
 
     package_suffixes = {
         'org.graalvm.collections' : ['', '.test'],
+        'org.graalvm.nativeimage' : ['', '.test'],
+        'org.graalvm.options'     : [''],
         'org.graalvm.word'        : [''],
-        'org.graalvm.libgraal'    : ['', '.jdk8', '.jdk11', '.jdk13']
+        'org.graalvm.libgraal'    : ['', '.jdk8', '.jdk11', '.jdk13', '.jni', '.jni.annotation', '.jni.processor']
     }
     vc_dir = mx.primary_suite().vc_dir
 
     # rename packages
+    def rename_one(filepath, old, new):
+        with open(filepath) as fp:
+            contents = fp.read()
+        new_contents = contents
+        new_contents = new_contents.replace(old, new)
+        if new_contents != contents:
+            with open(filepath, 'w') as fp:
+                fp.write(new_contents)
+
     for proj_dir in [join(vc_dir, x) for x in os.listdir(vc_dir) if exists(join(vc_dir, x, 'mx.' + x, 'suite.py'))]:
         for dirpath, _, filenames in os.walk(proj_dir):
             if args.version >= 15 and "sparc" in dirpath:
@@ -56,32 +67,34 @@ def renamegraalpackages(args):
                 shutil.rmtree(dirpath)
             else:
                 for filename in filenames:
-                    if filename.endswith('.java') or filename == 'suite.py' or filename == 'generate_unicode_properties.py':
-                        rename_packages(join(dirpath, filename))
+                    if filename.endswith('.java') or filename == 'suite.py' or filename == 'generate_unicode_properties.py' or filename == "mx_substratevm.py" or "META-INF" in dirpath:
+                        filepath = join(dirpath, filename)
+                        rename_packages(filepath)
 
         # move directories according to new package name
         for old_name, new_name in package_renamings.items():
             for sfx in package_suffixes[old_name]:
-                old_dir = join(proj_dir, 'src', old_name + sfx, 'src', old_name.replace('.', os.sep))
+                old_path = join(proj_dir, 'src', old_name + sfx)
+                old_dir = join(old_path, 'src', old_name.replace('.', os.sep))
                 if exists(old_dir):
                     new_name_sfx = new_name + sfx
-                    if exists(join(proj_dir, 'src', new_name_sfx)):
-                        shutil.rmtree(join(proj_dir, 'src', new_name_sfx))
-                    new_dir = join(proj_dir, 'src', new_name_sfx, 'src', new_name.replace('.', os.sep))
+                    new_path = join(proj_dir, 'src', new_name_sfx)
+                    if exists(new_path):
+                        shutil.rmtree(new_path)
+                    new_dir = join(new_path, 'src', new_name.replace('.', os.sep))
                     os.makedirs(new_dir)
+                    mx.log("Move files to new dir: " + new_dir)
                     for f in os.listdir(old_dir):
                         shutil.move(os.path.join(old_dir, f), new_dir)
-                    shutil.rmtree(join(proj_dir, 'src', old_name + sfx))
+                    old_meta = join(old_path, 'src', 'META-INF')
+                    if exists(old_meta):
+                        new_meta = join(new_path, 'src', 'META-INF')
+                        shutil.move(old_meta, new_meta)
+                    shutil.rmtree(old_path)
 
     # rename in additional place
     package = 'com.oracle.svm.graal.hotspot.libgraal'
     filepath = join(vc_dir, 'substratevm', 'src', package, 'src', package.replace('.', os.sep), 'LibGraalEntryPoints.java')
-    with open(filepath) as fp:
-        contents = fp.read()
-    new_contents = contents
-    old_name = 'Java_org_graalvm_libgraal'
-    new_name = 'Java_jdk_internal_vm_compiler_libgraal'
-    new_contents = new_contents.replace(old_name, new_name)
-    if new_contents != contents:
-        with open(filepath, 'w') as fp:
-            fp.write(new_contents)
+    old = 'Java_org_graalvm_libgraal'
+    new = 'Java_jdk_internal_vm_compiler_libgraal'
+    rename_one(filepath, old, new)
