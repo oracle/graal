b1e2c888fd7247b1c257c26c39a551708d592249
Jan Stola <jan.stola@oracle.com>
2020-06-14 09:58:45 +0000

    [GR-23929] Upgrading the underlying Node.js to version 12.18.0.
    PullRequest: js/1528

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "ff157737baf76e184e946d6a0150f8e121c281ac",
+                "version": "b1777ea8abf17726806b04f3a8ff8f201e34bed0",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "ff157737baf76e184e946d6a0150f8e121c281ac",
+                "version": "b1777ea8abf17726806b04f3a8ff8f201e34bed0",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
