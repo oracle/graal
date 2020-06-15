0101521fa26a5e4b45b53fda031f509458bc21ff
Christian Wirth <christian.wirth@oracle.com>
2020-06-15 14:29:27 +0000

    [GR-16406] Remove the leniency towards timeouts in TestSuites.
    PullRequest: js/1533

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "b1777ea8abf17726806b04f3a8ff8f201e34bed0",
+                "version": "8fb94483c971a234e5eb37e946df02979109f593",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "b1777ea8abf17726806b04f3a8ff8f201e34bed0",
+                "version": "8fb94483c971a234e5eb37e946df02979109f593",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
