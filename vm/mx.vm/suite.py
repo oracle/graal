96e5390ce29d8204dfc7b07816f74a6c3ae18650
Christian Wirth <christian.wirth@oracle.com>
2020-06-18 13:47:16 +0000

    [GR-24243] Exclude package-lock.json with outdated dependencies.
    PullRequest: js/1544

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "8fb94483c971a234e5eb37e946df02979109f593",
+                "version": "f59ffbdeef2d88823d409edcfe335dfffb7b45b8",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "8fb94483c971a234e5eb37e946df02979109f593",
+                "version": "f59ffbdeef2d88823d409edcfe335dfffb7b45b8",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
