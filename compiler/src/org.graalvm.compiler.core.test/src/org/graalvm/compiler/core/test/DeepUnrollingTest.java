cd9328f2f7ab6deed3937fa0158b67979516f453
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2020-06-18 10:27:58 +0200

    Fix transient failure of DeepUnrollingTest
    * Increase margin a bit.
    * Print timing in case of failure.

--- compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/DeepUnrollingTest.java
+++ compiler/src/org.graalvm.compiler.core.test/src/org/graalvm/compiler/core/test/DeepUnrollingTest.java
@@ -59,6 +59,8 @@ public class DeepUnrollingTest extends SubprocessTest {
         return v;
     }
 
+    private static final int ACCEPTABLE_FACTOR = 50;
+
     public void loopTest() {
         // warmup
         time("reference");
@@ -66,7 +68,9 @@ public class DeepUnrollingTest extends SubprocessTest {
         long reference = time("reference");
         long loops = time("loops");
         // observed ratio is ~20-30x. Pathological case before fix was ~300x
-        assertTrue("Compilation of the loop nest is too slow", loops < reference * 45);
+        if (loops > reference * ACCEPTABLE_FACTOR) {
+            fail("Compilation of the loop nest is too slow. loops: %dms > %d * reference: %dms", loops, ACCEPTABLE_FACTOR, reference);
+        }
     }
 
     public long time(String methodName) {
