b68b5a1fa64efdcd3ea3d17744cd014aef6a06e3
Esteban Ginez <esteban.ginez@oracle.com>
2020-06-23 10:04:22 -0700

    Adds changes to support static linked image with libc linked dynamically

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeBootImageViaCC.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeBootImageViaCC.java
@@ -30,9 +30,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Formatter;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -82,6 +85,9 @@ public abstract class NativeBootImageViaCC extends NativeBootImage {
     }
 
     class BinutilsCCLinkerInvocation extends CCLinkerInvocation {
+
+        private final boolean shouldDynamicallyLinkLibC = SubstrateOptions.StaticExecutableWithDynamicLibC.getValue();
+        private final Set<String> libCLibaries = new HashSet<>(Arrays.asList("pthread", "dl", "rt"));
 
         BinutilsCCLinkerInvocation() {
             additionalPreOptions.add("-z");
@@ -136,7 +142,9 @@ public abstract class NativeBootImageViaCC extends NativeBootImage {
                 case EXECUTABLE:
                     break;
                 case STATIC_EXECUTABLE:
-                    cmd.add("-static");
+                    if (!shouldDynamicallyLinkLibC) {
+                        cmd.add("-static");
+                    }
                     break;
                 case SHARED_LIBRARY:
                     cmd.add("-shared");
@@ -146,6 +154,20 @@ public abstract class NativeBootImageViaCC extends NativeBootImage {
             }
         }
 
+        @Override
+        protected List<String> getLibrariesCommand() {
+            List<String> cmd = new ArrayList<>();
+            for (String lib : libs) {
+                if (shouldDynamicallyLinkLibC) {
+                    String linkingMode = libCLibaries.contains(lib)
+                                    ? "dynamic"
+                                    : "static";
+                    cmd.add("-Wl,-B" + linkingMode);
+                }
+                cmd.add("-l" + lib);
+            }
+            return cmd;
+        }
     }
 
     class DarwinCCLinkerInvocation extends CCLinkerInvocation {
