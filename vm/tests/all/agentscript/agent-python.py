974283418f336b10f5a3c6aa09e380988b52c8d9
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2020-06-25 12:19:50 +0200

    Python GraalVM Insight script to print out values on a function entry

--- vm/tests/all/agentscript/agent-python.py
+++ vm/tests/all/agentscript/agent-python.py
@@ -1,3 +1,17 @@
 print("Python: Insight version {} is launching".format(insight.version))
 insight.on("source", lambda env : print("Python: observed loading of {}".format(env.name)))
 print("Python: Hooks are ready!")
+
+def onEnter(ctx, frame):
+    print('minusOne {}'.format(frame.n))
+
+class Roots:
+    roots = True
+
+    def sourceFilter(self, src):
+        return src.name == 'agent-fib.js'
+
+    def rootNameFilter(self, n):
+        return n == "minusOne"
+
+insight.on("enter", onEnter, Roots())
