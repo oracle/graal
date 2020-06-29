7806cbc48629e3feb9c4f99ddbdf60a2b48528cd
Andrew Dinn <adinn@redhat.com>
2020-06-29 14:55:26 +0100

    ensure frame size changes allow for code beyond prologue end

--- compiler/src/org.graalvm.compiler.hotspot.sparc/src/org/graalvm/compiler/hotspot/sparc/SPARCHotSpotBackend.java
+++ compiler/src/org.graalvm.compiler.hotspot.sparc/src/org/graalvm/compiler/hotspot/sparc/SPARCHotSpotBackend.java
@@ -216,6 +216,11 @@ public class SPARCHotSpotBackend extends HotSpotHostBackend implements LIRGenera
             SPARCMacroAssembler masm = (SPARCMacroAssembler) crb.asm;
             masm.restoreWindow();
         }
+
+        @Override
+        public void returned(CompilationResultBuilder crb) {
+            // nothing to do
+        }
     }
 
     @Override
