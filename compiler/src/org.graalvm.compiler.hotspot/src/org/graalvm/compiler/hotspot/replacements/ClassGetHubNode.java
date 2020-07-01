b49fe3646f058c1c2aceba7bb81edb7879caf5b6
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2020-07-01 19:30:13 +0200

    Use asConstant to reflectively obtain the Class type.
    Add more detailed information to graph error reports.

--- compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/replacements/ClassGetHubNode.java
+++ compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/replacements/ClassGetHubNode.java
@@ -37,6 +37,7 @@ import org.graalvm.compiler.graph.spi.CanonicalizerTool;
 import org.graalvm.compiler.hotspot.nodes.type.KlassPointerStamp;
 import org.graalvm.compiler.hotspot.word.KlassPointer;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
+import org.graalvm.compiler.nodeinfo.Verbosity;
 import org.graalvm.compiler.nodes.ConstantNode;
 import org.graalvm.compiler.nodes.NodeView;
 import org.graalvm.compiler.nodes.PiNode;
@@ -94,10 +95,9 @@ public final class ClassGetHubNode extends FloatingNode implements Lowerable, Ca
         } else {
             if (clazz.isConstant() && !clazz.isNullConstant()) {
                 if (metaAccess != null) {
-                    ResolvedJavaType exactType = constantReflection.asJavaType(clazz.asJavaConstant());
-                    if (exactType == null) {
-                        return null;
-                    } else if (exactType.isPrimitive()) {
+                    ResolvedJavaType exactType = constantReflection.asJavaType(clazz.asConstant());
+                    assert exactType != null : classGetHubNode.toString(Verbosity.Debugger);
+                    if (exactType.isPrimitive()) {
                         return ConstantNode.forConstant(stamp, JavaConstant.NULL_POINTER, metaAccess);
                     } else {
                         return ConstantNode.forConstant(stamp, constantReflection.asObjectHub(exactType), metaAccess);
