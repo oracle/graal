430c32a70e027dbe4f712fa8cccccf1d8990c1eb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2020-07-09 14:11:12 +0200

    Initialize java.security at run time

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/jdk/JDKInitializationFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/jdk/JDKInitializationFeature.java
@@ -24,9 +24,8 @@
  */
 package com.oracle.svm.hosted.jdk;
 
-import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.impl.RuntimeClassInitializationSupport;
+import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
 
@@ -34,125 +33,83 @@ import com.oracle.svm.core.annotate.AutomaticFeature;
 public class JDKInitializationFeature implements Feature {
     @Override
     public void afterRegistration(AfterRegistrationAccess access) {
-        RuntimeClassInitializationSupport rci = ImageSingletons.lookup(RuntimeClassInitializationSupport.class);
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.crypto.provider", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("apple.security", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.java.util.jar.pack", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("com.sun.crypto.provider", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.management", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("com.sun.java.util.jar.pack", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.naming.internal", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("com.sun.management", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.net.ssl", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("com.sun.naming.internal", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.nio.file", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.nio.sctp", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("com.sun.nio.zipfs", "Native Image classes are always initialized at build time");
 
-        rci.initializeAtBuildTime("com.sun.net.ssl", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.io", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("com.sun.nio.file", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("com.sun.nio.sctp", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("com.sun.nio.zipfs", "Native Image classes are always initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.lang", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("com.sun.security.auth", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("com.sun.security.jgss", "Core JDK classes are initialized at build time for better performance");
-        rci.initializeAtBuildTime("com.sun.security.cert.internal.x509", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("com.sun.security.ntlm", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("com.sun.security.sasl", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.math", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.io", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.net", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.lang", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.nio", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.math", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.text", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.net", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.time", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.nio", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("java.util", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.security", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("javax.annotation.processing", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.text", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("javax.crypto", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.time", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("javax.lang.model", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("java.util", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("javax.management", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("javax.annotation.processing", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("javax.naming", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("javax.crypto", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("javax.net", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("javax.lang.model", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("javax.tools", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("javax.management", "Core JDK classes are initialized at build time");
-
-        rci.initializeAtBuildTime("javax.naming", "Core JDK classes are initialized at build time");
-
-        rci.initializeAtBuildTime("javax.net", "Core JDK classes are initialized at build time");
-
-        rci.initializeAtBuildTime("javax.security.auth", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("javax.security.cert", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("javax.security.sasl", "Core JDK classes are initialized at build time");
-
-        rci.initializeAtBuildTime("javax.tools", "Core JDK classes are initialized at build time");
-
-        rci.initializeAtBuildTime("jdk.internal", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("jdk.internal", "Core JDK classes are initialized at build time");
 
         /* Needed by our substitutions */
-        rci.initializeAtBuildTime("jdk.jfr", "Core JDK classes are initialized at build time for better performance");
+        RuntimeClassInitialization.initializeAtBuildTime("jdk.jfr", "Core JDK classes are initialized at build time for better performance");
 
-        rci.initializeAtBuildTime("jdk.net", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("jdk.net", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("jdk.nio", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("jdk.nio", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("org.ietf.jgss", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("org.ietf.jgss", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.invoke", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.invoke", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.launcher", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.launcher", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.management", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.management", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.misc", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.misc", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.net", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.net", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.nio", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.nio", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.reflect", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.reflect", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.security.action", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.ec", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.internal.interfaces", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.internal.spec", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.jca", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.jgss", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.krb5", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.pkcs", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.pkcs10", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.pkcs11", "Core JDK classes are initialized at build time for better performance");
-        rci.initializeAtBuildTime("sun.security.pkcs12", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.provider", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.rsa", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.ssl", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.timestamp", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.tools", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.util", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.validator", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.x509", "Core JDK classes are initialized at build time");
-        rci.initializeAtBuildTime("sun.security.smartcardio", "Core JDK classes are initialized at build time");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.text", "Core JDK classes are initialized at build time");
 
-        rci.initializeAtBuildTime("sun.text", "Core JDK classes are initialized at build time");
-
-        rci.initializeAtBuildTime("sun.util", "Core JDK classes are initialized at build time");
-
-        rci.initializeAtBuildTime("org.ietf.jgss", "Core JDK classes are initialized at build time for better performance");
+        RuntimeClassInitialization.initializeAtBuildTime("sun.util", "Core JDK classes are initialized at build time");
 
         /* Minor fixes to make the list work */
-        rci.initializeAtRunTime("com.sun.naming.internal.ResourceManager$AppletParameter", "Initializes AWT");
-        rci.initializeAtBuildTime("org.jcp.xml.dsig.internal.dom.XMLDSigRI", "Required for sun.security.jca");
-        rci.initializeAtBuildTime("java.awt.font.TextAttribute", "Required for sun.text.bidi.BidiBase.NumericShapings");
-        rci.initializeAtBuildTime("java.awt.font.NumericShaper", "Required for sun.text.bidi.BidiBase.NumericShapings");
-
-        /* TODO move to features */
-        rci.initializeAtBuildTime("com.sun.jmx.mbeanserver.DefaultMXBeanMappingFactory", "Avoids unnecessary reflection in the image");
+        RuntimeClassInitialization.initializeAtRunTime("com.sun.naming.internal.ResourceManager$AppletParameter", "Initializes AWT");
+        RuntimeClassInitialization.initializeAtBuildTime("org.jcp.xml.dsig.internal.dom.XMLDSigRI", "Required for sun.security.jca");
+        RuntimeClassInitialization.initializeAtBuildTime("java.awt.font.TextAttribute", "Required for sun.text.bidi.BidiBase.NumericShapings");
+        RuntimeClassInitialization.initializeAtBuildTime("java.awt.font.NumericShaper", "Required for sun.text.bidi.BidiBase.NumericShapings");
     }
 }
