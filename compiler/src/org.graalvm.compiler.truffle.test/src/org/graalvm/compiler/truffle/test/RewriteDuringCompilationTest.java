d9075edf0351afe1f9b25775bc30e8984e102c83
Jakub Chaloupka <jakub.chaloupka@oracle.com>
2020-07-10 11:51:41 +0200

    Remove unused invalidation backoff from OptimizedOSRLoopNode. Simplify and make more robust the tests of node rewriting during compilations.

--- compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/RewriteDuringCompilationTest.java
+++ compiler/src/org.graalvm.compiler.truffle.test/src/org/graalvm/compiler/truffle/test/RewriteDuringCompilationTest.java
@@ -1,54 +1,47 @@
 /*
- * Copyright (c) 2020, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * The Universal Permissive License (UPL), Version 1.0
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
- * Subject to the condition set forth below, permission is hereby granted to any
- * person obtaining a copy of this software, associated documentation and/or
- * data (collectively the "Software"), free of charge and under any and all
- * copyright rights in the Software, and any and all patent rights owned or
- * freely licensable by each licensor hereunder covering either (i) the
- * unmodified Software as contributed to or provided by such licensor, or (ii)
- * the Larger Works (as defined below), to deal in both
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
  *
- * (a) the Software, and
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
- * one is included with the Software each a "Larger Work" to which the Software
- * is contributed by such licensors),
- *
- * without restriction, including without limitation the rights to copy, create
- * derivative works of, display, perform, and distribute the Software and make,
- * use, sell, offer for sale, import, export, have made, and have sold the
- * Software and the Larger Work(s), and to sublicense the foregoing rights on
- * either these or other terms.
- *
- * This license is subject to the following condition:
- *
- * The above copyright notice and either this complete permission notice or at a
- * minimum a reference to the UPL must be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
-package com.oracle.truffle.api.instrumentation.test;
+package org.graalvm.compiler.truffle.test;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.oracle.truffle.api.CallTarget;
@@ -57,6 +50,7 @@ import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.frame.FrameSlot;
 import com.oracle.truffle.api.frame.FrameSlotTypeException;
 import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.instrumentation.test.InstrumentationTestLanguage;
 import com.oracle.truffle.api.nodes.LoopNode;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.api.nodes.RepeatingNode;
@@ -66,35 +60,33 @@ import com.oracle.truffle.api.test.polyglot.AbstractPolyglotTest;
 import com.oracle.truffle.api.test.polyglot.ProxyLanguage;
 
 public class RewriteDuringCompilationTest extends AbstractPolyglotTest {
-    private static int staticRewriteCount = 0;
-    private static List<Integer> nodeExecutionSequence = Collections.synchronizedList(new ArrayList<>());
     private static volatile UpdateStaticFieldNode updateStaticFieldNode;
-    private static volatile int lastWrittenValue = -1;
+    private static volatile LoopNode loopNode;
 
     abstract static class BaseNode extends Node {
         abstract Object execute(VirtualFrame frame);
     }
 
     static final class UpdateStaticFieldNode extends BaseNode {
-        private final int c;
-
-        UpdateStaticFieldNode(int c) {
-            this.c = c;
-        }
+        private volatile boolean valid = true;
+        private boolean invalidTwice = false;
 
         @Override
         public Object execute(@SuppressWarnings("unused") VirtualFrame frame) {
-            return addToList(c);
-        }
-
-        @CompilerDirectives.TruffleBoundary
-        private static boolean addToList(int value) {
-            if (nodeExecutionSequence.size() == 0 || nodeExecutionSequence.get(nodeExecutionSequence.size() - 1) != value) {
-                System.out.println("Adding value " + value);
-                nodeExecutionSequence.add(value);
-                lastWrittenValue = value;
+            if (!valid) {
+                /*
+                 * In the interpreter, setting valid to false might happen just before the if
+                 * statement, yielding false test failures, and so we fail only if the invalid node
+                 * is executed twice.
+                 */
+                if (invalidTwice) {
+                    CompilerDirectives.transferToInterpreterAndInvalidate();
+                    throw new AssertionError("Obsolete code got executed!");
+                } else {
+                    invalidTwice = true;
+                }
             }
-            return value < staticRewriteCount;
+            return false;
         }
     }
 
@@ -107,6 +99,7 @@ public class RewriteDuringCompilationTest extends AbstractPolyglotTest {
 
         WhileLoopNode(Object loopCount, BaseNode child) {
             this.loop = Truffle.getRuntime().createLoopNode(new LoopConditionNode(loopCount, child));
+            loopNode = this.loop;
         }
 
         FrameSlot getLoopIndex() {
@@ -152,7 +145,7 @@ public class RewriteDuringCompilationTest extends AbstractPolyglotTest {
 
         final class LoopConditionNode extends BaseNode implements RepeatingNode {
 
-            @Child private BaseNode child;
+            @Child private volatile BaseNode child;
 
             private final int loopCount;
             private final boolean infinite;
@@ -174,6 +167,7 @@ public class RewriteDuringCompilationTest extends AbstractPolyglotTest {
 
             }
 
+            @Override
             public boolean executeRepeating(VirtualFrame frame) {
                 int i;
                 try {
@@ -199,18 +193,25 @@ public class RewriteDuringCompilationTest extends AbstractPolyglotTest {
         }
     }
 
-    @Test
-    public void testRootCompilation() throws IOException, InterruptedException {
-        testCompilation(updateStaticFieldNode = new UpdateStaticFieldNode(0), 1000, 10);
+    @Before
+    public void setUp() {
+        loopNode = null;
+        updateStaticFieldNode = null;
     }
 
     @Test
-    public void testLoopCompilation() throws IOException, InterruptedException {
-        testCompilation(new WhileLoopNode(1000000, updateStaticFieldNode = new UpdateStaticFieldNode(0)), 1000, 20);
+    public void testRootCompilation() throws IOException, InterruptedException, ExecutionException {
+        testCompilation(updateStaticFieldNode = new UpdateStaticFieldNode(), 1000, 20);
     }
 
-    private void testCompilation(BaseNode testedCode, int rewriteCount, int maxDelayBeforeRewrite) throws IOException, InterruptedException {
-        staticRewriteCount = rewriteCount;
+    @Test
+    public void testLoopCompilation() throws IOException, InterruptedException, ExecutionException {
+        testCompilation(new WhileLoopNode(10000000, updateStaticFieldNode = new UpdateStaticFieldNode()), 1000, 40);
+    }
+
+    private volatile boolean rewriting = false;
+
+    private void testCompilation(BaseNode testedCode, int rewriteCount, int maxDelayBeforeRewrite) throws IOException, InterruptedException, ExecutionException {
         setupEnv(Context.create(), new ProxyLanguage() {
             private final List<CallTarget> targets = new LinkedList<>(); // To prevent from GC
 
@@ -219,7 +220,7 @@ public class RewriteDuringCompilationTest extends AbstractPolyglotTest {
                 com.oracle.truffle.api.source.Source source = request.getSource();
                 CallTarget target = Truffle.getRuntime().createCallTarget(new RootNode(languageInstance) {
 
-                    @Node.Child private BaseNode child = testedCode;
+                    @Node.Child private volatile BaseNode child = testedCode;
 
                     @Override
                     public Object execute(VirtualFrame frame) {
@@ -238,46 +239,72 @@ public class RewriteDuringCompilationTest extends AbstractPolyglotTest {
         });
 
         Random rnd = new Random();
-        Thread mainThread = Thread.currentThread();
-        Thread t = new Thread(() -> {
-            for (int i = 1; i <= rewriteCount; i++) {
-                do {
+        CountDownLatch nodeRewritingLatch = new CountDownLatch(1);
+        List<Object> callTargetsToCheck = new ArrayList<>();
+        rewriting = true;
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        Future<?> future = executor.submit(() -> {
+            try {
+                for (int i = 1; i <= rewriteCount && rewriting; i++) {
                     try {
                         Thread.sleep(rnd.nextInt(maxDelayBeforeRewrite));
+                        nodeRewritingLatch.await();
                     } catch (InterruptedException ie) {
                     }
-                } while (lastWrittenValue < (i - 1));
-                updateStaticFieldNode.replace(updateStaticFieldNode = new UpdateStaticFieldNode(i));
+                    Object loopNodeCallTarget = getLoopNodeCallTarget();
+                    if (loopNodeCallTarget != null) {
+                        callTargetsToCheck.add(loopNodeCallTarget);
+                    }
+                    UpdateStaticFieldNode previousNode = updateStaticFieldNode;
+                    updateStaticFieldNode.replace(updateStaticFieldNode = new UpdateStaticFieldNode());
+                    previousNode.valid = false;
+                }
+            } finally {
+                rewriting = false;
             }
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException ie) {
-            }
-            mainThread.interrupt();
         });
-        t.start();
         Source source = Source.newBuilder(ProxyLanguage.ID, "", "DummySource").build();
-        long iterationCount = 0;
-        for (;;) {
-            iterationCount++;
-            context.eval(source);
-            if (Thread.interrupted()) {
-                break;
+        try {
+            for (;;) {
+                context.eval(source);
+                nodeRewritingLatch.countDown();
+                if (!rewriting) {
+                    break;
+                }
+            }
+        } finally {
+            rewriting = false;
+            future.get();
+            executor.shutdownNow();
+            executor.awaitTermination(100, TimeUnit.SECONDS);
+        }
+        for (Object callTarget : callTargetsToCheck) {
+            Assert.assertFalse("Obsolete loop call target is still valid", isLoopNodeCallTargetValid(callTarget));
+        }
+    }
+
+    private static Object getLoopNodeCallTarget() {
+        Object toRet = null;
+        if (loopNode != null && loopNode.getClass().getSuperclass() != null) {
+            try {
+                Field callTargetField = loopNode.getClass().getSuperclass().getDeclaredField("compiledOSRLoop");
+                callTargetField.setAccessible(true);
+                toRet = callTargetField.get(loopNode);
+            } catch (Exception e) {
             }
         }
-        t.join();
-        int problemCount = 0;
-        int prevInt = -1;
-        for (int i : nodeExecutionSequence) {
-            if (prevInt >= i) {
-                System.out.println("Wrong order " + prevInt + ", " + i);
-                problemCount++;
+        return toRet;
+    }
+
+    private static boolean isLoopNodeCallTargetValid(Object callTarget) {
+        boolean toRet = false;
+        if (callTarget != null) {
+            try {
+                Method isValidMethod = callTarget.getClass().getMethod("isValid");
+                toRet = (Boolean) isValidMethod.invoke(callTarget);
+            } catch (Exception e) {
             }
-            prevInt = i;
         }
-        System.out.println("Iteration Count = " + iterationCount);
-        System.out.println("Problem count = " + problemCount);
-        // System.out.println("Execution sequence = " +
-        // nodeExecutionSequence.stream().map(Object::toString).collect(Collectors.joining(", ")));
+        return toRet;
     }
 }
