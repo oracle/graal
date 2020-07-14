97e9039b1302b87fc4b1ebafa6be1814b791fafd
Katja Goltsova <katja.goltsova@oracle.com>
2020-07-14 09:32:49 +0200

    Espresso Polyglot API documentation 0.0.2

--- src/com.oracle.truffle.espresso.polyglot/src/com/oracle/truffle/espresso/polyglot/Polyglot.java
+++ src/com.oracle.truffle.espresso.polyglot/src/com/oracle/truffle/espresso/polyglot/Polyglot.java
@@ -23,16 +23,22 @@
 package com.oracle.truffle.espresso.polyglot;
 
 /**
-* Represents the Polyglot API, available from Espresso.
-* The method implementations are substituted, the code in this class is not used.
-*/
+ * Polyglot API for Espresso.
+ * <p>
+ * Allows Espresso to interact with other Truffle languages by {@link #eval evaluating} code in a
+ * different language, {@link #importObject importing} symbols, exported by other languages and
+ * making Espresso objects available to other languages by {@link #exportObject exporting} them.
+ * </p>
+ *
+ * NB: At runtime, the method implementations are substituted, only the definitions of this class
+ * are used.
+ */
 public final class Polyglot {
     private Polyglot() {
     }
 
     /**
-     * @param object the object to test
-     * @return true if the object is an interop object, i.e. comes from a different Truffle language.
+     * Tests if an object is an interop object, i.e. originates from a different Truffle language.
      */
     @SuppressWarnings("unused")
     public static boolean isInteropObject(Object object) {
@@ -40,27 +46,48 @@ public final class Polyglot {
     }
 
     /**
-     * If <code>value</code> is an interop object, from now on treat it as an object of <code>targetClass</code> type. Otherwise the case is a
-     * no-op, e.g. the existence of methods is not verified and if a method does not exist, an exception will be thrown
-     * <i> on method invocation </i>.
+     * If {@code value} is an interop object, changes its type to {@code targetClass}. The existence
+     * of methods, defined in {@code targetClass}, is not verified and if a method does not exist,
+     * an exception will be thrown only when this method is invoked.
      * <p>
-     * If <code>value</code> is a regular Espresso object, perform checkcast.
-     * @throws ClassCastException if <code>value</code> is a regular Espresso object and cannot be cast to <code>targetClass</code>.
+     * If {@code value} is a regular Espresso object, performs {@link Class#cast checkcast}.
+     *
+     * @throws ClassCastException if {@code value} is a regular Espresso object and cannot be cast
+     *             to {@code targetClass}.
      */
     public static <T> T cast(Class<? extends T> targetClass, Object value) throws ClassCastException {
         return targetClass.cast(value);
     }
 
+    // TODO: link something about language ids?
+    /**
+     * Evaluates the given code in the given language.
+     *
+     * @return the result of the evaluation wrapped as {@link Object}. To access members of the
+     *         underlying interop object, write a corresponding class or interface stub in Java and
+     *         cast the eval result to it using {@link #cast Polyglot.cast}.
+     */
     @SuppressWarnings("unused")
     public static Object eval(String language, String code) {
         return null;
     }
 
+    /**
+     * Imports {@code name} from global Polyglot scope. If {@code name} does not exist in the scope,
+     * returns {@code null}.
+     *
+     * The returned interop value is wrapped as {@link Object}. To access members of the underlying
+     * interop object, write a corresponding class or interface stub in Java and cast the eval
+     * result to it using {@link #cast Polyglot.cast}.
+     */
     @SuppressWarnings("unused")
     public static Object importObject(String name) {
         return null;
     }
 
+    /**
+     * Exports {@code value} under {@code name} to the Polyglot scope.
+     */
     @SuppressWarnings("unused")
     public static void exportObject(String name, Object value) {
     }
