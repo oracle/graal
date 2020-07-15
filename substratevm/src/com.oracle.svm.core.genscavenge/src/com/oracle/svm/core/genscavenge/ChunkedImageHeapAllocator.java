de0dbd5eb03391b6de584105d1f2568770c6754d
Peter Hofer <peter.hofer@oracle.com>
2020-07-15 17:05:36 +0200

    Never add a hash code field to FillerObject, which would make it larger than the object alignment.

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/ChunkedImageHeapAllocator.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/ChunkedImageHeapAllocator.java
@@ -33,6 +33,8 @@ import org.graalvm.nativeimage.Platforms;
 import org.graalvm.word.UnsignedWord;
 import org.graalvm.word.WordFactory;
 
+import com.oracle.svm.core.config.ConfigurationValues;
+import com.oracle.svm.core.config.ObjectLayout;
 import com.oracle.svm.core.image.ImageHeap;
 import com.oracle.svm.core.image.ImageHeapObject;
 import com.oracle.svm.core.image.ImageHeapPartition;
@@ -169,6 +171,9 @@ class ChunkedImageHeapAllocator {
     private AlignedChunk currentAlignedChunk;
 
     ChunkedImageHeapAllocator(ImageHeap imageHeap, long position) {
+        ObjectLayout layout = ConfigurationValues.getObjectLayout();
+        assert layout.getMinimumObjectSize() == layout.getAlignment() : "Must be able to fill any gap";
+
         this.imageHeap = imageHeap;
         this.alignedChunkSize = UnsignedUtils.safeToInt(HeapPolicy.getAlignedHeapChunkSize());
         this.alignedChunkAlignment = UnsignedUtils.safeToInt(HeapPolicy.getAlignedHeapChunkAlignment());
