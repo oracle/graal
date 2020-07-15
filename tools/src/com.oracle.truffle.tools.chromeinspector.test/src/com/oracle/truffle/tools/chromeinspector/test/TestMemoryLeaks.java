e5807bf7d0e9399f738807db57f1ca6d9abc7746
Martin Entlicher <martin.entlicher@oracle.com>
2020-07-15 15:11:46 +0200

    Prevent from memory and thread leaks in Inspector and improve tests (GR-24906).

--- tools/src/com.oracle.truffle.tools.chromeinspector.test/src/com/oracle/truffle/tools/chromeinspector/test/TestMemoryLeaks.java
+++ tools/src/com.oracle.truffle.tools.chromeinspector.test/src/com/oracle/truffle/tools/chromeinspector/test/TestMemoryLeaks.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -125,8 +125,10 @@ public class TestMemoryLeaks {
         assertTrue(tester.compareReceivedMessages("" +
                         "{\"result\":{},\"id\":30}\n" +
                         "{\"method\":\"Debugger.resumed\"}\n"));
-        tester.finish();
+        tester.finishNoGC();
         assertEmptyRemoteObjectsMaps(remoteObjectsHandler);
+        remoteObjectsHandler = null;
+        tester.finish();
     }
 
     private static void assertEmptyRemoteObjectsMaps(RemoteObjectsHandler remoteObjectsHandler) {
