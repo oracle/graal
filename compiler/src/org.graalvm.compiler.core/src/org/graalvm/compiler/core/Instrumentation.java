2c3c8aba153c0948878a529681dd8f1e384e5e77
Charles Parzy Turlat <charles.parzy.turlat@oracle.com>
2020-07-18 13:55:57 +0200

    Move Instrumentation out of HotSpot.

--- compiler/src/org.graalvm.compiler.core/src/org/graalvm/compiler/core/Instrumentation.java
+++ compiler/src/org.graalvm.compiler.core/src/org/graalvm/compiler/core/Instrumentation.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package org.graalvm.compiler.hotspot;
+package org.graalvm.compiler.core;
 
 /**
  * Interface in charge of holding the instrumentation data structures.
