70c16f859064c695b265f6eac882fa6ba7f1cccf
Gergö Barany <gergo.barany@oracle.com>
2020-07-22 17:08:17 +0200

    Remove write barrier addition post-processing hook

--- compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/WriteBarrierAdditionPhase.java
+++ compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/WriteBarrierAdditionPhase.java
@@ -25,10 +25,8 @@
 package org.graalvm.compiler.phases.common;
 
 import org.graalvm.compiler.debug.DebugCloseable;
-import org.graalvm.compiler.graph.Node;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.gc.BarrierSet;
-import org.graalvm.compiler.nodes.gc.NeedsWriteBarrierPostProcessing;
 import org.graalvm.compiler.nodes.memory.FixedAccessNode;
 import org.graalvm.compiler.nodes.spi.CoreProviders;
 import org.graalvm.compiler.phases.BasePhase;
@@ -41,12 +39,6 @@ public class WriteBarrierAdditionPhase extends BasePhase<CoreProviders> {
         for (FixedAccessNode n : graph.getNodes().filter(FixedAccessNode.class)) {
             try (DebugCloseable scope = n.graph().withNodeSourcePosition(n)) {
                 barrierSet.addBarriers(n);
-            }
-        }
-
-        for (Node node : graph.getNodes().snapshot()) {
-            if (node instanceof NeedsWriteBarrierPostProcessing) {
-                ((NeedsWriteBarrierPostProcessing) node).postProcessWriteBarriers();
             }
         }
     }
