90fe2e0e36dda0b4f4528381ff2b6a64c3b57e8d
Paul Wögerer <paul.woegerer@oracle.com>
2020-07-27 14:43:45 +0200

    Remove superfluous locking for loadClass.invoke

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageSystemClassLoader.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageSystemClassLoader.java
@@ -110,8 +110,6 @@ public final class NativeImageSystemClassLoader extends SecureClassLoader {
      */
     private static final Method loadClass = ReflectionUtil.lookupMethod(ClassLoader.class, "loadClass",
                     String.class, boolean.class);
-    private static final Method getClassLoadingLock = ReflectionUtil.lookupMethod(ClassLoader.class, "getClassLoadingLock",
-                    String.class);
     private static final Method findResource = ReflectionUtil.lookupMethod(ClassLoader.class, "findResource",
                     String.class);
 
@@ -121,11 +119,8 @@ public final class NativeImageSystemClassLoader extends SecureClassLoader {
     static Class<?> loadClass(ClassLoader classLoader, String name, boolean resolve) throws ClassNotFoundException {
         Class<?> loadedClass = null;
         try {
-            final Object lock = getClassLoadingLock.invoke(classLoader, name);
-            synchronized (lock) {
-                // invoke the "loadClass" method on the current class loader
-                loadedClass = ((Class<?>) loadClass.invoke(classLoader, name, resolve));
-            }
+            /* invoke the "loadClass" method on the current class loader */
+            loadedClass = ((Class<?>) loadClass.invoke(classLoader, name, resolve));
         } catch (Exception e) {
             if (e.getCause() instanceof ClassNotFoundException) {
                 throw ((ClassNotFoundException) e.getCause());
