4da11e2763d7afdbe772a18de9961acb9dd9fdcf
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2020-07-29 15:59:06 +0000

    [GR-25198] Correctly handle foreign null `this` in non-strict functions.
    PullRequest: js/1621

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "105d85f3ebe2d0f670ca6dc5010fa84139668e97",
+                "version": "96fa84b285fcde4dea5a766f79fde291aefa4438",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "105d85f3ebe2d0f670ca6dc5010fa84139668e97",
+                "version": "96fa84b285fcde4dea5a766f79fde291aefa4438",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
