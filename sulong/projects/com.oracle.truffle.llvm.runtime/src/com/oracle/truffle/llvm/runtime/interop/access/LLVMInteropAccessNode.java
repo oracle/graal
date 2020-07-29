faf4ae45e7398a905727513250339611b9a46fbd
Christoph Pichler <christoph.pichler@jku.at>
2020-07-29 14:48:01 +0200

    remove unnecessary method call

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropAccessNode.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/interop/access/LLVMInteropAccessNode.java
@@ -96,7 +96,6 @@ abstract class LLVMInteropAccessNode extends LLVMNode {
                         return doClazz(classType.getSuperclass(), foreign, offset, makeAccessLocation2);
                     }
                 }
-                return doClazz(classType.getSuperclass(), foreign, offset, makeAccessLocation);
             }
             // no other working solution found
             throw e;
