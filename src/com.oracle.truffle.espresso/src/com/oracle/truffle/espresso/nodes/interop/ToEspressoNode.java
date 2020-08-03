642eeb11e41dbfc3de2288423d223396102f41cb
Katja Goltsova <katja.goltsova@oracle.com>
2020-08-03 13:23:33 +0200

    Cast to array in ToEspressoNode in the same way as in Polyglot.cast

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/interop/ToEspressoNode.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/interop/ToEspressoNode.java
@@ -230,7 +230,22 @@ public abstract class ToEspressoNode extends Node {
     // TODO(goltsova): remove !isStringArray(klass) once array bytecodes support foreign arrays
     @Specialization(guards = {"!isStaticObject(value)", "!interop.isNull(value)", "!isStringArray(klass)"})
     Object doForeignArray(Object value, ArrayKlass klass,
-                    @SuppressWarnings("unused") @CachedLibrary(limit = "LIMIT") InteropLibrary interop) {
+                    @SuppressWarnings("unused") @CachedLibrary(limit = "LIMIT") InteropLibrary interop) throws UnsupportedTypeException {
+        /*
+         * Check that the component of the (possibly multi-dimensional) array is either of a
+         * primitive type or of a concrete class
+         */
+        Klass componentKlass = klass.getComponentType();
+        while (componentKlass.isArray()) {
+            componentKlass = ((ArrayKlass) componentKlass).getComponentType();
+        }
+        if (!componentKlass.isPrimitive() && !componentKlass.isConcrete()) {
+            throw UnsupportedTypeException.create(new Object[]{value}, "Casting to an array with elements of an abstract type is not allowed");
+        }
+
+        if (!interop.hasArrayElements(value)) {
+            throw UnsupportedTypeException.create(new Object[]{value}, "Cannot cast a non-array value to an array type");
+        }
         return StaticObject.createForeign(klass, value, interop);
     }
 
