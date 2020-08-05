faf2ccfc896ce992c7fa09d409f0cd7f5f74db04
Tomas Zezula <tomas.zezula@oracle.com>
2020-08-05 16:53:22 +0200

    [GR-25384] NPE in compiler when using Dgraal.Dump=Truffle:1.

--- compiler/src/org.graalvm.compiler.truffle.common/src/org/graalvm/compiler/truffle/common/TruffleOutputGroup.java
+++ compiler/src/org.graalvm.compiler.truffle.common/src/org/graalvm/compiler/truffle/common/TruffleOutputGroup.java
@@ -43,7 +43,7 @@ public final class TruffleOutputGroup implements Closeable {
 
     private final GraphOutput<Void, ?> output;
 
-    private TruffleOutputGroup(TruffleDebugContext debug, CompilableTruffleAST compilable, Map<Object, Object> properties) {
+    private TruffleOutputGroup(TruffleDebugContext debug, CompilableTruffleAST compilable, Map<Object, Object> properties) throws IOException {
         String name = "Truffle::" + compilable.getName();
         GraphOutput<Void, ?> out = null;
         try {
@@ -58,9 +58,13 @@ public final class TruffleOutputGroup implements Closeable {
             out.beginGroup(null, name, name, null, 0, effectiveProperties);
         } catch (Throwable e) {
             if (out != null) {
-                out.close();
-                out = null;
+                try {
+                    out.close();
+                } catch (Throwable closeException) {
+                    e.addSuppressed(closeException);
+                }
             }
+            throw e;
         }
         this.output = out;
     }
@@ -81,8 +85,9 @@ public final class TruffleOutputGroup implements Closeable {
      * @param compilable the compiled AST
      * @param properties additional group properties or {@code null}
      * @return the opened {@link TruffleOutputGroup}
+     * @throws IOException in case of IO error
      */
-    public static TruffleOutputGroup open(TruffleDebugContext debug, CompilableTruffleAST compilable, Map<Object, Object> properties) {
+    public static TruffleOutputGroup open(TruffleDebugContext debug, CompilableTruffleAST compilable, Map<Object, Object> properties) throws IOException {
         if (debug != null && debug.isDumpEnabled()) {
             return new TruffleOutputGroup(debug, compilable, properties);
         }
