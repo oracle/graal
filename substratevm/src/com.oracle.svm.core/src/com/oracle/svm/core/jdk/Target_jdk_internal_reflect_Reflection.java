4771ab4c550ccc078d8968289e744a3677a727d9
Christian Wimmer <christian.wimmer@oracle.com>
2020-08-10 10:37:38 -0700

    JDK11 StackWalker.getCallerFrame must ignore lambda frames

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_jdk_internal_reflect_Reflection.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_jdk_internal_reflect_Reflection.java
@@ -37,7 +37,7 @@ public final class Target_jdk_internal_reflect_Reflection {
     @Substitute
     @NeverInline("Starting a stack walk in the caller frame")
     private static Class<?> getCallerClass() {
-        return StackTraceUtils.getCallerClass(KnownIntrinsics.readCallerStackPointer());
+        return StackTraceUtils.getCallerClass(KnownIntrinsics.readCallerStackPointer(), true);
     }
 
     @Substitute
@@ -45,13 +45,13 @@ public final class Target_jdk_internal_reflect_Reflection {
     @NeverInline("Starting a stack walk in the caller frame")
     private static Class<?> getCallerClass(int depth) {
         if (depth == -1) { // means: behave same as getCallerClass()
-            StackTraceUtils.getCallerClass(KnownIntrinsics.readCallerStackPointer());
+            StackTraceUtils.getCallerClass(KnownIntrinsics.readCallerStackPointer(), true);
         } else if (depth < 0) {
             return null;
         } else if (depth == 0) {
             return Target_jdk_internal_reflect_Reflection.class;
         }
-        return StackTraceUtils.getCallerClass(KnownIntrinsics.readCallerStackPointer(), depth - 1, false);
+        return StackTraceUtils.getCallerClass(KnownIntrinsics.readCallerStackPointer(), true, depth - 1, false);
     }
 
     @Substitute
