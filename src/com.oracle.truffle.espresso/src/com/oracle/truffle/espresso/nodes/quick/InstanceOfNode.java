f10a426758eb757311963512cc923f69c0f3bdb7
thomas.garcia <thomas.garcia@oracle.com>
2020-08-18 17:25:37 +0200

    remove obsolete assert

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/quick/InstanceOfNode.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/quick/InstanceOfNode.java
@@ -58,7 +58,6 @@ public abstract class InstanceOfNode extends QuickNode {
 
     InstanceOfNode(char cpIndex, int top, int curBCI) {
         super(top, curBCI);
-        assert !typeToCheck.isPrimitive();
         this.cpIndex = cpIndex;
     }
 
