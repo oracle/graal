aaf7247bdbfcc691d1eb53f277cf1f9c78fcf3e6
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2020-08-18 15:23:13 +0200

    Next dev cycle

--- sdk/mx.sdk/suite.py
+++ sdk/mx.sdk/suite.py
@@ -41,8 +41,8 @@
 suite = {
   "mxversion" : "5.251.0",
   "name" : "sdk",
-  "version" : "20.2.0",
-  "release" : True,
+  "version" : "20.2.1",
+  "release" : False,
   "sourceinprojectwhitelist" : [],
   "url" : "https://github.com/oracle/graal",
   "groupId" : "org.graalvm.sdk",
