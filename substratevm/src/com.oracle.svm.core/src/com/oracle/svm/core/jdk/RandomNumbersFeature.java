326e60af90eb800c43f7822fbe9d1efc8a8206bb
Jaikiran Pai <jaikiran.pai@gmail.com>
2020-08-25 09:29:59 +0530

    Mark RandomNumbersFeature as an @AutomaticFeature

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/RandomNumbersFeature.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/RandomNumbersFeature.java
@@ -26,10 +26,12 @@ package com.oracle.svm.core.jdk;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import com.oracle.svm.core.annotate.AutomaticFeature;
 import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.impl.RuntimeClassInitializationSupport;
 
+@AutomaticFeature
 public class RandomNumbersFeature implements Feature {
 
     @Override
