476cd45f626d760a8c5f36119e0fd7272f6b5caa
Katja Goltsova <katja.goltsova@oracle.com>
2020-09-03 13:53:57 +0200

    Throw shouldNotReachHere on a cast to void, because a cast to void cannot originate from the user's code

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/interop/ToEspressoNode.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/interop/ToEspressoNode.java
@@ -22,6 +22,7 @@
  */
 package com.oracle.truffle.espresso.nodes.interop;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.CachedContext;
@@ -102,6 +103,9 @@ public abstract class ToEspressoNode extends Node {
                         return interop.asDouble(value);
                     }
                     break;
+                case Void:
+                    CompilerDirectives.transferToInterpreter();
+                    throw EspressoError.shouldNotReachHere("Unexpected cast to void");
             }
         } catch (UnsupportedMessageException e) {
             exceptionProfile.enter();
