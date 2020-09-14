0040b87e60f4a30f74041d2292222130758656d8
Allan Gregersen <allan.gregersen@oracle.com>
2020-09-14 13:21:59 +0200

    on connection close exceptions, make sure thread is interrupted to tear down gracefully

--- src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/SocketConnection.java
+++ src/com.oracle.truffle.espresso.jdwp/src/com/oracle/truffle/espresso/jdwp/impl/SocketConnection.java
@@ -81,9 +81,7 @@ public final class SocketConnection implements Runnable {
             } catch (IOException ex) {
                 throw new RuntimeException("Failed sending packet to debugger instance", ex);
             } catch (ConnectionClosedException e) {
-                if (!Thread.currentThread().isInterrupted()) {
-                    throw new RuntimeException("connection closed for an unknown reason", e.getCause());
-                }
+               Thread.currentThread().interrupt();
             }
         }
     }
