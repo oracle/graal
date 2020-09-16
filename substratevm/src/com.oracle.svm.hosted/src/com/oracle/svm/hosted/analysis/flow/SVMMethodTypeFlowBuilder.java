dfb5de2967a08cddafebfeb0418f2777518c8b1e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2020-09-16 16:02:53 +0200

    Restrict ThrowUnsafeOffsetErrors check to static fields only

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/flow/SVMMethodTypeFlowBuilder.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/flow/SVMMethodTypeFlowBuilder.java
@@ -120,15 +120,16 @@ public class SVMMethodTypeFlowBuilder extends MethodTypeFlowBuilder {
         }
 
         /*
-         * Offset fields used in unsafe operations need value recomputation. Check that they are
-         * properly intercepted. Detection of offset fields that need value re-computation is best
-         * effort. For some node types (e.g., SignExtendNode, LoadFieldNode, InvokeNode, AddNode,
-         * ParameterNode, AndNode, ValueProxyNode, LoadIndexedNode) that can be used as an offset in
-         * an unsafe operation we cannot determine if the value was properly intercepted or not by
-         * simply looking at the node itself.
+         * Offset static fields that are initialized at build time and used in unsafe operations
+         * need value recomputation. Check that they are properly intercepted. Detection of offset
+         * fields that need value re-computation is best effort. For some node types (e.g.,
+         * SignExtendNode, LoadFieldNode, InvokeNode, AddNode, ParameterNode, AndNode,
+         * ValueProxyNode, LoadIndexedNode) that can be used as an offset in an unsafe operation we
+         * cannot determine if the value was properly intercepted or not by simply looking at the
+         * node itself.
          *
          * Determining if an offset that comes from a ConstantNode was properly intercepted is not
-         * reliable . First the canonicalization in UnsafeAccessNode tries to replace the unsafe
+         * reliable. First the canonicalization in UnsafeAccessNode tries to replace the unsafe
          * access with a field access when the offset is a constant. Thus, we don't actually see the
          * unsafe access node. Second if the field is intercepted but RecomputeFieldValue.isFinal is
          * set to true then the recomputed value is constant folded, i.e., there is no load of the
@@ -143,8 +144,9 @@ public class SVMMethodTypeFlowBuilder extends MethodTypeFlowBuilder {
         if (offsetNode instanceof LoadFieldNode) {
             LoadFieldNode offsetLoadNode = (LoadFieldNode) offsetNode;
             AnalysisField field = (AnalysisField) offsetLoadNode.field();
-            if (!field.getDeclaringClass().unsafeFieldsRecomputed() &&
+            if (field.isStatic() &&
                             !getHostVM().getClassInitializationSupport().shouldInitializeAtRuntime(field.getDeclaringClass()) &&
+                            !field.getDeclaringClass().unsafeFieldsRecomputed() &&
                             !(field.wrapped instanceof ComputedValueField) &&
                             !(base.isConstant() && base.asConstant().isDefaultForKind())) {
                 String message = String.format("Field %s is used as an offset in an unsafe operation, but no value recomputation found.%n Wrapped field: %s", field, field.wrapped);
