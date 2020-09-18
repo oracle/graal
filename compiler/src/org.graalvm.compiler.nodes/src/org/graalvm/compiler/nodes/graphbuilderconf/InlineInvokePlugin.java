9234888966b063e7f4d6ee5b7b601f73d6b61937
Maxime Kjaer <maxime.kjaer@oracle.com>
2020-09-18 17:03:33 +0200

    Improve documentation of intrinsic inlining
    The previous Javadoc comment was a run-on sentence which was difficult
    to understand, and which contained a typo.

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/graphbuilderconf/InlineInvokePlugin.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/graphbuilderconf/InlineInvokePlugin.java
@@ -99,10 +99,11 @@ public interface InlineInvokePlugin extends GraphBuilderPlugin {
         }
 
         /**
-         * Gets the provider of bytecode to be parsed for {@link #getMethodToInline()} if is is an
-         * intrinsic for the original method (i.e., the {@code method} passed to
-         * {@link InlineInvokePlugin#shouldInlineInvoke}). A {@code null} return value indicates
-         * that this is not an intrinsic inlining.
+         * Gets a provider for the bytecode of the intrinsic to use when inlining the method
+         * returned by {@link #getMethodToInline()}, if this inlining is an intrinsic inlining.
+         *
+         * @return A non-null {@link BytecodeProvider} if the method to inline should be
+         *         intrinsified, or {@code null} if this is not an intrinsic inlining.
          */
         public BytecodeProvider getIntrinsicBytecodeProvider() {
             return intrinsicBytecodeProvider;
