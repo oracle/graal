0ef7859093cbbeeac9fef5e1f86a4275c1c9217d
Josef Haider <josef.haider@oracle.com>
2020-09-23 12:25:26 +0200

    TRegex: add explicit sanity check for fromIndex argument

--- regex/src/com.oracle.truffle.regex.test/src/com/oracle/truffle/regex/tregex/test/PythonTests.java
+++ regex/src/com.oracle.truffle.regex.test/src/com/oracle/truffle/regex/tregex/test/PythonTests.java
@@ -40,6 +40,8 @@
  */
 package com.oracle.truffle.regex.tregex.test;
 
+import org.graalvm.polyglot.PolyglotException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PythonTests extends RegexTestBase {
@@ -68,5 +70,16 @@ public class PythonTests extends RegexTestBase {
     @Test
     public void gr23871() {
         test("[^ ]+?(?:-(?:(?<=[a-z]{2}-)|(?<=[a-z]-[a-z]-)))", "su", "this-is-a-useful-feature", 8, true, 8, 10);
+    }
+
+    @Test
+    public void gr26246() {
+        try {
+            test(".*", "", "abc", 4, false);
+        } catch (PolyglotException e) {
+            Assert.assertTrue(e.getMessage().contains("illegal fromIndex"));
+            return;
+        }
+        Assert.fail();
     }
 }
