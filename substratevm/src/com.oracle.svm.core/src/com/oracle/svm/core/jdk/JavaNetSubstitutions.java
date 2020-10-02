a2d01dffe1a7acfc5a3a25c0d0b262a625097f20
Jaikiran Pai <jaikiran.pai@gmail.com>
2020-10-02 21:48:51 +0530

    Substitute java.net.URL.setURLStreamHandlerFactory to prevent the deleted "handlers" field from being reachable

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaNetSubstitutions.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaNetSubstitutions.java
@@ -33,6 +33,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -68,6 +69,12 @@ final class Target_java_net_URL {
          * available, and how to add a protocol at image build time.
          */
         return JavaNetSubstitutions.getURLStreamHandler(protocol);
+    }
+
+    @SuppressWarnings("unused")
+    @Substitute
+    public static void setURLStreamHandlerFactory(URLStreamHandlerFactory fac) {
+        throw VMError.unsupportedFeature("java.net.URL.setURLStreamHandlerFactory(URLStreamHandlerFactory)");
     }
 }
 
