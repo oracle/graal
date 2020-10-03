16e76527e1c86fcbe519701fa428b38dd9407090
Andreas Woess <andreas.woess@oracle.com>
2020-10-03 00:33:06 +0000

    [GR-26394] Adopt NodeLibrary.
    PullRequest: js/1697

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "70574861e3a6bd9c76714155b3f2db6d926ec237",
+                "version": "94870977f3066234e0ab312cbffed52e9bf4445c",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "70574861e3a6bd9c76714155b3f2db6d926ec237",
+                "version": "94870977f3066234e0ab312cbffed52e9bf4445c",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
