76da75acc28f4b18036ce2120ad63d46160a6b85
Aleksandar Pejović <aleksandar.pejovic@oracle.com>
2020-10-05 12:55:01 +0200

    Fix Class.desiredAssertionStatus intrinsic

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/snippets/SubstrateGraphBuilderPlugins.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/snippets/SubstrateGraphBuilderPlugins.java
@@ -922,7 +922,7 @@ public class SubstrateGraphBuilderPlugins {
                     } else {
                         throw VMError.shouldNotReachHere("Unexpected class object: " + clazzOrHub);
                     }
-                    b.addPush(JavaKind.Boolean, ConstantNode.forBoolean(!desiredAssertionStatus));
+                    b.addPush(JavaKind.Boolean, ConstantNode.forBoolean(desiredAssertionStatus));
                     return true;
                 }
                 return false;
