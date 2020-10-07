f76137d361b5730dc1feec13755d84bc835a8372
Allan Gregersen <allan.gregersen@oracle.com>
2020-10-07 12:38:39 +0200

    initialize sub types array list on demand and keep the backing array trimmed down

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
@@ -121,7 +121,7 @@ public final class ObjectKlass extends Klass {
     @CompilationFinal volatile RedefinitionCache redefineCache;
 
     // used for class redefintion when refreshing vtables etc.
-    private final ArrayList<WeakReference<ObjectKlass>> subTypes = new ArrayList<>(8);
+    private ArrayList<WeakReference<ObjectKlass>> subTypes;
 
     public static final int LOADED = 0;
     public static final int LINKED = 1;
@@ -208,6 +208,9 @@ public final class ObjectKlass extends Klass {
     }
 
     private void addSubType(ObjectKlass objectKlass) {
+        if (subTypes == null) {
+            subTypes = new ArrayList<>(1);
+        }
         subTypes.add(new WeakReference<>(objectKlass));
     }
 
@@ -1209,11 +1212,13 @@ public final class ObjectKlass extends Klass {
 
     private List<ObjectKlass> getSubTypes() {
         List<ObjectKlass> result = new ArrayList<>();
-        for (WeakReference<ObjectKlass> subType : subTypes) {
-            ObjectKlass sub = subType.get();
-            if (sub != null) {
-                result.add(sub);
-                result.addAll(sub.getSubTypes());
+        if (subTypes != null) {
+            for (WeakReference<ObjectKlass> subType : subTypes) {
+                ObjectKlass sub = subType.get();
+                if (sub != null) {
+                    result.add(sub);
+                    result.addAll(sub.getSubTypes());
+                }
             }
         }
         return result;
