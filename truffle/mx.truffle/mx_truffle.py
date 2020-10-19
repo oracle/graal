e49cd903f30f855511fbc0b8e7a4e0d9ddd27f08
Roland Schatz <roland.schatz@oracle.com>
2020-10-19 15:45:56 +0200

    Disable deprecated libffi java_raw_api, misc build fixes.

--- truffle/mx.truffle/mx_truffle.py
+++ truffle/mx.truffle/mx_truffle.py
@@ -643,7 +643,7 @@ class LibffiBuilderProject(mx.AbstractNativeProject, mx_native.NativeDependency)
                                                            mx.join(self.out_dir, 'libffi-3.3'),
                                                            'static_lib',
                                                            deliverable='ffi',
-                                                           cflags=['-MD', '-O2'])
+                                                           cflags=['-MD', '-O2', '-DFFI_BUILDING_DLL'])
             self.delegate._source = dict(tree=['include',
                                                'src',
                                                mx.join('src', 'x86')],
@@ -652,12 +652,11 @@ class LibffiBuilderProject(mx.AbstractNativeProject, mx_native.NativeDependency)
                                                        mx.join('src', 'fficonfig.h'),
                                                        mx.join('src', 'ffi_common.h')],
                                                 '.c': [mx.join('src', 'closures.c'),
-                                                       mx.join('src', 'java_raw_api.c'),
                                                        mx.join('src', 'prep_cif.c'),
                                                        mx.join('src', 'raw_api.c'),
                                                        mx.join('src', 'types.c'),
-                                                       mx.join('src', 'x86', 'ffi.c')],
-                                                '.S': [mx.join('src', 'x86', 'win64.S')]})
+                                                       mx.join('src', 'x86', 'ffiw64.c')],
+                                                '.S': [mx.join('src', 'x86', 'win64_intel.S')]})
         else:
             class LibtoolNativeProject(mx.NativeProject,  # pylint: disable=too-many-ancestors
                                        mx_native.NativeDependency):
@@ -688,6 +687,7 @@ class LibffiBuilderProject(mx.AbstractNativeProject, mx_native.NativeDependency)
                     'CFLAGS="{}"'.format(' '.join(
                         ['-g', '-O3'] + (['-m64'] if mx.get_os() == 'solaris' else [])
                     )),
+                    'CPPFLAGS="-DNO_JAVA_RAW_API"',
                 ])
             )
 
