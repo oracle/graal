4e279698f81628d022c39628ae8a96e41ceb6c36
Paley Li <paley.li@oracle.com>
2020-10-20 19:05:53 +0200

    Sulong: do not cache the call target of the slow path

--- truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/impl/FunctionExecuteNode.java
+++ truffle/src/com.oracle.truffle.nfi/src/com/oracle/truffle/nfi/impl/FunctionExecuteNode.java
@@ -149,8 +149,9 @@ abstract class FunctionExecuteNode extends Node {
     @ExplodeLoop
     @Specialization(replaces = "cachedSignature", guards = "signature.getArgTypes().length == libs.length")
     protected Object cachedArgCount(NativePointer receiver, LibFFISignature signature, Object[] args,
+                    @CachedLanguage() NFILanguageImpl language,
                     @Cached("getGenericNativeArgumentLibraries(signature.getArgTypes().length)") NativeArgumentLibrary[] libs,
-                    @Cached("createSlowPathCall()") DirectCallNode slowPathCall,
+                    @Cached() IndirectCallNode slowPathCall,
                     @Cached BranchProfile exception) throws ArityException, UnsupportedTypeException {
         LibFFIType[] argTypes = signature.getArgTypes();
 
@@ -176,15 +177,10 @@ abstract class FunctionExecuteNode extends Node {
         }
 
         try {
-            return slowPathCall.call(receiver, signature, buffer);
+            return slowPathCall.call(language.getSlowPathCall(), receiver, signature, buffer);
         } finally {
             assert keepAlive(args);
         }
-    }
-
-    DirectCallNode createSlowPathCall() {
-        NFILanguageImpl language = lookupLanguageReference(NFILanguageImpl.class).get();
-        return DirectCallNode.create(language.getSlowPathCall());
     }
 
     private static void raiseArityException(LibFFIType[] argTypes, int actualArgCount) throws ArityException {
