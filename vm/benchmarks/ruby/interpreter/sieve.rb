c45a1b47bf36eed6e135f5d1722094bfaf0099c3
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2020-10-21 14:52:17 +0200

    Add ruby version of the sieve benchmark.

---
+++ vm/benchmarks/ruby/interpreter/sieve.rb
@@ -0,0 +1,27 @@
+
+def run()
+  number = 400000
+
+  primes = Array.new(number + 1)
+
+  for i in 2..number
+    primes[i] = i
+  end
+
+  i = 2
+  while i * i <= number do
+    if primes[i] != 0 then
+      for j in 2..(number - 1)
+        if primes[i] * j > number then
+          break
+        else 
+          primes[primes[i] * j] = 0
+        end
+      end
+    end
+    i += 1
+  end
+
+  return primes[number]
+end
+
