7caf3ecf786a4d240a6cce4ad554742ac87ecf85
Serhii Ivanenko <serhii.ivanenko@oracle.com>
2020-10-24 21:16:57 +0000

    [GR-25075] JSToPrimitiveNode.doTruffleJavaObject should not forward to interop.toDisplayString.
    PullRequest: js/1617

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "94870977f3066234e0ab312cbffed52e9bf4445c",
+                "version": "2e2b05980c061cbb0546e6d1f245f561f9e971c4",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "94870977f3066234e0ab312cbffed52e9bf4445c",
+                "version": "2e2b05980c061cbb0546e6d1f245f561f9e971c4",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
