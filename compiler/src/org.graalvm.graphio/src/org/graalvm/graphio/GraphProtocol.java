8c5c4438d944a8c7b0548a1694477f93a32fbbbf
Ondřej Douda <ondrej.douda@oracle.com>
2020-11-18 15:21:36 +0100

    [GR-27484] Added tests for Int properties.

--- compiler/src/org.graalvm.graphio/src/org/graalvm/graphio/GraphProtocol.java
+++ compiler/src/org.graalvm.graphio/src/org/graalvm/graphio/GraphProtocol.java
@@ -853,8 +853,9 @@ abstract class GraphProtocol<Graph, Node, NodeClass, Edges, Block, ResolvedJavaM
             if (versionMajor > 7) {
                 writeShort(Character.MAX_VALUE);
                 writeInt(size);
-            } else
-                throw new IOException("Property count is too big.");
+            } else {
+                throw new IllegalArgumentException("Property count is too big. Properties can contain only " + (Character.MAX_VALUE - 1) + " in version < 8.");
+            }
         } else {
             writeShort((char) size);
         }
