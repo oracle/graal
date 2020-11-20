e97ad6237638246ff555657ddf48976a03a53d2e
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2020-11-20 14:02:54 +0100

    Next dev cycle

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -1,8 +1,8 @@
 suite = {
     "name": "vm",
-    "version" : "20.3.0",
+    "version" : "20.3.1",
     "mxversion" : "5.256.0",
-    "release" : True,
+    "release" : False,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "b53c4e144b5909876b377e31be69e3b3f381ae0b",
+                "version": "a266890b0d4e3bc5a235090de529e901365b9552",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "b53c4e144b5909876b377e31be69e3b3f381ae0b",
+                "version": "a266890b0d4e3bc5a235090de529e901365b9552",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -75,7 +75,7 @@ suite = {
             },
             {
                 "name": "graalpython",
-                "version": "304a73fdb30be1137a0cb5a865d101d422bec2d6",
+                "version": "da45477c8e3edd1094fe017452947fc6413bbea2",
                 "dynamic": True,
                 "urls": [
                     {"url": "https://github.com/graalvm/graalpython.git", "kind": "git"},
--- wasm/mx.wasm/suite.py
+++ wasm/mx.wasm/suite.py
@@ -42,7 +42,8 @@ suite = {
   "mxversion" : "5.249.5",
   "name" : "wasm",
   "groupId" : "org.graalvm.wasm",
-  "version" : "20.3.0",
+  "version" : "20.3.1",
+  "release": False,
   "versionConflictResolution" : "latest",
   "url" : "http://graalvm.org/",
   "developer" : {
