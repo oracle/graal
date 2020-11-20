967b8a5bbb4eb112134465679d6a7d6878c025f6
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2020-11-20 18:04:38 +0100

    Improve JavaDoc of HostCompilerDirectives.

--- truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/HostCompilerDirectives.java
+++ truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/HostCompilerDirectives.java
@@ -46,16 +46,22 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Directives that influence the optimizations of the host compiler.
- * These operations affect how the Truffle interpreter is itself compiled.
+ * Directives that influence the optimizations of the host compiler. These operations affect how the
+ * Truffle interpreter is itself compiled.
  *
  * @since 21.0
  */
 public final class HostCompilerDirectives {
 
     /**
-     * Marks a method that is an implementation of a Truffle interpreter,
-     * and which should receive additional optimization budget.
+     * Marks a method that is an implementation of a Truffle interpreter, and which should receive
+     * additional optimization budget.
+     *
+     * This annotation is used to annotate the root method of a bytecode interpreter, and it hints
+     * the compiler to invest extra effort into optimizing such methods. Language implementers are
+     * advised to inspect the IR of the interpreter when using this.
+     *
+     * @see BytecodeInterpreterSwitchBoundary to control the boundaries of inlining
      *
      * @since 21.0
      */
@@ -65,8 +71,14 @@ public final class HostCompilerDirectives {
     }
 
     /**
-     * Marks a method that is called from a Truffle interpreter,
-     * but is not called frequently and is not important for interpreter performance.
+     * Marks a method that is called from a Truffle interpreter, but is not called frequently and is
+     * not important for interpreter performance.
+     *
+     * This annotation is used to annotate methods that are called from a bytecode interpreter, but
+     * should generally not be inlined into the body of the bytecode interpreter. Language
+     * implementers are advised to inspect the IR of the interpreter when using this.
+     *
+     * @see BytecodeInterpreterSwitch to annotate the root method of a bytecode interpreter
      *
      * @since 21.0
      */
@@ -76,8 +88,10 @@ public final class HostCompilerDirectives {
     }
 
     /**
-     * Consume a value, making sure the compiler doesn't optimize away the computation of this
+     * Consume a value, making sure the compiler does not optimize away the computation of this
      * value, even if it is otherwise unused.
+     *
+     * @since 21.0
      */
     @SuppressWarnings("unused")
     public static void consume(int value) {
