9779192e471ed21414dcf5e35b5421731467d9ca
Christian Humer <christian.humer@oracle.com>
2020-11-24 22:04:32 +0100

    Cleanup wasm usages of ExportLibrary.

--- wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/api/TableDescriptor.java
+++ wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/api/TableDescriptor.java
@@ -40,10 +40,6 @@
  */
 package org.graalvm.wasm.api;
 
-import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.library.ExportLibrary;
-
-@ExportLibrary(InteropLibrary.class)
 public class TableDescriptor extends Dictionary {
     private final TableKind kind;
     private final Integer initial;
