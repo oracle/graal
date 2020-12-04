7e0351201373631700e78c1e4a391f95a3458d26
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2020-12-04 13:40:22 +0100

    Refactor EspressoBindings. Be explicit about side effects when reading members. Propagate exceptions thrown during class loading.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoBindings.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoBindings.java
@@ -2,8 +2,11 @@ package com.oracle.truffle.espresso;
 
 import javax.lang.model.SourceVersion;
 
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.CachedContext;
 import com.oracle.truffle.api.interop.ArityException;
 import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.InvalidArrayIndexException;
@@ -16,8 +19,10 @@ import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
 import com.oracle.truffle.api.profiles.BranchProfile;
 import com.oracle.truffle.espresso.meta.EspressoError;
+import com.oracle.truffle.espresso.meta.Meta;
 import com.oracle.truffle.espresso.runtime.EspressoException;
 import com.oracle.truffle.espresso.runtime.StaticObject;
+import com.oracle.truffle.espresso.vm.InterpreterToVM;
 
 @ExportLibrary(InteropLibrary.class)
 public final class EspressoBindings implements TruffleObject {
@@ -26,36 +31,6 @@ public final class EspressoBindings implements TruffleObject {
 
     public EspressoBindings(StaticObject loader) {
         this.loader = loader;
-    }
-
-    @ExportLibrary(InteropLibrary.class)
-    static final class EmptyKeysArray implements TruffleObject {
-
-        static final TruffleObject INSTANCE = new EmptyKeysArray();
-
-        @ExportMessage
-        @SuppressWarnings("static-method")
-        boolean hasArrayElements() {
-            return true;
-        }
-
-        @ExportMessage
-        @SuppressWarnings("static-method")
-        long getArraySize() {
-            return 0;
-        }
-
-        @ExportMessage
-        @SuppressWarnings("static-method")
-        boolean isArrayElementReadable(@SuppressWarnings("unused") long index) {
-            return false;
-        }
-
-        @ExportMessage
-        @SuppressWarnings("static-method")
-        Object readArrayElement(long index) throws InvalidArrayIndexException {
-            throw InvalidArrayIndexException.create(index);
-        }
     }
 
     @ExportMessage
@@ -71,7 +46,9 @@ public final class EspressoBindings implements TruffleObject {
     }
 
     @ExportMessage
+    @ExportMessage(name = "hasMemberReadSideEffects")
     @SuppressWarnings("static-method")
+    @TruffleBoundary
     boolean isMemberReadable(String member) {
         return SourceVersion.isName(member);
     }
@@ -79,16 +56,22 @@ public final class EspressoBindings implements TruffleObject {
     @ExportMessage
     Object readMember(String member, @CachedLibrary("this.loader") InteropLibrary interop, @Cached BranchProfile error) throws UnsupportedMessageException, UnknownIdentifierException {
         if (!isMemberReadable(member)) {
+            error.enter();
             throw UnknownIdentifierException.create(member);
         }
         try {
             StaticObject clazz = (StaticObject) interop.invokeMember(loader, "loadClass:(Ljava/lang/String;)Ljava/lang/Class;", member);
             return clazz.getMirrorKlass();
-        } catch (ArityException | UnsupportedTypeException e) {
-            throw EspressoError.shouldNotReachHere(e);
         } catch (EspressoException e) {
             error.enter();
-            throw UnknownIdentifierException.create(member, e);
+            Meta meta = loader.getKlass().getMeta();
+            if (InterpreterToVM.instanceOf(e.getExceptionObject(), meta.java_lang_ClassNotFoundException)) {
+                throw UnknownIdentifierException.create(member, e);
+            }
+            throw e; // exception during class loading
+        } catch (ArityException | UnsupportedTypeException e) {
+            CompilerDirectives.transferToInterpreter();
+            throw EspressoError.shouldNotReachHere(e);
         }
     }
 
@@ -96,12 +79,6 @@ public final class EspressoBindings implements TruffleObject {
     @SuppressWarnings("static-method")
     boolean isScope() {
         return true;
-    }
-
-    @ExportMessage
-    @SuppressWarnings("static-method")
-    Object toDisplayString(@SuppressWarnings("unused") boolean allowSideEffects) {
-        return "espresso-system-classloader";
     }
 
     @ExportMessage
@@ -114,5 +91,41 @@ public final class EspressoBindings implements TruffleObject {
     @SuppressWarnings("static-method")
     Class<? extends TruffleLanguage<?>> getLanguage() {
         return EspressoLanguage.class;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    Object toDisplayString(@SuppressWarnings("unused") boolean allowSideEffects) {
+        return "espresso-system-classloader";
+    }
+}
+
+@ExportLibrary(InteropLibrary.class)
+final class EmptyKeysArray implements TruffleObject {
+
+    static final TruffleObject INSTANCE = new EmptyKeysArray();
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    boolean hasArrayElements() {
+        return true;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    long getArraySize() {
+        return 0;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    boolean isArrayElementReadable(@SuppressWarnings("unused") long index) {
+        return false;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    Object readArrayElement(long index) throws InvalidArrayIndexException {
+        throw InvalidArrayIndexException.create(index);
     }
 }
