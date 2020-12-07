43d28e1b2161d278585e9fe4c5c03f249bb835a8
Christoph Pichler <christoph.pichler@jku.at>
2020-12-07 09:56:58 +0100

    add internal virtual method resolving tests

--- sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/CxxVTableTest3.java
+++ sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/CxxVTableTest3.java
@@ -35,17 +35,38 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.oracle.truffle.api.CallTarget;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.interop.InteropException;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.library.CachedLibrary;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.nodes.RootNode;
+import com.oracle.truffle.llvm.tests.interop.CxxVTableTest3Factory.CallNodeGen;
 import com.oracle.truffle.tck.TruffleRunner;
+import com.oracle.truffle.tck.TruffleRunner.Inject;
 
 @RunWith(TruffleRunner.class)
 public class CxxVTableTest3 extends InteropTestBase {
 
     private static Value testCppLibrary;
+    private static Object testCppLibraryInternal;
+
+    private static Object[] args;
+    private static String methodName;
 
     private static Value getA1;
+    private static Object getA1Internal;
     private static Value getA2;
+    private static Object getA2Internal;
     private static Value getA3;
+    private static Object getA3Internal;
     private static Value getA4;
+    private static Object getA4Internal;
 
     private static Value consA1;
     private static Value consA2;
@@ -56,6 +77,7 @@ public class CxxVTableTest3 extends InteropTestBase {
     @BeforeClass
     public static void loadTestBitcode() {
         testCppLibrary = loadTestBitcodeValue("vtableTest3.cpp");
+        testCppLibraryInternal = loadTestBitcodeInternal("vtableTest3.cpp");
         getA1 = testCppLibrary.getMember("getA1");
         getA2 = testCppLibrary.getMember("getA2");
         getA3 = testCppLibrary.getMember("getA3");
@@ -65,6 +87,63 @@ public class CxxVTableTest3 extends InteropTestBase {
         consA3 = testCppLibrary.getMember("A3");
         consA4 = testCppLibrary.getMember("A4");
         consImpl = testCppLibrary.getMember("Impl");
+
+        try {
+            getA1Internal = InteropLibrary.getUncached().readMember(testCppLibraryInternal, "getA1");
+            getA2Internal = InteropLibrary.getUncached().readMember(testCppLibraryInternal, "getA2");
+            getA3Internal = InteropLibrary.getUncached().readMember(testCppLibraryInternal, "getA3");
+            getA4Internal = InteropLibrary.getUncached().readMember(testCppLibraryInternal, "getA4");
+        } catch (UnsupportedMessageException | UnknownIdentifierException e) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            throw new IllegalStateException(e);
+        }
+    }
+
+    abstract static class CallNode extends Node {
+        abstract Object execute(Object a);
+
+        @Specialization(limit = "3")
+        Object doFoo1(Object a,
+                        @CachedLibrary("a") InteropLibrary interop) {
+            try {
+                return interop.invokeMember(a, methodName, args);
+            } catch (InteropException ex) {
+                CompilerDirectives.transferToInterpreterAndInvalidate();
+                throw new IllegalStateException(ex);
+            }
+        }
+    }
+
+    public static class TestVirtualCallNode extends RootNode {
+        @Child CallNode call = CallNodeGen.create();
+
+        public TestVirtualCallNode() {
+            super(null);
+        }
+
+        @Override
+        public Object execute(VirtualFrame frame) {
+            return call.execute(frame.getArguments()[0]);
+        }
+    }
+
+    @Test
+    public void testA2Internal(@Inject(TestVirtualCallNode.class) CallTarget call) throws InteropException {
+
+        args = new Object[0];
+        methodName = "a2";
+        Object a2 = InteropLibrary.getUncached().execute(getA2Internal);
+        Object ret = call.call(a2);
+        Assert.assertEquals(12, InteropLibrary.getUncached().asInt(ret));
+    }
+
+    @Test
+    public void testA4Internal(@Inject(TestVirtualCallNode.class) CallTarget call) throws InteropException {
+        methodName = "a4";
+        args = new Object[]{14};
+        Object a4 = InteropLibrary.getUncached().execute(getA4Internal);
+        Object ret = call.call(a4);
+        Assert.assertEquals(28, InteropLibrary.getUncached().asInt(ret));
     }
 
     @Test
