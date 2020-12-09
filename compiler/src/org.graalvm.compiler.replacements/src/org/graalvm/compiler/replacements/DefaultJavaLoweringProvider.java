08060d422f1a7733e96b0e90842ef56528393e27
David Leopoldseder <david.leopoldseder@oracle.com>
2020-12-09 11:44:22 +0100

    box lowering: canonicalize box nodes before lowering

--- compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/DefaultJavaLoweringProvider.java
+++ compiler/src/org.graalvm.compiler.replacements/src/org/graalvm/compiler/replacements/DefaultJavaLoweringProvider.java
@@ -44,7 +44,6 @@ import java.util.List;
 
 import org.graalvm.compiler.api.directives.GraalDirectives;
 import org.graalvm.compiler.api.replacements.SnippetReflectionProvider;
-import org.graalvm.compiler.core.common.GraalOptions;
 import org.graalvm.compiler.core.common.LIRKind;
 import org.graalvm.compiler.core.common.spi.ForeignCallsProvider;
 import org.graalvm.compiler.core.common.spi.MetaAccessExtensionProvider;
@@ -78,7 +77,6 @@ import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.ValuePhiNode;
 import org.graalvm.compiler.nodes.calc.AddNode;
 import org.graalvm.compiler.nodes.calc.ConditionalNode;
-import org.graalvm.compiler.nodes.calc.FloatingNode;
 import org.graalvm.compiler.nodes.calc.IntegerBelowNode;
 import org.graalvm.compiler.nodes.calc.IntegerConvertNode;
 import org.graalvm.compiler.nodes.calc.IntegerEqualsNode;
@@ -166,7 +164,6 @@ import org.graalvm.word.LocationIdentity;
 import jdk.vm.ci.code.CodeUtil;
 import jdk.vm.ci.code.MemoryBarriers;
 import jdk.vm.ci.code.TargetDescription;
-import jdk.vm.ci.meta.ConstantReflectionProvider;
 import jdk.vm.ci.meta.DeoptimizationAction;
 import jdk.vm.ci.meta.DeoptimizationReason;
 import jdk.vm.ci.meta.JavaConstant;
@@ -239,7 +236,6 @@ public abstract class DefaultJavaLoweringProvider implements LoweringProvider {
     @SuppressWarnings("try")
     public void lower(Node n, LoweringTool tool) {
         assert n instanceof Lowerable;
-        StructuredGraph graph = (StructuredGraph) n.graph();
         try (DebugCloseable context = n.withNodeSourcePosition()) {
             if (n instanceof LoadFieldNode) {
                 lowerLoadFieldNode((LoadFieldNode) n, tool);
@@ -282,18 +278,7 @@ public abstract class DefaultJavaLoweringProvider implements LoweringProvider {
             } else if (n instanceof CommitAllocationNode) {
                 lowerCommitAllocationNode((CommitAllocationNode) n, tool);
             } else if (n instanceof BoxNode) {
-                if (tool.getLoweringStage() == LoweringTool.StandardLoweringStage.HIGH_TIER) {
-                    /*
-                     * We do not perform box canonicalization directly in the node since want
-                     * virtualization of box nodes. Creating a boxed constant early on inhibits PEA
-                     * so we do it after PEA.
-                     */
-                    FloatingNode canonical = canonicalizeBoxing((BoxNode) n, metaAccess, tool.getConstantReflection());
-                    if (canonical != null) {
-                        n.replaceAtUsages((ValueNode) ((BoxNode) n).getLastLocationAccess(), InputType.Memory);
-                        graph.replaceFixedWithFloating((FixedWithNextNode) n, canonical);
-                    }
-                } else if (tool.getLoweringStage() == LoweringTool.StandardLoweringStage.MID_TIER) {
+                if (tool.getLoweringStage() == LoweringTool.StandardLoweringStage.MID_TIER) {
                     boxingSnippets.lower((BoxNode) n, tool);
                 }
             } else if (n instanceof UnboxNode) {
@@ -328,34 +313,6 @@ public abstract class DefaultJavaLoweringProvider implements LoweringProvider {
                 throw GraalError.shouldNotReachHere("Node implementing Lowerable not handled: " + n);
             }
         }
-    }
-
-    public static FloatingNode canonicalizeBoxing(BoxNode box, MetaAccessProvider metaAccess, ConstantReflectionProvider constantReflection) {
-        ValueNode value = box.getValue();
-        if (value.isConstant() && !GraalOptions.ImmutableCode.getValue(box.getOptions())) {
-            JavaConstant sourceConstant = value.asJavaConstant();
-            if (sourceConstant.getJavaKind() != box.getBoxingKind() && sourceConstant.getJavaKind().isNumericInteger()) {
-                switch (box.getBoxingKind()) {
-                    case Boolean:
-                        sourceConstant = JavaConstant.forBoolean(sourceConstant.asLong() != 0L);
-                        break;
-                    case Byte:
-                        sourceConstant = JavaConstant.forByte((byte) sourceConstant.asLong());
-                        break;
-                    case Char:
-                        sourceConstant = JavaConstant.forChar((char) sourceConstant.asLong());
-                        break;
-                    case Short:
-                        sourceConstant = JavaConstant.forShort((short) sourceConstant.asLong());
-                        break;
-                }
-            }
-            JavaConstant boxedConstant = constantReflection.boxPrimitive(sourceConstant);
-            if (boxedConstant != null && sourceConstant.getJavaKind() == box.getBoxingKind()) {
-                return ConstantNode.forConstant(boxedConstant, metaAccess, box.graph());
-            }
-        }
-        return null;
     }
 
     private void lowerSecondHalf(UnpackEndianHalfNode n) {
