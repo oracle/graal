3eb3d476695b7cd37ad2961d819b45650843e12f
Paul Wögerer <paul.woegerer@oracle.com>
2020-12-09 17:57:15 +0100

    Add support for multiple checksum values for serialization-config.json

--- substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/serialize/hosted/SerializationFeature.java
+++ substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/serialize/hosted/SerializationFeature.java
@@ -27,6 +27,20 @@ package com.oracle.svm.reflect.serialize.hosted;
 
 // Checkstyle: allow reflection
 
+import java.io.Externalizable;
+import java.io.ObjectStreamClass;
+import java.io.ObjectStreamField;
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.graalvm.nativeimage.ImageSingletons;
+import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.nativeimage.hosted.RuntimeReflection;
+
 import com.oracle.svm.core.annotate.AutomaticFeature;
 import com.oracle.svm.core.configure.ConfigurationFiles;
 import com.oracle.svm.core.configure.SerializationConfigurationParser;
@@ -41,19 +55,6 @@ import com.oracle.svm.hosted.NativeImageOptions;
 import com.oracle.svm.hosted.config.ConfigurationParserUtils;
 import com.oracle.svm.reflect.serialize.SerializationSupport;
 import com.oracle.svm.util.ReflectionUtil;
-import org.graalvm.nativeimage.ImageSingletons;
-import org.graalvm.nativeimage.hosted.Feature;
-import org.graalvm.nativeimage.hosted.RuntimeReflection;
-
-import java.io.Externalizable;
-import java.io.ObjectStreamClass;
-import java.io.ObjectStreamField;
-import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.HashSet;
-import java.util.Set;
 
 @AutomaticFeature
 public class SerializationFeature implements Feature {
@@ -67,12 +68,12 @@ public class SerializationFeature implements Feature {
         SerializationSupport serializationSupport = new SerializationSupport(imageClassLoader);
         ImageSingletons.add(SerializationRegistry.class, serializationSupport);
 
-        SerializationParserFunction serializationAdapter = (strTargetSerializationClass, checksum) -> {
+        SerializationParserFunction serializationAdapter = (strTargetSerializationClass, checksums) -> {
             Class<?> serializationTargetClass = resolveClass(strTargetSerializationClass, access);
             UserError.guarantee(serializationTargetClass != null, "Cannot find serialization target class %s. The missing of this class can't be ignored even if -H:+AllowIncompleteClasspath is set." +
                             " Please make sure it is in the classpath", strTargetSerializationClass);
             if (Serializable.class.isAssignableFrom(serializationTargetClass)) {
-                Class<?> targetConstructor = serializationSupport.addSerializationConstructorAccessorClass(serializationTargetClass, checksum, access);
+                Class<?> targetConstructor = serializationSupport.addSerializationConstructorAccessorClass(serializationTargetClass, checksums, access);
                 addReflections(serializationTargetClass, targetConstructor);
             }
         };
