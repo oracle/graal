868cdc2805d0112ca1d9de1df55032c1dd530643
David Leopoldseder <david.leopoldseder@oracle.com>
2020-12-15 12:47:08 +0100

    checkstyle fixes

--- compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/VerifyIterableNodeTypes.java
+++ compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/VerifyIterableNodeTypes.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package org.graalvm.compiler.hotspot.test;
 
 import org.graalvm.compiler.graph.Graph;
@@ -43,7 +67,7 @@ public class VerifyIterableNodeTypes extends VerifyPhase<CoreProviders> {
                     ResolvedJavaMethod callee = t.targetMethod();
                     if (callee.getDeclaringClass().equals(nodeIterableType)) {
                         if (callee.getName().equals("filter")) {
-                            ResolvedJavaMethod.Parameter params[] = callee.getParameters();
+                            ResolvedJavaMethod.Parameter[] params = callee.getParameters();
                             if (params.length == 1 && params[0].getType().equals(classType)) {
                                 // call to filter
                                 ValueNode v = t.arguments().get(1);
