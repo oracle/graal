e9917d4c1e2d8546b454942f8f3cca9c5c96ccc6
Paley Li <paley.li@oracle.com>
2020-12-16 16:35:05 +0100

    Sulong: initial planning

---
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/intrinsics/LLVMNativeFunctionPointerNode.java
@@ -0,0 +1,9 @@
+package com.oracle.truffle.llvm.runtime.nodes.intrinsics;
+
+import com.oracle.truffle.llvm.runtime.nodes.api.LLVMNode;
+
+public abstract class LLVMNativeFunctionPointerNode extends LLVMNode {
+
+    // 
+
+}
