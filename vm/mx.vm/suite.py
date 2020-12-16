235ed0304956269ec5d427618a96321e437f8b22
Andreas Woess <andreas.woess@oracle.com>
2020-12-16 17:21:46 +0000

    [GR-28203] ByteBuffer.duplicate() is not designed for PE.
    PullRequest: js/1815

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "bcbb63edbe9be75b7f3b3a059545ac34f737fd1b",
+                "version": "7060e6c1d8879f102e2c3bdd3c4267751c5e0179",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "bcbb63edbe9be75b7f3b3a059545ac34f737fd1b",
+                "version": "7060e6c1d8879f102e2c3bdd3c4267751c5e0179",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
