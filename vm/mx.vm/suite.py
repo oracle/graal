b14157060a22751c5bf932a851353b652c577e74
Jan Stola <jan.stola@oracle.com>
2020-12-17 12:43:54 +0000

    [GR-28112] Backport: Allow parsing of legacy dates without time-zone offset as being in local time.
    PullRequest: js/1816

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "7060e6c1d8879f102e2c3bdd3c4267751c5e0179",
+                "version": "a91cf043c483a5c018952d065a391089a9f1516c",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "7060e6c1d8879f102e2c3bdd3c4267751c5e0179",
+                "version": "a91cf043c483a5c018952d065a391089a9f1516c",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
