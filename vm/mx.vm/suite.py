a7d5ba64f0de4234a49696ed0139f3f6961bdc3c
Jan Stola <jan.stola@oracle.com>
2020-12-18 09:21:27 +0000

    [GR-28227] Backport: Java null should behave like null during Object creation.
    PullRequest: js/1820

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a91cf043c483a5c018952d065a391089a9f1516c",
+                "version": "012aa859d636c4b214f20c3423e57c584f1b6c54",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a91cf043c483a5c018952d065a391089a9f1516c",
+                "version": "012aa859d636c4b214f20c3423e57c584f1b6c54",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
