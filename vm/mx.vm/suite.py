27bdb0b4086fb8063d3fe113a17db2cdd259703c
Taha Basri <taha.basri@oracle.com>
2020-12-28 14:02:04 +0000

    [GR-28387] Backport to 20.3.1: Object(javaNull) should behave as Object(null).
    PullRequest: js/1825

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "27825d0543bc502707550948ae57ea28d9d98602",
+                "version": "6bd9f12ddb9474e5b106057844b18aa6c7329870",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "27825d0543bc502707550948ae57ea28d9d98602",
+                "version": "6bd9f12ddb9474e5b106057844b18aa6c7329870",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
