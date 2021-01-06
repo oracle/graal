c7f623e7908c0f5deb30ff95fe29987fe42b339d
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2021-01-06 18:56:47 +0100

    Track allocated objects with AllocationReporter.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/EspressoContext.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/EspressoContext.java
@@ -34,6 +34,7 @@ import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
+import com.oracle.truffle.api.instrumentation.AllocationReporter;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.espresso.EspressoBindings;
 import org.graalvm.polyglot.Engine;
@@ -94,6 +95,10 @@ public final class EspressoContext {
     // region Debug
     private final TimerCollection timers;
     // endregion Debug
+
+    // region Profiling
+    private AllocationReporter allocationReporter;
+    // endregion Profiling
 
     // region Runtime
     private final StringTable strings;
@@ -206,6 +211,7 @@ public final class EspressoContext {
         this.shutdownManager = new EspressoShutdownHandler(this, threadManager, referenceDrainer);
 
         this.timers = TimerCollection.create(env.getOptions().get(EspressoOptions.EnableTimers));
+        this.allocationReporter = env.lookup(AllocationReporter.class);
 
         // null if not specified
         this.JDWPOptions = env.getOptions().get(EspressoOptions.JDWPOptions);
@@ -522,6 +528,14 @@ public final class EspressoContext {
 
     public EspressoException getOutOfMemory() {
         return outOfMemory;
+    }
+
+    public <T> T trackAllocation(T object) {
+        if (allocationReporter != null) {
+            allocationReporter.onEnter(null, 0, AllocationReporter.SIZE_UNKNOWN);
+            allocationReporter.onReturnValue(object, 0, AllocationReporter.SIZE_UNKNOWN);
+        }
+        return object;
     }
 
     public void prepareDispose() {
