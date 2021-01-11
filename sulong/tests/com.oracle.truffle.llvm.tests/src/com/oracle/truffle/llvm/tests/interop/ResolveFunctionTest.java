621546ab2ee9b8342178dfdb5d720363c1da68b6
Paley Li <paley.li@oracle.com>
2021-01-11 14:31:39 +0100

    Sulong: add case for resolving native long pointer.

--- sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/ResolveFunctionTest.java
+++ sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/ResolveFunctionTest.java
@@ -1,9 +1,37 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.oracle.truffle.llvm.tests.interop;
 
 import com.oracle.truffle.api.CallTarget;
 import com.oracle.truffle.api.interop.InteropException;
 import com.oracle.truffle.api.interop.InteropLibrary;
-import com.oracle.truffle.api.nodes.DirectCallNode;
 import com.oracle.truffle.llvm.runtime.LLVMFunctionDescriptor;
 import com.oracle.truffle.llvm.runtime.pointer.LLVMManagedPointer;
 import com.oracle.truffle.tck.TruffleRunner;
@@ -17,12 +45,19 @@ import org.junit.runner.RunWith;
 public class ResolveFunctionTest extends InteropTestBase {
 
     private static Object testLibrary;
+    private static Object fortytwoFunction;
+    private static Object maxFunction;
 
     @BeforeClass
     public static void loadTestBitcode() {
         testLibrary = loadTestBitcodeInternal("createResolveFunction.c");
+        try {
+            fortytwoFunction = InteropLibrary.getFactory().getUncached().readMember(testLibrary, "fortytwo");
+            maxFunction = InteropLibrary.getFactory().getUncached().readMember(testLibrary, "max");
+        } catch (InteropException ex) {
+            throw new AssertionError(ex);
+        }
     }
-
 
     public static class fortytwoFunctionNode extends SulongTestNode {
         public fortytwoFunctionNode() {
@@ -31,55 +66,59 @@ public class ResolveFunctionTest extends InteropTestBase {
     }
 
     @Test
-    public void testfortytwoFunction(@Inject(fortytwoFunctionNode.class) CallTarget function) {
+    public void testFunctionFortytwo(@Inject(fortytwoFunctionNode.class) CallTarget function) throws InteropException {
         Object ret = function.call();
         if (LLVMManagedPointer.isInstance(ret)) {
             LLVMFunctionDescriptor retFunction = (LLVMFunctionDescriptor) LLVMManagedPointer.cast(ret).getObject();
             String name = retFunction.getLLVMFunction().getName();
             Assert.assertEquals("fortytwo", name);
-            DirectCallNode call = DirectCallNode.create(retFunction.getFunctionCode().getLLVMIRFunctionSlowPath());
-            Assert.assertEquals("42", call.call());
+            Assert.assertEquals(42, InteropLibrary.getUncached().execute(retFunction));
         }
-        throw new AssertionError();
     }
-
-    private Object fortytwoFunction;
-    private Object maxFunction;
 
     public class ResolveFunctionNode extends SulongTestNode {
         public ResolveFunctionNode() {
             super(testLibrary, "test_resolve_function");
-            try {
-                fortytwoFunction = InteropLibrary.getFactory().getUncached().readMember(testLibrary, "fortytwo");
-                maxFunction = InteropLibrary.getFactory().getUncached().readMember(testLibrary, "max");
-            } catch (InteropException ex) {
-                throw new AssertionError(ex);
-            }
         }
     }
 
     @Test
-    public void testFortytwoResolveFunction(@Inject(ResolveFunctionNode.class) CallTarget function) {
+    public void testResolveFunctionFortytwo(@Inject(ResolveFunctionNode.class) CallTarget function) throws InteropException {
         Object ret = function.call(fortytwoFunction);
         if (LLVMManagedPointer.isInstance(ret)) {
             LLVMFunctionDescriptor retFunction = (LLVMFunctionDescriptor) LLVMManagedPointer.cast(ret).getObject();
-            DirectCallNode call = DirectCallNode.create(retFunction.getFunctionCode().getLLVMIRFunctionSlowPath());
-            //CallTarget call = retFunction.getFunctionCode().getLLVMIRFunctionSlowPath();
-            //Assert.assertEquals("42", IndirectCallNode.create().call(call));
-            Assert.assertEquals("42", call.call());
+            Assert.assertEquals(42, InteropLibrary.getUncached().execute(retFunction));
         }
-        throw new AssertionError();
     }
 
     @Test
-    public void testMaxResolveFunction(@Inject(ResolveFunctionNode.class) CallTarget function) {
+    public void testResolveNativeFunctionFortytwo(@Inject(ResolveFunctionNode.class) CallTarget function) throws InteropException {
+        InteropLibrary.getUncached().toNative(fortytwoFunction);
+        long pointer = InteropLibrary.getUncached().asPointer(fortytwoFunction);
+        Object ret = function.call(pointer);
+        if (LLVMManagedPointer.isInstance(ret)) {
+            LLVMFunctionDescriptor retFunction = (LLVMFunctionDescriptor) LLVMManagedPointer.cast(ret).getObject();
+            Assert.assertEquals(42, InteropLibrary.getUncached().execute(retFunction));
+        }
+    }
+
+    @Test
+    public void testResolveNativeFunctionMax(@Inject(ResolveFunctionNode.class) CallTarget function) throws InteropException {
+        InteropLibrary.getUncached().toNative(maxFunction);
+        long pointer = InteropLibrary.getUncached().asPointer(maxFunction);
+        Object ret = function.call(pointer);
+        if (LLVMManagedPointer.isInstance(ret)) {
+            LLVMFunctionDescriptor retFunction = (LLVMFunctionDescriptor) LLVMManagedPointer.cast(ret).getObject();
+            Assert.assertEquals(42, InteropLibrary.getUncached().execute(retFunction, 1, 42));
+        }
+    }
+
+    @Test
+    public void testResolveFunctionMax(@Inject(ResolveFunctionNode.class) CallTarget function) throws InteropException {
         Object ret = function.call(maxFunction);
         if (LLVMManagedPointer.isInstance(ret)) {
             LLVMFunctionDescriptor retFunction = (LLVMFunctionDescriptor) LLVMManagedPointer.cast(ret).getObject();
-            DirectCallNode call = DirectCallNode.create(retFunction.getFunctionCode().getLLVMIRFunctionSlowPath());
-            Object arguments = new int[]{41, 42};
-            Assert.assertEquals("42", call.call(arguments));
+            Assert.assertEquals(42, InteropLibrary.getUncached().execute(retFunction, 1, 42));
         }
-        throw new AssertionError();
     }
 }
