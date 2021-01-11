d92494dd8088e426b7104bf7d596ac39b9982ae1
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2021-01-11 19:13:15 +0100

    AllocationReporter must be final.

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/EspressoContext.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/EspressoContext.java
@@ -97,7 +97,7 @@ public final class EspressoContext {
     // endregion Debug
 
     // region Profiling
-    private AllocationReporter allocationReporter;
+    private final AllocationReporter allocationReporter;
     // endregion Profiling
 
     // region Runtime
