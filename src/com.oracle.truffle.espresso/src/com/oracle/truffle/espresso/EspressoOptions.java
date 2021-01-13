70e6813bb795b8aa47d9564a865af4861f74e6de
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2021-01-13 22:11:40 +0100

    Formatting

--- src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoOptions.java
+++ src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoOptions.java
@@ -346,7 +346,7 @@ public final class EspressoOptions {
     public static final OptionKey<Boolean> Polyglot = new OptionKey<>(true);
 
     @Option(help = "Expose the <JavaVM> binding.", //
-            category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL) //
+                    category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL) //
     public static final OptionKey<Boolean> ExposeNativeJavaVM = new OptionKey<>(false);
 
     public static final String INCEPTION_NAME = System.getProperty("espresso.inception.name", "#");
