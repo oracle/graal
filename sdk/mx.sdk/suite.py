37cf7b591eba9f16c37a0f935f6a2e7fe4df79e9
Josef Eisl <josef.eisl@oracle.com>
2021-01-20 10:22:36 +0100

    sdk: bump mx version

--- sdk/mx.sdk/suite.py
+++ sdk/mx.sdk/suite.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -39,7 +39,7 @@
 # SOFTWARE.
 #
 suite = {
-  "mxversion" : "5.279.1",
+  "mxversion" : "5.282.0",
   "name" : "sdk",
   "version" : "21.1.0",
   "release" : False,
