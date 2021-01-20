b7679897ae8b2e0f4baf6195ea40c1441ec32b9a
Andreas Woess <andreas.woess@oracle.com>
2021-01-21 00:47:29 +0100

    Move onPropertyTransition into LayoutStrategy.

--- truffle/src/com.oracle.truffle.object/src/com/oracle/truffle/object/ShapeImpl.java
+++ truffle/src/com.oracle.truffle.object/src/com/oracle/truffle/object/ShapeImpl.java
@@ -552,7 +552,6 @@ public abstract class ShapeImpl extends Shape {
     @Override
     public ShapeImpl addProperty(Property property) {
         assert isValid();
-        onPropertyTransition(property);
 
         return layout.getStrategy().addProperty(this, property);
     }
