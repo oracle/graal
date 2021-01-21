14806d58e5cb99cb35333cca7b4c6e7ef8109280
Daniele Bonetta <daniele.bonetta@oracle.com>
2021-01-21 14:36:14 +0000

    [GR-28200] Update graal import.
    PullRequest: js/1854

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "adaf96be807ca968623c6e90ff09a971e798dd8f",
+                "version": "9cb60c5a0f7f4cf9f1769b90ad9e3125d1977b63",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "adaf96be807ca968623c6e90ff09a971e798dd8f",
+                "version": "9cb60c5a0f7f4cf9f1769b90ad9e3125d1977b63",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
