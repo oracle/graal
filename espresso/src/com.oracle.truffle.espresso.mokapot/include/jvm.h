d82f6725a6d61ee260336860e0b8f85d259f5168
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2021-01-21 19:24:16 +0100

    Fix copyright headers.

--- espresso/src/com.oracle.truffle.espresso.mokapot/include/jvm.h
+++ espresso/src/com.oracle.truffle.espresso.mokapot/include/jvm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -19,7 +19,6 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
- *
  */
 #ifndef _JAVASOFT_JVM_H_
 #define _JAVASOFT_JVM_H_
--- espresso/src/com.oracle.truffle.espresso.mokapot/src/mokapot_posix.c
+++ espresso/src/com.oracle.truffle.espresso.mokapot/src/mokapot_posix.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -19,7 +19,6 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
- *
  */
 
 #if !defined(_WIN32)
