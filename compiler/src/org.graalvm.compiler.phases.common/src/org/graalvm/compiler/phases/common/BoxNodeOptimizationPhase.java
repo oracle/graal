2644d6a7b4da11cbfa84501dcd2550e58655c05d
David Leopoldseder <david.leopoldseder@oracle.com>
2021-01-28 10:00:38 +0100

    box optimization: doc fixes

--- compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/BoxNodeOptimizationPhase.java
+++ compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/BoxNodeOptimizationPhase.java
@@ -57,21 +57,21 @@ import jdk.vm.ci.meta.MetaAccessProvider;
 /**
  * Phase that tries to optimize Java boxing (auto-boxing of primitive values) operations.
  *
- * This phase performs to distinct optimizations that are grouped into one phase for simplicity.
+ * This phase performs two distinct optimizations that are grouped into one phase for simplicity.
  *
- * (1) First Transformation: Box node canonicalization
+ * (PART 1) First Transformation: Box node canonicalization
  *
  * Perform canonicalization of box nodes before lowering. We do not perform box canonicalization
  * directly in the node since want virtualization of box nodes. Creating a boxed constant early on
  * inhibits PEA so we do it after PEA but before lowering.
  *
  *
- * (2) Second Transformation: Out-of-cache boxed value reuse
+ * (PART 2) Second Transformation: Out-of-cache boxed value reuse
  *
  * Try to replace box operations with dominating box/unbox values. There are two distinct cases
  * covered in this phase (they are marked in the code of the phase below)
  *
- * case 1:
+ * PART 2.1:
  *
  * <pre>
  * unboxedVal = unbox(a)
@@ -94,7 +94,7 @@ import jdk.vm.ci.meta.MetaAccessProvider;
  *
  *
  *
- * case 2:
+ * PART 2.2:
  *
  * <pre>
  * boxedVal1 = box(primitiveVal)
@@ -134,12 +134,12 @@ public class BoxNodeOptimizationPhase extends BasePhase<CoreProviders> {
         boxLoop: for (BoxNode box : graph.getNodes(BoxNode.TYPE)) {
             FloatingNode canonical = canonicalizeBoxing(box, context.getMetaAccess(), context.getConstantReflection());
             if (canonical != null) {
-                // case 1 from javadoc
+                // PART 1
                 box.replaceAtUsages((ValueNode) box.getLastLocationAccess(), InputType.Memory);
                 graph.replaceFixedWithFloating(box, canonical);
             }
             if (box.isAlive() && OptimizedBoxVersions.contains(box.getBoxingKind())) {
-                // case 2 from javadoc
+                // PART 2
                 if (box instanceof OptimizedAllocatingBoxNode) {
                     continue;
                 }
@@ -148,11 +148,13 @@ public class BoxNodeOptimizationPhase extends BasePhase<CoreProviders> {
                     assert boxedVal != null : "Box " + box + " has no value";
                     // try to optimize with dominating unbox of the same value
                     if (boxedVal instanceof UnboxNode && ((UnboxNode) boxedVal).getBoxingKind() == box.getBoxingKind()) {
+                        // PART 2.2
                         optimziBoxed(box, ((UnboxNode) boxedVal).getValue());
                         continue boxLoop;
                     }
                     // try to optimize with dominating box of the same value
                     if (box.isAlive()) {
+                        // PART 2.2
                         boxedValUsageLoop: for (Node usage : boxedVal.usages().snapshot()) {
                             if (usage == box) {
                                 continue;
