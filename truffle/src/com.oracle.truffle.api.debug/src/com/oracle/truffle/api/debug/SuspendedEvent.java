46d93292dcd93770798e0ccfe268df48f8479298
Allan Gregersen <allan.gregersen@oracle.com>
2021-02-04 14:04:19 +0100

    fix parameter name hiding a field

--- truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/SuspendedEvent.java
+++ truffle/src/com.oracle.truffle.api.debug/src/com/oracle/truffle/api/debug/SuspendedEvent.java
@@ -675,21 +675,21 @@ public final class SuspendedEvent {
 
     /**
      * Prepare to unwind a frame. This frame and all frames above it are unwound off the execution
-     * stack and the frame will return immediately with <code>returnValue</code>. If the return
+     * stack and the frame will return immediately with <code>immediateReturnValue</code>. If the return
      * value is <code>null</code>, the unwound frame will instead be reentered upon thread
      * resumption. The frame needs to be on the {@link #getStackFrames() execution stack of this
      * event}.
      *
      * @param frame the frame to unwind
-     * @param returnValue an Interop value to return
+     * @param immediateReturnValue an Interop value to return
      * @throws IllegalArgumentException when the frame is not on the execution stack of this event
      * @since 21.1.0
      */
-    public void prepareUnwindFrame(DebugStackFrame frame, Object returnValue) throws IllegalArgumentException {
+    public void prepareUnwindFrame(DebugStackFrame frame, Object immediateReturnValue) throws IllegalArgumentException {
         if (frame.event != this) {
             throw new IllegalArgumentException("The stack frame is not in the scope of this event.");
         }
-        setNextStrategy(SteppingStrategy.createUnwind(frame.getDepth(), returnValue));
+        setNextStrategy(SteppingStrategy.createUnwind(frame.getDepth(), immediateReturnValue));
     }
 
     /**
