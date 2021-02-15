542ee44551cbc5461536fa0218eec117dd450e59
Boris Spasojevic <boris.spasojevic@oracle.com>
2021-02-15 10:46:52 +0100

    Prefer first tier compilations.

--- compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/collection/TraversingQueue.java
+++ compiler/src/org.graalvm.compiler.truffle.runtime/src/org/graalvm/compiler/truffle/runtime/collection/TraversingQueue.java
@@ -24,13 +24,14 @@
  */
 package org.graalvm.compiler.truffle.runtime.collection;
 
-import org.graalvm.compiler.truffle.runtime.CompilationTask;
-
 import java.util.LinkedList;
 import java.util.List;
 
+import org.graalvm.compiler.truffle.runtime.CompilationTask;
+
 public class TraversingQueue<E> implements SerialQueue<E> {
     List<E> entries = new LinkedList<>();
+
     @Override
     public void add(E x) {
         entries.add(x);
@@ -45,22 +46,33 @@ public class TraversingQueue<E> implements SerialQueue<E> {
 
     @Override
     public E peek() {
-        CompilationTask.ExecutorServiceWrapper max = null;
-        int maxIncrease = -1;
+        CompilationTask.ExecutorServiceWrapper firstTierMax = null;
+        int firstTierMaxInc = -1;
+        CompilationTask.ExecutorServiceWrapper lastTierMax = null;
+        int lastTierMaxInc = -1;
         for (E entry : entries) {
-            if (entry instanceof CompilationTask.ExecutorServiceWrapper) {
-                CompilationTask compileTask = ((CompilationTask.ExecutorServiceWrapper) entry).getCompileTask();
-                if (compileTask == null) {
-                    continue;
+            CompilationTask compileTask = ((CompilationTask.ExecutorServiceWrapper) entry).getCompileTask();
+            if (compileTask == null) {
+                continue;
+            }
+            int increase = compileTask.getIncrease();
+            if (compileTask.isFirstTier()) {
+                if (increase > firstTierMaxInc) {
+                    firstTierMaxInc = increase;
+                    firstTierMax = (CompilationTask.ExecutorServiceWrapper) entry;
                 }
-                int increase = compileTask.getIncrease();
-                if (increase > maxIncrease) {
-                    maxIncrease = increase;
-                    max = (CompilationTask.ExecutorServiceWrapper) entry;
+            } else {
+                if (increase > lastTierMaxInc) {
+                    lastTierMaxInc = increase;
+                    lastTierMax = (CompilationTask.ExecutorServiceWrapper) entry;
                 }
             }
         }
-        return (E) max;
+        if (firstTierMax != null) {
+            return (E) firstTierMax;
+        } else {
+            return (E) lastTierMax;
+        }
     }
 
     @Override
