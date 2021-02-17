90358fd1134fd7a9149aabec09bb026c31f29b18
Josef Eisl <josef.eisl@oracle.com>
2021-02-17 09:39:55 +0100

    sulong: SulongSuite imports

--- sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/SulongSuite.java
+++ sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/SulongSuite.java
@@ -30,15 +30,9 @@
 package com.oracle.truffle.llvm.tests;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
