306d2be283f3a75d1c9c631bc2f52db55901666e
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2021-02-19 02:40:07 +0100

    Remove redundant constructor.

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/_native/nfi/NativeUtils.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/_native/nfi/NativeUtils.java
@@ -41,7 +41,7 @@ import com.oracle.truffle.espresso.meta.JavaKind;
 
 public final class NativeUtils {
     public static ByteBuffer directByteBuffer(@Pointer TruffleObject addressPtr, long size, JavaKind kind) {
-        return directByteBuffer(interopAsPointer(addressPtr), Math.multiplyExact(size, kind.getByteCount()));
+        return directByteBuffer(addressPtr, Math.multiplyExact(size, kind.getByteCount()));
     }
 
     private static final Constructor<? extends ByteBuffer> constructor;
@@ -96,15 +96,7 @@ public final class NativeUtils {
 
     @TruffleBoundary
     public static ByteBuffer directByteBuffer(@Pointer TruffleObject addressPtr, long capacity) {
-        ByteBuffer buffer = null;
-        try {
-            long address = interopAsPointer(addressPtr);
-            buffer = constructor.newInstance(address, Math.toIntExact(capacity));
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-            throw EspressoError.shouldNotReachHere(e);
-        }
-        buffer.order(ByteOrder.nativeOrder());
-        return buffer;
+        return directByteBuffer(interopAsPointer(addressPtr), capacity);
     }
 
     @TruffleBoundary
