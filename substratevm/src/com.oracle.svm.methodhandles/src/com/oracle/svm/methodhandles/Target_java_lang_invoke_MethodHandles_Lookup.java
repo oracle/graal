9a62eedf97ed450219e02014490c1f70cbf30bfc
Yichen Yan <yichen.yan@oracle.com>
2021-02-20 14:27:41 +0100

    Consolidate classes.

--- substratevm/src/com.oracle.svm.methodhandles/src/com/oracle/svm/methodhandles/Target_java_lang_invoke_MethodHandles_Lookup.java
+++ substratevm/src/com.oracle.svm.methodhandles/src/com/oracle/svm/methodhandles/Target_java_lang_invoke_MethodHandles_Lookup.java
@@ -29,14 +29,18 @@ import static com.oracle.svm.core.util.VMError.unimplemented;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 
+import com.oracle.svm.core.SubstrateUtil;
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
 import com.oracle.svm.core.annotate.TargetElement;
+import com.oracle.svm.core.hub.DynamicHub;
 import com.oracle.svm.core.jdk.JDK11_0_10OrEarlier;
 import com.oracle.svm.core.jdk.JDK11_0_11OrLater;
+import com.oracle.svm.core.jdk.JDK15OrLater;
 import com.oracle.svm.core.jdk.JDK16OrLater;
+import com.oracle.svm.core.jdk.Target_java_lang_Module;
 
 @TargetClass(value = MethodHandles.class, innerClass = "Lookup", onlyWith = MethodHandlesSupported.class)
 final class Target_java_lang_invoke_MethodHandles_Lookup {
@@ -64,6 +68,42 @@ final class Target_java_lang_invoke_MethodHandles_Lookup {
                     throws IllegalAccessException {
         /* Binding the caller triggers the generation of an invoker */
         return mh;
+    }
+
+    @Alias //
+    @TargetElement(onlyWith = JDK15OrLater.class) //
+    private Class<?> lookupClass;
+
+    @Alias //
+    @TargetElement(onlyWith = JDK15OrLater.class) //
+    private Class<?> prevLookupClass;
+
+    @Alias //
+    @TargetElement(onlyWith = JDK15OrLater.class) //
+    private int allowedModes;
+
+    @Substitute
+    @TargetElement(onlyWith = JDK15OrLater.class)
+    private IllegalAccessException makeAccessException(Class<?> targetClass) {
+        String message = "access violation: " + targetClass;
+        if (this == SubstrateUtil.cast(MethodHandles.publicLookup(), Target_java_lang_invoke_MethodHandles_Lookup.class)) {
+            message += ", from public Lookup";
+        } else {
+            Target_java_lang_Module m = SubstrateUtil.cast(lookupClass, DynamicHub.class).getModule();
+            message += ", from " + lookupClass + " (" + m + ")";
+            if (prevLookupClass != null) {
+                message += ", previous lookup " +
+                                prevLookupClass.getName() + " (" + SubstrateUtil.cast(prevLookupClass, DynamicHub.class).getModule() + ")";
+            }
+        }
+        return new IllegalAccessException(message);
+    }
+
+    /** This call is a noop without the security manager. */
+    @SuppressWarnings("unused")
+    @Substitute
+    @TargetElement(onlyWith = JDK15OrLater.class)
+    void checkSecurityManager(Class<?> refc) {
     }
 
     @Alias @TargetElement(onlyWith = JDK16OrLater.class)//
