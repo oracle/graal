f52ef37434bdaf394b693ec5561fe5fb2af8da62
Allan Gregersen <allan.gregersen@oracle.com>
2021-02-26 08:20:34 +0100

    merge for loops and register generated method parameters under 'param_index' name

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoInstrumentableNode.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoInstrumentableNode.java
@@ -33,7 +33,6 @@ import com.oracle.truffle.api.library.ExportMessage;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.espresso.EspressoScope;
 import com.oracle.truffle.espresso.classfile.attributes.Local;
-import com.oracle.truffle.espresso.classfile.constantpool.Utf8Constant;
 import com.oracle.truffle.espresso.descriptors.ByteSequence;
 import com.oracle.truffle.espresso.descriptors.Symbol;
 import com.oracle.truffle.espresso.descriptors.Utf8ConstantTable;
@@ -75,26 +74,24 @@ public abstract class EspressoInstrumentableNode extends Node implements Instrum
         if (liveLocals.length == 0) {
             // class was compiled without a local variable table
             // include "this" in method arguments throughout the method
-            int localCount = !method.isStatic() ? 1 : 0;
+            boolean hasReceiver = !method.isStatic();
+            int localCount = hasReceiver ? 1 : 0;
             localCount += method.getParameterCount();
             liveLocals = new Local[localCount];
             Klass[] parameters = (Klass[]) method.getParameters();
             Utf8ConstantTable utf8Constants = getContext().getLanguage().getUtf8ConstantTable();
-            if (!method.isStatic()) {
+            int startslot = 0;
+
+            if (hasReceiver) {
                 // include 'this' and method arguments
                 liveLocals[0] = new Local(utf8Constants.getOrCreate(Symbol.Name.thiz), utf8Constants.getOrCreate(method.getDeclaringKlass().getType()), 0, 65536, 0);
-                for (int i = 1; i < localCount; i++) {
-                    Klass param = parameters[i - 1];
-                    Utf8Constant name = utf8Constants.getOrCreate(ByteSequence.create("" + (i - 1)));
-                    Utf8Constant type = utf8Constants.getOrCreate(param.getType());
-                    liveLocals[i] = new Local(name, type, 0, 65536, i);
-                }
-            } else {
-                // only include method arguments
-                for (int i = 0; i < localCount; i++) {
-                    Klass param = parameters[i];
-                    liveLocals[i] = new Local(utf8Constants.getOrCreate(ByteSequence.create("" + (i - 1))), utf8Constants.getOrCreate(param.getType()), 0, 65536, i);
-                }
+                startslot++;
+            }
+
+            // include method parameters
+            for (int i = startslot; i < localCount; i++) {
+                Klass param = hasReceiver ? parameters[i - 1] : parameters[i];
+                liveLocals[i] = new Local(utf8Constants.getOrCreate(ByteSequence.create("param_" + (i))), utf8Constants.getOrCreate(param.getType()), 0, 65536, i);
             }
         }
         Object scopeName = method.getMeta().toGuestString(method.getNameAsString());
