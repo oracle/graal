e196ad760cba67ba9a653c819ae9816c390bb22c
Paley Li <paley.li@oracle.com>
2021-03-01 12:34:00 +0100

    Sulong: format and copyright for test

--- sulong/tests/com.oracle.truffle.llvm.tests.sulong.native/c/pthread_test.c
+++ sulong/tests/com.oracle.truffle.llvm.tests.sulong.native/c/pthread_test.c
@@ -1,48 +1,90 @@
-// #define _GNU_SOURCE is needed for the resolution of the following warnings
-//warning: implicit declaration of function ‘pthread_setname_np’ [-Wimplicit-function-declaration]
-//warning: implicit declaration of function ‘pthread_getname_np’ [-Wimplicit-function-declaration]
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <pthread.h>
 
 void *set_named_thread(void *data) {
+
     pthread_t *thread_info = (pthread_t *) data;
     const int setname_rv = pthread_setname_np(*thread_info, "sulong pthread");
+    
     if (setname_rv) {
         printf("Could not set pthread name\n");
     }
+    
     char thread_name[16];
     const int getname_rv = pthread_getname_np(*thread_info, thread_name, 16);
+    
     if (getname_rv) {
         printf("Could not get pthread name\n");
     }
+    
     fprintf(stdout, "My name is '%s'\n", thread_name);
 
     const pthread_t self = pthread_self();
     const int setname_rv_self = pthread_setname_np(self, "self pthread");
+    
     if (setname_rv_self) {
         printf("Could not set pthread name\n");
     }
+    
     char thread_name_self[16];
     const int getname_rv_self = pthread_getname_np(self, thread_name_self, 16);
+    
     if (getname_rv_self) {
         printf("Could not get pthread name\n");
     }
+    
     fprintf(stdout, "My name is '%s'\n", thread_name_self);
+    
     return NULL;
 }
 
 int main() {
-    pthread_t thread_idd;
-    const int create_rv = pthread_create(&(thread_idd), NULL, &set_named_thread, (void *) &thread_idd);
+
+    pthread_t thread;
+    const int create_rv = pthread_create(&(thread), NULL, &set_named_thread, (void *) &thread);
+    
     if (create_rv) {
         printf("Could not create thread\n");
         return create_rv;
     }
-    const int join_rv = pthread_join(thread_idd, NULL);
+    
+    const int join_rv = pthread_join(thread, NULL);
+    
     if (join_rv) {
         printf("Could not join thread\n");
         return join_rv;
     }
+    
     return 0;
 }
