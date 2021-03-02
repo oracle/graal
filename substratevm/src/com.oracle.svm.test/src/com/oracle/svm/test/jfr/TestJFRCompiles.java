d100a457fda250569cee0005d2c45187a98e13b2
Roman Kennke <rkennke@redhat.com>
2021-03-02 10:37:22 +0100

    Add test to verify that basic JFR app compiles, remove wrong assert in SizableInfo

---
+++ substratevm/src/com.oracle.svm.test/src/com/oracle/svm/test/jfr/TestJFRCompiles.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020, 2021, Red Hat, Inc.
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This file is part of the Red Hat GraalVM Testing Suite (the suite).
+ *
+ * The suite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.oracle.svm.test.jfr;
+
+import jdk.jfr.Recording;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.io.RandomAccessFile;
+
+public class TestJFRCompiles {
+
+    @Test
+    public void test() throws Exception {
+        JFR jfr = new LocalJFR();
+        Recording recording = jfr.startRecording("TestSingleEvent");
+
+        StringEvent event = new StringEvent();
+        event.message = "Event has been generated!";
+        event.commit();
+
+        jfr.endRecording(recording);
+    }
+}
