5145e2f4b4c884a253740238e9b76d828295540f
Tom Rodriguez <tom.rodriguez@oracle.com>
2021-03-04 01:20:46 -0800

    Switch to new JavaThread local storage

--- compiler/src/org.graalvm.compiler.truffle.runtime.hotspot/src/org/graalvm/compiler/truffle/runtime/hotspot/AbstractHotSpotTruffleRuntime.java
+++ compiler/src/org.graalvm.compiler.truffle.runtime.hotspot/src/org/graalvm/compiler/truffle/runtime/hotspot/AbstractHotSpotTruffleRuntime.java
@@ -169,7 +169,7 @@ public abstract class AbstractHotSpotTruffleRuntime extends GraalTruffleRuntime 
         setDontInlineCallBoundaryMethod(boundaryMethods);
         this.vmConfigAccess = new HotSpotVMConfigAccess(HotSpotJVMCIRuntime.runtime().getConfigStore());
 
-        int counters = vmConfigAccess.getFieldOffset("JavaThread::_jvmci_counters", Integer.class, "jlong*");
+        int counters = vmConfigAccess.getFieldOffset("JavaThread::_jvmci_reserved0", Integer.class, "intptr_t*");
         this.threadLocalPendingHandshakeOffset = counters;
     }
 
