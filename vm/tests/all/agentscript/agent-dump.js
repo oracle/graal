345030e632d1356b5ecc391a5bb67d6dffdeff48
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2021-03-05 08:50:44 +0100

    Specify format when requesting a heap.dump

--- vm/tests/all/agentscript/agent-dump.js
+++ vm/tests/all/agentscript/agent-dump.js
@@ -4,12 +4,18 @@ insight.on('return', (ctx, frame) => {
     if (frame.n !== 2) {
         return;
     }
-    heap.record([{
-        stack: [{
-            at: ctx,
-            frame: frame
-        }]
-    }], 10);
+    heap.dump({
+        format: "1.0",
+        depth: 10,
+        events: [
+          {
+            stack: [{
+                at: ctx,
+                frame: frame
+            }]
+          }
+        ]
+    });
     throw 'Heap dump written!';
 }, {
     roots: true,
