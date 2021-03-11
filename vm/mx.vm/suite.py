70a73aa965cff375cf5c2a005626022a67b7cf0e
Jan Stola <jan.stola@oracle.com>
2021-03-11 19:14:41 +0000

    [GR-29626] Updating ICU library to version 68.2.
    PullRequest: js/1913

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "f985e53a203afdbf72f2b2199008048b9c94af82",
+                "version": "54aa3862cfdf17631def6e46c30d1eaac027a30e",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "f985e53a203afdbf72f2b2199008048b9c94af82",
+                "version": "54aa3862cfdf17631def6e46c30d1eaac027a30e",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
