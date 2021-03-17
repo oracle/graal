a04857d832beb3e6170437cabf50c7bbd499bf3e
David Kozak <david.kozak@oracle.com>
2021-03-17 20:12:05 +0100

    Localization: remove unnecessary if check

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/localization/LocalizationFeature.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/localization/LocalizationFeature.java
@@ -234,9 +234,7 @@ public abstract class LocalizationFeature implements Feature {
         allLocales = processLocalesOption();
         defaultLocale = parseLocaleFromTag(Options.DefaultLocale.getValue());
         UserError.guarantee(defaultLocale != null, "Invalid default locale %s", Options.DefaultLocale.getValue());
-        if (!allLocales.contains(defaultLocale)) {
-            allLocales.add(defaultLocale);
-        }
+        allLocales.add(defaultLocale);
         support = selectLocalizationSupport();
         ImageSingletons.add(LocalizationSupport.class, support);
         ImageSingletons.add(LocalizationFeature.class, this);
