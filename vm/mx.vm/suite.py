2597cce3dc8262516aa9e71ff5e79c96f07b7d3b
Jan Stola <jan.stola@oracle.com>
2021-03-24 18:45:17 +0000

    [GR-30290] Backport: ArrayBuffer.prototype.byteLength should handle detached interop array buffer.
    PullRequest: js/1944

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "31fbb8bcd7a88ea3a57de94114f7c53f322b7176",
+                "version": "73a6f6aec6ce1b3bf35487edec9e64f97ffa9163",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "31fbb8bcd7a88ea3a57de94114f7c53f322b7176",
+                "version": "73a6f6aec6ce1b3bf35487edec9e64f97ffa9163",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
