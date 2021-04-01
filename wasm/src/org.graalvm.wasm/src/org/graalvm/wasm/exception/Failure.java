6412bb12fe056e721f26ff188c205ae732360319
Jan Stola <jan.stola@oracle.com>
2021-04-01 12:29:04 +0200

    Type of failure should not be based on the message of ArithmeticException.

--- wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/exception/Failure.java
+++ wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/exception/Failure.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -149,13 +149,4 @@ public enum Failure {
         this.name = name;
     }
 
-    public static Failure fromArithmeticException(ArithmeticException exception) {
-        switch (exception.getMessage()) {
-            case "/ by zero":
-            case "BigInteger divide by zero":
-                return Failure.INT_DIVIDE_BY_ZERO;
-            default:
-                throw exception;
-        }
-    }
 }
--- wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/nodes/WasmBlockNode.java
+++ wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/nodes/WasmBlockNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -383,10 +383,11 @@ public final class WasmBlockNode extends WasmNode implements RepeatingNode {
         check(intConstants.length, (1 << 31) - 1);
         check(profileCounters.length, (1 << 31) - 1);
         check(stacklocals.length, (1 << 31) - 1);
+        int opcode = UNREACHABLE;
         try {
             while (offset < offsetLimit) {
                 byte byteOpcode = BinaryStreamParser.rawPeek1(data, offset);
-                int opcode = byteOpcode & 0xFF;
+                opcode = byteOpcode & 0xFF;
                 offset++;
                 CompilerAsserts.partialEvaluationConstant(offset);
                 switch (opcode) {
@@ -1368,7 +1369,11 @@ public final class WasmBlockNode extends WasmNode implements RepeatingNode {
                 }
             }
         } catch (ArithmeticException e) {
-            throw WasmException.fromArithmeticException(this, e);
+            if (opcode == I32_DIV_S || opcode == I32_DIV_U || opcode == I32_REM_S || opcode == I32_REM_U || opcode == I64_DIV_S || opcode == I64_DIV_U || opcode == I64_REM_S || opcode == I64_REM_U) {
+                throw WasmException.create(Failure.INT_DIVIDE_BY_ZERO, this);
+            } else {
+                throw e;
+            }
         }
         return -1;
     }
