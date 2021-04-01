435b9f94b500ffe84b817c23ffe248eec97c4a50
abdelhamid haira <abdelhamid.haira@oracle.com>
2021-04-01 13:36:52 +0100

    Add trace logging in ReferenceObjectProcessing.discover

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/ReferenceObjectProcessing.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/ReferenceObjectProcessing.java
@@ -87,10 +87,12 @@ final class ReferenceObjectProcessing {
 
     private static void discover(Object obj, ObjectReferenceVisitor refVisitor) {
         Reference<?> dr = KnownIntrinsics.convertUnknownValue(obj, Reference.class);
+        Log trace = Log.noopLog().string("[ReferenceObjectProcessing.discover: ").object(dr);
         // The discovered field might contain an object with a forwarding header
         // to avoid issues during the cast just look at it as a raw pointer
         if (ReferenceInternals.getDiscoveredPointer(dr).isNonNull()) {
             // Was already discovered earlier.
+            trace.string(" already discovered]").newline();
             return;
         }
         Pointer referentAddr = ReferenceInternals.getReferentPointer(dr);
