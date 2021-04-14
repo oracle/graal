e24b347f510699d479cc99e4d3b8d2f64863a39d
Nicolas Laurent <nicolas.laurent@oracle.com>
2021-04-14 16:05:42 +0200

    add RootNode#countsTowardsStackTraceLimit

--- truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/TruffleException.java
+++ truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/TruffleException.java
@@ -209,9 +209,8 @@ public interface TruffleException {
      * Returns the number of guest language frames that should be collected for this exception.
      * Returns a negative integer by default for unlimited guest language frames. This is intended
      * to be used by guest languages to limit the number of guest language stack frames. Languages
-     * might want to limit the number of frames for performance reasons. Frames that point to
-     * {@link RootNode#isInternal() internal} internal root nodes are not counted when the stack
-     * trace limit is computed.
+     * might want to limit the number of frames for performance reasons. Only frames whose
+     * {@link RootNode#countsTowardsStackTraceLimit()} method return true count towards the limit.
      *
      * @since 0.27
      * @deprecated Pass the limit into the
