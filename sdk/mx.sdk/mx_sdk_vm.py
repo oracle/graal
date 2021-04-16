bb5114d1c72e7877fe6194c6f09f569e1ebea2ea
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2021-04-16 12:46:53 +0200

    Use stability attribute when provided

--- sdk/mx.sdk/mx_sdk_vm.py
+++ sdk/mx.sdk/mx_sdk_vm.py
@@ -276,16 +276,17 @@ class GraalVmComponent(object):
                 raise mx.abort("{}: Cannot use `stability` attribute in combination with deprecated `supported` and `early_adopter` attributes".format(name))
             mx.warn("{}: `supported` and `early_adopter` attributes are deprecated, please use `stability`".format(name))
 
-        if supported:
-            if early_adopter:
-                stability = "earlyadopter"
+        if stability is None:
+            if supported:
+                if early_adopter:
+                    stability = "earlyadopter"
+                else:
+                    stability = "supported"
             else:
-                stability = "supported"
-        else:
-            if early_adopter:
-                stability = "experimental-earlyadopter"
-            else:
-                stability = "experimental"
+                if early_adopter:
+                    stability = "experimental-earlyadopter"
+                else:
+                    stability = "experimental"
         self.stability = stability
 
         assert isinstance(self.jar_distributions, list)
