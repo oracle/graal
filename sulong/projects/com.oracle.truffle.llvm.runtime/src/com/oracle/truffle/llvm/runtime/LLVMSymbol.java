320581666ea3f225a31ffc46132955838110cbce
Paley Li <paley.li@oracle.com>
2021-04-23 11:16:36 +0200

    Sulong: remove possible null dereferencing.

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/LLVMSymbol.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/LLVMSymbol.java
@@ -99,7 +99,7 @@ public abstract class LLVMSymbol {
             return bitcodeID;
         }
         CompilerDirectives.transferToInterpreter();
-        throw new LLVMIllegalSymbolIndexException("Invalid function ID: " + bitcodeID.toString());
+        throw new LLVMIllegalSymbolIndexException("Invalid function ID: " + bitcodeID);
     }
 
     public final boolean hasValidIndexAndID() {
