b2551ff4af7681922e3f5321ef9d95f65dee9d9f
Christian Wimmer <christian.wimmer@oracle.com>
2021-05-05 11:42:09 -0700

    Reset DirectByteBuffer.cleaner on all JDK versions

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_java_nio_DirectByteBuffer_JDK11OrEarlier.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/Target_java_nio_DirectByteBuffer_JDK11OrEarlier.java
@@ -22,34 +22,17 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package com.oracle.svm.core.jdk8;
+package com.oracle.svm.core.jdk;
 
 import java.io.FileDescriptor;
 
 import com.oracle.svm.core.annotate.Alias;
-import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
-import com.oracle.svm.core.heap.Target_jdk_internal_ref_Cleaner;
-import com.oracle.svm.core.jdk.JDK11OrEarlier;
 
-/**
- * We disallow direct byte buffers ({@link java.nio.MappedByteBuffer} instances) in the image heap,
- * with one exception: we allow 0-length non-file-based buffers. For example, Netty has a singleton
- * empty buffer referenced from a static field, and a lot of Netty classes reference this buffer
- * statically.
- *
- * Such buffers do actually have an address to memory that is allocated during image generation and
- * therefore no longer available at run time. But since the capacity is 0, no memory can ever be
- * accessed. We therefore allow this "dangling" address. However, we must never call free() for that
- * address, so we remove the Cleaner registered for the buffer by resetting the field
- * {@link #cleaner}.
- */
 @TargetClass(className = "java.nio.DirectByteBuffer", onlyWith = JDK11OrEarlier.class)
 @SuppressWarnings("unused")
 final class Target_java_nio_DirectByteBuffer_JDK11OrEarlier {
-    @Alias @RecomputeFieldValue(kind = RecomputeFieldValue.Kind.Reset) //
-    Target_jdk_internal_ref_Cleaner cleaner;
 
     @Alias
     protected Target_java_nio_DirectByteBuffer_JDK11OrEarlier(int cap, long addr, FileDescriptor fd, Runnable unmapper) {
