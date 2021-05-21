296ccadd706d943a121f4b8951a00698644a51c4
Allan Gregersen <allan.gregersen@oracle.com>
2021-05-21 12:01:40 +0200

    remove the need for state on Member and move specific handling to subclasses

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/Field.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/Field.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -50,17 +50,26 @@ public final class Field extends Member<Type> implements FieldRef {
     private final LinkedField linkedField;
     private final ObjectKlass holder;
     private volatile Klass typeKlassCache;
+    private final boolean hidden;
 
     @CompilationFinal private Symbol<ModifiedUTF8> genericSignature = null;
 
     public Field(ObjectKlass holder, LinkedField linkedField, boolean hidden) {
-        super(hidden ? null : linkedField.getType(), linkedField.getName());
         this.linkedField = linkedField;
         this.holder = holder;
+        this.hidden = hidden;
+    }
+
+    @Override
+    public Symbol<Name> getName() {
+        return linkedField.getName();
     }
 
     public Symbol<Type> getType() {
-        return descriptor;
+        if (hidden) {
+            return null;
+        }
+        return linkedField.getType();
     }
 
     public Attribute[] getAttributes() {
@@ -80,7 +89,7 @@ public final class Field extends Member<Type> implements FieldRef {
     }
 
     public boolean isHidden() {
-        return getDescriptor() == null;
+        return hidden;
     }
 
     public JavaKind getKind() {
@@ -616,12 +625,12 @@ public final class Field extends Member<Type> implements FieldRef {
 
     @Override
     public String getNameAsString() {
-        return super.getName().toString();
+        return getName().toString();
     }
 
     @Override
     public String getTypeAsString() {
-        return super.getDescriptor().toString();
+        return getType().toString();
     }
 
     @Override
