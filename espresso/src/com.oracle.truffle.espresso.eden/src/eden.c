0d45526e6a5112eae0667aa0609fc7732c1a149f
Alfonso² Peterssen <alfonso.peterssen@oracle.com>
2021-06-07 11:18:35 +0200

    Fix crash on (old) glibc 2.17: Use __libc_dlsym instead of the hooked dlsym in eden_ctypeInit. Improve logging to better diagnose such errors in the future.

--- espresso/src/com.oracle.truffle.espresso.eden/src/eden.c
+++ espresso/src/com.oracle.truffle.espresso.eden/src/eden.c
@@ -29,12 +29,17 @@
 #include <unistd.h>
 #include <dlfcn.h>
 
+#define UNINITIALIZED ((void*) ~0)
+#define NOT_USED(x) ((void)(x))
+
 extern void *__libc_dlsym(void *handle, const char *symbol);
 extern void *__libc_dlopen_mode(const char *filename, int flags);
 extern const unsigned short int ** __ctype_b_loc (void);
 
+// Linking namespace where this library is loaded.
 static Lmid_t namespace_id = 0;
 
+// Debug flags, can be set with EDEN_DEBUG=true|1
 static int eden_debug = 0;
 
 #define LOG(fmt, ...) do { if (eden_debug) fprintf(stderr, "[eden #%ld] " fmt, namespace_id, ##__VA_ARGS__); } while (0)
@@ -42,48 +47,90 @@ static int eden_debug = 0;
 static void *get_libc() {
     static void *libc = NULL;
     if (libc == NULL) {
+        LOG("__libc_dlopen_mode(libc.so.6, RTLD_LAZY)\n");
         libc = __libc_dlopen_mode("libc.so.6", RTLD_LAZY);
-        LOG("get_libc(libc.so.6) => %p\n", libc);
     }
+    LOG("get_libc(libc.so.6) => %p\n", libc);
     return libc;
 }
 
 static void *get_libdl() {
     static void *libdl = NULL;
     if (libdl == NULL) {
+        LOG("__libc_dlopen_mode(libdl.so, RTLD_LAZY)\n");
         libdl = __libc_dlopen_mode("libdl.so", RTLD_LAZY);
     }
+    LOG("get_libc(libdl.so) => %p\n", libdl);
     return libdl;
-}
-
-static void *real_dlmopen(Lmid_t lmid, const char *filename, int flags) {
-    static void *(*the_real_dlmopen)(Lmid_t, const char *, int) = NULL;
-    if (the_real_dlmopen == NULL) {
-        the_real_dlmopen = __libc_dlsym(get_libdl(), "dlmopen");
-    }
-    return the_real_dlmopen(lmid, filename, flags);
-}
-
-void *dlmopen(Lmid_t lmid, const char *filename, int flags) {
-    void * result = real_dlmopen(lmid, filename, flags);
-    LOG("dlmopen(%s, %d) => %p\n", filename, flags, result);
-    return result;
-}
-
-static void *real_dlopen(const char *filename, int flags) {
-    static void *(*the_real_dlopen)(const char *, int) = NULL;
-    if (the_real_dlopen == NULL) {
-        the_real_dlopen = __libc_dlsym(get_libdl(), "dlopen");
-    }
-    return the_real_dlopen(filename, flags);
 }
 
 static void *get_libeden() {
     static void *libeden = NULL;
     if (libeden == NULL) {
-        libeden = real_dlopen("libeden.so", RTLD_LAZY);
+        LOG("__libc_dlopen_mode(libeden.so, RTLD_LAZY)\n");
+        libeden = __libc_dlopen_mode("libeden.so", RTLD_LAZY);
     }
+    LOG("get_libeden(libeden.so) => %p\n", libeden);
     return libeden;
+}
+
+static void *real_dlmopen(Lmid_t lmid, const char *filename, int flags) {
+    LOG("real_dlmopen(%ld, %s, %d)\n", lmid, filename, flags);
+    static void *(*the_real_dlmopen)(Lmid_t, const char *, int) = NULL;
+    if (the_real_dlmopen == NULL) {
+        LOG("__libc_dlsym(get_libdl(), dlmopen)\n");
+        the_real_dlmopen = __libc_dlsym(get_libdl(), "dlmopen");
+        LOG("__libc_dlsym(get_libdl(), dlmopen) => %p\n", the_real_dlmopen);
+    }
+    void *result = the_real_dlmopen(lmid, filename, flags);
+    LOG("real_dlmopen(%ld, %s, %d) => %p\n", lmid, filename, flags, result);
+    return result;
+}
+
+static void *real_dlopen(const char *filename, int flags) {
+    LOG("real_dlopen(%s, %d)\n", filename, flags);
+    static void *(*the_real_dlopen)(const char *, int) = NULL;
+    if (the_real_dlopen == NULL) {
+        LOG("__libc_dlsym(get_libdl(), dlopen)\n");
+        the_real_dlopen = __libc_dlsym(get_libdl(), "dlopen");
+        LOG("__libc_dlsym(get_libdl(), dlopen) => %p\n", the_real_dlopen);
+    }
+    void *result = the_real_dlopen(filename, flags);
+    LOG("real_dlopen(%s, %d) => %p\n", filename, flags, result);
+    return result;
+}
+
+static int real_dlclose(void *handle) {
+    LOG("real_dlclose(%p)\n", handle);
+    static int (*the_real_dlclose)(void *) = NULL;
+    if (the_real_dlclose == NULL) {
+        LOG("__libc_dlsym(get_libdl(), dlclose)\n");
+        the_real_dlclose = __libc_dlsym(get_libdl(), "dlclose");
+        LOG("__libc_dlsym(get_libdl(), dlclose) => %p\n", the_real_dlclose);
+    }
+    int result = the_real_dlclose(handle);
+    LOG("real_dlclose(%p) => %d\n", handle, result);
+    return result;
+}
+
+static void *real_dlsym(void *handle, const char *symbol) {
+    LOG("real_dlsym(%p, %s)\n", handle, symbol);
+    static void *(*the_real_dlsym)(void *, const char *) = NULL;
+    if (the_real_dlsym == NULL) {
+        LOG("__libc_dlsym(get_libdl(), dlsym)\n");
+        the_real_dlsym = __libc_dlsym(get_libdl(), "dlsym");
+        LOG("__libc_dlsym(get_libdl(), dlsym) => %p\n", the_real_dlsym);
+    }
+    void *result = the_real_dlsym(handle, symbol);
+    LOG("real_dlsym(%p, %s) => %p\n", handle, symbol, result);
+    return result;
+}
+
+void *dlmopen(Lmid_t lmid, const char *filename, int flags) {
+    LOG("dlmopen(%ld, %s, %d)\n", lmid, filename, flags);
+    void *result = real_dlmopen(lmid, filename, flags);
+    LOG("dlmopen(%ld, %s, %d) => %p\n", lmid, filename, flags, result);
+    return result;
 }
 
 static int is_absolute_path(const char *path) {
@@ -91,6 +138,11 @@ static int is_absolute_path(const char *path) {
 }
 
 void *dlopen(const char *filename, int flags) {
+
+    // real_dlopen is never used inside the namespace.
+    NOT_USED(real_dlopen);
+
+    LOG("dlopen(%s, %d)\n", filename, flags);
     if (flags & RTLD_GLOBAL) {
         // dlmopen does not support RTLD_GLOBAL.
         // See https://patchwork.ozlabs.org/project/glibc/patch/55A73673.3060104@redhat.com/
@@ -107,41 +159,29 @@ void *dlopen(const char *filename, int flags) {
     LOG("dlopen => dlmopen: %s\n", filename);
     void * result = real_dlmopen(namespace_id, filename, flags);
     if (result == NULL) {
-        LOG("dlopen(%s, %d): %s\n", filename, flags, dlerror());
+        LOG("dlopen(%s, %d) => error: %s\n", filename, flags, dlerror());
     } else {
         LOG("dlopen(%s, %d) => %p\n", filename, flags, result);
     }
     return result;
 }
 
-static int real_dlclose(void *handle) {
-    static int (*the_real_dlclose)(void *) = NULL;
-    if (the_real_dlclose == NULL) {
-        the_real_dlclose = __libc_dlsym(get_libdl(), "dlclose");
-    }
-    return the_real_dlclose(handle);
-}
-
 int dlclose(void *handle) {
     LOG("dlclose(%p)\n", handle);
-    return real_dlclose(handle);
-}
-
-static void *real_dlsym(void *handle, const char *symbol) {
-    static void *(*the_real_dlsym)(void *, const char *) = NULL;
-    if (the_real_dlsym == NULL) {
-        the_real_dlsym = __libc_dlsym(get_libdl(), "dlsym");
-    }
-    return the_real_dlsym(handle, symbol);
+    int result = real_dlclose(handle);
+    LOG("dlclose(%p) => %d\n", handle, result);
+    return result;
 }
 
 void *dlsym(void *handle, const char *symbol) {
-    void * result = real_dlsym(handle, symbol);
+    LOG("dlsym(%p, %s)\n", handle, symbol);
+    void *result = real_dlsym(handle, symbol);
     LOG("dlsym(%p, %s) => %p\n", handle, symbol, result);
     return result;
 }
 
 static __attribute__((constructor)) void initialize(void) {
+    LOG("initialize()\n");
     const char * mode = getenv("EDEN_DEBUG");
     if (mode != NULL) {
         eden_debug = (strcmp("true", mode) == 0) || (strcmp("1", mode) == 0);
@@ -153,24 +193,28 @@ static __attribute__((constructor)) void initialize(void) {
     if (!namespace_id) {
         LOG("init libeden.so should't be loaded in the default namespace\n");
     }
-    LOG("init &__ctype_b_loc: %p\n", &__ctype_b_loc);
-    LOG("init *__ctype_b_loc() = %p\n", *__ctype_b_loc());
+    LOG("initialize &__ctype_b_loc: %p\n", &__ctype_b_loc);
+    LOG("initialize *__ctype_b_loc() = %p\n", *__ctype_b_loc());
     LOG("Current locale: %s\n", setlocale(LC_ALL, NULL));
 }
 
-#define UNINITIALIZED ((void*) ~0)
-
 void eden_ctypeInit(void) {
+    LOG("eden_ctypeInit()\n");
     static void (*__ctype_init)(void) = UNINITIALIZED;
     if (__ctype_init == UNINITIALIZED) {
-        __ctype_init = dlsym(get_libc(), "__ctype_init");
+        // __libc_dlsym is used here instead of the hooked dlsym to avoid crashes on glibc 2.17.
+        LOG("__libc_dlsym(get_libc(), __ctype_init)\n");
+        __ctype_init = __libc_dlsym(get_libc(), "__ctype_init");
     }
+    LOG("__libc_dlsym(get_libc(), __ctype_init) => %p\n", __ctype_init);
     // Older versions of glibc do not have __ctype_init since they do not use TLS.
     if (__ctype_init != NULL) {
+        LOG("calling __ctype_init");
         __ctype_init();
     }
 }
 
 void* eden_RTLD_DEFAULT(void) {
+    LOG("eden_RTLD_DEFAULT()\n");
     return RTLD_DEFAULT;
 }
