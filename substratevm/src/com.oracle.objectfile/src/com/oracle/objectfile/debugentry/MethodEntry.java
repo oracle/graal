f5450b325d0adf3d666c68b2fff854c423065873
Foivos Zakkak <fzakkak@redhat.com>
2021-06-09 00:43:36 +0300

    Refactor: Simplify MethodEntry::compareTo by generating signature String
    Note: Generating the signatures on demand and not in the constructor
    seems beneficial as it results in producing ~10900 signatures for the
    ~16600 `MethodEntry`s that we generate for the `hello.Hello` example we
    use in debuginfo testing.

--- substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/debugentry/MethodEntry.java
+++ substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/debugentry/MethodEntry.java
@@ -28,6 +28,9 @@ package com.oracle.objectfile.debugentry;
 
 import com.oracle.objectfile.debuginfo.DebugInfoProvider.DebugRangeInfo;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class MethodEntry extends MemberEntry implements Comparable<MethodEntry> {
     final TypeEntry[] paramTypes;
     final String[] paramNames;
@@ -35,6 +38,7 @@ public class MethodEntry extends MemberEntry implements Comparable<MethodEntry> 
     boolean isInRange;
 
     final String symbolName;
+    private String signature;
 
     public MethodEntry(FileEntry fileEntry, String symbolName, String methodName, ClassEntry ownerType,
                     TypeEntry valueType, TypeEntry[] paramTypes, String[] paramNames, int modifiers,
@@ -120,6 +124,13 @@ public class MethodEntry extends MemberEntry implements Comparable<MethodEntry> 
         return symbolName;
     }
 
+    private String getSignature() {
+        if (signature == null) {
+            signature = Arrays.stream(paramTypes).map(TypeEntry::getTypeName).collect(Collectors.joining(", "));
+        }
+        return signature;
+    }
+
     public int compareTo(String methodName, String paramSignature, String returnTypeName) {
         int nameComparison = memberName.compareTo(methodName);
         if (nameComparison != 0) {
@@ -129,24 +140,7 @@ public class MethodEntry extends MemberEntry implements Comparable<MethodEntry> 
         if (typeComparison != 0) {
             return typeComparison;
         }
-        String[] paramTypeNames = paramSignature.split((","));
-        int length;
-        if (paramSignature.trim().length() == 0) {
-            length = 0;
-        } else {
-            length = paramTypeNames.length;
-        }
-        int paramCountComparison = getParamCount() - length;
-        if (paramCountComparison != 0) {
-            return paramCountComparison;
-        }
-        for (int i = 0; i < getParamCount(); i++) {
-            int paraComparison = getParamTypeName(i).compareTo(paramTypeNames[i].trim());
-            if (paraComparison != 0) {
-                return paraComparison;
-            }
-        }
-        return 0;
+        return getSignature().compareTo(paramSignature);
     }
 
     @Override
@@ -160,16 +154,6 @@ public class MethodEntry extends MemberEntry implements Comparable<MethodEntry> 
         if (typeComparison != 0) {
             return typeComparison;
         }
-        int paramCountComparison = getParamCount() - other.getParamCount();
-        if (paramCountComparison != 0) {
-            return paramCountComparison;
-        }
-        for (int i = 0; i < getParamCount(); i++) {
-            int paramComparison = getParamTypeName(i).compareTo(other.getParamTypeName(i));
-            if (paramComparison != 0) {
-                return paramComparison;
-            }
-        }
-        return 0;
+        return getSignature().compareTo(other.getSignature());
     }
 }
