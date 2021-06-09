7c36816889bb1b0a6b6d9d78add3c2aa4fe8a7eb
Loic Ottet <loic.ottet@oracle.com>
2021-06-09 10:16:35 +0200

    Print image output to stdout for build statistics

--- vm/mx.vm/mx_vm_benchmark.py
+++ vm/mx.vm/mx_vm_benchmark.py
@@ -374,7 +374,7 @@ class NativeImageVM(GraalVm):
             return self
 
         def execute_command(self, vm=None):
-            write_output = self.current_stage == 'run' or self.is_gate
+            write_output = self.current_stage == 'run' or self.current_stage == 'image' or self.is_gate
             cmd = self.command
             self.exit_code = self.config.bmSuite.run_stage(vm, self.current_stage, cmd, self.stdout(write_output), self.stderr(write_output), self.cwd, False)
             if "image" not in self.current_stage and self.config.bmSuite.validateReturnCode(self.exit_code):
@@ -557,7 +557,7 @@ class NativeImageVM(GraalVm):
         pgo_args += ['-H:+AOTInliner'] if self.pgo_aot_inline else ['-H:-AOTInliner']
         final_image_command = config.base_image_build_args + executable_name_args + (pgo_args if self.pgo_instrumented_iterations > 0 or self.hotspot_pgo else [])
         with stages.set_command(final_image_command) as s:
-            s.execute_command(True)
+            s.execute_command()
 
     def run_stage_run(self, config, stages, out):
         image_path = os.path.join(config.output_dir, config.final_image_name)
