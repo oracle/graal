37c51f39c13125b00df4f9588db9318253c182d6
Christian Wimmer <christian.wimmer@oracle.com>
2021-06-17 23:20:50 -0700

    Track individual classes that need to be processed for reflection

--- substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/ReflectionDataBuilder.java
+++ substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/ReflectionDataBuilder.java
@@ -32,7 +32,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -62,7 +61,7 @@ public class ReflectionDataBuilder implements RuntimeReflectionSupport {
     public static final Constructor<?>[] EMPTY_CONSTRUCTORS = new Constructor<?>[0];
     public static final Class<?>[] EMPTY_CLASSES = new Class<?>[0];
 
-    private boolean modified;
+    private final Set<Class<?>> modifiedClasses = ConcurrentHashMap.newKeySet();
     private boolean sealed;
 
     private final DynamicHub.ReflectionData arrayReflectionData;
@@ -110,16 +109,20 @@ public class ReflectionDataBuilder implements RuntimeReflectionSupport {
     @Override
     public void register(Class<?>... classes) {
         checkNotSealed();
-        if (reflectionClasses.addAll(Arrays.asList(classes))) {
-            modified = true;
+        for (Class<?> clazz : classes) {
+            if (reflectionClasses.add(clazz)) {
+                modifiedClasses.add(clazz);
+            }
         }
     }
 
     @Override
     public void register(Executable... methods) {
         checkNotSealed();
-        if (reflectionMethods.addAll(Arrays.asList(methods))) {
-            modified = true;
+        for (Executable method : methods) {
+            if (reflectionMethods.add(method)) {
+                modifiedClasses.add(method.getDeclaringClass());
+            }
         }
     }
 
@@ -127,8 +130,10 @@ public class ReflectionDataBuilder implements RuntimeReflectionSupport {
     public void register(boolean finalIsWritable, Field... fields) {
         checkNotSealed();
         // Unsafe and write accesses are always enabled for fields because accessors use Unsafe.
-        if (reflectionFields.addAll(Arrays.asList(fields))) {
-            modified = true;
+        for (Field field : fields) {
+            if (reflectionFields.add(field)) {
+                modifiedClasses.add(field.getDeclaringClass());
+            }
         }
     }
 
@@ -187,17 +192,15 @@ public class ReflectionDataBuilder implements RuntimeReflectionSupport {
     }
 
     private void processRegisteredElements(DuringAnalysisAccessImpl access) {
-        if (!modified) {
+        if (modifiedClasses.isEmpty()) {
             return;
         }
-        modified = false;
         access.requireAnalysisIteration();
 
-        Set<Class<?>> allClasses = new HashSet<>(reflectionClasses);
-        reflectionMethods.stream().map(Executable::getDeclaringClass).forEach(allClasses::add);
-        reflectionFields.stream().map(Field::getDeclaringClass).forEach(allClasses::add);
-
-        allClasses.forEach(clazz -> processClass(access, clazz));
+        for (Class<?> clazz : modifiedClasses) {
+            processClass(access, clazz);
+        }
+        modifiedClasses.clear();
     }
 
     private void processClass(DuringAnalysisAccessImpl access, Class<?> clazz) {
@@ -324,7 +327,7 @@ public class ReflectionDataBuilder implements RuntimeReflectionSupport {
 
     protected void afterAnalysis() {
         sealed = true;
-        if (modified) {
+        if (!modifiedClasses.isEmpty()) {
             throw UserError.abort("Registration of classes, methods, and fields for reflective access during analysis must set DuringAnalysisAccess.requireAnalysisIteration().");
         }
     }
