cf802df003eb65ecde711af7217059b719095273
Aleksandar Gradinac <aleksandar.gradinac@oracle.com>
2021-06-21 12:45:27 +0200

    Fix resource inclusion on Windows

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/ResourcesFeature.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/ResourcesFeature.java
@@ -259,6 +259,11 @@ public final class ResourcesFeature implements Feature {
             String relativeFilePath = "";
             if (file != root) {
                 relativeFilePath = file.getAbsolutePath().substring(root.getAbsolutePath().length() + 1);
+                /*
+                 * Java resources always use / as the path separator, as do our resource inclusion
+                 * patterns.
+                 */
+                relativeFilePath = relativeFilePath.replace(File.separatorChar, '/');
             }
             if (file.isDirectory()) {
                 if (!relativeFilePath.isEmpty()) {
