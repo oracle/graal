223c68691e703481817f3257cb300415717da6f8
Felix Berlakovich <felix.berlakovich@oracle.com>
2021-06-21 16:36:00 +0200

    Add LIRGenerator method for guaranteed bitwise XOR of two values
    patch

--- compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64ArithmeticLIRGenerator.java
+++ compiler/src/org.graalvm.compiler.core.aarch64/src/org/graalvm/compiler/core/aarch64/AArch64ArithmeticLIRGenerator.java
@@ -40,6 +40,7 @@ import org.graalvm.compiler.core.common.NumUtil;
 import org.graalvm.compiler.core.common.calc.FloatConvert;
 import org.graalvm.compiler.core.common.spi.ForeignCallLinkage;
 import org.graalvm.compiler.debug.GraalError;
+import org.graalvm.compiler.lir.CastValue;
 import org.graalvm.compiler.lir.ConstantValue;
 import org.graalvm.compiler.lir.LIRFrameState;
 import org.graalvm.compiler.lir.Variable;
@@ -211,6 +212,29 @@ public class AArch64ArithmeticLIRGenerator extends ArithmeticLIRGenerator implem
     public Value emitXor(Value a, Value b) {
         assert isNumericInteger(a.getPlatformKind());
         return emitBinary(LIRKind.combine(a, b), AArch64ArithmeticOp.XOR, true, a, b);
+    }
+
+    @Override
+    public Value emitXorFP(Value a, Value b) {
+        assert (a.getPlatformKind() == AArch64Kind.SINGLE || a.getPlatformKind() == AArch64Kind.DOUBLE) &&
+                        a.getPlatformKind() == b.getPlatformKind();
+
+        // AArch64 does not support direct XOR of FP numbers. We need to cast them to the correct
+        // type for the XOR operation.
+        LIRKind calculationKind = LIRKind.combine(a, b);
+        LIRKind resultKind = calculationKind;
+        if (a.getPlatformKind() == AArch64Kind.SINGLE) {
+            calculationKind = LIRKind.value(AArch64Kind.V32_BYTE);
+        } else if (a.getPlatformKind() == AArch64Kind.DOUBLE) {
+            calculationKind = LIRKind.value(AArch64Kind.V64_BYTE);
+        }
+        Variable result = getLIRGen().newVariable(calculationKind);
+
+        CastValue castA = new CastValue(calculationKind, asAllocatable(a));
+        CastValue castB = new CastValue(calculationKind, asAllocatable(b));
+        getLIRGen().append(AArch64ArithmeticOp.generateASIMDBinaryInstruction(AArch64ArithmeticOp.XOR, result, castA, castB));
+
+        return new CastValue(resultKind, result);
     }
 
     @Override
