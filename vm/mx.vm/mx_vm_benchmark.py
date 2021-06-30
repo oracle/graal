e77167b6965a69132c9337dc5e9fcfd1c06b3397
Josef Eisl <josef.eisl@oracle.com>
2021-06-30 13:18:56 +0200

    svm/benchmark: make NativeImageVM config safer

--- vm/mx.vm/mx_vm_benchmark.py
+++ vm/mx.vm/mx_vm_benchmark.py
@@ -123,10 +123,11 @@ class NativeImageVM(GraalVm):
             self.benchmark_name = bm_suite.benchmarkName()
             self.executable, self.classpath_arguments, self.system_properties, cmd_line_image_run_args = NativeImageVM.extract_benchmark_arguments(args)
             self.extra_image_build_arguments = bm_suite.extra_image_build_argument(self.benchmark_name, args)
-            self.image_run_args = bm_suite.extra_run_arg(self.benchmark_name, args, cmd_line_image_run_args)
-            self.extra_agent_run_args = bm_suite.extra_agent_run_arg(self.benchmark_name, args, cmd_line_image_run_args)
-            self.extra_profile_run_args = bm_suite.extra_profile_run_arg(self.benchmark_name, args, cmd_line_image_run_args)
-            self.extra_agent_profile_run_args = bm_suite.extra_agent_profile_run_arg(self.benchmark_name, args, cmd_line_image_run_args)
+            # use list() to create fresh copies to safeguard against accidental modification
+            self.image_run_args = bm_suite.extra_run_arg(self.benchmark_name, args, list(cmd_line_image_run_args))
+            self.extra_agent_run_args = bm_suite.extra_agent_run_arg(self.benchmark_name, args, list(cmd_line_image_run_args))
+            self.extra_profile_run_args = bm_suite.extra_profile_run_arg(self.benchmark_name, args, list(cmd_line_image_run_args))
+            self.extra_agent_profile_run_args = bm_suite.extra_agent_profile_run_arg(self.benchmark_name, args, list(cmd_line_image_run_args))
             self.benchmark_output_dir = bm_suite.benchmark_output_dir(self.benchmark_name, args)
             self.pgo_iteration_num = None
             self.params = ['extra-image-build-argument', 'extra-run-arg', 'extra-agent-run-arg', 'extra-profile-run-arg',
