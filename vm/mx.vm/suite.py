220df3ab04aad1b6d9ed91879da7ffcb8881782a
Jan Stola <jan.stola@oracle.com>
2021-07-01 05:04:48 +0000

    [GR-30232] Date.prototype built-ins should use ICU (algorithms and data - like timezone data).
    PullRequest: js/2036

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a01bc5413ecb85501ef067b71351836a1f50bca0",
+                "version": "f2cada078663f617c35c8b37cc438ba885e7639b",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a01bc5413ecb85501ef067b71351836a1f50bca0",
+                "version": "f2cada078663f617c35c8b37cc438ba885e7639b",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
