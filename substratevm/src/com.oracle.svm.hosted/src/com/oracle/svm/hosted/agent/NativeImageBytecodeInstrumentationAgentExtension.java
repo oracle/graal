2216b771254dd6c2684844788fb96a340bcc0ce4
Paul Wögerer <paul.woegerer@oracle.com>
2021-07-12 17:18:44 +0200

    Add minimal JavaDoc for NativeImageBytecodeInstrumentationAgentExtension service

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/agent/NativeImageBytecodeInstrumentationAgentExtension.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/agent/NativeImageBytecodeInstrumentationAgentExtension.java
@@ -2,6 +2,14 @@ package com.oracle.svm.hosted.agent;
 
 import java.lang.instrument.Instrumentation;
 
+/**
+ * Defines a service for specifying additional ClassInstrumentationTransformers.
+ *
+ * {@link NativeImageBytecodeInstrumentationAgent} uses
+ * {@code ServiceLoader.load(NativeImageBytecodeInstrumentationAgentExtension.class)} to register
+ * additional transformers. This is used to specify transformers that should only be used when
+ * running the agent on JDK8.
+ */
 public interface NativeImageBytecodeInstrumentationAgentExtension {
 
     void addClassInstrumentationTransformer(Instrumentation inst);
