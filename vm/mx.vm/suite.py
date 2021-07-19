a04331300baee3c6288a3c26228989dd4ae0bd81
Taha BASRI <taha.basri@oracle.com>
2021-07-19 18:02:46 +0100

    [GR-31643] Release GraalVM 20.3.3.

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -2,7 +2,7 @@ suite = {
     "name": "vm",
     "version" : "20.3.3",
     "mxversion" : "5.256.0",
-    "release" : False,
+    "release" : True,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "35d52de4175ae48253b40fb9d3810941d848045c",
+                "version": "06be9659b4264c5801293ab68bf73947c37cccd2",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "35d52de4175ae48253b40fb9d3810941d848045c",
+                "version": "06be9659b4264c5801293ab68bf73947c37cccd2",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -75,7 +75,7 @@ suite = {
             },
             {
                 "name": "graalpython",
-                "version": "ee6ffac91230168310d724254fe080c38bde6b8c",
+                "version": "1bdde5f2b34f133df4a1ee9f1e8c0b7888c63999",
                 "dynamic": True,
                 "urls": [
                     {"url": "https://github.com/graalvm/graalpython.git", "kind": "git"},
