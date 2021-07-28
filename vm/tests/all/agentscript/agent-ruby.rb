c12fbf0d03e810e1e0fd62e2274a86b49fec8f9d
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2021-07-28 17:11:24 +0200

    Only log loading of sources with well known name

--- vm/tests/all/agentscript/agent-ruby.rb
+++ vm/tests/all/agentscript/agent-ruby.rb
@@ -1,7 +1,9 @@
 puts("Ruby: Insight version " + insight[:version] + " is launching")
 
-insight.on("source", -> (env) { 
-  puts "Ruby: observed loading of " + env[:name]
+insight.on("source", -> (env) {
+  if env[:name].index('agent-fib') or env[:name].end_with?('test.rb')
+    puts "Ruby: observed loading of " + env[:name]
+  end
 })
 puts("Ruby: Hooks are ready!")
 
