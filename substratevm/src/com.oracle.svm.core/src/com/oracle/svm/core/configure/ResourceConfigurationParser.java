b24db311d974b5259feb7a0bfc36583759adbb65
David Kozak <david.kozak@oracle.com>
2021-08-12 15:33:31 +0200

    only register bundle in all locales if no specific locales or classnames are provided

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/ResourceConfigurationParser.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/ResourceConfigurationParser.java
@@ -113,8 +113,6 @@ public class ResourceConfigurationParser extends ConfigurationParser {
                             .collect(Collectors.toList());
             UserError.guarantee(!asList.isEmpty(), "List of locales for %s is empty", basename);
             registry.addResourceBundles(basename, asList);
-        } else {
-            registry.addResourceBundles(basename);
         }
         Object classNames = resource.get("classNames");
         if (classNames != null) {
@@ -124,6 +122,10 @@ public class ResourceConfigurationParser extends ConfigurationParser {
                 String className = asString(o, "Elements of 'classNames' must of strings.");
                 registry.addClassBasedResourceBundle(className);
             }
+        }
+        if (locales == null && classNames == null) {
+            // register in every locale
+            registry.addResourceBundles(basename);
         }
     }
 
