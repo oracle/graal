c28ac9f87325616ce6e60d66bf4a2e7061adbdbc
Christian Humer <christian.humer@oracle.com>
2021-08-13 17:32:42 +0200

    Fix failing assertion.

--- tools/src/com.oracle.truffle.tools.profiler/src/com/oracle/truffle/tools/profiler/SafepointStackSampler.java
+++ tools/src/com.oracle.truffle.tools.profiler/src/com/oracle/truffle/tools/profiler/SafepointStackSampler.java
@@ -271,14 +271,12 @@ final class SafepointStackSampler {
             if (useSyntheticFrames) {
                 SyntheticFrame syntheticFrame = syntheticFrameThreadLocal.get();
                 if (syntheticFrame != null) {
-                    assert !completed.containsKey(access.getThread());
                     completed.put(access.getThread(), syntheticFrame);
                     return;
                 }
             }
             StackVisitor visitor = fetchStackVisitor();
             visitor.iterateFrames();
-            assert !completed.containsKey(access.getThread());
             completed.put(access.getThread(), visitor);
         }
 
