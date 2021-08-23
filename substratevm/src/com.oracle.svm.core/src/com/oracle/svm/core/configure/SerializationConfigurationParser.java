Wesley-yang/graal-1

Relax the configuration format and report unknown attributes once per configuration object.

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/SerializationConfigurationParser.java
@@ -48,13 +48,16 @@ public class SerializationConfigurationParser extends ConfigurationParser {
         JSONParser parser = new JSONParser(reader);
         Object json = parser.parse();
         for (Object serializationKey : asList(json, "first level of document must be an array of serialization lists")) {
-            Map<String, Object> data = asMap(serializationKey, "second level of document must be serialization descriptor objects ");
-            checkAttributes(data, "serialization descriptor object", Collections.singleton(NAME_KEY));
-            String targetSerializationClass = asString(data.get(NAME_KEY));
-            Object optionalCustomCtorValue = data.get(CUSTOM_TARGET_CONSTRUCTOR_CLASS_KEY);
-            String customTargetConstructorClass = optionalCustomCtorValue != null ? asString(optionalCustomCtorValue) : null;
-            consumer.accept(targetSerializationClass, customTargetConstructorClass);
+            parseSerializationDescriptorObject(asMap(serializationKey, "second level of document must be serialization descriptor objects"));
         }
+    }
+
+    private void parseSerializationDescriptorObject(Map<String, Object> data) {
+        checkAttributes(data, "serialization descriptor object", Collections.singleton(NAME_KEY), Collections.singleton(CUSTOM_TARGET_CONSTRUCTOR_CLASS_KEY));
+        String targetSerializationClass = asString(data.get(NAME_KEY));
+        Object optionalCustomCtorValue = data.get(CUSTOM_TARGET_CONSTRUCTOR_CLASS_KEY);
+        String customTargetConstructorClass = optionalCustomCtorValue != null ? asString(optionalCustomCtorValue) : null;
+        consumer.accept(targetSerializationClass, customTargetConstructorClass);
     }
 
     @FunctionalInterface

oracle/graal

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/SerializationConfigurationParser.java
@@ -49,12 +49,15 @@ public class SerializationConfigurationParser extends ConfigurationParser {
         JSONParser parser = new JSONParser(reader);
         Object json = parser.parse();
         for (Object serializationKey : asList(json, "first level of document must be an array of serialization lists")) {
-            Map<String, Object> data = asMap(serializationKey, "second level of document must be serialization descriptor objects ");
-            checkAttributes(data, "serialization descriptor object", Collections.singleton(NAME_KEY));
-            String targetSerializationClass = asString(data.get(NAME_KEY));
-            Object optionalCustomCtorValue = data.get(CUSTOM_TARGET_CONSTRUCTOR_CLASS_KEY);
-            String customTargetConstructorClass = optionalCustomCtorValue != null ? asString(optionalCustomCtorValue) : null;
-            serializationSupport.registerWithTargetConstructorClass(targetSerializationClass, customTargetConstructorClass);
+            parseSerializationDescriptorObject(asMap(serializationKey, "second level of document must be serialization descriptor objects"));
         }
+    }
+
+    private void parseSerializationDescriptorObject(Map<String, Object> data) {
+        checkAttributes(data, "serialization descriptor object", Collections.singleton(NAME_KEY), Collections.singleton(CUSTOM_TARGET_CONSTRUCTOR_CLASS_KEY));
+        String targetSerializationClass = asString(data.get(NAME_KEY));
+        Object optionalCustomCtorValue = data.get(CUSTOM_TARGET_CONSTRUCTOR_CLASS_KEY);
+        String customTargetConstructorClass = optionalCustomCtorValue != null ? asString(optionalCustomCtorValue) : null;
+        serializationSupport.registerWithTargetConstructorClass(targetSerializationClass, customTargetConstructorClass);
     }
 }