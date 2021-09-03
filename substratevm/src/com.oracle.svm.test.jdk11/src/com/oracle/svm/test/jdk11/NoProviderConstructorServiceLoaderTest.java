5f4403fe604f5d58b622eddedb693d8f488037ae
Christian Wimmer <christian.wimmer@oracle.com>
2021-09-03 15:26:17 -0700

    Fix checkstyle violations

--- substratevm/src/com.oracle.svm.test.jdk11/src/com/oracle/svm/test/jdk11/NoProviderConstructorServiceLoaderTest.java
+++ substratevm/src/com.oracle.svm.test.jdk11/src/com/oracle/svm/test/jdk11/NoProviderConstructorServiceLoaderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -63,7 +63,7 @@ public class NoProviderConstructorServiceLoaderTest {
     public static class ProperService implements ServiceInterface {
     }
 
-    public static abstract class NoProviderConstructorService implements ServiceInterface {
+    public abstract static class NoProviderConstructorService implements ServiceInterface {
         /**
          * Not a provider constructor. This violates the contract of {@link ServiceLoader}.
          */
