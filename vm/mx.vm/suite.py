36cfbfa27096003482d11865ed5845ecd6f0d4cb
Jan Stola <jan.stola@oracle.com>
2021-09-06 15:35:33 +0000

    [GR-33313] JSRealm should be initialized before it is passed to onLanguageContextInitialized().
    PullRequest: js/2129

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "453bfed89537508473981add97647ab9dc27842f",
+                "version": "42a0ba1f59b81a6e2bbcda0d0d10c15cd48fbe6b",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "453bfed89537508473981add97647ab9dc27842f",
+                "version": "42a0ba1f59b81a6e2bbcda0d0d10c15cd48fbe6b",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
