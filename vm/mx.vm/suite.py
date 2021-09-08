ec2efa31f1236578c89428d3fcbcef48795b5819
Andreas Woess <andreas.woess@oracle.com>
2021-09-08 00:56:35 +0000

    [GR-33313] Ensure frame is materialized in getScope.
    PullRequest: js/2130

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "42a0ba1f59b81a6e2bbcda0d0d10c15cd48fbe6b",
+                "version": "66faeb56dd170e65e018de522069cfd826eb79f4",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "42a0ba1f59b81a6e2bbcda0d0d10c15cd48fbe6b",
+                "version": "66faeb56dd170e65e018de522069cfd826eb79f4",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
