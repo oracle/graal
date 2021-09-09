4ce092fe78bacd88b3edd6e9a8eb21325a8a4330
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2021-09-09 10:03:34 +0200

    Fixes for serialization and reflection features

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/ReflectionConfigurationParser.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/configure/ReflectionConfigurationParser.java
@@ -86,6 +86,7 @@ public final class ReflectionConfigurationParser<T> extends ConfigurationParser 
         TypeResult<ConfigurationCondition> conditionResult = delegate.resolveCondition(parseCondition(data).getTypeName());
         if (!conditionResult.isPresent()) {
             handleError("Could not resolve condition " + parseCondition(data).getTypeName() + " for reflection.", conditionResult.getException());
+            return;
         }
         ConfigurationCondition condition = conditionResult.get();
 
