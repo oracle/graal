e0b4f4535bef624096382ee1974488015ac6c3f4
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2021-09-22 06:08:16 +0000

    [GR-33413] Next dev cycle.
    PullRequest: js/2153

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "35a10cf868b801c69651155c2dd3306405c9975f",
+                "version": "5d6e27792ff4814ce47ff49f7b9845231e2048ec",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "35a10cf868b801c69651155c2dd3306405c9975f",
+                "version": "5d6e27792ff4814ce47ff49f7b9845231e2048ec",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
