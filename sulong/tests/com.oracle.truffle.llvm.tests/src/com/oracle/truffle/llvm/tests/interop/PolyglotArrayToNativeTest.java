449042237ddbbe3b25ac9a88d52eb1a7465a7c69
Florian Angerer <florian.angerer@oracle.com>
2021-09-24 10:49:51 +0200

    Fix style and copyright.

--- sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/PolyglotArrayToNativeTest.java
+++ sulong/tests/com.oracle.truffle.llvm.tests/src/com/oracle/truffle/llvm/tests/interop/PolyglotArrayToNativeTest.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.oracle.truffle.llvm.tests.interop;
 
 import org.graalvm.polyglot.Value;
@@ -44,7 +73,7 @@ public class PolyglotArrayToNativeTest extends PolyglotArrayTestBase {
     @ExportLibrary(InteropLibrary.class)
     @ExportLibrary(value = NativeTypeLibrary.class, useForAOT = false)
     @SuppressWarnings("static-method")
-    static class SimpleWrapper implements TruffleObject {
+    static final class SimpleWrapper implements TruffleObject {
         private final long delegate;
 
         private SimpleWrapper(long l) {
@@ -104,7 +133,7 @@ public class PolyglotArrayToNativeTest extends PolyglotArrayTestBase {
     @ExportLibrary(value = NativeTypeLibrary.class, useForAOT = false)
     @ExportLibrary(InteropLibrary.class)
     @SuppressWarnings("static-method")
-    static class SimpleArrayWrapper implements TruffleObject {
+    static final class SimpleArrayWrapper implements TruffleObject {
         private final SimpleWrapper[] objects;
 
         private SimpleArrayWrapper(SimpleWrapper[] objects) {
