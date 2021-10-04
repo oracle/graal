6fed128a9b8494c99698d4e03bcf60de2d9102d3
Andreas Woess <andreas.woess@oracle.com>
2021-10-04 20:25:43 +0000

    [GR-23997] Periodic update of the graal import (2021-10-01).
    PullRequest: js/2175

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "cfd51cdf034ed67678574ad99b957cb7cb752c31",
+                "version": "082c333db671f03aba82d15e6d572e196b3878ea",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "cfd51cdf034ed67678574ad99b957cb7cb752c31",
+                "version": "082c333db671f03aba82d15e6d572e196b3878ea",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
