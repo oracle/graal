50e19544930bcfe0d6117cb3ccb7764d090a0c87
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2021-10-07 09:31:45 +0200

    Espresso: mark 17 as supported in version metadata

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoLanguage.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/EspressoLanguage.java
@@ -73,7 +73,7 @@ public final class EspressoLanguage extends TruffleLanguage<EspressoContext> {
     public static final String ID = "java";
     public static final String NAME = "Java";
     public static final String IMPLEMENTATION_NAME = "Espresso";
-    public static final String VERSION = "1.8|11";
+    public static final String VERSION = "1.8|11|17";
 
     // Espresso VM info
     public static final String VM_SPECIFICATION_NAME = "Java Virtual Machine Specification";
