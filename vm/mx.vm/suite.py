fd9e929c00bd66d36ae8ac828a9519041175b12f
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2021-10-08 07:58:53 +0000

    [GR-34219] [GR-34235] Backport: Instrumentation-related fixes.
    PullRequest: js/2186

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "9145ce7b16846cef4db43c19b55f5f053220ec10",
+                "version": "e15520b1731df3c67628e49bb7dc58b8152cf13e",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "9145ce7b16846cef4db43c19b55f5f053220ec10",
+                "version": "e15520b1731df3c67628e49bb7dc58b8152cf13e",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
