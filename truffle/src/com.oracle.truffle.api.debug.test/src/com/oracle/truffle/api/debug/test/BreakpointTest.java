454ef132877e8dbe2f303821b287355874d6faba
Martin Entlicher <martin.entlicher@oracle.com>
2021-11-05 23:46:05 +0100

    Resolve breakpoint to the next location if out of RootNode, but on the same line as the next location. (GR-34678)
    (cherry picked from commit b75b51b75658750a9c39a7eb46244207221b18b3)

--- truffle/src/com.oracle.truffle.api.debug.test/src/com/oracle/truffle/api/debug/test/BreakpointTest.java
+++ truffle/src/com.oracle.truffle.api.debug.test/src/com/oracle/truffle/api/debug/test/BreakpointTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -1190,6 +1190,15 @@ public class BreakpointTest extends AbstractDebugTest {
         tester.assertColumnBreakpointsResolution(source, "B", "R", InstrumentablePositionsTestLanguage.ID);
         tester.close();
         tester = new DebuggerTester(org.graalvm.polyglot.Context.newBuilder().allowExperimentalOptions(true).option(InstrumentablePositionsTestLanguage.ID + ".PreMaterialize", "2"));
+        tester.assertColumnBreakpointsResolution(source, "B", "R", InstrumentablePositionsTestLanguage.ID);
+    }
+
+    @Test
+    public void testOutOfRootBreakpointPositions() {
+        String source = "  \n" +
+                        " B1_     < {F R1_[SB]\n" +
+                        "B2_R2_{S}  } \n" +
+                        "\n";
         tester.assertColumnBreakpointsResolution(source, "B", "R", InstrumentablePositionsTestLanguage.ID);
     }
 
