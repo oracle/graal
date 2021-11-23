b37568f04f6a6b3fddb434dcb36b4e7296a4f685
Tom Shull <tom.shull@oracle.com>
2021-11-23 15:07:21 +0100

    Prevent unneeded zero checks on arithmetic snippets.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/snippets/ArithmeticSnippets.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/snippets/ArithmeticSnippets.java
@@ -233,7 +233,8 @@ public abstract class ArithmeticSnippets extends SubstrateTemplates implements S
             args.add("y", node.getY());
 
             IntegerStamp yStamp = (IntegerStamp) node.getY().stamp(NodeView.DEFAULT);
-            args.addConst("needsZeroCheck", node.getZeroCheck() == null && yStamp.contains(0));
+            boolean needsZeroCheck = node.canDeoptimize() && (node.getZeroCheck() == null && yStamp.contains(0));
+            args.addConst("needsZeroCheck", needsZeroCheck);
 
             template(node, args).instantiate(providers.getMetaAccess(), node, SnippetTemplate.DEFAULT_REPLACER, args);
         }
