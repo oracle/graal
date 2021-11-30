612bab4a63519ee21a9a6c4f4dec3bcb7bf5ec98
Fengyun Liu <fengyun.liu@oracle.com>
2021-11-30 12:04:55 +0100

    Use correctly packed asset

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -163,8 +163,8 @@ suite = {
         # Note: small warmup benchmarks can be placed directly under `graal/vm/benchmarks/warmup`
         # and uncomment the corresponding line for the `layout` of `POLYBENCH_BENCHMARKS` in current suite.
         "WARMUP_BENCHMARKS" : {
-            "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/polybench/warmup-benchmarks-0.2.tar.gz"],
-            "sha1" : "29908a8b877f9aeb963577bef516341f2eb0a082"
+            "urls" : ["https://lafo.ssw.uni-linz.ac.at/pub/graal-external-deps/polybench/warmup-benchmarks-0.3.tar.gz"],
+            "sha1" : "4e23a980be3cb92a183efe7d6d3354438134a55f"
         }
     },
 
