18136f01c1cb4128842a3452533a9a2fe8fd70ea
Allan Gregersen <allan.gregersen@oracle.com>
2021-12-07 08:15:59 +0100

    eagerly load new super classes and implemented interfaces if needed before redefining subclass

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/redefinition/ClassRedefinition.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/redefinition/ClassRedefinition.java
@@ -183,7 +183,7 @@ public final class ClassRedefinition {
         }
     }
 
-    public List<ChangePacket> detectClassChanges(HotSwapClassInfo[] classInfos) {
+    public List<ChangePacket> detectClassChanges(HotSwapClassInfo[] classInfos) throws RedefintionNotSupportedException {
         List<ChangePacket> result = new ArrayList<>(classInfos.length);
         for (HotSwapClassInfo hotSwapInfo : classInfos) {
             ObjectKlass klass = hotSwapInfo.getKlass();
@@ -258,19 +258,25 @@ public final class ClassRedefinition {
 
     // detect all types of class changes, but return early when a change that require arbitrary
     // changes
-    private static ClassChange detectClassChanges(ParserKlass newParserKlass, ObjectKlass oldKlass, DetectedChange collectedChanges, ParserKlass finalParserKlass) {
+    private static ClassChange detectClassChanges(ParserKlass newParserKlass, ObjectKlass oldKlass, DetectedChange collectedChanges, ParserKlass finalParserKlass) throws RedefintionNotSupportedException {
         ClassChange result = ClassChange.NO_CHANGE;
         ParserKlass oldParserKlass = oldKlass.getLinkedKlass().getParserKlass();
         boolean isPatched = finalParserKlass != null;
 
         // detect changes to superclass and implemented interfaces
         if (!newParserKlass.getSuperKlass().equals(oldParserKlass.getSuperKlass())) {
-            Klass loadedKlass = oldKlass.getContext().getRegistries().findLoadedClass(newParserKlass.getSuperKlass(), oldKlass.getDefiningClassLoader());
-            if (loadedKlass != null) {
-                collectedChanges.addSuperKlass((ObjectKlass) loadedKlass);
-            } else {
-                throw new RuntimeException("Not yet implemented!");
+            Klass loadedSuperKlass = oldKlass.getContext().getRegistries().findLoadedClass(newParserKlass.getSuperKlass(), oldKlass.getDefiningClassLoader());
+            if (loadedSuperKlass == null) {
+                // new super interface must be loaded eagerly then
+                StaticObject resourceGuestString = oldKlass.getMeta().toGuestString(Types.binaryName(newParserKlass.getSuperKlass()));
+                try {
+                    StaticObject loadedClass = (StaticObject) oldKlass.getMeta().java_lang_ClassLoader_loadClass.invokeDirect(oldKlass.getDefiningClassLoader(), resourceGuestString);
+                    loadedSuperKlass = loadedClass.getMirrorKlass();
+                } catch (Throwable t) {
+                    throw new RedefintionNotSupportedException(ErrorCodes.ABSENT_INFORMATION);
+                }
             }
+            collectedChanges.addSuperKlass((ObjectKlass) loadedSuperKlass);
         } else {
             collectedChanges.addSuperKlass(oldKlass.getSuperKlass());
         }
@@ -279,20 +285,21 @@ public final class ClassRedefinition {
             for (int i = 0; i < newParserKlass.getSuperInterfaces().length; i++) {
                 Symbol<Symbol.Type> superInterfaceType = newParserKlass.getSuperInterfaces()[i];
                 Klass loadedInterface = oldKlass.getContext().getRegistries().findLoadedClass(superInterfaceType, oldKlass.getDefiningClassLoader());
-                if (loadedInterface != null) {
-                    newSuperInterfaces[i] = (ObjectKlass) loadedInterface;
-                } else {
-                    throw new RuntimeException("Not yet implemented!");
+                if (loadedInterface == null) {
+                    // new super interface must be loaded eagerly then
+                    StaticObject resourceGuestString = oldKlass.getMeta().toGuestString(Types.binaryName(superInterfaceType));
+                    try {
+                        StaticObject loadedSuperInterface = (StaticObject) oldKlass.getMeta().java_lang_ClassLoader_loadClass.invokeDirect(oldKlass.getDefiningClassLoader(), resourceGuestString);
+                        loadedInterface = loadedSuperInterface.getMirrorKlass();
+                    } catch (Throwable t) {
+                        throw new RedefintionNotSupportedException(ErrorCodes.ABSENT_INFORMATION);
+                    }
                 }
+                newSuperInterfaces[i] = (ObjectKlass) loadedInterface;
             }
             collectedChanges.addSuperInterfaces(newSuperInterfaces);
         } else {
             collectedChanges.addSuperInterfaces(oldKlass.getSuperInterfaces());
-        }
-
-        if (!oldKlass.isInitialized() && oldKlass.isInterface() && !Modifier.isInterface(newParserKlass.getFlags())) {
-            // interface changed to concrete or abstract class
-            //oldKlass.initialize();
         }
 
         // detect method changes (including constructors)
