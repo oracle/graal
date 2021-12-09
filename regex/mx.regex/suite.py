fcfe37ed058e06c3d9b32b93ce669c6c0e5762d7
Mohaned Ez-zarghili <mohamed.ez.zarghili@oracle.com>
2021-12-09 11:07:01 +0100

    Revert "one-Off release 20.3.4.1"
    This reverts commit bf32b4af16b5af974a936258a28fd80bf8a35914.

--- regex/mx.regex/suite.py
+++ regex/mx.regex/suite.py
@@ -43,8 +43,8 @@ suite = {
 
   "name" : "regex",
 
-  "version" : "20.3.4.1",
-  "release" : True,
+  "version" : "20.3.5",
+  "release" : False,
   "groupId" : "org.graalvm.regex",
   "url" : "http://www.graalvm.org/",
   "developer" : {
--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -1,8 +1,8 @@
 suite = {
     "name": "vm",
-    "version" : "20.3.4.1",
+    "version" : "20.3.5",
     "mxversion" : "5.256.0",
-    "release" : True,
+    "release" : False,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "6168b57364c9c8f882136031220ca4825a17e5bd",
+                "version": "3217126769a47aae1af43dfce9a648bbac244e4b",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "6168b57364c9c8f882136031220ca4825a17e5bd",
+                "version": "3217126769a47aae1af43dfce9a648bbac244e4b",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
