e03abf2a4c7d4fcf34cbfad55d8218c499c8acfa
Andreas Woess <andreas.woess@oracle.com>
2021-12-10 01:29:47 +0000

    [GR-24501] Removing the definition of ICU4J library from graal-js.
    PullRequest: js/2257

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "48dff62c232078d20b2f00e8bbd8a257cb3bb69d",
+                "version": "809719576a2d1349bcbebc30472dfe88650a6c31",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "48dff62c232078d20b2f00e8bbd8a257cb3bb69d",
+                "version": "809719576a2d1349bcbebc30472dfe88650a6c31",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
