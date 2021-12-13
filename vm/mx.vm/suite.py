3d0ab17eb0c4a19f369d168da892113171b53886
Jan Stola <jan.stola@oracle.com>
2021-12-13 20:23:18 +0000

    [GR-35748] Constructor of DynamicArray should not invoke withIntegrityLevel().
    PullRequest: js/2268

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "809719576a2d1349bcbebc30472dfe88650a6c31",
+                "version": "89e2b935199db94004fb4dc2f6156b86c73e48d1",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "809719576a2d1349bcbebc30472dfe88650a6c31",
+                "version": "89e2b935199db94004fb4dc2f6156b86c73e48d1",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
