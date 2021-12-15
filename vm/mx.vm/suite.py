a8e3d29028a6d86bf8fda82462443f5cea64f1e3
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2021-12-15 02:36:25 +0000

    [GR-35646] Update Truffle import to 22.0 release branch.
    PullRequest: js/2261

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "4d5a31e1af1b007390754f18625f2912092147c6",
+                "version": "c173cf910f0872e7af1d3cf7c3f2d4e40ff5682c",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "4d5a31e1af1b007390754f18625f2912092147c6",
+                "version": "c173cf910f0872e7af1d3cf7c3f2d4e40ff5682c",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
