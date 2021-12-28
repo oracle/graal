e0b53778c7bfedb7f7038d0cb5f14751a8f77655
taha alamine boukhalf <taha.alamine.boukhalf@oracle.com>
2021-12-28 12:38:03 +0100

    fix GraalIntrinsics issue

--- compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/CheckGraalIntrinsics.java
+++ compiler/src/org.graalvm.compiler.hotspot.test/src/org/graalvm/compiler/hotspot/test/CheckGraalIntrinsics.java
@@ -265,15 +265,14 @@ public class CheckGraalIntrinsics extends GraalTest {
                         "java/lang/Math.negateExact(J)J",
                         // HotSpot MacroAssembler-based intrinsic
                         "java/lang/String.indexOf(Ljava/lang/String;)I",
+                        "java/lang/StringCoding.implEncodeAsciiArray([CI[BII)I",
                         // Can share most implementation parts with with
                         // Unsafe.allocateUninitializedArray0
                         "java/lang/reflect/Array.newArray(Ljava/lang/Class;I)Ljava/lang/Object;",
                         // HotSpot MacroAssembler-based intrinsic
                         "sun/nio/cs/ISO_8859_1$Encoder.encodeISOArray([CI[BII)I",
                         // We have implemented implCompressMultiBlock0 on JDK9+.
-                        "sun/security/provider/DigestBase.implCompressMultiBlock([BII)I",
-                        "java/lang/StringCoding.implEncodeAsciiArray([CI[BII)I"
-                );
+                        "sun/security/provider/DigestBase.implCompressMultiBlock([BII)I");
 
         // See JDK-8207146.
         String oopName = isJDK12OrHigher() ? "Reference" : "Object";
