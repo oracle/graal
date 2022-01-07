82b0ae3812d4ed48c18394697685876f1aa63fb5
David Kozak <david.kozak@oracle.com>
2022-01-07 18:25:53 +0100

    make beginObject and endObject in StatisticsPrinter private again

--- substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/StatisticsPrinter.java
+++ substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/reports/StatisticsPrinter.java
@@ -95,11 +95,11 @@ public final class StatisticsPrinter {
 
     static final String INDENT = "   ";
 
-    public static PrintWriter endObject(PrintWriter out) {
+    private static PrintWriter endObject(PrintWriter out) {
         return out.format("}%n");
     }
 
-    public static PrintWriter beginObject(PrintWriter out) {
+    private static PrintWriter beginObject(PrintWriter out) {
         return out.format("{%n");
     }
 
