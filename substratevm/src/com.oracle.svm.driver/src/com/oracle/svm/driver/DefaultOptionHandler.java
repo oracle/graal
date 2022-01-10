22aa7a2cf30f5b2f6ca8f822c40a3464e59a86f5
Marouane el hallaoui <marouane.el.hallaoui@oracle.com>
2022-01-10 16:37:44 +0100

    Revert "Make bash launcher --version print GraalVM version"
    This reverts commit cfdef22a914e71010ac26dac152388eef830bdda.

--- substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/DefaultOptionHandler.java
+++ substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/DefaultOptionHandler.java
@@ -80,10 +80,7 @@ class DefaultOptionHandler extends NativeImage.OptionHandler<NativeImage> {
             case "--version":
                 args.poll();
                 singleArgumentCheck(args, headArg);
-                String message = "GraalVM Version " + NativeImage.graalvmVersion;
-                if (!NativeImage.graalvmConfig.isEmpty()) {
-                    message += " " + NativeImage.graalvmConfig;
-                }
+                String message = System.getProperty("java.vm.version");
                 message += " (Java Version " + javaRuntimeVersion + ")";
                 nativeImage.showMessage(message);
                 System.exit(0);
