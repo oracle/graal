9e4c055532a5f912ca84cd3f67b150fcec5a1b82
Jakub Chaloupka <jakub.chaloupka@oracle.com>
2022-01-19 21:39:59 +0100

    Add JavaDoc to IndexedValueMap.
    (cherry picked from commit c28402ce824cd40335414869b51a657d51d05a47)

--- compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/util/IndexedValueMap.java
+++ compiler/src/org.graalvm.compiler.lir/src/org/graalvm/compiler/lir/util/IndexedValueMap.java
@@ -37,17 +37,61 @@ import org.graalvm.compiler.lir.LIRInstruction.OperandMode;
 
 import jdk.vm.ci.meta.Value;
 
+/**
+ * A map-like data structure for directly mapping int indexes to {@link Value} objects using an
+ * array. If the size of the array grows beyond {@link IndexedValueMap#MAP_FALLBACK_THRESHOLD} then
+ * this data structure does a switch to {@link EconomicMap} backing. When the data structure is
+ * populated via the {@link IndexedValueMap#IndexedValueMap(IndexedValueMap) copy constructor}, or
+ * the {@link IndexedValueMap#putAll(IndexedValueMap)} method while being empty, the backing array
+ * or map are not copied, instead the backing array or map from the source are used. They are copied
+ * only on the next modification. Therefore, {@link IndexedValueMap} instances must not be passed to
+ * other threads.
+ */
 public final class IndexedValueMap {
+    /**
+     * Maximum number of elements in the {@link IndexedValueMap#values} array. If mappings for
+     * higher indexes are needed, the data structure does a switch to
+     * {@link IndexedValueMap#fallbackMap} backing.
+     */
     private static final int MAP_FALLBACK_THRESHOLD = 128;
 
+    /**
+     * An array backing this data structure as long as all indexes are below
+     * {@link IndexedValueMap#MAP_FALLBACK_THRESHOLD}, otherwise equal to {@code null}.
+     */
     private Value[] values;
-    private boolean copyOnWrite;
+
+    /**
+     * A fallback map backing this data structure since the occurrence of a mapping with an index
+     * greater or equal to {@link IndexedValueMap#MAP_FALLBACK_THRESHOLD}. The data structure is
+     * backed by the fallback map, if and only if the {@link IndexedValueMap#values} array is equal
+     * to {@code null}. The data structure can switch back to being backed by the array if all
+     * indexes are mapped to {@code null} and {@link IndexedValueMap#putAll(IndexedValueMap)} is
+     * called with a map backed by an array.
+     */
     private EconomicMap<Integer, Value> fallbackMap;
 
+    /**
+     * A boolean indicating whether the data structure should copy its backing array or map on the
+     * next modification.
+     */
+    private boolean copyOnWrite;
+
+    /**
+     * Construct a new empty map.
+     */
     public IndexedValueMap() {
         values = Value.NO_VALUES;
     }
 
+    /**
+     * Constructs a new map containing the same mappings as the {@code other} map. Note that the
+     * mappings are not copied, instead the new map uses the same backing as the source map, copying
+     * it only on the next modification. As a result, the backing of the source map has to be copied
+     * on the next modification as well.
+     *
+     * @param other the source map whose mappings are to be placed in this map
+     */
     public IndexedValueMap(IndexedValueMap other) {
         values = other.values;
         fallbackMap = other.fallbackMap;
@@ -55,6 +99,14 @@ public final class IndexedValueMap {
         other.copyOnWrite = true;
     }
 
+    /**
+     * Returns the value to which the specified index is mapped, or {@code null} if the map contains
+     * no mapping for the index.
+     *
+     * @param index the index whose associated value is to be returned
+     * @return the value to which the specified index is mapped, or {@code null} if the map contains
+     *         no mapping for the index
+     */
     public Value get(int index) {
         if (values != null) {
             if (index < values.length) {
@@ -68,6 +120,17 @@ public final class IndexedValueMap {
         return null;
     }
 
+    /**
+     * Associates the specified value with the specified index in this map. If the map previously
+     * contained a mapping for the index, the old value is replaced. If this operation requires a
+     * modification of the backing array or map and {@link IndexedValueMap#copyOnWrite} is
+     * {@code true}, the backing array or map are copied. If this map is backed by the array and
+     * this operation requires it to grow above {@link IndexedValueMap#MAP_FALLBACK_THRESHOLD}, the
+     * array is copied to the fallback map which becomes the new backing for this map.
+     *
+     * @param index the index with which the specified value is to be associated
+     * @param value value to be associated with the specified index
+     */
     public void put(int index, Value value) {
         if (values != null) {
             if (values.length <= index) {
@@ -122,6 +185,11 @@ public final class IndexedValueMap {
         }
     }
 
+    /**
+     * Copies the backing array to a new backing array.
+     *
+     * @param minLimit the minimal count of elements in the new array
+     */
     private void doCopyOnWrite(int minLimit) {
         int limit = values.length;
         while (limit > minLimit) {
@@ -141,8 +209,15 @@ public final class IndexedValueMap {
         copyOnWrite = false;
     }
 
-    private void doCopyMapOnWrite(boolean allowNullValues) {
-        EconomicMap<Integer, Value> newFallbackMap = allowNullValues ? null : EconomicMap.create();
+    /**
+     * Copies the backing map to a new backing map.
+     *
+     * @param allowNullFallbackMap specifies whether the backing map containing no mapping with
+     *            non-null value should be represented by {@code null} backing map or empty backing
+     *            map
+     */
+    private void doCopyMapOnWrite(boolean allowNullFallbackMap) {
+        EconomicMap<Integer, Value> newFallbackMap = allowNullFallbackMap ? null : EconomicMap.create();
         MapCursor<Integer, Value> valueEntry = fallbackMap.getEntries();
         while (valueEntry.advance()) {
             if (valueEntry.getValue() != null) {
@@ -156,6 +231,11 @@ public final class IndexedValueMap {
         copyOnWrite = false;
     }
 
+    /**
+     * Determine whether the map contains no mapping with non-null value.
+     *
+     * @return {@code true} if and only if the map contains no mapping with non-null value
+     */
     private boolean isEmpty() {
         boolean empty = true;
         if (values != null) {
@@ -179,6 +259,9 @@ public final class IndexedValueMap {
         return empty;
     }
 
+    /**
+     * Copy the backing array to a map which becomes the new backing for this map.
+     */
     private void fallbackToMap() {
         for (int i = 0; i < values.length; i++) {
             if (values[i] != null) {
@@ -192,6 +275,20 @@ public final class IndexedValueMap {
         copyOnWrite = false;
     }
 
+    /**
+     * Copies all of the mappings with non-null value from the specified map to this map. These
+     * mappings replace any mappings that this map had for any of the indexes currently in the
+     * specified map. If this operation requires a modification of the backing array or map and
+     * {@link IndexedValueMap#copyOnWrite} is {@code true}, the backing array or map are copied. If
+     * this map is backed by the array and this operation requires it to grow above
+     * {@link IndexedValueMap#MAP_FALLBACK_THRESHOLD}, the array is copied to the fallback map which
+     * becomes the new backing for this map. If this map is empty, the mappings from the specified
+     * map are not copied, instead the new map uses the same backing as the source map, copying it
+     * only on the next modification. As a result, the backing of the source map has to be copied on
+     * the next modification as well.
+     *
+     * @param stack mappings to be stored in this map
+     */
     public void putAll(IndexedValueMap stack) {
         if (stack.isEmpty()) {
             return;
@@ -265,6 +362,14 @@ public final class IndexedValueMap {
         }
     }
 
+    /**
+     * Determine whether the specified object is an {@link IndexedValueMap} and all the mappings
+     * with non-null values are equal in this map and the specified map.
+     *
+     * @param other the object to compare this map with
+     * @return {@code true} if and only if the specified object is an {@link IndexedValueMap} and
+     *         all the mappings with non-null values are equal in this map and the specified map.
+     */
     @Override
     public boolean equals(Object other) {
         if (other instanceof IndexedValueMap) {
@@ -274,6 +379,14 @@ public final class IndexedValueMap {
         return false;
     }
 
+    /**
+     * Determine whether all the mappings with non-null values in this map are also present in the
+     * specified map.
+     *
+     * @param other the object to compare this map with
+     * @return {@code true} if and only if all the mappings with non-null values in this map are
+     *         also present in the specified map
+     */
     private boolean isSubsetOf(IndexedValueMap other) {
         boolean isSubset = true;
         if (values != null) {
@@ -297,6 +410,16 @@ public final class IndexedValueMap {
         return isSubset;
     }
 
+    /**
+     * Iterate over all non-null values and, optionally, modify them. For each mapping in this map
+     * with a non-null value, apply the specified procedure to the value and replace the value in
+     * the mapping by the value returned by the procedure.
+     *
+     * @param inst current instruction
+     * @param mode the operand mode for each value
+     * @param flags a set of flags for each value
+     * @param proc procedure to apply to each value
+     */
     public void forEach(LIRInstruction inst, OperandMode mode, EnumSet<OperandFlag> flags, InstructionValueProcedure proc) {
         if (values != null) {
             for (int i = 0; i < values.length; i++) {
@@ -316,6 +439,15 @@ public final class IndexedValueMap {
         }
     }
 
+    /**
+     * Iterate over all non-null values without modifying them. For each mapping in this map with a
+     * non-null value, pass the value to the specified consumer function.
+     *
+     * @param inst current instruction
+     * @param mode the operand mode for each value
+     * @param flags a set of flags for each value
+     * @param consumer consumer function to be called for each value
+     */
     public void visitEach(LIRInstruction inst, OperandMode mode, EnumSet<OperandFlag> flags, InstructionValueConsumer consumer) {
         if (values != null) {
             for (Value v : values) {
@@ -340,6 +472,11 @@ public final class IndexedValueMap {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Get a string representation for all mappings in this map with a non-null value.
+     *
+     * @return a string representation for all mappings in this map with a non-null value
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
