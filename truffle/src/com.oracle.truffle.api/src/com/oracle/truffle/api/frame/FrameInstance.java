2b1a76baf0baa6d4b587a7d54cc21cc3d5270797
Christian Humer <christian.humer@oracle.com>
2022-01-25 21:56:26 +0100

    Fix javadoc referring to deprecated API.

--- truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameInstance.java
+++ truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/frame/FrameInstance.java
@@ -115,7 +115,7 @@ public interface FrameInstance {
     /**
      * Returns whether or not the current frame is a compilation root. A compilation root is a
      * compiled {@link CallTarget} which was itself compiled i.e. not inlined into another target.
-     * 
+     *
      * @since 21.3.0
      */
     default boolean isCompilationRoot() {
@@ -129,9 +129,9 @@ public interface FrameInstance {
      *
      * <pre>
      *                      ===============
-     *  {@link TruffleRuntime#getCurrentFrame() Current}:         ,>|  CallTarget   | FrameInstance
+     *  Current:         ,>|  CallTarget   | FrameInstance
      *                   |  ===============
-     *  {@link TruffleRuntime#getCallerFrame() Caller}:          '-|  CallNode     | FrameInstance
+     *  Caller:          '-|  CallNode     | FrameInstance
      *                   ,>|  CallTarget   |
      *                   |  ===============
      *                   '-|  CallNode     | FrameInstance
