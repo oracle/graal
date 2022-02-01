c8d4a77363cf97b353594f93c5b79812a67eb66b
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2022-02-01 11:26:58 +0100

    Dump JavaScript Array as Object[] into the .hprof file
    (cherry picked from commit 70ae4030c395c4e7bbea4ce5039edea36eab5bee)

--- tools/src/org.graalvm.tools.insight.heap/src/org/graalvm/tools/insight/heap/instrument/HeapGenerator.java
+++ tools/src/org.graalvm.tools.insight.heap/src/org/graalvm/tools/insight/heap/instrument/HeapGenerator.java
@@ -46,6 +46,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.TreeSet;
 import org.graalvm.tools.insight.Insight;
+import org.graalvm.tools.insight.heap.HeapDump.ArrayBuilder;
 
 final class HeapGenerator {
     private final HeapDump.Builder generator;
@@ -273,17 +274,48 @@ final class HeapGenerator {
         if (clazz == null) {
             return unreachable;
         }
-        InstanceBuilder builder = seg.newInstance(clazz);
-        objects.put(obj, builder.id());
-        pending.add(() -> {
-            for (String n : clazz.names()) {
-                final Object v = iop.readMember(obj, n);
-                ObjectInstance vId = dumpObject(iop, seg, null, v, depth - 1);
-                builder.put(n, vId);
+
+        int len = findArrayLength(iop, obj);
+        if (len >= 0) {
+            ArrayBuilder builder = seg.newArray(len);
+            objects.put(obj, builder.id());
+            pending.add(() -> {
+                for (int i = 0; i < len; i++) {
+                    Object v = iop.readArrayElement(obj, i);
+                    ObjectInstance vId = dumpObject(iop, seg, null, v, depth - 1);
+                    builder.put(i, vId);
+                }
+                builder.dumpInstance();
+            });
+            return builder.id();
+        } else {
+            InstanceBuilder builder = seg.newInstance(clazz);
+            objects.put(obj, builder.id());
+            pending.add(() -> {
+                for (String n : clazz.names()) {
+                    final Object v = iop.readMember(obj, n);
+                    ObjectInstance vId = dumpObject(iop, seg, null, v, depth - 1);
+                    builder.put(n, vId);
+                }
+                builder.dumpInstance();
+            });
+            return builder.id();
+        }
+    }
+
+    private int findArrayLength(InteropLibrary iop, Object obj) {
+        if (!iop.hasArrayElements(obj)) {
+            return -1;
+        }
+        try {
+            long len = iop.getArraySize(obj);
+            if (len > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
             }
-            builder.dumpInstance();
-        });
-        return builder.id();
+            return (int) len;
+        } catch (UnsupportedMessageException ex) {
+            return -1;
+        }
     }
 
     ClassInstance findClass(InteropLibrary iop, HeapDump seg, String metaHint, Object obj) throws IOException {
@@ -390,7 +422,7 @@ final class HeapGenerator {
     }
 
     private interface Dump {
-        void dump() throws UnknownIdentifierException, IOException, UnsupportedMessageException;
+        void dump() throws UnknownIdentifierException, IOException, UnsupportedMessageException, InvalidArrayIndexException;
     }
 
     private static final class SourceKey {
