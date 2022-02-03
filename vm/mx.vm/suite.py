e507647a0d668a522eaa05bd33e1f09e58f9b42d
Andreas Woess <andreas.woess@oracle.com>
2022-02-03 22:14:10 +0000

    [GR-36286] Disable shape merging.
    PullRequest: js/2293

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "fa0a6ba5a75486473701a39f562a4dafde929728",
+                "version": "5caf150bf292d077b8fa730bac8f0a99762d62d4",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "fa0a6ba5a75486473701a39f562a4dafde929728",
+                "version": "5caf150bf292d077b8fa730bac8f0a99762d62d4",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
