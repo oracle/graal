ebd92c98ae8e95a4e67230b4c2ae5e910260794e
Fabio Niephaus <fabio.niephaus@oracle.com>
2022-02-10 08:59:27 +0100

    Bump javaCompliance of sdk to 11+.

--- sdk/src/org.graalvm.options/src/org/graalvm/options/OptionType.java
+++ sdk/src/org.graalvm.options/src/org/graalvm/options/OptionType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -53,7 +53,7 @@ import java.util.function.Function;
  */
 public final class OptionType<T> {
 
-    private static final Consumer<?> EMPTY_VALIDATOR = new Consumer<Object>() {
+    private static final Consumer<?> EMPTY_VALIDATOR = new Consumer<>() {
         public void accept(Object t) {
         }
     };
