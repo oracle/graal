9ee7c27adc787b8c30beaf9b79452c58c9ced590
Marouane el hallaoui <marouane.el.hallaoui@oracle.com>
2022-02-15 12:08:00 +0100

    update intrinsics

--- compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/meta/UnimplementedGraalIntrinsics.java
+++ compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/meta/UnimplementedGraalIntrinsics.java
@@ -213,6 +213,7 @@ public final class UnimplementedGraalIntrinsics {
         // Compact string support - HotSpot MacroAssembler-based intrinsic or complex C2 logic.
         add(toBeInvestigated,
                         "java/lang/StringCoding.hasNegatives([BII)Z",
+                        "java/lang/StringCoding.implEncodeAsciiArray([CI[BII)I",
                         "java/lang/StringCoding.implEncodeISOArray([BI[BII)I");
         add(ignore,
                         // handled through an intrinsic for String.equals itself
