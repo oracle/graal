dd05c37e458f0f655a08eb5fa593c84908a67469
Taha Alamine Boukhalf <taha.alamine.boukhalf@oracle.com>
2022-02-22 11:19:16 +0000

    [GR-37001] Backport to 21.3: update ES module loading error message.
    PullRequest: js/2335

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "5caf150bf292d077b8fa730bac8f0a99762d62d4",
+                "version": "c7c10cd264692a749d8bbdec134ae2e9433ace7b",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "5caf150bf292d077b8fa730bac8f0a99762d62d4",
+                "version": "c7c10cd264692a749d8bbdec134ae2e9433ace7b",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
