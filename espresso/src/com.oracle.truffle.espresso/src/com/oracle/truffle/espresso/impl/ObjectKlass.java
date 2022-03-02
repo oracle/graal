2a10ed83f6b148b17a8c4c114ac11796ed02cab3
Allan Gregersen <allan.gregersen@oracle.com>
2022-03-02 14:48:43 +0100

    make method static

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/impl/ObjectKlass.java
@@ -201,7 +201,7 @@ public final class ObjectKlass extends Klass {
         assert verifyTables();
     }
 
-    private boolean isEnumValuesField(LinkedField lkStaticFields) {
+    private static boolean isEnumValuesField(LinkedField lkStaticFields) {
         return lkStaticFields.getName() == Name.$VALUES ||
                 lkStaticFields.getName() == Name.ENUM$VALUES;
     }
