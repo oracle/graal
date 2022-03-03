15a353423dcbf948a4fb635f3e0fd3519ebf6549
Zhengyu Gu <zgu@redhat.com>
2022-03-03 10:04:23 -0500

    Fix style

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/events/ExecuteVMOperationEvent.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jfr/events/ExecuteVMOperationEvent.java
@@ -59,7 +59,7 @@ public class ExecuteVMOperationEvent {
             JfrNativeEventWriter.putLong(data, JfrTicks.elapsedTicks() - startTicks);
             JfrNativeEventWriter.putEventThread(data);
             JfrNativeEventWriter.putLong(data, JfrVMOperations.singleton().getVMOperationId(vmOperation.getClass()));
-            JfrNativeEventWriter.putBoolean(data, vmOperation.getCausesSafepoint());  // At a safepoint
+            JfrNativeEventWriter.putBoolean(data, vmOperation.getCausesSafepoint());
             JfrNativeEventWriter.putBoolean(data, true);  // Blocking
             JfrNativeEventWriter.putThread(data, requestingThread);
             JfrNativeEventWriter.putLong(data, vmOperation.getCausesSafepoint() ? Safepoint.Master.singleton().getSafepointId().rawValue() : 0);
