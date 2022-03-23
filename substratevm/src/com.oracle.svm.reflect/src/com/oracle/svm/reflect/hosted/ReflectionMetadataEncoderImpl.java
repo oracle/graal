8c54c7695520856558362115b9dd009eae3d62f7
Fabio Niephaus <fabio.niephaus@oracle.com>
2022-03-23 12:20:51 +0100

    Avoid NPE in `ReflectionMetadataEncoderImpl`.
    Fixes #4388

--- substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/ReflectionMetadataEncoderImpl.java
+++ substratevm/src/com.oracle.svm.reflect/src/com/oracle/svm/reflect/hosted/ReflectionMetadataEncoderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -497,7 +497,7 @@ public class ReflectionMetadataEncoderImpl implements ReflectionMetadataEncoder 
             // Checkstyle: allow direct annotation access
             Annotation annotation = typeAnnotation.getAnnotation();
             // Checkstyle: disallow direct annotation access
-            if (registerAnnotation(metaAccess, annotation)) {
+            if (annotation != null && registerAnnotation(metaAccess, annotation)) {
                 includedTypeAnnotations.add(typeAnnotation);
             }
         }
