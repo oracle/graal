c1b09de23b75cb4e3d6cccdd18a9d40cab6eae29
Jan Stola <jan.stola@oracle.com>
2022-03-24 17:53:41 +0000

    [GR-37628] Backport: Recent changes in the specification of top level await.
    PullRequest: js/2390

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "428c3ad88a0849d277a0b07dcd89aa8769dd3400",
+                "version": "674b8dd7df6be4584bf774aa9040a272a768e035",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "428c3ad88a0849d277a0b07dcd89aa8769dd3400",
+                "version": "674b8dd7df6be4584bf774aa9040a272a768e035",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
