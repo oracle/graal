47525a5736ba7868c1183e502929c05e4707fb54
Martin Entlicher <martin.entlicher@oracle.com>
2022-04-25 10:22:52 +0200

    Heap buffer for collecting data in memory. (GR-37442)

--- tools/src/org.graalvm.tools.insight.test/src/org/graalvm/tools/insight/test/heap/HeapObjectStreamTest.java
+++ tools/src/org.graalvm.tools.insight.test/src/org/graalvm/tools/insight/test/heap/HeapObjectStreamTest.java
@@ -40,12 +40,15 @@ import org.graalvm.tools.insight.test.heap.HeapApi.Source;
 import org.graalvm.tools.insight.test.heap.HeapApi.StackElement;
 import org.graalvm.tools.insight.test.heap.HeapApi.StackEvent;
 import static org.graalvm.tools.insight.test.heap.HeapApi.invokeDump;
+import static org.graalvm.tools.insight.test.heap.HeapApi.invokeFlush;
+import org.graalvm.tools.insight.test.heap.HeapResourceRule.HeapParams;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class HeapObjectStreamTest {
@@ -53,28 +56,34 @@ public class HeapObjectStreamTest {
     private Value heap;
     private ByteArrayOutputStream heapOutput;
 
-    @Before
-    public void prepareHeap() throws Exception {
-        Context.Builder b = Context.newBuilder();
-        Context ctx = InsightObjectFactory.newContext(b);
+    @Rule public HeapResourceRule heapResource = new HeapResourceRule() {
+        @Override
+        protected void before(int cacheSize, String cacheReplacement) throws Throwable {
+            Context.Builder b = Context.newBuilder();
+            b.option("heap.cacheSize", Integer.toString(cacheSize));
+            if (cacheReplacement != null) {
+                b.option("heap.cacheReplacement", cacheReplacement);
+            }
+            Context ctx = InsightObjectFactory.newContext(b);
 
-        // BEGIN: org.graalvm.tools.insight.test.heap.HeapObjectStreamTest
-        Map<String, Instrument> instruments = ctx.getEngine().getInstruments();
-        Instrument heapInstrument = instruments.get("heap");
+            // BEGIN: org.graalvm.tools.insight.test.heap.HeapObjectStreamTest
+            Map<String, Instrument> instruments = ctx.getEngine().getInstruments();
+            Instrument heapInstrument = instruments.get("heap");
 
-        @SuppressWarnings("unchecked")
-        Consumer<OutputStream> consumeOutput = heapInstrument.lookup(Consumer.class);
+            @SuppressWarnings("unchecked")
+            Consumer<OutputStream> consumeOutput = heapInstrument.lookup(Consumer.class);
 
-        heapOutput = new ByteArrayOutputStream();
-        consumeOutput.accept(heapOutput);
-        // END: org.graalvm.tools.insight.test.heap.HeapObjectStreamTest
+            heapOutput = new ByteArrayOutputStream();
+            consumeOutput.accept(heapOutput);
+            // END: org.graalvm.tools.insight.test.heap.HeapObjectStreamTest
 
-        heap = InsightObjectFactory.readObject(ctx, "heap");
-        assertFalse("Heap object is defined", heap.isNull());
-    }
+            heap = InsightObjectFactory.readObject(ctx, "heap");
+            assertFalse("Heap object is defined", heap.isNull());
+        }
+    };
 
     @Test
-    public void cannotSetNullStream() {
+    public void cannotSetNullStream() throws Exception {
         @SuppressWarnings("unchecked")
         Consumer<OutputStream> osSetup = heap.getContext().getEngine().getInstruments().get("heap").lookup(Consumer.class);
         assertNotNull("Consumer for OutputStream", osSetup);
@@ -104,7 +113,7 @@ public class HeapObjectStreamTest {
         assertEquals("JAVA PROFILE 1.0.1", header);
 
         final int heapSize = heapOutput.size();
-        if (heapSize < 3000) {
+        if (heapSize != 4427) {
             fail("Heap dump should be generated to the stream: " + heapOutput.size());
         }
 
@@ -122,5 +131,115 @@ public class HeapObjectStreamTest {
 
         String anotherHeader = new String(anotherStream.toByteArray(), 0, 18);
         assertEquals("JAVA PROFILE 1.0.1", anotherHeader);
+    }
+
+    @Test(timeout = 10_000)
+    @HeapParams(cacheSize = 10_000, cacheReplacement = "flush")
+    public void dumpToStreamCached() throws Exception {
+        Source nullSource = new Source("no.src", null, null, null, null);
+        invokeDump(heap, 1, new Event[]{
+                        new StackEvent(new StackElement[]{new StackElement(new At(null, nullSource, 1, 0, 5), new HashMap<>())})
+        });
+
+        Source source = new Source("a.text", "application/x-test", "test", "file://a.test", "aaaaa");
+        invokeDump(heap, 1, new Event[]{
+                        new StackEvent(new StackElement[]{new StackElement(new At("a", source, null, null, null), new HashMap<>())})
+        });
+
+        if (heapOutput.size() > 0) {
+            fail("Heap dump should not be written to the stream yet: " + heapOutput.size());
+        }
+
+        invokeFlush(heap);
+
+        String header = new String(heapOutput.toByteArray(), 0, 18);
+        assertEquals("JAVA PROFILE 1.0.1", header);
+
+        final int heapSize = heapOutput.size();
+        if (heapSize != 2509) {
+            fail("Heap dump should be generated to the stream: " + heapOutput.size());
+        }
+    }
+
+    @Test
+    @HeapParams(cacheSize = 2, cacheReplacement = "flush")
+    public void dumpToStreamCachedDumps() throws Exception {
+        Source source = new Source("a.text", "application/x-test", "test", "file://a.test", "aaaaa");
+        invokeDump(heap, 1, new Event[]{
+                        new StackEvent(new StackElement[]{new StackElement(new At("a", source, 1, 0, 2), new HashMap<>())})
+        });
+        invokeDump(heap, 1, new Event[]{
+                        new StackEvent(new StackElement[]{new StackElement(new At("a", source, 2, 2, 2), new HashMap<>())})
+        });
+
+        if (heapOutput.size() > 0) {
+            fail("Heap dump should not be written to the stream yet: " + heapOutput.size());
+        }
+
+        invokeDump(heap, 1, new Event[]{
+                        new StackEvent(new StackElement[]{new StackElement(new At("a", source, 3, 4, 2), new HashMap<>())})
+        });
+
+        final int heapSize = heapOutput.size();
+        if (heapSize != 2500) {
+            fail("Heap dump should be generated to the stream: " + heapOutput.size());
+        }
+
+        invokeFlush(heap);
+        if (heapOutput.size() <= heapSize) {
+            fail("Flush should write the remaining cached events.");
+        }
+    }
+
+    @Test
+    @HeapParams(cacheSize = 2, cacheReplacement = "lru")
+    public void dumpToStreamCachedLRU() throws Exception {
+        Source source = new Source("a.text", "application/x-test", "test", "file://a.test", "aaaaa");
+        invokeDump(heap, 1, new Event[]{
+                        new StackEvent(new StackElement[]{new StackElement(new At("a", source, 1, 0, 2), new HashMap<>())})
+        });
+        invokeDump(heap, 1, new Event[]{
+                        new StackEvent(new StackElement[]{new StackElement(new At("a", source, 2, 2, 2), new HashMap<>())})
+        });
+
+        if (heapOutput.size() > 0) {
+            fail("Heap dump should not be written to the stream yet: " + heapOutput.size());
+        }
+
+        invokeFlush(heap);
+
+        final int heapSize = heapOutput.size();
+        if (heapSize != 2286) {
+            fail("Heap dump should be generated to the stream: " + heapOutput.size());
+        }
+
+        @SuppressWarnings("unchecked")
+        Consumer<OutputStream> osSetup = heap.getContext().getEngine().getInstruments().get("heap").lookup(Consumer.class);
+        assertNotNull("Consumer for OutputStream", osSetup);
+        final ByteArrayOutputStream anotherStream = new ByteArrayOutputStream();
+
+        osSetup.accept(anotherStream);
+        for (int i = 0; i < 4; i++) {
+            invokeDump(heap, 1, new Event[]{
+                            new StackEvent(new StackElement[]{new StackElement(new At("a", source, i, 2 * i, 2), new HashMap<>())})
+            });
+        }
+        invokeFlush(heap);
+
+        assertEquals("Size of first stream remains unchaged", heapSize, heapOutput.size());
+        assertEquals("Size of the second stream is the same due to LRU caching", heapSize, anotherStream.size());
+        assertNotEquals("Heap contents differ", true, equals(heapOutput.toByteArray(), anotherStream.toByteArray()));
+    }
+
+    private static boolean equals(byte[] arr1, byte[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- tools/src/org.graalvm.tools.insight.test/src/org/graalvm/tools/insight/test/heap/HeapObjectTest.java
+++ tools/src/org.graalvm.tools.insight.test/src/org/graalvm/tools/insight/test/heap/HeapObjectTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,7 @@
 package org.graalvm.tools.insight.test.heap;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.function.Consumer;
@@ -40,24 +41,44 @@ import org.graalvm.tools.insight.test.heap.HeapApi.StackElement;
 import org.graalvm.tools.insight.test.heap.HeapApi.StackEvent;
 import org.graalvm.tools.insight.test.heap.HeapApi.UnrelatedEvent;
 import static org.graalvm.tools.insight.test.heap.HeapApi.invokeDump;
+import static org.graalvm.tools.insight.test.heap.HeapApi.invokeFlush;
+import org.graalvm.tools.insight.test.heap.HeapResourceRule.HeapParams;
+import org.junit.After;
+import org.junit.Assert;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class HeapObjectTest {
+    private Context context;
     private Value heap;
+    private File dumpFile;
 
-    @Before
-    public void prepareHeap() throws Exception {
-        Context.Builder b = Context.newBuilder();
-        b.option("heap.dump", "x.hprof");
-        b.allowIO(true);
-        Context ctx = InsightObjectFactory.newContext(b);
-        heap = InsightObjectFactory.readObject(ctx, "heap");
-        assertFalse("Heap object is defined", heap.isNull());
+    @Rule public HeapResourceRule heapResource = new HeapResourceRule() {
+        @Override
+        protected void before(int cacheSize, String cacheReplacement) throws Throwable {
+            Context.Builder b = Context.newBuilder();
+            b.option("heap.cacheSize", Integer.toString(cacheSize));
+            if (cacheReplacement != null) {
+                b.option("heap.cacheReplacement", cacheReplacement);
+            }
+            dumpFile = File.createTempFile("heap", ".hprof");
+            dumpFile.deleteOnExit();
+            b.option("heap.dump", dumpFile.getAbsolutePath());
+            b.allowIO(true);
+            Context ctx = InsightObjectFactory.newContext(b);
+            context = ctx;
+            heap = InsightObjectFactory.readObject(ctx, "heap");
+            assertFalse("Heap object is defined", heap.isNull());
+        }
+    };
+
+    @After
+    public void after() {
+        context.close();
     }
 
     @Test
@@ -153,11 +174,19 @@ public class HeapObjectTest {
         invokeDump(heap, 1, new Event[]{
                         new StackEvent(new StackElement[]{new StackElement(new At(null, nullSource, 1, 0, 5), new HashMap<>())})
         });
+        long heapSize = dumpFile.length();
+        if (heapSize != 2747) {
+            fail("Heap dump should be generated. Size = " + heapSize);
+        }
 
         Source source = new Source("a.text", "application/x-test", "test", "file://a.test", "aaaaa");
         invokeDump(heap, 1, new Event[]{
                         new StackEvent(new StackElement[]{new StackElement(new At("a", source, null, null, null), new HashMap<>())})
         });
+        heapSize = dumpFile.length() - heapSize;
+        if (heapSize != 4436 - 2747) {
+            fail("Heap dump should be generated. Size = " + dumpFile.length());
+        }
     }
 
     private static void assertMessage(Throwable ex, String msg, String exp) {
@@ -166,6 +195,78 @@ public class HeapObjectTest {
             fail(msg +
                             "\nexpecting:" + exp +
                             "\nwas      : " + ex.getMessage());
+        }
+    }
+
+    @Test
+    @HeapParams(cacheSize = 2, cacheReplacement = "flush")
+    public void memoryDumpTest1() throws Exception {
+        checkDumpAfter(2, 10);
+    }
+
+    @Test
+    @HeapParams(cacheSize = 3, cacheReplacement = "flush")
+    public void memoryDumpTest2() throws Exception {
+        checkDumpAfter(3, 10);
+    }
+
+    @Test
+    @HeapParams(cacheSize = 1, cacheReplacement = "flush")
+    public void memoryDumpTest3() throws Exception {
+        checkDumpAfter(1, 10);
+    }
+
+    @Test
+    @HeapParams(cacheSize = -1, cacheReplacement = "flush")
+    public void memoryDumpTest4() throws Exception {
+        checkDumpAfter(-1, 100);
+    }
+
+    @Test
+    @HeapParams(cacheSize = 0, cacheReplacement = "flush")
+    public void memoryDumpTest5() throws Exception {
+        checkDumpAfter(0, 10);
+    }
+
+    private void checkDumpAfter(int period, int count) {
+        Source source = new Source("a.text", "application/x-test", "test", "file://a.test", "aaaaa");
+        int p = 0;
+        long oldSize = dumpFile.length();
+        for (int i = 0; i < count; i++) {
+            invokeDump(heap, 1, new Event[]{
+                            new StackEvent(new StackElement[]{new StackElement(new At("a" + p, source, i, i * 10, 10), new HashMap<>())})
+            });
+            long heapSize = dumpFile.length() - oldSize;
+            if (p++ == period) {
+                if (heapSize < 1000) {
+                    fail("Heap dump should be generated. p = " + p + ", i = " + i + ", size = " + heapSize);
+                }
+                p = 0;
+            } else {
+                if (heapSize > 0) {
+                    fail("No heap dump should be generated yet. p = " + p + ", i = " + i + ", size = " + heapSize);
+                }
+            }
+            oldSize += heapSize;
+        }
+    }
+
+    @Test
+    @HeapParams(cacheSize = 5, cacheReplacement = "lru")
+    public void memoryLRUTest() throws Exception {
+        Source source = new Source("a.text", "application/x-test", "test", "file://a.test", "aaaaa");
+        long oldSize = dumpFile.length();
+        for (int i = 0; i < 100; i++) {
+            invokeDump(heap, 1, new Event[]{
+                            new StackEvent(new StackElement[]{new StackElement(new At("a" + i, source, i, i * 10, 10), new HashMap<>())})
+            });
+        }
+        long heapSize = dumpFile.length() - oldSize;
+        Assert.assertEquals(0L, heapSize);
+        invokeFlush(heap);
+        heapSize = dumpFile.length() - oldSize;
+        if (heapSize != 3016) {
+            fail("Heap dump should be generated. Size = " + dumpFile.length());
         }
     }
 
