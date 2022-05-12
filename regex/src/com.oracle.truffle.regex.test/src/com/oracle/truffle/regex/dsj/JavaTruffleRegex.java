caeaea3fb99fb5bba40d2ac9beb6635a3209e1e8
danieljaburek <k11915134@students.jku.at>
2022-05-12 15:08:09 +0200

    Working on Unittests

---
+++ regex/src/com.oracle.truffle.regex.test/src/com/oracle/truffle/regex/dsj/JavaTruffleRegex.java
@@ -0,0 +1,41 @@
+package com.oracle.truffle.regex.dsj;
+
+import com.oracle.truffle.regex.tregex.test.TRegexTestDummyLanguage;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Value;
+
+public class JavaTruffleRegex {
+
+    private static Context context;
+
+    private static Value tregexPattern;
+
+    private static void setUpContext() {
+        context = Context.newBuilder().build();
+        context.enter();
+    }
+
+//    abstract String getEngineOptions();
+
+    /**
+     * Build like Java Regex compile function
+     * @param regex
+     * @return
+     */
+    public static Value compile(String regex) {
+        setUpContext();
+
+        tregexPattern = context.eval(TRegexTestDummyLanguage.ID, "Flavor=JavaUtilPattern" + '/' + regex + '/');
+
+        return tregexPattern;
+
+//        StringBuilder combinedOptions = new StringBuilder("RegressionTestMode=true");
+//        if (!getEngineOptions().isEmpty()) {
+//            combinedOptions.append("," + getEngineOptions());
+//        }
+//        if (!options.isEmpty()) {
+//            combinedOptions.append("," + options);
+//        }
+//        return context.eval("regexDummyLang", combinedOptions.toString() + '/' + pattern + '/' + flags);
+    }
+}
