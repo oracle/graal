2e477ec26e284f6b6db12bb1a17942c71783b5c2
Jan Stola <jan.stola@oracle.com>
2022-05-14 07:20:56 +0000

    [GR-38400] Preparation for the update to ICU4J 71.1.
    PullRequest: js/2455

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "dd1b9a3acf2a8957362bfe6437adac87af25611c",
+                "version": "26ce1f72adf6e8d9154c13e0bd5bd09eaf3735ca",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "dd1b9a3acf2a8957362bfe6437adac87af25611c",
+                "version": "26ce1f72adf6e8d9154c13e0bd5bd09eaf3735ca",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
