921fd355bd090e426b343bbd2e5b294ac792cfd3
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2022-06-02 09:22:52 +0200

    Fix missing delegation in EspressoBaseMethodNode wrapper

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoBaseMethodNode.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/EspressoBaseMethodNode.java
@@ -25,9 +25,13 @@ package com.oracle.truffle.espresso.nodes;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.instrumentation.GenerateWrapper;
 import com.oracle.truffle.api.instrumentation.ProbeNode;
+import com.oracle.truffle.api.source.SourceSection;
 import com.oracle.truffle.espresso.impl.Method;
-import com.oracle.truffle.espresso.meta.EspressoError;
 
+/*
+ All methods in this class that can be overridden in subclasses must be abstract.
+ If a generic implementation should be provided it should be in EspressoMethodNode.
+ */
 @GenerateWrapper
 public abstract class EspressoBaseMethodNode extends EspressoInstrumentableNode {
     abstract Object executeBody(VirtualFrame frame);
@@ -36,14 +40,13 @@ public abstract class EspressoBaseMethodNode extends EspressoInstrumentableNode 
 
     public abstract Method.MethodVersion getMethodVersion();
 
-    public boolean canSplit() {
-        return false;
-    }
+    // the wrapper must delegate this
+    @Override
+    public abstract SourceSection getSourceSection();
 
-    // Overridden in children which support splitting
-    public EspressoBaseMethodNode split() {
-        throw EspressoError.shouldNotReachHere();
-    }
+    public abstract boolean canSplit();
+
+    public abstract EspressoBaseMethodNode split();
 
     @Override
     public final Object execute(VirtualFrame frame) {
@@ -56,7 +59,5 @@ public abstract class EspressoBaseMethodNode extends EspressoInstrumentableNode 
         return new EspressoBaseMethodNodeWrapper(this, probeNode);
     }
 
-    protected boolean isTrivial() {
-        return false;
-    }
+    protected abstract boolean isTrivial();
 }
