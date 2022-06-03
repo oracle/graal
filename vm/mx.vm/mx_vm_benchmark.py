b4186163237fee890ce734f1b9e3b999da49f4b6
Boris Spasojevic <boris.spasojevic@oracle.com>
2022-06-03 15:42:27 +0200

    Use the correct polybenchmark_vm_registry.

--- vm/mx.vm/mx_vm_benchmark.py
+++ vm/mx.vm/mx_vm_benchmark.py
@@ -1253,8 +1253,8 @@ def register_graalvm_vms():
             _gu_vm_registry.add_vm(GuVm(host_vm_name, 'default', [], []), _suite, 10)
         if _suite.get_import("polybenchmarks") != None:
             import mx_polybenchmarks_benchmark
-            mx_polybenchmarks_benchmark._polybenchmark_vm_registry.add_vm(PolyBenchVm(host_vm_name, "jvm", [], ["--jvm"]))
-            mx_polybenchmarks_benchmark._polybenchmark_vm_registry.add_vm(PolyBenchVm(host_vm_name, "native", [], ["--native"]))
+            mx_polybenchmarks_benchmark.polybenchmark_vm_registry.add_vm(PolyBenchVm(host_vm_name, "jvm", [], ["--jvm"]))
+            mx_polybenchmarks_benchmark.polybenchmark_vm_registry.add_vm(PolyBenchVm(host_vm_name, "native", [], ["--native"]))
 
     # Inlining before analysis is done by default
     analysis_context_sensitivity = ['insens', 'allocsens', '1obj', '2obj1h', '3obj2h', '4obj3h']
