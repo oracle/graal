4bfb3782d7cf86429fe68a8d32e49b458db7a0fa
David Nestorovic <david.nestorovic@oracle.com>
2022-06-17 14:05:41 +0200

    Make docs folder an independent suite

---
+++ docs/mx.docs/suite.py
@@ -0,0 +1,11 @@
+suite = {
+    "mxversion" : "6.0.1",
+    "name" : "docs",
+
+    "externalProjects": {
+        "docs": {
+            "type": "docs",
+            "path": '.'
+        }
+    }
+}
