cdf505d3643a29dccb667411c605016a4943c00c
Severin Gehwolf <sgehwolf@redhat.com>
2022-06-24 13:30:26 +0200

    Initial implmentation for json file build output
    This uses a simple backing Map (HashMap), for collecting the data. It's
    entirely driven by ProgressReporter and if not requested by the user, by
    specifying: `-H:BuildOutputJSONFile=<file>` on the command line,
    entirely disabled.
    
    It also adds a JSON schema to the native image manual as an asset.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateOptions.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateOptions.java
@@ -415,6 +415,11 @@ public class SubstrateOptions {
     @Option(help = "Print GC warnings as part of build output", type = OptionType.User)//
     public static final HostedOptionKey<Boolean> BuildOutputGCWarnings = new HostedOptionKey<>(true);
 
+    @Option(help = "Print build output statistics as JSON to the specified file. " +
+                    "The output is according to the JSON schema located at: " +
+                    "docs/reference-manual/native-image/assets/build-output-schema-v1.json", type = OptionType.User)//
+    public static final HostedOptionKey<String> BuildOutputJSONFile = new HostedOptionKey<>("");
+
     /*
      * Object and array allocation options.
      */
