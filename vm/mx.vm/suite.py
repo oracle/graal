238a4b82c9d1501ef4a3b6f9b7b118e98739e79d
Jan Stola <jan.stola@oracle.com>
2022-07-07 18:48:27 +0000

    [GR-39319] Removing the temporary workaround for foreignObject instanceof Date.
    PullRequest: js/2519

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "c582c165aacd1eb96e0f59a7b0d6b5a8a4978576",
+                "version": "7919a252e50b7ef16fad26f0e6b316015d10ac96",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "c582c165aacd1eb96e0f59a7b0d6b5a8a4978576",
+                "version": "7919a252e50b7ef16fad26f0e6b316015d10ac96",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
