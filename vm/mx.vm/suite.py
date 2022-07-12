da8fe804e8fb5c96b168b397a10540d592e88f72
Jan Stola <jan.stola@oracle.com>
2022-07-12 09:30:46 +0000

    [GR-39662] Upgrading the underlying Node.js to version 16.16.0.
    PullRequest: js/2522

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0c08c6d5ef0c9ea790a962f1adfba67475c7780a",
+                "version": "a7cfee82cd65ae475c130578679d4e5f66ad8b4a",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0c08c6d5ef0c9ea790a962f1adfba67475c7780a",
+                "version": "a7cfee82cd65ae475c130578679d4e5f66ad8b4a",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
