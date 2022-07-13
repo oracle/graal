513d215a2905b9ad7bbf254143532243457fa4ac
Marouane El Hallaoui <marouane.el.hallaoui@oracle.com>
2022-07-13 12:07:42 +0100

    [GR-38389] Release GraalVM 21.3.3

--- tools/mx.tools/suite.py
+++ tools/mx.tools/suite.py
@@ -27,7 +27,7 @@ suite = {
 
     "groupId" : "org.graalvm.tools",
     "version" : "21.3.3",
-    "release" : False,
+    "release" : True,
     "url" : "http://openjdk.java.net/projects/graal",
     "developer" : {
         "name" : "GraalVM Development",
