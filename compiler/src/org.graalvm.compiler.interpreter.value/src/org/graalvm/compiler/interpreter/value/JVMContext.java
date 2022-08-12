d4941e8d343a1731eef6874232e6c158c55bc9b8
WeiyiDong <100402693+WeiyiDong@users.noreply.github.com>
2022-08-12 19:37:43 +0800

    support cross-module
    Added this interface to support cross-module class loading and VarHandle use for InterpreterValueMutableObject.

--- compiler/src/org.graalvm.compiler.interpreter.value/src/org/graalvm/compiler/interpreter/value/JVMContext.java
+++ compiler/src/org.graalvm.compiler.interpreter.value/src/org/graalvm/compiler/interpreter/value/JVMContext.java
@@ -12,15 +12,6 @@ import java.lang.invoke.MethodHandles;
  * 
  * This interface must be put here, or it will cause cyclic reference between modules.
  * 
- * GraalInterpreterTest 和这个文件夹里的类属于不同的module，不同的module有不同的 class
- * loader 和 Lookup，而且一个 module 的 class loader 不能加载另一个 module 的 class，
- * 所以这个文件夹里的 InterpreterValueMutableObject 不能直接用它自己的 class loader 加载
- * GraalInterpreterTest 里面的测试类，需要额外定义这个接口来把 GraalInterpreterTest
- * 的 class loader 和 Lookup 传给 InterpreterValueMutableObject。
- * 
- * 这个接口会在 GraalInterpreter 里由 JVMContextImpl 类实现。这个接口只能放在这里，如果放在其他地方，
- * 比如 org.graalvm.compiler.core，会造成循环依赖：compiler.core 依赖 interpter.value，同时
- * interpter.value 依赖 compiler.core
  */
 public interface JVMContext {
   ClassLoader getClassLoader();
