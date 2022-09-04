12d08fd319661995677c91032c0be4544ab95cee
ol-automation_ww <ol-automation_ww@oracle.com>
2022-09-04 02:25:22 +0000

    [GR-23997] Periodic update of the graal import (2022-09-02).
    PullRequest: js/2573

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "e2591755c6aa1e8f1d2c23c737624f2c255001c0",
+                "version": "cc5cfa627838fe3a65308dd27c2cb5ce24f8ea38",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "e2591755c6aa1e8f1d2c23c737624f2c255001c0",
+                "version": "cc5cfa627838fe3a65308dd27c2cb5ce24f8ea38",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
