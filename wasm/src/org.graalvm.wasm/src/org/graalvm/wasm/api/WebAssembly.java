ae3259bf57824696904c9ea5976be8780a9d0b90
Florian Huemer <florian.huemer@oracle.com>
2022-09-06 11:27:37 +0200

    Fixed rebase.

--- wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/api/WebAssembly.java
+++ wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/api/WebAssembly.java
@@ -51,6 +51,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import com.oracle.truffle.api.CompilerDirectives;
 import org.graalvm.collections.EconomicMap;
 import org.graalvm.collections.Pair;
 import org.graalvm.wasm.EmbedderDataHolder;
