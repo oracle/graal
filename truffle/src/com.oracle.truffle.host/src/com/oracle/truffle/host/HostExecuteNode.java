c506645ca7f80a830d5d7fe67260e032ba7d1072
Allan Gregersen <allan.gregersen@oracle.com>
2022-09-06 08:08:04 +0200

    remove unused import

--- truffle/src/com.oracle.truffle.host/src/com/oracle/truffle/host/HostExecuteNode.java
+++ truffle/src/com.oracle.truffle.host/src/com/oracle/truffle/host/HostExecuteNode.java
@@ -53,7 +53,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
