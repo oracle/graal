440e928364cd76dca62fd155281efe36ec49c9c9
Daniele Bonetta <daniele.bonetta@oracle.com>
2022-09-06 17:30:54 +0200

    [GR-40743] Fix condition profile in TruffleString.checkContentEquals.

--- truffle/src/com.oracle.truffle.api.strings/src/com/oracle/truffle/api/strings/TruffleString.java
+++ truffle/src/com.oracle.truffle.api.strings/src/com/oracle/truffle/api/strings/TruffleString.java
@@ -4846,7 +4846,7 @@ public final class TruffleString extends AbstractTruffleString {
                         @Cached TStringInternalNodes.GetCodeRangeNode getCodeRangeANode,
                         @Cached TStringInternalNodes.GetCodeRangeNode getCodeRangeBNode,
                         @Cached ConditionProfile lengthAndCodeRangeCheckProfile,
-                        @Cached ConditionProfile compareHashProfile,
+                        @Cached BranchProfile compareHashProfile,
                         @Cached ConditionProfile checkFirstByteProfile) {
             final int codeRangeA = getCodeRangeANode.execute(a);
             final int codeRangeB = getCodeRangeBNode.execute(b);
@@ -4861,7 +4861,7 @@ public final class TruffleString extends AbstractTruffleString {
                         ToIndexableNode toIndexableNodeA,
                         ToIndexableNode toIndexableNodeB,
                         ConditionProfile lengthAndCodeRangeCheckProfile,
-                        ConditionProfile compareHashProfile,
+                        BranchProfile compareHashProfile,
                         ConditionProfile checkFirstByteProfile,
                         EqualNode equalNode) {
             assert TSCodeRange.isKnown(codeRangeA, codeRangeB);
@@ -4869,8 +4869,11 @@ public final class TruffleString extends AbstractTruffleString {
             if (lengthAndCodeRangeCheckProfile.profile(lengthCMP != b.length() || codeRangeA != codeRangeB)) {
                 return false;
             }
-            if (compareHashProfile.profile(a.isHashCodeCalculated() && b.isHashCodeCalculated()) && a.getHashCodeUnsafe() != b.getHashCodeUnsafe()) {
-                return false;
+            if (a.isHashCodeCalculated() && b.isHashCodeCalculated()) {
+                compareHashProfile.enter();
+                if (a.getHashCodeUnsafe() != b.getHashCodeUnsafe()) {
+                    return false;
+                }
             }
             if (lengthCMP == 0) {
                 return true;
