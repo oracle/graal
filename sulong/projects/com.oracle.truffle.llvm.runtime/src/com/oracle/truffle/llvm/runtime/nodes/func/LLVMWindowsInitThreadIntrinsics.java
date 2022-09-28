20b6ef1af60fecee609aa313095f10bb8806e4b0
Rudi Horn <rudi.horn@oracle.com>
2022-09-28 13:51:27 +0200

    Add Windows thread init intrinsics

---
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/nodes/func/LLVMWindowsInitThreadIntrinsics.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written
+ * permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.oracle.truffle.llvm.runtime.nodes.func;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.oracle.truffle.api.dsl.NodeChild;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.llvm.runtime.nodes.api.LLVMExpressionNode;
+import com.oracle.truffle.llvm.runtime.pthread.LLVMThreadException;
+
+public class LLVMWindowsInitThreadIntrinsics {
+    static ReentrantLock lock = new ReentrantLock();
+    static Condition condition = lock.newCondition();
+
+    public abstract static class InitThreadLock extends LLVMExpressionNode {
+        abstract Object execute();
+
+        @Specialization
+        public Object doLock() {
+            lock.lock();
+            return null;
+        }
+    }
+
+    public abstract static class InitThreadUnlock extends LLVMExpressionNode {
+        abstract Object execute();
+
+        @Specialization
+        public Object doUnlock() {
+            lock.unlock();
+            return null;
+        }
+    }
+
+    @NodeChild("timeout")
+    public abstract static class InitThreadWait extends LLVMExpressionNode {
+        abstract Object execute(int timeout);
+
+        @Specialization
+        public Object doWait(int timeout) {
+            try {
+                condition.await(timeout, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException ex) {
+                throw new LLVMThreadException(this, "Thread interrupted during _Init_thread_wait", ex);
+            }
+            return null;
+        }
+    }
+
+    public abstract static class InitThreadNotify extends LLVMExpressionNode {
+        abstract Object execute();
+
+        @Specialization
+        public Object doWait() {
+            lock.lock();
+            condition.signalAll();
+            lock.unlock();
+            return null;
+        }
+    }
+
+}
