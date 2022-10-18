7bc0b67a91c206ec6e68cf5abffbd04be31ca653
Marouane El Hallaoui <marouane.el.hallaoui@oracle.com>
2022-10-18 09:13:42 +0000

    [GR-41762] Backport to 21.3.5: Inconsistent decimal digits in string formatting for Romanian ROL currency across Java versions.
    PullRequest: js/2624

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "bc24e40c0174e10b8ccd9dacd36be357975418e4",
+                "version": "1544a63f1bf107e45ad7818d3ad83350d64b6486",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "bc24e40c0174e10b8ccd9dacd36be357975418e4",
+                "version": "1544a63f1bf107e45ad7818d3ad83350d64b6486",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
