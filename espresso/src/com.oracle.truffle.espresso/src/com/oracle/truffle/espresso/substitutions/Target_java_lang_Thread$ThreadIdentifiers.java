43618e95b86eeca2f3dfae97c2a682d97e523fd7
Bernhard Urban-Forster <bernhard.urban-forster@oracle.com>
2022-11-02 16:56:16 +0100

    jdk19: substitution for j.l.Thread$ThreadIdentifiers

---
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/substitutions/Target_java_lang_Thread$ThreadIdentifiers.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.oracle.truffle.espresso.substitutions;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@EspressoSubstitutions
+public final class Target_java_lang_Thread$ThreadIdentifiers {
+    private static volatile AtomicLong next_thread_id = new AtomicLong(2);
+
+    @Substitution
+    public static long next() {
+        return next_thread_id.getAndIncrement();
+    }
+}
