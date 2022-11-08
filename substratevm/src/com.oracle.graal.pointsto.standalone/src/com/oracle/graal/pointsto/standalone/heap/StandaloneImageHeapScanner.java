989316d68d11dc05caf59bc85955a08b63fe304e
Doug Simon <doug.simon@oracle.com>
2022-11-08 21:54:20 +0100

    fixed StandaloneImageHeapScanner

--- substratevm/src/com.oracle.graal.pointsto.standalone/src/com/oracle/graal/pointsto/standalone/heap/StandaloneImageHeapScanner.java
+++ substratevm/src/com.oracle.graal.pointsto.standalone/src/com/oracle/graal/pointsto/standalone/heap/StandaloneImageHeapScanner.java
@@ -26,6 +26,8 @@
 
 package com.oracle.graal.pointsto.standalone.heap;
 
+import org.graalvm.compiler.api.replacements.SnippetReflectionProvider;
+
 import com.oracle.graal.pointsto.BigBang;
 import com.oracle.graal.pointsto.ObjectScanningObserver;
 import com.oracle.graal.pointsto.heap.ImageHeap;
@@ -33,11 +35,10 @@ import com.oracle.graal.pointsto.heap.ImageHeapScanner;
 import com.oracle.graal.pointsto.heap.value.ValueSupplier;
 import com.oracle.graal.pointsto.meta.AnalysisField;
 import com.oracle.graal.pointsto.meta.AnalysisMetaAccess;
-import com.oracle.graal.pointsto.meta.UninitializedStaticFieldValueReader;
 import com.oracle.graal.pointsto.util.AnalysisError;
+
 import jdk.vm.ci.meta.ConstantReflectionProvider;
 import jdk.vm.ci.meta.JavaConstant;
-import org.graalvm.compiler.api.replacements.SnippetReflectionProvider;
 
 public class StandaloneImageHeapScanner extends ImageHeapScanner {
     private ClassLoader classLoader;
@@ -63,8 +64,15 @@ public class StandaloneImageHeapScanner extends ImageHeapScanner {
     protected ValueSupplier<JavaConstant> readHostedFieldValue(AnalysisField field, JavaConstant receiver) {
         ValueSupplier<JavaConstant> ret = super.readHostedFieldValue(field, receiver);
         if (ret.get() == null) {
-            JavaConstant constant = UninitializedStaticFieldValueReader.readUninitializedStaticValue(field, value -> universe.getSnippetReflection().forObject(value));
-            return ValueSupplier.eagerValue(constant);
+            /*
+             * Use the value from the constant pool attribute for the static field. That is the
+             * value before the class initializer is executed.
+             */
+            JavaConstant constantValue = field.getConstantValue();
+            if (constantValue == null) {
+                constantValue = JavaConstant.defaultForKind(field.getStorageKind());
+            }
+            return ValueSupplier.eagerValue(constantValue);
         } else {
             return ret;
         }
