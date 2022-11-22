53e15e0361cdb86d180e0ae903f2c2a37b3876fa
Andreas Woess <andreas.woess@oracle.com>
2022-11-22 12:04:40 +0000

    [GR-42337] Backport GR-42288 to 21.3: Returning void await this in async function.
    PullRequest: js/2643

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "1544a63f1bf107e45ad7818d3ad83350d64b6486",
+                "version": "a178a37506070cd5310a31f34feea0a0ef6c42fd",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "1544a63f1bf107e45ad7818d3ad83350d64b6486",
+                "version": "a178a37506070cd5310a31f34feea0a0ef6c42fd",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
