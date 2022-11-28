lusou-zhangquan/ziyi-forked-graal

Support delegating SVM's feature
Using DelegateFeature to delegate a compatible SVM feature in
standalone pointsto, so that the existing SVM feature can be reused.

DashboardDumpFeature is also modified for invoking from standalone
pointsto.

substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/PointsToAnalyzer.java
@@ -273,6 +273,9 @@ public final class PointsToAnalyzer {
     private void registerFeatures() {
         analysisFeatureManager.registerFeaturesFromOptions();
         analysisFeatureManager.registerFeature(PointstoClassInitializationFeature.class);
+        // Register DashboardDump feature by default, user can enable the feature by setting
+        // -H:+DumpAnalysisReports
+        analysisFeatureManager.registerFeature("com.oracle.graal.pointsto.standalone.features.DashboardDumpDelegate$Feature");
     }
 
     private void registerEntryMethod() {

substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/standalone/features/DelegateFeature.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (c) 2022, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2022, Alibaba Group Holding Limited. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.oracle.graal.pointsto.standalone.features;
+
+import com.oracle.graal.pointsto.util.AnalysisError;
+import com.oracle.svm.util.ReflectionUtil;
+import org.graalvm.compiler.options.OptionValues;
+import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.util.DirectAnnotationAccess;
+
+/**
+ * This class supports invoking SVM Features from standalone pointsto analysis. Some SVM features
+ * are useful in standalone pointsto analysis, but difficult to extract into the common place. For
+ * example, com.oracle.svm.hosted.dashboard.DashboardDumpFeature can dump pointsto results for
+ * further visualized analysis. We introduce the Feature delegating mechanism to reuse the features
+ * defined in SVM. User can extend this class to delegate an SVM feature.
+ * <p>
+ * The subclasses must be annotated with @{@link DelegateSVMFeature} to specify which SVM feature is
+ * delegated.
+ * <p>
+ * Sometimes, the SVM feature may need to set its functions via HostedOptionKey which is
+ * inaccessible from pointsto. We use {@link HostedOptionValueUpdater.HostedOptionValue} to store
+ * the option and reflectively update the option value before analysis starts.
+ * <p>
+ * An example usage of this class is {@link DashboardDumpDelegate.Feature}. Please refer it for more
+ * details.
+ */
+public abstract class DelegateFeature implements Feature {
+    protected Feature original;
+
+    @SuppressWarnings("unchecked")
+    public DelegateFeature(OptionValues options) {
+        DelegateSVMFeature annotation = DirectAnnotationAccess.getAnnotation(this.getClass(), DelegateSVMFeature.class);
+        if (annotation == null) {
+            String annotationName = DelegateSVMFeature.class.getName();
+            AnalysisError.shouldNotReachHere("Annotation " + annotationName + " is not found on class " + this.getClass().getName() + "\n. The subtype of " + DelegateFeature.class.getName() +
+                            " must have annotation " + annotationName + " to specify which original SVM feature is delegated.");
+        }
+        // Stop initialization if the feature is not enabled.
+        if (!isEnabled(options)) {
+            throw new FeatureNotEnabledException();
+        }
+        String originalFeatureName = annotation.value();
+        try {
+            Class<? extends Feature> originalFeatureClass = (Class<? extends Feature>) Class.forName(originalFeatureName);
+            Feature originalFeature = ReflectionUtil.lookupConstructor(originalFeatureClass).newInstance();
+            original = originalFeature;
+        } catch (ReflectiveOperationException e) {
+            AnalysisError.dependencyNotExist(originalFeatureName, "svm.jar", e);
+        }
+    }
+
+    /**
+     * The Feature instance of this class wouldn't get created if this method returns false.
+     * 
+     * @param options
+     * @return true if the feature is enabled.
+     */
+    protected abstract boolean isEnabled(OptionValues options);
+}

oracle/graal

substratevm/src/com.oracle.graal.pointsto.standalone/src/com/oracle/graal/pointsto/standalone/PointsToAnalyzer.java
@@ -277,6 +277,9 @@ public final class PointsToAnalyzer {
 
     private void registerFeatures() {
         standaloneAnalysisFeatureManager.registerFeaturesFromOptions();
+        // Register DashboardDump feature by default, user can enable the feature by setting
+        // -H:+DumpAnalysisReports
+        standaloneAnalysisFeatureManager.registerFeature("com.oracle.graal.pointsto.standalone.features.DashboardDumpDelegate$Feature");
     }
 
     private void registerEntryMethod() {

substratevm/src/com.oracle.graal.pointsto.standalone/src/com/oracle/graal/pointsto/standalone/features/DelegateFeature.java
@@ -1,0 +1,83 @@
+/*
+ * Copyright (c) 2022, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2022, Alibaba Group Holding Limited. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package com.oracle.graal.pointsto.standalone.features;
+
+import com.oracle.graal.pointsto.util.AnalysisError;
+import com.oracle.svm.util.ReflectionUtil;
+import org.graalvm.compiler.options.OptionValues;
+import org.graalvm.nativeimage.AnnotationAccess;
+import org.graalvm.nativeimage.hosted.Feature;
+
+/**
+ * This class supports invoking SVM Features from standalone pointsto analysis. Some SVM features
+ * are useful in standalone pointsto analysis, but difficult to extract into the common place. For
+ * example, com.oracle.svm.hosted.dashboard.DashboardDumpFeature can dump pointsto results for
+ * further visualized analysis. We introduce the Feature delegating mechanism to reuse the features
+ * defined in SVM. User can extend this class to delegate an SVM feature.
+ * <p>
+ * The subclasses must be annotated with @{@link DelegateSVMFeature} to specify which SVM feature is
+ * delegated.
+ * <p>
+ * Sometimes, the SVM feature may need to set its functions via HostedOptionKey which is
+ * inaccessible from pointsto. We use {@link HostedOptionValueUpdater.HostedOptionValue} to store
+ * the option and reflectively update the option value before analysis starts.
+ * <p>
+ * An example usage of this class is {@link DashboardDumpDelegate.Feature}. Please refer it for more
+ * details.
+ */
+public abstract class DelegateFeature implements Feature {
+    protected Feature original;
+
+    @SuppressWarnings("unchecked")
+    public DelegateFeature(OptionValues options) {
+        DelegateSVMFeature annotation = AnnotationAccess.getAnnotation(this.getClass(), DelegateSVMFeature.class);
+        if (annotation == null) {
+            String annotationName = DelegateSVMFeature.class.getName();
+            AnalysisError.shouldNotReachHere("Annotation " + annotationName + " is not found on class " + this.getClass().getName() + "\n. The subtype of " + DelegateFeature.class.getName() +
+                            " must have annotation " + annotationName + " to specify which original SVM feature is delegated.");
+        }
+        // Stop initialization if the feature is not enabled.
+        if (!isEnabled(options)) {
+            throw new FeatureNotEnabledException();
+        }
+        String originalFeatureName = annotation.value();
+        try {
+            Class<? extends Feature> originalFeatureClass = (Class<? extends Feature>) Class.forName(originalFeatureName);
+            original = ReflectionUtil.lookupConstructor(originalFeatureClass).newInstance();
+        } catch (ReflectiveOperationException e) {
+            AnalysisError.dependencyNotExist(originalFeatureName, "svm.jar", e);
+        }
+    }
+
+    /**
+     * The Feature instance of this class wouldn't get created if this method returns false.
+     * 
+     * @param options options
+     * @return true if the feature is enabled.
+     */
+    protected abstract boolean isEnabled(OptionValues options);
+}