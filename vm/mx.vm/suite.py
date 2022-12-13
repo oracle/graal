cbd56a060998428cb7f35e4ddbb8fe5e859d4ec5
Andreas Woess <andreas.woess@oracle.com>
2022-12-13 00:49:08 +0000

    [GR-42807] Ensure deleted elements are filled with hole values.
    PullRequest: js/2676

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "4f5e0e0d84c770af19d91f4d3dbad5157a892b51",
+                "version": "6b78b316db52fe792682653d760b677ac9b7a172",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "4f5e0e0d84c770af19d91f4d3dbad5157a892b51",
+                "version": "6b78b316db52fe792682653d760b677ac9b7a172",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
