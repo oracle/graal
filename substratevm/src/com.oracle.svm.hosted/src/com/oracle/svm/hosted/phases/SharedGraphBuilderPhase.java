b187cc688ddb88f8efc7871b890b5003fd5bdf74
Paul Wögerer <paul.woegerer@oracle.com>
2022-12-13 10:14:01 +0100

    Pass BootstrapMethodError as cause to UnresolvedElementException if class is linkAtBuildTime

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/phases/SharedGraphBuilderPhase.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/phases/SharedGraphBuilderPhase.java
@@ -325,7 +325,7 @@ public abstract class SharedGraphBuilderPhase extends GraphBuilderPhase.Instance
         @Override
         protected void handleBootstrapMethodError(BootstrapMethodError be, JavaMethod javaMethod) {
             if (linkAtBuildTime) {
-                reportUnresolvedElement("method", javaMethod.format("%H.%n(%P)"));
+                reportUnresolvedElement("method", javaMethod.format("%H.%n(%P)"), be);
             } else {
                 ExceptionSynthesizer.throwException(this, be.getClass(), javaMethod.format("%H.%n"));
             }
@@ -380,9 +380,13 @@ public abstract class SharedGraphBuilderPhase extends GraphBuilderPhase.Instance
         }
 
         private void reportUnresolvedElement(String elementKind, String elementAsString) {
+            reportUnresolvedElement(elementKind, elementAsString, null);
+        }
+
+        private void reportUnresolvedElement(String elementKind, String elementAsString, Throwable cause) {
             String message = "Discovered unresolved " + elementKind + " during parsing: " + elementAsString + ". " +
                             LinkAtBuildTimeSupport.singleton().errorMessageFor(method.getDeclaringClass());
-            throw new UnresolvedElementException(message);
+            throw new UnresolvedElementException(message, cause);
         }
 
         @Override
