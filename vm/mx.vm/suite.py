4b7c019c73d00f43f22e21657061cff920094ee9
Jan Stola <jan.stola@oracle.com>
2023-01-28 07:24:56 +0000

    [GR-23997] Periodic update of the graal import (2023-01-27).
    PullRequest: js/2710

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0445dd0f89795b0ffd1d103a809c8fda032c0a1b",
+                "version": "f0db8c7c7360eb8de2e5ca65149737798ea00c0a",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0445dd0f89795b0ffd1d103a809c8fda032c0a1b",
+                "version": "f0db8c7c7360eb8de2e5ca65149737798ea00c0a",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
