a4b076892ab4129cd4a46f0d2f9b6e439bac09e9
Ziyi Lin <cengfeng.lzy@alibaba-inc.com>
2023-02-02 16:25:59 +0800

    Adapt to upstream updates

--- substratevm/src/com.oracle.graal.pointsto.standalone/src/com/oracle/graal/pointsto/standalone/StandalonePointsToAnalysis.java
+++ substratevm/src/com.oracle.graal.pointsto.standalone/src/com/oracle/graal/pointsto/standalone/StandalonePointsToAnalysis.java
@@ -70,7 +70,7 @@ public class StandalonePointsToAnalysis extends PointsToAnalysis {
     public void onTypeReachable(AnalysisType type) {
         AnalysisMethod clinitMethod = type.getClassInitializer();
         if (clinitMethod != null && !addedClinits.contains(clinitMethod)) {
-            addRootMethod(clinitMethod, true).registerAsImplementationInvoked("static root method");
+            addRootMethod(clinitMethod, true);
             addedClinits.add(clinitMethod);
         }
     }
