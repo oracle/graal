c0f71449d37547bf8553e83ea43fbd7ec9e59443
Allan Gregersen <allan.gregersen@oracle.com>
2023-02-06 09:08:37 +0100

    make sure jdwp control thread is never suspended by truffle debugger

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/JDWPContextImpl.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/runtime/JDWPContextImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -692,7 +692,7 @@ public final class JDWPContextImpl implements JDWPContext {
 
     @Override
     public boolean isSystemThread() {
-        return classRedefinition.isRedefineThread();
+        return controller.isControlThread() || classRedefinition.isRedefineThread();
     }
 
     public long getBCI(Node rawNode, Frame frame) {
