9b09188ec304482d56b5ffcc6a6b45f58202fd38
Tristan Trouwen <tristan.trouwen@oracle.com>
2023-02-09 14:43:06 +0100

    Make getting upcall handle possible

--- truffle/src/com.oracle.truffle.nfi.backend.panama/src/com/oracle/truffle/nfi/backend/panama/ClosureArgumentNode.java
+++ truffle/src/com.oracle.truffle.nfi.backend.panama/src/com/oracle/truffle/nfi/backend/panama/ClosureArgumentNode.java
@@ -77,6 +77,9 @@ abstract class ClosureArgumentNode extends Node {
         @Override
         public Object execute(VirtualFrame frame) {
             Object[] args = (Object[]) frame.getArguments()[1];
+            if (index == args.length) { // TODO refactor
+                return frame.getArguments()[0];
+            }
             return args[index];
         }
     }
