dc6328393a11056840fb563423ea36afea95d397
Josef Haider <josef.haider@oracle.com>
2023-02-16 16:47:55 +0100

    TruffleStrings: remove problematic compiler assertion

--- regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/nodes/nfa/TRegexNFAExecutorNode.java
+++ regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/nodes/nfa/TRegexNFAExecutorNode.java
@@ -43,9 +43,6 @@ package com.oracle.truffle.regex.tregex.nodes.nfa;
 
 import static com.oracle.truffle.api.CompilerDirectives.injectBranchProbability;
 
-import org.graalvm.nativeimage.ImageInfo;
-
-import com.oracle.truffle.api.CompilerAsserts;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.strings.TruffleString;
@@ -156,8 +153,6 @@ public final class TRegexNFAExecutorNode extends TRegexExecutorNode {
         while (true) {
             if (dfaGeneratorBailedOut) {
                 locals.incLoopCount(this);
-            } else if (!ImageInfo.inImageCode()) {
-                CompilerAsserts.neverPartOfCompilation("NFA executor should have been replaced with DFA executor before compilation");
             }
             if (CompilerDirectives.inInterpreter()) {
                 RegexRootNode.checkThreadInterrupted();
