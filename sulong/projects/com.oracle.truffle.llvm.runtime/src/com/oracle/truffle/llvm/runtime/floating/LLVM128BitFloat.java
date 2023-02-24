e858b5bf90cc1873876ed7ccefc4f99f26a572af
Paley Li <paley.li@oracle.com>
2023-02-24 09:14:07 +0100

    Sulong: copyright fix

--- sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/floating/LLVM128BitFloat.java
+++ sulong/projects/com.oracle.truffle.llvm.runtime/src/com/oracle/truffle/llvm/runtime/floating/LLVM128BitFloat.java
@@ -27,7 +27,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package com.oracle.truffle.llvm.runtime.floating;
 
 import com.oracle.truffle.api.CompilerAsserts;
