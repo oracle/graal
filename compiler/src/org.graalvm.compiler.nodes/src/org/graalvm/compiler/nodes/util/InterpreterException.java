655b3949cea4276465344502690bb1077000250f
Mark Utting <m.utting@uq.edu.au>
2023-02-26 21:31:53 +1000

    Suppress serialization warning because we do not serialize interpreter exceptions.

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/util/InterpreterException.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/util/InterpreterException.java
@@ -1,9 +1,10 @@
 package org.graalvm.compiler.nodes.util;
 
 /**
- * This excpetion is thrown when the IR execution throws an exception, it is used
- * to distinguish IR exception with the exception caused by the interpreter malfunciton.
+ * This exception is thrown when the IR execution throws an exception, it is used
+ * to distinguish IR exceptions from exceptions caused by interpreter malfunctions.
  */
+@SuppressWarnings("serial")
 public class InterpreterException extends RuntimeException {
   public InterpreterException(Throwable cause) {
     super(cause);
