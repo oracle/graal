678c491ead4681dd45fd14eadd57259445200a5d
Christian Wirth <christian.wirth@oracle.com>
2023-02-26 19:28:03 +0100

    unused code in svm.hosted

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageGeneratorRunner.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageGeneratorRunner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -531,10 +531,6 @@ public class NativeImageGeneratorRunner {
             }
         }
         System.out.println(message);
-    }
-
-    public static String getJavaVersion() {
-        return System.getProperty("java.version");
     }
 
     /**
