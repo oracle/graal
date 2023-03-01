1e615285e00627b146e0a413a9cfa4af138fb6d8
Max Bernstein <max@bernsteinbear.com>
2023-03-01 18:23:06 -0500

    WIP: what's wrong with my treeset?

--- substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/typestate/SmallBitSet.java
+++ substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/typestate/SmallBitSet.java
@@ -5,52 +5,116 @@ import java.util.BitSet;
 import java.util.Iterator;
 import java.util.TreeSet;
 
-// This bitset does not support modification after it has been created.
+// // This bitset does not support modification after it has been created.
+// public class SmallBitSet {
+//     public SmallBitSet(BitSet other) {
+//         assert other.cardinality() <= MAX_CARDINALITY;
+//         for (int i = other.nextSetBit(0); i >= 0; i = other.nextSetBit(i+1)) {
+//             set[cardinality++] = i;
+//         }
+//         // Arrays.fill(set, UNSET, cardinality, MAX_CARDINALITY);
+//     }
+// 
+//     public boolean get(int bit) {
+//         assert bit >= 0: "get() bit must be non-negative";
+//         for (int i = 0; i < cardinality(); i++) {
+//             if (set[i] == bit) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// 
+//     public int cardinality() {
+//         return cardinality;
+//     }
+// 
+//     // // This is O(n^2) where n is MAX_CARDINALITY, so it's actually O(1).
+//     // public boolean isSuperSet(SmallBitSet other) {
+//     //     for (int i = 0; i < other.cardinality(); i++) {
+//     //         if (!get(other.set[i])) {
+//     //             return false;
+//     //         }
+//     //     }
+//     //     return true;
+//     // }
+// 
+// 
+//     private int lastSetBit() {
+//       assert cardinality > 0 : "SmallBitSet should never be empty";
+//       return nextSetBit(cardinality-1);
+//     }
+// 
+//     public BitSet asBitSet() {
+//         BitSet result = new BitSet(lastSetBit());
+//         for (int i = 0; i < cardinality(); i++) {
+//             result.set(set[i]);
+//         }
+//         // Clone will trim to size
+//         return (BitSet) result.clone();
+//     }
+// 
+//     public int nextSetBit(int fromIndex) {
+//         if (fromIndex < 0) {
+//             throw new IndexOutOfBoundsException();
+//         }
+//         if (fromIndex >= cardinality()) {
+//             return UNSET;
+//         }
+//         int result = set[fromIndex];
+//         // assert result != UNSET : "Should not have UNSET inside valid section of bitset";
+//         return result;
+//     }
+// 
+//     public String toString() {
+//         String result = "{";
+//         String sep = "";
+//         for (int i = 0; i < cardinality(); i++) {
+//             result += sep;
+//             result += String.valueOf(set[i]);
+//             sep = ", ";
+//         }
+//         result += "}";
+//         return result;
+//     }
+// 
+//     public String repr() {
+//         String result = "{";
+//         String sep = "";
+//         for (int i = 0; i < MAX_CARDINALITY; i++) {
+//             result += sep;
+//             result += String.valueOf(set[i]);
+//             sep = ", ";
+//         }
+//         result += "}";
+//         return result;
+//     }
+// 
+//     public static final int UNSET = -1;
+//     public static final int MAX_CARDINALITY = 10;
+//     int set[] = new int[MAX_CARDINALITY];
+//     int cardinality = 0;
+// }
+
 public class SmallBitSet {
     public SmallBitSet(BitSet other) {
         assert other.cardinality() <= MAX_CARDINALITY;
+        set = new TreeSet<Integer>();
         for (int i = other.nextSetBit(0); i >= 0; i = other.nextSetBit(i+1)) {
-            set[cardinality++] = i;
+            set.add(i);
         }
-        // Arrays.fill(set, UNSET, cardinality, MAX_CARDINALITY);
     }
 
     public boolean get(int bit) {
-        assert bit >= 0: "get() bit must be non-negative";
-        for (int i = 0; i < cardinality(); i++) {
-            if (set[i] == bit) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public int cardinality() {
-        return cardinality;
-    }
-
-    // // This is O(n^2) where n is MAX_CARDINALITY, so it's actually O(1).
-    // public boolean isSuperSet(SmallBitSet other) {
-    //     for (int i = 0; i < other.cardinality(); i++) {
-    //         if (!get(other.set[i])) {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-
-
-    private int lastSetBit() {
-      assert cardinality > 0 : "SmallBitSet should never be empty";
-      return nextSetBit(cardinality-1);
+        return set.contains(bit);
     }
 
     public BitSet asBitSet() {
-        BitSet result = new BitSet(lastSetBit());
-        for (int i = 0; i < cardinality(); i++) {
-            result.set(set[i]);
+        // System.out.printf("creating BitSet with cardinality %d and last bit %d\n", set.size(), set.last());
+        BitSet result = new BitSet(); //set.last());
+        for (Integer i : set) {
+            result.set(i);
         }
-        // Clone will trim to size
         return (BitSet) result.clone();
     }
 
@@ -58,78 +122,15 @@ public class SmallBitSet {
         if (fromIndex < 0) {
             throw new IndexOutOfBoundsException();
         }
-        if (fromIndex >= cardinality()) {
-            return UNSET;
+        for (Integer i : set) {
+            if (i >= fromIndex) {
+                return i;
+            }
         }
-        int result = set[fromIndex];
-        // assert result != UNSET : "Should not have UNSET inside valid section of bitset";
-        return result;
-    }
-
-    public String toString() {
-        String result = "{";
-        String sep = "";
-        for (int i = 0; i < cardinality(); i++) {
-            result += sep;
-            result += String.valueOf(set[i]);
-            sep = ", ";
-        }
-        result += "}";
-        return result;
-    }
-
-    public String repr() {
-        String result = "{";
-        String sep = "";
-        for (int i = 0; i < MAX_CARDINALITY; i++) {
-            result += sep;
-            result += String.valueOf(set[i]);
-            sep = ", ";
-        }
-        result += "}";
-        return result;
+        return UNSET;
     }
 
     public static final int UNSET = -1;
     public static final int MAX_CARDINALITY = 10;
-    int set[] = new int[MAX_CARDINALITY];
-    int cardinality = 0;
+    TreeSet<Integer> set;
 }
-
-// public class SmallBitSet {
-//     public SmallBitSet(BitSet other) {
-//         assert other.cardinality() <= MAX_CARDINALITY;
-//         set = new TreeSet<Integer>();
-//         for (int i = other.nextSetBit(0); i >= 0; i = other.nextSetBit(i+1)) {
-//             set.add(i);
-//         }
-//     }
-// 
-//     public boolean get(int bit) {
-//         return set.contains(bit);
-//     }
-// 
-//     public BitSet asBitSet() {
-//         BitSet result = new BitSet();
-//         for (Integer i : set) {
-//             result.set(i);
-//         }
-//         return result;
-//     }
-// 
-//     public int nextSetBit(int fromIndex) {
-//         if (fromIndex < 0) {
-//             throw new IndexOutOfBoundsException();
-//         }
-//         for (Integer i : set) {
-//             if (i >= fromIndex) {
-//                 return i;
-//             }
-//         }
-//         return UNSET;
-//     }
-// 
-//     public static final int UNSET = -1;
-//     public static final int MAX_CARDINALITY = 10;
-//     TreeSet<Integer> set;
-// }
