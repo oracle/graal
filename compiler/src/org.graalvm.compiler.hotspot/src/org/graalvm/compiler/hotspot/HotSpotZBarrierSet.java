1d8651c144020780bb9964fab4303a400879e9eb
Yudi Zheng <yudi.zheng@oracle.com>
2023-03-11 13:33:27 +0100

    add specialized handle locations.

--- compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/HotSpotZBarrierSet.java
+++ compiler/src/org.graalvm.compiler.hotspot/src/org/graalvm/compiler/hotspot/HotSpotZBarrierSet.java
@@ -24,7 +24,9 @@
  */
 package org.graalvm.compiler.hotspot;
 
+import static org.graalvm.compiler.hotspot.replacements.HotSpotReplacementsUtil.HOTSPOT_CARRIER_THREAD_OOP_HANDLE_LOCATION;
 import static org.graalvm.compiler.hotspot.replacements.HotSpotReplacementsUtil.HOTSPOT_CURRENT_THREAD_OOP_HANDLE_LOCATION;
+import static org.graalvm.compiler.hotspot.replacements.HotSpotReplacementsUtil.HOTSPOT_JAVA_THREAD_SCOPED_VALUE_CACHE_HANDLE_LOCATION;
 import static org.graalvm.compiler.hotspot.replacements.HotSpotReplacementsUtil.HOTSPOT_OOP_HANDLE_LOCATION;
 
 import org.graalvm.compiler.core.common.memory.BarrierType;
@@ -47,7 +49,8 @@ public class HotSpotZBarrierSet extends ZBarrierSet {
 
     @Override
     protected BarrierType barrierForLocation(BarrierType currentBarrier, LocationIdentity location, JavaKind storageKind) {
-        if (location.equals(HOTSPOT_OOP_HANDLE_LOCATION) || location.equals(HOTSPOT_CURRENT_THREAD_OOP_HANDLE_LOCATION)) {
+        if (location.equals(HOTSPOT_OOP_HANDLE_LOCATION) || location.equals(HOTSPOT_CURRENT_THREAD_OOP_HANDLE_LOCATION) || location.equals(HOTSPOT_CARRIER_THREAD_OOP_HANDLE_LOCATION) ||
+                        location.equals(HOTSPOT_JAVA_THREAD_SCOPED_VALUE_CACHE_HANDLE_LOCATION)) {
             return BarrierType.READ;
         }
         return super.barrierForLocation(currentBarrier, location, storageKind);
@@ -55,7 +58,8 @@ public class HotSpotZBarrierSet extends ZBarrierSet {
 
     @Override
     public BarrierType readBarrierType(LocationIdentity location, ValueNode address, Stamp loadStamp) {
-        if (location.equals(HOTSPOT_OOP_HANDLE_LOCATION) || location.equals(HOTSPOT_CURRENT_THREAD_OOP_HANDLE_LOCATION)) {
+        if (location.equals(HOTSPOT_OOP_HANDLE_LOCATION) || location.equals(HOTSPOT_CURRENT_THREAD_OOP_HANDLE_LOCATION) || location.equals(HOTSPOT_CARRIER_THREAD_OOP_HANDLE_LOCATION) ||
+                        location.equals(HOTSPOT_JAVA_THREAD_SCOPED_VALUE_CACHE_HANDLE_LOCATION)) {
             assert loadStamp instanceof AbstractObjectStamp : loadStamp;
             return BarrierType.READ;
         }
