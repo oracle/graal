f9a0b474b61007d8d96beb4c519f164127658826
Jan Stola <jan.stola@oracle.com>
2023-03-13 05:17:35 +0000

    [GR-44780] Do not use ICELAKE implementation of simdutf on macOS.
    PullRequest: js/2748

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "65df0be63983e4e93e015758eb1f88a8019c136a",
+                "version": "ed69b91e7113b05be4d28352ac7dbbc81ddd89c9",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "65df0be63983e4e93e015758eb1f88a8019c136a",
+                "version": "ed69b91e7113b05be4d28352ac7dbbc81ddd89c9",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
