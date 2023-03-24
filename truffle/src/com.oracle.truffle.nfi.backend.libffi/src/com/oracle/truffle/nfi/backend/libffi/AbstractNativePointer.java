0312370924ed093476c67825aedceb48c420dae5
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2023-03-24 19:18:49 +0100

    Use hex for LibFFILibrary AbstractNativePointer display strings

--- truffle/src/com.oracle.truffle.nfi.backend.libffi/src/com/oracle/truffle/nfi/backend/libffi/AbstractNativePointer.java
+++ truffle/src/com.oracle.truffle.nfi.backend.libffi/src/com/oracle/truffle/nfi/backend/libffi/AbstractNativePointer.java
@@ -90,6 +90,6 @@ abstract class AbstractNativePointer implements TruffleObject {
     @ExportMessage
     @TruffleBoundary
     Object toDisplayString(@SuppressWarnings("unused") boolean allowSideEffects) {
-        return "NativePointer(" + nativePointer + ")";
+        return String.format("NativePointer(0x%016x)", nativePointer);
     }
 }
