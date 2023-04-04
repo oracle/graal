04306951ec58002b56a8f130d4946e40d8f78c42
Fabio Niephaus <fabio.niephaus@oracle.com>
2023-04-04 09:03:38 +0200

    Access version / supportUrl without VM indirection.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/VM.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/VM.java
@@ -25,10 +25,8 @@
 package com.oracle.svm.core;
 
 import org.graalvm.nativeimage.ImageInfo;
-import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.Platform;
 import org.graalvm.nativeimage.Platforms;
-import org.graalvm.nativeimage.impl.ImageSingletonsSupport;
 
 public final class VM {
 
@@ -37,16 +35,19 @@ public final class VM {
     public final String vendor;
     public final String vendorUrl;
     public final String vendorVersion;
-    public final String supportURL;
 
     @Platforms(Platform.HOSTED_ONLY.class)
     public VM(String vmInfo) {
         info = vmInfo;
-        supportURL = System.getProperty("org.graalvm.supporturl", "https://graalvm.org/native-image/error-report/");
-        version = stripJVMCISuffix(System.getProperty("java.runtime.version"));
+        version = getVersion();
         vendor = System.getProperty("org.graalvm.vendor", "GraalVM Community");
         vendorUrl = System.getProperty("org.graalvm.vendorurl", "https://www.graalvm.org/");
         vendorVersion = getVendorVersion();
+    }
+
+    @Platforms(Platform.HOSTED_ONLY.class)
+    public static String getSupportUrl() {
+        return System.getProperty("org.graalvm.supporturl", "https://graalvm.org/native-image/error-report/");
     }
 
     public static String getVendorVersion() {
@@ -58,13 +59,8 @@ public final class VM {
     }
 
     @Platforms(Platform.HOSTED_ONLY.class)
-    public static VM getErrorReportingInstance() {
-        if (ImageSingletonsSupport.isInstalled() && ImageSingletons.contains(VM.class)) {
-            return ImageSingletons.lookup(VM.class);
-        } else {
-            // create a fall back instance
-            return new VM(System.getProperty("java.vm.info", ""));
-        }
+    public static String getVersion() {
+        return stripJVMCISuffix(System.getProperty("java.runtime.version"));
     }
 
     @Platforms(Platform.HOSTED_ONLY.class)
