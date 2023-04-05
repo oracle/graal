6bddcd345309382e6e7a2471cb966cc6a7da7bfa
Marouane El Hallaoui <marouane.el.hallaoui@oracle.com>
2023-04-05 18:42:05 +0000

    substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageViaCC.java edited online with Bitbucket
    fix compile issue

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageViaCC.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/image/NativeImageViaCC.java
@@ -456,7 +456,7 @@ public abstract class NativeImageViaCC extends NativeImage {
                     /* Provide an import library for the built shared library. */
                     Path importLib = inv.getTempDirectory().resolve(imageName + ".lib");
                     Path importLibCopy = Files.copy(importLib, imagePath.resolveSibling(importLib.getFileName()), StandardCopyOption.REPLACE_EXISTING);
-                    BuildArtifacts.singleton().add(ArtifactType.IMPORT_LIBRARY, importLibCopy);
+                    BuildArtifacts.singleton().add(ArtifactType.IMPORT_LIB, importLibCopy);
                     }
                 if (SubstrateOptions.GenerateDebugInfo.getValue() > 0) {
                     BuildArtifacts.singleton().add(ArtifactType.DEBUG_INFO, SubstrateOptions.getDebugInfoSourceCacheRoot());
