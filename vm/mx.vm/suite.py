0ddca9992e8628a0acce4cd37487115bb61d1a27
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2023-04-05 18:23:21 +0000

    [GR-45354] Fix globalThis in vm context.
    PullRequest: js/2773

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a8192749db284049545f28e6056a6fb33ae55945",
+                "version": "3bbe7e597c3c6e84da8c239afa450bf6791ca62a",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "a8192749db284049545f28e6056a6fb33ae55945",
+                "version": "3bbe7e597c3c6e84da8c239afa450bf6791ca62a",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
