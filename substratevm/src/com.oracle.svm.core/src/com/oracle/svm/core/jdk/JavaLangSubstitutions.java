be34c52da8c0911724f5dc31e13ee250d344a077
Josef Eisl <josef.eisl@oracle.com>
2023-04-18 14:52:20 +0200

    svm: fix typo in javadoc in fillInStackTrace

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaLangSubstitutions.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/JavaLangSubstitutions.java
@@ -305,9 +305,9 @@ final class Target_java_lang_Throwable {
 
         if (DeoptimizationSupport.enabled()) {
             /*
-             * Runtime compilation and deoptimized frames not yet optimized. Store the eagerly
-             * constructed stack trace in `backtrace`. We cannot directly use `stackTrace` because
-             * it is overwritten by the caller.
+             * Runtime compilation and deoptimized frames are not yet optimized. Eagerly construct a
+             * stack trace and store it in backtrace. We cannot directly use `stackTrace` because it
+             * is overwritten by the caller.
              */
             backtrace = JavaThreads.getStackTrace(true, Thread.currentThread());
             return this;
