4ab3d92db9041784d962b0479049e65f8c28945b
Tom Rodriguez <tom.rodriguez@oracle.com>
2023-04-27 09:05:56 -0700

    Eliminate use of InvocationPluginHelper.scale

--- compiler/src/org.graalvm.compiler.replacements.aarch64/src/org/graalvm/compiler/replacements/aarch64/AArch64GraphBuilderPlugins.java
+++ compiler/src/org.graalvm.compiler.replacements.aarch64/src/org/graalvm/compiler/replacements/aarch64/AArch64GraphBuilderPlugins.java
@@ -85,13 +85,13 @@ import org.graalvm.compiler.replacements.nodes.HasNegativesNode;
 import org.graalvm.compiler.replacements.nodes.ReverseBitsNode;
 import org.graalvm.compiler.replacements.nodes.UnaryMathIntrinsicNode;
 import org.graalvm.compiler.replacements.nodes.UnaryMathIntrinsicNode.UnaryOperation;
+import org.graalvm.compiler.serviceprovider.JavaVersionUtil;
 
 import jdk.vm.ci.code.Architecture;
 import jdk.vm.ci.code.CodeUtil;
 import jdk.vm.ci.meta.JavaKind;
 import jdk.vm.ci.meta.MetaAccessProvider;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
-import org.graalvm.compiler.serviceprovider.JavaVersionUtil;
 
 public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
     @Override
@@ -339,9 +339,9 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
                 // @formatter:off
                 //         if (injectBranchProbability(SLOWPATH_PROBABILITY, len < 0) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len > src.length) ||
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len |>| src.length) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex * 2 + len * 2 > dest.length)) {
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex + len |>| dest.length >> 1)) {
                 //            DeoptimizeNode.deopt(DeoptimizationAction.None, DeoptimizationReason.BoundsCheckException);
                 //        }
                 //
@@ -355,16 +355,16 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
 
                     helper.intrinsicRangeCheck(srcIndex, Condition.LT, ConstantNode.forInt(0));
                     ValueNode srcLength = helper.length(b.nullCheckedValue(src));
-                    helper.intrinsicRangeCheck(helper.add(srcIndex, len), Condition.GT, srcLength);
+                    helper.intrinsicRangeCheck(helper.add(srcIndex, len), Condition.AT, srcLength);
 
-                    ValueNode scaledDestIndex = helper.scale(destIndex, JavaKind.Char);
-                    helper.intrinsicRangeCheck(scaledDestIndex, Condition.LT, ConstantNode.forInt(0));
-                    ValueNode end = helper.add(scaledDestIndex, helper.scale(len, JavaKind.Char));
+                    helper.intrinsicRangeCheck(destIndex, Condition.LT, ConstantNode.forInt(0));
+                    ValueNode endIndex = helper.add(destIndex, len);
                     ValueNode destLength = helper.length(b.nullCheckedValue(dest));
-                    helper.intrinsicRangeCheck(end, Condition.GT, destLength);
+                    ValueNode shiftDestLength = helper.shr(destLength, 1);
+                    helper.intrinsicRangeCheck(endIndex, Condition.AT, shiftDestLength);
 
                     ValueNode srcPointer = helper.arrayElementPointer(src, JavaKind.Byte, srcIndex);
-                    ValueNode destPointer = helper.arrayElementPointer(dest, JavaKind.Byte, scaledDestIndex);
+                    ValueNode destPointer = helper.arrayElementPointerScaled(dest, JavaKind.Char, destIndex);
                     b.add(new StringLatin1InflateNode(srcPointer, destPointer, len, JavaKind.Byte));
                 }
                 return true;
@@ -377,9 +377,9 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
                 // @formatter:off
                 //         if (injectBranchProbability(SLOWPATH_PROBABILITY, len < 0) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len > src.length) ||
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len |>| src.length) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex + len > dest.length)) {
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex + len |>| dest.length)) {
                 //            DeoptimizeNode.deopt(DeoptimizationAction.None, DeoptimizationReason.BoundsCheckException);
                 //        }
                 //
@@ -393,12 +393,12 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
 
                     helper.intrinsicRangeCheck(srcIndex, Condition.LT, ConstantNode.forInt(0));
                     ValueNode srcLength = helper.length(b.nullCheckedValue(src));
-                    helper.intrinsicRangeCheck(helper.add(srcIndex, len), Condition.GT, srcLength);
+                    helper.intrinsicRangeCheck(helper.add(srcIndex, len), Condition.AT, srcLength);
 
                     helper.intrinsicRangeCheck(destIndex, Condition.LT, ConstantNode.forInt(0));
-                    ValueNode end = helper.add(destIndex, len);
+                    ValueNode endIndex = helper.add(destIndex, len);
                     ValueNode destLength = helper.length(b.nullCheckedValue(dest));
-                    helper.intrinsicRangeCheck(end, Condition.GT, destLength);
+                    helper.intrinsicRangeCheck(endIndex, Condition.AT, destLength);
 
                     ValueNode srcPointer = helper.arrayElementPointer(src, JavaKind.Byte, srcIndex);
                     ValueNode destPointer = helper.arrayElementPointer(dest, JavaKind.Char, destIndex);
@@ -436,13 +436,13 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
                 // @formatter:off
                 //         if (injectBranchProbability(SLOWPATH_PROBABILITY, len < 0) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len > src.length >> 1) ||
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len |>| src.length >> 1) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex + len > dest.length)) {
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex + len |>| dest.length)) {
                 //            DeoptimizeNode.deopt(DeoptimizationAction.None, DeoptimizationReason.BoundsCheckException);
                 //        }
                 //
-                //        Pointer srcPointer = Word.objectToTrackedPointer(src).add(byteArrayBaseOffset(INJECTED)).add(srcIndex * 2 * byteArrayIndexScale(INJECTED));
+                //        Pointer srcPointer = Word.objectToTrackedPointer(src).add(byteArrayBaseOffset(INJECTED)).add(srcIndex * charArrayIndexScale(INJECTED));
                 //        Pointer destPointer = Word.objectToTrackedPointer(dest).add(byteArrayBaseOffset(INJECTED)).add(destIndex * byteArrayIndexScale(INJECTED));
                 //        return StringUTF16CompressNode.compress(srcPointer, destPointer, len, JavaKind.Byte);
                 // @formatter:on
@@ -450,17 +450,17 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
                 try (InvocationPluginHelper helper = new InvocationPluginHelper(b, targetMethod)) {
                     helper.intrinsicRangeCheck(len, Condition.LT, ConstantNode.forInt(0));
 
-                    ValueNode scaledSrcIndex = helper.scale(srcIndex, JavaKind.Char);
-                    helper.intrinsicRangeCheck(scaledSrcIndex, Condition.LT, ConstantNode.forInt(0));
-                    ValueNode end = helper.add(scaledSrcIndex, helper.scale(len, JavaKind.Char));
+                    helper.intrinsicRangeCheck(srcIndex, Condition.LT, ConstantNode.forInt(0));
+                    ValueNode endIndex = helper.add(destIndex, len);
                     ValueNode srcLength = helper.length(b.nullCheckedValue(src));
-                    helper.intrinsicRangeCheck(end, Condition.GT, srcLength);
+                    ValueNode shiftedSrcLength = helper.shr(srcLength, 1);
+                    helper.intrinsicRangeCheck(endIndex, Condition.AT, shiftedSrcLength);
 
                     helper.intrinsicRangeCheck(destIndex, Condition.LT, ConstantNode.forInt(0));
                     ValueNode destLength = helper.length(b.nullCheckedValue(dest));
-                    helper.intrinsicRangeCheck(helper.add(destIndex, len), Condition.GT, destLength);
+                    helper.intrinsicRangeCheck(helper.add(destIndex, len), Condition.AT, destLength);
 
-                    ValueNode srcPointer = helper.arrayElementPointer(src, JavaKind.Byte, scaledSrcIndex);
+                    ValueNode srcPointer = helper.arrayElementPointerScaled(src, JavaKind.Char, srcIndex);
                     ValueNode destPointer = helper.arrayElementPointer(dest, JavaKind.Byte, destIndex);
                     b.addPush(JavaKind.Int, new StringUTF16CompressNode(srcPointer, destPointer, len, JavaKind.Byte));
                 }
@@ -474,9 +474,9 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
                 // @formatter:off
                 //         if (injectBranchProbability(SLOWPATH_PROBABILITY, len < 0) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len > src.length) ||
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, srcIndex + len |>| src.length) ||
                 //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex < 0) ||
-                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex + len > dest.length)) {
+                //                        injectBranchProbability(SLOWPATH_PROBABILITY, destIndex + len |>| dest.length)) {
                 //            DeoptimizeNode.deopt(DeoptimizationAction.None, DeoptimizationReason.BoundsCheckException);
                 //        }
                 //
@@ -488,13 +488,13 @@ public class AArch64GraphBuilderPlugins implements TargetGraphBuilderPlugins {
                     helper.intrinsicRangeCheck(len, Condition.LT, ConstantNode.forInt(0));
 
                     helper.intrinsicRangeCheck(srcIndex, Condition.LT, ConstantNode.forInt(0));
-                    ValueNode end = helper.add(srcIndex, len);
+                    ValueNode endIndex = helper.add(srcIndex, len);
                     ValueNode srcLength = helper.length(b.nullCheckedValue(src));
-                    helper.intrinsicRangeCheck(end, Condition.GT, srcLength);
+                    helper.intrinsicRangeCheck(endIndex, Condition.AT, srcLength);
 
                     helper.intrinsicRangeCheck(destIndex, Condition.LT, ConstantNode.forInt(0));
                     ValueNode destLength = helper.length(b.nullCheckedValue(dest));
-                    helper.intrinsicRangeCheck(helper.add(destIndex, len), Condition.GT, destLength);
+                    helper.intrinsicRangeCheck(helper.add(destIndex, len), Condition.AT, destLength);
 
                     ValueNode srcPointer = helper.arrayElementPointer(src, JavaKind.Char, srcIndex);
                     ValueNode destPointer = helper.arrayElementPointer(dest, JavaKind.Byte, destIndex);
