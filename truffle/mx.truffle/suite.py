7add1f4f920928581cb43c1ea4646642beecd2c1
Matt D'Souza <matt.dsouza@oracle.com>
2023-05-01 14:08:17 -0400

    fix missing Unsafe requirement; parametrize tests (while testing runtime compilation)

--- truffle/mx.truffle/suite.py
+++ truffle/mx.truffle/suite.py
@@ -500,6 +500,9 @@ suite = {
         "mx:JUNIT",
         "mx:JMH_1_21",
       ],
+      "requires" : [
+        "jdk.unsupported", # sun.misc.Unsafe
+      ],
       "checkstyle" : "com.oracle.truffle.dsl.processor",
       "javaCompliance" : "17+",
       "annotationProcessors" : ["mx:JMH_1_21", "TRUFFLE_DSL_PROCESSOR"],
@@ -584,7 +587,8 @@ suite = {
       ],
       "requires" : [
         "java.compiler",
-        "jdk.management"
+        "jdk.management",
+        "jdk.unsupported", # sun.misc.Unsafe
       ],
       "checkstyle" : "com.oracle.truffle.dsl.processor",
       "javaCompliance" : "17+",
