c6e6d302c8144241f6f5d21e8ddaf2a84bc615df
Gergö Barany <gergo.barany@oracle.com>
2023-05-15 16:01:40 +0200

    Helpers for creating arithmetic nodes and conversions

--- compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/nodes/calc/UnaryArithmeticNode.java
+++ compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/nodes/calc/UnaryArithmeticNode.java
@@ -26,17 +26,23 @@ package org.graalvm.compiler.nodes.calc;
 
 import static org.graalvm.compiler.nodes.calc.BinaryArithmeticNode.getArithmeticOpTable;
 
+import java.util.Set;
+
 import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 import org.graalvm.compiler.core.common.type.ArithmeticOpTable.UnaryOp;
+import org.graalvm.compiler.core.common.type.FloatStamp;
+import org.graalvm.compiler.core.common.type.IntegerStamp;
 import org.graalvm.compiler.core.common.type.Stamp;
+import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.graph.NodeClass;
-import org.graalvm.compiler.nodes.spi.CanonicalizerTool;
 import org.graalvm.compiler.nodeinfo.NodeInfo;
 import org.graalvm.compiler.nodes.ArithmeticOperation;
 import org.graalvm.compiler.nodes.ConstantNode;
 import org.graalvm.compiler.nodes.NodeView;
+import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.spi.ArithmeticLIRLowerable;
+import org.graalvm.compiler.nodes.spi.CanonicalizerTool;
 
 @NodeInfo
 public abstract class UnaryArithmeticNode<OP> extends UnaryNode implements ArithmeticOperation, ArithmeticLIRLowerable {
@@ -62,6 +68,46 @@ public abstract class UnaryArithmeticNode<OP> extends UnaryNode implements Arith
     public Stamp foldStamp(Stamp newStamp) {
         assert newStamp.isCompatible(getValue().stamp(NodeView.DEFAULT));
         return getOp(getValue()).foldStamp(newStamp);
+    }
+
+    public static ValueNode unaryIntegerOp(StructuredGraph graph, ValueNode v, NodeView view, ArithmeticOpTable.UnaryOp<?> op) {
+        return graph.addOrUniqueWithInputs(unaryIntegerOp(v, view, op));
+    }
+
+    public static ValueNode unaryIntegerOp(ValueNode v, NodeView view, ArithmeticOpTable.UnaryOp<?> op) {
+        if (IntegerStamp.OPS.getNeg().equals(op)) {
+            return NegateNode.create(v, view);
+        } else if (IntegerStamp.OPS.getNot().equals(op)) {
+            return NotNode.create(v);
+        } else if (IntegerStamp.OPS.getAbs().equals(op)) {
+            return AbsNode.create(v, view);
+        } else if (Set.of(IntegerStamp.OPS.getUnaryOps()).contains(op)) {
+            GraalError.unimplemented(String.format("creating %s via UnaryArithmeticNode#unaryIntegerOp is not implemented yet", op));
+        } else {
+            GraalError.shouldNotReachHere(String.format("%s is not a unary operation!", op));
+        }
+        return null;
+    }
+
+    public static ValueNode unaryFloatOp(StructuredGraph graph, ValueNode v, NodeView view, ArithmeticOpTable.UnaryOp<?> op) {
+        return graph.addOrUniqueWithInputs(unaryFloatOp(v, view, op));
+    }
+
+    public static ValueNode unaryFloatOp(ValueNode v, NodeView view, ArithmeticOpTable.UnaryOp<?> op) {
+        if (FloatStamp.OPS.getNeg().equals(op)) {
+            return NegateNode.create(v, view);
+        } else if (FloatStamp.OPS.getNot().equals(op)) {
+            return NotNode.create(v);
+        } else if (FloatStamp.OPS.getAbs().equals(op)) {
+            return AbsNode.create(v, view);
+        } else if (FloatStamp.OPS.getSqrt().equals(op)) {
+            return SqrtNode.create(v, view);
+        } else if (Set.of(FloatStamp.OPS.getUnaryOps()).contains(op)) {
+            GraalError.unimplemented(String.format("creating %s via UnaryArithmeticNode#unaryFloatOp is not implemented yet", op));
+        } else {
+            GraalError.shouldNotReachHere(String.format("%s is not a unary operation!", op));
+        }
+        return null;
     }
 
     @Override
