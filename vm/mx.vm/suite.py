55806442753b75f40cd0182dbb39417c7d6d43a6
Ossama Ismaili <ossama.ismaili@oracle.com>
2023-05-31 13:19:02 +0000

    [GR-45763] Backport 22.3 : Upload JavaScript data to testing dashboard.
    PullRequest: js/2800

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "7187529be65b88335870ae93ad1a548894f40ef2",
+                "version": "68d63badde5187bd4175afda80f33c6cf69592fc",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "7187529be65b88335870ae93ad1a548894f40ef2",
+                "version": "68d63badde5187bd4175afda80f33c6cf69592fc",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
