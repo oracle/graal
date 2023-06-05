b430cb1e410b26c80e6bc2e4ce417c85c6c7884b
Foivos Zakkak <fzakkak@redhat.com>
2023-06-05 17:37:24 +0300

    Add jdk.internal.platform to built-in package natives
    Enables invocations to
    `jdk.internal.platform.CgroupMetrics#getTotalSwapSize0` introduced by
    JDK-8299858
    
    Closes: #6739

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/PlatformNativeLibrarySupport.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/PlatformNativeLibrarySupport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -98,6 +98,12 @@ public abstract class PlatformNativeLibrarySupport {
         builtInPkgNatives = new ArrayList<>();
         if (Platform.includedIn(InternalPlatform.PLATFORM_JNI.class)) {
             builtInPkgNatives.addAll(Arrays.asList(defaultBuiltInPkgNatives));
+            /*
+             * Support builtin methods for Containers and Metrics internal API which is Linux only
+             */
+            if (Platform.includedIn(Platform.LINUX.class)) {
+                builtInPkgNatives.add("jdk_internal_platform");
+            }
         }
     }
 
