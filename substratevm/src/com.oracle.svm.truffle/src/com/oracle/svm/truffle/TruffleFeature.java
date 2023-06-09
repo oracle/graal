e7f30465de199d54a8d1c07a29063e4ec35fc5dc
Josef Eisl <josef.eisl@oracle.com>
2023-06-09 13:50:09 +0200

    Revert "svm: disable Throwable#getOurStackTrace in TruffleFeature"
    This reverts commit 27a1cc8648b5e358ce25b2581acea314853c0222.

--- substratevm/src/com.oracle.svm.truffle/src/com/oracle/svm/truffle/TruffleFeature.java
+++ substratevm/src/com.oracle.svm.truffle/src/com/oracle/svm/truffle/TruffleFeature.java
@@ -583,7 +583,6 @@ public class TruffleFeature implements InternalFeature {
         blocklistMethod(metaAccess, Throwable.class, "fillInStackTrace");
         // Implementations which don't call Throwable.fillInStackTrace are allowed
         implementationOnlyBlocklist(metaAccess, Throwable.class, "fillInStackTrace");
-        blocklistMethod(metaAccess, Throwable.class, "getOurStackTrace");
         blocklistMethod(metaAccess, Throwable.class, "initCause", Throwable.class);
         blocklistMethod(metaAccess, Throwable.class, "addSuppressed", Throwable.class);
         blocklistMethod(metaAccess, System.class, "getProperty", String.class);
