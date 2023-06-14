ca0771fecd5eb0f22fc4b91ccaa9a4d646aca5c1
Josef Eisl <josef.eisl@oracle.com>
2023-06-14 09:32:40 +0200

    svm: better error message if preview features are not enabled but required by the class file

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageGeneratorRunner.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/NativeImageGeneratorRunner.java
@@ -434,11 +434,15 @@ public class NativeImageGeneratorRunner {
                     } catch (ClassNotFoundException ex) {
                         throw UserError.abort(classLoader.getMainClassNotFoundErrorMessage(className));
                     } catch (UnsupportedClassVersionError ex) {
-                        throw UserError.abort("Unable to load '%s' due to a Java version mismatch.%n" +
-                                        "Please take one of the following actions:%n" +
-                                        " 1) Recompile the source files for your application using Java %s, then try running native-image again%n" +
-                                        " 2) Use a version of native-image corresponding to the version of Java with which you compiled the source files for your application%n",
-                                        className, Runtime.version().feature());
+                        if (ex.getMessage().startsWith("Preview features are not enabled")) {
+                            throw UserError.abort(ex.getMessage());
+                        } else {
+                            throw UserError.abort("Unable to load '%s' due to a Java version mismatch.%n" +
+                                            "Please take one of the following actions:%n" +
+                                            " 1) Recompile the source files for your application using Java %s, then try running native-image again%n" +
+                                            " 2) Use a version of native-image corresponding to the version of Java with which you compiled the source files for your application%n",
+                                            className, Runtime.version().feature());
+                        }
                     }
                     String mainEntryPointName = SubstrateOptions.Method.getValue(parsedHostedOptions);
                     if (mainEntryPointName.isEmpty()) {
