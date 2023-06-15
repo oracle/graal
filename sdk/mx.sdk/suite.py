39e5f2914665b9904963413b2f4a4c871945600b
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2023-06-15 14:31:59 +0200

    Update mx version.

--- sdk/mx.sdk/suite.py
+++ sdk/mx.sdk/suite.py
@@ -39,7 +39,7 @@
 # SOFTWARE.
 #
 suite = {
-  "mxversion": "6.25.2",
+  "mxversion": "6.27.1",
   "name" : "sdk",
   "version" : "23.1.0",
   "release" : False,
--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -1,7 +1,7 @@
 suite = {
     "name": "vm",
     "version" : "23.1.0",
-    "mxversion": "6.24.0",
+    "mxversion": "6.27.1",
     "release" : False,
     "groupId" : "org.graalvm",
 
