175ab887c23b0ef14677964a320218865a102281
dominikmascherbauer <dominik.mascherbauer@jku.at>
2023-06-20 17:32:57 +0200

    Use ContainerSupport for building containerized native image bundles

--- substratevm/src/com.oracle.svm.driver.launcher/src/com/oracle/svm/driver/launcher/ContainerSupport.java
+++ substratevm/src/com.oracle.svm.driver.launcher/src/com/oracle/svm/driver/launcher/ContainerSupport.java
@@ -32,15 +32,16 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 public class ContainerSupport {
-    // TODO put anything container related in here, maybe even with function references(for logging), take care
-
     public String containerTool;
     public String bundleContainerTool;
     public String containerToolVersion;
@@ -49,17 +50,28 @@ public class ContainerSupport {
     public String bundleContainerImage;
     public Path dockerfile;
 
-    private static final List<String> SUPPORTED_CONTAINER_TOOLS = List.of("podman", "docker");
-    private static final String CONTAINER_TOOL_JSON_KEY = "containerTool";
-    private static final String CONTAINER_TOOL_VERSION_JSON_KEY = "containerToolVersion";
-    private static final String CONTAINER_IMAGE_JSON_KEY = "containerImage";
+    public static final List<String> SUPPORTED_CONTAINER_TOOLS = List.of("podman", "docker");
+    public static final String CONTAINER_TOOL_JSON_KEY = "containerTool";
+    public static final String CONTAINER_TOOL_VERSION_JSON_KEY = "containerToolVersion";
+    public static final String CONTAINER_IMAGE_JSON_KEY = "containerImage";
 
     private static final String BUNDLE_INFO_MESSAGE_PREFIX = "Native Image Bundles: ";
 
     public static final Path CONTAINER_GRAAL_VM_HOME = Path.of("/graalvm");
 
+    private final BiFunction<String, Throwable, Error> errorFunction;
+    private final Consumer<String> warningPrinter;
+    private final Consumer<String> messagePrinter;
+
     public ContainerSupport(Path dockerfile, Path bundleStageDir) {
+        this(dockerfile, bundleStageDir, Error::new, (msg) -> System.out.println("Warning: " + msg), System.out::println);
+    }
+
+    public ContainerSupport(Path dockerfile, Path bundleStageDir, BiFunction<String, Throwable, Error> errorFunction, Consumer<String> warningPrinter, Consumer<String> messagePrinter) {
         this.dockerfile = dockerfile;
+        this.errorFunction = errorFunction;
+        this.warningPrinter = warningPrinter;
+        this.messagePrinter = messagePrinter;
 
         if (bundleStageDir != null) {
             Path containerFile = bundleStageDir.resolve("container.json");
@@ -71,11 +83,11 @@ public class ContainerSupport {
                     bundleContainerTool = containerSettings.getOrDefault(CONTAINER_TOOL_JSON_KEY, bundleContainerTool);
                     bundleContainerToolVersion = containerSettings.getOrDefault(CONTAINER_TOOL_VERSION_JSON_KEY, bundleContainerToolVersion);
                 } catch (IOException e) {
-                    throw new Error("Failed to read bundle-file " + containerFile, e);
+                    throw errorFunction.apply("Failed to read bundle-file " + containerFile, e);
                 }
                 if (bundleContainerTool != null) {
                     String containerToolVersionString = bundleContainerToolVersion == null ? "" : String.format(" (%s)", bundleContainerToolVersion);
-                    System.out.printf("%sBundled native-image was created in a container with %s%s.%n", BUNDLE_INFO_MESSAGE_PREFIX, bundleContainerTool, containerToolVersionString);
+                    messagePrinter.accept(String.format("%sBundled native-image was created in a container with %s%s.%n", BUNDLE_INFO_MESSAGE_PREFIX, bundleContainerTool, containerToolVersionString));
                 }
             }
         }
@@ -85,11 +97,11 @@ public class ContainerSupport {
         try {
             containerImage = BundleLauncherUtil.digest(Files.readString(dockerfile));
         } catch (IOException e) {
-            throw new Error("Could not read Dockerfile " + dockerfile);
+            throw errorFunction.apply("Could not read Dockerfile " + dockerfile, e);
         }
 
         if (bundleContainerImage != null && !bundleContainerImage.equals(containerImage)) {
-            System.out.println("Warning: The bundled image was created with a different dockerfile.");
+            warningPrinter.accept("The bundled image was created with a different dockerfile.");
         }
 
         if (bundleContainerTool != null && containerTool == null) {
@@ -98,24 +110,24 @@ public class ContainerSupport {
 
         if (containerTool != null) {
             if (!isToolAvailable(containerTool)) {
-                throw new Error("Configured container tool not available.");
+                throw errorFunction.apply("Configured container tool not available.", null);
             } else if (containerTool.equals("docker") && !isRootlessDocker()) {
-                throw new Error("Only rootless docker is supported for containerized builds.");
+                throw errorFunction.apply("Only rootless docker is supported for containerized builds.", null);
             }
             containerToolVersion = getContainerToolVersion(containerTool);
 
             if (bundleContainerTool != null) {
                 if (!containerTool.equals(bundleContainerTool)) {
-                    System.out.printf("Warning: The bundled image was created with container tool '%s' (using '%s').%n", bundleContainerTool, containerTool);
+                    warningPrinter.accept(String.format("The bundled image was created with container tool '%s' (using '%s').%n", bundleContainerTool, containerTool));
                 } else if (containerToolVersion != null && bundleContainerToolVersion != null && !containerToolVersion.equals(bundleContainerToolVersion)) {
-                    System.out.printf("Warning: The bundled image was created with different %s version '%s' (installed '%s').%n", containerTool, bundleContainerToolVersion, containerToolVersion);
+                    warningPrinter.accept(String.format("The bundled image was created with different %s version '%s' (installed '%s').%n", containerTool, bundleContainerToolVersion, containerToolVersion));
                 }
             }
         } else {
             for (String tool : SUPPORTED_CONTAINER_TOOLS) {
                 if (isToolAvailable(tool)) {
                     if (tool.equals("docker") && !isRootlessDocker()) {
-                        System.out.println(BUNDLE_INFO_MESSAGE_PREFIX + "Rootless context missing for docker.");
+                        messagePrinter.accept(BUNDLE_INFO_MESSAGE_PREFIX + "Rootless context missing for docker.");
                         continue;
                     }
                     containerTool = tool;
@@ -124,7 +136,7 @@ public class ContainerSupport {
                 }
             }
             if (containerTool == null) {
-                throw new Error(String.format("Please install one of the following tools before running containerized native image builds: %s", SUPPORTED_CONTAINER_TOOLS));
+                throw errorFunction.apply(String.format("Please install one of the following tools before running containerized native image builds: %s", SUPPORTED_CONTAINER_TOOLS), null);
             }
         }
 
@@ -139,7 +151,7 @@ public class ContainerSupport {
         if (imageId == null) {
             pb.inheritIO();
         } else {
-            System.out.printf("%sReusing container image %s.%n", BUNDLE_INFO_MESSAGE_PREFIX, containerImage);
+            messagePrinter.accept(String.format("%sReusing container image %s.%n", BUNDLE_INFO_MESSAGE_PREFIX, containerImage));
         }
 
         Process p = null;
@@ -148,13 +160,13 @@ public class ContainerSupport {
             int status = p.waitFor();
             if (status == 0 && imageId != null && !imageId.equals(getFirstProcessResultLine(pbCheckForImage))) {
                 try (var processResult = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
-                    System.out.printf("%sUpdated container image %s.%n", BUNDLE_INFO_MESSAGE_PREFIX, containerImage);
-                    processResult.lines().forEach(System.out::println);
+                    messagePrinter.accept(String.format("%sUpdated container image %s.%n", BUNDLE_INFO_MESSAGE_PREFIX, containerImage));
+                    processResult.lines().forEach(messagePrinter);
                 }
             }
             return status;
         } catch (IOException | InterruptedException e) {
-            throw new Error(e.getMessage());
+            throw errorFunction.apply(e.getMessage(), e);
         } finally {
             if (p != null) {
                 p.destroy();
@@ -162,23 +174,23 @@ public class ContainerSupport {
         }
     }
 
-    private static boolean isToolAvailable(String tool) {
+    private boolean isToolAvailable(String tool) {
         return Arrays.stream(System.getenv("PATH").split(":"))
                 .map(str -> Path.of(str).resolve(tool))
                 .anyMatch(Files::isExecutable);
     }
 
-    private static String getContainerToolVersion(String tool) {
+    private String getContainerToolVersion(String tool) {
         ProcessBuilder pb = new ProcessBuilder(tool, "--version");
         return getFirstProcessResultLine(pb);
     }
 
-    private static boolean isRootlessDocker() {
+    private boolean isRootlessDocker() {
         ProcessBuilder pb = new ProcessBuilder("docker", "context", "show");
         return getFirstProcessResultLine(pb).equals("rootless");
     }
 
-    private static String getFirstProcessResultLine(ProcessBuilder pb) {
+    private String getFirstProcessResultLine(ProcessBuilder pb) {
         Process p = null;
         try {
             p = pb.start();
@@ -187,7 +199,7 @@ public class ContainerSupport {
                 return processResult.readLine();
             }
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e.getMessage());
+            throw errorFunction.apply(e.getMessage(), e);
         } finally {
             if (p != null) {
                 p.destroy();
@@ -196,6 +208,22 @@ public class ContainerSupport {
     }
 
     public record TargetPath(Path path, boolean readonly) {
+        public static TargetPath readonly(Path target) {
+            return of(target, true);
+        }
+
+        public static TargetPath of(Path target, boolean readonly) {
+            return new TargetPath(target, readonly);
+        }
+    }
+
+    public static Map<Path, TargetPath> mountMappingFor(Path javaHome, Path inputDir, Path outputDir) {
+        Map<Path, TargetPath> mountMapping = new HashMap<>();
+        Path containerRoot = Paths.get("/");
+        mountMapping.put(javaHome, TargetPath.readonly(containerRoot.resolve(CONTAINER_GRAAL_VM_HOME)));
+        mountMapping.put(inputDir, ContainerSupport.TargetPath.readonly(containerRoot.resolve(BundleLauncher.INPUT_DIR_NAME)));
+        mountMapping.put(outputDir, ContainerSupport.TargetPath.of(containerRoot.resolve(BundleLauncher.OUTPUT_DIR_NAME), false));
+        return mountMapping;
     }
 
     public List<String> createContainerCommand(Map<String, String> containerEnvironment, Map<Path, TargetPath> mountMapping) {
