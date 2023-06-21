0bb59013621f859ad8a5e2f7d6b5988001e1021d
Andrej Pecimuth <andrej.pecimuth@oracle.com>
2023-06-21 12:17:31 +0200

    Add profdiff tests for AOT-related commands.

--- compiler/src/org.graalvm.profdiff.test/src/org/graalvm/profdiff/test/MainTest.java
+++ compiler/src/org.graalvm.profdiff.test/src/org/graalvm/profdiff/test/MainTest.java
@@ -48,6 +48,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.graalvm.profdiff.parser.ExperimentParser.CODE;
+import static org.graalvm.profdiff.parser.ExperimentParser.COMPILATION_AOT;
+import static org.graalvm.profdiff.parser.ExperimentParser.COMPILATION_KIND;
 import static org.graalvm.profdiff.parser.ExperimentParser.COMPILE_ID;
 import static org.graalvm.profdiff.parser.ExperimentParser.EXECUTION_ID;
 import static org.graalvm.profdiff.parser.ExperimentParser.GRAAL_COMPILER_LEVEL;
@@ -123,7 +125,7 @@ public class MainTest {
 
     @Test
     public void reportEmptyDirectory() throws IOException {
-        Path tmpDir = new OptimizationLogMock("profdiff_report_empty").writeToTempDirectory();
+        Path tmpDir = new OptimizationLogMock("profdiff_empty").writeToTempDirectory();
         try {
             String[] args = {"report", tmpDir.toAbsolutePath().toString()};
             Profdiff.main(args);
@@ -135,7 +137,7 @@ public class MainTest {
 
     @Test
     public void reportOptimizationLog() throws IOException {
-        OptimizationLogMock optLog = new OptimizationLogMock("profdiff_report_logs");
+        OptimizationLogMock optLog = new OptimizationLogMock("profdiff_logs");
         optLog.addLogFile().addCompilationUnit("foo()", "10-foo");
         optLog.addLogFile().addCompilationUnit("bar()", "20-bar").addCompilationUnit("baz()", "30-baz").addCompilationUnit("foo()", "40-foo");
         Path logDir = optLog.writeToTempDirectory();
@@ -151,12 +153,12 @@ public class MainTest {
 
     @Test
     public void reportExperiment() throws IOException {
-        OptimizationLogMock optLog = new OptimizationLogMock("profdiff_report_experiment");
+        OptimizationLogMock optLog = new OptimizationLogMock("profdiff_report");
         optLog.addLogFile().addCompilationUnit("foo()", "10-foo").addCompilationUnit("foo()", "20-foo");
         optLog.addLogFile().addCompilationUnit("bar()", "30-bar").addCompilationUnit("baz()", "40-baz");
         Path logDir = optLog.writeToTempDirectory();
-        ProftoolJITMock proftool = new ProftoolJITMock("profdiff_report_profile", "1000");
-        proftool.addOSRMethod("foo()", "10-foo", 10).addGraalMethod("foo()", "20-foo", 20).addGraalMethod("bar()", "30-bar", 30).addForeignMethod("foreign_method", 40);
+        ProftoolJITMock proftool = new ProftoolJITMock("profdiff_report_prof", "1000").addOSRMethod("foo()", "10-foo", 10).addGraalMethod("foo()", "20-foo", 20).addGraalMethod("bar()", "30-bar",
+                        30).addForeignMethod("foreign_method", 40);
         Path profFile = proftool.writeToTempFile();
         try {
             String[] args = {"report", logDir.toAbsolutePath().toString(), profFile.toAbsolutePath().toString()};
@@ -175,37 +177,132 @@ public class MainTest {
 
     @Test
     public void compareJITExperiments() throws IOException {
-        OptimizationLogMock optLog1 = new OptimizationLogMock("profdiff_compare_jit_1");
-        optLog1.addLogFile().addCompilationUnit("foo()", "10-foo")
-                        .addCompilationUnit("bar()", "20-bar");
-        Path logDir1 = optLog1.writeToTempDirectory();
-        ProftoolJITMock proftool1 = new ProftoolJITMock("profdiff_compare_jit_prof_1", "1000");
-        proftool1.addGraalMethod("foo()", "10-foo", 60)
-                        .addGraalMethod("bar()", "20-bar", 40);
-        Path profFile1 = proftool1.writeToTempFile();
-
-        OptimizationLogMock optLog2 = new OptimizationLogMock("profdiff_compare_jit_2");
-        optLog2.addLogFile().addCompilationUnit("foo()", "30-foo")
-                        .addCompilationUnit("baz()", "40-baz");
-        Path logDir2 = optLog2.writeToTempDirectory();
-        ProftoolJITMock proftool2 = new ProftoolJITMock("profdiff_compare_jit_prof_2", "2000");
-        proftool2.addGraalMethod("foo()", "30-foo", 30)
-                        .addGraalMethod("baz()", "40-baz", 70);
-        Path profFile2 = proftool2.writeToTempFile();
-
+        ExperimentMock exp1 = createJITExperiment1("profdiff_jit_1", "profdiff_jit_prof_1");
+        ExperimentMock exp2 = createJITExperiment2("profdiff_jit_2", "profdiff_jit_prof_2");
         try {
-            String[] args = {"jit-vs-jit", logDir1.toAbsolutePath().toString(), profFile1.toAbsolutePath().toString(),
-                            logDir2.toAbsolutePath().toString(), profFile2.toAbsolutePath().toString()};
+            String[] args = {"jit-vs-jit", exp1.logDir.toAbsolutePath().toString(), exp1.profFile.toAbsolutePath().toString(),
+                            exp2.logDir.toAbsolutePath().toString(), exp2.profFile.toAbsolutePath().toString()};
             Profdiff.main(args);
         } finally {
+            exp1.delete();
+            exp2.delete();
+        }
+        assertNoError();
+        assertOutputContains(exp1.optLog.allCompilationIDs());
+        assertOutputContains(exp2.optLog.allCompilationIDs());
+    }
+
+    @Test
+    public void compareAOTExperiments() throws IOException {
+        ExperimentMock exp1 = createAOTExperiment1("profdiff_aot_1", "profdiff_aot_prof_1");
+        ExperimentMock exp2 = createAOTExperiment2("profdiff_aot_2", "profdiff_aot_prof_2");
+        try {
+            String[] args = {"--hot-percentile", "1", "aot-vs-aot", exp1.logDir.toAbsolutePath().toString(), exp1.profFile.toAbsolutePath().toString(),
+                            exp2.logDir.toAbsolutePath().toString(), exp2.profFile.toAbsolutePath().toString()};
+            Profdiff.main(args);
+        } finally {
+            exp1.delete();
+            exp2.delete();
+        }
+        assertNoError();
+        assertOutputContains(exp1.optLog.allCompilationIDs());
+        assertOutputContains(exp2.optLog.allCompilationIDs());
+    }
+
+    @Test
+    public void compareJITAndAOT() throws IOException {
+        ExperimentMock exp1 = createJITExperiment1("profdiff_jit_aot_1", "profdiff_jit_aot_prof_1");
+        ExperimentMock exp2 = createAOTExperiment2("profdiff_jit_aot_2", "profdiff_jit_aot_prof_2");
+        try {
+            String[] args = {"jit-vs-aot", exp1.logDir.toAbsolutePath().toString(), exp1.profFile.toAbsolutePath().toString(),
+                            exp2.logDir.toAbsolutePath().toString(), exp2.profFile.toAbsolutePath().toString()};
+            Profdiff.main(args);
+        } finally {
+            exp1.delete();
+            exp2.delete();
+        }
+        assertNoError();
+        assertOutputContains(exp1.optLog.allCompilationIDs());
+        assertOutputContains(exp2.optLog.allCompilationIDs());
+    }
+
+    @Test
+    public void compareAOTExperimentsWithExternalProfile() throws IOException {
+        ExperimentMock exp = createJITExperiment1("profdiff_jit_aot_ext_0", "profdiff_jit_aot_ext_prof_0");
+
+        OptimizationLogMock optLog1 = new OptimizationLogMock("profdiff_jit_aot_ext_1");
+        optLog1.addLogFile().addCompilationUnit("foo()", "10-foo").addCompilationUnit("bar()", "20-bar");
+        Path logDir1 = optLog1.writeToTempDirectory();
+
+        OptimizationLogMock optLog2 = new OptimizationLogMock("profdiff_jit_aot_ext_2");
+        optLog2.addLogFile().addCompilationUnit("foo()", "30-foo").addCompilationUnit("baz()", "40-baz");
+        Path logDir2 = optLog2.writeToTempDirectory();
+
+        try {
+            String[] args = {"aot-vs-aot-jit-profile", exp.logDir.toAbsolutePath().toString(), exp.profFile.toAbsolutePath().toString(),
+                            logDir1.toAbsolutePath().toString(), logDir2.toAbsolutePath().toString()};
+            Profdiff.main(args);
+        } finally {
+            exp.delete();
             deleteTree(logDir1);
-            Files.delete(profFile1);
             deleteTree(logDir2);
-            Files.delete(profFile2);
         }
         assertNoError();
         assertOutputContains(optLog1.allCompilationIDs());
-        assertOutputContains(optLog2.allCompilationIDs());
+    }
+
+    /**
+     * Holds the files related to an experiment mock.
+     *
+     * @param optLog the mocked the optimization log
+     * @param logDir the path to the optimization log directory
+     * @param profFile the path to the file with proftool profiles
+     */
+    private record ExperimentMock(OptimizationLogMock optLog, Path logDir, Path profFile) {
+        void delete() throws IOException {
+            deleteTree(logDir);
+            Files.delete(profFile);
+        }
+    }
+
+    private ExperimentMock createJITExperiment1(String directoryPrefix, String profPrefix) throws IOException {
+        OptimizationLogMock optLog = new OptimizationLogMock(directoryPrefix);
+        optLog.addLogFile().addCompilationUnit("foo()", "10-foo").addCompilationUnit("bar()", "20-bar");
+        Path logDir = optLog.writeToTempDirectory();
+        ProftoolJITMock proftool = new ProftoolJITMock(profPrefix, "1000");
+        proftool.addGraalMethod("foo()", "10-foo", 60).addGraalMethod("bar()", "20-bar", 40);
+        Path profFile = proftool.writeToTempFile();
+        return new ExperimentMock(optLog, logDir, profFile);
+    }
+
+    private ExperimentMock createJITExperiment2(String directoryPrefix, String profPrefix) throws IOException {
+        OptimizationLogMock optLog = new OptimizationLogMock(directoryPrefix);
+        optLog.addLogFile().addCompilationUnit("foo()", "30-foo").addCompilationUnit("baz()", "40-baz");
+        Path logDir = optLog.writeToTempDirectory();
+        ProftoolJITMock proftool = new ProftoolJITMock(profPrefix, "2000");
+        proftool.addGraalMethod("foo()", "30-foo", 30).addGraalMethod("baz()", "40-baz", 70);
+        Path profFile = proftool.writeToTempFile();
+        return new ExperimentMock(optLog, logDir, profFile);
+    }
+
+    private ExperimentMock createAOTExperiment1(String directoryPrefix, String profPrefix) throws IOException {
+        OptimizationLogMock optLog = new OptimizationLogMock(directoryPrefix);
+        optLog.addLogFile().addCompilationUnit("foo()", "10-foo").addCompilationUnit("bar()", "20-bar");
+        Path logDir = optLog.writeToTempDirectory();
+        ProftoolAOTMock proftool = new ProftoolAOTMock(profPrefix);
+        proftool.addMethod("foo()", 50).addMethod("bar()", 30).addMethod("foreign_method()", 20);
+        Path profFile = proftool.writeToTempFile();
+        return new ExperimentMock(optLog, logDir, profFile);
+    }
+
+    private ExperimentMock createAOTExperiment2(String directoryPrefix, String profPrefix) throws IOException {
+        OptimizationLogMock optLog = new OptimizationLogMock(directoryPrefix);
+        optLog.addLogFile().addCompilationUnit("foo()", "30-foo").addCompilationUnit("baz()", "40-baz");
+        Path logDir = optLog.writeToTempDirectory();
+        ProftoolAOTMock proftool = new ProftoolAOTMock(profPrefix);
+        proftool.addMethod("foo()", 30).addMethod("baz()", 50).addMethod("foreign_method()", 20);
+        Path profFile = proftool.writeToTempFile();
+        return new ExperimentMock(optLog, logDir, profFile);
     }
 
     /**
@@ -409,14 +506,42 @@ public class MainTest {
     }
 
     /**
-     * Mocks a proftool profile for a JIT experiment.
+     * Mocks a proftool profile for an experiment (either JIT or AOT).
      */
-    private static final class ProftoolJITMock {
-
+    private abstract static class ProftoolMock {
         /**
          * The prefix of the file where the profile should be saved.
          */
         private final String fileNamePrefix;
+
+        protected ProftoolMock(String fileNamePrefix) {
+            this.fileNamePrefix = fileNamePrefix;
+        }
+
+        /**
+         * Formats and returns the profile as a JSON map.
+         */
+        protected abstract EconomicMap<String, Object> asJSONMap();
+
+        /**
+         * Writes the profile to a temporary file.
+         *
+         * @return the path to the temporary file with the profile
+         * @throws IOException failed to write the profile to a file
+         */
+        Path writeToTempFile() throws IOException {
+            Path path = Files.createTempFile(Paths.get("."), fileNamePrefix, ".json");
+            try (FileWriter writer = new FileWriter(path.toFile())) {
+                writer.write(JSONFormatter.formatJSON(asJSONMap()));
+            }
+            return path;
+        }
+    }
+
+    /**
+     * Mocks a proftool profile for a JIT experiment.
+     */
+    private static final class ProftoolJITMock extends ProftoolMock {
 
         /**
          * The execution ID of the experiment.
@@ -429,7 +554,7 @@ public class MainTest {
         private final List<ProftoolJITMethodMock> methodMocks;
 
         private ProftoolJITMock(String fileNamePrefix, String executionID) {
-            this.fileNamePrefix = fileNamePrefix;
+            super(fileNamePrefix);
             this.executionID = executionID;
             this.methodMocks = new ArrayList<>();
         }
@@ -458,29 +583,65 @@ public class MainTest {
             return this;
         }
 
-        /**
-         * Formats and returns the profile as a JSON map.
-         */
-        EconomicMap<String, Object> asJSONMap() {
+        @Override
+        protected EconomicMap<String, Object> asJSONMap() {
             EconomicMap<String, Object> map = EconomicMap.create();
             map.put(EXECUTION_ID, executionID);
             map.put(TOTAL_PERIOD, methodMocks.stream().mapToLong(ProftoolJITMethodMock::period).sum());
             map.put(CODE, methodMocks.stream().map(ProftoolJITMethodMock::toJSONMap).collect(Collectors.toList()));
             return map;
         }
+    }
+
+    /**
+     * Mocks a native method recorded in an AOT profile.
+     *
+     * @param methodName the name of the method (Java or foreign)
+     * @param period the total recorded period for the given method
+     */
+    private record ProftoolAOTMethodMock(String methodName, long period) {
 
         /**
-         * Writes the profile to a temporary file.
-         *
-         * @return the path to the temporary file with the profile
-         * @throws IOException failed to write the profile to a file
+         * Formats the method as a JSON map.
          */
-        Path writeToTempFile() throws IOException {
-            Path path = Files.createTempFile(Paths.get("."), fileNamePrefix, ".json");
-            try (FileWriter writer = new FileWriter(path.toFile())) {
-                writer.write(JSONFormatter.formatJSON(asJSONMap()));
-            }
-            return path;
+        EconomicMap<String, Object> toJSONMap() {
+            EconomicMap<String, Object> map = EconomicMap.create();
+            map.put(NAME, methodName);
+            map.put(PERIOD, period);
+            return map;
+        }
+    }
+
+    /**
+     * Mocks a proftool profile for an AOT experiment.
+     */
+    private static final class ProftoolAOTMock extends ProftoolMock {
+
+        /**
+         * The list of recorded methods in the profile.
+         */
+        private final List<ProftoolAOTMethodMock> methodMocks;
+
+        private ProftoolAOTMock(String fileNamePrefix) {
+            super(fileNamePrefix);
+            this.methodMocks = new ArrayList<>();
+        }
+
+        /**
+         * Adds a method to the profile.
+         */
+        ProftoolAOTMock addMethod(String methodName, long period) {
+            methodMocks.add(new ProftoolAOTMethodMock(methodName, period));
+            return this;
+        }
+
+        @Override
+        protected EconomicMap<String, Object> asJSONMap() {
+            EconomicMap<String, Object> map = EconomicMap.create();
+            map.put(COMPILATION_KIND, COMPILATION_AOT);
+            map.put(TOTAL_PERIOD, methodMocks.stream().mapToLong(ProftoolAOTMethodMock::period).sum());
+            map.put(CODE, methodMocks.stream().map(ProftoolAOTMethodMock::toJSONMap).collect(Collectors.toList()));
+            return map;
         }
     }
 }
