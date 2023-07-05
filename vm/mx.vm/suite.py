cfbf0ea53e3bcf5e04576934dbd2de68093536cd
Florian Angerer <florian.angerer@oracle.com>
2023-07-05 22:14:28 +0000

    [GR-21590] Update Python imports.
    PullRequest: graalpython/2856

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -65,7 +65,7 @@ suite = {
             },
             {
                 "name": "graalpython",
-                "version": "349135f2d9ae1ba881608d3c0ee8e7390ce85fde",
+                "version": "19b363d95d3c0b7396da392c71125aacfe66b506",
                 "dynamic": True,
                 "urls": [
                     {"url": "https://github.com/graalvm/graalpython.git", "kind": "git"},
