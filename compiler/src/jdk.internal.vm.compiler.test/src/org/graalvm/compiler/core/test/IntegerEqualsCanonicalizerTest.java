bc9ff12a28cf8b10edd0962310bbb7710ceb2d9f
Raphael Mosaner <raphael.m.mosaner@oracle.com>
2023-07-06 13:04:21 +0200

    [GR-46915] Remove erroneous canonicalization during creation of IntegerEqualsNode.

--- compiler/src/jdk.internal.vm.compiler.test/src/org/graalvm/compiler/core/test/IntegerEqualsCanonicalizerTest.java
+++ compiler/src/jdk.internal.vm.compiler.test/src/org/graalvm/compiler/core/test/IntegerEqualsCanonicalizerTest.java
@@ -24,13 +24,28 @@
  */
 package org.graalvm.compiler.core.test;
 
+import java.util.Optional;
+
+import org.graalvm.compiler.api.directives.GraalDirectives;
+import org.graalvm.compiler.core.GraalCompiler;
+import org.graalvm.compiler.nodes.ConstantNode;
 import org.graalvm.compiler.nodes.FrameState;
+import org.graalvm.compiler.nodes.GraphState;
+import org.graalvm.compiler.nodes.LogicNode;
+import org.graalvm.compiler.nodes.NodeView;
+import org.graalvm.compiler.nodes.ReturnNode;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.StructuredGraph.AllowAssumptions;
 import org.graalvm.compiler.nodes.calc.ConditionalNode;
 import org.graalvm.compiler.nodes.calc.XorNode;
 import org.graalvm.compiler.nodes.java.StoreFieldNode;
 import org.graalvm.compiler.phases.common.CanonicalizerPhase;
+import org.graalvm.compiler.nodes.ValueNode;
+import org.graalvm.compiler.nodes.calc.IntegerEqualsNode;
+import org.graalvm.compiler.phases.BasePhase;
+import org.graalvm.compiler.phases.common.FinalCanonicalizerPhase;
+import org.graalvm.compiler.phases.tiers.LowTierContext;
+import org.graalvm.compiler.phases.tiers.Suites;
 import org.junit.Test;
 
 public class IntegerEqualsCanonicalizerTest extends GraalCompilerTest {
@@ -253,6 +268,54 @@ public class IntegerEqualsCanonicalizerTest extends GraalCompilerTest {
 
     public static void testXorEqualsZeroReference(int x, int y) {
         field = x == y ? 1 : 0;
+    }
+
+    @Test
+    public void testInsertIntegerEquals() {
+        StructuredGraph graph = parseEager("testInsertIntegerEqualsSnippet", AllowAssumptions.NO);
+
+        Suites suites = createSuites(graph.getOptions());
+        int idx = suites.getLowTier().findPhase(FinalCanonicalizerPhase.class).previousIndex();
+
+        suites.getLowTier().insertAtIndex(idx, new BasePhase<LowTierContext>() {
+
+            @Override
+            protected void run(StructuredGraph g, LowTierContext context) {
+                // make "a == 0 ? 3 : b" to "(a == 0 ? 3 : b) == 3 ? 1 : 0"
+                ValueNode cond = (ValueNode) g.getNodes().stream().filter(n -> n instanceof ConditionalNode).toList().get(0);
+                ValueNode y = g.addOrUnique(ConstantNode.forInt(3, g));
+
+                LogicNode iEq = g.addOrUnique(IntegerEqualsNode.create(cond, y, NodeView.DEFAULT));
+                ValueNode cond2 = g.addOrUnique(ConditionalNode.create(iEq, ConstantNode.forInt(1, g), ConstantNode.forInt(0, g), NodeView.DEFAULT));
+
+                cond.replaceAtUsages(cond2, n -> n instanceof ReturnNode);
+            }
+
+            @Override
+            public Optional<NotApplicable> notApplicableTo(GraphState graphState) {
+                return ALWAYS_APPLICABLE;
+            }
+        });
+
+        GraalCompiler.emitFrontEnd(getProviders(), getBackend(), graph, getDefaultGraphBuilderSuite(), getOptimisticOptimizations(), graph.getProfilingInfo(), suites);
+
+        // create reference snippet
+        StructuredGraph ref = parseEager("testInsertIntegerEqualsReference", AllowAssumptions.NO);
+        GraalCompiler.emitFrontEnd(getProviders(), getBackend(), ref, getDefaultGraphBuilderSuite(), getOptimisticOptimizations(), ref.getProfilingInfo(), createSuites(ref.getOptions()));
+
+        assertEquals(ref, graph);
+    }
+
+    public static int testInsertIntegerEqualsSnippet(int a, int b) {
+        int result = a == 0 ? 3 : b;
+        GraalDirectives.controlFlowAnchor();
+        return result;
+    }
+
+    public static int testInsertIntegerEqualsReference(int a, int b) {
+        int result = (a == 0 ? 3 : b) == 3 ? 1 : 0;
+        GraalDirectives.controlFlowAnchor();
+        return result;
     }
 
     private void test(String snippet, String referenceSnippet) {
