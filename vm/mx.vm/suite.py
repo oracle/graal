84069263e8634372dd3f5a38d7056f9ab8e6790e
Andreas Woess <andreas.woess@oracle.com>
2023-07-18 09:29:48 +0000

    [GR-46913] Allow setting more JS options in SandboxPolicy.CONSTRAINED.
    PullRequest: js/2860

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "e1c4cd892cb5247fed72a166bfdb7d09b4067c39",
+                "version": "5660d69438259402350be3975f110cb5d8e26b9c",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "e1c4cd892cb5247fed72a166bfdb7d09b4067c39",
+                "version": "5660d69438259402350be3975f110cb5d8e26b9c",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
