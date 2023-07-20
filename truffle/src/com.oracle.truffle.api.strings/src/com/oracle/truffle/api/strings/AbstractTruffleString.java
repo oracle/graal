a7d0905cac8fb869b3f18997f4f297779827b96c
Josef Haider <josef.haider@oracle.com>
2023-07-20 14:54:23 +0200

    TruffleString: fix NPE in AbstractTruffleString.equals()

--- truffle/src/com.oracle.truffle.api.strings/src/com/oracle/truffle/api/strings/AbstractTruffleString.java
+++ truffle/src/com.oracle.truffle.api.strings/src/com/oracle/truffle/api/strings/AbstractTruffleString.java
@@ -1302,7 +1302,7 @@ public abstract class AbstractTruffleString {
                 return false;
             }
         }
-        return TruffleString.EqualNode.checkContentEquals(null, this, b,
+        return TruffleString.EqualNode.checkContentEquals(TruffleString.EqualNode.getUncached(), this, b,
                         ToIndexableNodeGen.getUncached(),
                         ToIndexableNodeGen.getUncached(),
                         InlinedConditionProfile.getUncached(),
@@ -1432,7 +1432,7 @@ public abstract class AbstractTruffleString {
 
         @TruffleBoundary
         private static void copy(Node location, TruffleString src, byte[] dst, int dstFrom, int dstStride) {
-            Object arrayA = ToIndexableNodeGen.getUncached().execute(null, src, src.data());
+            Object arrayA = ToIndexableNodeGen.getUncached().execute(location, src, src.data());
             TStringOps.arraycopyWithStride(location,
                             arrayA, src.offset(), src.stride(), 0,
                             dst, 0, dstStride, dstFrom, src.length());
