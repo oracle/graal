a62e802c1da1a0657edf828ffc42de2036848ff0
Christian Humer <christian.humer@oracle.com>
2023-07-24 22:54:12 +0200

    Fix initializeEngineCacheSupport was never initializing the engine cache support.

--- truffle/src/com.oracle.truffle.runtime/src/com/oracle/truffle/runtime/OptimizedTruffleRuntime.java
+++ truffle/src/com.oracle.truffle.runtime/src/com/oracle/truffle/runtime/OptimizedTruffleRuntime.java
@@ -218,7 +218,7 @@ public abstract class OptimizedTruffleRuntime implements TruffleRuntime, Truffle
     }
 
     public final void initializeEngineCacheSupport(EngineCacheSupport support) {
-        if (engineCacheSupport == null) {
+        if (this.engineCacheSupport instanceof EngineCacheSupport.Disabled) {
             this.engineCacheSupport = support;
             this.runtimeOptionDescriptors.add(support.getEngineOptions());
         }
