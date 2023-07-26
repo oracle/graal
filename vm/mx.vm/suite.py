a450875e29420921af73ef18d90f963d580f3d8e
Jirka Marsik <jiri.marsik@oracle.com>
2023-07-26 14:48:18 +0200

    Bump js import

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "77a4265db2ab84acdf914b16bb7462ff9a3ce5f4",
+                "version": "aa0547deae836361fab063dbd398999fcdc5dd58",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "77a4265db2ab84acdf914b16bb7462ff9a3ce5f4",
+                "version": "aa0547deae836361fab063dbd398999fcdc5dd58",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
