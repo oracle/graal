8d68669ef6fe234a861fa83d11a7fcb99f4346f2
Tomas Zezula <tomas.zezula@oracle.com>
2023-08-07 18:29:50 +0200

    Added graalpython meta-POM.

--- vm/mx.vm/mx_vm.py
+++ vm/mx.vm/mx_vm.py
@@ -263,6 +263,7 @@ def _register_all_languages_distribution(register_distribution):
     """
     community_languages = [
         'graal-js:GRAALJS_COMMUNITY',
+        'graalpyton:GRAALPYTHON_COMMUNITY',
         'sulong:SULONG_COMMUNITY',
         'wasm:WASM_COMMUNITY',
     ]
