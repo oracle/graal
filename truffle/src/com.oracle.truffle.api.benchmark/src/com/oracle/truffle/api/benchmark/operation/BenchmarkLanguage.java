5256debafa2da2ca69c2b705ac5e3a7639ed3ab7
Matt D'Souza <matt.dsouza@oracle.com>
2023-08-08 12:03:48 -0400

    Move benchmarks to truffle folder

--- truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/operation/BenchmarkLanguage.java
+++ truffle/src/com.oracle.truffle.api.benchmark/src/com/oracle/truffle/api/benchmark/operation/BenchmarkLanguage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -38,7 +38,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.oracle.truffle.api.operation.test.bml;
+package com.oracle.truffle.api.benchmark.operation;
 
 import java.util.HashMap;
 import java.util.Map;
