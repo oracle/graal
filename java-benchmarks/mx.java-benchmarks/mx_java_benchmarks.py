47ae9faf25812cf0f77a3eafcf2b6c7cd70d5567
Olya Gupalo <olga.gupalo@oracle.com>
2023-08-11 13:27:02 +0300

    Add missing license headers to files reported by mx

--- java-benchmarks/mx.java-benchmarks/mx_java_benchmarks.py
+++ java-benchmarks/mx.java-benchmarks/mx_java_benchmarks.py
@@ -1,7 +1,5 @@
 #
-# ----------------------------------------------------------------------------------------------------
-#
-# Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -24,7 +22,6 @@
 # or visit www.oracle.com if you need additional information or have any
 # questions.
 #
-# ----------------------------------------------------------------------------------------------------
 
 import sys
 import re
