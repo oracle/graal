7fbe2f0dc2243138cc236382e527fcec5b5ec47d
Jan Stola <jan.stola@oracle.com>
2023-08-25 07:02:55 +0000

    [GR-46765] Upgrading the underlying Node.js to version 18.17.1.
    PullRequest: js/2868

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0d157c23908acb77b1e30de3128d907663ea5ab7",
+                "version": "d0f359c29ec97722301997359ee878ea053da19e",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "0d157c23908acb77b1e30de3128d907663ea5ab7",
+                "version": "d0f359c29ec97722301997359ee878ea053da19e",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
