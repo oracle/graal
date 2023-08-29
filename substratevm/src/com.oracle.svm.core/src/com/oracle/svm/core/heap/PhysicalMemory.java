838a5aaaee54fe4dfbf9c0b88bf18e6da254106b
Christian Haeubl <christian.haeubl@oracle.com>
2023-08-29 17:35:23 +0200

    Minor fix.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/PhysicalMemory.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/PhysicalMemory.java
@@ -29,8 +29,8 @@ import org.graalvm.word.UnsignedWord;
 import org.graalvm.word.WordFactory;
 
 import com.oracle.svm.core.Containers;
-import com.oracle.svm.core.Uninterruptible;
 import com.oracle.svm.core.SubstrateOptions;
+import com.oracle.svm.core.Uninterruptible;
 import com.oracle.svm.core.jdk.UninterruptibleUtils.AtomicInteger;
 import com.oracle.svm.core.stack.StackOverflowCheck;
 import com.oracle.svm.core.thread.PlatformThreads;
@@ -59,6 +59,7 @@ public class PhysicalMemory {
     private static final UnsignedWord UNSET_SENTINEL = UnsignedUtils.MAX_VALUE;
     private static UnsignedWord cachedSize = UNSET_SENTINEL;
 
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     public static boolean isInitialized() {
         return cachedSize != UNSET_SENTINEL;
     }
@@ -101,7 +102,6 @@ public class PhysicalMemory {
         }
 
         return cachedSize;
-    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     }
 
     /**
