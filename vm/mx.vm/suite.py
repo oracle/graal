f664277a41ff27e8cd5ac778af0f8809b4f4bc0f
Fabio Niephaus <fabio.niephaus@oracle.com>
2023-08-31 15:34:28 +0200

    Update TruffleRuby import.

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -49,7 +49,7 @@ suite = {
             },
             {
                 "name": "truffleruby",
-                "version": "2ed10973e791d80ca7333f2a7eba543e23bd2c6c",
+                "version": "4c27ab012054c1a0e7dbff338d0a7b5282114816",
                 "dynamic": True,
                 "urls": [
                     {"url": "https://github.com/oracle/truffleruby.git", "kind": "git"},
