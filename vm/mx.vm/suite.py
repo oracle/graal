fb39e2f15ea3042062a1717ce611e3839409667a
Benoit Daloze <benoit.daloze@oracle.com>
2023-09-05 17:51:18 +0200

    [GR-47432] Move maven downloader from vm to sdk suite
    * So languages do not need to import /vm to use it.

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -83,19 +83,6 @@ suite = {
     },
 
     "projects": {
-        "org.graalvm.maven.downloader" : {
-            "subDir" : "src",
-            "sourceDirs" : ["src"],
-            "javaCompliance" : "17+",
-            "license" : "UPL",
-            "dependencies": [
-                "sdk:NATIVEIMAGE",
-            ],
-            "requires": [
-                "java.logging",
-                "java.xml",
-            ],
-        },
         "org.graalvm.component.installer" : {
             "subDir" : "src",
             "sourceDirs" : ["src"],
@@ -204,17 +191,6 @@ suite = {
     },
 
     "distributions": {
-        "MAVEN_DOWNLOADER": {
-            "defaultBuild": False,
-            "mainClass": "org.graalvm.maven.downloader.Main",
-            "dependencies": [
-                "org.graalvm.maven.downloader",
-            ],
-            "distDependencies": [
-                "sdk:NATIVEIMAGE",
-            ],
-            "maven": False,
-        },
         "INSTALLER": {
             "subDir": "src",
             "mainClass": "org.graalvm.component.installer.ComponentInstaller",
