d5ea0fc3f2b81d18cead334ef8563eb1c8523d61
Benoit Daloze <benoit.daloze@oracle.com>
2023-09-06 14:12:27 +0200

    [GR-47432] Make sdk:MAVEN_DOWNLOADER a module

--- sdk/mx.sdk/suite.py
+++ sdk/mx.sdk/suite.py
@@ -769,6 +769,12 @@ suite = {
     },
 
     "MAVEN_DOWNLOADER": {
+      "moduleInfo" : {
+        "name" : "org.graalvm.maven.downloader",
+        "exports" : [
+          "org.graalvm.maven.downloader",
+        ],
+      },
       "defaultBuild": False,
       "mainClass": "org.graalvm.maven.downloader.Main",
       "dependencies": [
