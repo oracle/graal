817b4a25e21b04c4028ffa6802d1329fdca40ef0
Jan Stola <jan.stola@oracle.com>
2023-09-18 10:48:45 +0200

    Testing the sorting of JSONTruffleArray.

---
+++ tools/src/com.oracle.truffle.tools.chromeinspector.test/src/com/oracle/truffle/tools/chromeinspector/test/InteropTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023, 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.tools.chromeinspector.test;
+
+import static org.junit.Assert.assertTrue;
+
+import com.oracle.truffle.api.interop.InteropException;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.tools.chromeinspector.objects.JSONTruffleArray;
+import org.graalvm.shadowed.org.json.JSONArray;
+
+import org.junit.Test;
+
+public class InteropTest {
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testJSONTruffleArraySort() throws InteropException {
+        Object array = new JSONTruffleArray(new JSONArray("[2, 7, 1, 8]"));
+        InteropLibrary interop = InteropLibrary.getUncached(array);
+        long length = interop.getArraySize(array);
+
+        for (int iter = 1; iter < length; iter++) {
+            for (int idx = 1; idx < length; idx++) {
+                Object prev = interop.readArrayElement(array, idx - 1);
+                Object curr = interop.readArrayElement(array, idx);
+                if (((Comparable<Object>) prev).compareTo(curr) > 0) {
+                    // swap
+                    interop.writeArrayElement(array, idx - 1, curr);
+                    interop.writeArrayElement(array, idx, prev);
+                }
+            }
+        }
+
+        for (int idx = 1; idx < length; idx++) {
+            Object prev = interop.readArrayElement(array, idx - 1);
+            Object curr = interop.readArrayElement(array, idx);
+            assertTrue(((Comparable<Object>) prev).compareTo(curr) <= 0);
+        }
+    }
+
+}
