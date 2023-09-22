acd2e3c25f8b3e80bf1d4dcacbf31aab2175d600
Tom Rodriguez <tom.rodriguez@oracle.com>
2023-09-22 12:17:30 -0700

    Clean up condition usages during CE
    (cherry picked from commit 198d53eb4275410a00a28fd129ebcdc8344492bd)

--- compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/phases/common/ConditionalEliminationPhase.java
+++ compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/phases/common/ConditionalEliminationPhase.java
@@ -452,7 +452,11 @@ public class ConditionalEliminationPhase extends BasePhase<CoreProviders> {
         protected void processGuard(GuardNode node) {
             if (!tryProveGuardCondition(node, node.getCondition(), (guard, result, guardedValueStamp, newInput) -> {
                 if (result != node.isNegated()) {
+                    ValueNode condition = node.getCondition();
                     node.replaceAndDelete(guard.asNode());
+                    if (condition.hasNoUsages()) {
+                        GraphUtil.killWithUnusedFloatingInputs(condition);
+                    }
                     if (guard instanceof DeoptimizingGuard && !((DeoptimizingGuard) guard).isNegated()) {
                         rebuildPiNodes((DeoptimizingGuard) guard);
                     }
