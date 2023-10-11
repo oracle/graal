lusou-zhangquan/ziyi-forked-graal

Report UnresolvedElementException as warning
Adding an option PrintUnresolvedElementWarning to print
UnresolvedElementException encountered during loadReferencedType in
NoClassInitializationPlugin as warning, so that user knows what happens
when the anaysis result is not as expected.

substratevm/src/com.oracle.graal.pointsto/src/com/oracle/graal/pointsto/PointsToAnalyzer.java
@@ -197,7 +197,7 @@ public final class PointsToAnalyzer {
             bigbang.getMetaAccess().lookupJavaType(JavaKind.Void.toJavaClass()).registerAsReachable();
 
             GraphBuilderConfiguration.Plugins plugins = new GraphBuilderConfiguration.Plugins(new InvocationPlugins());
-            NoClassInitializationPlugin classInitializationPlugin = new NoClassInitializationPlugin();
+            NoClassInitializationPlugin classInitializationPlugin = new NoClassInitializationPlugin(PointstoOptions.PrintUnresolvedElementWarning.getValue(options));
             plugins.setClassInitializationPlugin(classInitializationPlugin);
             aProviders.setGraphBuilderPlugins(plugins);
             registerInvocationPlugins(providers.getSnippetReflection(), plugins.getInvocationPlugins(), providers.getReplacements(),

oracle/graal

substratevm/src/com.oracle.graal.pointsto.standalone/src/com/oracle/graal/pointsto/standalone/PointsToAnalyzer.java
@@ -210,7 +210,7 @@ public final class PointsToAnalyzer {
             bigbang.getMetaAccess().lookupJavaType(JavaKind.Void.toJavaClass()).registerAsReachable("root class");
 
             GraphBuilderConfiguration.Plugins plugins = new GraphBuilderConfiguration.Plugins(new InvocationPlugins());
-            NoClassInitializationPlugin classInitializationPlugin = new NoClassInitializationPlugin();
+            NoClassInitializationPlugin classInitializationPlugin = new NoClassInitializationPlugin(StandaloneOptions.PrintUnresolvedElementWarning.getValue(options));
             plugins.setClassInitializationPlugin(classInitializationPlugin);
             aProviders.setGraphBuilderPlugins(plugins);
             registerInvocationPlugins(originalProviders.getSnippetReflection(), plugins.getInvocationPlugins(), originalProviders.getReplacements(),