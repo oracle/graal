cee17cc0a9263961ac6ca2a118f0d8498e555306
Marouane El Hallaoui <marouane.el.hallaoui@oracle.com>
2023-10-17 12:52:23 +0100

    update labsjdk 21

--- compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/hotspot/JVMCIVersionCheck.java
+++ compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/hotspot/JVMCIVersionCheck.java
@@ -47,7 +47,7 @@ public final class JVMCIVersionCheck {
      * Minimum JVMCI version supported by Graal.
      */
     private static final Map<String, Version> JVMCI_MIN_VERSIONS = Map.of(
-                    "21", new Version(23, 1, 20),
+                    "21", new Version(23, 1, 21),
                     "22", new Version("22+18", 2));
 
     private static final int NA = 0;
