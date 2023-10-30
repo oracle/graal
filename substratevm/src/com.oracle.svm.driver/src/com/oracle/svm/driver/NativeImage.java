08c288c86ac3a84fa2633b45f8a0f3ce0073a880
Fabio Niephaus <fabio.niephaus@oracle.com>
2023-10-30 11:10:54 +0100

    Count number of `-H:±UnlockExperimentalVMOptions`.

--- substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
+++ substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
@@ -161,6 +161,7 @@ public class NativeImage {
 
         private final ArrayDeque<String> queue;
         public final String argumentOrigin;
+        public int numberOfFirstObservedActiveUnlockExperimentalVMOptions = -1;
 
         ArgumentQueue(String argumentOrigin) {
             queue = new ArrayDeque<>();
