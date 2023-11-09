f161e868e306687f5ef6ab558944114f9d687af1
David Leopoldseder <david.leopoldseder@oracle.com>
2023-11-09 15:06:38 +0100

    address review comments

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/replacements/amd64/AMD64FloatConvertNode.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/replacements/amd64/AMD64FloatConvertNode.java
@@ -28,16 +28,16 @@ import static jdk.graal.compiler.nodeinfo.NodeCycles.CYCLES_8;
 import static jdk.graal.compiler.nodeinfo.NodeSize.SIZE_1;
 import static jdk.graal.compiler.nodes.calc.BinaryArithmeticNode.getArithmeticOpTable;
 
-import java.math.BigDecimal;
-
+import jdk.graal.compiler.core.common.NumUtil;
 import jdk.graal.compiler.core.common.calc.FloatConvert;
 import jdk.graal.compiler.core.common.type.ArithmeticOpTable;
-import jdk.graal.compiler.core.common.type.FloatStamp;
 import jdk.graal.compiler.core.common.type.ArithmeticOpTable.FloatConvertOp;
 import jdk.graal.compiler.core.common.type.ArithmeticOpTable.UnaryOp;
+import jdk.graal.compiler.core.common.type.FloatStamp;
 import jdk.graal.compiler.core.common.type.IntegerStamp;
 import jdk.graal.compiler.core.common.type.Stamp;
 import jdk.graal.compiler.core.common.type.StampFactory;
+import jdk.graal.compiler.debug.Assertions;
 import jdk.graal.compiler.graph.NodeClass;
 import jdk.graal.compiler.lir.gen.ArithmeticLIRGeneratorTool;
 import jdk.graal.compiler.nodeinfo.NodeInfo;
@@ -49,7 +49,6 @@ import jdk.graal.compiler.nodes.calc.UnaryArithmeticNode;
 import jdk.graal.compiler.nodes.spi.ArithmeticLIRLowerable;
 import jdk.graal.compiler.nodes.spi.CanonicalizerTool;
 import jdk.graal.compiler.nodes.spi.NodeLIRBuilderTool;
-
 import jdk.vm.ci.meta.JavaConstant;
 
 /**
@@ -69,7 +68,6 @@ public final class AMD64FloatConvertNode extends UnaryArithmeticNode<FloatConver
     public AMD64FloatConvertNode(FloatConvert op, ValueNode value) {
         super(TYPE, getArithmeticOpTable(value).getFloatConvert(op), value);
         this.op = op;
-        this.stamp = this.stamp.meet(createInexactCaseStamp());
     }
 
     @Override
@@ -109,9 +107,9 @@ public final class AMD64FloatConvertNode extends UnaryArithmeticNode<FloatConver
          */
         if (resultStamp instanceof IntegerStamp && newStamp instanceof FloatStamp inputStamp) {
             final boolean canBeNan = inputStamp.canBeNaN();
-            final boolean canBeInifity = Double.isInfinite(inputStamp.lowerBound()) || Double.isInfinite(inputStamp.upperBound());
-            final boolean conversionCanOverflow = integralPartLargerMaxValue(Long.MAX_VALUE, inputStamp.upperBound()) || integralPartSmallerMinValue(Long.MIN_VALUE, inputStamp.lowerBound());
-            final boolean canGenerateSpecialValue = canBeNan || canBeInifity || conversionCanOverflow;
+            final boolean canBeInfinity = Double.isInfinite(inputStamp.lowerBound()) || Double.isInfinite(inputStamp.upperBound());
+            final boolean conversionCanOverflow = integralPartLargerMaxValue(inputStamp) || integralPartSmallerMinValue(inputStamp);
+            final boolean canGenerateSpecialValue = canBeNan || canBeInfinity || conversionCanOverflow;
             if (canGenerateSpecialValue) {
                 return resultStamp.meet(createInexactCaseStamp());
             } else {
@@ -122,18 +120,38 @@ public final class AMD64FloatConvertNode extends UnaryArithmeticNode<FloatConver
         }
     }
 
-    private static boolean integralPartLargerMaxValue(long maxValue, double d) {
-        if (Double.isInfinite(d) || Double.isNaN(d)) {
+    private static boolean integralPartLargerMaxValue(FloatStamp stamp) {
+        double upperBound = stamp.upperBound();
+        if (Double.isInfinite(upperBound) || Double.isNaN(upperBound)) {
             return true;
         }
-        return new BigDecimal(d).compareTo(new BigDecimal(maxValue)) > 0;
+        int bits = stamp.getBits();
+        assert bits == 32 || bits == 64 : "Must be int or long " + Assertions.errorMessage(stamp, upperBound);
+        long maxValue = NumUtil.maxValue(bits);
+        /*
+         * There could be conversion loss here when casting maxValue from long to double - given
+         * that max can be Long.MAX_VALUE. In order to avoid checking if upperBound is large than
+         * max with arbitrary precision numbers (BigDecimal for example) we use some trick.
+         *
+         * (double) Long.MAX_VALUE is 9223372036854775808, which is 1 more than Long.MAX_VALUE. So
+         * (long) (double) Long.MAX_VALUE will already overflow. So we need upperBound >= (double)
+         * maxValue, with >= instead of >. The next lower double value is Math.nextDown((double)
+         * Long.MAX_VALUE) == 9223372036854774784, which fits into long. So if upperBound >=
+         * (double) maxValue does not hold, i.e., upperBound < (double) maxValue does hold, then
+         * (long) upperBound will not overflow.
+         */
+        return upperBound >= maxValue;
     }
 
-    private static boolean integralPartSmallerMinValue(long minValue, double d) {
-        if (Double.isInfinite(d) || Double.isNaN(d)) {
+    private static boolean integralPartSmallerMinValue(FloatStamp stamp) {
+        double lowerBound = stamp.lowerBound();
+        if (Double.isInfinite(lowerBound) || Double.isNaN(lowerBound)) {
             return true;
         }
-        return new BigDecimal(d).compareTo(new BigDecimal(minValue)) < 0;
+        int bits = stamp.getBits();
+        assert bits == 32 || bits == 64 : "Must be int or long " + Assertions.errorMessage(stamp, lowerBound);
+        long minValue = NumUtil.minValue(bits);
+        return lowerBound <= minValue;
     }
 
     private Stamp createInexactCaseStamp() {
