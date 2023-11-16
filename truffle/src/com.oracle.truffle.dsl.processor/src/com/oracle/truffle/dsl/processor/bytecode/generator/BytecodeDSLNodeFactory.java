3b8bca1505b7d238a326c1894b30d7aa61f419e4
Matt D'Souza <matt.dsouza@oracle.com>
2023-11-16 09:27:32 -0500

    Fix generation of getIntrospectionData to import Instruction

--- truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/bytecode/generator/BytecodeDSLNodeFactory.java
+++ truffle/src/com.oracle.truffle.dsl.processor/src/com/oracle/truffle/dsl/processor/bytecode/generator/BytecodeDSLNodeFactory.java
@@ -1126,7 +1126,9 @@ public class BytecodeDSLNodeFactory implements ElementHelpers {
         b.end();
 
         b.end().startCatchBlock(type(Throwable.class), "e");
-        b.statement("String parsed = String.join(\"\\n    \", instructions.stream().map((i) -> new Instruction(i).toString()).toList())");
+        b.startAssign("String parsed");
+        b.string("String.join(\"\\n    \", instructions.stream().map((i) -> ").startNew(types.Instruction).string("i").end().string(".toString()).toList())");
+        b.end();
         b.startThrow().startNew(type(IllegalStateException.class));
         b.startGroup();
         b.doubleQuote("Error parsing instructions at ").string(" + bci[0] + ").doubleQuote(". Parsed instructions so far: \\n    ").string(" + parsed");
