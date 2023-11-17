7d02a0f4eda69b34d1a1844a21534fcc46935fc4
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2023-11-17 19:02:40 +0100

    Also `BUILD_TARGETS` might affect the GraalVM config.

--- sdk/mx.sdk/mx_sdk_vm_impl.py
+++ sdk/mx.sdk/mx_sdk_vm_impl.py
@@ -4133,7 +4133,7 @@ def graalvm_clean_env(out_env=None):
     Returns an env var that does not define variables that configure the GraalVM
     """
     env = out_env or os.environ.copy()
-    for env_var in ['DYNAMIC_IMPORTS', 'COMPONENTS', 'NATIVE_IMAGES', 'EXCLUDE_COMPONENTS', 'DISABLE_INSTALLABLES', 'NON_REBUILDABLE_IMAGES']:
+    for env_var in ['DYNAMIC_IMPORTS', 'COMPONENTS', 'NATIVE_IMAGES', 'EXCLUDE_COMPONENTS', 'DISABLE_INSTALLABLES', 'NON_REBUILDABLE_IMAGES', 'BUILD_TARGETS']:
         if env_var in env:
             env.pop(env_var)
     return env
