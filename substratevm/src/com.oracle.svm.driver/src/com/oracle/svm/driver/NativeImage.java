d6cf96277ad5a38db44fec60cb63097e72b1cb22
Paul Wögerer <paul.woegerer@oracle.com>
2023-12-05 09:29:45 +0100

    Add comments
    (cherry picked from commit e7ee33487219d10f3ec15bd1861467c3c2ca4373)

--- substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
+++ substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/NativeImage.java
@@ -2182,12 +2182,17 @@ public class NativeImage {
 
         Path classpathEntryFinal = useBundle() ? bundleSupport.substituteClassPath(classpathEntry) : classpathEntry;
         if (!imageClasspath.contains(classpathEntryFinal) && !customImageClasspath.contains(classpathEntryFinal)) {
+            /*
+             * Maintain correct order by adding entry before processing its potential "Class-Path"
+             * attributes from META-INF/MANIFEST.MF (in case the entry is a jar-file).
+             */
             boolean added = destination.add(classpathEntryFinal);
             if (ClasspathUtils.isJar(classpathEntryFinal)) {
                 processJarManifestMainAttributes(classpathEntryFinal, (jarFilePath, attributes) -> handleClassPathAttribute(destination, jarFilePath, attributes));
             }
-            boolean ignore = processClasspathNativeImageMetaInf(classpathEntryFinal);
-            if (added && ignore) {
+            boolean forcedOnModulePath = processClasspathNativeImageMetaInf(classpathEntryFinal);
+            if (added && forcedOnModulePath) {
+                /* Entry makes use of ForceOnModulePath. Undo adding to classpath. */
                 destination.remove(classpathEntryFinal);
             }
         }
