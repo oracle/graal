f90e059b505318edd1df198ea8d3a9b93a771ad7
maoldani <matteo.oldani@oracle.com>
2023-12-26 22:36:26 +0100

    Trying to fix tainting.

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/asm/amd64/AMD64MacroAssembler.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/asm/amd64/AMD64MacroAssembler.java
@@ -777,8 +777,6 @@ public class AMD64MacroAssembler extends AMD64Assembler {
      */
     public final void movSZx(Stride strideSrc, ExtendMode extendMode, Register dst, AMD64Address src) {
         sfence();
-        sfence();
-        sfence();
         switch (strideSrc) {
             case S1:
                 if (extendMode == ExtendMode.SIGN_EXTEND) {
