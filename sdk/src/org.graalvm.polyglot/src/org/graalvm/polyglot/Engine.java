28459403cff6adaf95a0dd5a7bae177ce6af471e
Tomas Zezula <tomas.zezula@oracle.com>
2024-01-03 18:37:56 +0100

    [GR-51149] Wrong error message when polyglot implementation is missing.
    (cherry picked from commit 6be80470451914889e9888622429559967a95c38)

--- sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/Engine.java
+++ sdk/src/org.graalvm.polyglot/src/org/graalvm/polyglot/Engine.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -2227,6 +2227,10 @@ public final class Engine implements AutoCloseable {
             return null;
         }
 
+        @Override
+        public String getTruffleVersion() {
+            return getPolyglotVersion().toString();
+        }
     }
 
     private static final class EngineShutDownHook implements Runnable {
