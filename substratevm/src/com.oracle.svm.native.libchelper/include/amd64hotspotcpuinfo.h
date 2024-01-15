3b90bd5908d4a0af024c63589fdddd5d4ba4c2c4
Fabio Niephaus <fabio.niephaus@oracle.com>
2024-01-15 08:27:43 +0100

    Fix Cpuid1Ecx feature parsing for AMD CPUs.
    See https://bugs.openjdk.org/browse/JDK-8280867

--- substratevm/src/com.oracle.svm.native.libchelper/include/amd64hotspotcpuinfo.h
+++ substratevm/src/com.oracle.svm.native.libchelper/include/amd64hotspotcpuinfo.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -141,12 +141,11 @@ typedef union {
     uint32_t LahfSahf     : 1,
              CmpLegacy    : 1,
                           : 3,
-             lzcnt_intel  : 1,
              lzcnt        : 1,
              sse4a        : 1,
              misalignsse  : 1,
              prefetchw    : 1,
-                          : 22;
+                          : 23;
   } bits;
 } ExtCpuid1Ecx;
 
