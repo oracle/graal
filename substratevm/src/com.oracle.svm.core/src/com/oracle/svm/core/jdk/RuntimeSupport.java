3429c66e2bd49a8486668b4777f15f6d931e1d87
Christian Haeubl <christian.haeubl@oracle.com>
2024-02-01 17:45:41 +0100

    Parse compilation isolate arguments even if ParseRuntimeOptions is disabled.
    (cherry picked from commit 7a0ea1b40b58249ef186ccaa07bb234b77312a07)

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/RuntimeSupport.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/RuntimeSupport.java
@@ -35,6 +35,7 @@ import org.graalvm.nativeimage.Platforms;
 import org.graalvm.nativeimage.VMRuntime;
 import org.graalvm.nativeimage.impl.VMRuntimeSupport;
 
+import com.oracle.svm.core.IsolateArgumentParser;
 import com.oracle.svm.core.Isolates;
 import com.oracle.svm.core.feature.AutomaticallyRegisteredImageSingleton;
 import com.oracle.svm.core.heap.HeapSizeVerifier;
@@ -93,6 +94,7 @@ public final class RuntimeSupport implements VMRuntimeSupport {
         boolean shouldInitialize = initializationState.compareAndSet(InitializationState.Uninitialized, InitializationState.InProgress);
         if (shouldInitialize) {
             // GR-35186: we should verify that none of the early parsed isolate arguments changed.
+            IsolateArgumentParser.verifyOptionValues();
             HeapSizeVerifier.verifyHeapOptions();
 
             executeHooks(startupHooks);
