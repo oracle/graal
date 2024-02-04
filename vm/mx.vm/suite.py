bea58b8ce4945b846845351353fd86bcac9ea8e0
Jan Stola <jan.stola@oracle.com>
2024-02-04 20:36:34 +0000

    [GR-48915] Disabling Test262 tests that run too long in compile mode.
    PullRequest: js/3048

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "c99a3a869bc3cf7e61799ab58d30f12a297773a8",
+                "version": "729f35fc1c299ae1aad589d148739216a8ea7eed",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "c99a3a869bc3cf7e61799ab58d30f12a297773a8",
+                "version": "729f35fc1c299ae1aad589d148739216a8ea7eed",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
