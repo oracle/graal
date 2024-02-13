cc2d642846016cf5b4fae7f999207e9497876564
Anas El Massafi <anas.el.massafi@oracle.com>
2024-02-13 21:23:46 +0000

    [GR-51744] Backport to 23.1: Fix possible infinite recursion in AbstractJSObjectArray.setElementImpl.
    PullRequest: js/3052

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "b52f2e491d3a30614713d6372ff023dca3d11973",
+                "version": "4b98544353e2208f40cf23e5d597beec608d2456",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "b52f2e491d3a30614713d6372ff023dca3d11973",
+                "version": "4b98544353e2208f40cf23e5d597beec608d2456",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
