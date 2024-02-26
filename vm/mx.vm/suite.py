a51b19ad0619d480fdbaf1379cacf37529586828
Andreas Woess <andreas.woess@oracle.com>
2024-02-26 17:44:56 +0000

    [GR-52327] Backport to 21.3: Deopt loops in NetSuite JS code.
    PullRequest: js/3072

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -39,7 +39,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "45a08d8085e8543b87886aebcd9770a0b81a16e9",
+                "version": "85cbca75b6d3c39a2c78a973eadbfe2811b20a00",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -49,7 +49,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "45a08d8085e8543b87886aebcd9770a0b81a16e9",
+                "version": "85cbca75b6d3c39a2c78a973eadbfe2811b20a00",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
