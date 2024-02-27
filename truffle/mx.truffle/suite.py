0b8b893eeb87c8e4d181884a0aa5ca0ae10e12c6
Christian Humer <christian.humer@oracle.com>
2024-02-27 15:16:56 +0100

    Add dist dependency to fix annotation processor handling.

--- truffle/mx.truffle/suite.py
+++ truffle/mx.truffle/suite.py
@@ -2079,7 +2079,7 @@ suite = {
       "dependencies" : [
         "com.oracle.truffle.dsl.processor",
       ],
-      "distDependencies" : ["TRUFFLE_JSON"],
+      "distDependencies" : [],
       "description" : "The Truffle DSL Processor generates source code for nodes that are declared using the DSL.",
       "allowsJavadocWarnings": True,
       "maven": {
