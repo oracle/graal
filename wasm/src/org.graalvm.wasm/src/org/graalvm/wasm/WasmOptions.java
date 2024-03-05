a838a26a4766192c88fb0ee51d829aaacf795bd6
Jirka Marsik <jiri.marsik@oracle.com>
2024-03-05 12:59:28 +0100

    Enable SIMD by default

--- wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/WasmOptions.java
+++ wasm/src/org.graalvm.wasm/src/org/graalvm/wasm/WasmOptions.java
@@ -104,8 +104,8 @@ public class WasmOptions {
     public static final OptionKey<Boolean> ExtendedConstExpressions = new OptionKey<>(false);
 
     @Option(help = "Enable support for the v128 type and vector instructions", //
-                    category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL, usageSyntax = "false|true") //
-    public static final OptionKey<Boolean> SIMD = new OptionKey<>(false);
+                    category = OptionCategory.EXPERT, stability = OptionStability.EXPERIMENTAL, usageSyntax = "true|false") //
+    public static final OptionKey<Boolean> SIMD = new OptionKey<>(true);
 
     @Option(help = "In this mode memories and tables are not initialized.", category = OptionCategory.INTERNAL, stability = OptionStability.EXPERIMENTAL, usageSyntax = "false|true") //
     public static final OptionKey<Boolean> MemoryOverheadMode = new OptionKey<>(false);
