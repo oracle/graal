2ec05da0657971dcddc63a0e3fea27c7943c40ea
Raphael Mosaner <raphael.m.mosaner@oracle.com>
2024-03-13 16:36:10 +0100

    [GR-52657] Fix simplification of always-deopt FixedGuard.
    (cherry picked from commit 1b7238b93811e3335fa58d4b64e0f7dd39bcefd1)

--- compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/FixedGuardNode.java
+++ compiler/src/org.graalvm.compiler.nodes/src/org/graalvm/compiler/nodes/FixedGuardNode.java
@@ -40,6 +40,7 @@ import org.graalvm.compiler.nodes.spi.Lowerable;
 import org.graalvm.compiler.nodes.spi.LoweringTool;
 import org.graalvm.compiler.nodes.spi.SimplifierTool;
 import org.graalvm.compiler.nodes.spi.SwitchFoldable;
+import org.graalvm.compiler.nodes.util.GraphUtil;
 
 import jdk.vm.ci.meta.DeoptimizationAction;
 import jdk.vm.ci.meta.DeoptimizationReason;
@@ -77,14 +78,15 @@ public final class FixedGuardNode extends AbstractFixedGuardNode implements Lowe
         if (getCondition() instanceof LogicConstantNode) {
             LogicConstantNode c = (LogicConstantNode) getCondition();
             if (c.getValue() == isNegated()) {
-                FixedNode currentNext = this.next();
-                if (currentNext != null) {
-                    tool.deleteBranch(currentNext);
-                }
-
+                /*
+                 * The guard will always deoptimize. Replace the guard and the remaining branch with
+                 * the corresponding deopt.
+                 */
                 DeoptimizeNode deopt = graph().add(new DeoptimizeNode(getAction(), getReason(), getSpeculation()));
                 deopt.setStateBefore(stateBefore());
-                setNext(deopt);
+                predecessor().replaceFirstSuccessor(this, deopt);
+                GraphUtil.killCFG(this);
+                return;
             }
             this.replaceAtUsages(null);
             graph().removeFixed(this);
