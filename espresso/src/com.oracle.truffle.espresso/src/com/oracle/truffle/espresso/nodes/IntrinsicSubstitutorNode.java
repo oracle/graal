5f3d069c803616a2aebf45c15e35425cb99c90eb
Allan Gregersen <allan.gregersen@oracle.com>
2024-03-14 12:03:49 +0100

    bci for native methods should be -1, not -2 (unknown). Intrinsified java methods should return 0 as bci, otherwise the debugger gets confused

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/IntrinsicSubstitutorNode.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/IntrinsicSubstitutorNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,7 @@ package com.oracle.truffle.espresso.nodes;
 
 import java.util.Arrays;
 
+import com.oracle.truffle.api.frame.Frame;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
 import com.oracle.truffle.espresso.impl.Method;
@@ -88,5 +89,12 @@ final class IntrinsicSubstitutorNode extends EspressoInstrumentableRootNodeImpl 
     @Override
     boolean isTrivial() {
         return substitution.isTrivial();
+    }
+
+    @Override
+    public int getBci(Frame frame) {
+        // when we intrinsify a Java method, we need to return a non-native
+        // bci, so we just use 0 to signal the beginning of the method
+        return 0;
     }
 }
