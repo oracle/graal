46bfeb2cff13ea44fc9610032aa17bc696b546d7
Christian Haeubl <christian.haeubl@oracle.com>
2024-03-26 13:07:20 +0100

    Change CCharPointer from "char" to "signed char".
    (cherry picked from commit 6a5861e7a4a19cdd2b84ae5f3bbad3ed5696d2eb)

--- sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/c/type/CCharPointer.java
+++ sdk/src/org.graalvm.nativeimage/src/org/graalvm/nativeimage/c/type/CCharPointer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -49,7 +49,7 @@ import org.graalvm.word.SignedWord;
  *
  * @since 19.0
  */
-@CPointerTo(nameOfCType = "char")
+@CPointerTo(nameOfCType = "signed char")
 public interface CCharPointer extends PointerBase {
 
     /**
