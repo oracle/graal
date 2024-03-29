8353cdaf424ba97335b2ed7456665a9e4ef90805
Roland Schatz <roland.schatz@oracle.com>
2024-03-29 09:30:26 +0100

    SVM changes for exception handling in frontend.

--- substratevm/src/com.oracle.svm.truffle.nfi/src/com/oracle/svm/truffle/nfi/Target_com_oracle_truffle_nfi_backend_libffi_NativeLibVersion.java
+++ substratevm/src/com.oracle.svm.truffle.nfi/src/com/oracle/svm/truffle/nfi/Target_com_oracle_truffle_nfi_backend_libffi_NativeLibVersion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,6 +32,6 @@ final class Target_com_oracle_truffle_nfi_backend_libffi_NativeLibVersion {
 
     @Substitute
     private static int get() {
-        return 1;
+        return 2;
     }
 }
