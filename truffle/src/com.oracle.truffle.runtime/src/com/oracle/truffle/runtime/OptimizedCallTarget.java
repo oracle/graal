3a043d3f2cfdbe0db7b16ab10cad778be4098ef6
Andreas Woess <andreas.woess@oracle.com>
2024-04-03 23:49:16 +0200

    Use concrete VirtualFrame return type to help static analysis.
    (cherry picked from commit 75d4a150e6a7f04f2fb0f7c2c69380f15f035953)

--- truffle/src/com.oracle.truffle.runtime/src/com/oracle/truffle/runtime/OptimizedCallTarget.java
+++ truffle/src/com.oracle.truffle.runtime/src/com/oracle/truffle/runtime/OptimizedCallTarget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -1127,7 +1127,7 @@ public abstract class OptimizedCallTarget implements TruffleCompilable, RootCall
     /**
      * Intrinsifiable compiler directive for creating a frame.
      */
-    public static VirtualFrame createFrame(FrameDescriptor descriptor, Object[] args) {
+    public static FrameWithoutBoxing createFrame(FrameDescriptor descriptor, Object[] args) {
         return new FrameWithoutBoxing(descriptor, args);
     }
 
