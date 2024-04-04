8be6f9492132eb1495336c3ce562c7005cfcbc88
Peter Hofer <peter.hofer@oracle.com>
2024-04-04 15:19:10 +0200

    Make reference processing more robust, especially against OOME during tear-down.
    (cherry picked from commit 8319494c2222f5b36f3b0fc19d9b0e52ad06fa2a)

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/ReferenceInternals.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/ReferenceInternals.java
@@ -38,11 +38,12 @@ import org.graalvm.compiler.word.Word;
 import org.graalvm.word.Pointer;
 import org.graalvm.word.WordFactory;
 
+import com.oracle.svm.core.NeverInline;
 import com.oracle.svm.core.SubstrateUtil;
 import com.oracle.svm.core.Uninterruptible;
 import com.oracle.svm.core.thread.VMOperation;
+import com.oracle.svm.core.util.BasedOnJDKClass;
 import com.oracle.svm.core.util.TimeUtils;
-import com.oracle.svm.core.util.VMError;
 
 import jdk.vm.ci.meta.MetaAccessProvider;
 import jdk.vm.ci.meta.ResolvedJavaField;
@@ -53,6 +54,7 @@ import jdk.vm.ci.meta.ResolvedJavaType;
  * not injected into {@link Target_java_lang_ref_Reference} so that subclasses of {@link Reference}
  * cannot interfere with them.
  */
+@BasedOnJDKClass(Reference.class)
 public final class ReferenceInternals {
     public static final String REFERENT_FIELD_NAME = "referent";
 
@@ -159,12 +161,20 @@ public final class ReferenceInternals {
     private static final Object processPendingLock = new Object();
     private static boolean processPendingActive = false;
 
+    @NeverInline("Ensure that every exception can be caught, including implicit exceptions.")
     public static void waitForPendingReferences() throws InterruptedException {
         Heap.getHeap().waitForReferencePendingList();
     }
 
+    @NeverInline("Ensure that every exception can be caught, including implicit exceptions.")
     @SuppressFBWarnings(value = "NN_NAKED_NOTIFY", justification = "Notifies on progress, not a specific state change.")
     public static void processPendingReferences() {
+        /*
+         * Note that catching an OutOfMemoryError (e.g. from failing to allocate another exception)
+         * from here and resuming is not advisable because it can break synchronization. We should
+         * generally see these only when processing individual references or cleaners.
+         */
+
         Target_java_lang_ref_Reference<?> pendingList;
         synchronized (processPendingLock) {
             if (processPendingActive) {
@@ -184,32 +194,28 @@ public final class ReferenceInternals {
 
         // Process all references that were discovered by the GC.
         do {
-            try {
-                while (pendingList != null) {
-                    Target_java_lang_ref_Reference<?> ref = pendingList;
-                    pendingList = ref.discovered;
-                    ref.discovered = null;
+            while (pendingList != null) {
+                Target_java_lang_ref_Reference<?> ref = pendingList;
+                pendingList = ref.discovered;
+                ref.discovered = null;
 
-                    if (Target_jdk_internal_ref_Cleaner.class.isInstance(ref)) {
-                        Target_jdk_internal_ref_Cleaner cleaner = Target_jdk_internal_ref_Cleaner.class.cast(ref);
-                        // Cleaner catches all exceptions, cannot be overridden due to private c'tor
-                        cleaner.clean();
-                        synchronized (processPendingLock) {
-                            // Notify any waiters that progress has been made. This improves latency
-                            // for nio.Bits waiters, which are the only important ones.
-                            processPendingLock.notifyAll();
-                        }
-                    } else {
-                        @SuppressWarnings("unchecked")
-                        Target_java_lang_ref_ReferenceQueue<? super Object> queue = SubstrateUtil.cast(ref.queue, Target_java_lang_ref_ReferenceQueue.class);
-                        if (queue != Target_java_lang_ref_ReferenceQueue.NULL) {
-                            // Enqueues, avoiding the potentially overridden Reference.enqueue().
-                            queue.enqueue(ref);
-                        }
+                if (Target_jdk_internal_ref_Cleaner.class.isInstance(ref)) {
+                    Target_jdk_internal_ref_Cleaner cleaner = Target_jdk_internal_ref_Cleaner.class.cast(ref);
+                    // Cleaner catches all exceptions, cannot be overridden due to private c'tor
+                    cleaner.clean();
+                    synchronized (processPendingLock) {
+                        // Notify any waiters that progress has been made. This improves latency
+                        // for nio.Bits waiters, which are the only important ones.
+                        processPendingLock.notifyAll();
+                    }
+                } else {
+                    @SuppressWarnings("unchecked")
+                    Target_java_lang_ref_ReferenceQueue<? super Object> queue = SubstrateUtil.cast(ref.queue, Target_java_lang_ref_ReferenceQueue.class);
+                    if (queue != Target_java_lang_ref_ReferenceQueue.NULL) {
+                        // Enqueues, avoiding the potentially overridden Reference.enqueue().
+                        queue.enqueue(ref);
                     }
                 }
-            } catch (Throwable t) {
-                VMError.shouldNotReachHere("ReferenceQueue and Cleaner must handle all potential exceptions", t);
             }
 
             synchronized (processPendingLock) {
