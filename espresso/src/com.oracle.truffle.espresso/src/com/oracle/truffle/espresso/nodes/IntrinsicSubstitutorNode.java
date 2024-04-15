6a088a71dfa795808ac572689b79dea41819bffd
Allan Gregersen <allan.gregersen@oracle.com>
2024-04-15 09:25:26 +0200

    add scope to intrinsified methods for frame arguments

--- espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/IntrinsicSubstitutorNode.java
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/IntrinsicSubstitutorNode.java
@@ -23,24 +23,14 @@
 
 package com.oracle.truffle.espresso.nodes;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 
-import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.frame.Frame;
-import com.oracle.truffle.api.frame.MaterializedFrame;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.interop.NodeLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
 import com.oracle.truffle.api.library.ExportMessage;
 import com.oracle.truffle.api.nodes.Node;
-import com.oracle.truffle.espresso.EspressoScope;
-import com.oracle.truffle.espresso.classfile.attributes.Local;
-import com.oracle.truffle.espresso.descriptors.ByteSequence;
-import com.oracle.truffle.espresso.descriptors.Symbol;
-import com.oracle.truffle.espresso.descriptors.Utf8ConstantTable;
-import com.oracle.truffle.espresso.impl.Klass;
 import com.oracle.truffle.espresso.impl.Method;
 import com.oracle.truffle.espresso.meta.EspressoError;
 import com.oracle.truffle.espresso.perf.DebugCounter;
@@ -118,36 +108,6 @@ public final class IntrinsicSubstitutorNode extends EspressoInstrumentableRootNo
 
     @ExportMessage
     public Object getScope(Frame frame, @SuppressWarnings("unused") boolean nodeEnter) {
-        return getScopeSlowPath(frame != null ? frame.materialize() : null);
-    }
-
-    @TruffleBoundary
-    private Object getScopeSlowPath(MaterializedFrame frame) {
-        // Local variable information for an intrinsified Java method
-        // consists of ´this´(if instance method) and all method arg locals
-        Method method = getMethodVersion().getMethod();
-        ArrayList<Local> constructedLiveLocals = new ArrayList<>();
-
-        boolean hasReceiver = !method.isStatic();
-        int localCount = hasReceiver ? 1 : 0;
-        localCount += method.getParameterCount();
-
-        Klass[] parameters = (Klass[]) method.getParameters();
-        Utf8ConstantTable utf8Constants = method.getLanguage().getUtf8ConstantTable();
-        int startslot = 0;
-
-        if (hasReceiver) {
-            // include 'this'
-            constructedLiveLocals.add(new Local(utf8Constants.getOrCreate(Symbol.Name.thiz), utf8Constants.getOrCreate(method.getDeclaringKlass().getType()), 0, 65536, 0));
-            startslot++;
-        }
-
-        // include method parameters
-        for (int i = startslot; i < localCount; i++) {
-            Klass param = hasReceiver ? parameters[i - 1] : parameters[i];
-            constructedLiveLocals.add(new Local(utf8Constants.getOrCreate(ByteSequence.create("param_" + (i))), utf8Constants.getOrCreate(param.getType()), 0, 65536, i));
-        }
-        Local[] liveLocals = constructedLiveLocals.toArray(Local.EMPTY_ARRAY);
-        return EspressoScope.createVariables(liveLocals, frame, method.getName());
+        return new SubstitutionScope(frame.getArguments());
     }
 }
---
+++ espresso/src/com.oracle.truffle.espresso/src/com/oracle/truffle/espresso/nodes/SubstitutionScope.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2024, 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.truffle.espresso.nodes;
+
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.espresso.EspressoLanguage;
+
+@ExportLibrary(InteropLibrary.class)
+final class SubstitutionScope implements TruffleObject {
+    private final Object[] args;
+
+    public SubstitutionScope(Object[] arguments) {
+        this.args = arguments;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    boolean hasMembers() {
+        return true;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    boolean isScope() {
+        return true;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    boolean hasLanguage() {
+        return true;
+    }
+
+    @ExportMessage
+    @SuppressWarnings("static-method")
+    Class<? extends TruffleLanguage<?>> getLanguage() {
+        return EspressoLanguage.class;
+    }
+
+    @ExportMessage
+    @CompilerDirectives.TruffleBoundary
+    Object readMember(String member) throws UnknownIdentifierException {
+        try {
+            int index = Integer.parseInt(member);
+            return args[index];
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
+            throw UnknownIdentifierException.create(member);
+        }
+    }
+
+    @ExportMessage
+    Object getMembers(@SuppressWarnings("unused") boolean includeInternal) throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    boolean isMemberReadable(@SuppressWarnings("unused") String member) {
+        try {
+            int index = Integer.parseInt(member);
+            return index > -1 && index < args.length;
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
+            return false;
+        }
+    }
+
+    @ExportMessage
+    Object toDisplayString(@SuppressWarnings("unused") boolean allowSideEffects) {
+        return "<intrinsified>";
+    }
+
+}
