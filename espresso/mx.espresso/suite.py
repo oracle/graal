e8bbab0287e3c8f790a3c4bf98c49b6e4f4361a2
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2024-04-26 11:50:41 +0200

    Depend on llvm-native-community from espresso's pom distribution

--- espresso/mx.espresso/suite.py
+++ espresso/mx.espresso/suite.py
@@ -358,8 +358,7 @@ suite = {
                 "truffle:TRUFFLE_NFI_LIBFFI",
                 "truffle:TRUFFLE_RUNTIME",
                 # sulong is not strictly required but it'll work out of the box in more cases if it's there
-                "sulong:SULONG_NFI",
-                "sulong:SULONG_NATIVE",
+                "sulong:LLVM_NATIVE_COMMUNITY",
             ],
             "description": "Java on Truffle (aka Espresso): a Java bytecode interpreter",
             "maven": {
