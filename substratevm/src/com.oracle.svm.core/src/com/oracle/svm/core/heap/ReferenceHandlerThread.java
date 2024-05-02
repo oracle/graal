77afc95d185dc4ee59bf8063cd5d353e3cdecc08
Christian Haeubl <christian.haeubl@oracle.com>
2024-05-02 17:34:16 +0200

    [GR-53407] Optionally ignore OutOfMemoryErrors while in VM-internal code.
    (cherry picked from commit 7922f87b7e2cf44f084a1ea359da0c3643d269b3)

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/ReferenceHandlerThread.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/heap/ReferenceHandlerThread.java
@@ -24,7 +24,10 @@
  */
 package com.oracle.svm.core.heap;
 
+import static com.oracle.svm.core.Uninterruptible.CALLED_FROM_UNINTERRUPTIBLE_CODE;
+
 import jdk.graal.compiler.api.replacements.Fold;
+
 import org.graalvm.nativeimage.CurrentIsolate;
 import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.IsolateThread;
@@ -32,6 +35,7 @@ import org.graalvm.nativeimage.Platform;
 import org.graalvm.nativeimage.Platforms;
 
 import com.oracle.svm.core.SubstrateOptions;
+import com.oracle.svm.core.Uninterruptible;
 import com.oracle.svm.core.feature.AutomaticallyRegisteredFeature;
 import com.oracle.svm.core.feature.InternalFeature;
 import com.oracle.svm.core.thread.ThreadingSupportImpl;
@@ -55,6 +59,7 @@ public final class ReferenceHandlerThread implements Runnable {
         }
     }
 
+    @Uninterruptible(reason = CALLED_FROM_UNINTERRUPTIBLE_CODE, mayBeInlined = true)
     public static boolean isReferenceHandlerThread() {
         if (isSupported()) {
             return CurrentIsolate.getCurrentThread() == singleton().isolateThread;
