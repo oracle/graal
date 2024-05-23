08170ef96e1d072fb69c53d403d14a654e12802a
Jan Stola <jan.stola@oracle.com>
2024-05-23 02:32:02 +0000

    [GR-54168] Upgrading the underlying Node.js to version 20.13.1.
    PullRequest: js/3151

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "6eb9525fb1354aaaa15056563a131403caf6ed8a",
+                "version": "3e92f510c84e5027036583cfe20c648c857357ad",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "6eb9525fb1354aaaa15056563a131403caf6ed8a",
+                "version": "3e92f510c84e5027036583cfe20c648c857357ad",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
