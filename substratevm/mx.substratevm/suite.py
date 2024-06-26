8ff2d55b5cf777da5953037376e16c80efcf7079
dominik.mascherbauer@oracle.com <dominik.mascherbauer@oracle.com>
2024-06-26 11:04:33 +0200

    Create a test to manually trigger runtime compilation

--- substratevm/mx.substratevm/suite.py
+++ substratevm/mx.substratevm/suite.py
@@ -1077,6 +1077,10 @@ suite = {
                     "jdk.internal.misc",
                     "sun.security.jca",
                 ],
+                "jdk.internal.vm.ci" : [
+                    "jdk.vm.ci.code",
+                    "jdk.vm.ci.meta",
+                ],
             },
             "checkstyle": "com.oracle.svm.test",
             "checkstyleVersion" : "10.21.0",
