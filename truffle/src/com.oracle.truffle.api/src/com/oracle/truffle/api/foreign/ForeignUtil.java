5d17c05ff516e905db47e0bc87d749152d7ab983
Tomas Zezula <tomas.zezula@oracle.com>
2024-07-02 11:31:21 +0200

    Move restricted calls to Truffle API.

--- truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/foreign/ForeignUtil.java
+++ truffle/src/com.oracle.truffle.api/src/com/oracle/truffle/api/foreign/ForeignUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -38,45 +38,46 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.oracle.truffle.nfi.backend.panama;
+package com.oracle.truffle.api.foreign;
 
-import com.oracle.truffle.api.exception.AbstractTruffleException;
-import com.oracle.truffle.api.impl.Accessor.ForeignSupport;
-import com.oracle.truffle.api.nodes.EncapsulatingNodeReference;
-import com.oracle.truffle.api.nodes.Node;
+import java.lang.invoke.MethodHandle;
 
-@SuppressWarnings("serial")
-final class NFIError extends AbstractTruffleException {
+/**
+ * Support for foreign function linking. The implementation resides in the
+ * {@code com.oracle.truffle.api.jdk22} project and is added as an overlay into the multi-version
+ * JAR.
+ */
+final class ForeignUtil {
 
-    NFIError(String message) {
-        this(message, null);
+    private ForeignUtil() {
     }
 
-    NFIError(String message, Node location) {
-        super(message, resolveLocation(location));
+    @SuppressWarnings("unused")
+    public static Object libraryLookup(String libraryName, Object arena) {
+        throw unsupported();
     }
 
-    private static Node resolveLocation(Node node) {
-        if (node == null || node.isAdoptable()) {
-            return node;
-        } else {
-            return EncapsulatingNodeReference.getCurrent().get();
-        }
+    @SuppressWarnings("unused")
+    public static MethodHandle downcallHandle(String symbolName, Object functionDescriptor) {
+        throw unsupported();
     }
 
-    static NFIError illegalNativeAccess(Node location) {
-        Module truffleModule = ForeignSupport.class.getModule();
-        String targetModule;
-        String error;
-        if (truffleModule.isNamed()) {
-            targetModule = truffleModule.getName();
-            error = String.format("Illegal native access from module %s.", targetModule);
-        } else {
-            targetModule = "ALL-UNNAMED";
-            error = "Illegal native access from an unnamed module.";
-        }
-        return new NFIError(String.format("%s To enable the native access required by the NFI Panama backend, please add '--enable-native-access=%s' to the JVM command line options.",
-                        error, targetModule), location);
+    @SuppressWarnings("unused")
+    public static MethodHandle downcallHandle(Object functionDescriptor) {
+        throw unsupported();
     }
 
+    @SuppressWarnings("unused")
+    public static Object upcallStub(MethodHandle methodHandle, Object functionDescriptor, Object arena) {
+        throw unsupported();
+    }
+
+    @SuppressWarnings("unused")
+    public static Object reinterpret(Object memorySegment, long newSize) {
+        throw unsupported();
+    }
+
+    private static RuntimeException unsupported() {
+        throw new UnsupportedOperationException("Foreign access is not supported on JDK < 22.");
+    }
 }
