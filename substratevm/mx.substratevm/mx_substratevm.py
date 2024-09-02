3cccc46d83db101009c2c1e761c3edf5ad1b751d
Loic Ottet <loic.ottet@oracle.com>
2024-09-02 10:54:45 +0200

    Prevent emission of typeReachable in reachability-metadata.json

--- substratevm/mx.substratevm/mx_substratevm.py
+++ substratevm/mx.substratevm/mx_substratevm.py
@@ -1285,7 +1285,9 @@ native_image = mx_sdk_vm.GraalVmJreComponent(
             build_args=driver_build_args + [
                 '--features=com.oracle.svm.agent.NativeImageAgent$RegistrationFeature',
                 '--enable-url-protocols=jar',
-            ],
+            ] + svm_experimental_options([
+                '-H:+TreatAllTypeReachableConditionsAsTypeReached',
+            ]),
             headers=False,
             home_finder=False,
         ),
@@ -1574,6 +1576,7 @@ mx_sdk_vm.register_graalvm_component(mx_sdk_vm.GraalVmJreComponent(
             main_class='com.oracle.svm.configure.ConfigurationTool',
             build_args=svm_experimental_options([
                 '-H:-ParseRuntimeOptions',
+                '-H:+TreatAllTypeReachableConditionsAsTypeReached',
             ]),
             extra_jvm_args=_native_image_configure_extra_jvm_args(),
             home_finder=False,
