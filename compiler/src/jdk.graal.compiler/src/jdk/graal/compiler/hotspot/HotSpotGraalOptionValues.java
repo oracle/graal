5b5f1b5cfbd953b71c94c6dd1cce5e36238d3e17
Doug Simon <doug.simon@oracle.com>
2024-09-04 22:33:49 +0200

    delete com.oracle.svm.graal.hotspot.libgraal project

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotGraalOptionValues.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotGraalOptionValues.java
@@ -168,8 +168,7 @@ public class HotSpotGraalOptionValues {
     }
 
     /**
-     * Substituted by
-     * {@code com.oracle.svm.graal.hotspot.libgraal.Target_jdk_graal_compiler_hotspot_HotSpotGraalOptionValues}.
+     * Substituted by {@code Target_jdk_graal_compiler_hotspot_HotSpotGraalOptionValues}.
      *
      * @param settings unparsed libgraal option values
      */
@@ -195,8 +194,7 @@ public class HotSpotGraalOptionValues {
     }
 
     /**
-     * Substituted by
-     * {@code com.oracle.svm.graal.hotspot.libgraal.Target_jdk_graal_compiler_hotspot_HotSpotGraalOptionValues}.
+     * Substituted by {@code Target_jdk_graal_compiler_hotspot_HotSpotGraalOptionValues}.
      *
      * @param out where help is to be printed
      * @param prefix system property prefix for libgraal VM options
