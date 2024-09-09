2e181ce3b7f8134fadc0fbc2cec057c196578df2
Josef Eisl <josef.eisl@oracle.com>
2024-09-09 08:54:01 +0200

    compiler: fix unused imports

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/GraalHotSpotVMConfigAccess.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/GraalHotSpotVMConfigAccess.java
@@ -32,7 +32,6 @@ import java.util.Formatter;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
