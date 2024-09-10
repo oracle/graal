28e15266bc7fa780d7dcb1c93bc9a08cf075824d
Tomas Stupka <tomas.stupka@oracle.com>
2024-09-10 09:08:35 +0000

    [GR-21590] Update imports.
    PullRequest: graalpython/3470

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -65,7 +65,7 @@ suite = {
             },
             {
                 "name": "graalpython",
-                "version": "5bfb7f937b1f4eb15925c41c9b8528766364b321",
+                "version": "b24e61820ef576786dfd4fff90b8889a3564c656",
                 "dynamic": True,
                 "urls": [
                     {"url": "https://github.com/graalvm/graalpython.git", "kind": "git"},
