6e0ecc0c899dae87fbc96ce497eed9e4710bf821
David Leopoldseder <david.leopoldseder@oracle.com>
2024-10-21 13:53:09 +0200

    safepoint elimination: refactorings
    (cherry picked from commit c0405ac1a583a53982b5c60562693cb4fd3e6b9f)

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/SafepointNode.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/SafepointNode.java
@@ -29,10 +29,13 @@ import static jdk.graal.compiler.nodeinfo.NodeSize.SIZE_1;
 
 import jdk.graal.compiler.core.common.type.StampFactory;
 import jdk.graal.compiler.graph.NodeClass;
+import jdk.graal.compiler.nodeinfo.InputType;
 import jdk.graal.compiler.nodeinfo.NodeInfo;
 import jdk.graal.compiler.nodes.spi.LIRLowerable;
 import jdk.graal.compiler.nodes.spi.Lowerable;
 import jdk.graal.compiler.nodes.spi.NodeLIRBuilderTool;
+import jdk.graal.compiler.nodes.spi.Simplifiable;
+import jdk.graal.compiler.nodes.spi.SimplifierTool;
 
 /**
  * Marks a position in the graph where a safepoint should be emitted.
@@ -42,12 +45,20 @@ import jdk.graal.compiler.nodes.spi.NodeLIRBuilderTool;
           cyclesRationale = "read",
           size = SIZE_1)
 // @formatter:on
-public final class SafepointNode extends DeoptimizingFixedWithNextNode implements Lowerable, LIRLowerable {
+public final class SafepointNode extends DeoptimizingFixedWithNextNode implements Lowerable, LIRLowerable, Simplifiable {
 
     public static final NodeClass<SafepointNode> TYPE = NodeClass.create(SafepointNode.class);
 
+    @OptionalInput(InputType.Association) protected AbstractBeginNode loop;
+
     public SafepointNode() {
         super(TYPE, StampFactory.forVoid());
+    }
+
+    public SafepointNode(LoopBeginNode loop) {
+        super(TYPE, StampFactory.forVoid());
+        this.loop = loop;
+
     }
 
     @Override
@@ -59,4 +70,25 @@ public final class SafepointNode extends DeoptimizingFixedWithNextNode implement
     public boolean canDeoptimize() {
         return true;
     }
+
+    @Override
+    public void simplify(SimplifierTool tool) {
+        if (next() instanceof SafepointNode) {
+            this.graph().removeFixed(this);
+        }
+        if (loop != null && !(loop instanceof LoopBeginNode)) {
+            // if any optimization rendered this safepoint to be decoupled from a loop, drop it
+            this.graph().removeFixed(this);
+        }
+    }
+
+    public void setLoop(AbstractBeginNode loop) {
+        updateUsagesInterface(this.loop, loop);
+        this.loop = loop;
+    }
+
+    public AbstractBeginNode getLoop() {
+        return loop;
+    }
+
 }
