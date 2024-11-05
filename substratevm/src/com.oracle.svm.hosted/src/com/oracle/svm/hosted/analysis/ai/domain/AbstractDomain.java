15a8eef2c2427107a57c738b1033582d83030611
Samuel Malec <xmalec@fi.muni.cz>
2024-11-05 15:28:18 +0100

    feat: write docs for AbstractDomain

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/AbstractDomain.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/AbstractDomain.java
@@ -12,28 +12,85 @@ package com.oracle.svm.hosted.analysis.ai.domain;
  * Sample usage:
  * public class MyCustomDomain extends AbstractDomain<MyCustomDomain> {}
  *
- * @param <T> derived AbstractDomain
+ * @param <T> type of the derived AbstractDomain
  */
 
 public abstract class AbstractDomain<T extends AbstractDomain<T>> {
+    /**
+     * Creates a copy of the domain element
+     *
+     * @return copy of the domain element
+     */
     protected abstract T copyOf();
 
+    /**
+     * Checks if the domain is the bottom element
+     *
+     * @return true if the domain is the bottom element
+     */
     public abstract boolean isBot();
 
+    /**
+     * Checks if the domain is the top element
+     *
+     * @return true if the domain is the top element
+     */
     public abstract boolean isTop();
 
+    /**
+     * Checks if the domain is less or equal to the other domain
+     *
+     * @param other domain to compare with
+     * @return true if the domain is less or equal to the other domain
+     */
     public abstract boolean leq(T other);
 
+    /**
+     * Checks if the domain is equal to the other domain
+     *
+     * @param obj domain to compare with
+     * @return true if the domain is equal to the other domain
+     */
+    @Override
+    public abstract boolean equals(Object obj);
+
+    /**
+     * Checks if the domain is equal to the other domain
+     *
+     * @param other domain to compare with
+     * @return true if the domain is equal to the other domain
+     */
     public abstract boolean equals(T other);
 
+    /**
+     * Sets the domain to the bottom element
+     */
     public abstract void setToBot();
 
+    /**
+     * Sets the domain to the top element
+     */
     public abstract void setToTop();
 
+    /**
+     * Joins the domain with the other domain, modifying the domain
+     *
+     * @param other domain to join with
+     */
     public abstract void joinWith(T other);
 
+    /**
+     * Widens the domain with the other domain, modifying the domain
+     *
+     * @param other domain to widen with
+     */
     public abstract void widenWith(T other);
 
+    /**
+     * Meets the domain with the other domain, modifying the domain
+     *
+     * @param other domain to meet with
+     */
     public abstract void meetWith(T other);
 
     public T join(T other) {
@@ -72,14 +129,5 @@ public abstract class AbstractDomain<T extends AbstractDomain<T>> {
         } catch (Exception e) {
             throw new IllegalArgumentException("Error creating bottom instance of " + clazz.getName(), e);
         }
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        @SuppressWarnings("unchecked")
-        T other = (T) obj;
-        return this.equals(other);
     }
 }
