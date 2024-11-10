5639b955c03497ace3af7370624bfbcafbe7e996
Samuel Malec <xmalec@fi.muni.cz>
2024-11-10 23:28:47 +0100

    feat: implement Environment using AbstractState

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/Environment.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/Environment.java
@@ -1,54 +1,54 @@
 package com.oracle.svm.hosted.analysis.ai.fixpoint;
 
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
-import com.oracle.svm.hosted.analysis.ai.value.AbstractValue;
-import com.oracle.svm.hosted.analysis.ai.value.AbstractValueKind;
 import jdk.graal.compiler.graph.Node;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * This class maps Nodes of StructuredGraph to a common abstract domain,
+ * This class maps Nodes of StructuredGraph to a common abstract domain
+ * and also keeps track of some metadata about nodes like visit count.
  * acting as a memory of FixpointIterator.
  * An example could be mapping Nodes to intervals or constants.
  *
  * @param <Domain> type of the derived abstract domain
  */
-public final class Environment<
-        Domain extends AbstractDomain<Domain>> {
-    private final Map<Node, Domain> nodeToDomainMap;
-    private final Domain topValue;
 
-    public Environment(Domain topValue) {
-        this.nodeToDomainMap = new HashMap<>();
-        this.topValue = topValue.copyOf();
+public class Environment<Domain extends AbstractDomain<Domain>> {
+    private final Domain defaultDomain;
+    private final Map<Node, AbstractState<Domain>> stateMap;
+
+    public Environment(Domain defaultDomain, int nodeCount) {
+        this.defaultDomain = defaultDomain;
+        this.stateMap = new HashMap<>(nodeCount);
     }
 
-    public Environment(Domain topValue, Map<Node, Domain> initialBindings) {
-        this.nodeToDomainMap = new HashMap<>();
-        this.topValue = topValue;
-        for (Map.Entry<Node, Domain> entry : initialBindings.entrySet()) {
-            if (entry.getValue().isBot()) {
-                return;
-            }
-            this.nodeToDomainMap.put(entry.getKey(), entry.getValue());
+    public AbstractState<Domain> getState(Node node) {
+        return stateMap.get(node);
+    }
+
+    public Domain getDomain(Node node) {
+        var state = stateMap.getOrDefault(node, null);
+        if (state == null) {
+            return defaultDomain;
         }
+        return state.getDomain();
     }
 
-    public int size() {
-        return nodeToDomainMap.size();
+    public void setDomain(Node node, Domain domain) {
+        var state = stateMap.getOrDefault(node, null);
+        if (state == null) {
+            return;
+        }
+        state.setDomain(domain);
     }
 
-    public Domain get(Node node) {
-        return this.nodeToDomainMap.getOrDefault(node, topValue);
-    }
-
-    public void set(Node node, Domain value) {
-        this.nodeToDomainMap.put(node, value);
-    }
-
-    public void reset() {
-        this.nodeToDomainMap.clear();
+    public int getVisitCount(Node node) {
+        var state = stateMap.getOrDefault(node, null);
+        if (state == null) {
+            return 0;
+        }
+        return state.getVisitedCount();
     }
 }
