1cdc6a85180e412bc9083067b76000c1211da1d7
Samuel Malec <xmalec@fi.muni.cz>
2024-11-13 12:44:38 +0100

    feat: implement policy inside AnalyzerFactory

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/AnalyzerFactory.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/AnalyzerFactory.java
@@ -1,19 +1,34 @@
 package com.oracle.svm.hosted.analysis.ai.analyzer;
 
+import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.FixpointIterator;
+import com.oracle.svm.hosted.analysis.ai.transfer.TransferFunction;
+import com.oracle.svm.hosted.analysis.ai.transfer.policy.IteratorPolicy;
+import com.oracle.svm.hosted.analysis.ai.transfer.policy.PolicyBuilder;
 import jdk.graal.compiler.debug.DebugContext;
-
-/**
- * Factory for creating analyzers
- */
+import jdk.graal.compiler.nodes.StructuredGraph;
 
 public class AnalyzerFactory {
 
-    public static InterProceduralAnalyzer createInterproceduralAnalyzer(FixpointIterator<?, ?> fixpointIterator, DebugContext debug) {
-        return new InterProceduralAnalyzer(fixpointIterator, debug);
+    public static <Domain extends AbstractDomain<Domain>> Analyzer<Domain> createInterProceduralAnalyzer(
+            StructuredGraph graph,
+            TransferFunction<Domain> transferFunction,
+            Domain initialDomain,
+            DebugContext debug) {
+        PolicyBuilder builder = new PolicyBuilder();
+        builder.setShouldSkipCallNodes(false);
+        IteratorPolicy policy = builder.build();
+        return new InterProceduralAnalyzer<>(graph, transferFunction, policy, initialDomain, debug);
     }
 
-    public static IntraProceduralAnalyzer createIntraproceduralAnalyzer(FixpointIterator<?, ?> fixpointIterator, DebugContext debug) {
-        return new IntraProceduralAnalyzer(fixpointIterator, debug);
+    public static <Domain extends AbstractDomain<Domain>> Analyzer<Domain> createIntraProceduralAnalyzer(
+            StructuredGraph graph,
+            TransferFunction<Domain> transferFunction,
+            Domain initialDomain,
+            DebugContext debug) {
+        PolicyBuilder builder = new PolicyBuilder();
+        builder.setShouldSkipCallNodes(true);
+        IteratorPolicy policy = builder.build();
+        return new IntraProceduralAnalyzer<>(graph, transferFunction, policy, initialDomain, debug);
     }
 }
