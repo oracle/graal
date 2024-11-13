2c9fd717106355159e07f59a123f637fa355ebab
Samuel Malec <xmalec@fi.muni.cz>
2024-11-13 12:47:09 +0100

    feat: implement example IntegerDomain

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/IntegerDomain.java
@@ -0,0 +1,87 @@
+package com.oracle.svm.hosted.analysis.ai.domain;
+
+/*
+    Simple domain for integers.
+    This domain servers as an example for the AbstractDomain class.
+    Join and Widening are not implemented, because the only thing we need is to increment and decrement the value.
+ */
+
+public class IntegerDomain extends AbstractDomain<IntegerDomain> {
+    private int value;
+
+    public IntegerDomain() {
+        value = 0;
+    }
+
+    public IntegerDomain(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public IntegerDomain copyOf() {
+        return new IntegerDomain(value);
+    }
+
+    @Override
+    public boolean isBot() {
+        return false;
+    }
+
+    @Override
+    public boolean isTop() {
+        return false;
+    }
+
+    @Override
+    public boolean leq(IntegerDomain other) {
+        return value <= other.value;
+    }
+
+    @Override
+    public boolean equals(IntegerDomain other) {
+        return value == other.value;
+    }
+
+    @Override
+    public void setToBot() {
+    }
+
+    @Override
+    public void setToTop() {
+    }
+
+    @Override
+    public void joinWith(IntegerDomain other) {
+        value = Math.max(value, other.value);
+    }
+
+    @Override
+    public void widenWith(IntegerDomain other) {
+        joinWith(other);
+    }
+
+    @Override
+    public void meetWith(IntegerDomain other) {
+    }
+
+    @Override
+    public String toString() {
+        return "IntegerDomain{" +
+                "value=" + value +
+                '}';
+    }
+
+    public void incrementValue() {
+        value++;
+    }
+
+    public void decrementValue() {
+        value--;
+        if ( value < 0 )
+            value = 0;
+    }
+}
