cccba8579a4d3b95078dd4a3e7b10cb96e5eafd2
Samuel Malec <xmalec@fi.muni.cz>
2024-11-15 23:33:03 +0100

    feat: create SetDomain

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/SetDomain.java
@@ -0,0 +1,47 @@
+package com.oracle.svm.hosted.analysis.ai.domain;
+
+import com.oracle.svm.hosted.analysis.ai.value.AbstractValueKind;
+import com.oracle.svm.hosted.analysis.ai.value.SetValue;
+
+/*
+    This abstract domain represents a set of elements.
+    For example the domain could represent set of live variables
+ */
+public final class SetDomain<Element> extends LatticeDomain<SetValue<Element>, SetDomain<Element>> {
+    public SetDomain() {
+        super(SetValue::new);
+    }
+
+    public SetDomain(AbstractValueKind kind) throws IllegalAccessException {
+        super(kind, SetValue::new);
+    }
+
+    public SetDomain(SetValue<Element> setValue) {
+        super(() -> setValue);
+    }
+
+    public SetDomain(SetDomain<Element> other) {
+        super(() -> new SetValue<>(other.getValue()));
+        this.kind = other.kind;
+    }
+
+    public void add(Element element) {
+        getValue().add(element);
+    }
+
+    public void remove(Element element) {
+        getValue().remove(element);
+    }
+
+    @Override
+    public String toString() {
+        return "SetDomain{" +
+                "setValue=" + getValue() +
+                '}';
+    }
+
+    @Override
+    public SetDomain<Element> copyOf() {
+        return new SetDomain<>(this);
+    }
+}
