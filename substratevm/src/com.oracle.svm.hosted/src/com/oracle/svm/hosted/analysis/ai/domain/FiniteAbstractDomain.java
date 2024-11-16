a3cd6935683fd3d52e945f7a587cd8405a3a6edc
Samuel Malec <xmalec@fi.muni.cz>
2024-11-16 23:56:45 +0100

    feat: create FiniteAbstractDomain

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/FiniteAbstractDomain.java
@@ -0,0 +1,115 @@
+package com.oracle.svm.hosted.analysis.ai.domain;
+
+import com.oracle.svm.hosted.analysis.ai.value.AbstractValueKind;
+
+/**
+ * A generic wrapper class for handling domains with finitely many states.
+ * For example SignDomain, ParityDomain, etc.
+ *
+ * @param <State> the type of the finite state
+ */
+public abstract class FiniteAbstractDomain<State> extends AbstractDomain<FiniteAbstractDomain<State>> {
+    private State state;
+    private AbstractValueKind kind;
+
+    public FiniteAbstractDomain(State initialState, AbstractValueKind initialKind) {
+        this.state = initialState;
+        this.kind = initialKind;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public AbstractValueKind getKind() {
+        return kind;
+    }
+
+    public void setKind(AbstractValueKind kind) {
+        this.kind = kind;
+    }
+
+    @Override
+    public boolean isBot() {
+        return kind == AbstractValueKind.BOT;
+    }
+
+    @Override
+    public boolean isTop() {
+        return kind == AbstractValueKind.TOP;
+    }
+
+    @Override
+    public boolean leq(FiniteAbstractDomain<State> other) {
+        if (isBot()) return true;
+        if (other.isBot()) return false;
+        if (other.isTop()) return true;
+        if (isTop()) return false;
+        return state.equals(other.state);
+    }
+
+    @Override
+    public boolean equals(FiniteAbstractDomain<State> other) {
+        return state.equals(other.state) && kind == other.kind;
+    }
+
+    @Override
+    public void setToBot() {
+        kind = AbstractValueKind.BOT;
+    }
+
+    @Override
+    public void setToTop() {
+        kind = AbstractValueKind.TOP;
+    }
+
+    @Override
+    public void joinWith(FiniteAbstractDomain<State> other) {
+        if (isTop() || other.isBot()) return;
+        if (other.isTop()) {
+            setToTop();
+            return;
+        }
+        if (isBot()) {
+            state = other.state;
+            kind = other.kind;
+            return;
+        }
+        kind = state.equals(other.state) ? AbstractValueKind.VAL : AbstractValueKind.TOP;
+    }
+
+    @Override
+    public void widenWith(FiniteAbstractDomain<State> other) {
+        joinWith(other);
+    }
+
+    @Override
+    public void meetWith(FiniteAbstractDomain<State> other) {
+        if (isBot() || other.isTop()) return;
+        if (other.isBot()) {
+            setToBot();
+            return;
+        }
+        if (isTop()) {
+            state = other.state;
+            kind = other.kind;
+            return;
+        }
+        kind = state.equals(other.state) ? AbstractValueKind.VAL : AbstractValueKind.BOT;
+    }
+
+    @Override
+    public String toString() {
+        return "FiniteAbstractDomain{" +
+                "state=" + state +
+                ", kind=" + kind +
+                '}';
+    }
+
+    @Override
+    public abstract FiniteAbstractDomain<State> copyOf();
+}
