f5baec877116bf22c559bfa4f11a869d919d4266
Samuel Malec <xmalec@fi.muni.cz>
2024-11-16 23:55:24 +0100

    feat: create Parity enum

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/value/Sign.java
@@ -0,0 +1,13 @@
+package com.oracle.svm.hosted.analysis.ai.value;
+
+/**
+ * Represents the sign of a value in the abstract domain.
+ */
+
+public enum Sign {
+    TOP,
+    POS,
+    NEG,
+    ZERO,
+    BOT
+}
