932a80e8e028fdcb925c8abc21adf02a82bbd4fb
Samuel Malec <xmalec@fi.muni.cz>
2024-11-16 23:55:29 +0100

    feat: create Parity enum

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/value/Parity.java
@@ -0,0 +1,11 @@
+package com.oracle.svm.hosted.analysis.ai.value;
+
+/**
+ * Represents the parity of a value in the abstract domain.
+ */
+public enum Parity {
+    TOP,
+    ODD,
+    EVEN,
+    BOT
+}
