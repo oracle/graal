c3593db6e9fe6a6b4394f273edcb2b1efd3a23fa
Samuel Malec <xmalec@fi.muni.cz>
2024-11-17 00:54:18 +0100

    refactor: remove unused classes

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/transfer/policy/IteratorPolicy.java
+++
@@ -1,9 +0,0 @@
-package com.oracle.svm.hosted.analysis.ai.transfer.policy;
-
-public interface IteratorPolicy {
-    int getMaxJoinIterations();
-
-    int getMaxWidenIterations();
-
-    boolean shouldSkipCallNodes();
-}
