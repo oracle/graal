ffb87756b69bef35be055a3ce0f085fa5ea3c361
Samuel Malec <xmalec@fi.muni.cz>
2024-11-16 23:57:10 +0100

    fix: rewrite SignDomain using FiniteAbstractDomain

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/SignDomain.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/SignDomain.java
@@ -1,88 +1,23 @@
 package com.oracle.svm.hosted.analysis.ai.domain;
 
 import com.oracle.svm.hosted.analysis.ai.value.AbstractValueKind;
-import com.oracle.svm.hosted.analysis.ai.value.SignValue;
-import com.oracle.svm.hosted.analysis.ai.value.SignValue.Sign;
+import com.oracle.svm.hosted.analysis.ai.value.Sign;
 
-/**
- * The `SignDomain` class represents an abstract domain for sign analysis.
- * It extends the `AbstractDomain` class and uses `SignValue` to represent
- * the sign of values in the abstract domain.
- * <p>
- * The sign can be positive, negative, zero, top (unknown), or bottom (unreachable).
- */
-
-public final class SignDomain extends AbstractDomain<SignDomain> {
-    private SignValue value;
-
+public final class SignDomain extends FiniteAbstractDomain<Sign> {
     public SignDomain() {
-        this.value = new SignValue(Sign.BOT);
+        super(Sign.BOT, AbstractValueKind.BOT);
     }
 
     public SignDomain(Sign sign) {
-        this.value = new SignValue(sign);
+        super(sign, AbstractValueKind.VAL);
     }
 
     public SignDomain(SignDomain signDomain) {
-        this.value = signDomain.value;
+        super(signDomain.getState(), signDomain.getKind());
     }
 
     @Override
     public SignDomain copyOf() {
-        return new SignDomain(value.sign());
-    }
-
-    @Override
-    public boolean isBot() {
-        return value.kind() == AbstractValueKind.BOT;
-    }
-
-    @Override
-    public boolean isTop() {
-        return value.kind() == AbstractValueKind.TOP;
-    }
-
-    @Override
-    public boolean leq(SignDomain other) {
-        return value.leq(other.value);
-    }
-
-    @Override
-    public boolean equals(SignDomain other) {
-        return value.equals(other.value);
-    }
-
-    @Override
-    public void setToBot() {
-        value = new SignValue(Sign.BOT);
-    }
-
-    @Override
-    public void setToTop() {
-        value = new SignValue(Sign.TOP);
-    }
-
-    @Override
-    public void joinWith(SignDomain other) {
-        Sign newSign = value.joinWith(other.value) == AbstractValueKind.TOP ? Sign.TOP : value.sign();
-        value = new SignValue(newSign);
-    }
-
-    @Override
-    public void widenWith(SignDomain other) {
-        joinWith(other);
-    }
-
-    @Override
-    public void meetWith(SignDomain other) {
-        Sign newSign = value.meetWith(other.value) == AbstractValueKind.BOT ? Sign.BOT : value.sign();
-        value = new SignValue(newSign);
-    }
-
-    @Override
-    public String toString() {
-        return "SignDomain{" +
-                "sign=" + value +
-                '}';
+        return new SignDomain(this);
     }
 }
