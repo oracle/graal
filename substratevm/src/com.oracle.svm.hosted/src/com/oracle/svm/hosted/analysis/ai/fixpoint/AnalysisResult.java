47af27a62cd1ae46c141ae59553ce0769cd888cb
Samuel Malec <xmalec@fi.muni.cz>
2024-11-19 23:30:36 +0100

    delete: redundant logic

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/AnalysisResult.java
+++
@@ -1,7 +0,0 @@
-package com.oracle.svm.hosted.analysis.ai.fixpoint;
-
-public enum AnalysisResult {
-    OK,
-    WARNING,
-    ERROR
-}
--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/policy/IteratorPolicy.java
+++
@@ -1,7 +0,0 @@
-package com.oracle.svm.hosted.analysis.ai.fixpoint.policy;
-
-public interface IteratorPolicy {
-    int maxJoinIterations();
-
-    int maxWidenIterations();
-}
