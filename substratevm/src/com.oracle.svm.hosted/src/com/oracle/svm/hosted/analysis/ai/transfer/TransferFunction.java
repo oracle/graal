c87554ce168efcb50cac3d0288075ef9f4522b25
Samuel Malec <xmalec@fi.muni.cz>
2024-11-24 00:29:56 +0100

    docs: remove misleading javadoc

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/transfer/TransferFunction.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/transfer/TransferFunction.java
@@ -5,31 +5,20 @@ import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import jdk.graal.compiler.graph.Node;
 
 /**
- * Interface for transfer functions used in fixpoint analysis.
+ * Interface for transfer function used in fixpoint analysis.
  */
-
 public interface TransferFunction<
         Domain extends AbstractDomain<Domain>> {
+
     /**
-     * Basic types of nodes:
-     * 1. Constant -> ConstantNode
-     * 2. Binary arithmetic operations -> BinaryArithmeticNode
-     * 3. Binary relational operations -> CompareNode
-     * 4. Binary logical operations -> SLShortCircuitNode
-     * 5. Unary arithmetic operations -> UnaryArithmeticNode
-     * 6. Unary logical operations -> UnaryLogicalNode
-     * 7. Load -> LoadFieldNode
-     * 8. Store -> StoreFieldNode
-     * 9. If -> IfNode
-     * 10. LoopBegin -> LoopBeginNode
-     * 11. LoopEnd -> LoopEndNode
-     * 12. Merge -> MergeNode
-     * 13. Call -> CallNode
-     * 14. Invoke -> InvokeNode
+     * This method transforms an operation from original program semantics into
+     * an operation used on the abstract domain and returns the Domain
+     * representing the state of the program after executing the operation.
      *
      * @param node        node to analyze
      * @param environment environment to use of a FixpointIterator
      * @return Domain element representing the result of the analysis of the give node
      */
-    Domain analyzeNode(Node node, Environment<Domain> environment);
+    Domain computePostCondition(Node node, Environment<Domain> environment);
+
 }
