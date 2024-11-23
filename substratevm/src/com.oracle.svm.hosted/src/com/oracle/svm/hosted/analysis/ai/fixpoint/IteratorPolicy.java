9fbc92395eea4adaa73bb633107b93bb9268ec62
Samuel Malec <xmalec@fi.muni.cz>
2024-11-24 00:30:11 +0100

    feat: add IteratorPolicy record

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/IteratorPolicy.java
@@ -0,0 +1,10 @@
+package com.oracle.svm.hosted.analysis.ai.fixpoint;
+
+/**
+ * This class represents the policy for the fixpoint iterator.
+ * It contains the maximum number of iterations for the join and widen operations.
+ * In the future it could contain more information, like the direction of iteration, etc.
+ */
+public record IteratorPolicy(int maxJoinIterations, int maxWidenIterations) {
+    public static final IteratorPolicy DEFAULT = new IteratorPolicy(10, 10);
+}
