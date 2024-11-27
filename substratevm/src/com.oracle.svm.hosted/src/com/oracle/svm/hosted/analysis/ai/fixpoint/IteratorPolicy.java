829a04029d9502e657ab263758944706f78d834f
Samuel Malec <xmalec@fi.muni.cz>
2024-11-27 13:24:22 +0100

    docs

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/IteratorPolicy.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/IteratorPolicy.java
@@ -3,7 +3,7 @@ package com.oracle.svm.hosted.analysis.ai.fixpoint;
 /**
  * This class represents the policy for the fixpoint iterator.
  * It contains the maximum number of iterations for the join and widen operations.
- * In the future it could contain more information, like the direction of iteration, etc.
+ * In the future it could contain more information, like the direction of iteration (top-down, bottom-up, etc.)
  */
 public record IteratorPolicy(int maxJoinIterations, int maxWidenIterations) {
     public static final IteratorPolicy DEFAULT = new IteratorPolicy(10, 10);
