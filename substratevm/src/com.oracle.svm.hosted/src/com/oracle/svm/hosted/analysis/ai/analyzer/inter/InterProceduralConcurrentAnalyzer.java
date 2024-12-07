0a67cebb485f1acea0dacd0f836804af9ffc11a9
Samuel Malec <xmalec@fi.muni.cz>
2024-12-07 17:34:44 +0100

    feat: create CallInterpreter classes

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/inter/InterProceduralConcurrentAnalyzer.java
@@ -0,0 +1,31 @@
+package com.oracle.svm.hosted.analysis.ai.analyzer.inter;
+
+import com.oracle.svm.hosted.analysis.ai.analyzer.Analyzer;
+import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
+import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.ConcurrentWpoFixpointIterator;
+import com.oracle.svm.hosted.analysis.ai.interpreter.InterProceduralCallInterpreter;
+import com.oracle.svm.hosted.analysis.ai.interpreter.NodeInterpreter;
+import com.oracle.svm.hosted.analysis.ai.interpreter.TransferFunction;
+import jdk.graal.compiler.debug.DebugContext;
+import jdk.graal.compiler.nodes.cfg.ControlFlowGraph;
+
+public class InterProceduralConcurrentAnalyzer<
+        Domain extends AbstractDomain<Domain>>
+        extends Analyzer<Domain> {
+
+    private final DebugContext debug;
+
+    public InterProceduralConcurrentAnalyzer(DebugContext debug) {
+        this.debug = debug;
+    }
+
+    @Override
+    public void run(ControlFlowGraph graph, Domain initialDomain, NodeInterpreter<Domain> nodeInterpreter) {
+        var transferFunction = new TransferFunction<>(nodeInterpreter, new InterProceduralCallInterpreter());
+        var iterator = new ConcurrentWpoFixpointIterator<>(graph, transferFunction, initialDomain, debug);
+        debug.log("Starting analysis");
+        var stateMap = iterator.iterateUntilFixpoint();
+        debug.log("Analysis finished");
+        debug.log(stateMap.toString());
+    }
+}
