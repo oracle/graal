7d2cc282007f319d9454b8f5e2cca323ff27537e
Samuel Malec <xmalec@fi.muni.cz>
2024-12-11 12:55:24 +0100

    remove unused classes

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/CallInterpreter.java
+++
@@ -1,7 +0,0 @@
-package com.oracle.svm.hosted.analysis.ai.interpreter;
-
-import jdk.graal.compiler.nodes.InvokeNode;
-
-public interface CallInterpreter {
-    void exec(InvokeNode node);
-}
--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/example/IntIntervalNodeInterpreter.java
+++
@@ -1,4 +0,0 @@
-package com.oracle.svm.hosted.analysis.ai.interpreter.example;
-
-public class IntIntervalNodeInterpreter {
-}
