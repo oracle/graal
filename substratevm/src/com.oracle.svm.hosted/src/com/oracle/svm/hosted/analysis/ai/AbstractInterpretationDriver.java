338fcc5779e444d45e41d0d56582984f28af5d7a
Samuel Malec <xmalec@fi.muni.cz>
2024-12-11 12:53:04 +0100

    wip

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/AbstractInterpretationDriver.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/AbstractInterpretationDriver.java
@@ -5,6 +5,8 @@ import com.oracle.graal.pointsto.meta.InvokeInfo;
 import com.oracle.svm.hosted.ProgressReporter;
 
 import jdk.graal.compiler.debug.DebugContext;
+import jdk.graal.compiler.graph.Node;
+import jdk.graal.compiler.nodes.StructuredGraph;
 import jdk.graal.compiler.nodes.cfg.ControlFlowGraph;
 import jdk.graal.compiler.nodes.cfg.ControlFlowGraphBuilder;
 
@@ -25,51 +27,43 @@ public class AbstractInterpretationDriver {
              * Make a new scope for logging, run with -H:Log=AbstractInterpretation to activate it
              */
             try (var scope = debug.scope("AbstractInterpretation")) {
-                debug.log("Starting the analysis");
                 doRun();
-                debug.log("Analysis finished");
             }
         }
     }
 
     private void doRun() {
         var cfgGraph = getGraph(root);
-        debug.log("Printing the control flow graph");
-        for (var block : cfgGraph.getBlocks()) {
-            debug.log("Block: " + block);
-            for (var node : block.getNodes()) {
-                debug.log("\tNode: " + node);
-                debug.log("\t\tPredecessors: " + node.cfgPredecessors());
-                debug.log("\t\tSuccessors: " + node.cfgSuccessors());
-                debug.log("\t\tInputs: " + node.inputs());
-                debug.log("\t\t" + node.getNodeSourcePosition());
-            }
-
-            int count = block.getSuccessorCount();
-            debug.log("\tSuccessors: ");
-            for (int i = 0; i < count; i++) {
-                debug.log("\t\t" + block.getSuccessorAt(i));
-            }
-
-            count = block.getPredecessorCount();
-            debug.log("\tPredecessors: ");
-            for (int i = 0; i < count; i++) {
-                debug.log("\t\t" + block.getPredecessorAt(i));
-            }
+        debug.log("Printing ControlFlowGraph nodes");
+        for (Node node : cfgGraph.graph.getNodes()) {
+            debug.log("\tNode: " + node);
+            debug.log("\t\tPredecessors: " + node.cfgPredecessors());
+            debug.log("\t\tSuccessors: " + node.cfgSuccessors());
+            debug.log("\t\tInputs: " + node.inputs());
+            debug.log("\t\t" + node.getNodeSourcePosition());
         }
 
         debug.log("Printing the invokes");
         for (InvokeInfo invoke : root.getInvokes()) {
+            var method = invoke.getTargetMethod();
+            var g2 = method.decodeAnalyzedGraph(debug, null);
+            for (Node node : g2.getNodes()) {
+                debug.log("\t" + node);
+            }
             debug.log("Has invoke: " + invoke);
             for (AnalysisMethod callee : invoke.getOriginalCallees()) {
                 debug.log("\tCallee: " + callee);
             }
         }
 
+
     }
 
     private ControlFlowGraph getGraph(AnalysisMethod method) {
-        var structuredGraph = method.decodeAnalyzedGraph(debug, null);
+        StructuredGraph structuredGraph = method.decodeAnalyzedGraph(debug, null);
+        for (var node : structuredGraph.getNodes()) {
+            debug.log("\t" + node);
+        }
         return new ControlFlowGraphBuilder(structuredGraph).build();
     }
 }
