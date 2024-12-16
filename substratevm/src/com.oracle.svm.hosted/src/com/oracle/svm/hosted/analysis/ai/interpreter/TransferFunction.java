a5d49b658562e1af1185db02c5013f4208cb5352
Samuel Malec <xmalec@fi.muni.cz>
2024-12-16 22:44:15 +0100

    refactor: naming

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/TransferFunction.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/TransferFunction.java
@@ -3,7 +3,7 @@ package com.oracle.svm.hosted.analysis.ai.interpreter;
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy.IteratorPolicy;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractStateMap;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.summary.FixpointCache;
+import com.oracle.svm.hosted.analysis.ai.fixpoint.summary.SummaryCache;
 import com.oracle.svm.hosted.analysis.ai.interpreter.call.CallInterpreter;
 import com.oracle.svm.hosted.analysis.ai.interpreter.node.NodeInterpreter;
 import jdk.graal.compiler.debug.DebugContext;
@@ -27,7 +27,6 @@ public final class TransferFunction<Domain extends AbstractDomain<Domain>> {
     private final IteratorPolicy policy;
     private final DebugContext debug;
 
-
     public TransferFunction(NodeInterpreter<Domain> nodeInterpreter, CallInterpreter<Domain> callInterpreter, Domain initialDomain, IteratorPolicy policy, DebugContext debug) {
         this.nodeInterpreter = nodeInterpreter;
         this.callInterpreter = callInterpreter;
@@ -43,14 +42,15 @@ public final class TransferFunction<Domain extends AbstractDomain<Domain>> {
      * @param node             to analyze
      * @param abstractStateMap current state of the analysis
      */
-    public void analyzeNode(Node node, AbstractStateMap<Domain> abstractStateMap, FixpointCache<Domain> fixpointCache) {
+    public void analyzeNode(Node node, AbstractStateMap<Domain> abstractStateMap, SummaryCache<Domain> summaryCache) {
         debug.log("Analyzing node: " + node);
-        collectInvariantsFromPredecessors(node, abstractStateMap);
+        collectInvariantsFromCfgPredecessors(node, abstractStateMap);
         if (node instanceof InvokeNode invokeNode) {
-            callInterpreter.execInvoke(invokeNode, abstractStateMap, fixpointCache, this, initialDomain, policy, debug);
+            callInterpreter.execInvoke(invokeNode, abstractStateMap, summaryCache, this, initialDomain, policy, debug);
         } else {
             nodeInterpreter.execNode(node, abstractStateMap);
         }
+        abstractStateMap.incrementVisitCount(node);
     }
 
     /**
@@ -64,17 +64,18 @@ public final class TransferFunction<Domain extends AbstractDomain<Domain>> {
      * @param abstractStateMap current state of the analysis
      */
     public void analyzeEdge(Node sourceNode, Node destinationNode, AbstractStateMap<Domain> abstractStateMap) {
+        debug.log("Analyzing edge: " + sourceNode + " -> " + destinationNode);
         nodeInterpreter.execEdge(sourceNode, destinationNode, abstractStateMap);
     }
 
     /**
-     * Collect invariants from predecessors of the given node.
+     * Collect invariants from CFG predecessors of the given node.
      * NOTE: This method should modify the precondition of the destination node directly, to avoid creating copies.
      *
      * @param destinationNode  the node to which the edge goes
      * @param abstractStateMap current state of the analysis
      */
-    public void collectInvariantsFromPredecessors(Node destinationNode, AbstractStateMap<Domain> abstractStateMap) {
+    public void collectInvariantsFromCfgPredecessors(Node destinationNode, AbstractStateMap<Domain> abstractStateMap) {
         for (Node predecessor : destinationNode.cfgPredecessors()) {
             analyzeEdge(predecessor, destinationNode, abstractStateMap);
         }
@@ -88,9 +89,9 @@ public final class TransferFunction<Domain extends AbstractDomain<Domain>> {
         }
     }
 
-    public void analyzeBlock(HIRBlock block, AbstractStateMap<Domain> abstractStateMap, FixpointCache<Domain> fixpointCache) {
+    public void analyzeBlock(HIRBlock block, AbstractStateMap<Domain> abstractStateMap, SummaryCache<Domain> summaryCache) {
         for (Node node : block.getNodes()) {
-            analyzeNode(node, abstractStateMap, fixpointCache);
+            analyzeNode(node, abstractStateMap, summaryCache);
         }
     }
 }
