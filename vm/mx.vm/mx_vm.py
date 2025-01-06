eb7793c57ceb1b0ce85509902af67a2ba8bbeeba
Jirka Marsik <jiri.marsik@oracle.com>
2025-01-06 08:58:27 +0100

    Update vm_config for ce_nodejs

--- vm/mx.vm/mx_vm.py
+++ vm/mx.vm/mx_vm.py
@@ -155,7 +155,7 @@ mx_sdk_vm.register_vm_config('community', ce_unchained_components, _suite, env_f
 mx_sdk_vm.register_vm_config('espresso-community', ['antlr4', 'cov', 'dap', 'ejc', 'ejvm', 'ellvm', 'gvm', 'icu4j', 'xz', 'ins', 'insight', 'insightheap', 'java', 'llp', 'llrc', 'llrlf', 'llrn', 'lsp', 'nfi', 'nfi-libffi', 'nr_lib_javavm', 'pro', 'rgx', 'sdk', 'sdkc', 'sdkl', 'sdkni', 'tfl', 'tfla', 'tflc', 'tflm', 'truffle-json'], _suite, env_file='ce-llvm-espresso')
 mx_sdk_vm.register_vm_config('espresso-community', ['cov', 'dap', 'ejc', 'ejvm', 'gvm', 'icu4j', 'xz', 'ins', 'insight', 'insightheap', 'java', 'lsp', 'nfi', 'nfi-libffi', 'nr_lib_javavm', 'pro', 'rgx', 'sdk', 'sdkc', 'sdkl', 'sdkni', 'tfl', 'tfla', 'tflc', 'tflm', 'truffle-json'], _suite, env_file='ce-espresso')
 mx_sdk_vm.register_vm_config('ce', ce_components + ['icu4j', 'xz', 'js', 'jsl', 'jss', 'rgx', 'bnative-image', 'snative-image-agent', 'snative-image-diagnostics-agent'], _suite, dist_name='ce-js', env_file='ce-js')
-mx_sdk_vm.register_vm_config('ce', ce_components + ['icu4j', 'xz', 'js', 'jsl', 'jss', 'libpoly', 'njs', 'njsl', 'rgx', 'sjsvm', 'spolyglot'], _suite, dist_name='ce', env_file='ce-nodejs')
+mx_sdk_vm.register_vm_config('ce', ce_components + ['gwal', 'gwa', 'icu4j', 'xz', 'js', 'jsl', 'jss', 'libpoly', 'njs', 'njsl', 'rgx', 'sjsvm', 'spolyglot', 'swasmvm'], _suite, dist_name='ce', env_file='ce-nodejs')
 mx_sdk_vm.register_vm_config('ce', ce_components_minimal + ['antlr4', 'llrn', 'llp', 'llrc', 'llrl', 'llrlf'], _suite, env_file='ce-llvm')
 mx_sdk_vm.register_vm_config('ce-python', ce_python_components, _suite)
 mx_sdk_vm.register_vm_config('ce-fastr', ce_fastr_components, _suite)
