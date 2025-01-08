c9b09fe35c79064bc917e82edf04e4b5f2a19c60
Samuel Malec <xmalec@fi.muni.cz>
2025-01-08 10:31:05 +0100

    refactor: cleanup CallInterpreter api

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/CallInterpreter.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/CallInterpreter.java
@@ -1,38 +1,17 @@
 package com.oracle.svm.hosted.analysis.ai.interpreter.call;
 
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy.IteratorPolicy;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractStateMap;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.summary.SummaryCache;
-import com.oracle.svm.hosted.analysis.ai.interpreter.TransferFunction;
-import jdk.graal.compiler.debug.DebugContext;
+import jdk.graal.compiler.graph.Node;
+import jdk.graal.compiler.nodes.Invoke;
 import jdk.graal.compiler.nodes.InvokeNode;
 
 /**
- * Represents a function call interpreter.
+ * Represents an interpreter of method calls.
  *
  * @param <Domain> type of the derived {@link AbstractDomain}
  */
 public interface CallInterpreter<Domain extends AbstractDomain<Domain>> {
 
-    /**
-     * Executes the given invoke node.
-     * We need to be able to create a new fixpointIterator in place to iterate over the function graph
-     * TODO: this is pretty ugly, consult this how to potentially rewrite this
-     *
-     * @param invokeNode             the invoke node to execute
-     * @param abstractStateMap       the abstract state map
-     * @param summaryCache          the fixpoint cache
-     * @param domainTransferFunction the domain transfer function
-     * @param initialDomain          the initial domain
-     * @param policy                 the iterator policy
-     * @param debug                  the debug context
-     */
-    void execInvoke(InvokeNode invokeNode,
-                    AbstractStateMap<Domain> abstractStateMap,
-                    SummaryCache<Domain> summaryCache,
-                    TransferFunction<Domain> domainTransferFunction,
-                    Domain initialDomain,
-                    IteratorPolicy policy,
-                    DebugContext debug);
+    void execInvoke(Invoke invoke, Node invokeNode, AbstractStateMap<Domain> abstractStateMap);
 }
