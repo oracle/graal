9e4cf6f198bedc9c642e0d8888014690d22933f8
Samuel Malec <xmalec@fi.muni.cz>
2025-01-08 10:35:01 +0100

    feat: implement isConcurrent() + isSequential()

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/iterator/policy/IteratorPolicy.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/iterator/policy/IteratorPolicy.java
@@ -1,13 +1,19 @@
 package com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy;
 
 /**
- * Represents the policy for a fixpoint iterator.
- * In the future it could contain more information, like the direction of iteration (top-down, bottom-up, etc.),
- * also the widening limit could be removed and instead use something to hint the widening limit
- * for example:
- * for (int i = 0; i < 10; i++) {} // widening limit is 10
+ * Represents the parameters of a fixpoint iterator.
+ * In the future it could contain more information, like the direction of iteration (forward, backward, etc.),
+ * also the widening limit could be removed and instead use some techniques to deduce the widening limit
  */
-public record IteratorPolicy(int maxJoinIterations, int maxWidenIterations, IteratorStrategy strategy) {
-    public static final IteratorPolicy DEFAULT_CONCURRENT = new IteratorPolicy(10, 10, IteratorStrategy.CONCURRENT);
-    public static final IteratorPolicy DEFAULT_SEQUENTIAL = new IteratorPolicy(10, 20, IteratorStrategy.SEQUENTIAL);
+public record IteratorPolicy(int maxJoinIterations, int maxWidenIterations, IterationStrategy strategy) {
+    public static final IteratorPolicy DEFAULT_CONCURRENT = new IteratorPolicy(10, 10, IterationStrategy.CONCURRENT);
+    public static final IteratorPolicy DEFAULT_SEQUENTIAL = new IteratorPolicy(10, 20, IterationStrategy.SEQUENTIAL);
+
+    public boolean isConcurrent() {
+        return strategy == IterationStrategy.CONCURRENT;
+    }
+
+    public boolean isSequential() {
+        return strategy == IterationStrategy.SEQUENTIAL;
+    }
 }
