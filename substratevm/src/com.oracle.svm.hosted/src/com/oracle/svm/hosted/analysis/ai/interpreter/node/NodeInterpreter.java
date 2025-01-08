c59c629ded57337f3854966fb6e97f31c0e3d0be
Samuel Malec <xmalec@fi.muni.cz>
2025-01-08 10:35:24 +0100

    docs: add javadoc

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/node/NodeInterpreter.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/node/NodeInterpreter.java
@@ -4,6 +4,12 @@ import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractStateMap;
 import jdk.graal.compiler.graph.Node;
 
+/**
+ * Interface for interpreting the effects of nodes and edges of Graal IR
+ * in the abstract interpretation.
+ *
+ * @param <Domain> type of the derived {@link AbstractDomain}
+ */
 public interface NodeInterpreter<Domain extends AbstractDomain<Domain>> {
 
     void execEdge(Node source, Node destination, AbstractStateMap<Domain> abstractStateMap);
