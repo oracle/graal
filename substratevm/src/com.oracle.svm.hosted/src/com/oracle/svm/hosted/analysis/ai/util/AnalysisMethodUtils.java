c953f4d15f45b36f91754f45d8a9c2a670200907
Samuel Malec <xmalec@fi.muni.cz>
2025-01-08 10:28:43 +0100

    feat: implement AnalysisMethodUtils

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/util/AnalysisMethodUtils.java
@@ -0,0 +1,9 @@
+package com.oracle.svm.hosted.analysis.ai.util;
+
+import com.oracle.graal.pointsto.meta.AnalysisMethod;
+
+public class AnalysisMethodUtils {
+    public static boolean isJavaLangMethod(AnalysisMethod analysisMethod) {
+        return analysisMethod.getQualifiedName().startsWith("java") || analysisMethod.getQualifiedName().startsWith("jdk");
+    }
+}
