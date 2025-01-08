dcc9f81cc61d1a98b04db202e979e1d939a6b5a4
Samuel Malec <xmalec@fi.muni.cz>
2025-01-08 10:34:25 +0100

    feat: implement CallInterpreters

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/InterProceduralCallInterpreter.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/InterProceduralCallInterpreter.java
@@ -1,41 +1,70 @@
 package com.oracle.svm.hosted.analysis.ai.interpreter.call;
 
+import com.oracle.graal.pointsto.meta.AnalysisMethod;
+import com.oracle.svm.hosted.analysis.ai.analyzer.context.InterProceduralAnalysisContext;
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.ConcurrentWpoFixpointIterator;
+import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.FixpointIterator;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.SequentialWtoFixpointIterator;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy.IteratorPolicy;
+import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractState;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractStateMap;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.summary.SummaryCache;
 import com.oracle.svm.hosted.analysis.ai.interpreter.TransferFunction;
-import jdk.graal.compiler.debug.DebugContext;
-import jdk.graal.compiler.nodes.InvokeNode;
-import jdk.graal.compiler.nodes.cfg.ControlFlowGraphBuilder;
+import com.oracle.svm.hosted.analysis.ai.summary.Summary;
+import com.oracle.svm.hosted.analysis.ai.summary.SummaryCache;
+import com.oracle.svm.hosted.analysis.ai.util.GraphUtils;
+import com.oracle.svm.hosted.analysis.ai.util.AnalysisMethodUtils;
+import jdk.graal.compiler.graph.Node;
+import jdk.graal.compiler.nodes.Invoke;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
 
 public final class InterProceduralCallInterpreter<
         Domain extends AbstractDomain<Domain>>
         implements CallInterpreter<Domain> {
 
-    /* TODO implement summaries */
-    @Override
-    public void execInvoke(InvokeNode invokeNode,
-                           AbstractStateMap<Domain> abstractStateMap,
-                           SummaryCache<Domain> summaryCache,
-                           TransferFunction<Domain> transferFunction,
-                           Domain initialDomain,
-                           IteratorPolicy policy,
-                           DebugContext debug) {
+    private final InterProceduralAnalysisContext<Domain> analysisContext;
 
-        var callTargetCfg = new ControlFlowGraphBuilder(invokeNode.callTarget().graph()).build();
-        switch (policy.strategy()) {
-            case CONCURRENT:
-                var concurrentIterator = new ConcurrentWpoFixpointIterator<>(callTargetCfg, policy, transferFunction, initialDomain, debug);
-                concurrentIterator.iterateUntilFixpoint();
-                break;
-            case SEQUENTIAL:
-                var sequentialIterator = new SequentialWtoFixpointIterator<>(callTargetCfg, policy, transferFunction, initialDomain, debug);
-                sequentialIterator.iterateUntilFixpoint();
-                break;
+    public InterProceduralCallInterpreter(InterProceduralAnalysisContext<Domain> analysisContext) {
+        this.analysisContext = analysisContext;
+    }
+
+    @Override
+    public void execInvoke(Invoke invoke, Node invokeNode, AbstractStateMap<Domain> abstractStateMap) {
+        analysisContext.getLogger().logToFile("InterProceduralCallInterpreter::execInvoke invokeNode: " + invoke);
+        ResolvedJavaMethod targetMethod = invoke.getTargetMethod();
+        SummaryCache<Domain> summaryCache = analysisContext.getSummaryCache();
+        Summary<Domain> invokeSummary = analysisContext.getSummarySupplier().get(invoke, abstractStateMap.getState(invokeNode));
+        AnalysisMethod targetAnalysisMethod = GraphUtils.getInvokeAnalysisMethod(analysisContext.getRoot(), invoke);
+        analysisContext.getLogger().logHighlightedDebugInfo("Analyzing AnalysisMethod: " + targetAnalysisMethod.getQualifiedName());
+
+        if (analysisContext.getMethodFilter().shouldSkip(targetAnalysisMethod)) {
+            return;
         }
 
+        if (summaryCache.contains(targetMethod.getName(), invokeSummary)) {
+            analysisContext.getLogger().logToFile("Summary cache contains targetMethod: " + targetMethod.getName());
+            Domain domain = summaryCache.getPostCondition(targetMethod.getName(), invokeSummary);
+            analysisContext.getLogger().logToFile("The summary is: " + domain);
+            abstractStateMap.setPostCondition(invokeNode, domain);
+            return;
+        }
+
+        /* Create new fixpoint iterator for the target method */
+        TransferFunction<Domain> transferFunction = new TransferFunction<>(analysisContext.getNodeInterpreter(), new InterProceduralCallInterpreter<>(analysisContext), analysisContext.getLogger());
+        FixpointIterator<Domain> fixpointIterator;
+        analysisContext.getCallStack().push(targetAnalysisMethod);
+        analysisContext.getLogger().logToFile("Call stack: " + analysisContext.getCallStack());
+
+        if (analysisContext.getIteratorPolicy().isConcurrent()) {
+            fixpointIterator = new ConcurrentWpoFixpointIterator<>(analysisContext, transferFunction);
+        } else {
+            fixpointIterator = new SequentialWtoFixpointIterator<>(analysisContext, transferFunction);
+        }
+
+        AbstractStateMap<Domain> invokeAbstractStateMap = fixpointIterator.iterateUntilFixpoint();
+        AbstractState<Domain> returnAbstractState = invokeAbstractStateMap.getReturnState();
+        analysisContext.getLogger().logToFile("Analyzing AnalysisMethod finished with abstract context: " + returnAbstractState.toString());
+        Summary<Domain> completeSummary = analysisContext.getSummarySupplier().createCompleteSummary(invokeSummary, returnAbstractState);
+        summaryCache.put(targetMethod.getName(), completeSummary);
+        analysisContext.getCallStack().pop();
     }
 }
--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/IntraProceduralCallInterpreter.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/IntraProceduralCallInterpreter.java
@@ -1,47 +1,34 @@
 package com.oracle.svm.hosted.analysis.ai.interpreter.call;
 
+import com.oracle.svm.hosted.analysis.ai.analyzer.context.IntraProceduralAnalysisContext;
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.ConcurrentWpoFixpointIterator;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.SequentialWtoFixpointIterator;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy.IteratorPolicy;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractStateMap;
-import com.oracle.svm.hosted.analysis.ai.fixpoint.summary.SummaryCache;
-import com.oracle.svm.hosted.analysis.ai.interpreter.TransferFunction;
-import jdk.graal.compiler.debug.DebugContext;
+import jdk.graal.compiler.graph.Node;
+import jdk.graal.compiler.nodes.Invoke;
 import jdk.graal.compiler.nodes.InvokeNode;
-import jdk.graal.compiler.nodes.cfg.ControlFlowGraphBuilder;
 
 /**
  * Represents a context insensitive call interpreter.
- * It ignores function calls except for direct native calls.
+ * It ignores methods in the sense that every method is a black box,
+ * and therefore produces the `top` value of the used abstract domain.
  *
  * @param <Domain> type of the derived {@link AbstractDomain}
  */
-public final class IntraProceduralCallInterpreter<Domain extends AbstractDomain<Domain>>
+public final class IntraProceduralCallInterpreter<
+        Domain extends AbstractDomain<Domain>>
         implements CallInterpreter<Domain> {
 
-    @Override
-    public void execInvoke(InvokeNode invokeNode,
-                           AbstractStateMap<Domain> abstractStateMap,
-                           SummaryCache<Domain> summaryCache,
-                           TransferFunction<Domain> transferFunction,
-                           Domain initialDomain,
-                           IteratorPolicy policy,
-                           DebugContext debug) {
+    private final IntraProceduralAnalysisContext<Domain> analysisContext;
 
-        /* If the method is native java method, execute it */
-        if (invokeNode.callTarget().targetMethod().isDefault()) {
-            var callTargetCfg = new ControlFlowGraphBuilder(invokeNode.callTarget().graph()).build();
-            switch (policy.strategy()) {
-                case CONCURRENT:
-                    var concurrentIterator = new ConcurrentWpoFixpointIterator<>(callTargetCfg, policy, transferFunction, initialDomain, debug);
-                    concurrentIterator.iterateUntilFixpoint();
-                case SEQUENTIAL:
-                    var sequentialIterator = new SequentialWtoFixpointIterator<>(callTargetCfg, policy, transferFunction, initialDomain, debug);
-                    sequentialIterator.iterateUntilFixpoint();
-            }
-        }
-        abstractStateMap.setPostCondition(invokeNode, initialDomain);
-        /* Otherwise, do nothing */
+    public IntraProceduralCallInterpreter(IntraProceduralAnalysisContext<Domain> analysisContext) {
+        this.analysisContext = analysisContext;
+    }
+
+    @Override
+    public void execInvoke(Invoke invoke, Node invokeNode, AbstractStateMap<Domain> abstractStateMap) {
+        analysisContext.getLogger().logToFile("IntraProceduralCallInterpreter::execInvoke invoke: " + invoke);
+        analysisContext.getLogger().logToFile("IntraProceduralCallInterpreter::execInvoke targetMethod: " + invoke.getTargetMethod());
+        /* We set the post condition to top since we don't know the side effect of this call  */
+        abstractStateMap.setPostCondition(invokeNode, AbstractDomain.createTop(analysisContext.getInitialDomain()));
     }
 }
