fc78f5eebff7e26bf2dddeca82a7ca376ce6ef9e
Samuel Malec <xmalec@fi.muni.cz>
2025-01-08 10:32:38 +0100

    feat: create MethodFilter

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/context/filter/MethodFilter.java
@@ -0,0 +1,12 @@
+package com.oracle.svm.hosted.analysis.ai.analyzer.context.filter;
+
+import com.oracle.graal.pointsto.meta.AnalysisMethod;
+
+/**
+ * Represents a method filter that can be used to filter methods during analysis.
+ * Used in some cases to skip methods that are not relevant for the analysis.
+ * For example when analyzing a specific package, the filter can be used to skip methods that are not in the package.
+ */
+public interface MethodFilter {
+    boolean shouldSkip(AnalysisMethod method);
+}
