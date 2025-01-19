84b4527af89a74ac3d26209c89c52d1612c5e3eb
Samuel Malec <xmalec@fi.muni.cz>
2025-01-19 19:05:04 +0100

    refactor: CheckStatus -> CheckerStatus

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/checker/CheckerResult.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/checker/CheckerResult.java
@@ -4,7 +4,7 @@ package com.oracle.svm.hosted.analysis.ai.checker;
  * Represents the result of a check performed by a checker.
  * If the Checker has additional information to provide, it can be included in the details field.
  */
-public record CheckerResult(CheckStatus result, String details) {
+public record CheckerResult(CheckerStatus result, String details) {
 
     @Override
     public String toString() {
