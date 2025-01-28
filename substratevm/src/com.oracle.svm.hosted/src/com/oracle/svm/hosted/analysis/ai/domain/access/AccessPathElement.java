adcc67648993f85934299ede9c70e13370476893
Samuel Malec <xmalec@fi.muni.cz>
2025-01-28 20:59:58 +0100

    feat: implement AccessPathElement

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/access/AccessPathElement.java
@@ -0,0 +1,13 @@
+package com.oracle.svm.hosted.analysis.ai.domain.access;
+
+/**
+ * Represents an element in an access path - either a field access or an array index
+ */
+public interface AccessPathElement {
+
+    String toString();
+
+    boolean equals(Object other);
+
+    int hashCode();
+}
