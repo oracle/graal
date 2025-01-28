76cbe2ec4c0584ae53950b049b52d312fcc7cc46
Samuel Malec <xmalec@fi.muni.cz>
2025-01-28 10:38:51 +0100

    feat: add wrapper methods for SetValue

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/SetDomain.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/SetDomain.java
@@ -3,6 +3,9 @@ package com.oracle.svm.hosted.analysis.ai.domain;
 import com.oracle.svm.hosted.analysis.ai.value.AbstractValueKind;
 import com.oracle.svm.hosted.analysis.ai.value.SetValue;
 
+import java.util.Set;
+import java.util.function.Predicate;
+
 /*
     This abstract domain represents a set of elements.
     For example the domain could represent set of live variables
@@ -32,6 +35,38 @@ public final class SetDomain<Element> extends LatticeDomain<SetValue<Element>, S
 
     public void remove(Element element) {
         getValue().remove(element);
+        updateKind();
+    }
+
+    public boolean empty() {
+        return getValue().empty();
+    }
+
+    public int getSize() {
+        return getValue().getSize();
+    }
+
+    public Set<Element> getSet() {
+        return getValue().getSet();
+    }
+
+    public void filter(Predicate<Element> predicate) {
+        getValue().filter(predicate);
+        updateKind();
+    }
+
+    public void unionWith(SetDomain<Element> other) {
+        getValue().unionWith(other.getValue());
+        updateKind();
+    }
+
+    public void intersectionWith(SetDomain<Element> other) {
+        getValue().intersectionWith(other.getValue());
+        updateKind();
+    }
+
+    public void differenceWith(SetDomain<Element> other) {
+        getValue().differenceWith(other.getValue());
         updateKind();
     }
 
