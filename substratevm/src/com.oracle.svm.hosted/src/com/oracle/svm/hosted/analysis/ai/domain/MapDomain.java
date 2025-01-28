0a78fd8a21ee35a351e69303c8ea266041543a17
Samuel Malec <xmalec@fi.muni.cz>
2025-01-28 01:16:06 +0100

    fix: update after changing the map

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/MapDomain.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/MapDomain.java
@@ -2,13 +2,18 @@ package com.oracle.svm.hosted.analysis.ai.domain;
 
 import com.oracle.svm.hosted.analysis.ai.value.AbstractValueKind;
 import com.oracle.svm.hosted.analysis.ai.value.MapValue;
+import jdk.vm.ci.meta.Value;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * This abstract domain maps elements (variables, memory locations, etc.) to a common
  * abstract domain.
  * One example could be mapping variables to intervals, signs, etc.
- * We do not represent AbstractValueKind.TOP values in this domain, to minimize the size of the used memory,
- * if a Key is not present inside the map, we return TOP
+ * In order to minimize the size of the used Map,
+ * if a Key is not present, we return TOP value of the {@link AbstractDomain}
  */
 public final class MapDomain<
         Key,
@@ -33,8 +38,14 @@ public final class MapDomain<
         this.initialDomain = initialDomain;
     }
 
+    public MapDomain(Map<Key, Domain> map, Domain initialDomain) {
+        super(() -> new MapValue<>(initialDomain));
+        this.initialDomain = initialDomain.copyOf();
+        map.forEach(this::put);
+    }
+
     public MapDomain(MapDomain<Key, Domain> other) {
-        super(() -> new MapValue<>(other.initialDomain));
+        super(() -> new MapValue<>(other.getValue()));
         this.initialDomain = other.initialDomain.copyOf();
     }
 
@@ -42,8 +53,28 @@ public final class MapDomain<
         return getValue().getDomainAtKey(key);
     }
 
+    public void filter(Predicate<Map.Entry<Key, Domain>> predicate) {
+        getValue().filter(predicate);
+        updateKind();
+    }
+
+    public void transform(Function<Domain, Domain> function) {
+        getValue().transform(function);
+        updateKind();
+    }
+
+    public void update(Function<Domain, Domain> function, Key key) {
+        getValue().update(function, key);
+        updateKind();
+    }
+
     public void put(Key key, Domain value) {
         getValue().insertOrAssign(key, value);
+        updateKind();
+    }
+
+    public int getSize() {
+        return getValue().getSize();
     }
 
     @Override
