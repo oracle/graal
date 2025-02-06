b9bd7242d73f8460f6312ba6413e8d7a39a10bf0
Josef Haider <josef.haider@oracle.com>
2025-02-06 17:04:16 +0100

    TRegex: multiple fixes for look-behind merging across main expression boundary.

--- regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/ast/RegexASTSubtreeRootNode.java
+++ regex/src/com.oracle.truffle.regex/src/com/oracle/truffle/regex/tregex/parser/ast/RegexASTSubtreeRootNode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -61,7 +61,6 @@ public abstract class RegexASTSubtreeRootNode extends Term implements RegexASTVi
     private MatchFound unAnchoredInitialState;
     private PositionAssertion anchoredFinalState;
     private MatchFound matchFound;
-    private MatchFound matchFoundChecked;
     private boolean visitorGroupVisited = false;
 
     private final SubTreeIndex subtrees = new SubTreeIndex();
@@ -230,14 +229,5 @@ public abstract class RegexASTSubtreeRootNode extends Term implements RegexASTVi
     @Override
     protected JsonObject toJson(String typeName) {
         return super.toJson(typeName).append(Json.prop("group", astNodeId(group)));
-    }
-
-    public void setMatchFoundChecked(MatchFound matchFoundChecked) {
-        this.matchFoundChecked = matchFoundChecked;
-    }
-
-    public MatchFound getMatchFoundChecked() {
-        assert matchFoundChecked != null;
-        return matchFoundChecked;
     }
 }
