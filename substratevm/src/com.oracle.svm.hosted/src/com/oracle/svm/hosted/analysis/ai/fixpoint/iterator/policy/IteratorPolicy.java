a8a0d3f15760f49bd35c8a0de83763145ca53b88
Samuel Malec <xmalec@fi.muni.cz>
2025-02-10 20:33:25 +0100

    feat: add maxRecursionDepth

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/iterator/policy/IteratorPolicy.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/fixpoint/iterator/policy/IteratorPolicy.java
@@ -5,9 +5,13 @@ package com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy;
  * In the future it could contain more information, like the direction of iteration (forward, backward, etc.),
  * also the widening limit could be removed and instead use some techniques to deduce the widening limit
  */
-public record IteratorPolicy(int maxJoinIterations, int maxWidenIterations, IterationStrategy strategy) {
-    public static final IteratorPolicy DEFAULT_CONCURRENT = new IteratorPolicy(10, 10, IterationStrategy.CONCURRENT);
-    public static final IteratorPolicy DEFAULT_SEQUENTIAL = new IteratorPolicy(10, 20, IterationStrategy.SEQUENTIAL);
+public record IteratorPolicy(int maxJoinIterations,
+                             int maxWidenIterations,
+                             int maxRecursionDepth,
+                             IterationStrategy strategy) {
+
+    public static final IteratorPolicy DEFAULT_CONCURRENT = new IteratorPolicy(10, 10, 10, IterationStrategy.CONCURRENT);
+    public static final IteratorPolicy DEFAULT_SEQUENTIAL = new IteratorPolicy(10, 20, 10, IterationStrategy.SEQUENTIAL);
 
     public boolean isConcurrent() {
         return strategy == IterationStrategy.CONCURRENT;
