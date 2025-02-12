784b656467ed3aa51d766fa2d34389d9f218a5c2
Samuel Malec <xmalec@fi.muni.cz>
2025-02-12 15:24:48 +0100

    docs

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/NodeInterpreter.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/NodeInterpreter.java
@@ -1,6 +1,5 @@
 package com.oracle.svm.hosted.analysis.ai.interpreter;
 
-import com.oracle.svm.hosted.analysis.ai.analyzer.payload.AnalysisPayload;
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractStateMap;
 import jdk.graal.compiler.graph.Node;
@@ -13,7 +12,24 @@ import jdk.graal.compiler.graph.Node;
  */
 public interface NodeInterpreter<Domain extends AbstractDomain<Domain>> {
 
-    Domain execEdge(Node source, Node destination, AbstractStateMap<Domain> abstractStateMap, AnalysisPayload<Domain> payload);
+    /**
+     * Execute the edge between two nodes in a CFG of a single method.
+     * This method should update the pre-condition of the destination node according to the post-condition of the source.
+     *
+     * @param source      the node from which the edge originates
+     * @param destination the node to which the edge goes
+     * @param abstractStateMap current state of the analysis
+     * @return the updated abstract domain of the destination node
+     */
+    Domain execEdge(Node source, Node destination, AbstractStateMap<Domain> abstractStateMap);
 
-    Domain execNode(Node node, AbstractStateMap<Domain> abstractStateMap, AnalysisPayload<Domain> payload);
+    /**
+     * Execute the given node in the CFG of a single method.
+     * This method should compute the post-condition of the node according to the given Graal IR {@link Node}
+     * .
+     * @param node the node to analyze
+     * @param abstractStateMap current state of the analysis
+     * @return the updated abstract domain of the node
+     */
+    Domain execNode(Node node, AbstractStateMap<Domain> abstractStateMap);
 }
