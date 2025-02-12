c2e3bd316c76d98384ef97ffdc263906f4e29fbe
Samuel Malec <xmalec@fi.muni.cz>
2025-02-12 18:36:59 +0100

    fix: set domain to top

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/IntraProceduralCallInterpreter.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/IntraProceduralCallInterpreter.java
@@ -1,6 +1,6 @@
 package com.oracle.svm.hosted.analysis.ai.interpreter.call;
 
-import com.oracle.svm.hosted.analysis.ai.analyzer.context.IntraProceduralAnalysisContext;
+import com.oracle.svm.hosted.analysis.ai.analyzer.payload.AnalysisPayload;
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.state.AbstractStateMap;
 import jdk.graal.compiler.graph.Node;
@@ -13,20 +13,14 @@ import jdk.graal.compiler.nodes.Invoke;
  *
  * @param <Domain> type of the derived {@link AbstractDomain}
  */
-public final class IntraProceduralCallInterpreter<
-        Domain extends AbstractDomain<Domain>>
+public final class IntraProceduralCallInterpreter<Domain extends AbstractDomain<Domain>>
         implements CallInterpreter<Domain> {
 
-    private final IntraProceduralAnalysisContext<Domain> analysisContext;
-
-    public IntraProceduralCallInterpreter(IntraProceduralAnalysisContext<Domain> analysisContext) {
-        this.analysisContext = analysisContext;
-    }
-
     @Override
-    public void execInvoke(Invoke invoke, Node invokeNode, AbstractStateMap<Domain> abstractStateMap) {
-        analysisContext.getLogger().logToFile("IntraProceduralCallInterpreter::execInvoke invoke: " + invoke);
+    public void execInvoke(Invoke invoke, Node invokeNode, AbstractStateMap<Domain> abstractStateMap, AnalysisPayload<Domain> payload) {
+        payload.getLogger().logToFile("IntraProceduralCallInterpreter::execInvoke invoke: " + invoke);
+
         /* We set the post condition to top since we don't know the side effect of this call  */
-        abstractStateMap.setPostCondition(invokeNode, AbstractDomain.createTop(analysisContext.getInitialDomain()));
+        abstractStateMap.setPostCondition(invokeNode, AbstractDomain.createTop(payload.getInitialDomain()));
     }
 }
