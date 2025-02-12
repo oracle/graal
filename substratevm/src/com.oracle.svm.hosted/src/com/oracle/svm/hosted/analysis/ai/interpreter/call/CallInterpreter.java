625d4caa4d80e60f2b8e144465a2156ee692f455
Samuel Malec <xmalec@fi.muni.cz>
2025-02-12 23:16:14 +0100

    docs

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/CallInterpreter.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/interpreter/call/CallInterpreter.java
@@ -7,12 +7,20 @@ import jdk.graal.compiler.graph.Node;
 import jdk.graal.compiler.nodes.Invoke;
 
 /**
- * Represents an interpreter of method calls.
+ * Interface for interpreting invokes within the context of abstract interpretation.
  *
  * @param <Domain> type of the derived {@link AbstractDomain}
  */
 public interface CallInterpreter<Domain extends AbstractDomain<Domain>> {
 
+    /**
+     * Execute the given invoke in the CFG of a single method.
+     *
+     * @param invoke the invoke information
+     * @param invokeNode the node
+     * @param abstractStateMap the current state of the analysis
+     * @param payload the analysis payload
+     */
     void execInvoke(Invoke invoke,
                     Node invokeNode,
                     AbstractStateMap<Domain> abstractStateMap,
