1711e34c1f2e181c1f4beafcfcc72066b92de647
Samuel Malec <xmalec@fi.muni.cz>
2025-02-15 01:36:31 +0100

    docs

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/Analyzer.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/Analyzer.java
@@ -10,8 +10,23 @@ import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy.IteratorPolicy
 import com.oracle.svm.hosted.analysis.ai.interpreter.NodeInterpreter;
 import com.oracle.svm.hosted.analysis.ai.interpreter.TransferFunction;
 import com.oracle.svm.hosted.analysis.ai.interpreter.call.CallHandler;
+import com.oracle.svm.hosted.analysis.ai.summary.Summary;
+import com.oracle.svm.hosted.analysis.ai.summary.SummaryFactory;
 import jdk.graal.compiler.debug.DebugContext;
 
+
+/**
+ * Represents an abstract analyzer framework for performing analyses driven by a specific domain.
+ * The analyzer uses transfer functions, interpreters, and iterator policies to compute analysis results.
+ * It encapsulates logic for creating analysis payloads and facilitates method-specific analysis.
+ * To create an intra-procedural analyzer, it is sufficient to provide the initial domain, and the node interpreter.
+ * To create an inter-procedural analyzer, we need to also add an implementation of {@link Summary}, as well as logic
+ * for creating the summary from an abstract context -> handled by {@link SummaryFactory}.
+ * We can also add additional parameters, like list of checkers to be used during the analysis, or method filters,
+ * to restrict the analysis of specific methods.
+ *
+ * @param <Domain> the type of the abstract domain used for the analysis.
+ */
 public abstract class Analyzer<Domain extends AbstractDomain<Domain>> {
 
     protected final Domain initialDomain;
