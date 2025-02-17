9377b7b810fcbfa3b96d4f113e40db440b468a97
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2025-02-17 15:24:51 +0100

    espresso-shared: apply CPE

--- espresso-shared/mx.espresso-shared/suite.py
+++ espresso-shared/mx.espresso-shared/suite.py
@@ -4,7 +4,9 @@
 #
 # This code is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 only, as
-# published by the Free Software Foundation.
+# published by the Free Software Foundation.  Oracle designates this
+# particular file as subject to the "Classpath" exception as provided
+# by Oracle in the LICENSE file that accompanied this code.
 #
 # This code is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -20,7 +22,6 @@
 # or visit www.oracle.com if you need additional information or have any
 # questions.
 #
-
 suite = {
     "mxversion": "7.38.0",
     "name": "espresso-shared",
