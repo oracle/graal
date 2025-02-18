8d1c20c83b40bf0bb593d1d3644eaaee0caa294e
Samuel Malec <xmalec@fi.muni.cz>
2025-02-18 18:38:31 +0100

    feat: create InterProceduralAnalyzer.java

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/InterProceduralAnalyzer.java
@@ -0,0 +1,63 @@
+package com.oracle.svm.hosted.analysis.ai.analyzer;
+
+import com.oracle.graal.pointsto.meta.AnalysisMethod;
+import com.oracle.svm.hosted.analysis.ai.analyzer.call.InterProceduralCallHandler;
+import com.oracle.svm.hosted.analysis.ai.analyzer.payload.IteratorPayload;
+import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
+import com.oracle.svm.hosted.analysis.ai.interpreter.NodeInterpreter;
+import com.oracle.svm.hosted.analysis.ai.summary.SummaryFactory;
+import com.oracle.svm.hosted.analysis.ai.util.AbstractInterpretationLogger;
+import com.oracle.svm.hosted.analysis.ai.util.InterAnalysisConstants;
+import jdk.graal.compiler.debug.DebugContext;
+
+import java.io.IOException;
+
+public final class InterProceduralAnalyzer<Domain extends AbstractDomain<Domain>> extends Analyzer<Domain> {
+
+    private final SummaryFactory<Domain> summaryFactory;
+    private final int maxRecursionDepth;
+
+    private InterProceduralAnalyzer(Builder<Domain> builder) {
+        super(builder);
+        this.summaryFactory = builder.summaryFactory;
+        this.maxRecursionDepth = builder.maxRecursionDepth;
+    }
+
+    @Override
+    public void analyzeMethod(AnalysisMethod method, DebugContext debug) throws IOException {
+        AbstractInterpretationLogger logger = AbstractInterpretationLogger.getInstance(method, debug, null);
+        logger.logHighlightedDebugInfo("Inter Procedural Analyzer running on analysisMethod: " + method.toString());
+
+        IteratorPayload iteratorPayload = new IteratorPayload(iteratorPolicy);
+        InterProceduralCallHandler<Domain> callHandler = new InterProceduralCallHandler<>(initialDomain, nodeInterpreter, checkerManager, methodFilterManager, iteratorPayload, summaryFactory, maxRecursionDepth);
+        callHandler.handleRootCall(method, debug);
+
+        logger.logHighlightedDebugInfo("The computed summaries are: ");
+        logger.logDebugInfo(callHandler.getSummaryCache().toString());
+    }
+
+    public static class Builder<Domain extends AbstractDomain<Domain>> extends Analyzer.Builder<Builder<Domain>, Domain> {
+
+        private final SummaryFactory<Domain> summaryFactory;
+        private int maxRecursionDepth = InterAnalysisConstants.MAX_RECURSION_DEPTH;
+
+        public Builder(Domain initialDomain, NodeInterpreter<Domain> nodeInterpreter, SummaryFactory<Domain> summaryFactory) {
+            super(initialDomain, nodeInterpreter);
+            this.summaryFactory = summaryFactory;
+        }
+
+        public Builder<Domain> maxRecursionDepth(int maxRecursionDepth) {
+            this.maxRecursionDepth = maxRecursionDepth;
+            return self();
+        }
+
+        public InterProceduralAnalyzer<Domain> build() {
+            return new InterProceduralAnalyzer<>(this);
+        }
+
+        @Override
+        protected Builder<Domain> self() {
+            return this;
+        }
+    }
+}
---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/util/InterAnalysisConstants.java
@@ -0,0 +1,8 @@
+package com.oracle.svm.hosted.analysis.ai.util;
+
+public final class InterAnalysisConstants {
+    public static final int MAX_RECURSION_DEPTH = 10;
+
+    private InterAnalysisConstants() {
+    }
+}
