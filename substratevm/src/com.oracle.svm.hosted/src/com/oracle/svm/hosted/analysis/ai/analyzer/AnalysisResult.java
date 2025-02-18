927207ac762c136460713a1140a011be01a33d38
Samuel Malec <xmalec@fi.muni.cz>
2025-02-18 15:42:38 +0100

    docs

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/AnalysisResult.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/AnalysisResult.java
@@ -5,10 +5,16 @@ package com.oracle.svm.hosted.analysis.ai.analyzer;
  */
 public enum AnalysisResult {
 
-    OK, /* Analysis successfully finished */
-    ANALYSIS_FAILED, /* Some internal error occurred ( RuntimeException, IOException ) */
-    IN_SKIP_LIST, /* The method should be skipped according to the provided method filters */
-    MUTUAL_RECURSION_CYCLE, /* Two methods calling each other recursively -> I have no solution for this */
-    UNKNOWN_METHOD, /* The AnalysisMethod of an Invoke was not found in the current DebugContext */
-    RECURSION_LIMIT_OVERFLOW, /* The recursion limit was reached */
+    /* Analysis successfully finished */
+    OK,
+    /* Some internal error occurred ( RuntimeException, IOException ) */
+    ANALYSIS_FAILED,
+    /* The analysisMethod should be skipped according to the provided analysisMethod filters */
+    IN_SKIP_LIST,
+    /* Two methods calling each other recursively -> I have no solution for this */
+    MUTUAL_RECURSION_CYCLE,
+    /* The AnalysisMethod of an Invoke was not found in the current DebugContext */
+    UNKNOWN_METHOD,
+    /* The recursion limit was reached */
+    RECURSION_LIMIT_OVERFLOW,
 }
