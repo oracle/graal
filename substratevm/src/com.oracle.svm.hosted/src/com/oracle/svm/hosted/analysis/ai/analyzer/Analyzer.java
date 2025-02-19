8236073b07f0181a62b789c2e12a0ff307ea8056
Samuel Malec <xmalec@fi.muni.cz>
2025-02-19 13:58:14 +0100

    feat: custom builder

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/Analyzer.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/analyzer/Analyzer.java
@@ -1,19 +1,16 @@
 package com.oracle.svm.hosted.analysis.ai.analyzer;
 
 import com.oracle.graal.pointsto.meta.AnalysisMethod;
-import com.oracle.svm.hosted.analysis.ai.analyzer.payload.AnalysisPayload;
-import com.oracle.svm.hosted.analysis.ai.analyzer.payload.IteratorPayload;
 import com.oracle.svm.hosted.analysis.ai.analyzer.payload.filter.AnalysisMethodFilterManager;
 import com.oracle.svm.hosted.analysis.ai.checker.CheckerManager;
 import com.oracle.svm.hosted.analysis.ai.domain.AbstractDomain;
 import com.oracle.svm.hosted.analysis.ai.fixpoint.iterator.policy.IteratorPolicy;
 import com.oracle.svm.hosted.analysis.ai.interpreter.NodeInterpreter;
-import com.oracle.svm.hosted.analysis.ai.interpreter.TransferFunction;
-import com.oracle.svm.hosted.analysis.ai.interpreter.call.CallHandler;
 import com.oracle.svm.hosted.analysis.ai.summary.Summary;
 import com.oracle.svm.hosted.analysis.ai.summary.SummaryFactory;
 import jdk.graal.compiler.debug.DebugContext;
 
+import java.io.IOException;
 
 /**
  * Represents an abstract analyzer framework for performing analyses driven by a specific domain.
@@ -35,7 +32,7 @@ public abstract class Analyzer<Domain extends AbstractDomain<Domain>> {
     protected final CheckerManager checkerManager;
     protected final AnalysisMethodFilterManager methodFilterManager;
 
-    protected Analyzer(Builder<Domain> builder) {
+    protected Analyzer(Builder<?, Domain> builder) {
         this.initialDomain = builder.initialDomain;
         this.nodeInterpreter = builder.nodeInterpreter;
         this.iteratorPolicy = builder.iteratorPolicy;
@@ -43,39 +40,35 @@ public abstract class Analyzer<Domain extends AbstractDomain<Domain>> {
         this.methodFilterManager = builder.methodFilterManager;
     }
 
-    protected AnalysisPayload<Domain> createPayload(AnalysisMethod method, DebugContext debug, CallHandler<Domain> callHandler) {
-        TransferFunction<Domain> transferFunction = new TransferFunction<>(nodeInterpreter, callHandler);
-        IteratorPayload iteratorPayload = new IteratorPayload(iteratorPolicy);
-        return new AnalysisPayload<>(initialDomain, iteratorPayload, method, debug, transferFunction, checkerManager, methodFilterManager);
-    }
+    public abstract void analyzeMethod(AnalysisMethod method, DebugContext debug) throws IOException;
 
-    public abstract void analyzeMethod(AnalysisMethod method, DebugContext debug);
-
-    public static class Builder<Domain extends AbstractDomain<Domain>> {
+    public static abstract class Builder<T extends Builder<T, Domain>, Domain extends AbstractDomain<Domain>> {
         protected final Domain initialDomain;
         protected final NodeInterpreter<Domain> nodeInterpreter;
         protected IteratorPolicy iteratorPolicy = IteratorPolicy.DEFAULT_SEQUENTIAL;
         protected CheckerManager checkerManager = new CheckerManager();
         protected AnalysisMethodFilterManager methodFilterManager = new AnalysisMethodFilterManager();
 
-        public Builder(Domain initialDomain, NodeInterpreter<Domain> nodeInterpreter) {
+        protected Builder(Domain initialDomain, NodeInterpreter<Domain> nodeInterpreter) {
             this.initialDomain = initialDomain;
             this.nodeInterpreter = nodeInterpreter;
         }
 
-        public Builder<Domain> iteratorPolicy(IteratorPolicy iteratorPolicy) {
+        public T iteratorPolicy(IteratorPolicy iteratorPolicy) {
             this.iteratorPolicy = iteratorPolicy;
-            return this;
+            return self();
         }
 
-        public Builder<Domain> checkerManager(CheckerManager checkerManager) {
+        public T checkerManager(CheckerManager checkerManager) {
             this.checkerManager = checkerManager;
-            return this;
+            return self();
         }
 
-        public Builder<Domain> methodFilterManager(AnalysisMethodFilterManager methodFilterManager) {
+        public T methodFilterManager(AnalysisMethodFilterManager methodFilterManager) {
             this.methodFilterManager = methodFilterManager;
-            return this;
+            return self();
         }
+
+        protected abstract T self();
     }
 }
