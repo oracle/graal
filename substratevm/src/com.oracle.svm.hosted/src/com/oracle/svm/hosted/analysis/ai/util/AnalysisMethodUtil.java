76a31f4701237366d5b29056ade3804541a0abad
Samuel Malec <xmalec@fi.muni.cz>
2025-04-10 13:54:22 +0200

    feat: add private ctor

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/util/AnalysisMethodUtil.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/util/AnalysisMethodUtil.java
@@ -3,6 +3,10 @@ package com.oracle.svm.hosted.analysis.ai.util;
 import com.oracle.graal.pointsto.meta.AnalysisMethod;
 
 public final class AnalysisMethodUtil {
+
+    private AnalysisMethodUtil() {
+    }
+
     public static boolean isJavaLangMethod(AnalysisMethod analysisMethod) {
         return analysisMethod.getQualifiedName().startsWith("java") || analysisMethod.getQualifiedName().startsWith("jdk");
     }
