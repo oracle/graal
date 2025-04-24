ab6a44cade2f37dfd8c88a5f4709df68112307a7
Roland Schatz <roland.schatz@oracle.com>
2025-04-24 15:53:05 +0200

    Update libffi to version 3.4.8.

--- truffle/mx.truffle/mx_truffle.py
+++ truffle/mx.truffle/mx_truffle.py
@@ -1653,7 +1653,7 @@ class LibffiBuilderProject(mx.AbstractNativeProject, mx_native.NativeDependency)
         self.out_dir = self.get_output_root()
         if mx.get_os() == 'windows':
             self.delegate = mx_native.DefaultNativeProject(suite, name, subDir, [], [], None,
-                                                           os.path.join(self.out_dir, 'libffi-3.4.6'),
+                                                           os.path.join(self.out_dir, 'libffi-3.4.8'),
                                                            'static_lib',
                                                            deliverable='ffi',
                                                            cflags=['-MD', '-O2', '-DFFI_STATIC_BUILD'])
@@ -1690,7 +1690,7 @@ class LibffiBuilderProject(mx.AbstractNativeProject, mx_native.NativeDependency)
                                                   'include/ffi.h',
                                                   'include/ffitarget.h'],
                                                  os.path.join(self.out_dir, 'libffi-build'),
-                                                 os.path.join(self.out_dir, 'libffi-3.4.6'))
+                                                 os.path.join(self.out_dir, 'libffi-3.4.8'))
             configure_args = ['--disable-dependency-tracking',
                               '--disable-shared',
                               '--with-pic']
