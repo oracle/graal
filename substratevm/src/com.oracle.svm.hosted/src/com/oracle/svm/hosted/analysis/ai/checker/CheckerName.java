a7f7939849087b8c0f07a81c7b48047922077988
Samuel Malec <xmalec@fi.muni.cz>
2025-05-02 14:24:18 +0200

    remove: unused classes

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/checker/CheckerName.java
+++
@@ -1,8 +0,0 @@
-package com.oracle.svm.hosted.analysis.ai.checker;
-
-/*
- * Enumerates the names of the checkers that can be created by the {@link CheckerFactory}.
- */
-public enum CheckerName {
-    DivisionByZero
-}
