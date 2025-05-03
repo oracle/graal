092c2fd1cdef20ce685681165397c37c5ad90501
Samuel Malec <xmalec@fi.muni.cz>
2025-05-03 18:55:10 +0200

    refactor: implement arithmetical operations

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/value/Sign.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/domain/value/Sign.java
@@ -9,5 +9,34 @@ public enum Sign {
     POS,
     ZERO,
     NEG,
-    BOT
+    BOT;
+
+    public Sign plus(Sign other) {
+        if (this == BOT || other == BOT) return BOT;
+        if (this == TOP || other == TOP) return TOP;
+        if (this == ZERO) return other;
+        if (other == ZERO) return this;
+        if (this == POS && other == POS) return POS;
+        if (this == NEG && other == NEG) return NEG;
+        return TOP;
+    }
+
+    public Sign minus(Sign other) {
+        if (this == BOT || other == BOT) return BOT;
+        if (this == TOP || other == TOP) return TOP;
+        if (this == ZERO) return other.negate();
+        if (other == ZERO) return this;
+        if (this == POS && other == POS) return TOP;
+        if (this == NEG && other == POS) return NEG;
+        if (this == POS && other == NEG) return POS;
+        return TOP;
+    }
+
+    public Sign negate() {
+        if (this == BOT) return BOT;
+        if (this == TOP) return TOP;
+        if (this == ZERO) return ZERO;
+        if (this == POS) return NEG;
+        return POS;
+    }
 }
