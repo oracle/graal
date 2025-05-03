0ea7697107418bb100137a4919515d532e7c7a16
Samuel Malec <xmalec@fi.muni.cz>
2025-05-03 19:09:49 +0200

    refactor: setup and engine instantiation

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/AbstractInterpretationDriver.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/AbstractInterpretationDriver.java
@@ -1,46 +1,62 @@
 package com.oracle.svm.hosted.analysis.ai;
 
+import com.oracle.graal.pointsto.BigBang;
 import com.oracle.graal.pointsto.meta.AnalysisMethod;
 import com.oracle.svm.hosted.ProgressReporter;
-import com.oracle.svm.hosted.analysis.ai.example.access.inter.AccessPathIntervalInterAnalyzer;
-import com.oracle.svm.hosted.analysis.ai.util.AbstractInterpretationLogger;
-import com.oracle.svm.hosted.analysis.ai.util.LoggerVerbosity;
+import com.oracle.svm.hosted.analysis.ai.analyzer.AnalyzerManager;
+import com.oracle.svm.hosted.analysis.ai.example.pentagon.IntraProceduralPentagonAnalyzerWrapper;
+import com.oracle.svm.hosted.analysis.ai.log.AbstractInterpretationLogger;
+import com.oracle.svm.hosted.analysis.ai.log.LoggerVerbosity;
 import jdk.graal.compiler.debug.DebugContext;
 
 import java.io.IOException;
 
+/**
+ * The entry point of the abstract interpretation framework.
+ */
 public class AbstractInterpretationDriver {
 
     private final DebugContext debug;
-    private final AnalysisMethod root;
+    private final AnalyzerManager analyzerManager;
+    private final AbstractInterpretationEngine engine;
 
-    public AbstractInterpretationDriver(DebugContext debug, AnalysisMethod root) {
+    public AbstractInterpretationDriver(DebugContext debug, AnalysisMethod root, BigBang bigBang) {
         this.debug = debug;
-        this.root = root;
+        this.analyzerManager = new AnalyzerManager();
+        this.engine = new AbstractInterpretationEngine(analyzerManager, root, debug, bigBang);
     }
 
     @SuppressWarnings("try")
     public void run() {
         try (ProgressReporter.ReporterClosable c = ProgressReporter.singleton().printAbstractInterpretation()) {
-            /*
-             * Make a new scope for logging, run with -H:Log=AbstractInterpretation to activate it
-             */
+            /* Creating a new scope for logging, run with -H:Log=AbstractInterpretation to activate it */
             try (var scope = debug.scope("AbstractInterpretation")) {
-                doRun();
+                setupFramework();
+                engine.execute();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
-    // TODO: rebase master
-    // TODO: StrengthenGraph integerStamps, makeUnreachable
-    private void doRun() throws IOException {
-        /* Firstly, create a logger instance, the default logger is initialized to Verbosity INFO  */
-        AbstractInterpretationLogger logger = AbstractInterpretationLogger.getInstance(root, debug, LoggerVerbosity.SUMMARY);
+    /**
+     * This is the entry method for setting up the abstract interpretation framework.
+     * We can:
+     * 1. Provide the {@link com.oracle.svm.hosted.analysis.ai.analyzer.Analyzer} to the {@link AnalyzerManager}.
+     * These analyzers will then run as a part of the Native Image compilation process.
+     * 2. Create and configure the {@link AbstractInterpretationLogger}, most importantly the name of the dump-file and verbosity.
+     *
+     * @throws IOException in case of I/O errors during logger initialization.
+     */
+    private void setupFramework() throws IOException {
+        /* Firstly, let's create a logger instance  */
+        AbstractInterpretationLogger logger = AbstractInterpretationLogger.getInstance(debug, "myLogger", LoggerVerbosity.INFO);
+        logger.log("Hello from the abstract interpretation", LoggerVerbosity.INFO);
 
-        /* We can instantiate an existing analyzer or build our own from the builders in {@link IntraProceduralAnalyzer} and {@link InterProceduralAnalyzer} */
-        var analyzer = new AccessPathIntervalInterAnalyzer();
-        analyzer.run(root, debug);
+        /* We can instantiate an existing analyzer or implement a new one in {@link IntraProceduralAnalyzer} and {@link InterProceduralAnalyzer} */
+//        var accessAnalyzer = new AccessPathIntervalInterAnalyzerWrapper();
+//        analyzerManager.registerAnalyzer(accessAnalyzer.getAnalyzer());
+        var analyzer = new IntraProceduralPentagonAnalyzerWrapper().getAnalyzer();
+        analyzerManager.registerAnalyzer(analyzer);
     }
 }
