21ae2403a11a26917b0fc803dd864bf6a20f68b5
Mihailo Markovic <mihailo.markovic@oracle.com>
2025-05-07 15:48:08 +0200

    Changed typeReachable conditions into typeReached.

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/reflect/ReflectionDataBuilder.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/reflect/ReflectionDataBuilder.java
@@ -609,7 +609,7 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
             }
 
             if (declaringClass.isAnnotation()) {
-                processAnnotationField(cnd, reflectField);
+                processAnnotationField(reflectField);
             }
         }
 
@@ -650,12 +650,7 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
         Class<?> annotationClass = method.getDeclaringClass();
         Class<?> proxyClass = Proxy.getProxyClass(annotationClass.getClassLoader(), annotationClass);
         try {
-            /*
-             * build-time condition as it is registered during analysis GR-62516, this should be
-             * deleted
-             */
-            var condition = TypeReachabilityCondition.create(proxyClass, false);
-            register(condition, queriedOnly, proxyClass.getDeclaredMethod(method.getName(), method.getParameterTypes()));
+            register(RegistrationCondition.always(), queriedOnly, proxyClass.getDeclaredMethod(method.getName(), method.getParameterTypes()));
         } catch (NoSuchMethodException e) {
             /*
              * The annotation member is not present in the proxy class so we don't add it.
@@ -664,11 +659,11 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
     }
 
     @SuppressWarnings("deprecation")
-    private void processAnnotationField(RegistrationCondition cnd, Field field) {
+    private void processAnnotationField(Field field) {
         Class<?> annotationClass = field.getDeclaringClass();
         Class<?> proxyClass = Proxy.getProxyClass(annotationClass.getClassLoader(), annotationClass);
         try {
-            register(cnd, false, proxyClass.getDeclaredField(field.getName()));
+            register(RegistrationCondition.always(), false, proxyClass.getDeclaredField(field.getName()));
         } catch (NoSuchFieldException e) {
             /*
              * The annotation member is not present in the proxy class so we don't add it.
@@ -1229,7 +1224,7 @@ public class ReflectionDataBuilder extends ConditionalConfigurationRegistry impl
             if (!SubstitutionReflectivityFilter.shouldExclude(reflectField, metaAccess, universe)) {
                 registerTypesForField(analysisField, reflectField, false);
                 if (analysisField.getDeclaringClass().isAnnotation()) {
-                    processAnnotationField(RegistrationCondition.always(), reflectField);
+                    processAnnotationField(reflectField);
                 }
             }
         }
