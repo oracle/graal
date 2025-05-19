c3efa03ed9112b47b1ef946e5a0c18288f54e8e0
ol-automation_ww <ol-automation_ww@oracle.com>
2025-05-19 15:01:58 +0000

    [GR-64696] Backport to 24.2: Correction of Array.prototype.flat() producing a foreign array.
    PullRequest: js/3496

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "53ceb45d7e5fd2d65252d6403d03f97d19369054",
+                "version": "850509e443d67099a673517bcb07a404769611ca",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "53ceb45d7e5fd2d65252d6403d03f97d19369054",
+                "version": "850509e443d67099a673517bcb07a404769611ca",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
