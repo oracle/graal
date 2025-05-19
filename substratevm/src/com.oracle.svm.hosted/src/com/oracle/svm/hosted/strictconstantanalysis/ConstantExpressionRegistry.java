1ce71251db51902448e4a15687cae35bdf8638dc
Aca-S <aleksandar.stefanovic@oracle.com>
2025-05-19 12:32:29 +0200

    Implement a bytecode-level analysis for compile time constant expressions.

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/strictconstantanalysis/ConstantExpressionRegistry.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2025, 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.svm.hosted.strictconstantanalysis;
+
+import com.oracle.svm.core.util.VMError;
+import com.oracle.svm.util.LogUtils;
+import jdk.graal.compiler.bytecode.Bytecode;
+import jdk.graal.compiler.bytecode.BytecodeProvider;
+import jdk.graal.compiler.bytecode.ResolvedJavaMethodBytecodeProvider;
+import jdk.graal.compiler.java.dataflow.AbstractFrame;
+import jdk.graal.compiler.java.dataflow.DataFlowAnalysisException;
+import jdk.graal.compiler.nodes.graphbuilderconf.IntrinsicContext;
+import jdk.vm.ci.meta.JavaKind;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+import org.graalvm.collections.Pair;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConstantExpressionRegistry {
+
+    private static final Object NULL_MARKER = new Object();
+
+    private final Map<Pair<ResolvedJavaMethod, Integer>, AbstractFrame<ConstantExpressionAnalyzer.Value>> registry;
+
+    public ConstantExpressionRegistry() {
+        registry = new ConcurrentHashMap<>();
+    }
+
+    public void analyzeAndStore(ConstantExpressionAnalyzer analyzer, ResolvedJavaMethod method, IntrinsicContext intrinsicContext) {
+        Bytecode bytecode = getBytecode(method, intrinsicContext);
+        try {
+            Map<Integer, AbstractFrame<ConstantExpressionAnalyzer.Value>> abstractFrames = analyzer.analyze(bytecode);
+            abstractFrames.forEach((key, value) -> registry.put(Pair.create(method, key), value));
+        } catch (DataFlowAnalysisException e) {
+            LogUtils.warning("Constant expression analysis failed for " + method.format("%H.%n(%p)") + ": " + e.getMessage());
+        }
+    }
+
+    private static Bytecode getBytecode(ResolvedJavaMethod method, IntrinsicContext intrinsicContext) {
+        BytecodeProvider bytecodeProvider = intrinsicContext == null
+                        ? ResolvedJavaMethodBytecodeProvider.INSTANCE
+                        : intrinsicContext.getBytecodeProvider();
+        return bytecodeProvider.getBytecode(method);
+    }
+
+    public Optional<Object> getReceiver(ResolvedJavaMethod callerMethod, int bci, ResolvedJavaMethod targetMethod) {
+        VMError.guarantee(targetMethod.hasReceiver(), "Receiver requested for static method");
+        AbstractFrame<ConstantExpressionAnalyzer.Value> frame = registry.get(Pair.create(callerMethod, bci));
+        if (frame == null) {
+            return Optional.empty();
+        }
+        int numOfParameters = targetMethod.getSignature().getParameterCount(false);
+        ConstantExpressionAnalyzer.Value receiver = frame.operandStack().getOperand(numOfParameters);
+        if (receiver instanceof ConstantExpressionAnalyzer.CompileTimeConstant constant) {
+            Object receiverValue = constant.getValue();
+            return Optional.of(receiverValue == null ? NULL_MARKER : receiverValue);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public <T> T getReceiver(ResolvedJavaMethod callerMethod, int bci, ResolvedJavaMethod targetMethod, Class<T> type) {
+        Optional<Object> receiver = getReceiver(callerMethod, bci, targetMethod);
+        if (receiver.isEmpty()) {
+            return null;
+        }
+        Object receiverValue = receiver.get();
+        if (isNull(receiverValue) || !type.isAssignableFrom(receiverValue.getClass())) {
+            return null;
+        }
+        return type.cast(receiver.get());
+    }
+
+    public Optional<Object> getArgument(ResolvedJavaMethod callerMethod, int bci, ResolvedJavaMethod targetMethod, int index) {
+        int numOfParameters = targetMethod.getSignature().getParameterCount(false);
+        VMError.guarantee(index >= 0 && index < numOfParameters, "Argument index out of bounds");
+        AbstractFrame<ConstantExpressionAnalyzer.Value> frame = registry.get(Pair.create(callerMethod, bci));
+        if (frame == null) {
+            return Optional.empty();
+        }
+        ConstantExpressionAnalyzer.Value argument = frame.operandStack().getOperand(numOfParameters - index - 1);
+        if (argument instanceof ConstantExpressionAnalyzer.CompileTimeConstant constant) {
+            Object argumentValue = constant.getValue();
+            if (argumentValue == null) {
+                return Optional.of(NULL_MARKER);
+            } else if (argumentValue instanceof Integer n) {
+                /*
+                 * Since the analyzer doesn't differentiate between boolean, byte, short, char and
+                 * int types, we have to check what the expected type is based on the signature of
+                 * the target method and cast the value appropriately.
+                 */
+                JavaKind parameterKind = targetMethod.getSignature().getParameterKind(index);
+                Object properlyTypedValue = switch (parameterKind) {
+                    case JavaKind.Boolean -> n != 0;
+                    case JavaKind.Byte -> n.byteValue();
+                    case JavaKind.Short -> n.shortValue();
+                    case JavaKind.Char -> (char) ('0' + n);
+                    default -> argumentValue;
+                };
+                return Optional.of(properlyTypedValue);
+            } else {
+                return Optional.of(argumentValue);
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public <T> T getArgument(ResolvedJavaMethod callerMethod, int bci, ResolvedJavaMethod targetMethod, int index, Class<T> type) {
+        Optional<Object> argument = getArgument(callerMethod, bci, targetMethod, index);
+        if (argument.isEmpty()) {
+            return null;
+        }
+        Object argumentValue = argument.get();
+        if (isNull(argumentValue) || !type.isAssignableFrom(argumentValue.getClass())) {
+            return null;
+        }
+        return type.cast(argumentValue);
+    }
+
+    public static boolean isNull(Object object) {
+        return object == NULL_MARKER;
+    }
+}
---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/strictconstantanalysis/StrictConstantAnalysisFeature.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2025, 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.svm.hosted.strictconstantanalysis;
+
+import com.oracle.svm.core.ParsingReason;
+import com.oracle.svm.core.feature.AutomaticallyRegisteredFeature;
+import com.oracle.svm.core.feature.InternalFeature;
+import com.oracle.svm.core.option.HostedOptionKey;
+import com.oracle.svm.hosted.FeatureImpl;
+import com.oracle.svm.hosted.ImageClassLoader;
+import com.oracle.svm.util.ReflectionUtil;
+import jdk.graal.compiler.nodes.graphbuilderconf.GraphBuilderConfiguration;
+import jdk.graal.compiler.options.Option;
+import jdk.graal.compiler.phases.util.Providers;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+import org.graalvm.nativeimage.ImageSingletons;
+import org.graalvm.nativeimage.hosted.RuntimeReflection;
+
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
+
+/**
+ * Feature which enables a graph IR optimization independent analysis of compile-time inferrable
+ * method invocations which require dynamic access and would otherwise require a manual reachability
+ * registration.
+ * <p>
+ * The targeted methods are:
+ * <ul>
+ * <li>{@link java.lang.Class#forName(String)}</li>
+ * <li>{@link java.lang.Class#forName(String, boolean, ClassLoader)}</li>
+ * <li>{@link java.lang.Class#getField(String)}</li>
+ * <li>{@link java.lang.Class#getDeclaredField(String)}</li>
+ * <li>{@link java.lang.Class#getConstructor(Class[])}</li>
+ * <li>{@link java.lang.Class#getDeclaredConstructor(Class[])}</li>
+ * <li>{@link java.lang.Class#getMethod(String, Class[])}</li>
+ * <li>{@link java.lang.Class#getDeclaredMethod(String, Class[])}</li>
+ * <li>{@link java.lang.Class#getFields()}</li>
+ * <li>{@link java.lang.Class#getDeclaredFields()}</li>
+ * <li>{@link java.lang.Class#getConstructors()}</li>
+ * <li>{@link java.lang.Class#getDeclaredConstructors()}</li>
+ * <li>{@link java.lang.Class#getMethods()}</li>
+ * <li>{@link java.lang.Class#getDeclaredMethods()}</li>
+ * <li>{@link java.lang.Class#getClasses()}</li>
+ * <li>{@link java.lang.Class#getDeclaredClasses()}</li>
+ * <li>{@link java.lang.Class#getNestMembers()}</li>
+ * <li>{@link java.lang.Class#getPermittedSubclasses()}</li>
+ * <li>{@link java.lang.Class#getRecordComponents()}</li>
+ * <li>{@link java.lang.Class#getSigners()}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findClass(String)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findVirtual(Class, String, MethodType)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findStatic(Class, String, MethodType)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findConstructor(Class, MethodType)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findGetter(Class, String, Class)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findStaticGetter(Class, String, Class)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findSetter(Class, String, Class)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findStaticSetter(Class, String, Class)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findVarHandle(Class, String, Class)}}</li>
+ * <li>{@link java.lang.invoke.MethodHandles.Lookup#findStaticVarHandle(Class, String, Class)}}</li>
+ * <li>{@link java.lang.Class#getResource(String)}</li>
+ * <li>{@link java.lang.Class#getResourceAsStream(String)}</li>
+ * <li>{@link java.io.ObjectInputFilter.Config#createFilter(String)}</li>
+ * <li>{@link java.lang.reflect.Proxy#newProxyInstance(ClassLoader, Class[], InvocationHandler)}</li>
+ * </ul>
+ */
+@AutomaticallyRegisteredFeature
+public class StrictConstantAnalysisFeature implements InternalFeature {
+
+    public static class Options {
+
+        public enum Mode {
+            Disable,
+            Warn,
+            Enforce
+        }
+
+        @Option(help = """
+                        Select the mode for the strict, build-time analysis for calls requiring dynamic access.
+                        Possible values are:
+                         "Disable" (default): Disable the strict mode and fall back to the optimization dependent analysis for inferrable dynamic calls;
+                         "Warn": Fold both the calls inferred with the strict mode analysis and the optimization dependant analysis, but print a warning for non-strict call folding;
+                         "Enforce": Fold only the calls inferred by the strict analysis mode.""")//
+        public static final HostedOptionKey<Mode> StrictConstantAnalysis = new HostedOptionKey<>(Mode.Disable);
+    }
+
+    private ImageClassLoader loader;
+
+    @Override
+    public boolean isInConfiguration(IsInConfigurationAccess a) {
+        return Options.StrictConstantAnalysis.getValue() != Options.Mode.Disable;
+    }
+
+    @Override
+    public void afterRegistration(AfterRegistrationAccess a) {
+        FeatureImpl.AfterRegistrationAccessImpl access = (FeatureImpl.AfterRegistrationAccessImpl) a;
+        loader = access.getImageClassLoader();
+        ImageSingletons.add(ConstantExpressionRegistry.class, new ConstantExpressionRegistry());
+    }
+
+    @Override
+    public void beforeAnalysis(BeforeAnalysisAccess access) {
+        /*
+         * The strict analysis mode disables constant folding through method inlining, which leads
+         * to <clinit> of sun.nio.ch.DatagramChannelImpl. This is a temporary fix until annotation
+         * guided analysis is implemented.
+         *
+         * An alternative to this approach would be creating invocation plugins for the methods
+         * defined in jdk.internal.invoke.MhUtil.
+         */
+        Class<?> datagramChannelImpl = ReflectionUtil.lookupClass(true, "sun.nio.ch.DatagramChannelImpl");
+        if (datagramChannelImpl != null) {
+            Field socketField = ReflectionUtil.lookupField(datagramChannelImpl, "socket");
+            access.registerReachabilityHandler(a -> RuntimeReflection.register(socketField), datagramChannelImpl);
+        }
+    }
+
+    @Override
+    public void registerInvocationPlugins(Providers providers, GraphBuilderConfiguration.Plugins plugins, ParsingReason reason) {
+        ConstantExpressionRegistry registry = ImageSingletons.lookup(ConstantExpressionRegistry.class);
+        ConstantExpressionAnalyzer analyzer = new ConstantExpressionAnalyzer(providers, loader);
+        plugins.appendMethodParsingPlugin((method, intrinsicContext) -> registry.analyzeAndStore(analyzer, method, intrinsicContext));
+    }
+
+    /**
+     * Utility method which attempts to infer {@code targetMethod} according to the {@code Disable},
+     * {@code Warn} and {@code Enforce} options of {@code StrictConstantAnalysis}.
+     */
+    public static boolean tryToInfer(Predicate<ConstantExpressionRegistry> strictModeRoutine, BooleanSupplier graphModeRoutine, ResolvedJavaMethod targetMethod,
+                    Predicate<ResolvedJavaMethod> strictModeTarget) {
+        Options.Mode analysisMode = Options.StrictConstantAnalysis.getValue();
+        boolean isTarget = strictModeTarget.test(targetMethod);
+        if (analysisMode != Options.Mode.Disable && isTarget) {
+            ConstantExpressionRegistry registry = ImageSingletons.lookup(ConstantExpressionRegistry.class);
+            if (strictModeRoutine.test(registry)) {
+                return true;
+            }
+        }
+        if (analysisMode != Options.Mode.Enforce || !isTarget) {
+            return graphModeRoutine.getAsBoolean();
+        }
+        return false;
+    }
+
+    /**
+     * Utility method which attempts to infer {@code targetMethod} according to the {@code Disable},
+     * {@code Warn} and {@code Enforce} options of {@code StrictConstantAnalysis}.
+     */
+    public static boolean tryToInfer(Predicate<ConstantExpressionRegistry> strictModeRoutine, BooleanSupplier graphModeRoutine) {
+        return tryToInfer(strictModeRoutine, graphModeRoutine, null, (method) -> true);
+    }
+}
