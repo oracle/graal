c585f27c20bb0986ce350c85cccc015e2e4d3458
Andreas Woess <andreas.woess@oracle.com>
2025-06-09 15:26:32 +0200

    Add unsigned integer from/to float conversion ops.
    (cherry picked from commit 9e70aac0b09df6e573abe4c83de05aefc43172a0)

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/spi/LoweringProvider.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/spi/LoweringProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -123,4 +123,12 @@ public interface LoweringProvider {
      * {@link ExtendableMemoryAccess}.
      */
     boolean supportsFoldingExtendIntoAccess(ExtendableMemoryAccess access, MemoryExtendKind extendKind);
+
+    /**
+     * Indicates whether this target platform supports lowering floating point conversions to and
+     * from unsigned integer.
+     */
+    default boolean supportsUnsignedFloatConvert() {
+        return false;
+    }
 }
