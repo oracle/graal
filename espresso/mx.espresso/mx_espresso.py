8978b5fe0927b616144c9442d5b4a8e0ea8a476f
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2025-06-11 18:16:30 +0200

    Add java version in the espresso root directory name

--- espresso/mx.espresso/mx_espresso.py
+++ espresso/mx.espresso/mx_espresso.py
@@ -574,8 +574,8 @@ def mx_register_dynamic_suite_constituents(register_project, register_distributi
     register_espresso_runtime_resources(register_project, register_distribution, _suite)
     register_distribution(DeliverableStandaloneArchive(_suite,
         standalone_dist='ESPRESSO_NATIVE_STANDALONE',
-        community_archive_name="espresso-community",
-        enterprise_archive_name="espresso",
+        community_archive_name=f"espresso-community-java{java_home_dep.major_version}",
+        enterprise_archive_name=f"espresso-java{java_home_dep.major_version}",
         community_dist_name=f'GRAALVM_ESPRESSO_COMMUNITY_JAVA{java_home_dep.major_version}',
         enterprise_dist_name=f'GRAALVM_ESPRESSO_JAVA{java_home_dep.major_version}'))
 
