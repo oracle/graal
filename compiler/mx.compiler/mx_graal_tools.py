157b4b71685cb23de228a97b384ba60df84010c9
Tom Rodriguez <tom.rodriguez@oracle.com>
2025-06-18 12:29:15 -0700

    Parameterize IGV version warning

--- compiler/mx.compiler/mx_graal_tools.py
+++ compiler/mx.compiler/mx_graal_tools.py
@@ -96,10 +96,12 @@ def igv(args):
     help for the NetBeans launcher.
 
     """
-    v17 = mx.VersionSpec("17")
-    v25 = mx.VersionSpec("25")
+    min_version = 17
+    max_version = 24
+    min_version_spec = mx.VersionSpec(str(min_version))
+    next_version_spec = mx.VersionSpec(str(max_version + 1))
     def _igvJdkVersionCheck(version):
-        return v17 <= version < v25
+        return min_version_spec <= version < next_version_spec
 
     jdkhome = None
     if not '--jdkhome' in args:
@@ -107,7 +109,7 @@ def igv(args):
             pass
 
         # try to find a fully supported version first
-        jdk = mx.get_tools_jdk(versionCheck=_igvJdkVersionCheck, versionDescription='IGV prefers JDK 17 through JDK 24', abortCallback=_do_not_abort)
+        jdk = mx.get_tools_jdk(versionCheck=_igvJdkVersionCheck, versionDescription=f'IGV prefers JDK {min_version} through JDK {max_version}', abortCallback=_do_not_abort)
         if jdk is None:
             # try any JDK
             jdk = mx.get_jdk()
@@ -117,7 +119,7 @@ def igv(args):
             mx.log(f'Launching IGV with {jdkhome}')
             if not _igvJdkVersionCheck(jdk.version):
                 mx.warn(f'{jdk.home} is not an officially supported JDK for IGV.')
-                mx.warn('If you experience any problems try to use a JDK 17 or JDK 21 instead.')
+                mx.warn(f'If you experience any problems try to use an LTS release between JDK {min_version} and JDK {max_version} instead.')
                 mx.warn(f'mx help igv provides more details.')
 
     run_netbeans_app('IdealGraphVisualizer', jdkhome, args=args, dist='IDEALGRAPHVISUALIZER_DIST')
