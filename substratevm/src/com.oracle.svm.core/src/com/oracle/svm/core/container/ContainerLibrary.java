89115443da169c42a44342584a79aab0c1801516
jovsteva <jovan.j.stevanovic@oracle.com>
2025-06-23 09:51:12 +0200

    Update BasedOnJDK annotation.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/container/ContainerLibrary.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/container/ContainerLibrary.java
@@ -85,8 +85,8 @@ import com.oracle.svm.core.util.BasedOnJDKFile;
 // The following annotations are for files in `src/svm`, which are completely customized for SVM
 @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+18/src/hotspot/share/logging/log.hpp")
 @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+26/src/hotspot/share/memory/allocation.cpp")
-@BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+26/src/hotspot/share/runtime/globals.hpp")
-@BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+21/src/hotspot/share/utilities/debug.cpp")
+@BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-26+3/src/hotspot/share/runtime/globals.hpp")
+@BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-26+3/src/hotspot/share/utilities/debug.cpp")
 @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+18/src/hotspot/share/utilities/debug.hpp")
 public class ContainerLibrary {
     static final int VERSION = 240100;
