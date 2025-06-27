1177e3d28f43129056a9c4d9ff489045d535aacc
Yudi Zheng <yudi.zheng@oracle.com>
2025-06-27 16:07:05 +0200

    Adapt JDK-8359435: AArch64: add support for SB instruction to MacroAssembler::spin_wait

--- substratevm/src/com.oracle.svm.native.libchelper/src/cpuid.c
+++ substratevm/src/com.oracle.svm.native.libchelper/src/cpuid.c
@@ -690,6 +690,7 @@ void determineCPUFeatures(CPUFeatures* features) {
   features->fLSE =    !!(cpu_has("hw.optional.arm.FEAT_LSE"))    | !!(cpu_has("hw.optional.armv8_1_atomics"));
   features->fSHA512 = !!(cpu_has("hw.optional.arm.FEAT_SHA512")) | !!(cpu_has("hw.optional.armv8_2_sha512"));
   features->fSHA3 =   !!(cpu_has("hw.optional.arm.FEAT_SHA3"))   | !!(cpu_has("hw.optional.armv8_2_sha3"));
+  features->fSB =     !!(cpu_has("hw.optional.arm.FEAT_SB"));
   // Not (yet) checked in the Hotspot code.
   features->fDCPOP = 0;
   features->fSVE = 0;
@@ -751,6 +752,9 @@ void determineCPUFeatures(CPUFeatures* features) {
 #ifndef HWCAP_SVE
 #define HWCAP_SVE           (1L << 22)
 #endif
+#ifndef HWCAP_SB
+#define HWCAP_SB            (1L << 29)
+#endif
 #ifndef HWCAP_PACA
 #define HWCAP_PACA          (1L << 30)
 #endif
@@ -795,6 +799,7 @@ void determineCPUFeatures(CPUFeatures* features) {
   features->fSTXR_PREFETCH = 0;
   features->fA53MAC = 0;
   features->fDMB_ATOMICS = 0;
+  features->fSB = !!(auxv & HWCAP_SB);
   features->fPACA = !!(auxv & HWCAP_PACA);
   features->fSVEBITPERM = !!(auxv2 & HWCAP2_SVEBITPERM);
   features->fFPHP = !!(auxv & HWCAP_FPHP);
