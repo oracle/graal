8ac11cea4c618c9dc5822e1b4908cba7963c6063
Tomas Zezula <tomas.zezula@oracle.com>
2025-07-02 11:48:55 +0200

    Backport to 25.0: Update versions in Truffle version check.

--- truffle/src/com.oracle.truffle.runtime/src/com/oracle/truffle/runtime/OptimizedTruffleRuntime.java
+++ truffle/src/com.oracle.truffle.runtime/src/com/oracle/truffle/runtime/OptimizedTruffleRuntime.java
@@ -169,8 +169,8 @@ public abstract class OptimizedTruffleRuntime implements TruffleRuntime, Truffle
     private static final int JAVA_SPECIFICATION_VERSION = Runtime.version().feature();
     public static final Version MIN_COMPILER_VERSION = Version.create(23, 1, 2);
     public static final int MIN_JDK_VERSION = 21;
-    public static final int MAX_JDK_VERSION = 29;
-    public static final Version NEXT_VERSION_UPDATE = Version.create(29, 1);
+    public static final int MAX_JDK_VERSION = 26;
+    public static final Version NEXT_VERSION_UPDATE = Version.create(26, 0);
 
     /**
      * Used only to reset state for native image compilation.
