667f092280cd9ffa993b1a3ab9d6df866ca3885a
Josef Eisl <josef.eisl@oracle.com>
2025-07-16 14:33:12 +0200

    svm: review ContainerLibrary changes

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/container/ContainerLibrary.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/container/ContainerLibrary.java
@@ -85,7 +85,7 @@ import com.oracle.svm.core.util.BasedOnJDKFile;
 // The following annotations are for files in `src/svm`, which are completely customized for SVM
 @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+18/src/hotspot/share/logging/log.hpp")
 @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+26/src/hotspot/share/memory/allocation.cpp")
-@BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+26/src/hotspot/share/runtime/globals.hpp")
+@BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+30/src/hotspot/share/runtime/globals.hpp")
 @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+21/src/hotspot/share/utilities/debug.cpp")
 @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25+18/src/hotspot/share/utilities/debug.hpp")
 public class ContainerLibrary {
