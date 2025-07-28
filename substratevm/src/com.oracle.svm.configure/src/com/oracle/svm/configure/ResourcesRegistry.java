76e848776044192e282fc6c61f6482e5a0798068
David Nestorovic <david.nestorovic@oracle.com>
2025-07-28 10:32:32 +0200

    Print warning if provided pattern is invalid

--- substratevm/src/com.oracle.svm.configure/src/com/oracle/svm/configure/ResourcesRegistry.java
+++ substratevm/src/com.oracle.svm.configure/src/com/oracle/svm/configure/ResourcesRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -51,7 +51,7 @@ public interface ResourcesRegistry<C> extends RuntimeResourceSupport<C> {
     }
 
     @Override
-    void ignoreResources(C condition, String pattern);
+    void ignoreResources(C condition, String pattern, Object origin);
 
     @Override
     void addResourceBundles(C condition, String name);
