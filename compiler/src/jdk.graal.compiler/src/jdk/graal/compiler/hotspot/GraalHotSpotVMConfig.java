624d1a7b8b6a73bf02ce2f661049fccab9e45bd9
Yudi Zheng <yudi.zheng@oracle.com>
2025-08-04 19:53:44 +0200

    Adapt JDK-8343218: Add option to disable allocating interface and abstract classes in non-class metaspace
    (cherry picked from commit feac5dc3ce9ca67cff24956c7f61c79113565426)
    (cherry picked from commit 8db55ae7872a550a8eec875054845030e3878bb8)

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/GraalHotSpotVMConfig.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/GraalHotSpotVMConfig.java
@@ -205,6 +205,9 @@ public class GraalHotSpotVMConfig extends GraalHotSpotVMConfigAccess {
     // Compressed Oops related values.
     public final boolean useCompressedOops = getFlag("UseCompressedOops", Boolean.class);
     public final boolean useCompressedClassPointers = getFlag("UseCompressedClassPointers", Boolean.class);
+
+    public final boolean useClassMetaspaceForAllClasses = getFlag("UseClassMetaspaceForAllClasses", Boolean.class);
+
     // JDK-8305895 allows storing the compressed class pointer in the upper 22 bits of the mark
     // word. This runtime optimization is guarded by the flag UseCompactObjectHeaders. It depends
     // on compressed class pointers, meaning that if useCompactObjectHeaders is true,
