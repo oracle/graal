a41d1ecdecd120a357a02b6cf8fb77c1ba58ff2b
Yudi Zheng <yudi.zheng@oracle.com>
2025-08-13 15:01:41 +0200

    Adapt JDK-8364141: Remove LockingMode related code from x86

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/replacements/MonitorSnippets.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/replacements/MonitorSnippets.java
@@ -197,10 +197,8 @@ import jdk.vm.ci.meta.SpeculationLog;
  * appropriately to comply with the layouts above.
  */
 // @formatter:off
-@SyncPort(from = "https://github.com/openjdk/jdk/blob/c2d76f9844aadf77a0b213a9169a7c5c8c8f1ffb/src/hotspot/cpu/x86/c2_MacroAssembler_x86.cpp#L457-L617",
-          sha1 = "2d66e0ccf8dbf69f575be2633d5a17f77a20131d")
-@SyncPort(from = "https://github.com/openjdk/jdk/blob/c2d76f9844aadf77a0b213a9169a7c5c8c8f1ffb/src/hotspot/cpu/x86/c2_MacroAssembler_x86.cpp#L619-L780",
-          sha1 = "e88d7b8c4bb85358c6a810ee1d7d92fde5db42e6")
+@SyncPort(from = "https://github.com/openjdk/jdk/blob/c2d76f9844aadf77a0b213a9169a7c5c8c8f1ffb/src/hotspot/cpu/x86/c2_MacroAssembler_x86.cpp#L149-L578",
+          sha1 = "ee04c86e8b12a9f8331c5e79f95a17b27615c845")
 // @formatter:on
 public class MonitorSnippets implements Snippets {
 
