f943756a7bd56044d68fd1e17922924a151c5fb3
Christian Haeubl <christian.haeubl@oracle.com>
2025-08-18 14:52:15 +0200

    Remove the default availability for @UnknownObjectField and @UnknownPrimitiveField.

--- substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/meta/SubstrateField.java
+++ substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/meta/SubstrateField.java
@@ -34,6 +34,7 @@ import org.graalvm.nativeimage.Platforms;
 
 import com.oracle.graal.pointsto.infrastructure.OriginalFieldProvider;
 import com.oracle.graal.pointsto.meta.AnalysisField;
+import com.oracle.svm.core.BuildPhaseProvider.AfterAnalysis;
 import com.oracle.svm.core.BuildPhaseProvider.AfterCompilation;
 import com.oracle.svm.core.heap.UnknownObjectField;
 import com.oracle.svm.core.heap.UnknownPrimitiveField;
@@ -52,8 +53,8 @@ public class SubstrateField implements SharedField {
 
     protected static final SubstrateField[] EMPTY_ARRAY = new SubstrateField[0];
 
-    @UnknownObjectField SubstrateType type;
-    @UnknownObjectField SubstrateType declaringClass;
+    @UnknownObjectField(availability = AfterAnalysis.class) SubstrateType type;
+    @UnknownObjectField(availability = AfterAnalysis.class) SubstrateType declaringClass;
     private final String name;
     private final int modifiers;
     private int hashCode;
