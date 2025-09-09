d985f57ea3583991b8823e0f26a6273a2838efa8
Benoit Daloze <benoit.daloze@oracle.com>
2025-09-09 15:58:20 +0200

    Cap Native Image at 31GiB and not 32GB (32 * 10^9)
    * Consistent with always reporting memory in GiB.

--- substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/MemoryUtil.java
+++ substratevm/src/com.oracle.svm.driver/src/com/oracle/svm/driver/MemoryUtil.java
@@ -63,10 +63,13 @@ public final class MemoryUtil {
     private static final int MIN_AVAILABLE_MEMORY_THRESHOLD_GB = 8;
 
     /*
-     * Builder uses at most 32GB to avoid disabling compressed oops (UseCompressedOops).
-     * Deliberately use GB (not GiB) to stay well below 32GiB when relative maximum is calculated.
+     * Builder uses at most 31GiB to avoid disabling compressed oops (UseCompressedOops).
+     * UseCompressedOops seems currently disabled on my machine at values > (32 GiB - 32 MiB), so we
+     * use 31 GiB to have some margin. The actual logic at
+     * https://github.com/openjdk/jdk/blob/jdk-26+10/src/hotspot/share/runtime/arguments.cpp#L1429
+     * depends on various factors so it seems unwise to hardcode that exact limit.
      */
-    public static final long MAX_HEAP_BYTES = 32_000_000_000L;
+    public static final long MAX_HEAP_BYTES = 31 * GiB_TO_BYTES;
 
     public static List<String> heuristicMemoryFlags(HostFlags hostFlags, List<String> memoryFlags) {
         /*
@@ -146,7 +149,7 @@ public final class MemoryUtil {
         // Ensure max memory size does not exceed upper limit
         if (maxMemory > MAX_HEAP_BYTES) {
             maxMemory = MAX_HEAP_BYTES;
-            reason = percentageOfSystemMemoryText(maxMemory, totalMemorySize) + ", capped at 32GB";
+            reason = percentageOfSystemMemoryText(maxMemory, totalMemorySize) + ", capped at 31GiB";
         }
 
         // Handle memory flags
