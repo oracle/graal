4390ce9c6e5ea322dc1d1e820e2994f9173b8efc
Josef Eisl <josef.eisl@oracle.com>
2025-09-17 14:30:03 +0200

    Reapply "[GR-68037] Use encoded snippets in jargraal."
    This reverts commit a8c8c7401923367cbf29bb8c0886911bf198c829.

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotBackendFactory.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/HotSpotBackendFactory.java
@@ -24,9 +24,7 @@
  */
 package jdk.graal.compiler.hotspot;
 
-import static jdk.graal.compiler.core.common.NativeImageSupport.inBuildtimeCode;
 import static jdk.graal.compiler.core.common.NativeImageSupport.inRuntimeCode;
-import static jdk.graal.compiler.hotspot.EncodedSnippets.isUsingEncodedSnippets;
 import static jdk.vm.ci.common.InitTimer.timer;
 
 import jdk.graal.compiler.bytecode.BytecodeProvider;
@@ -178,9 +176,7 @@ public abstract class HotSpotBackendFactory implements ArchitectureSpecific {
         HotSpotConstantReflectionProvider constantReflection = createConstantReflectionProvider(jvmci);
         afterJVMCIProvidersCreated();
         TargetDescription target = codeCache.getTarget();
-        if (inBuildtimeCode() || isUsingEncodedSnippets()) {
-            SnippetSignature.initPrimitiveKindCache(metaAccess);
-        }
+        SnippetSignature.initPrimitiveKindCache(metaAccess);
         ConstantFieldProvider constantFieldProvider = new HotSpotGraalConstantFieldProvider(config, metaAccess);
         HotSpotProviders providers;
         HotSpotReplacementsImpl replacements;
