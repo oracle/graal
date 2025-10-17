a3ac7e57c728701caf21935ba0f3324a172a5eb9
Samuel Malec <malec293@gmail.com>
2025-10-18 00:09:12 +0200

    refactor: rename inflation variable to bb and update related references

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/AbstractInterpretationDriver.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/ai/AbstractInterpretationDriver.java
@@ -22,15 +22,15 @@ public class AbstractInterpretationDriver {
     private final DebugContext debug;
     private final AnalyzerManager analyzerManager;
     private final AbstractInterpretationEngine engine;
-    private final Inflation inflation;
+    private final Inflation bb;
     private final AnalysisMethod root;
 
-    public AbstractInterpretationDriver(DebugContext debug, AnalysisMethod root, Inflation inflation) {
+    public AbstractInterpretationDriver(DebugContext debug, AnalysisMethod root, Inflation bb) {
         this.root = root;
-        this.inflation = inflation;
+        this.bb = bb;
         this.debug = debug;
         this.analyzerManager = new AnalyzerManager();
-        this.engine = new AbstractInterpretationEngine(analyzerManager, inflation);
+        this.engine = new AbstractInterpretationEngine(analyzerManager, bb.getUniverse());
     }
 
     /* To see the output of the abstract interpretation, run with -H:Log=AbstractInterpretation */
@@ -40,7 +40,7 @@ public class AbstractInterpretationDriver {
             /* Creating a new scope for logging, run with -H:Log=AbstractInterpretation to activate it */
             try (var scope = debug.scope("AbstractInterpretation")) {
                 setupFramework();
-                engine.execute();
+                engine.execute(root, bb);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
@@ -57,18 +57,15 @@ public class AbstractInterpretationDriver {
      * @throws IOException in case of I/O errors during logger initialization.
      */
     private void setupFramework() throws IOException {
-         /** We can creat the {@link AbstractInterpretationLogger} instance here. */
         AbstractInterpretationLogger logger = AbstractInterpretationLogger.getInstance(debug, "myLogger", LoggerVerbosity.INFO);
-        logger.log("Hello from the abstract interpretation", LoggerVerbosity.INFO);
+        logger.log("Example of logging", LoggerVerbosity.INFO);
 
-        /* We can instantiate an existing analyzer or implement a new one in {@link IntraProceduralAnalyzer} and {@link InterProceduralAnalyzer}
+        /**
+         * We can instantiate an existing analyzer or implement a new one in {@link IntraProceduralAnalyzer} and {@link InterProceduralAnalyzer}
          * If we wish to use analyzers during the native image build, we must register them here.
          * To get started quickly, we can use the sample analyzer wrappers provided in {@link com.oracle.svm.hosted.analysis.ai.example}.
-         * */
+         */
         var analyzer = new LeaksIdSetIntraAnalyzerWrapper().getAnalyzer();
         analyzerManager.registerAnalyzer(analyzer);
-
-        /* We can set what methods we want to analyze by engine.setAnalyzeMainOnly(); */
-        engine.setAnalyzeMainOnly(true);
     }
 }
