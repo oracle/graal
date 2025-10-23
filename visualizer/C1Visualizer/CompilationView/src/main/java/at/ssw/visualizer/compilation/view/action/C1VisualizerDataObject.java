255cf1cacc78e4a3d047460f7ba07787667b94d5
Tom Rodriguez <tom.rodriguez@oracle.com>
2025-10-23 10:05:42 -0700

    Support opening CFG files in existing instance

--- visualizer/C1Visualizer/CompilationView/src/main/java/at/ssw/visualizer/compilation/view/action/C1VisualizerDataObject.java
+++ visualizer/C1Visualizer/CompilationView/src/main/java/at/ssw/visualizer/compilation/view/action/C1VisualizerDataObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,9 +20,12 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package org.graalvm.visualizer.coordinator.impl;
+package at.ssw.visualizer.compilation.view.action;
 
+import at.ssw.visualizer.model.CompilationModel;
 import org.netbeans.api.actions.Openable;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.cookies.OpenCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
@@ -31,48 +34,38 @@ import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.MultiFileLoader;
-import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.logging.Logger;
 
 /**
- * DataObject that represents a .BGV file. This object is NOT designed for presentation
+ * DataObject that represents a .CFG file. This object is NOT designed for presentation
  * as is usual for NB dataobjects. It just serves to provide Open file from commandline
  * feature, nothing more.
- * <p/>
- * Before the object can be shown in the UI, the Coordinator cookies have to be refactored,
- * since Open on BinaryGraphDataObject means actually "import" its data to the current workspace
- * (coordinator's Outline tree).
- * <p/>
- * Warning: Duplicate opens will duplicate items in the Coordinator's view.
- *
- * @author sdedic
  */
 @NbBundle.Messages(
-        "LBL_GraalVMGraphBinaryFile=GraalVM Graph Binary Dump"
+        "LBL_GraalVMC1VisualizerFile=GraalVM C1Visualizer File"
 )
 @MIMEResolver.ExtensionRegistration(
-        displayName = "#LBL_GraalVMGraphBinaryFile",
-        mimeType = BinaryGraphDataObject.GRAAL_DUMP_MIME_TYPE,
+        displayName = "#LBL_GraalVMC1VisualizerFile",
+        mimeType = C1VisualizerDataObject.GRAAL_CFG_MIME_TYPE,
         position = 3232,
-        extension = {"bgv"}
+        extension = {"cfg"}
 )
 @DataObject.Registration(
-        displayName = "LBL_GraalVMGraphBinaryFile",
-        mimeType = BinaryGraphDataObject.GRAAL_DUMP_MIME_TYPE,
+        displayName = "LBL_GraalVMC1VisualizerFile",
+        mimeType = C1VisualizerDataObject.GRAAL_CFG_MIME_TYPE,
         position = 400
 )
-public class BinaryGraphDataObject extends MultiDataObject {
-    private static final Logger LOG = Logger.getLogger(BinaryGraphDataObject.class.getName());
+public class C1VisualizerDataObject extends MultiDataObject {
     /**
-     * MIME type for BGV dumps
+     * MIME type for CFG dumps
      */
-    public static final String GRAAL_DUMP_MIME_TYPE = "application/x-graalvm-graph-binary"; // NOI18N
+    public static final String GRAAL_CFG_MIME_TYPE = "application/x-c1visualizer"; // NOI18N
 
-    public BinaryGraphDataObject(FileObject fo, MultiFileLoader loader) throws DataObjectExistsException {
+    public C1VisualizerDataObject(FileObject fo, MultiFileLoader loader) throws DataObjectExistsException {
         super(fo, loader);
         getCookieSet().assign(Openable.class, new OpenImpl());
     }
@@ -84,23 +77,24 @@ public class BinaryGraphDataObject extends MultiDataObject {
         }
     }
 
-    @NbBundle.Messages({
-            "# {0} - file name",
-            "# {1} - error message",
-            "ERR_OpeningFile=Error importing from file {0}: {1}",
-    })
     public void importPrimaryFile() {
         FileObject fo = getPrimaryFile();
         File f = FileUtil.toFile(fo);
         if (f == null) {
             return;
         }
-        try {
-            FileImporter.asyncImportDocument(f.toPath(), true, true, null);
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(
-                    Exceptions.attachLocalizedMessage(ex, Bundle.ERR_OpeningFile(f.toPath(), ex.toString()))
-            );
-        }
+
+        final String fileName = f.getAbsolutePath();
+        RequestProcessor.getDefault().post(new Runnable() {
+            public void run() {
+                CompilationModel model = Lookup.getDefault().lookup(CompilationModel.class);
+                String errorMsg = model.parseInputFile(fileName);
+                if (errorMsg != null) {
+                    NotifyDescriptor d = new NotifyDescriptor.Message("Errors while parsing input:\n" + errorMsg, NotifyDescriptor.ERROR_MESSAGE);
+                    DialogDisplayer.getDefault().notify(d);
+                }
+            }
+        });
     }
 }
+
