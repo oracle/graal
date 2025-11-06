4c6a495c666dfa34cbb3eccdda251b4389e45c58
David Kozak <david.kozak@oracle.com>
2025-11-06 12:38:35 +0100

    Use StandaloneGraphBuilderPhase outside of standalone analysis

--- substratevm/mx.substratevm/suite.py
+++ substratevm/mx.substratevm/suite.py
@@ -2421,6 +2421,7 @@ suite = {
                 "name" : "org.graalvm.nativeimage.pointsto.standalone",
                 "exports" : [
                     "com.oracle.graal.pointsto.standalone",
+                    "com.oracle.graal.pointsto.standalone.plugins",
                 ],
                 "requires": [
                     "java.management",
