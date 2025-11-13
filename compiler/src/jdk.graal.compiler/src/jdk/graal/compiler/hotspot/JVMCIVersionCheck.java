3b64a8202c447113ec54434481a5b37e49826d13
Doug Simon <doug.simon@oracle.com>
2025-11-13 23:49:36 +0100

    update JVMCI to 25.0.1+8-jvmci-25.1-b10

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/JVMCIVersionCheck.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/JVMCIVersionCheck.java
@@ -58,8 +58,8 @@ public final class JVMCIVersionCheck {
     // Checkstyle: stop stable iteration order check
     private static final Map<String, Map<String, Version>> JVMCI_MIN_VERSIONS = Map.of(
                     "25", Map.of(
-                                    "Oracle Corporation", createLabsJDKVersion("25.0.1+8", "25.1", 9),
-                                    DEFAULT_VENDOR_ENTRY, createLabsJDKVersion("25.0.1+8", "25.1", 9)));
+                                    "Oracle Corporation", createLabsJDKVersion("25.0.1+8", "25.1", 10),
+                                    DEFAULT_VENDOR_ENTRY, createLabsJDKVersion("25.0.1+8", "25.1", 10)));
     // Checkstyle: resume stable iteration order check
 
     private static final int NA = 0;
