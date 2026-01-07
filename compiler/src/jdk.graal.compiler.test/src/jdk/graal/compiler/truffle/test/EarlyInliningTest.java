cffb110fb1a38a3682618483143a71af06c6cc85
Christian Humer <christian.humer@oracle.com>
2026-01-07 16:58:43 +0100

    Improve EarlyInliningTest to capture loop detection.

--- compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/truffle/test/EarlyInliningTest.java
+++ compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/truffle/test/EarlyInliningTest.java
@@ -31,39 +31,74 @@ import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.EarlyEscapeAnalysis;
 import com.oracle.truffle.api.CompilerDirectives.EarlyInline;
+import com.oracle.truffle.api.CompilerDirectives.ValueType;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
 import com.oracle.truffle.api.nodes.ExplodeLoop.LoopExplosionKind;
 
 public class EarlyInliningTest extends PartialEvaluationTest {
 
-    @Test
-    public void testExplodeLoopBasic() {
-        assertPartialEvalEquals((v) -> v ? 42 : 41, (v) -> testExplodeLoopBasic(v), Boolean.TRUE);
+    public static int expected(int arg) {
+        int v = 0;
+        while (v < arg) {
+            v++;
+        }
+        return v;
     }
 
-    // 0: nop;
-    // 1: branchTrue 5
-    // 3: return 41
-    // 4: return 42
-    @CompilationFinal(dimensions = 1) private final byte[] bytecodes = new byte[]{0, 1, 4, 2, 3};
+    @Test
+    public void testExplodeLoopBasic() {
+        assertPartialEvalEquals(EarlyInliningTest::expected, (a) -> explodeBasic(a), 42);
+    }
+
+    static final int SET = 1;
+    static final int BRANCH_LT = 2;
+    static final int BRANCH_BACKWARD = 3;
+    static final int INCREMENT = 4;
+    static final int RETURN = 5;
+
+    @CompilationFinal(dimensions = 1) private final int[] bytecodes = new int[]{
+                    // 0
+                    SET, 0,
+                    // 2
+                    BRANCH_LT, 7,
+                    // 4
+                    INCREMENT,
+                    // 5
+                    BRANCH_BACKWARD,
+                    2,
+                    // 7
+                    RETURN};
+
+    static final class Stack {
+
+        int accumulator;
+
+    }
 
     @ExplodeLoop(kind = LoopExplosionKind.MERGE_EXPLODE)
-    private int testExplodeLoopBasic(Boolean v) {
-        byte[] bc = this.bytecodes;
+    private int explodeBasic(int arg) {
+        int[] bc = this.bytecodes;
         int bci = 0;
+        Frame frame = new Frame();
         while (true) {
             CompilerAsserts.partialEvaluationConstant(bci);
             switch (bc[bci]) {
-                case 0:
-                    bci++;
+                case SET:
+                    frame.accumulator = bc[bci + 1];
+                    bci += 2;
                     break;
-                case 1:
-                    bci = branchTrue1(bc, bci, v);
+                case RETURN:
+                    return frame.accumulator;
+                case BRANCH_LT:
+                    bci = branchLT(bc, bci, frame, arg);
                     break;
-                case 2:
-                    return 41;
-                case 3:
-                    return 42;
+                case INCREMENT:
+                    frame.accumulator++;
+                    bci = bci + 1;
+                    break;
+                case BRANCH_BACKWARD:
+                    bci = branchBackward(bc, bci);
+                    break;
                 default:
                     throw CompilerDirectives.shouldNotReachHere();
             }
@@ -71,55 +106,79 @@ public class EarlyInliningTest extends PartialEvaluationTest {
     }
 
     @EarlyInline
-    private static int branchTrue1(byte[] bc, int bci, Boolean v) {
-        if (v) {
-            return bc[bci + 1];
-        } else {
+    private static int branchLT(int[] bc, int bci, Frame frame, int a) {
+        if (frame.accumulator < a) {
             return bci + 2;
+        } else {
+            return bc[bci + 1];
         }
     }
 
+    @EarlyInline
+    private static int branchBackward(int[] bc, int bci) {
+        return bc[bci + 1];
+    }
+
+    @ValueType
     static class BytecodeIndex {
         int bci;
+
+        @EarlyInline
+        BytecodeIndex() {
+        }
+    }
+
+    static class Frame {
+        int accumulator;
     }
 
     @Test
     public void testExplodeLoopEscapeAnalysis() {
-        assertPartialEvalEquals((v) -> v ? 42 : 41, (v) -> testExplodeLoopEscapeAnalysis(v), Boolean.TRUE);
+        assertPartialEvalEquals(EarlyInliningTest::expected, (a) -> explodeEscapeAnalysis(a, new Frame()), 42);
     }
 
-    @SuppressWarnings("static-method")
     @ExplodeLoop(kind = LoopExplosionKind.MERGE_EXPLODE)
     @EarlyEscapeAnalysis
-    private int testExplodeLoopEscapeAnalysis(Boolean v) {
-        byte[] bc = this.bytecodes;
+    private int explodeEscapeAnalysis(int arg, Frame frame) {
+        int[] bc = this.bytecodes;
         BytecodeIndex index = new BytecodeIndex();
         while (true) {
             CompilerAsserts.partialEvaluationConstant(index.bci);
             switch (bc[index.bci]) {
-                case 0:
-                    index.bci++;
+                case SET:
+                    frame.accumulator = bc[index.bci + 1];
+                    index.bci = index.bci + 2;
                     break;
-                case 1:
-                    branchTrue2(bc, index, v);
+                case RETURN:
+                    return frame.accumulator;
+                case BRANCH_LT:
+                    index.bci = branchLT2(bc, index, frame, arg);
                     break;
-                case 2:
-                    return 41;
-                case 3:
-                    return 42;
-                case 4:
+                case INCREMENT:
+                    frame.accumulator++;
+                    index.bci = index.bci + 1;
+                    break;
+                case BRANCH_BACKWARD:
+                    index.bci = branchBackward2(bc, index);
+                    break;
+                default:
                     throw CompilerDirectives.shouldNotReachHere();
             }
         }
     }
 
     @EarlyInline
-    private static void branchTrue2(byte[] bc, BytecodeIndex index, Boolean v) {
-        if (v) {
-            index.bci = bc[index.bci + 1];
+    private static int branchLT2(int[] bc, BytecodeIndex index, Frame frame, int arg) {
+        if (frame.accumulator < arg) {
+            return index.bci + 2;
         } else {
-            index.bci = index.bci + 2;
+            return bc[index.bci + 1];
         }
+    }
+
+    @EarlyInline
+    private static int branchBackward2(int[] bc, BytecodeIndex index) {
+        return bc[index.bci + 1];
     }
 
 }
