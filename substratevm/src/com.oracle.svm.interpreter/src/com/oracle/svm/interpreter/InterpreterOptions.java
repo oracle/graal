406a991ab174b1e8dd849339f6bd9b74127e1fb4
Bernhard Urban-Forster <bernhard.urban-forster@oracle.com>
2026-01-27 11:09:50 +0100

    [GR-54939] Flip default for InterpreterTraceSupport

--- substratevm/src/com.oracle.svm.interpreter/src/com/oracle/svm/interpreter/InterpreterOptions.java
+++ substratevm/src/com.oracle.svm.interpreter/src/com/oracle/svm/interpreter/InterpreterOptions.java
@@ -25,10 +25,16 @@
 package com.oracle.svm.interpreter;
 
 import org.graalvm.collections.EconomicMap;
+import org.graalvm.nativeimage.Platform.HOSTED_ONLY;
+import org.graalvm.nativeimage.Platforms;
 
 import com.oracle.svm.core.hub.RuntimeClassLoading;
 import com.oracle.svm.core.option.HostedOptionKey;
 import com.oracle.svm.core.option.RuntimeOptionKey;
+import com.oracle.svm.core.option.RuntimeOptionValidationSupport;
+import com.oracle.svm.core.option.RuntimeOptionValidationSupport.RuntimeOptionValidation;
+import com.oracle.svm.core.option.SubstrateOptionsParser;
+import com.oracle.svm.core.util.UserError;
 import com.oracle.svm.hosted.pltgot.PLTGOTOptions;
 
 import jdk.graal.compiler.options.Option;
@@ -53,17 +59,42 @@ public class InterpreterOptions {
     @Option(help = "Path to dump interpreter universe metadata as .class files", type = OptionType.Expert)//
     public static final HostedOptionKey<String> InterpreterDumpClassFiles = new HostedOptionKey<>("");
 
-    // GR-54939: Switch default to false, as this has roughly a 2x perf impact on interpreter
-    // performance.
-    @Option(help = "Include interpreter tracing code in image") public static final HostedOptionKey<Boolean> InterpreterTraceSupport = new HostedOptionKey<>(true);
+    // Defaults to false as this has roughly a 2x impact on interpreter dispatch performance.
+    @Option(help = "Include interpreter tracing code in image")//
+    public static final HostedOptionKey<Boolean> InterpreterTraceSupport = new HostedOptionKey<>(false);
 
-    @Option(help = "Trace Interpreter execution")//
-    public static final RuntimeOptionKey<Boolean> InterpreterTrace = new RuntimeOptionKey<>(false);
+    @Option(help = "Trace interpreter execution")//
+    public static final RuntimeOptionKey<Boolean> InterpreterTrace = new RuntimeOptionKey<>(false, InterpreterOptions::validateTraceFlagBuildtime);
+
+    @Option(help = "Enables backdoors in interpreter for testing", type = OptionType.Debug)//
+    public static final HostedOptionKey<Boolean> InterpreterBackdoor = new HostedOptionKey<>(false);
 
     public static boolean interpreterEnabled() {
         return DebuggerWithInterpreter.getValue() || RuntimeClassLoading.isSupported();
     }
 
-    @Option(help = "Enables backdoors in interpreter for testing", type = OptionType.Debug)//
-    public static final HostedOptionKey<Boolean> InterpreterBackdoor = new HostedOptionKey<>(false);
+    @Platforms(HOSTED_ONLY.class)
+    public static void registerInterpreterTraceOptionValidation() {
+        RuntimeOptionValidationSupport.singleton().register(new RuntimeOptionValidation<>(InterpreterOptions::validateTraceFlagRuntime, InterpreterTrace));
+    }
+
+    @Platforms(HOSTED_ONLY.class)
+    private static void validateTraceFlagBuildtime(RuntimeOptionKey<Boolean> optionKey) {
+        if (optionKey.getValue() && !InterpreterTraceSupport.getValue()) {
+            throw UserError.abort("Option '%s' requires trace support to be enabled via '%s'.", optionKey.getName(), Holder.TRACE_SUPPORT_OPTION);
+        }
+    }
+
+    private static void validateTraceFlagRuntime(RuntimeOptionKey<Boolean> optionKey) {
+        if (InterpreterTraceSupport.getValue()) {
+            return;
+        }
+        if (optionKey.getValue()) {
+            throw new IllegalArgumentException("Option '" + optionKey.getName() + "' requires '" + Holder.TRACE_SUPPORT_OPTION + "' to be enabled at build-time.");
+        }
+    }
+
+    private static final class Holder {
+        static final String TRACE_SUPPORT_OPTION = SubstrateOptionsParser.commandArgument(InterpreterTraceSupport, "+");
+    }
 }
