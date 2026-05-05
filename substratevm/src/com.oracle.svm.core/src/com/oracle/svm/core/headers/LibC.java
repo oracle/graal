ffb2198a7e4d92d23f9163d64890d9fc04013cfe
Christian Wirth <christian.wirth@oracle.com>
2026-05-05 20:10:14 +0200

    actually call LibC.abort

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/headers/LibC.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/headers/LibC.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -86,12 +86,7 @@ public class LibC {
 
     @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     public static void abort() {
-        /*
-         * Using the abort system call has unexpected performance implications on Oracle Enterprise
-         * Linux: Storing the crash dump information takes minutes even for tiny images. Therefore,
-         * we just exit with an otherwise unused exit code.
-         */
-        exit(EXIT_CODE_ABORT);
+        libc().abort();
     }
 
     /**
