566c400f22cfd6b055617de16a56212df80940ad
Josef Eisl <josef.eisl@oracle.com>
2026-06-03 08:26:24 +0200

    Refresh JDK source annotations for LabsJDK CE

--- substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/jvmstat/PosixPerfMemoryProvider.java
+++ substratevm/src/com.oracle.svm.core.posix/src/com/oracle/svm/core/posix/jvmstat/PosixPerfMemoryProvider.java
@@ -234,7 +234,7 @@ class PosixPerfMemoryProvider implements PerfMemoryProvider {
         }
     }
 
-    @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-24+13/src/hotspot/os/posix/perfMemory_posix.cpp#L853-L967")
+    @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-25.0.3-ga/src/hotspot/os/posix/perfMemory_posix.cpp#L852-L966")
     private static int createSharedMemFile(CCharPointer directoryPath, CCharPointer filename, int size) {
         if (!makeUserTmpDir(directoryPath)) {
             return -1;
