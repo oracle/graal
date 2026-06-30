9902a4d1a8d1af98540ad0919aabea3846951bfd
Jirka Marsik <jiri.marsik@oracle.com>
2026-06-30 18:06:48 +0200

    Fix Vector API guard hoisting condition insertion

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/vector/replacements/vectorapi/VectorAPIExpansionPhase.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/vector/replacements/vectorapi/VectorAPIExpansionPhase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1188,7 +1188,7 @@ public class VectorAPIExpansionPhase extends PostRunCanonicalizationPhase<HighTi
                                      */
                                     continue;
                                 }
-                                newCondition = graph.addOrUnique(newCondition);
+                                newCondition = graph.addOrUniqueWithInputs(newCondition);
                                 FixedGuardNode newGuard = graph.add(new FixedGuardNode(newCondition, guard.getReason(), guard.getAction(), hoistingSpeculation, guard.isNegated(),
                                                 guard.getNoDeoptSuccessorPosition()));
                                 graph.addBeforeFixed(phi.merge().phiPredecessorAt(i), newGuard);
