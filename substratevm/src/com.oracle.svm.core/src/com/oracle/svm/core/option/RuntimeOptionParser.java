5af6119a55d45b5da90d0b11a49e2183c4d03700
Josef Eisl <josef.eisl@oracle.com>
2026-07-14 14:38:28 +0200

    Update JDK source references
    (cherry picked from commit 4afe0306d202a9cd4191c18a23a4af6b63c07553)

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/option/RuntimeOptionParser.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/option/RuntimeOptionParser.java
@@ -339,7 +339,7 @@ public final class RuntimeOptionParser {
     /// See GR-75297 for work to implement the currently unimplemented options.
     ///
     /// @return the entries in `args` that were not consumed
-    @BasedOnJDKFile("https://github.com/graalvm/labs-openjdk/blob/jvmci-25.1-b18/src/hotspot/share/runtime/arguments.cpp")
+    @BasedOnJDKFile("https://github.com/graalvm/labs-openjdk/blob/jvmci-25.2-b20/src/hotspot/share/runtime/arguments.cpp")
     private static String[] parseJavaVMOptions(String[] args, ParseContext context) {
         int newIdx = 0;
         for (int oldIdx = 0; oldIdx < args.length; oldIdx++) {
