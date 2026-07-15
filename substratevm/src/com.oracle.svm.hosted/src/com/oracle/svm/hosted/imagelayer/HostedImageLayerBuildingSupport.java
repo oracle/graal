galderz/mandrel

Improve debugging on disallowed traits
HostedImageLayerBuildingSupport: Include key/value in DISALLOWED trait
error message for easier debugging.

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/imagelayer/HostedImageLayerBuildingSupport.java
@@ -281,7 +281,7 @@ public final class HostedImageLayerBuildingSupport extends ImageLayerBuildingSup
                     }
                 });
                 traitMap.getTrait(DisallowedSingletonTrait.class).ifPresent(_ -> {
-                    throw VMError.shouldNotReachHere("Singleton with %s trait should never be added to a layered build", SingletonTraitKind.DISALLOWED);
+                    throw VMError.shouldNotReachHere("Singleton with %s trait should never be added to a layered build: key=%s value=%s", SingletonTraitKind.DISALLOWED, key, value);
                 });
             }
             GuestAccess guestAccess = GuestAccess.get();

oracle/graal

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/imagelayer/HostedImageLayerBuildingSupport.java
@@ -283,7 +283,7 @@ public final class HostedImageLayerBuildingSupport extends ImageLayerBuildingSup
                 }
             });
             traitMap.getTrait(DisallowLayeredSingletonTrait.class).ifPresent(_ -> {
-                throw VMError.shouldNotReachHere("Singleton with %s trait should never be added to a layered build", SingletonTraitKind.DISALLOW_LAYERED);
+                throw VMError.shouldNotReachHere("Singleton with %s trait should never be added to a layered build: key=%s value=%s", SingletonTraitKind.DISALLOW_LAYERED, key, value);
             });
         }
         Module singletonModule = value.getClass().getModule();