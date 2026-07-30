cd649a5ecf71aed09403fc510aaa866b2fd84adc
Christian Haeubl <christian.haeubl@oracle.com>
2026-07-30 13:53:39 +0200

    Remove compatibility attribute.

--- sdk/mx.sdk/mx_sdk_benchmark.py
+++ sdk/mx.sdk/mx_sdk_benchmark.py
@@ -875,8 +875,6 @@ class NativeImageVM(StageAwareGraalVm):
         self.graalos = False
         self.graalhost_graalos = False
         self.layered = False
-        # Kept for compatibility with downstream benchmark suites. String inlining is no longer supported.
-        self.use_string_inlining = False
         self.static = False
         self.mostly_static = False
         self.is_llvm = False
