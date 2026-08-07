74710a1fc961b5c0d6f5fb1ee6a3346eae49d21f
Fabio Niephaus <fabio.niephaus@oracle.com>
2026-08-07 13:52:03 +0200

    Introduce `-H:±PreserveIncludesJNI`.

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateOptions.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/SubstrateOptions.java
@@ -1579,6 +1579,9 @@ public class SubstrateOptions {
     public static final HostedOptionKey<AccumulatingLocatableMultiOptionValue.Strings> IgnorePreserveForClasses = new HostedOptionKey<>(
                     AccumulatingLocatableMultiOptionValue.Strings.buildWithCommaDelimiter());
 
+    @Option(help = "Include JNI metadata for preserved types.")//
+    public static final HostedOptionKey<Boolean> PreserveIncludesJNI = new HostedOptionKey<>(true);
+
     @Fold
     public static boolean isForeignAPIEnabled() {
         Boolean value = ConcealedOptions.ForeignAPISupport.getValue();
