3c5d988cf071ccad561cbd614ec7948a2d111db5
Josef Eisl <josef.eisl@oracle.com>
2026-08-12 13:31:05 +0200

    svm: add guest callback bridge annotation

---
+++ substratevm/src/com.oracle.svm.util/src/com/oracle/svm/util/GuestCallbackBridge.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2026, 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.svm.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jdk.graal.compiler.vmaccess.VMAccess;
+
+/**
+ * Marks a host method invoked from the guest context through a host proxy (see
+ * {@link VMAccess#createHostProxy}).
+ * <p>
+ * Users of this annotation must add a Javadoc to each annotated method. The Javadoc
+ * must include an {@code @param} entry for every parameter, documenting the guest
+ * type it represents, and a link to the corresponding guest interface method.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GuestCallbackBridge {
+}
