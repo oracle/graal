b46098155486dafc8634c3dbdbd30d435b911a21
ol-automation_ww <ol-automation_ww@oracle.com>
2026-08-19 00:12:09 +0000

    update JVMCI to 25.0.4.1+1-jvmci-25.3-b23

--- compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/JVMCIVersionCheck.java
+++ compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/hotspot/JVMCIVersionCheck.java
@@ -62,8 +62,8 @@ public final class JVMCIVersionCheck {
     // Checkstyle: stop stable iteration order check
     private static final Map<String, Map<String, Version>> JVMCI_MIN_VERSIONS = Map.of(
                     "25", Map.of(
-                                    ORACLE_VM_VENDOR, createLabsJDKVersion("25.0.4.1+0", "25.3", 21),
-                                    DEFAULT_VENDOR_ENTRY, createLabsJDKVersion("25.0.4.1+0", "25.3", 21)));
+                                    ORACLE_VM_VENDOR, createLabsJDKVersion("25.0.4.1+1", "25.3", 22),
+                                    DEFAULT_VENDOR_ENTRY, createLabsJDKVersion("25.0.4.1+1", "25.3", 22)));
     // Checkstyle: resume stable iteration order check
 
     private static final int NA = 0;
