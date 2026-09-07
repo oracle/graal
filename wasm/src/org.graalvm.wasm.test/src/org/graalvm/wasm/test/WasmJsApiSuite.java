4a0ed838f66279a75d777350a9b6dc0dbc6045f7
Jan Stola <jan.stola@oracle.com>
2026-09-07 14:56:06 +0200

    Test memory without a declared maximum.

--- wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/WasmJsApiSuite.java
+++ wasm/src/org.graalvm.wasm.test/src/org/graalvm/wasm/test/WasmJsApiSuite.java
@@ -1496,6 +1496,16 @@ public class WasmJsApiSuite {
     }
 
     @Test
+    public void testMemoryWithoutDeclaredMaximum() throws IOException {
+        runMemoryTest(context -> {
+            WasmMemory memory = WebAssembly.memAlloc(1, Sizes.NO_MEMORY_MAXIMUM, false);
+            Assert.assertFalse(memory.hasDeclaredMaxSize());
+            Assert.assertEquals(Sizes.NO_MEMORY_MAXIMUM, memory.declaredMaxSize());
+            Assert.assertEquals(1, WebAssembly.memGrow(memory, 1));
+        });
+    }
+
+    @Test
     public void testInitialTableSizeOutOfBounds() throws IOException {
         runTest(context -> {
             final WebAssembly wasm = new WebAssembly(context);
