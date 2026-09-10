b0399a8ce82139e60dd88902899dd901d344cdf0
Yudi Zheng <yudi.zheng@oracle.com>
2026-09-10 20:22:29 +0200

    [GR-79530] Cache Crema invoke signature data

--- substratevm/src/com.oracle.svm.interpreter/src/com/oracle/svm/interpreter/Interpreter.java
+++ substratevm/src/com.oracle.svm.interpreter/src/com/oracle/svm/interpreter/Interpreter.java
@@ -3775,7 +3775,7 @@ public final class Interpreter {
         LinkedInvoke linkedInvoke = getOrLinkInvoke(method, code, curBCI, opcode);
         boolean hasReceiver = opcode != INVOKESTATIC && linkedInvoke.hasReceiver;
         Object appendix = linkedInvoke.appendix;
-        Object[] calleeArgs = virtualStack.popArguments(callerFrame, hasReceiver, linkedInvoke.signature, appendix);
+        Object[] calleeArgs = virtualStack.popArguments(callerFrame, linkedInvoke.argumentKinds, linkedInvoke.argumentCount, appendix);
         if (hasReceiver) {
             Object receiver = calleeArgs[0];
             profileType(methodProfile, curBCI, receiver);
@@ -3790,7 +3790,7 @@ public final class Interpreter {
         }
 
         Object retObj = InterpreterToVM.dispatchInvocation(linkedInvoke.seedMethod, calleeArgs, linkedInvoke.callKind, forceStayInInterpreter, preferStayInInterpreter, false);
-        virtualStack.pushKind(callerFrame, retObj, linkedInvoke.returnKind);
+        virtualStack.pushBasicType(callerFrame, retObj, linkedInvoke.returnKind);
     }
 
     @NeverInline("Keep INVOKEDYNAMIC first-link work out of the bytecode-handler stub")
